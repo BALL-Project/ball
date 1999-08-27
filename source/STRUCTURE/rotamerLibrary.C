4c136f8203f5b98187b350b77f6849856756a9a7
oliver <oliver>
1999-08-27 14:01:41 +0000

    added RotamerLibrary::readSQWRLLibraryFile()

--- source/STRUCTURE/rotamerLibrary.C
+++ source/STRUCTURE/rotamerLibrary.C
@@ -1,4 +1,4 @@
-// $Id: rotamerLibrary.C,v 1.2 1999/08/27 12:09:09 oliver Exp $
+// $Id: rotamerLibrary.C,v 1.3 1999/08/27 14:01:41 oliver Exp $
 
 #include <BALL/STRUCTURE/rotamerLibrary.h>
 #include <BALL/SYSTEM/file.h>
@@ -39,7 +39,7 @@ namespace BALL
 	}
 
 
-	bool RotamerLibrary::readSQWRLLibraryFile(const String& filename)
+	bool RotamerLibrary::readSQWRLLibraryFile(const String& filename, const FragmentDB& fragment_db)
 	{
 		// clear old stuff
 		variants_.clear();
@@ -73,24 +73,51 @@ namespace BALL
 			}
 		}
 		
-		Log.info() << "found " << amino_acids.size() << " amino acids." << endl;
-		
-		
-
 		// build list of variant names from template DB
 		// for each amino acid from the library file
 		vector<String> variant_names;
 		
 		
-		for (Size i = 0; i < variant_names.size() ; i++)
+		HashSet<String>::const_iterator set_it = amino_acids.begin();
+		list<String>::const_iterator list_it;
+		String s[18];
+		String name, variant_name;
+		for (; set_it != amino_acids.end() ; set_it++)
 		{
-			//ResidueRotamerSet rs(template_db.getResidue(variant_names[i]);
-			// for each rotamer entry in file:
-			//   create rotamer
-			//	 Rotamer r(P, chi1, chi2, chi3, chi4);
-			//	 rs.addRotamer(r);
-			//variants_.push_back(rs);
+			name = *set_it;
+			list<String> variant_names = template_db.getVariantNames(name);
+			for (list_it = variant_names.begin(); list_it != variant_names.end(); ++list_it)
+			{
+				ResidueRotamerSet rs(template_db.getResidue(*list_it));
+				for (Size i = 0; i < lines.size(); i++)
+				{
+					if (lines[i](0, 3) == name)
+					{
+						Size number_of_fields = lines[i].split(s, 18);
+						float P = line.getField(7).toFloat() / 100.0;
+						Angle chi1, chi2, chi3, chi4;
+						chi1.set(line.getField(11).toFloat(), false);
+						if (number_of_fields > 13)
+						{
+							chi2.set(line.getField(13).toFloat(), false);
+						}
+						if (number_of_fields > 15)
+						{
+							chi3.set(line.getField(15).toFloat(), false);
+						}
+						if (number_of_fields > 17)
+						{
+							chi4.set(line.getField(17).toFloat(), false);
+						}
+						rs.addRotamer(Rotamer(P, chi1, chi2, chi3, chi4));
+					}
+				}
+				
+				variants_.push_back(rs);
+			}
 		}
+
+		valid_ = true;
 			
 		return true;
 	}
