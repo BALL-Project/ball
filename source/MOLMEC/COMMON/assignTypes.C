92d054ffe140ee29c4d7211e54ef2bfb13af74b4
oliver <oliver>
1999-09-03 14:10:52 +0000

    modified AssignTypeNameProcessor to respect alreday set types

--- source/MOLMEC/COMMON/assignTypes.C
+++ source/MOLMEC/COMMON/assignTypes.C
@@ -1,12 +1,14 @@
-// $Id: assignTypes.C,v 1.1 1999/08/26 08:02:45 oliver Exp $
+// $Id: assignTypes.C,v 1.2 1999/09/03 14:10:52 oliver Exp $
 
 #include <BALL/MOLMEC/COMMON/assignTypes.h>
 #include <iostream.h>
 
-namespace BALL {
+namespace BALL 
+{
 
 	AssignTypeNameProcessor::AssignTypeNameProcessor
-		(const String& filename)
+		(const String& filename, bool overwrite)
+		:	overwrite_(overwrite)
 	{
 		ifstream	type_file(filename.c_str());
 
@@ -29,24 +31,28 @@ namespace BALL {
 
 	Processor::Result	AssignTypeNameProcessor::operator () (Atom& atom)
 	{
-		String name;
-		Fragment* frag = atom.getFragment();
-		if (frag != 0)
+		if (overwrite_ || atom.getTypeName() == BALL_ATOM_DEFAULT_TYPE_NAME)
 		{
-			name = frag->getName().trim();
-		}
-
-		name += ":";
-		name += atom.getName().trim();
-		
-		if (!type_map_.has(name))
-		{
-			if (atom.getTypeName() == BALL_ATOM_DEFAULT_TYPE_NAME)
+			
+			String name;
+			Fragment* frag = atom.getFragment();
+			if (frag != 0)
 			{
-				Log.level(LogStream::WARNING) << "Could not assign atom type name for " << name << endl;
+				name = frag->getName().trim();
 			}
-		} else {
-			atom.setTypeName(type_map_[name]);
+
+			name += ":";
+			name += atom.getName().trim();
+			
+			if (!type_map_.has(name))
+			{
+				if (atom.getTypeName() == BALL_ATOM_DEFAULT_TYPE_NAME)
+				{
+					Log.level(LogStream::WARNING) << "Could not assign atom type name for " << name << endl;
+				}
+			} else {
+				atom.setTypeName(type_map_[name]);
+			}
 		}
 
 		return Processor::CONTINUE;
