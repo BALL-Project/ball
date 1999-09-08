f1c111b0005474de8202b38c8722d621e80bddab
oliver <oliver>
1999-09-08 13:33:13 +0000

    added the evaluation of the BALL_DATA_PATH environment variable

--- source/COMMON/path.C
+++ source/COMMON/path.C
@@ -1,16 +1,18 @@
-// $Id: path.C,v 1.1 1999/08/26 08:02:33 oliver Exp $
+// $Id: path.C,v 1.2 1999/09/08 13:33:13 oliver Exp $
 
 #include <BALL/COMMON/global.h>
 #include <BALL/COMMON/path.h>
 #include <fstream.h>
+#include <stdlib.h>
 
-
-namespace BALL {
-
+namespace BALL 
+{
 	
 	string					Path::path_ = BALL_PATH"/data/";
 	bool						Path::path_array_valid_ = false;
+	bool						Path::environment_checked_ = false;
 	vector<string>	Path::path_array_;		
+	
 	
 	string Path::getDataPath()
 	{
@@ -28,27 +30,44 @@ namespace BALL {
 		return BALL_PATH"/data/";
 	}
 
-	void Path::buildPathArray_(void) 
+	void Path::buildPathArray_() 
 	{	
 		// everything is valid - return directly
 		if (path_array_valid_)
+		{
 			return;
+		}
 
 		// remove old path array
 		path_array_.clear();
+
+		// check whether the environment variable BALL_DATA_PATH is set
+		if (!environment_checked_)
+		{
+			char*	ball_data_path = getenv("BALL_DATA_PATH");
+			if (ball_data_path != 0)
+			{	
+				path_ = string(ball_data_path) + ":" + path_;
+			}
+			
+			// don`t try this again
+			environment_checked_ = true;
+		}
 
 		// segment the path string and insert each path 
 		// into the path array. append slashes where neccessary
 		string tmp = path_ + ":";
 		Size position;
-
-		while ((position = tmp.find(":")) != string::npos) {
+		while ((position = tmp.find(":")) != string::npos) 
+		{
 			// extract the next path...
-			string path = tmp.substr(0, position - 1);
+			string path = tmp.substr(0, position);
 
 			// append a '/' if neccessary (just to be sure...)
 			if (path[path.size() - 1] != '/')
+			{
 				path.append("/");
+			}
 
 			// store the path...
 			path_array_.push_back(path);
@@ -71,7 +90,9 @@ namespace BALL {
 			string tmp = name;
 			tmp.erase(0, tmp.rfind("/") + 1);
 			if (tmp != name)
+			{
 				result = findStrict(tmp);
+			}
 		}
 			
 		// return the path. "" means nothing found
@@ -94,8 +115,10 @@ namespace BALL {
 			ifstream file(filename.c_str());
 			
 			// if the file could be opened, we return its name
-			if (file)
+			if (file)	
+			{
 				return filename;
+			}
 		}
 			
 		// we didn't find anything - return an empty string
