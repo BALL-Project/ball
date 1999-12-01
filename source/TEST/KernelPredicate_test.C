8d6b8ce89fd376ce7ec98bf9457702a597a7263c
oliver <oliver>
1999-12-01 13:23:53 +0000

    new test

---
+++ source/TEST/KernelPredicate_test.C
@@ -0,0 +1,51 @@
+// $Id: KernelPredicate_test.C,v 1.1 1999/12/01 13:23:53 oliver Exp $
+#include <BALL/CONCEPT/classTest.h>
+
+///////////////////////////
+#include <BALL/KERNEL/predicate.h>
+#include <BALL/KERNEL/system.h>
+#include <BALL/KERNEL/molecule.h>
+#include <BALL/KERNEL/atom.h>
+#include <BALL/KERNEL/bond.h>
+#include <BALL/KERNEL/baseFragment.h>
+///////////////////////////
+
+START_TEST(KernelPredicate<T>, "$Id: KernelPredicate_test.C,v 1.1 1999/12/01 13:23:53 oliver Exp $")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+using namespace BALL;
+	
+CHECK(KernelPredicate::operator () (const Composite&) const)
+System S;
+Molecule M;
+Atom A;
+M.insert(A);
+S.insert(M);
+
+KernelPredicate<System>				system_predicate;
+KernelPredicate<Molecule>			molecule_predicate;
+KernelPredicate<Atom>					atom_predicate;
+KernelPredicate<Bond>					bond_predicate;
+KernelPredicate<BaseFragment>	base_fragment_predicate;
+TEST_EQUAL(system_predicate(S), true)
+TEST_EQUAL(system_predicate(M), false)
+TEST_EQUAL(system_predicate(A), false)
+TEST_EQUAL(molecule_predicate(S), false)
+TEST_EQUAL(molecule_predicate(M), true)
+TEST_EQUAL(molecule_predicate(A), false)
+TEST_EQUAL(atom_predicate(S), false)
+TEST_EQUAL(atom_predicate(M), false)
+TEST_EQUAL(atom_predicate(A), true)
+TEST_EQUAL(bond_predicate(S), false)
+TEST_EQUAL(bond_predicate(M), false)
+TEST_EQUAL(bond_predicate(A), false)
+TEST_EQUAL(base_fragment_predicate(S), false)
+TEST_EQUAL(base_fragment_predicate(M), true)
+TEST_EQUAL(base_fragment_predicate(A), false)
+RESULT
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
