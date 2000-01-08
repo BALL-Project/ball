e797f01d1af2edd17eaf87eea6ef0bde4810b688
oliver <oliver>
2000-01-08 20:52:11 +0000

    fixed: added workaround for incorrect linkage occuring with SUNPro CC 6.0ea

--- source/KERNEL/global.C
+++ source/KERNEL/global.C
@@ -1,4 +1,4 @@
-// $Id: global.C,v 1.3 2000/01/08 12:24:09 oliver Exp $
+// $Id: global.C,v 1.4 2000/01/08 20:52:11 oliver Exp $
 
 #include <BALL/KERNEL/global.h>
 #include <BALL/MATHS/vector3.h>
@@ -6,5 +6,8 @@ namespace BALL
 {
 
 	bool clone_bonds = true;
+	
+	// this is a workaround for the SUNPro CC 6.0ea compiler
+	Vector3 BALL_vtable_constructing_vector;
 
 } // namespace BALL
