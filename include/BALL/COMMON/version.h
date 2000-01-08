5218bfbb3bc7d44c58f142190ec62ea2bc3a57f7
oliver <oliver>
2000-01-08 11:59:07 +0000

    version string and numbers set to the first intended alpha release: 0.9a (version policy: version numbers ending with 'a' are alpha releases, with 'b' are beta releases (public) and without terminal letter are official releases

--- include/BALL/COMMON/version.h
+++ include/BALL/COMMON/version.h
@@ -1,10 +1,10 @@
-// $Id: version.h,v 1.1 1999/08/26 07:53:13 oliver Exp $
+// $Id: version.h,v 1.2 2000/01/08 11:59:07 oliver Exp $
 
 #ifndef BALL_COMMON_VERSION_H
 #define BALL_COMMON_VERSION_H
 
-#define BALL_RELEASE                    000902
-#define BALL_RELEASE_STRING             "0.9.2"
+#define BALL_RELEASE                    000900
+#define BALL_RELEASE_STRING             "0.9a"
 #define BALL_MAJOR_RELEASE              0
 #define BALL_MINOR_RELEASE              9
 
