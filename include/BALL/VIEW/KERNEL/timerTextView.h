e7b37b68ec994be0d6db5a24196ac8c613bdce21
hekl <hekl>
2000-01-09 12:37:42 +0000

    qttextview widget: extended with notification; catches logs

---
+++ include/BALL/VIEW/KERNEL/timerTextView.h
@@ -0,0 +1,135 @@
+// $Id: 
+
+#ifndef BALL_VIEW_KERNEL_TIMERTEXTVIEW_H
+#define BALL_VIEW_KERNEL_TIMERTEXTVIEW_H
+
+#ifndef BALL_COMMON_H
+#	include <BALL/common.h>
+#endif
+
+#include <strstream>
+
+#ifndef QAPPLICATION_H
+#	include <qapplication.h>
+#endif
+
+#ifndef QSTRING_H
+#	include <qstring.h>
+#endif
+
+#ifndef QTEXTVIEW_H
+#	include <qtextview.h>
+#endif
+
+#ifndef BALL_CONCEPT_NOTIFICATION_h
+# include <BALL/CONCEPT/notification.h>
+#endif
+
+
+namespace BALL
+{
+	
+	namespace VIEW
+	{
+
+		class TimerTextView
+			: public NotificationTarget<LogStreamNotifier>,
+			  public QTextView
+		{
+			public:
+
+			/**	@name	 Constructors and Destructors	
+			*/
+			//@{
+
+			/** default constructor. creates an instance of TimerTextView.
+			*/
+			TimerTextView(QWidget *parent = 0, const char *name = 0);
+
+			TimerTextView(const TimerTextView& view, bool deep = true);
+
+			/** default destructor. destroys an instance of TimerTextView.
+			*/
+			virtual ~TimerTextView();
+
+			/** clear methode. resets the timer used to control the TimerTextView
+			*/
+			virtual void clear();
+
+			/** destroy methode. resets the timer used to control the TimerTextView
+			*/
+			virtual void destroy();
+			//@}
+
+			/**	@name	Exceptions
+			*/
+			//@{
+			
+			/** Composite Exception. returned if the creation of a received object failed.
+			*/
+			//@}
+
+			/**	@name	Accessors
+			*/
+			//@{
+
+			//@}
+
+			/**	@name	Debugging and Diagnostics
+			*/
+			//@{
+
+			/** returns true if the underlying timer is valid.
+		  */
+			virtual bool isValid() const;
+
+			/** dumps the states.
+			*/
+			virtual void dump(std::ostream& s = std::cout, Size depth = 0) const;
+			//@}
+
+			/**	@name	Storers
+			*/	
+			//@{
+
+			/** Not yet implemented.
+			*/
+			virtual void read(std::istream& s);
+
+			/** Not yet implemented.
+			*/
+			virtual void write(std::ostream& s) const;
+			//@}
+
+			
+			protected:
+
+			/** overridden notify call.
+			*/
+			virtual bool onNotify(LogStreamNotifier &source);
+
+
+			private:
+
+			/** private methodes used for reacting to client requests.
+			*/
+
+
+			/** private storage variables.
+			*/
+			QString history_string_;
+
+			std::strstream strstream_;
+		};
+
+
+
+#		ifndef BALL_NO_INLINE_FUNCTIONS
+#			include <BALL/VIEW/KERNEL/timerTextView.iC>
+#		endif
+
+	}// namespace VIEW
+		
+}// namespace BALL
+
+#endif // BALL_VIEW_KERNEL_TIMERTEXTVIEW_H
---
+++ source/VIEW/KERNEL/timerTextView.C
@@ -0,0 +1,124 @@
+// $Id:
+
+#include <BALL/VIEW/KERNEL/timerTextView.h>
+
+using namespace std;
+
+namespace BALL
+{
+  
+	namespace VIEW
+	{
+  
+		TimerTextView::TimerTextView(QWidget *parent, const char *name)
+			:	NotificationTarget<LogStreamNotifier>(),
+			  QTextView(parent, name),
+				history_string_(),
+				strstream_()
+		{
+			Log.insert(strstream_);
+			Log.insertNotification(strstream_, *this);
+
+			setTextFormat(PlainText);
+		}
+
+		TimerTextView::TimerTextView(const TimerTextView& view, bool deep)
+			:	NotificationTarget<LogStreamNotifier>(),
+			  QTextView(),
+				history_string_(view.history_string_),
+				strstream_()
+		{
+			Log.insert(strstream_);
+			Log.insertNotification(strstream_, *this);
+
+			setTextFormat(PlainText);
+		}
+
+		TimerTextView::~TimerTextView()
+		{
+			#ifdef BALL_VIEW_DEBUG
+				cout << "Destructing object " << (void *)this 
+					<< " of class " << RTTI::getName<TimerTextView>() << endl;
+			#endif 
+
+			Log.remove(strstream_);
+
+			destroy();
+		}
+
+		void TimerTextView::clear()
+		{
+		}
+
+		void TimerTextView::destroy()
+		{
+			clear();
+		}
+
+		bool TimerTextView::isValid() const
+		{
+			return (bool)(true);
+		}
+
+		void TimerTextView::dump
+			(ostream& s, Size depth) const
+		{
+			BALL_DUMP_STREAM_PREFIX(s);
+			
+			BALL_DUMP_DEPTH(s, depth);
+			BALL_DUMP_HEADER(s, this, this);
+
+			BALL_DUMP_STREAM_SUFFIX(s);
+		}
+
+		void TimerTextView::read(istream & /* s */)
+		{
+			throw ::BALL::Exception::NotImplemented(__FILE__, __LINE__);
+		}
+
+		void TimerTextView::write(ostream & /* s */) const
+		{
+			throw ::BALL::Exception::NotImplemented(__FILE__, __LINE__);
+		}
+
+		// main event loop
+	  bool TimerTextView::onNotify(LogStreamNotifier &source)
+		{
+			cerr << "." << endl;
+
+			history_string_ = text();
+			string line;
+			
+			char c;
+			strstream_.get(c);
+
+			while (strstream_.gcount() > 0)
+			{
+				line += c;
+				strstream_.get(c);
+			}
+
+			strstream_.clear();
+
+			if (line.size() > 0)
+			{
+				history_string_ += QString(line.c_str());
+
+				setText(history_string_);
+				scrollBy(0, visibleHeight());
+				repaint();
+
+				qApp->processEvents();
+			}
+
+			return true;
+		}
+
+
+#		ifdef BALL_NO_INLINE_FUNCTIONS
+#			include <BALL/VIEW/KERNEL/timerTextView.iC>
+#		endif 
+
+	} // namespace VIEW
+
+} // namespace BALL
