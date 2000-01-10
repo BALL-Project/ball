7a3c8681ed4d09a949a18e52341ce780d67d7bab
oliver <oliver>
2000-01-10 16:44:33 +0000

    new dialog for selection using the Selector object.

---
+++ source/APPLICATIONS/MOLVIEW/DlgSelector.h
@@ -0,0 +1,27 @@
+/**********************************************************************
+
+	--- Qt Architect generated file ---
+
+	File: DlgSelector.h
+	Last generated: Mon Jan 10 17:41:50 2000
+
+ *********************************************************************/
+
+#ifndef DlgSelector_included
+#define DlgSelector_included
+
+#include "DlgSelectorData.h"
+
+class DlgSelector : public DlgSelectorData
+{
+    Q_OBJECT
+
+public:
+
+    DlgSelector(
+        QWidget *parent = NULL,
+        const char *name = NULL );
+
+    virtual ~DlgSelector();
+};
+#endif // DlgSelector_included
