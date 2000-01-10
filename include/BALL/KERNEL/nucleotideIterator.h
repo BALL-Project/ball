0cc3258ef942a3f9c7ca573f3f32e95104ec6404
oliver <oliver>
2000-01-10 21:29:11 +0000

    added: missing iterator header for nucleotide

---
+++ include/BALL/KERNEL/nucleotideIterator.h
@@ -0,0 +1,81 @@
+// $Id: nucleotideIterator.h,v 1.1 2000/01/10 21:29:11 oliver Exp $
+
+#ifndef BALL_KERNEL_NUCLEOTIDEITERATOR_H
+#define BALL_KERNEL_NUCLEOTIDEITERATOR_H
+
+#ifndef BALL_COMMON_H
+#	include <BALL/common.h>
+#endif
+
+#ifndef BALL_KERNEL_NUCLEOTIDE_H
+#	include <BALL/KERNEL/nucleotide.h>
+#endif
+
+#ifndef BALL_KERNEL_ITERATOR_H
+#	include <BALL/KERNEL/iterator.h>
+#endif
+
+#ifndef BALL_KERNEL_PREDICATE_H
+#	include <BALL/KERNEL/predicate.h>
+#endif
+
+namespace BALL 
+{
+
+	class NucleotideIteratorTraits_
+		: public CompositeIteratorTraits_
+	{
+		public:
+
+		BALL_CREATE(NucleotideIteratorTraits_)
+
+		NucleotideIteratorTraits_()
+			:	CompositeIteratorTraits_()
+		{
+			predicate_ = &RTTI::getDefault<KernelPredicate<Nucleotide> >();
+		}
+			
+		NucleotideIteratorTraits_(const Composite& composite)
+			:	CompositeIteratorTraits_(composite)
+		{
+			predicate_ = &RTTI::getDefault<KernelPredicate<Nucleotide> >();
+		}
+			
+		NucleotideIteratorTraits_(const NucleotideIteratorTraits_& traits, bool /* deep */ = true)
+			:	CompositeIteratorTraits_(traits)
+		{
+		}
+			
+		NucleotideIteratorTraits_& operator =(const NucleotideIteratorTraits_& traits)
+		{
+			CompositeIteratorTraits_::operator=(traits);
+			return *this;
+		}
+
+		void resetPredicate()
+		{
+			predicate_ = &RTTI::getDefault<KernelPredicate<Nucleotide> >();
+		}
+	};
+
+
+
+	typedef BidirectionalIterator
+		<Composite, Nucleotide, Composite::SubcompositeIterator, NucleotideIteratorTraits_>
+		NucleotideIterator;
+
+	typedef ConstBidirectionalIterator
+		<Composite, Nucleotide, Composite::SubcompositeIterator, NucleotideIteratorTraits_>
+		NucleotideConstIterator;
+
+	typedef ReverseBidirectionalIterator
+		<Composite, Nucleotide, Composite::SubcompositeIterator, NucleotideIteratorTraits_>
+		NucleotideReverseIterator;
+
+	typedef ConstReverseBidirectionalIterator
+		<Composite, Nucleotide, Composite::SubcompositeIterator, NucleotideIteratorTraits_>
+		NucleotideConstReverseIterator;
+
+} // namespace BALL
+
+#endif // BALL_KERNEL_NUCLEOTIDEITERATOR_H
