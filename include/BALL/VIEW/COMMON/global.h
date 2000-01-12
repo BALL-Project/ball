867fa60fa9763e89f0a7439ec859ab4b2c86b91f
oliver <oliver>
2000-01-12 00:14:16 +0000

    added: documentation

--- include/BALL/VIEW/COMMON/global.h
+++ include/BALL/VIEW/COMMON/global.h
@@ -27,48 +27,67 @@ namespace BALL
 		*/
 		//@{
 
-		/**     Command enums
-						Use this enums in the client server classes.
+		/**     Command enums.
+						Use these enums in the client server classes.
 						These enums specify the commands the server is able
 						to understand.
 		*/
 		enum Command
 		{
+			///
 			COMMAND__UNKOWN              = 0,
+			///
 			COMMAND__OPEN_SCENE          = 1,
+			///
 			COMMAND__CLOSE_SCENE         = 2,
+			///
 			COMMAND__RESIZE_SCENE        = 3,
+			///
 			COMMAND__SHOW_SCENE          = 4,
+			///
 			COMMAND__HIDE_SCENE          = 5,
+			///
 			COMMAND__DESTROY_SCENE       = 6,
+			///
 			COMMAND__SEND_OBJECT         = 7,
+			///
 			COMMAND__UPDATE_SCENE        = 8,
+			///
 			COMMAND__SET_CREATOR_VALUE   = 9,
+			///
 			COMMAND__GET_CREATOR_VALUE   = 10,
+			///
 			COMMAND__HAS_CREATOR_VALUE   = 11,
+			///
 			COMMAND__SET_PROCESSOR_VALUE = 12,
+			///
 			COMMAND__GET_PROCESSOR_VALUE = 13,
+			///
 			COMMAND__HAS_PROCESSOR_VALUE = 14,
 			
+			///
 			NUMBER_OF_COMMANDS
 		};	
 
-		/**     Range enums
+		/**     Range enum.
 		*/
 		enum Range
 		{
+			///
 			RANGE__ADDRESS = 50
 		};
 
-		/**     Value enums
+		/**     Value enum
 		*/
 		enum Value
 		{
+			///
 			VALUE__INVALID              = -1
 		};
 		//@}
 
 	} // namespace VIEW
+
 } // namespace BALL
 
 #endif
