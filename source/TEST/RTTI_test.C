7f33dea416a88546badcc9d302be684008e272d5
oliver <oliver>
2000-01-13 22:49:09 +0000

    added: several tests

--- source/TEST/RTTI_test.C
+++ source/TEST/RTTI_test.C
@@ -1,4 +1,4 @@
-// $Id: RTTI_test.C,v 1.2 2000/01/03 15:30:00 oliver Exp $
+// $Id: RTTI_test.C,v 1.3 2000/01/13 22:49:09 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,7 +7,7 @@
 
 ///////////////////////////
 
-START_TEST(RTTI, "$Id: RTTI_test.C,v 1.2 2000/01/03 15:30:00 oliver Exp $")
+START_TEST(RTTI, "$Id: RTTI_test.C,v 1.3 2000/01/13 22:49:09 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -49,6 +49,68 @@ TEST_EQUAL(isKindOf<Protein>(*b_ptr), false)
 TEST_EQUAL(isKindOf<BaseFragment>(*b_ptr), true)
 TEST_EQUAL(isKindOf<Residue>(*b_ptr), false)
 RESULT											
+
+CHECK(RTTI::isInstanceOf<>())
+Protein p;
+Protein*			p_ptr(&p);
+Molecule*			m_ptr(&p);
+BaseFragment*	b_ptr(&p);
+TEST_EQUAL(isInstanceOf<Molecule>(*p_ptr), false)
+TEST_EQUAL(isInstanceOf<Molecule>(*m_ptr), false)
+TEST_EQUAL(isInstanceOf<Molecule>(*b_ptr), false)
+TEST_EQUAL(isInstanceOf<Protein>(*p_ptr), true)
+TEST_EQUAL(isInstanceOf<Protein>(*m_ptr), true)
+TEST_EQUAL(isInstanceOf<Protein>(*b_ptr), true)
+TEST_EQUAL(isInstanceOf<BaseFragment>(*p_ptr), false)
+TEST_EQUAL(isInstanceOf<BaseFragment>(*m_ptr), false)
+TEST_EQUAL(isInstanceOf<BaseFragment>(*b_ptr), false)
+TEST_EQUAL(isInstanceOf<Residue>(*p_ptr), false)
+TEST_EQUAL(isInstanceOf<Residue>(*m_ptr), false)
+TEST_EQUAL(isInstanceOf<Residue>(*b_ptr), false)
+Molecule m;
+m_ptr = &m;
+b_ptr = &m;
+TEST_EQUAL(isInstanceOf<Molecule>(*m_ptr), true)
+TEST_EQUAL(isInstanceOf<Molecule>(*b_ptr), true)
+TEST_EQUAL(isInstanceOf<Protein>(*m_ptr), false)
+TEST_EQUAL(isInstanceOf<Protein>(*b_ptr), false)
+TEST_EQUAL(isInstanceOf<BaseFragment>(*m_ptr), false)
+TEST_EQUAL(isInstanceOf<BaseFragment>(*b_ptr), false)
+TEST_EQUAL(isInstanceOf<Residue>(*m_ptr), false)
+TEST_EQUAL(isInstanceOf<Residue>(*b_ptr), false)
+BaseFragment b;
+b_ptr = &b;
+TEST_EQUAL(isInstanceOf<Molecule>(*b_ptr), false)
+TEST_EQUAL(isInstanceOf<Protein>(*b_ptr), false)
+TEST_EQUAL(isInstanceOf<BaseFragment>(*b_ptr), true)
+TEST_EQUAL(isInstanceOf<Residue>(*b_ptr), false)
+RESULT											
+
+CHECK(getDefault<>())
+Protein p;
+p = getDefault<Protein>();
+RESULT
+
+CHECK(getNew<>())
+Protein* p = (Protein*)getNew<Protein>();
+TEST_NOT_EQUAL(p, 0)
+RESULT
+
+CHECK(getName<>())
+// there is not much to check - each damned compiler 
+// tries his own demangling!
+TEST_EQUAL(getName<Protein>(), String("BALL::Protein"))
+RESULT
+
+CHECK(getStreamName<>())
+// there is not much to check - each damned compiler 
+// tries his own demangling!
+TEST_EQUAL(getName<Protein>(), String("BALL::Protein"))
+RESULT
+
+CHECK(castTo<>())
+//BAUSTELLE
+RESULT
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
