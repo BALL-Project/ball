e5622952393776dca98cc2733ec19183c0c50c60
oliver <oliver>
2000-01-14 20:29:51 +0000

    added: easies interface, verbose messages

--- source/EXAMPLES/MOLVIEWClient.C
+++ source/EXAMPLES/MOLVIEWClient.C
@@ -9,21 +9,22 @@ int main(int argc, char** argv)
 {
 	
 	// check for command line arguments
-	if (argc != 4)
+	if (argc != 3)
 	{
-		cerr << argv[0] << " <host> <port> <PDB file>" << endl;
+		cerr << argv[0] << " <host>[:<port>] <PDB file>" << endl;
 		cerr << "  Reads a PDB file and sends its contents via the network" << endl;
 		cerr << "  to the MOLVIEW server listening on <port> of <host> and" << endl;
-		cerr << "  displays the contents of the file there." << endl << endl;
+		cerr << "  displays the contents of the file there." << endl;
+		cerr << "  If no port is given, the default port 20000 is assumed." << endl << endl;
 		return 1;
 	}
 	
 	// open a file for input
-	PDBFile	pdb_file(argv[3]);
+	PDBFile	pdb_file(argv[2]);
 	
 	if (pdb_file.bad())	
 	{
-		cerr << "Cannot open PDB file " << argv[3] << endl;
+		cerr << "Cannot open PDB file " << argv[2] << endl;
 		return 2;
 	}
 
@@ -33,14 +34,31 @@ int main(int argc, char** argv)
 	pdb_file >> S;
 	pdb_file.close();
 
+	// extract the hostname and port from the command line
+	String host_port(argv[1]);
+	String host;
+	Size port;
+	if (host_port.has(':'))
+	{
+		port = String(host_port.after(":")).toUnsignedInt();
+		host = host_port.before(":");
+	} else {
+		host = host_port;
+		port = VIEW_DEFAULT_PORT;
+	}
+	
 	// create a MOLVIEW client and
 	// connect to the given host and port
-	Size port = String(argv[1]).toInt();
-	Client client(argv[2], port);
+	cout << "connecting to port " << port << " of " << host << endl;
+	Client client(argv[1], port);
+
 
 	// transmit the system
+	cout << "sending " << S.countAtoms() << " atoms to " << host << endl;
 	client.getScene().insert(S);
 
+	
 	// that's it.
+	cout << "done." << endl;
 	return 0;
 }
