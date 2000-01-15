43221def8018877015ac14bcd05595246e9e950c
oliver <oliver>
2000-01-15 19:01:34 +0000

    added: more tests (copy constructor)

--- source/TEST/List_test.C
+++ source/TEST/List_test.C
@@ -1,4 +1,4 @@
-// $Id: List_test.C,v 1.1 1999/12/19 18:46:47 oliver Exp $
+// $Id: List_test.C,v 1.2 2000/01/15 19:01:34 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,7 +7,7 @@
 
 ///////////////////////////
 
-START_TEST(List<T>, "$Id: List_test.C,v 1.1 1999/12/19 18:46:47 oliver Exp $")
+START_TEST(List<T>, "$Id: List_test.C,v 1.2 2000/01/15 19:01:34 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -22,6 +22,34 @@ RESULT
 
 CHECK(List<int>::~List())
 delete lst_ptr;
+RESULT
+
+CHECK(List<int>::getSize())
+List<int> int_list;
+TEST_EQUAL(int_list.getSize(), 0)
+int_list.push_back(1);
+TEST_EQUAL(int_list.getSize(), 1)
+int_list.push_back(2);
+TEST_EQUAL(int_list.getSize(), 2)
+int_list.push_back(3);
+TEST_EQUAL(int_list.getSize(), 3)
+int_list.push_back(4);
+TEST_EQUAL(int_list.getSize(), 4)
+RESULT
+
+CHECK(List<int>::List(const List&))
+List<int> int_list;
+int_list.push_back(1);
+int_list.push_back(2);
+int_list.push_back(3);
+int_list.push_back(4);
+List<int> new_list(int_list);
+TEST_EQUAL(new_list.size(), int_list.size())
+TEST_EQUAL(new_list.front(), int_list.front())
+TEST_EQUAL(new_list.back(), int_list.back())
+int_list.clear();
+List<int> new_list2(int_list);
+TEST_EQUAL(new_list2.size(), int_list.size())
 RESULT
 
 /////////////////////////////////////////////////////////////
