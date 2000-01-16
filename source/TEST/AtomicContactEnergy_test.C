c61d9b01c80b542b267af13856792d1373c8f0dc
oliver <oliver>
2000-01-16 17:19:26 +0000

    fixed: changed precision and added more precise values

--- source/TEST/AtomicContactEnergy_test.C
+++ source/TEST/AtomicContactEnergy_test.C
@@ -1,4 +1,4 @@
-// $Id: AtomicContactEnergy_test.C,v 1.1 2000/01/16 16:34:14 oliver Exp $
+// $Id: AtomicContactEnergy_test.C,v 1.2 2000/01/16 17:19:26 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -8,7 +8,7 @@
 
 ///////////////////////////
 
-START_TEST(AtomicContactEnergy, "$Id: AtomicContactEnergy_test.C,v 1.1 2000/01/16 16:34:14 oliver Exp $")
+START_TEST(AtomicContactEnergy, "$Id: AtomicContactEnergy_test.C,v 1.2 2000/01/16 17:19:26 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -35,21 +35,23 @@ pdb_file_B.close();
 TEST_EQUAL(B.countAtoms(), 454)
 
 // calculate the atomic contact energy of A and B
+#undef PRECISION
+#define PRECISION 1e-2
 STATUS("calculating the ACE of A and B")
 float ACE_A = calculateACE(A);
-TEST_REAL_EQUAL(ACE_A, -2222.15)
+TEST_REAL_EQUAL(ACE_A, -2222.1489780506)
 float ACE_B = calculateACE(B);
-TEST_REAL_EQUAL(ACE_B, -562.372)
+TEST_REAL_EQUAL(ACE_B, -562.371947823661)
 
 // join A and B and calculate the total energy
 STATUS("calculating the ACE of A and B")
 A.splice(B);
 float ACE_AB = calculateACE(A);
-TEST_REAL_EQUAL(ACE_AB, -2806.1)
+TEST_REAL_EQUAL(ACE_AB, -2806.09735119048)
 
 // calculate the difference in binding energies
 float dG_bind = ACE_AB - ACE_A - ACE_B;
-TEST_REAL_EQUAL(dG_bind, -21.5764)
+TEST_REAL_EQUAL(dG_bind, -21.5764253162201)
 RESULT											
 
 /////////////////////////////////////////////////////////////
