65f9b5a59056d68dd1e3383428b4d900f0aac5d0
amoll <amoll>
2000-02-23 01:18:02 +0000

    new added test for line3.h

---
+++ source/TEST/Line3_test.C
@@ -0,0 +1,168 @@
+// $Id: Line3_test.C,v 1.1 2000/02/23 01:18:02 amoll Exp $
+#include <BALL/CONCEPT/classTest.h>
+
+///////////////////////////
+
+#	include <BALL/MATHS/vector3.h>
+#	include <BALL/MATHS/line3.h>
+///////////////////////////
+
+START_TEST(class_name, "$Id: Line3_test.C,v 1.1 2000/02/23 01:18:02 amoll Exp $")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+using namespace BALL;
+
+Vector3 v1, v2, v3, v4;
+
+//line37: method TLine3::BALL_CREATE(TLine3<T>)
+CHECK(TLine3::BALL_CREATE(TLine3<T>))
+  //BAUSTELLE
+RESULT
+
+//line57
+CHECK(TLine3::TLine3())
+  Line3* line;
+	line = new Line3();
+	TEST_NOT_EQUAL(line, 0)
+RESULT
+
+//line97
+CHECK(TLine3::~TLine3())
+  Line3* line;
+	line = new Line3();
+	delete line;
+RESULT
+
+Line3 line, line1, line2, line3;
+
+//line57 also test for Line3.get(TVector3<T>& point, const TVector3<T>& vector)
+CHECK(TLine3(const TVector3<T>& point, const TVector3<T>& vector, 
+					 Form form = FORM__PARAMETER))
+	v1 = Vector3(0, 1, 2);
+	v2 = Vector3(3, 4, 5);
+	line = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line.get(v3, v4, Line3::FORM__PARAMETER);
+	TEST_EQUAL(v1, v3)
+	TEST_EQUAL(v2, v4)
+
+	line = Line3(v1, v2, Line3::FORM__TWO_POINTS);
+	line.get(v3, v4, Line3::FORM__TWO_POINTS);
+	TEST_EQUAL(v1, v3)
+	TEST_EQUAL(v2, v4)
+RESULT
+
+//line215: method bool operator ==(const TLine3& line) const 
+CHECK(bool operator ==(const TLine3& line) const )
+	v1 = Vector3(0, 1, 2);
+	v2 = Vector3(0, 4, 2);
+	line2 = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line  = Line3(v1, v2, Line3::FORM__PARAMETER);
+	TEST_EQUAL(line == line2, true)
+	v2 = Vector3(0, 1, 2);
+	line2 = Line3(v1, v2, Line3::FORM__PARAMETER);
+	TEST_EQUAL(line == line2, false)
+RESULT
+
+//line110: method TLine3::swap(TLine3& line)
+CHECK(TLine3::swap(TLine3& line))
+	v1 = Vector3(0, 1, 2);
+	v2 = Vector3(3, 4, 5);
+	v3 = Vector3(6, 7, 8);
+	v4 = Vector3(9, 10, 11);
+	line = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line1 = Line3(v1, v2, Line3::FORM__PARAMETER);
+
+	line2 = Line3(v3, v4, Line3::FORM__PARAMETER);
+	line3 = Line3(v3, v4, Line3::FORM__PARAMETER);
+
+	line.swap(line2);
+	TEST_EQUAL(line, line2)
+	TEST_EQUAL(line1, line3)
+RESULT
+
+
+//line137: method TLine3::set(const TVector3<T>& point, const TVector3<T>& vector, Form form = FORM__PARAMETER)
+CHECK(TLine3::set(const TVector3<T>& point, const TVector3<T>& vector, Form form = FORM__PARAMETER))
+	v1 = Vector3(0, 1, 2);
+	v2 = Vector3(3, 4, 5);
+	line = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line2.set(v1, v2, Line3::FORM__PARAMETER);
+	TEST_EQUAL(line, line2)
+	line = Line3(v1, v2, Line3::FORM__TWO_POINTS);
+	line2.set(v1, v2, Line3::FORM__TWO_POINTS);
+	TEST_EQUAL(line, line2)
+RESULT
+
+
+//line165: method TLine3::get(TLine3& line, bool /* deep */ = true) const 
+CHECK(TLine3::get(TLine3& line, bool /* deep */ = true) const )
+	v1 = Vector3(0, 1, 2);
+	v2 = Vector3(3, 4, 5);
+	line = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line2.get(line);
+	TEST_EQUAL(line, line2)
+RESULT
+
+
+//line180: s.o.
+
+//line201
+CHECK(normalize())
+	v1 = Vector3(0, 0, 0);
+	v2 = Vector3(4, 9, 16);
+	line = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line.normalize();
+	v2.normalize();
+	line.get(v3, v4, Line3::FORM__PARAMETER);
+	TEST_EQUAL(v2, v4)
+RESULT
+
+//line215: s.o.
+
+//line223: method bool operator != (const TLine3& line) const 
+CHECK(bool operator != (const TLine3& line) const )
+	v1 = Vector3(0, 1, 2);
+	v2 = Vector3(0, 3, 2);
+	line  = Line3(v1, v2, Line3::FORM__PARAMETER);
+	line2 = Line3(v1, v2, Line3::FORM__PARAMETER);
+	TEST_EQUAL(line != line2, false)
+	v2 = Vector3(9, 1, 2);
+	line2 = Line3(v1, v2, Line3::FORM__PARAMETER);
+	TEST_EQUAL(line != line2, true)
+RESULT
+
+//line231: method has(const TVector3<T>& point) const 
+CHECK(has(const TVector3<T>& point) const )
+	v1 = Vector3(0, 0, 0);
+	v2 = Vector3(4, 4, 4);
+	line = Line3(v1, v2, Line3::FORM__TWO_POINTS);
+	v1 = Vector3(2, 2, 2);
+	TEST_EQUAL(line.has(v1), true)
+	v1 = Vector3(2, 2, 3);
+	TEST_EQUAL(line.has(v1), false)
+RESULT
+
+//line263: method isValid
+CHECK(isValid() const)
+	TEST_EQUAL(line.isValid(), true)
+RESULT
+
+//line294: method std::istream& operator >> (std::istream &s, TLine3& line)
+CHECK(std::istream& operator >> (std::istream &s, TLine3& line))
+  //BAUSTELLE
+RESULT
+
+//line302: method std::ostream& operator << (std::ostream& s, const TLine3& line)
+CHECK(std::ostream& operator << (std::ostream& s, const TLine3& line))
+  //BAUSTELLE
+RESULT
+
+CHECK(dump(std::ostream& s = std::cout, Size depth = 0) const)
+  //BAUSTELLE
+RESULT
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
