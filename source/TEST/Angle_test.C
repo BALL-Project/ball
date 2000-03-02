14252724f0aadfebdcac8bd0c7aae16f194ea0d7
oliver <oliver>
2000-03-02 22:12:49 +0000

    fixed: ints instead of float constants

--- source/TEST/Angle_test.C
+++ source/TEST/Angle_test.C
@@ -1,4 +1,4 @@
-// $Id: Angle_test.C,v 1.4 2000/03/02 21:23:24 oliver Exp $
+// $Id: Angle_test.C,v 1.5 2000/03/02 22:12:49 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,7 +7,7 @@
 
 ///////////////////////////
 
-START_TEST(class_name, "$Id: Angle_test.C,v 1.4 2000/03/02 21:23:24 oliver Exp $")
+START_TEST(class_name, "$Id: Angle_test.C,v 1.5 2000/03/02 22:12:49 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -378,26 +378,26 @@ RESULT
 //line 244: method TAngle::bool operator <        (const T& val) const 
 CHECK(TAngle::bool operator <   (const T& val) const )
 	b = Angle(1);
-	TEST_EQUAL(b<1, false )
-	TEST_EQUAL(b<2, true )
+	TEST_EQUAL(b < 1.0, false)
+	TEST_EQUAL(b < 2.0, true)
 RESULT
 
 
 //line 246: method TAngle::bool operator <= (const TAngle& angle) const 
 CHECK(TAngle::bool operator <= (const TAngle& angle) const )
 	b = Angle(0);
-	TEST_EQUAL(b<=a, true )
+	TEST_EQUAL(b <= a, true )
 	b = Angle(3);
-	TEST_EQUAL(b<=a, false )
+	TEST_EQUAL(b <= a, false )
 RESULT
 
 
 //line 248: method TAngle::bool operator >=       (const TAngle& angle) const 
 CHECK(TAngle::bool operator >=  (const TAngle& angle) const )
 	b = Angle(4);
-	TEST_EQUAL(b>=a, true )
+	TEST_EQUAL(b >= a, true )
 	b = Angle(0);
-	TEST_EQUAL(b>=a, false )
+	TEST_EQUAL(b >= a, false )
 RESULT
 
 
