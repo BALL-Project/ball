6fb84984a3894eb2fcfc5bbe0a65689cf3f18ab9
amoll <amoll>
2000-03-02 22:21:43 +0000

    fixed some bugs

--- source/TEST/Plane3_test.C
+++ source/TEST/Plane3_test.C
@@ -1,4 +1,4 @@
-// $Id: Plane3_test.C,v 1.2 2000/03/02 18:49:36 amoll Exp $
+// $Id: Plane3_test.C,v 1.3 2000/03/02 22:21:43 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,7 +7,7 @@
 #include <BALL/MATHS/line3.h>
 ///////////////////////////
 
-START_TEST(class_name, "$Id: Plane3_test.C,v 1.2 2000/03/02 18:49:36 amoll Exp $")
+START_TEST(class_name, "$Id: Plane3_test.C,v 1.3 2000/03/02 22:21:43 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -54,37 +54,37 @@ RESULT
 
 //line 449
 CHECK(TPlane3::bool operator == (const TPlane3& plane) const )
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3(v1, v2);
 	p2 = Plane3(v3, v4);
 	TEST_EQUAL(p == p2 , false)
-	v4 = Vector3(4, 5, 6);
+	v4 = Vector3(4.0, 5.0, 6.0);
 	p2 = Plane3(v3, v4);
 	TEST_EQUAL(p == p2 , true)
 RESULT
 
 //line 454
 CHECK(TPlane3::::bool operator != (const TPlane3& plane) const )
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3(v1, v2);
 	p2 = Plane3(v3, v4);
 	TEST_EQUAL(p != p2 , true)
-	v4 = Vector3(4, 5, 6);
+	v4 = Vector3(4.0, 5.0, 6.0);
 	p2 = Plane3(v3, v4);
 	TEST_EQUAL(p != p2 , false)
 RESULT
 
 CHECK(TPlane3::void get(TPlane3 &plane, bool /* deep */ = true) const)
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3(v1, v2);
 	p2 = Plane3(v3, v4);
 	p.get(p2);
@@ -92,10 +92,10 @@ CHECK(TPlane3::void get(TPlane3 &plane, bool /* deep */ = true) const)
 RESULT
 
 CHECK(TPlane3::void set(TPlane3 &plane, bool /* deep */ = true) const)
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3(v1, v2);
 	p2 = Plane3(v3, v4);
 	p.set(p2);
@@ -104,8 +104,8 @@ RESULT
 
 
 CHECK(TPlane3::void set(const TPlane3<T>& point, const TPlane3<T>& normal))
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3();
 	p.set(v3, v4);
 	p2 = Plane3(v3, v4);
@@ -114,10 +114,10 @@ RESULT
 
 
 CHECK(TPlane3::TPlane3& operator = (const TPlane3 &plane))
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3(v1, v2);
 	p2 = p;
 	TEST_EQUAL(p, p2)
@@ -125,68 +125,68 @@ RESULT
 
 
 CHECK(TPlane3::void get(TVector3<T>& point, TVector3<T>& normal) const)
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(4, 5, 5);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(4.0, 5.0, 5.0);
 	p = Plane3(v1, v2);
 	p.get(v3, v4);
-	TEST_EQUAL(v1, v2)
-	TEST_EQUAL(v3, v4)
+	TEST_EQUAL(v1, v3)
+	TEST_EQUAL(v2, v4)
 RESULT
 
 
 CHECK(TPlane3::bool has(const TVector3<T>& point) const)
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
 	p = Plane3(v1, v2);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(111, 211, 113);
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(111.0, 211.0, 113.0);
 	TEST_EQUAL(p.has(v3), true)
 	TEST_EQUAL(p.has(v4), false)
 RESULT
 
 
 CHECK(TPlane3::bool has(const TLine3<T>& line) const)
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(0, 0, 0);
 	Line3 l = Line3(v1, v2);
+	v2 = Vector3(10.0, 11.0, 12.0);
 	p = Plane3(v1, v2);
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(111, 211, 113);
 	TEST_EQUAL(p.has(l), true)
-	v3 = Vector3(1, 2, 3);
-	v4 = Vector3(111, 211, 113);
+
+	v3 = Vector3(1.0, 2.0, 3.0);
+	v4 = Vector3(111.0, 211.0, 113.0);
 	l = Line3(v3, v4);
 	TEST_EQUAL(p.has(l), false)
 RESULT
 
 
 CHECK(TPlane3<T>::swap(TPlane3<T>& plane))
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
-	v3 = Vector3(10, 20, 30);
-	v4 = Vector3(111, 211, 113);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
+	v3 = Vector3(10.0, 20.0, 30.0);
+	v4 = Vector3(111.0, 211.0, 113.0);
 	p = Plane3(v1, v2);
 	p2 = Plane3(v3, v4);
 	p.swap(p2);
-	TEST_EQUAL(p.p, v1)
-	TEST_EQUAL(p.n, v2)
-	TEST_EQUAL(p2.p, v3)
-	TEST_EQUAL(p2.n, v4)
+	TEST_EQUAL(p.p, v3)
+	TEST_EQUAL(p.n, v4)
+	TEST_EQUAL(p2.p, v1)
+	TEST_EQUAL(p2.n, v2)
 RESULT
 
 
 //line 179: method TPlane3::normalize()
 CHECK(TPlane3::normalize())
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 9, 16);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 9.0, 16.0);
 	p = Plane3(v1, v2);
 	p.normalize();
-	float erg = sqrt (4 *4 + 9 * 9 + 16 * 16);
-	TEST_REAL_EQUAL(p.n[0], 4 / erg)
-	TEST_REAL_EQUAL(p.n[1], 9 / erg)
-	TEST_REAL_EQUAL(p.n[2], 16 / erg)
+	float erg = sqrt (4 * 4 + 9 * 9 + 16 * 16);
+	TEST_REAL_EQUAL(p.n[0], 4.0 / erg)
+	TEST_REAL_EQUAL(p.n[1], 9.0 / erg)
+	TEST_REAL_EQUAL(p.n[2], 16.0 / erg)
 	v2 = Vector3(0, 0, 0);
 	p = Plane3(v1, v2);
 	TEST_EXCEPTION(Exception::DivisionByZero, p.normalize())
@@ -195,11 +195,11 @@ RESULT
 
 //line 197: method hessify()
 CHECK(hessify())
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 9, 16);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 9.0, 16.0);
 	p = Plane3(v1, v2);
 	p.normalize();
-	float erg = sqrt (4 *4 + 9 * 9 + 16 * 16);
+	float erg = sqrt (4 * 4 + 9 * 9 + 16 * 16);
 	TEST_REAL_EQUAL(p.n[0], 4 / erg)
 	TEST_REAL_EQUAL(p.n[1], 9 / erg)
 	TEST_REAL_EQUAL(p.n[2], 16 / erg)
@@ -216,8 +216,8 @@ RESULT
 //line 282: method std::istream& operator >> (std::istream& s, TPlane3& plane)
 CHECK(std::istream& operator >> (std::istream& s, TPlane3& plane))
 	std::ifstream instr("data/Plane3_test2.txt");
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
 	p = Plane3();
 	instr >> p;
 	instr.close();
@@ -229,8 +229,8 @@ RESULT
 //line 
 NEW_TMP_FILE(filename)
 CHECK(std::ostream& operator << (std::ostream& s, const TPlane3<T>& plane))
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
 	p = Plane3(v1, v2);
 	std::ofstream outstr(filename.c_str(), std::ios::out);
 	outstr << p;
@@ -240,8 +240,8 @@ RESULT
 
 
 CHECK(TPlane3::dump(std::ostream& s = std::cout, Size depth = 0) const )
-	v1 = Vector3(1, 2, 3);
-	v2 = Vector3(4, 5, 6);
+	v1 = Vector3(1.0, 2.0, 3.0);
+	v2 = Vector3(4.0, 5.0, 6.0);
 	p = Plane3(v1, v2);
 	String filename;
 	NEW_TMP_FILE(filename)
