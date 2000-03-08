16a4ebecd665e7478da6ad99d7a24c1735b530a6
amoll <amoll>
2000-03-08 18:29:20 +0000

    fixed bugs with TEST_REAL_EQUAL(bool, bool) added test for In- and Output operators

--- source/TEST/Box3_test.C
+++ source/TEST/Box3_test.C
@@ -1,4 +1,4 @@
-// $Id: Box3_test.C,v 1.4 2000/03/02 22:11:32 oliver Exp $
+// $Id: Box3_test.C,v 1.5 2000/03/08 18:29:20 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -6,7 +6,7 @@
 #	include <BALL/MATHS/vector3.h>
 ///////////////////////////
 
-START_TEST(class_name, "$Id: Box3_test.C,v 1.4 2000/03/02 22:11:32 oliver Exp $")
+START_TEST(class_name, "$Id: Box3_test.C,v 1.5 2000/03/08 18:29:20 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -16,16 +16,16 @@ using namespace BALL;
 
 //line 33: method TBox3::BALL_CREATE(TBox3<T>)
 CHECK(TVector3::BALL_CREATE(TBox3<T>))
-	Vector3 v1(1, 2, 3), v2(1, 2, 3);
+	Vector3 v1(1.0, 2.0, 3.0), v2(1.0, 2.0, 3.0);
 	Vector3 v0;
 	Box3 v(v1, v2);
 	Box3* v_ptr = (Box3*)v.create(false, true);
-	TEST_REAL_EQUAL(v_ptr->a == v0, true)
-	TEST_REAL_EQUAL(v_ptr->b == v0, true)
+	TEST_EQUAL(v_ptr->a == v0, true)
+	TEST_EQUAL(v_ptr->b == v0, true)
 	delete v_ptr;
 	v_ptr = (Box3*)v.create();
-	TEST_REAL_EQUAL(v_ptr->a == v1, true)
-	TEST_REAL_EQUAL(v_ptr->b == v2, true)
+	TEST_EQUAL(v_ptr->a == v1, true)
+	TEST_EQUAL(v_ptr->b == v2, true)
 	delete v_ptr;
 RESULT
 
@@ -38,12 +38,16 @@ RESULT
 
 Box3 box, box2;
 Vector3 v1, v2, v3, v4;
-float a = 1, b = 2, c = 3,
-			d = 6, e = 5, f = 4;
+float a = 1.0, b = 2.0, c = 3.0,
+			d = 6.0, e = 5.0, f = 4.0;
 float a1, b1, c1, d1, e1, f1;
 
-v1 = Vector3(1, 2, 3);
-v2 = Vector3(6, 5, 4);
+v1 = Vector3(1.0, 2.0, 3.0);
+v2 = Vector3(6.0, 5.0, 4.0);
+
+String filename;
+using std::ofstream;
+using std::ios;
 
 //line 246: method TBox3::TBox3(const T& ax, const T& ay, const T& az, const T& bx, const T& by, const T& bz)
 //also test for TBox3::get(T& ax, T& ay, T& az, T& bx, T& by, T& bz) const 
@@ -89,43 +93,43 @@ RESULT
 //line 147: method TBox3::getSurface() const 
 CHECK(TBox3::getSurface() const )
 	box = Box3(v1, v2);
-	TEST_REAL_EQUAL(box.getSurface(), 46)
+	TEST_REAL_EQUAL(box.getSurface(), 46.0)
 RESULT
 
 
 //line 152: method TBox3::getVolume() const 
 CHECK(TBox3::getVolume() const )
 	box = Box3(v1, v2);
-	TEST_REAL_EQUAL(box.getVolume(), 15)
+	TEST_REAL_EQUAL(box.getVolume(), 15.0)
 RESULT
 
 
 //line 157: method TBox3::getWidth() const 
 CHECK(TBox3::getWidth() const )
 	box = Box3(v1, v2);
-	TEST_REAL_EQUAL(box.getWidth(), 5)
+	TEST_REAL_EQUAL(box.getWidth(), 5.0)
 RESULT
 
 
 //line 162: method TBox3::getHeight() const 
 CHECK(TBox3::getHeight() const )
 	box = Box3(v1, v2);
-	TEST_REAL_EQUAL(box.getHeight(), 3)
+	TEST_REAL_EQUAL(box.getHeight(), 3.0)
 RESULT
 
 
 //line 167: method TBox3::getDepth() const 
 CHECK(TBox3::getDepth() const )
 	box = Box3(v1, v2);
-	TEST_REAL_EQUAL(box.getDepth(), 1)
+	TEST_REAL_EQUAL(box.getDepth(), 1.0)
 RESULT
 
 
 //line 174: method TBox3::join(const TBox3& box)
 CHECK(TBox3::join(const TBox3& box))
 	box = Box3(v1, v2);
-	v3 = Vector3(101, 102, 103);
-	v4 = Vector3(104, 105, 106);
+	v3 = Vector3(101.0, 102.0, 103.0);
+	v4 = Vector3(104.0, 105.0, 106.0);
 	box2 = Box3(v3, v4);
 	box.join(box2);
 	box.get(a1, b1, c1, d1, e1, f1);
@@ -182,7 +186,7 @@ RESULT
 //line 272: method TBox3::set(const T& ax, const T& ay, const T& az, const T& bx, const T& by, const T& bz)
 CHECK(TBox3::set(const T& ax, const T& ay, const T& az, const T& bx, const T& by, const T& bz))
 	box = Box3(v1, v2);
-	box2.set(1, 2, 3, 6, 5, 4);
+	box2.set(1.0, 2.0, 3.0, 6.0, 5.0, 4.0);
 	TEST_EQUAL(box == box2, true)
 RESULT
 
@@ -206,7 +210,7 @@ RESULT
 
 //line 424: method TBox3<T>::dump(std::ostream& s, Size depth) const
 CHECK(TVector3::dump(std::ostream& s = std::cout, Size depth = 0) const )
-	Box3 v(1, 2, 3, 4, 5, 6);
+	Box3 v(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
   String filename;
 	NEW_TMP_FILE(filename)
 	std::ofstream outfile(filename.c_str(), std::ios::out);
@@ -215,7 +219,6 @@ CHECK(TVector3::dump(std::ostream& s = std::cout, Size depth = 0) const )
 	TEST_FILE(filename.c_str(), "data/Box3_test.txt", true)
 RESULT
 
-/* not (yet?) included in Box3.h
 //line 
 CHECK(std::istream& operator >> (std::istream& s, TBox3<T>& Box3))
 	std::ifstream instr("data/Box3_test2.txt");
@@ -236,7 +239,7 @@ CHECK(std::ostream& operator << (std::ostream& s, const TBox3<T>& Box3))
 	outstr.close();
 	TEST_FILE(filename.c_str(), "data/Box3_test2.txt", false)
 RESULT
-*/
+
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 END_TEST
