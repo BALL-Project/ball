0d69a3bbff97399ef71d94a63ff49fa8ec64cfe5
amoll <amoll>
2000-03-21 12:38:54 +0000

    added test for getSpatProduct(v, v1, v2)

--- source/TEST/Vector3_test.C
+++ source/TEST/Vector3_test.C
@@ -1,4 +1,4 @@
-// $Id: Vector3_test.C,v 1.20 2000/03/17 13:47:03 amoll Exp $
+// $Id: Vector3_test.C,v 1.21 2000/03/21 12:38:54 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -9,7 +9,7 @@
 #include <BALL/MATHS/angle.h>
 ///////////////////////////
 
-START_TEST(TVector3, "$Id: Vector3_test.C,v 1.20 2000/03/17 13:47:03 amoll Exp $")
+START_TEST(TVector3, "$Id: Vector3_test.C,v 1.21 2000/03/21 12:38:54 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -558,6 +558,14 @@ CHECK(TVector3 operator * (const TVector3<T>& vector, const T& scalar))
 	TEST_REAL_EQUAL(v[1], 4.0)
 	TEST_REAL_EQUAL(v[2], 6.0)
 RESULT
+
+CHECK(T getSpatProduct (const TVector3<T>& a, const TVector3<T>& b, const TVector3<T>& c))
+ 	v = Vector3(1.0, 2.0, 3.0);
+ 	v1 = Vector3(2.0, 4.0, 6.0);
+ 	v2 = Vector3(3.0, 6.0, 9.0);
+	TEST_REAL_EQUAL(v.getSpatProduct(v, v1, v2), 0)
+RESULT
+
 
 //line 
 CHECK(std::istream& operator >> (std::istream& s, TVector3<T>& vector))
