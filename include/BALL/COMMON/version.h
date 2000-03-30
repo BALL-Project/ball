f138d00fa971f1da839eeb66991ba3db5fae0452
oliver <oliver>
2000-03-30 19:50:21 +0000

    changed: next release is 0.95a

--- include/BALL/COMMON/version.h
+++ include/BALL/COMMON/version.h
@@ -1,10 +1,10 @@
-// $Id: version.h,v 1.2 2000/01/08 11:59:07 oliver Exp $
+// $Id: version.h,v 1.3 2000/03/30 19:50:21 oliver Exp $
 
 #ifndef BALL_COMMON_VERSION_H
 #define BALL_COMMON_VERSION_H
 
-#define BALL_RELEASE                    000900
-#define BALL_RELEASE_STRING             "0.9a"
+#define BALL_RELEASE                    000950
+#define BALL_RELEASE_STRING             "0.95a"
 #define BALL_MAJOR_RELEASE              0
 #define BALL_MINOR_RELEASE              9
 
