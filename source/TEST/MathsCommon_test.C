a4145a8337ba93fdc22e26fcdd6de361319c9a4c
amoll <amoll>
2000-04-03 09:45:13 +0000

    fixed: test for compare()

--- source/TEST/MathsCommon_test.C
+++ source/TEST/MathsCommon_test.C
@@ -1,4 +1,4 @@
-// $Id: MathsCommon_test.C,v 1.1 2000/04/03 07:18:34 oliver Exp $
+// $Id: MathsCommon_test.C,v 1.2 2000/04/03 09:45:13 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,7 +7,7 @@
 
 ///////////////////////////
 
-START_TEST(class_name, "$Id: MathsCommon_test.C,v 1.1 2000/04/03 07:18:34 oliver Exp $")
+START_TEST(class_name, "$Id: MathsCommon_test.C,v 1.2 2000/04/03 09:45:13 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -168,14 +168,14 @@ RESULT
 
 
 CHECK(compare(const T1& a, const T2& b))
-	TEST_EQUAL(Maths::compare(-0.1, -0.1), 0)
-	TEST_EQUAL(Maths::compare(-0.11, -0.1), -1)
-	TEST_EQUAL(Maths::compare(-0.11, -0.12), 1)
+	TEST_EQUAL(Maths::compare(-0.1, -0.1), 0.0)
+	TEST_EQUAL(Maths::compare(-0.11, -0.1), -1.0)
+	TEST_EQUAL(Maths::compare(-0.11, -0.12), 1.0)
 RESULT
 
 
 CHECK(isNear(const T1& a, const T2& b, const T3& maxDiff))
-	TEST_EQUAL(Maths::isNear(-0.1, -0.1, 0), true)
+	TEST_EQUAL(Maths::isNear(-0.1, -0.1, 0.0), true)
 	TEST_EQUAL(Maths::isNear(-0.1, -0.11, 0.01), true)
 	TEST_EQUAL(Maths::isNear(-0.1, -0.12, 0.01), false)
 RESULT						
