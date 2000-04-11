8cb33ba8dc487fa93073de0778e53c236cf7d72d
amoll <amoll>
2000-04-11 15:26:04 +0000

    added commentary

--- include/BALL/KERNEL/molecule.h
+++ include/BALL/KERNEL/molecule.h
@@ -1,4 +1,4 @@
-// $Id: molecule.h,v 1.3 2000/03/30 10:16:07 oliver Exp $
+// $Id: molecule.h,v 1.4 2000/04/11 15:26:04 amoll Exp $
 
 #ifndef BALL_KERNEL_MOLECULE_H
 #define BALL_KERNEL_MOLECULE_H
@@ -47,16 +47,16 @@ namespace BALL
 		/**	@name	Constructors and Destructors */
 		//@{
 
-		///	Default constructor
+		/**	Default constructor */
 		Molecule();
 	
-		/// Copy constructor
+		/** Copy constructor */
 		Molecule(const Molecule& molecule, bool deep = true);
 	
-		/// Detailled constructor
+		/** Detailled constructor */
 		Molecule(const String& name);
 
-		/// Destructor
+		/** Destructor */
 		virtual ~Molecule();
 
 		/** Clears the contents of the molecule and removes its comopsite children.
@@ -74,10 +74,16 @@ namespace BALL
 		/** @name Persistence */
 		//@{
 
-		///
+		/**	Persistent writing.
+				Writes a Molecule object to a persistent stream.
+				@param pm the persistence manager
+		*/
 		void persistentWrite(PersistenceManager& pm, const char* name = 0) const;
 
-		///
+		/**	Persistent reading.
+				Reads a Molecule object from a persistent stream.
+				@param pm the persistence manager
+		*/
 		void persistentRead(PersistenceManager& pm);
 
 		//@}
@@ -86,16 +92,28 @@ namespace BALL
 		/**	@name	Assignment */
 		//@{
 
-		///
+		/**	Assign from another Molecule.
+				@param molecule	the Molecule object to assign from
+				@param deep
+		*/
 		void set(const Molecule& molecule, bool deep = true);
 
-		///
+		/**	Assignment operator.
+				Assign the components from another Molecule.
+				@param molecule the Molecule to assign from
+		**/
 		Molecule& operator = (const Molecule& molecule);
 
-		///
+		/**	Assign to another Molecule.
+				Assigns the components to another Molecule.
+				@param molecule	the Molecule to be assigned to
+				@param deep
+		*/
 		void get(Molecule& molecule, bool deep = true) const;
 
-		///
+		/**	Swap the contents of two Molecules.
+				@param	molecule the Molecule to swap contents with
+		*/
 		void swap(Molecule& molecule);
 	
 		//@}
@@ -104,19 +122,29 @@ namespace BALL
 		/**	@name	Accessors */
 		//@{
 
-		///
+		/**	Access the superior System.
+				@return	System* pointer to the superior System
+		*/
 		System* getSystem();
 
-		///
+		/**	Get a const pointer to the superior System.
+				@return	System* pointer to the superior System
+		*/
 		const System* getSystem() const;
 
-		///
+		/** Insert an atom as the first child of the instance
+				@param atom the atom to add
+		*/
 		void prepend(Atom& atom);
 
-		///
+		/** Insert an atom as the last child of the instance
+				@param atom the atom to add
+		*/
 		void append(Atom& atom);
 
-		///
+		/** Insert an atom as the last child of the instance
+				@param atom the atom to add
+		*/
 		void insert(Atom& atom);
 
 		///
@@ -125,16 +153,24 @@ namespace BALL
 		///
 		void insertAfter(Atom& atom, Composite& after);
 
-		///
+		/** Remove an Atom
+				@param atom the Atom to remove
+		*/
 		bool remove(Atom& atom);
 
-		///
+		/** Insert a BaseFragment as the first child of the instance
+				@param base_fragment the BaseFragment to add
+		*/
 		void prepend(BaseFragment& base_fragment);
 
-		///
+		/** Insert a BaseFragment as the last child of the instance
+				@param base_fragment the BaseFragment to add
+		*/
 		void append(BaseFragment& base_fragment);
 
-		///
+		/** Insert a BaseFragment as the last child of the instance
+				@param base_fragment the BaseFragment to add
+		*/
 		void insert(BaseFragment& base_fragment);
 
 		///
@@ -152,7 +188,9 @@ namespace BALL
 		///
 		void splice(BaseFragment& base_fragment);
 
-		///
+		/** Remove a BaseFragment
+				@param base_fragment the BaseFragment to remove
+		*/
 		bool remove(BaseFragment& base_fragment);
 
 		//@}
@@ -161,7 +199,9 @@ namespace BALL
 		/**	@name Debugging and Diagnostics */
 		//@{
 
-		///
+		/**	Test if instance is valid.
+				@return bool
+		*/
 		virtual bool isValid() const;
 
 		///
@@ -173,10 +213,14 @@ namespace BALL
 		/**	@name	Storers */
 		//@{
 
-		///
+		/**	Read the components of a molecule from an instream.
+				@param s the instream
+		*/
 		virtual void read(std::istream& s);
 
-		///
+		/**	Write the components of a molecule to an outstream.
+				@param s the outstream
+		*/
 		virtual void write(std::ostream& s) const;
 		
 		//@}
