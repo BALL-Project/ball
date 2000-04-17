21469e3e59eef32e92dd9ab2eca216b1fef350ae
amoll <amoll>
2000-04-17 20:52:17 +0000

    fixed: a problem about the non-const get-functions

--- include/BALL/KERNEL/atom.h
+++ include/BALL/KERNEL/atom.h
@@ -1,4 +1,4 @@
-// $Id: atom.h,v 1.17 2000/04/17 20:38:56 oliver Exp $
+// $Id: atom.h,v 1.18 2000/04/17 20:50:09 amoll Exp $
 
 #ifndef BALL_KERNEL_ATOM_H
 #define BALL_KERNEL_ATOM_H
@@ -82,9 +82,9 @@ namespace BALL
 			\end{itemize}
 			
 			@memo    Atom class (BALL kernel framework)
-			@author  $Author: oliver $
-			@version $Revision: 1.17 $
-			@date    $Date: 2000/04/17 20:38:56 $
+			@author  $Author: amoll $
+			@version $Revision: 1.18 $
+			@date    $Date: 2000/04/17 20:50:09 $
 	*/
 	class Atom
 		: public Composite,
@@ -493,6 +493,14 @@ namespace BALL
 			*/
 			void setPosition(const Vector3& position);
 
+			/** Mutable inspection of the atom's position vector.
+					Access a muteable reference to the position vector of {\em *this} atom.
+
+					@return      Vector3& - muteable reference to the position vector of {\em *this} atom
+					@see         Atom::setPosition
+			*/
+			Vector3& getPosition();
+
 			/** Constant inspection of the atom's position vector.
 					Access a constant reference to the position vector of {\em *this} atom.
 
@@ -500,14 +508,6 @@ namespace BALL
 					@see         Atom::setPosition
 			*/
 			const Vector3& getPosition() const;
-
-			/** Mutable inspection of the atom's position vector.
-					Access a mutable reference to the position vector of {\em *this} atom.
-
-					@return      Vector3& - constant reference to the position vector of {\em *this} atom
-					@see         Atom::setPosition
-			*/
-			Vector3& getPosition();
 
 			/** Change of the atom's radius.
 					Change the radius of {\em *this} atom to {\em radius}.
@@ -578,6 +578,15 @@ namespace BALL
 			*/
 			void setForce(const Vector3& force);
 
+			/** Mutable inspection of the atom's force vector.
+					Access a mutable reference to the force vector of {\em *this} atom.
+					BALL uses units of {\em Newton} (1 N = 1 J/m) as the unit of force.
+
+					@return      Vector3& - mutable reference to the force vector of {\em *this} atom
+					@see         Atom::setForce
+			*/
+			Vector3& getForce();
+
 			/** Constant inspection of the atom's force vector.
 					Access a constant reference to the force vector of {\em *this} atom.
 					BALL uses units of {\em Newton} (1 N = 1 J/m) as the unit of force.
@@ -586,15 +595,6 @@ namespace BALL
 					@see         Atom::setForce
 			*/
 			const Vector3& getForce() const;
-
-			/** Mutable inspection of the atom's force vector.
-					Access a mutable reference to the force vector of {\em *this} atom.
-					BALL uses units of {\em Newton} (1 N = 1 J/m) as the unit of force.
-
-					@return      Vector3& - constant reference to the force vector of {\em *this} atom
-					@see         Atom::setForce
-			*/
-			Vector3& getForce();
 
 			/** Constant inspection of the atom's number of bonds.
 					Access the number of bonds of {\em *this} atom.
