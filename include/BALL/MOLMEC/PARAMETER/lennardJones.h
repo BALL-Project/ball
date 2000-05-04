5c1699f5a27d27a365f92c0ce0d44e3693067bf2
oliver <oliver>
2000-05-04 21:01:28 +0000

    fixed: bug in LaTeX doc

--- include/BALL/MOLMEC/PARAMETER/lennardJones.h
+++ include/BALL/MOLMEC/PARAMETER/lennardJones.h
@@ -1,4 +1,4 @@
-// $Id: lennardJones.h,v 1.7 2000/02/16 19:04:35 oliver Exp $
+// $Id: lennardJones.h,v 1.8 2000/05/04 21:01:28 oliver Exp $
 // Molecular Mechanics Parameter: class describing the atom type section of a parameter file
  
 #ifndef BALL_MOLMEC_PARAMETER_LENNARDJONES_H
@@ -26,10 +26,12 @@ namespace BALL
 
 			If the Slater-Kirkwood format is used, the values for $A_{ij}$ and $B_{ij}$ are calculated
 			as follows (see e.g. Brooks et al., J. Comput. Chem, 4(2):187-217 (1983))
-			\[	
+			\[
+				\begin{aligned}
 				B_{ij} & = & \frac{3}{2} \left(\frac{1}{4 \pi \varepsilon_0}\right)^\frac{1}{2}
 						\frac{e \hbar \sqrt{m_e} \alpha_i \alpha_j}{\sqrt{\frac{\alpha_i}{N_i} + \sqrt{\frac{\alpha_j}{N_j}}}}\\
 				A_{ij} & = & \frac{1}2{} B_{ij} (R_i + R_j)^6
+				\end{aligned}
 			\]
 	*/
 	class LennardJones 
