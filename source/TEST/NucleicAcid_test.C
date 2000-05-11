01788bbcdd572731883098cbbc228521f65a3cc5
amoll <amoll>
2000-05-11 13:34:51 +0000

    added some tests

--- source/TEST/NucleicAcid_test.C
+++ source/TEST/NucleicAcid_test.C
@@ -1,97 +1,214 @@
-// $Id: NucleicAcid_test.C,v 1.2 2000/01/10 15:51:17 oliver Exp $
+// $Id: NucleicAcid_test.C,v 1.3 2000/05/11 13:34:51 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
 #include <BALL/KERNEL/nucleicAcid.h>
 ///////////////////////////
 
-START_TEST(NucleicAcid, "$Id: NucleicAcid_test.C,v 1.2 2000/01/10 15:51:17 oliver Exp $")
+START_TEST(NucleicAcid, "$Id: NucleicAcid_test.C,v 1.3 2000/05/11 13:34:51 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 
-NucleicAcid* na;
-CHECK(NucleicAcid::NucleicAcid())
-na = new NucleicAcid;
-TEST_NOT_EQUAL(na, 0)
+NucleicAcid*	r;
+CHECK(NucleicAcid())
+	r = new NucleicAcid;
+	TEST_NOT_EQUAL(r, 0)
 RESULT
 
-CHECK(NucleicAcid::~NucleicAcid())
-delete na;
-RESULT											
-
-CHECK(NucleicAcid::NucleicAcid(const NucleicAcid&, bool))
-//BAUSTELLE
+CHECK(~NucleicAcid())
+	delete r;
 RESULT
 
-CHECK(NucleicAcid::NucleicAcid(constr String&, const String&)
-//BAUSTELLE
+CHECK(NucleicAcid(NucleicAcid&, bool))
+	NucleicAcid* na1 = new NucleicAcid;
+	na1->setName("testname");
+	Nucleotide a("a");
+	na1->insert(a);
+	NucleicAcid* na2 = new NucleicAcid(*na1, true);
+	TEST_NOT_EQUAL(na2, 0)
+	if (na2 != 0)
+	{
+		TEST_EQUAL(na2->getName(), "testname")
+		TEST_EQUAL(na2->getNucleotide(0)->getName(), "a")
+		delete na2;
+	}
+	na2 = new NucleicAcid(*na1, false);
+	TEST_NOT_EQUAL(na2, 0)
+	if (na2 != 0)
+	{
+		TEST_EQUAL(na2->getName(), "testname")
+		delete na2;
+	}
+	delete na1;
+RESULT
+
+CHECK(NucleicAcid(const String& name,
+			  			const String& id = BALL_NUCLEICACID_DEFAULT_ID))
+	NucleicAcid* na1 = new NucleicAcid("na1", "id");
+	TEST_NOT_EQUAL(na1, 0)
+	if (na1 != 0)
+	{
+		TEST_EQUAL(na1->getName(), "na1")
+		TEST_EQUAL(na1->getID(), "id")
+		delete na1;
+	}
+	NucleicAcid* na2 = new NucleicAcid("na1");
+	TEST_NOT_EQUAL(na2, 0)
+	if (na2 != 0)
+	{
+		TEST_EQUAL(na2->getName(), "na1")
+		TEST_EQUAL(na2->getID(), "")
+		delete na2;
+	}
 RESULT
 
 CHECK(NucleicAcid::clear())
-//BAUSTELLE
+	NucleicAcid na("na1", "id");
+	Nucleotide a("a");
+	na.insert(a);
+	na.clear();
+	TEST_EQUAL(na.countNucleotides(), 0)
+	TEST_EQUAL(na.getID(), "")
 RESULT
 
 CHECK(NucleicAcid::destroy())
-//BAUSTELLE
+	NucleicAcid na("na1", "id");
+	Nucleotide a("a");
+	na.insert(a);
+	na.destroy();
+	TEST_EQUAL(na.countNucleotides(), 0)
+	TEST_EQUAL(na.getID(), "")
 RESULT
 
-CHECK(NucleicAcid::persistentWrite())
-//BAUSTELLE
+CHECK(NucleicAcid::set(const NucleicAcid& NucleicAcid, bool deep = true))
+	NucleicAcid na1("na1");
+	Nucleotide a("a");
+	na1.insert(a);
+	NucleicAcid na2("na2");
+	na2.set(na1, false);
+	TEST_EQUAL(na2.getName(), "na1")
+	TEST_EQUAL(na2.countNucleotides(), 0)
+	na2.setName("a");
+	na2.set(na1);
+	TEST_EQUAL(na2.getName(), "na1")
+	TEST_EQUAL(na2.countNucleotides(), 1)
 RESULT
 
-CHECK(NucleicAcid::persistentRead())
-//BAUSTELLE
+CHECK(NucleicAcid::NucleicAcid& operator = (const NucleicAcid& NucleicAcid))
+	NucleicAcid na1("na1");
+	Nucleotide a("a");
+	na1.insert(a);
+	NucleicAcid na2("na2");
+	na2 = na1;
+	TEST_EQUAL(na2.getName(), "na1")
+	TEST_EQUAL(na2.countNucleotides(), 1)
 RESULT
 
-CHECK(NucleicAcid::set(const NucleicAcid&, bool))
-//BAUSTELLE
-RESULT
-
-CHECK(NucleicAcid::operator = (const NucleicAcid&))
-//BAUSTELLE
-RESULT
-
-CHECK(NucleicAcid::get(NucleicAcid&, bool))
-//BAUSTELLE
+CHECK(NucleicAcid::get(NucleicAcid& NucleicAcid, bool deep = true) const )
+	NucleicAcid na1("na1");
+	Nucleotide a("a");
+	na1.insert(a);
+	NucleicAcid na2("na2");
+	na1.get(na2, false);
+	TEST_EQUAL(na2.getName(), "na1")
+	TEST_EQUAL(na2.countNucleotides(), 0)
+	na2.setName("a");
+	na1.get(na2);
+	TEST_EQUAL(na2.getName(), "na1")
+	TEST_EQUAL(na2.countNucleotides(), 1)
 RESULT
 
 CHECK(NucleicAcid::swap(NucleicAcid&))
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	NucleicAcid na2("na2");
+	Nucleotide n1("n1");
+	Nucleotide n2("n2");
+	na1.insert(n1);
+	na2.insert(n2);
+	na1.swap(na2);
+	TEST_EQUAL(na1.getName(), "na2")
+	TEST_EQUAL(na1.getNucleotide(0), &n2)
+	TEST_EQUAL(na2.getName(), "na1")
+	TEST_EQUAL(na2.getNucleotide(0), &n1)
 RESULT
 
 CHECK(NucleicAcid::get3Prime())
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	Nucleotide n1("n1");
+	na1.insert(n1);
+	na1.get3Prime()->setName("X");
+	TEST_EQUAL(na1.get3Prime()->getName(), "X")
 RESULT
 
 CHECK(NucleicAcid::get3Prime() const)
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	TEST_EQUAL(na1.get3Prime(), 0)
+	Nucleotide n1("n1");
+	Nucleotide n2("n2");
+	Nucleotide n3("n3");
+	na1.insert(n1);
+	TEST_EQUAL(na1.get3Prime(), &n1)
+	na1.insert(n2);
+	TEST_EQUAL(na1.get3Prime(), &n1)
+	na1.prepend(n3);
+	TEST_EQUAL(na1.get3Prime(), &n3)
 RESULT
 
 CHECK(NucleicAcid::get5Prime())
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	Nucleotide n1("n1");
+	na1.insert(n1);
+	na1.get5Prime()->setName("X");
+	TEST_EQUAL(na1.get5Prime()->getName(), "X")
 RESULT
 
 CHECK(NucleicAcid::get5Prime() const)
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	TEST_EQUAL(na1.get5Prime(), 0)
+	Nucleotide n1("n1");
+	Nucleotide n2("n2");
+	Nucleotide n3("n3");
+	na1.insert(n1);
+	TEST_EQUAL(na1.get5Prime(), &n1)
+	na1.prepend(n2);
+	TEST_EQUAL(na1.get5Prime(), &n1)
+	na1.insert(n3);
+	TEST_EQUAL(na1.get5Prime(), &n3)
 RESULT
 
 CHECK(NucleicAcid::getID() const)
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	TEST_EQUAL(na1.getID(), BALL_NUCLEICACID_DEFAULT_ID)
+	na1.setID("X");
+	TEST_EQUAL(na1.getID(), "X")
 RESULT
 
 CHECK(NucleicAcid::setID(const String&))
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	na1.setID("");
+	TEST_EQUAL(na1.getID(), BALL_NUCLEICACID_DEFAULT_ID)
+	na1.setID("X");
+	TEST_EQUAL(na1.getID(), "X")
 RESULT
 
 CHECK(NucleicAcid::countNucleotides())
-//BAUSTELLE
+	NucleicAcid na1("na1");
+	TEST_EQUAL(na1.countNucleotides(), 0)
+	Nucleotide n1("n1");
+	na1.insert(n1);
+	TEST_EQUAL(na1.countNucleotides(), 1)
 RESULT
 
 CHECK(NucleicAcid::isValid())
-//BAUSTELLE
+	NucleicAcid na1("");
+	TEST_EQUAL(na1.isValid(), true)
+	na1.setID("");
+	TEST_EQUAL(na1.isValid(), true)
+	na1.setID("X");
+	TEST_EQUAL(na1.isValid(), true)
 RESULT
 
 CHECK(NucleicAcid::dump(ostream&, Size))
@@ -106,6 +223,13 @@ CHECK(NucleicAcid::write(ostream&) const)
 //BAUSTELLE
 RESULT
 
+CHECK(NucleicAcid::persistentWrite())
+//BAUSTELLE
+RESULT
+
+CHECK(NucleicAcid::persistentRead())
+//BAUSTELLE
+RESULT
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
