49da29f9a978a0a57b2b3b6141b280537fc75bbf
oliver <oliver>
2000-05-15 19:11:50 +0000

    changed: constants are now declared extern const (problems with g++/Linux-alpha)

--- include/BALL/COMMON/constants.h
+++ include/BALL/COMMON/constants.h
@@ -1,4 +1,4 @@
-// $Id: constants.h,v 1.5 2000/05/04 18:28:59 oliver Exp $
+// $Id: constants.h,v 1.6 2000/05/15 19:11:50 oliver Exp $
 
 #ifndef BALL_COMMON_CONSTANTS_H
 #define BALL_COMMON_CONSTANTS_H
@@ -37,10 +37,10 @@ namespace BALL
 		//@{
 
 		/// PI
-		static const double  PI = 3.14159265358979323846L;
+		extern const double  PI;
 
 		/// Euler's number - base of the natural logarithm
-		static const double  E  = 2.718281828459045235L;
+		extern const double  E;
 
 		/**	Internal theshold for equality comparisons.
 				Default value is 1e-6.
@@ -64,81 +64,81 @@ namespace BALL
 		/**	Elementary charge.
 			  In units of C ($1.60217738 \cdot 10^{-19} C$).
 		*/
-		static const double	ELEMENTARY_CHARGE = 1.60217738E-19L;  	 // C     
+		extern const double	ELEMENTARY_CHARGE;  	 // C     
 	
 		/// Elementary charge (alias)
-		static const double	e0								=	ELEMENTARY_CHARGE;
+		extern const double	e0;
 
 		/** Electron mass.
 				In units of kg ($9.1093897 \cdot 10^{-31}$ kg).
 		*/
-		static const double	ELECTRON_MASS   	= 9.1093897E-31L;   	 // kg
+		extern const double	ELECTRON_MASS   	;   	 // kg
 
 		/** Proton mass.
 				In units of kg ($1.6726230 \cdot 10^{-27}$ kg).
 		*/
-		static const double	PROTON_MASS     	= 1.6726230E-27L;   	 // kg
+		extern const double	PROTON_MASS     	;   	 // kg
 
 		/** Neutron mass.
 				In units of kg ($1.6749286 \cdot 10^{-27}$ kg).
 		*/
-		static const double	NEUTRON_MASS    	= 1.6749286E-27L;   	 // kg
+		extern const double	NEUTRON_MASS    	;   	 // kg
 
 		/** Avogadro constant.
 				In units of $mol^{-1}$ ($6.0221367 \cdot 10^{23} mol^{-1}$).
 		*/
-		static const double	AVOGADRO        	= 6.0221367E+23L;   	 // 1 / mol
+		extern const double	AVOGADRO;
 
 		/** Avogadro constant (alias)
 		*/
-		static const double	NA								= AVOGADRO;
+		extern const double	NA;
 
 		/** Avogadro constant (alias)
 		*/
-		static const double	MOL             	= AVOGADRO;
+		extern const double	MOL;
 
 		/** Boltzmann constant.
 				In units of J/K ($1.380657 \cdot 10^{-23}$ J/K).
 		*/
-		static const double	BOLTZMANN       	= 1.380657E-23L;  	   // J / K
+		extern const double	BOLTZMANN;
 
 		/** Boltzmann constant (alias)
 		*/
-		static const double	k	        				= BOLTZMANN;
+		extern const double	k;
 		
 		/** Planck constant.
 				In units of Js ($6.6260754 \cdot 10^{-34}$ Js).
 		*/
-		static const double	PLANCK 	         	= 6.6260754E-34L;      // J * sec
+		extern const double	PLANCK;
 
 		/** Planck constant (alias)
 		*/
-		static const double	h       	   			= PLANCK;
+		extern const double	h;
 
 		/** Gas constant (= NA * k)	
 		*/
-		static const double	GAS_CONSTANT 	  	= NA * k;
+		extern const double	GAS_CONSTANT;
 
 		/** Gas constant (alias)
 		*/
-		static const double R 								= GAS_CONSTANT;
+		extern const double R;
 
 		/** Faraday constant (= NA * e0)
 		*/
-		static const double	FARADAY         	= NA * e0;
+		extern const double	FARADAY;
 
 		/** Faraday constant (alias)
 		*/
-		static const double	F    							= FARADAY;
+		extern const double	F;
 
 		/** Bohr radius.
 				In units m ($5.29177249 \cdot 10^{-11}$ m).
 		*/
-		static const double	BOHR_RADIUS     	= 5.29177249E-11L;     // m
+		extern const double	BOHR_RADIUS;
 
 		/** Bohr radius (alias)
 		*/
-		static const double	a0     						= BOHR_RADIUS;
+		extern const double	a0;
 
 		//  the following values from: 
 		//  P.W.Atkins: Physical Chemistry, 5th ed., Oxford University Press, 1995
@@ -146,31 +146,31 @@ namespace BALL
 		/** Vacuum permittivity.
 				In units of $C^2J^{-1}m^{-1}$ ($8.85419 \cdot 10^{-12} C^2J^{-1}m^{-1}$).
 		*/
-		static const double	VACUUM_PERMITTIVITY    	= 8.85419E-12L;     // C^2 / (J * m)
+		extern const double	VACUUM_PERMITTIVITY;
 
 		/** Vacuum permeability.
 				In units of $Js^2C^{-2}m^{-1}$ ($4\pi \cdot 10^{-7} Js^2C^{-2}m^{-1}$).
 		*/
-		static const double	VACUUM_PERMEABILITY     = (4 * PI * 1E-7L);	// J s^2 / (C^2 * m)
+		extern const double	VACUUM_PERMEABILITY;
 
 		/** Speed of light.
 				In units of m/s ($2.99792458 \cdot 10^8 ms^{-1}$).
 		*/
-		static const double	SPEED_OF_LIGHT          = 2.99792458E+8L;	  // m / s
+		extern const double	SPEED_OF_LIGHT;
 
 		/** Speed of Light (alias)
 		*/
-		static const double	c												= SPEED_OF_LIGHT;
+		extern const double	c;
 
 		/** Gravitational constant.
 				In units of $Nm^2kg^{-2}$ ($6.67259 \cdot 10^{-11} Nm^2kg^{-2}$).
 		*/
-		static const double	GRAVITATIONAL_CONSTANT  = 6.67259E-11L;    	// N m^2 / kg^2
+		extern const double	GRAVITATIONAL_CONSTANT;
 
 		/** Fine structure constant.
 				Without unit ($7.29735 \cdot 10^{-3}$).
 		*/
-		static const double	FINE_STRUCTURE_CONSTANT = 7.29735E-3L;   		// 1      
+		extern const double	FINE_STRUCTURE_CONSTANT;
 		//@}
 
 		/**	@name	Conversion factors
@@ -189,32 +189,32 @@ namespace BALL
 		/** Degree per rad.
 				57.2957795130823209
 		*/
-		static const double	DEG_PER_RAD				= 57.2957795130823209L;
+		extern const double	DEG_PER_RAD;
 
 		/** Rad per degree.
 				0.0174532925199432957
 		*/
-		static const double	RAD_PER_DEG			 	= 0.0174532925199432957L;
+		extern const double	RAD_PER_DEG;
 
 		/** mm per inch.
 				25.4
 		*/
-		static const double	MM_PER_INCH 			= 25.4L;
+		extern const double	MM_PER_INCH 			;
 
 		/** m per foot.
 				3.048
 		*/
-		static const double	M_PER_FOOT  			= 3.048L;
+		extern const double	M_PER_FOOT  			;
 
 		/** Joules per calorie.
 				4.184
 		*/
-		static const double	JOULE_PER_CAL     = 4.184;
+		extern const double	JOULE_PER_CAL;
 
 		/** Calories per Joule.
 				1/JOULE_PER_CAL
 		*/
-		static const double	CAL_PER_JOULE     = (1 / 4.184);
+		extern const double	CAL_PER_JOULE;
 
 		//@}
 
