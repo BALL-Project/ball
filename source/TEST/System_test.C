fc2dd5875018c7f9d182875987a8e96470070349
amoll <amoll>
2000-05-16 15:16:00 +0000

    added: test for count...() functions

--- source/TEST/System_test.C
+++ source/TEST/System_test.C
@@ -1,11 +1,11 @@
-// $Id: System_test.C,v 1.2 2000/05/09 16:21:07 amoll Exp $
+// $Id: System_test.C,v 1.3 2000/05/16 15:16:00 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 #include <BALL/KERNEL/system.h>
 #include <BALL/KERNEL/molecule.h>
 #include <BALL/KERNEL/fragment.h>
 #include <BALL/KERNEL/atom.h>
-START_TEST(System, "$Id: System_test.C,v 1.2 2000/05/09 16:21:07 amoll Exp $")
+START_TEST(System, "$Id: System_test.C,v 1.3 2000/05/16 15:16:00 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -127,6 +127,7 @@ RESULT
 
 CHECK(countMolecules())
 	System s;
+	TEST_EQUAL(s.countMolecules(), 0);
 	Molecule m;
 	s.insert(m);
 	TEST_EQUAL(s.countMolecules(), 1);
@@ -134,6 +135,7 @@ RESULT
 
 CHECK(countFragments())
 	System s;
+	TEST_EQUAL(s.countFragments(), 0);
 	Molecule m;
 	Fragment f;
 	s.insert(m);
@@ -143,11 +145,50 @@ RESULT
 
 CHECK(countAtoms())
 	System s;
+	TEST_EQUAL(s.countAtoms(), 0);
 	Molecule m;
 	Atom a;
 	s.insert(m);
 	m.insert(a);
 	TEST_EQUAL(s.countAtoms(), 1);
+RESULT
+
+CHECK(countSecondaryStructures())
+	System s;
+	TEST_EQUAL(s.countSecondaryStructures(), 0);
+	Protein p;
+	SecondaryStructure f;
+	p.insert(f);
+	s.insert(p);
+	TEST_EQUAL(s.countSecondaryStructures(), 1);
+RESULT
+
+CHECK(countChains())
+	System s;
+	TEST_EQUAL(s.countChains(), 0);
+	Protein p;
+	Chain f;
+	p.insert(f);
+	s.insert(p);
+	TEST_EQUAL(s.countChains(), 1);
+RESULT
+
+CHECK(countNucleotides())
+	System s;
+	TEST_EQUAL(s.countNucleotides(), 0);
+	NucleicAcid n;
+	Nucleotide f;
+	n.insert(f);
+	s.insert(n);
+	TEST_EQUAL(s.countNucleotides(), 1);
+RESULT
+
+CHECK(countNucleicAcids())
+	System s;
+	TEST_EQUAL(s.countNucleicAcids(), 0);
+	NucleicAcid n;
+	s.insert(n);
+	TEST_EQUAL(s.countNucleicAcids(), 1);
 RESULT
 
 CHECK(prepend(Molecule& molecule))
