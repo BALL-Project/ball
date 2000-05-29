cd380e4d1ad17d8f2e7f0aeb33a6ef0fbe1db70e
amoll <amoll>
2000-05-29 23:45:27 +0000

    added tests

--- source/TEST/LogStream_test.C
+++ source/TEST/LogStream_test.C
@@ -1,4 +1,4 @@
-// $Id: LogStream_test.C,v 1.5 2000/05/26 19:25:03 amoll Exp $
+// $Id: LogStream_test.C,v 1.6 2000/05/29 23:45:27 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,18 +7,21 @@
 #include <BALL/MATHS/common.h>
 ///////////////////////////
 
-START_TEST(class_name, "$Id: LogStream_test.C,v 1.5 2000/05/26 19:25:03 amoll Exp $")
+START_TEST(class_name, "$Id: LogStream_test.C,v 1.6 2000/05/29 23:45:27 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 using namespace std;
+String filename;
 
-LogStream* l1 = new LogStream();
-LogStream* l2 = new LogStream(true);
+LogStream* l1;
+LogStream* l2;
 
 CHECK(LogStream(bool associate_stdio = false))
+	l1 = new LogStream();
+	l2 = new LogStream(true);
 	TEST_NOT_EQUAL(l1, 0)
 	TEST_NOT_EQUAL(l2, 0)
 RESULT
@@ -29,21 +32,20 @@ CHECK(~LogStream())
 RESULT
 
 CHECK(LogStream(LogStreamBuf* buf))
-	LogStream l1;
-	l1.error() << "TEST" <<endl;
-	//LogStream* l2 = new LogStream(l1);
-	TEST_NOT_EQUAL(l2, 0)
+	LogStream* l1;
+	LogStreamBuf* lb1;
+	l1 = new LogStream(lb1);
+	TEST_NOT_EQUAL(l1, 0)
 RESULT
 
 CHECK(rdbuf())
 	LogStream l1;
-//	TEST_EQUAL(l1.rdbuf(), )
+	TEST_NOT_EQUAL(l1.rdbuf(), 0)
 RESULT
 
 CHECK(operator -> ())
 	LogStream l1;
-	l1 << "abc";
-	//TEST_EQUAL(l1->StreamStruct.min_level, 0)
+	l1->sync();
 RESULT
 
 CHECK(setLevel(int level))
@@ -101,14 +103,23 @@ CHECK(warn(int n = 0))
 RESULT
 
 CHECK(insert(std::ostream& s, int min_level = INT_MIN, int max_level = INT_MAX))
-	ofstream s;
+	NEW_TMP_FILE(filename)
 	LogStream l1;
-	l1.insert(s);
-// BAUSTELLE
+	ofstream s(filename.c_str(), std::ios::out);
+	l1.insert(s, 99, 99);
+	l1.level(98) << "X" << endl;
+	l1.level(99) << "1" << endl;
+	l1.info(99)  << "2" << endl;
+	l1.level(100)<< "X" << endl;
+	TEST_EQUAL(l1.getNumberOfLines(), 4)
+	TEST_FILE(filename.c_str(), "data/LogStream_test_general.txt", false)
 RESULT
 
 CHECK(remove(std::ostream& s))
-// BAUSTELLE
+	LogStream l1;
+	ofstream s;
+	l1.insert(s);
+	l1.remove(s);
 RESULT
 
 CHECK(insertNotification(const std::ostream& s, const Target& target))
@@ -120,38 +131,81 @@ CHECK(removeNotification(const std::ostream& s))
 RESULT
 
 CHECK(setMinLevel(const std::ostream& s, int min_level))
+	NEW_TMP_FILE(filename)
 	LogStream l1;
-//	l1.setMinLevel(
+	ofstream s(filename.c_str(), std::ios::out);
+	l1.insert(s, 0);
+	l1.setMinLevel(s, 98);
+	l1.info(97) << "X" << endl;
+	l1.info(98) << "1" << endl;
+	l1.info(99) << "2" << endl;
+	TEST_EQUAL(l1.getNumberOfLines(), 3)
+	TEST_FILE(filename.c_str(), "data/LogStream_test_general.txt", false)
 RESULT
 
 CHECK(setMaxLevel(const std::ostream& s, int max_level))
-// BAUSTELLE
+	NEW_TMP_FILE(filename)
+	LogStream l1;
+	ofstream s(filename.c_str(), std::ios::out);
+	l1.insert(s, 0);
+	l1.setMaxLevel(s, 98);
+	l1.info(97) << "1" << endl;
+	l1.info(98) << "2" << endl;
+	l1.info(99) << "X" << endl;
+	TEST_EQUAL(l1.getNumberOfLines(), 3)
+	TEST_FILE(filename.c_str(), "data/LogStream_test_general.txt", false)
 RESULT
 
 CHECK(setPrefix(const std::ostream& s, const string& prefix))
-// BAUSTELLE
+	NEW_TMP_FILE(filename)
+	LogStream l1;
+	ofstream s(filename.c_str(), std::ios::out);;
+	l1.insert(s, 0);
+	l1.setPrefix(s, "%l"); //loglevel
+	l1.info(1) << "  1." << endl;
+	l1.setPrefix(s, "%y"); //message type ("Error", "Warning", "Information", "-")
+	l1.info(2) << "  2." << endl;
+	l1.setPrefix(s, "%T"); //time (HH:MM:SS)
+	l1.info(3) << "  3." << endl;
+	l1.setPrefix(s, "%t"); //time in short format (HH:MM)
+	l1.info(4) << "  4." << endl;
+	l1.setPrefix(s, "%D"); //date (DD.MM.YYYY)
+	l1.info(5) << "  5." << endl;
+	l1.setPrefix(s, "%d"); // date in short format (DD.MM.)
+	l1.info(6) << "  6." << endl;
+	l1.setPrefix(s, "%S"); //time and date (DD.MM.YYYY, HH:MM:SS)
+	l1.info(7) << "  7." << endl;
+	l1.setPrefix(s, "%s"); //time and date in short format (DD.MM., HH:MM)
+	l1.info(8) << "  8." << endl;
+	l1.setPrefix(s, "%%"); //percent sign (escape sequence)
+	l1.info(9) << "  9." << endl;
+	l1.setPrefix(s, ""); //no prefix
+	l1.info(10) << " 10." << endl;
+	TEST_EQUAL(l1.getNumberOfLines(), 10)
+	TEST_FILE(filename.c_str(), "data/LogStream_test_setPrefix.txt", true)
 RESULT
 
 CHECK(clear())
 	LogStream l1;
 	l1.error() << "TEST" <<endl;
+	l1.clear();
 	TEST_EQUAL(l1.getNumberOfLines(), 0)
 RESULT
 
-CHECK(l1.getNumberOfLines(int min_level = INT_MIN, int max_level = INT_MAX))
+CHECK(getNumberOfLines(int min_level = INT_MIN, int max_level = INT_MAX))
 	LogStream l1;
 	TEST_EQUAL(l1.getNumberOfLines(), 0)
 	l1.error() << "TEST" <<endl;
 	TEST_EQUAL(l1.getNumberOfLines(), 1)
 RESULT
 
-CHECK(l1.getLineText(Index index))
+CHECK(getLineText(Index index))
 	LogStream l1;
 	l1.error() << "TEST" <<endl;
 	TEST_EQUAL(l1.getLineText(0), "TEST")
 RESULT
 
-CHECK(l1.getLineTime(Index index))
+CHECK(getLineTime(Index index))
 	LogStream l1;
 	time_t timer;
   timer = time(NULL);
@@ -160,26 +214,48 @@ CHECK(l1.getLineTime(Index index))
 	TEST_EQUAL(Maths::isNear(timer, l1.getLineTime(0), (long)1), true)
 RESULT
 
-CHECK(l1.getLineLevel(Index index))
+CHECK(getLineLevel(Index index))
 	LogStream l1;
 	l1.level(99) << "TEST" <<endl;
 	TEST_EQUAL(l1.getLineLevel(0), 99)
 RESULT
 
-CHECK(l1.getLineIndices(const int min_level = INT_MIN, const int max_level = INT_MAX, const time_t earliest = 0))
-// BAUSTELLE
-RESULT
+CHECK(filterLines(const int min_level = INT_MIN, const int max_level = INT_MAX,
+									const time_t earliest = 0, const time_t latest = LONG_MAX, const string& s = ""))
+	LogStream l1;
+  using std::list;													
+	list<int>	liste;
 
-CHECK(filterLinesAfterLevel(const list<int>, const int min_level = INT_MIN, int max_level = INT_MAX))
-// BAUSTELLE
-RESULT
+	l1.level(0) << "TEST1" << endl;
+	l1.level(1) << "TEST2" << endl;
+	l1.level(2) << "XXXXX" << endl;
+	time_t timer = time(NULL);
+	time_t x = timer;
+	while (x == timer)
+	{
+		x = time(NULL);
+	}
+	timer = time(NULL);
+	l1.level(3) << "XXXXX" << endl;
+	l1.level(4) << "TEST4" << endl;
+	TEST_EQUAL(l1.getNumberOfLines(), 5)
 
-CHECK(filterLinesAfterTime(const list<int>, const time_t earliest, const time_t latest))
-// BAUSTELLE
-RESULT
+	liste = l1.filterLines(1, 2, 0, LONG_MAX, "" );
+	TEST_EQUAL(liste.size(), 2)
+	TEST_EQUAL(liste.front(), 1)
+	liste.pop_front();
+	TEST_EQUAL(liste.front(), 2)
+	liste.clear();
 
-CHECK(filterLinesAfterContent(const list<int>, const string& s))
-// BAUSTELLE
+	liste = l1.filterLines(1, 2, 0, LONG_MAX, "XXXXX" );
+	TEST_EQUAL(liste.size(), 1)
+	TEST_EQUAL(liste.front(), 2)
+	liste.clear();
+
+	liste = l1.filterLines(3, 3, timer, LONG_MAX, "XXXXX");
+	TEST_EQUAL(liste.size(), 1)
+	TEST_EQUAL(liste.front(), 3)
+	liste.clear();	
 RESULT
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
