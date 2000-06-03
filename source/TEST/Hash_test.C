b687fa46abe92caf9972e63ec96d5057ad9ea373
amoll <amoll>
2000-06-03 20:11:59 +0000

    new added

---
+++ source/TEST/Hash_test.C
@@ -0,0 +1,61 @@
+// $Id: Hash_test.C,v 1.1 2000/06/03 20:11:59 amoll Exp $
+#include <BALL/CONCEPT/classTest.h>
+
+///////////////////////////
+#include <BALL/COMMON/hash.h>
+///////////////////////////
+
+START_TEST(class_name, "$Id: Hash_test.C,v 1.1 2000/06/03 20:11:59 amoll Exp $")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+using namespace BALL;
+char* c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+string s1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+String s2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+CHECK(hashString(const char* str))
+	TEST_EQUAL(hashString(c), 140)
+RESULT
+
+CHECK(hashString2(const char* str))
+	TEST_EQUAL(hashString2(c), 248)
+RESULT
+
+CHECK(hashString3(const char* str))
+	TEST_EQUAL(hashString3(c), 3780768363)
+RESULT
+
+CHECK(hashPJWString(const char* str))
+	TEST_EQUAL(hashPJWString(c), 1450744505)
+RESULT
+
+CHECK(hashElfString(const char* str))
+	TEST_EQUAL(hashElfString(c), 19269177)
+RESULT
+
+CHECK(Hash(const T& key))
+	TEST_EQUAL(Hash(1), 1)
+RESULT
+
+CHECK(Hash(const string& s))
+	TEST_EQUAL(Hash(s1), 140)
+RESULT
+
+CHECK(Hash(const String& s))
+	TEST_EQUAL(Hash(s2), 140)
+RESULT
+
+CHECK(Hash(void *const& ptr))
+	//TEST_EQUAL(Hash(c), 248)
+RESULT
+
+CHECK(getNextPrime(HashIndex l))
+	TEST_EQUAL(getNextPrime(4), 248)
+RESULT
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
