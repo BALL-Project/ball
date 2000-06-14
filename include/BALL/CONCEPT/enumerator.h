256d3597b98b2bfb347b22ed34b9c5dcce779a9c
anker <anker>
2000-06-14 14:45:42 +0000

    added: Enumerator class

---
+++ include/BALL/CONCEPT/enumerator.h
@@ -0,0 +1,318 @@
+// $Id: enumerator.h,v 1.1 2000/06/14 14:45:34 anker Exp $
+
+#ifndef BALL_CONCEPT_ENUMERATOR_H
+#define BALL_CONCEPT_ENUMERATOR_H
+
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <list>
+#include <vector>
+#include <math.h>
+
+#ifndef BALL_COMMON_H
+#	include <BALL/common.h>
+#endif
+
+#ifndef BALL_COMMON_EXCEPTION_H
+# include <BALL/COMMON/exception.h>
+#endif
+
+using namespace std;
+
+namespace BALL
+{
+
+	/** Enumerator Class.
+			The Enumerator class provides means for enumerating all possible
+			permutations of the variants given in a list (@see BAUSTELLE). Each
+			permutation thus can be denoted by an integer.
+			{\bf Definition:} \URL{BALL/CONCEPT/enumerator.h}
+	 */
+	
+	class EnumeratorIndex
+		: public vector<Position>
+	{
+		public:
+
+		/** @name Nested Classes
+		 */
+		//@{
+		/** Exception for reporting incompatible EnumeratorIndex instances, i.
+		 * e. instances with different moduli. {\bf Definition:}
+		 * \URL{BALL/CONCEPT/enumerator.h}
+		 */
+		 
+		class IncompatibleIndex
+			: public Exception::GeneralException
+		{
+			public:
+				IncompatibleIndex(const char* file, int line);
+		};
+		//@}
+
+		/** @name Constructors and Destructors
+		 */
+		//@{    
+			
+		/** Default Constructor
+		 */
+		EnumeratorIndex();
+
+		/** Detailled Constructor
+		 *  @param variant_list the list of variants to be applied
+		 */
+		template <typename Variant, typename Iterator>
+			EnumeratorIndex(const list<pair<Iterator, vector<Variant> > >& variant_list)
+			: vector<Position>(variant_list.size()),
+				modulus_(variant_list.size()),
+				base_multipliers_(variant_list.size())
+		{ 
+			// compute the base multipliers for later usage 
+			Index i;
+			Size multiplier = 1;
+			list<pair<Iterator, vector<Variant> > >::const_iterator list_it = variant_list.begin();
+			for (i = size() - 1; i >= 0; i--, list_it++)
+			{
+				operator[](i) = 0;
+				modulus_[i] = list_it->second.size();
+
+				base_multipliers_[i] = multiplier;
+				multiplier *= modulus_[i];
+			}
+		}
+
+		/** Destructor
+		 */
+		~EnumeratorIndex();
+		//@}
+
+		
+		/** @name Accessors
+		 */
+		//@{
+
+		/** access the modulus part of the enumerator
+		 */
+		const vector<Size>& getModulus() const;
+
+		/** increment an instance of EnumeratorIndex. Increment the least
+		 * significant component and apply any overflow to more signficant
+		 * components.
+		 */
+		EnumeratorIndex& operator ++ ();
+
+		/** decrement an instance of EnumeratorIndex. Decrement the least
+		 * significant component and apply any underflow to more signficant
+		 * components.
+		 */
+		EnumeratorIndex& operator -- ();
+
+		/** Set an instance of EnumeratorIndex to the value corresponding to
+		 * the number {\tt index}.
+		 * @param index the number of the permutation to which the instance
+		 * should be set
+		 */
+		void set(Position index);
+
+		/** Assign from a number. 
+		 * @param index the number to assign from
+		 * @see EnumeratorIndex::set
+		 */
+		EnumeratorIndex& operator = (Position index)
+		{
+			set(index);
+			return *this;
+		}
+
+		//@}
+
+		private:
+		/* The EnumeratorIndex class is derived from vector and additionally
+		 * contains two vectors of the same size which hold the modulus and the
+		 * base for each cell. The values of the EnumeratorIndex vector itself
+		 * are interpreted as an inhomogenous number consisting of figures
+		 * that have different bases. The bases are the numbers of
+		 * possibilities for each variant in the list. Most significant
+		 * component is operator[](0), so incrementing starts with
+		 * operator[](size())
+		 */
+			vector<Size>		 modulus_;
+			vector<Size>		 base_multipliers_;
+	};
+
+	/** @name Predicates for EnumeratorIndex class
+	 */
+	//@{
+	/** Equality of two instances of EnumeratorIndex.
+	 * @param x
+	 * @param y
+	 * @return true, if the instances are equal, false ow.
+	 */
+	bool operator == (const EnumeratorIndex& x, const EnumeratorIndex& y);
+
+	/** Inequality of two instances of EnumeratorIndex.
+	 * @param x
+	 * @param y
+	 * @return true, if the instances are not equal, false ow.
+	 */
+	bool operator != (const EnumeratorIndex& x, const EnumeratorIndex& y);
+
+	/** Test whether instance {\tt x} is less than {\tt y}
+	 * @param x
+	 * @param y
+	 * @return true, if {\tt x} is less than {\tt y}
+	 */
+	bool operator < (const EnumeratorIndex& x, const EnumeratorIndex& y);
+
+	/** Test whether instance {\tt x} is greater than {\tt y}
+	 * @param x
+	 * @param y
+	 * @return true, if {\tt x} is greater than {\tt y}
+	 */
+	bool operator > (const EnumeratorIndex& x, const EnumeratorIndex& y);
+
+	/** Test whether instance {\tt x} is less than or equal to {\tt y}
+	 * @param x
+	 * @param y
+	 * @return true, if {\tt x} is less than or equal to {\tt y}
+	 */
+	bool operator <= (const EnumeratorIndex& x, const EnumeratorIndex& y);
+	
+	/** Test whether an instance {\tt x} is greater than or equal to {\tt y}
+	 * @param x
+	 * @param y
+	 * @return true, if {\tt x} is greater than or equal to {\tt y}
+	 */
+	bool operator >= (const EnumeratorIndex& x, const EnumeratorIndex& y);
+
+	//@}
+
+	template <class Container, class Iterator, class Variant>
+		class Enumerator
+		{	
+			typedef void (*MutatorFunction)(Variant&, Variant&);
+
+			typedef vector<Variant>							
+				VariantVector;
+
+			typedef pair<Iterator, VariantVector>
+				Site;
+
+			typedef list<Site>
+				SiteList;
+
+			public:
+			
+			/** @name Constructors and Destructors
+			//@{
+			/** Default Constructor
+			 */
+			Enumerator()
+			{
+			}
+			
+			/** Detailled Constructor
+			 * @param container a Container class to be mutated
+			 * @param mutator the function defining the mutations to be applied
+			 */
+			Enumerator(Container& container, MutatorFunction mutator)
+				:	container_(container),
+			mutator_(mutator)
+			{
+			}
+
+			/** Default Destructor
+			 */
+			~Enumerator()
+			{
+			}
+			
+			//@}
+
+			/** @name Accessors
+			 */
+			//@{
+			/** Add variants to the list of variants
+			 */
+			void addVariants(const Iterator& it, const VariantVector& variants)
+			{
+				variant_sites_.push_back(Site(it, variants));
+			}
+
+			/** Delete variants from the list of variants
+			 */
+			void deleteVariants(const Iterator& it, const VariantVector& variants)
+			{
+				// BAUSTELLE
+			}
+
+			/** Count all variants.
+			 * @return the number of all possible variants
+			 */
+			Size countVariants()
+			{
+				Size total = 1;
+				SiteList::iterator it;
+				for (it = variant_sites_.begin(); it != variant_sites_.end(); ++it)
+				{
+					total *= it->second.size();
+				}
+				return total;
+			}
+
+			/** Access the current content
+			 * @return a reference to the container class of this enumerator
+			 */
+			Container& getCurrent()
+			{
+				return container_;
+			}
+
+			/** Create a permuatation denoted by its number.
+			 * @param index the number of the permutation to be created
+			 */
+			void createPermutation(const Position index)
+			{
+				EnumeratorIndex enumerator_index(variant_sites_);
+				enumerator_index = index;
+				createPermutation(enumerator_index);
+			}
+
+			/** Create a permutation denoted by an instance of EnumeratorIndex.
+			 * @param index the instance of EnumeratorIndex that describes the
+			 * permutation to be created
+			 */
+			void createPermutation(const EnumeratorIndex& index)
+			{
+				if (index.size() != variant_sites_.size())
+				{
+					throw EnumeratorIndex::IncompatibleIndex();
+				}
+
+				SiteList::iterator it = variant_sites_.begin();
+				Position i = index.size() - 1;
+				for (; it != variant_sites_.end(); ++it, --i)
+				{
+					mutate_(it->first, it->second[index[i]]);
+				}
+			}
+
+			//@}
+
+			protected:
+
+			void mutate_(Iterator& it, Variant& v)
+			{
+				mutator_(*it, v);
+			}
+
+			Container& container_;
+			MutatorFunction mutator_;
+			SiteList  variant_sites_;
+		};
+
+}
+
+#endif // BALL_CONCEPT_ENUMERATOR_H
---
+++ source/CONCEPT/enumerator.C
@@ -0,0 +1,137 @@
+// $Id: enumerator.C,v 1.1 2000/06/14 14:45:42 anker Exp $
+
+#include <BALL/COMMON/global.h>
+#include <BALL/CONCEPT/enumerator.h>
+
+
+using namespace BALL;
+
+
+EnumeratorIndex::IncompatibleIndex::IncompatibleIndex(const char* file, int line)
+{
+	// BAUSTELLE
+	Log.error() << "BAEH!" << endl;
+}
+
+
+EnumeratorIndex& EnumeratorIndex::operator ++ ()
+{
+	Index i;
+	bool add_one = true;
+	for (i = size() - 1; (i >= 0) && add_one; i--)
+	{
+		operator[](i)++;
+		add_one = false;
+		if (operator[](i) >= modulus_[i])
+		{
+			operator[](i) = 0;
+			add_one = true;
+		}
+	}
+
+	if (add_one)
+	{
+		throw Exception::IndexOverflow(__FILE__, __LINE__);
+	}
+
+	return *this;
+}
+
+
+EnumeratorIndex& EnumeratorIndex::operator -- () 
+{
+	Index i;
+	bool sub_one = true;
+	for (i = size() - 1; (i >= 0) && sub_one; i--)
+	{
+		if (operator[](i) == 0)
+		{
+			operator[](i) = modulus_[i] - 1;
+		} 
+		else 
+		{
+			operator[](i)--;
+			sub_one = false;
+		}
+	}
+
+	if (sub_one)
+	{
+		throw Exception::IndexUnderflow(__FILE__, __LINE__);
+	}
+
+	return *this;
+}
+
+
+void EnumeratorIndex::set(Position index)
+{	
+	for (Index i = 0; i < size(); ++i)
+	{
+		operator[](i) = index / base_multipliers_[i];
+		index -= base_multipliers_[i] * operator[](i);
+	}
+}
+
+
+bool operator == (const EnumeratorIndex& x, const EnumeratorIndex& y)
+{
+	if (x.getModulus() != y.getModulus())
+	{
+		throw EnumeratorIndex::IncompatibleIndex(__FILE__, __LINE__);
+	}
+
+	return (static_cast<const vector<Position>&>(x) == static_cast<const vector<Position>&>(y));
+}
+
+bool operator != (const EnumeratorIndex& x, const EnumeratorIndex& y)
+{
+	if (x.getModulus() != y.getModulus())
+	{
+		throw EnumeratorIndex::IncompatibleIndex(__FILE__, __LINE__);
+	}
+
+	return (static_cast<const vector<Position>&>(x) != static_cast<const vector<Position>&>(y));
+}
+
+bool operator < (const EnumeratorIndex& x, const EnumeratorIndex& y)
+{
+	if (x.getModulus() != y.getModulus())
+	{
+		throw EnumeratorIndex::IncompatibleIndex(__FILE__, __LINE__);
+	}
+
+	return (static_cast<const vector<Position>&>(x) < static_cast<const vector<Position>&>(y));
+}
+
+bool operator > (const EnumeratorIndex& x, const EnumeratorIndex& y)
+{
+	if (x.getModulus() != y.getModulus())
+	{
+		throw EnumeratorIndex::IncompatibleIndex(__FILE__, __LINE__);
+	}
+
+	return (static_cast<const vector<Position>&>(x) > static_cast<const vector<Position>&>(y));
+}
+
+bool operator <= (const EnumeratorIndex& x, const EnumeratorIndex& y)
+{
+	if (x.getModulus() != y.getModulus())
+	{
+		throw EnumeratorIndex::IncompatibleIndex(__FILE__, __LINE__);
+	}
+
+	return (static_cast<const vector<Position>&>(x) <= static_cast<const vector<Position>&>(y));
+}
+
+bool operator >= (const EnumeratorIndex& x, const EnumeratorIndex& y)
+{
+	if (x.getModulus() != y.getModulus())
+	{
+		throw EnumeratorIndex::IncompatibleIndex(__FILE__, __LINE__);
+	}
+
+	return (static_cast<const vector<Position>&>(x) >= static_cast<const vector<Position>&>(y));
+}
+
+
