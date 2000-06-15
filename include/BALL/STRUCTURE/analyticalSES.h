c123bee2d812b4a3af84b04582abe6072e4cf7a6
oliver <oliver>
2000-06-15 17:12:09 +0000

    added: function for volume calculation

--- include/BALL/STRUCTURE/analyticalSES.h
+++ include/BALL/STRUCTURE/analyticalSES.h
@@ -1,4 +1,4 @@
-// $Id: analyticalSES.h,v 1.2 2000/05/30 10:43:26 oliver Exp $
+// $Id: analyticalSES.h,v 1.3 2000/06/15 17:12:09 oliver Exp $
 
 #ifndef BALL_STRUCTURE_ANALYTICALSES_H
 #define BALL_STRUCTURE_ANALYTICALSES_H
@@ -19,8 +19,21 @@ namespace BALL
 			@param	fragment the kernel object containing the atoms
 			@param	probe_radius the probe radius used for the SAS
 			@param	number_of_dots the number of dots used per atom
+			@return the SES area in $\AA^2$
 	*/
 	float calculateSESArea
+		(const BaseFragment& fragment, float probe_radius = 1.5);
+	
+	/**	Calculate the solvent excluded volume analytically.
+			This method uses the algorithm by Michael L. Connolly.
+			BAUSTELLE
+		
+			@param	fragment the kernel object containing the atoms
+			@param	probe_radius the probe radius used for the SAS
+			@param	number_of_dots the number of dots used per atom
+			@return the volume in $\AA^3$
+	*/
+	float calculateSESVolume
 		(const BaseFragment& fragment, float probe_radius = 1.5);
 	
 } // namespace BALL
