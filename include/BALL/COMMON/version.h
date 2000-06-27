3de8a7324231690dea11c68e4a67f52fe349523a
oliver <oliver>
2000-06-27 23:11:18 +0000

    added: VersionInfo object

--- include/BALL/COMMON/version.h
+++ include/BALL/COMMON/version.h
@@ -1,11 +1,62 @@
-// $Id: version.h,v 1.3 2000/03/30 19:50:21 oliver Exp $
+// $Id: version.h,v 1.4 2000/06/27 23:11:18 oliver Exp $
 
 #ifndef BALL_COMMON_VERSION_H
 #define BALL_COMMON_VERSION_H
 
-#define BALL_RELEASE                    000950
-#define BALL_RELEASE_STRING             "0.95a"
-#define BALL_MAJOR_RELEASE              0
-#define BALL_MINOR_RELEASE              9
+//
+//  master data for all revisions is stored in this define!
+//
+#define BALL_RELEASE_STRING             "0.96a"
+
+namespace BALL
+{
+
+	/**	@name Version information class.
+			The BALL release, release data and related version
+			can be retrieved by these methods.\\
+			BALL releases ending on "a" are alpha releases and
+			release strings ending on "b" are beta releases.
+			Stable releases are not suffixed by a letter.
+			The VersionInfo class contains only static methods
+			that can be used to retrieve the desired information.
+			\\
+			{\bf Definition:}\URL{BALL/COMMON/version.h}
+			\\
+	*/
+	class VersionInfo
+	{
+		public:
+		/**
+		*/
+		enum Type
+		{
+			STABLE,
+			ALPHA,
+			BETA,
+			NONPUBLIC,
+			UNKNOWN
+		};
+
+		/**
+		*/
+		static const char* getVersion();
+
+		/**
+		*/
+		static int getMajorRevision();
+
+		/**
+		*/
+		static int getMinorRevision();
+
+		/**
+		*/
+		static Type getType();
+		
+		protected:
+		static const char* VersionInfo::RELEASE_DATE_AND_TIME;
+	  static const char* VersionInfo::RELEASE_WHAT_STRING;
+	};
+}
 
 #endif // BALL_COMMON_VERSION_H
