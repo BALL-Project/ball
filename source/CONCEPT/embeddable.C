702d5c195cdd25d688da02cc72912c0edcecf745
oliver <oliver>
2000-07-16 16:14:44 +0000

    new: Embeddable concept allows the use of classes from an embedded   Python interpreter. Classes possess static methods to access all instances   of that class that are registered.

---
+++ source/CONCEPT/embeddable.C
@@ -0,0 +1,126 @@
+// $Id: embeddable.C,v 1.1 2000/07/16 16:14:44 oliver Exp $
+
+#include <BALL/CONCEPT/embeddable.h>
+
+using namespace std;
+
+namespace BALL
+{
+	Embeddable::~Embeddable()
+	{
+		// make sure destructed instances are in certainly unregistered
+		unregisterInstance_(this);
+	}
+
+	void Embeddable::unregisterThis() throw()
+	{
+		Embeddable::unregisterInstance_(this);
+	}
+
+	// protected:
+
+	void Embeddable::registerInstance_(const type_info& type, Embeddable* instance) 
+		throw()
+	{
+		// retrieve the class name
+		const char* class_id_string = type.name();
+
+		if (!instance_to_type_map_.has(instance))
+		{
+			// this is fact a new registration
+			if (!instance_lists_.has(class_id_string))
+			{
+				// create a new list for instances of this type
+				instance_lists_.insert(class_id_string, EmbeddableList());
+			}
+			// store the pointer to the instance in the instance list...
+			instance_lists_[class_id_string].push_back(instance);
+			
+			// ...and in the hash map for fast retrieval of the class id string
+			instance_to_type_map_.insert(pair<Embeddable*, string>(instance, class_id_string));
+		}
+		else 
+		{
+			Log.warn() << "Embeddable::registerInstance_: trying to register an already registered instance at " 
+								 << instance << " (type = " << class_id_string << ")" << endl;
+		}
+	}
+
+	void Embeddable::unregisterInstance_(Embeddable* instance) 
+		throw()
+	{
+		// check whether this instance was registered...
+		if (instance_to_type_map_.has(instance))
+		{
+			// retrieve the class id string for this specific instance
+			string class_id_string = instance_to_type_map_[instance];
+			if (instance_lists_.has(class_id_string))
+			{
+				// remove the instance from the instance list
+				instance_lists_[class_id_string].remove(instance);
+
+				// remove it from the instance hash map
+				instance_to_type_map_.erase(instance);
+			}
+			else 
+			{
+				// something terrible has happened: the instance was in the
+				// hash map but not in the list!
+				Log.error() << "Embeddable::unregisterInstance_: Internal error: "
+										<< "instance_lists_ and instance_to_type_map_ are inconsistent!" << endl;
+			}
+		}
+		else 
+		{
+			// complain if trying to unregister an unregistered instance
+			Log.warn() << "Embeddable::unregisterInstance_: trying to unregister unregistered instance at " << instance << endl;
+		}	
+	}
+
+	Size Embeddable::countInstances_(const type_info& type) 
+		throw()
+	{
+		Size number = 0;
+		// check whether we got a list with that name
+		if (instance_lists_.has(type.name()))
+		{
+			// retrieve the size of the coresponding instance list
+			number = instance_lists_[type.name()].size();
+		}
+
+		return number;
+	}
+	
+	Embeddable* Embeddable::getInstance_(const type_info& type, Position index)
+		throw()
+	{
+		// the return value;
+		Embeddable* instance = 0;
+
+		// check whether we got a list with that name
+		if (instance_lists_.has(type.name()))
+		{
+			// walk along the list, decrement the counter until
+			// the desired list element is reached
+			Index counter = (Index)index + 1;
+			EmbeddableList::Iterator it = instance_lists_[type.name()].begin();	
+			for (; it != instance_lists_[type.name()].end() && counter != 0; ++it, --counter)
+			{
+				instance = *it;
+			}
+			if (counter != 0)
+			{
+				// we didn't find the correct index - return NULL
+				instance = 0;
+			}
+		}
+
+		return instance;
+	}
+
+	StringHashMap<Embeddable::EmbeddableList>				
+		Embeddable::instance_lists_;
+
+	HashMap<Embeddable*, string>	
+		Embeddable::instance_to_type_map_;	
+}
