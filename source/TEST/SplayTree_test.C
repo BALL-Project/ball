f50afaa288ec7665669f639d31730024441465dd
amoll <amoll>
2000-08-19 13:59:38 +0000

    added some tests

--- source/TEST/SplayTree_test.C
+++ source/TEST/SplayTree_test.C
@@ -1,143 +1,145 @@
-// $Id: SplayTree_test.C,v 1.1 2000/08/09 10:18:58 amoll Exp $
+// $Id: SplayTree_test.C,v 1.2 2000/08/19 13:59:38 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
 
 #include <BALL/DATATYPE/splayTree.h>
+#include <BALL/DATATYPE/binarySearchTree.h>
+#include <BALL/CONCEPT/visitor.h>
 
 ///////////////////////////
 
-START_TEST(class_name, "$Id: SplayTree_test.C,v 1.1 2000/08/09 10:18:58 amoll Exp $")
+START_TEST(class_name, "$Id: SplayTree_test.C,v 1.2 2000/08/19 13:59:38 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
-	
+
+TSplayTree<int>* t;
+const Comparator<int> comp = Comparator<int>();
+const Comparator<int> comp2 = Comparator<int>();
+
 CHECK(TSplayTree::TSplayTree())
-  //BAUSTELLE
+	t = new TSplayTree<int>();
+	TEST_NOT_EQUAL(t, 0)	
 RESULT
-
-
-CHECK(TSplayTree::TSplayTree(const TSplayTree& splay_tree, bool deep = true))
-  //BAUSTELLE
-RESULT
-
 
 CHECK(TSplayTree::~TSplayTree())
-  //BAUSTELLE
+	delete t;
 RESULT
 
+TSplayTree<int> tree, tree2;
+tree.setComparator(comp);
+
+CHECK(TSplayTree::TSplayTree(const TSplayTree& splay_tree, bool deep = true))
+	TSplayTree<int>* t;
+	TEST_EQUAL(tree.getComparator(), &comp)
+	t = new TSplayTree<int>(tree);
+	TEST_NOT_EQUAL(t, 0)	
+	TEST_EQUAL(t->getComparator(), &comp)
+RESULT
 
 CHECK(TSplayTree::set(const TSplayTree& splay_tree, bool deep = true))
-  //BAUSTELLE
+  tree2.set(tree);
+	TEST_EQUAL(tree2.getComparator(), &comp)
+	tree2.setComparator(comp2);
 RESULT
 
-
-CHECK(TSplayTree::set(const TSplayTree &splay_tree, BSTreeIterator::WalkOrder walk_order))
-  //BAUSTELLE
+CHECK(TSplayTree::set(const TSplayTree &splay_tree, BSTreeIterator::WALK_ORDER__PREORDER))
+  tree2.set(tree, BSTreeIterator::WALK_ORDER__PREORDER);
+	TEST_EQUAL(tree2.getComparator(), &comp)
+	tree2.setComparator(comp2);
 RESULT
-
 
 CHECK(TSplayTree::TSplayTree& operator = (const TSplayTree& splay_tree))
-  //BAUSTELLE
+  tree2 = tree;
+	TEST_EQUAL(tree2.getComparator(), &comp)
+	tree2.setComparator(comp2);
 RESULT
-
 
 CHECK(TSplayTree::get(TSplayTree& splay_tree, bool deep = true) const )
-  //BAUSTELLE
+  tree.get(tree2, true);
+	TEST_EQUAL(tree2.getComparator(), &comp)
+	tree2.setComparator(comp2);
 RESULT
 
-
-CHECK(TSplayTree::get(TSplayTree& splay_tree, BSTreeIterator::WalkOrder walk_order) const )
-  //BAUSTELLE
+CHECK(TSplayTree::get(TSplayTree& splay_tree, BSTreeIterator::WALK_ORDER__PREORDER) const )
+  tree.get(tree2, BSTreeIterator::WALK_ORDER__PREORDER);
+	TEST_EQUAL(tree2.getComparator(), &comp)
+	tree2.setComparator(comp2);
 RESULT
 
+//Visitor<TSplayTree> visitor;
+//Visitor<int> visitor;
 
-CHECK(TSplayTree::getMinimum())
-  //BAUSTELLE
+CHECK(TSplayTree::host(Visitor<TSplayTree>& visitor))
+ // tree.visit(visitor);
 RESULT
-
 
 CHECK(TSplayTree::getMinimum() const )
   //BAUSTELLE
 RESULT
 
+CHECK(TSplayTree::getMaximum() const )
+  //BAUSTELLE
+RESULT
+
+CHECK(TSplayTree::getMinimum())
+  //BAUSTELLE
+RESULT
 
 CHECK(TSplayTree::getMaximum())
   //BAUSTELLE
 RESULT
 
-
-CHECK(TSplayTree::getMaximum() const )
-  //BAUSTELLE
-RESULT
-
-
 CHECK(TSplayTree::find(const DataType& data))
   //BAUSTELLE
 RESULT
-
 
 CHECK(TSplayTree::find(const DataType& data) const )
   //BAUSTELLE
 RESULT
 
-
 CHECK(TSplayTree::insert(const DataType& data, bool multiple = true))
   //BAUSTELLE
 RESULT
-
 
 CHECK(TSplayTree::insertNotSplayed(const DataType& data, bool multiple = true))
   //BAUSTELLE
 RESULT
 
-
 CHECK(TSplayTree::detach(const DataType& data))
   //BAUSTELLE
 RESULT
-
 
 CHECK(TSplayTree::detachMinimum())
   //BAUSTELLE
 RESULT
 
-
 CHECK(TSplayTree::detachMaximum())
   //BAUSTELLE
 RESULT
-
 
 CHECK(TSplayTree::remove(const DataType& data))
   //BAUSTELLE
 RESULT
 
-
 CHECK(TSplayTree::removeAll(const DataType& data))
   //BAUSTELLE
 RESULT
-
 
 CHECK(TSplayTree::removeMinimum())
   //BAUSTELLE
 RESULT
 
-
 CHECK(TSplayTree::removeMaximum())
   //BAUSTELLE
 RESULT
 
-
-CHECK(TSplayTree::host(Visitor<TSplayTree>& visitor))
-  //BAUSTELLE
-RESULT
-
-
 CHECK(TSplayTree::has(const DataType& data))
   //BAUSTELLE
 RESULT
-
 
 CHECK(TSplayTree::has(const DataType& data) const )
   //BAUSTELLE
