bc60bdaaa91338c039d353b299f543a7c5e464cd
oliver <oliver>
2000-08-28 20:27:59 +0000

    added: tests for TimeStamp

--- source/TEST/TimeStamp_test.C
+++ source/TEST/TimeStamp_test.C
@@ -1,4 +1,4 @@
-// $Id: TimeStamp_test.C,v 1.1 2000/08/28 16:10:26 oliver Exp $
+// $Id: TimeStamp_test.C,v 1.2 2000/08/28 20:27:59 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,50 +7,93 @@
 
 ///////////////////////////
 
-START_TEST(TimeStamp, "$Id: TimeStamp_test.C,v 1.1 2000/08/28 16:10:26 oliver Exp $")
+START_TEST(TimeStamp, "$Id: TimeStamp_test.C,v 1.2 2000/08/28 20:27:59 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 
+TimeStamp* ts = 0;
 CHECK(Time::TimeStamp::TimeStamp())
-  //BAUSTELLE
+	ts = new TimeStamp;
+	TEST_NOT_EQUAL(ts, 0)
 RESULT
 
 
 CHECK(Time::TimeStamp::~TimeStamp())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(Time::TimeStamp::isNewerThan(const Time& time) const  throw())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(Time::TimeStamp::isOlderThan(const Time& time) const  throw())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(Time::TimeStamp::isNewerThan(const TimeStamp& stamp) const  throw())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(Time::TimeStamp::isOlderThan(const TimeStamp& stamp) const  throw())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(Time::TimeStamp::stamp(const Time& time = Time::ZERO) throw())
-  //BAUSTELLE
+	delete ts;
 RESULT
 
 
 CHECK(Time::TimeStamp::getTime() const  throw())
-  //BAUSTELLE
+  TimeStamp t1;
+  TimeStamp t2;
+	TEST_NOT_EQUAL(t1.getTime(), t2.getTime())
+	TEST_EQUAL((t1.getTime() < t2.getTime()), true)
+	TEST_EQUAL((t1.getTime() > t2.getTime()), false)
+RESULT
+
+CHECK(Time::TimeStamp::isNewerThan(const Time& time) const  throw())
+	TimeStamp ts1;
+	STATUS(ts1)
+	TimeStamp ts2;
+	STATUS(ts2)
+	TEST_EQUAL(ts1.isNewerThan(ts1.getTime()), false)
+	TEST_EQUAL(ts1.isNewerThan(ts2.getTime()), false)
+	TEST_EQUAL(ts2.isNewerThan(ts1.getTime()), true)
+	TEST_EQUAL(ts2.isNewerThan(ts2.getTime()), false)
+RESULT
+
+
+CHECK(Time::TimeStamp::isOlderThan(const Time& time) const  throw())
+	TimeStamp ts1;
+	STATUS(ts1)
+	TimeStamp ts2;
+	STATUS(ts2)
+	TEST_EQUAL(ts1.isOlderThan(ts1.getTime()), false)
+	TEST_EQUAL(ts1.isOlderThan(ts2.getTime()), true)
+	TEST_EQUAL(ts2.isOlderThan(ts1.getTime()), false)
+	TEST_EQUAL(ts2.isOlderThan(ts2.getTime()), false)
+RESULT
+
+
+CHECK(Time::TimeStamp::isNewerThan(const TimeStamp& stamp) const  throw())
+	TimeStamp ts1;
+	STATUS(ts1)
+	TimeStamp ts2;
+	STATUS(ts2)
+	TEST_EQUAL(ts1.isNewerThan(ts1), false)
+	TEST_EQUAL(ts1.isNewerThan(ts2), false)
+	TEST_EQUAL(ts2.isNewerThan(ts1), true)
+	TEST_EQUAL(ts2.isNewerThan(ts2), false)
+RESULT
+
+
+CHECK(Time::TimeStamp::isOlderThan(const TimeStamp& stamp) const  throw())
+	TimeStamp ts1;
+	STATUS(ts1)
+	TimeStamp ts2;
+	STATUS(ts2)
+	TEST_EQUAL(ts1.isOlderThan(ts1), false)
+	TEST_EQUAL(ts1.isOlderThan(ts2), true)
+	TEST_EQUAL(ts2.isOlderThan(ts1), false)
+	TEST_EQUAL(ts2.isOlderThan(ts2), false)
+RESULT
+
+
+CHECK(Time::TimeStamp::stamp(const Time& time = Time::ZERO) throw())
+	TimeStamp ts1;
+	TimeStamp ts2;
+	TEST_EQUAL(ts1.isNewerThan(ts1), false)
+	TEST_EQUAL(ts1.isNewerThan(ts2), false)
+	TEST_EQUAL(ts2.isNewerThan(ts1), true)
+	TEST_EQUAL(ts2.isNewerThan(ts2), false)
+	ts1.stamp();
+	TEST_EQUAL(ts1.isNewerThan(ts1), false)
+	TEST_EQUAL(ts1.isNewerThan(ts2), true)
+	TEST_EQUAL(ts2.isNewerThan(ts1), false)
+	TEST_EQUAL(ts2.isNewerThan(ts2), false)
 RESULT
 
 
