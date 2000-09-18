03fd58f6ff3d0946d806489eb305cf2f8146bf73
amoll <amoll>
2000-09-18 16:46:57 +0000

    fixed test

--- source/TEST/NMRStarFile_test.C
+++ source/TEST/NMRStarFile_test.C
@@ -1,35 +1,27 @@
-// $Id: NMRStarFile_test.C,v 1.1 2000/09/13 01:03:54 amoll Exp $
-
-//#include <BALL/CONCEPT/classTest.h>
+// $Id: NMRStarFile_test.C,v 1.2 2000/09/18 16:46:57 amoll Exp $
+#include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
 #include <BALL/FORMAT/NMRStarFile.h>
 ///////////////////////////
 
-//START_TEST(String,"$Id: NMRStarFile_test.C,v 1.1 2000/09/13 01:03:54 amoll Exp $")
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
 using namespace BALL;
-using std::string;
 
-int main(int argc, char *argv[])
-{/*
-	if (strcmp(argv[1], "/KM/comp-bio/data/oliver/BMRB/bmr4089.str") == 0)
-	{
-		exit(1);
-	}*/
-	cout << argv[1] << "            ";
-	NMRStarFile rs(argv[1]);
-	cout << rs.getNumberOfAtoms() << endl;
-}
+START_TEST(String,"$Id: NMRStarFile_test.C,v 1.2 2000/09/18 16:46:57 amoll Exp $")
 
-/*
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
 CHECK(NMRStarFile::NMRStarFile(filename))
-	NMRStarFile rs("NMR/bmr1000.str");
-RESULT*/
+	NMRStarFile rs("data/bmr4318.str");
+	TEST_EQUAL(rs.getData().size(), 1)
+	TEST_EQUAL(rs.getNumberOfAtoms(), 1914)
+	if (rs.getData().size() == 1 && rs.getNumberOfAtoms() == 1914)
+	{
+		TEST_EQUAL(rs.getData()[0]->atomData[1913]->atomName, "N")
+	}
+RESULT
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
-//END_TEST
+END_TEST
