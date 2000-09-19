f10294e0e17fec04655b8405ba9526006ae90ad4
oliver <oliver>
2000-09-19 15:53:02 +0000

    added: test skeleton

--- source/TEST/ParameterSection_test.C
+++ source/TEST/ParameterSection_test.C
@@ -1,4 +1,4 @@
-// $Id: ParameterSection_test.C,v 1.1 2000/09/16 08:33:55 oliver Exp $
+// $Id: ParameterSection_test.C,v 1.2 2000/09/19 15:53:02 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -7,12 +7,82 @@
 
 ///////////////////////////
 
-START_TEST(Parameters, "$Id: ParameterSection_test.C,v 1.1 2000/09/16 08:33:55 oliver Exp $")
+START_TEST(Parameters, "$Id: ParameterSection_test.C,v 1.2 2000/09/19 15:53:02 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
+
+CHECK(ParameterSection::ParameterSection())
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::~ParameterSection())
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::destroy())
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::extractSection(Parameters& parameters, const String& section_name))
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::getValue(const String& key, const String& variable) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::has(const String& key, const String& variable) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::has(const String& key) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::hasVariable(const String& variable) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::getColumnIndex(const String& variable) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::getNumberOfVariables() const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::getNumberOfKeys() const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::getValue(Position key_index, Position variable_index) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::getKey(Position key_index) const )
+  //BAUSTELLE
+RESULT
+
+
+CHECK(ParameterSection::isValid() const )
+  //BAUSTELLE
+RESULT
+
 
 											
 
