3a2fb7932012cbb407958a7106c1679f351ca056
oliver <oliver>
2000-09-26 19:29:46 +0000

    changed: now derived from ShiftModule changed: added functionality to remove rapidly exchanging nuclei and to   average equivalent nuclei

--- include/BALL/NMR/createSpectrumProcessor.h
+++ include/BALL/NMR/createSpectrumProcessor.h
@@ -1,14 +1,22 @@
-// $Id: createSpectrumProcessor.h,v 1.6 2000/09/20 18:31:35 amoll Exp $
+// $Id: createSpectrumProcessor.h,v 1.7 2000/09/26 19:29:46 oliver Exp $
 
 #ifndef BALL_NMR_CREATESPECTRUMPROCESSOR_H
 #define BALL_NMR_CREATESPECTRUMPROCESSOR_H
 
 #ifndef BALL_NMR_PEAK_H
-#	include<BALL/NMR/peak.h>
+#	include <BALL/NMR/peak.h>
 #endif
 
 #ifndef BALL_KERNEL_ATOM_H
-# include<BALL/KERNEL/atom.h>
+# include <BALL/KERNEL/atom.h>
+#endif
+
+#ifndef BALL_DATATYPE_STRINGHASHSET_H
+# include <BALL/DATATYPE/stringHashSet.h>
+#endif
+
+#ifndef BALL_NMR_SHIFTMODULE_H
+#	include <BALL/NMR/shiftModule.h>
 #endif
 
 #include <list>
@@ -17,18 +25,12 @@ namespace BALL
 {
 	class Atom;
 
-	/**@name	CreateSpectrumProcessor
-	*/
-
-	//@{
-
 	/**	Processor creates {\tt peaklist\_}. 
 			Each atom큦 chemical shift will create a new peak.
 			Thist peak큦 {\tt atomlist\_} will contain the corresponding atom.
 	*/
-
 	class CreateSpectrumProcessor
-		:	public UnaryProcessor<Atom>
+		:	public ShiftModule
 	{
 		public:
 
@@ -43,7 +45,8 @@ namespace BALL
 		
 		/**	Destructor
 		*/
-		virtual ~CreateSpectrumProcessor();
+		virtual ~CreateSpectrumProcessor()
+			throw();
 		
 		//@}
 
@@ -52,29 +55,36 @@ namespace BALL
 
 		//@{
 
-		/**	start-function
+		/**	Application method.
 		*/
-		virtual bool start();
-		
-		/**	operator-function.
-				If object is kind of PDBAtom a new peak is created ,
-				peak큦 parameters are set and the atom is added to the peak큦
-				{\tt atomlist\_}
-		*/
-		virtual Processor::Result operator () (Atom& atom);
+		virtual Processor::Result operator () (Composite& atom)
+			throw();
 		
 		/**	Returns a reference to {\tt peaklist\_}.
 		*/
 		const list<Peak1D>& getPeakList() const;
 		
 		//@}
+
+		/**	@name Accessors
+		*/
+		//@{
+		
+		/**
+		*/
+		virtual void init()
+			throw();
+		//@}
 		protected:
 
 		std::list<Peak1D> peaklist_;
+
+		StringHashSet			ignore_atoms_;
+
+		vector<String>						equivalency_residues_;
+		vector<vector<String> >		equivalency_atoms_;
 	};
 
-	//@}
-
-} //namespace Ball
+} //namespace BALL
 	 
 #endif // BALL_NMR_CREATESPECTRUMPROCESSOR_H
