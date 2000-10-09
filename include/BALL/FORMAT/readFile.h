7fb50485b513381f6a11b56534ee4209880b2613
oliver <oliver>
2000-10-09 12:58:07 +0000

    changes: interface should be adapted to File class

--- include/BALL/FORMAT/readFile.h
+++ include/BALL/FORMAT/readFile.h
@@ -15,119 +15,130 @@
 namespace BALL 
 {
 
-	class ReadFile
+	/**
+	*/
+	class LineBasedFile
+		:	public File
 	{
 		public:
 
-			/** Exception thrown if a file could not be processed right.
-			*/
-			class ReadFileError
-				: public Exception::GeneralException
-			{
-				public:
-				ReadFileError(const char* file, int line, const ReadFile& rf, const String& message = "");
-			};
+		BALL_CREATE(LineBasedFile)
 
-			/// Get the last line number in the file.
-			Position getLineNumber() const;
+		/**	@name Exceptions
+		*/
+		//@{
+		/** Exception thrown if a file could not be processed right.
+		*/
+		class LineBasedFileError
+			: public Exception::GeneralException
+		{
+			public:
+			LineBasedFileError(const char* file, int line, const LineBasedFile* rf = 0, const string& message = "");
+		};
+		//@}
 
-			/// Get the contens in the current line in the open file.
-			const String& getLineContens() const;
+		/**	@name Constructors and Destructors
+		*/
+		//@{
 
-		protected:
-
-			/*_	@name	Constructors
-			*/
-			//_@{
-
-			/** Standard constructor
-			*/	
-			ReadFile() {};
-
-			/** Detailled constuctor.
-					Opens the given file.
-			*/
-			ReadFile(const String& file_name);
-
-			/** Copy constructor
-			*/
-			ReadFile(const ReadFile& rf);
-
-			//_@}
-
-
-			/** Assignment operator
-			*/
-			ReadFile& operator = (const ReadFile& rf);
-
-			/*_	@name	Help-Methods for File Acces
-			*/
-			//_@{
-
-			/** Function to get a token surrounded by delimiter
-					starting at position pos in the String
-			*/
-			String getToken_(Position& pos, char delimiter) const;
-
-			/** Function to get a token starting at position pos in the String
-			*/
-			String getToken_(Position& pos) const;
-
-			/// Function to get the first token from a String
-			String getToken_() const;
-
-			/// Copy a subString to a new String
-			String copyString_(Position start, Position end = 0) const;
-
-			/// Function to test if a String starts like an other String
-			bool startsWith_(const String& text) const;
-
-			/// Function to test if a String starts like an other String
-			bool search_(const String& text);
-
-			/// Like search above, but stops search when coming to a line staring with stop
-			bool search_(const String& text, const String& stop);
-
-			/// Return the position of line_ in data or -1 if it does not exist in data
-			int switch_(const std::vector<String>& data) const;
-
-			/// Tests a condition, if false prints an errormsg and terminates the programm
-			void test_(const char* file, int line, bool condition, const String& msg) const;
-
-			/// Reads a line and counts the line number
-			void readLine_();
-
-			/// skip a given number of lines
-			void skipLines_(Size number = 0);
-
-			/// Return true if line_ has text
-			bool has_(const String& text) const;
+		/// Default constructor
+		LineBasedFile()
+			throw();
 			
-			/** Spool back to the start of the file.
-					This is done by closing and reopening the file.
-			*/
-			void rewind_();
+		/** Detailed constuctor.
+				Open the given file.
+		*/
+		LineBasedFile(const String& filename, File::OpenMode open_mode = File::IN)
+			throw(FileNotFound);
 
-			//_@}
+		/** Copy constructor
+		*/
+		LineBasedFile(const LineBasedFile& rf);
 
-			/*_	@name	Help-Attributes for File Access
-			*/
-			//_@{
+		//@}
 
-			/// filestream to read
-			std::ifstream in;
+		/**	@name Assignment
+		*/
+		//@{
+			
+		/// Assignment operator
+		const LineBasedFile& operator = (const LineBasedFile& file)
+			const throw();
+			
+		/**	Clear method.
+		*/
+		void clear() 
+			throw();
+		//@}
 
-			/// buffer for the line in use
-			String line_;
+		/**	@name Accessors
+		*/
+		//@{
+		/// Get the last line number in the file.
+		Position getLineNumber() const;
 
-			/// line number in the file
-			Position line_number_;
+		/// Return the current line
+		const String& getLine() const;
 
-			/// filename
-			String file_name_;
+		//@}
 
-			//_@}
 
+		/** Assignment operator
+		*/
+		LineBasedFile& operator = (const LineBasedFile& rf);
+
+		/*_	@name	Help-Methods for File Acces
+		*/
+		//_@{
+
+		/** Function to get a token surrounded by delimiter
+				starting at position pos in the String
+		*/
+		String getField(Position& pos, char delimiter) const;
+
+		/** Function to get a token starting at position pos in the String
+		*/
+		String getToken_(Position& pos) const;
+
+		/// Function to get the first token from a String
+		String getToken_() const;
+
+		/// Copy a subString to a new String
+		String copyString_(Position start, Position end = 0) const;
+
+		/// Function to test if a String starts like an other String
+		bool startsWith_(const String& text) const;
+
+		/// Function to test if a String starts like an other String
+		bool search_(const String& text);
+
+		/// Like search above, but stops search when coming to a line staring with stop
+		bool search_(const String& text, const String& stop);
+
+		/// Return the position of line_ in data or -1 if it does not exist in data
+		int switch_(const std::vector<String>& data) const;
+
+		/// Tests a condition, if false prints an errormsg and terminates the programm
+		void test_(const char* file, int line, bool condition, const String& msg) const;
+
+		/// Reads a line and counts the line number
+		void readLine();
+
+		/// skip a given number of lines
+		void skipLines(Size number = 1);
+
+		//_@}
+
+		/*	@name	Protected Attributes
+		*/
+		//_@{
+
+		/// buffer for the line in use
+		String line_;
+
+		/// line number in the file
+		Position line_number_;
+		//_@}
 	};
 
 } // namespace BALL
