f6fe5956572c1fd8d550e4be8adb9fc6e9400c4b
oliver <oliver>
2000-10-11 09:40:20 +0000

    fixed: several methods did not work at all, some not as expected added: getSurface(Surface&)

--- include/BALL/STRUCTURE/surfaceProcessor.h
+++ include/BALL/STRUCTURE/surfaceProcessor.h
@@ -1,89 +1,105 @@
+// $Id: surfaceProcessor.h,v 1.2 2000/10/11 09:40:20 oliver Exp $
+
 #include "reducedSurface.h"
 #include "solventExcludedSurface.h"
 #include "singularities.h"
 #include "triangulatedSurface.h"
 #include "triangulation.h"
 #include <BALL/MATHS/surface.h>
+#include <BALL/CONCEPT/processor.h>
+#include <BALL/KERNEL/atom.h>
 
-class SurfaceProcessor
-	:	public UnaryProcessor<Atom>
+namespace BALL
 {
-	public:
+	/**
+	*/
+	class SurfaceProcessor
+		:	public UnaryProcessor<Atom>
+	{
+		public:
 
-	virtual bool start();
-	virtual bool finish();
+		virtual bool start();
+		virtual bool finish();
 
-	virtual Processor::Result operator () (Atom&);
+		virtual Processor::Result operator () (Atom&);
 
-	const Surface& getSurface() const;
+		const Surface& getSurface() const;
+		void getSurface(Surface& surface) const;
 
-	void setProbeRadius(float radius);
+		void setProbeRadius(float radius);
 
-	float getProbeRadius();
+		float getProbeRadius();
 
-	void setDensity(float radius);
+		void setDensity(float radius);
 
- 	float getDensity();
+		float getDensity();
 
-	protected:
-	Surface					surface_;
-	vector<Sphere3>	spheres_;
-	float						density_;
-	float						probe_radius_;
-};
-
-
-bool SurfaceProcessor::start()
-{
-	spheres_.clear();
-	return true;
-}
-
-
-Processor::Result SurfaceProcessor::operator () (Atom& atom)
-{
-	spheres_.push_back(Sphere3(atom.getPosition(), atom.getRadius()));
-	return Processor::CONTINUE;
-}
+		protected:
+		Surface					surface_;
+		vector<Sphere3>	spheres_;
+		float						density_;
+		float						probe_radius_;
+	};
 
 
-bool SurfaceProcessor::finish()
-{
-	ReducedSurface* rs = new ReducedSurface(spheres_,probe_radius_);
-	rs->compute();
-	SolventExcludedSurface* ses = new SolventExcludedSurface(rs);
-	ses->get(rs);
-	TreatSingularities(ses,probe_radius_);
-	TriangulatedSurface* surface = new TriangulatedSurface(Triangulate(ses,rs,density_));
-	surface_ = surface.export();
-	delete surface_;
-	delete ses;
-	delete rs;
-	return true;
-}
+	bool SurfaceProcessor::start()
+	{
+		spheres_.clear();
+		return true;
+	}
 
 
-const Surface& getSurface() const
-{
-	return surface_;
-}
+	Processor::Result SurfaceProcessor::operator () (Atom& atom)
+	{
+		// spheres_.push_back(Sphere3(atom.getPosition(), atom.getRadius()));
+		spheres_.push_back(Sphere3(atom.getPosition(), 1.2));
+		return Processor::CONTINUE;
+	}
 
-void setProbeRadius(float radius)
-{
-	probe_radius_ = radius;
-}
 
-float getProbeRadius()
-{
-	return probe_radius_;
-}
+	bool SurfaceProcessor::finish()
+	{
+		ReducedSurface* rs = new ReducedSurface(spheres_,probe_radius_);
+		rs->compute();
+		SolventExcludedSurface* ses = new SolventExcludedSurface(rs);
+		ses->get(rs);
+		TreatSingularities(ses,probe_radius_);
+		TriangulatedSurface* surface = new TriangulatedSurface(Triangulate(ses,rs,density_));
+		surface_ = surface->exportSurface();
+		delete surface;
+		delete ses;
+		delete rs;
+		return true;
+	}
 
-void setDensity(float density);
-{
-	density_ = density;
-}
 
-float getDensity()
-{
-	return density_;
+	const Surface& SurfaceProcessor::getSurface() const
+	{
+		return surface_;
+	}
+
+	void SurfaceProcessor::getSurface(Surface& surface) const
+	{
+		surface = surface_;
+	}
+
+	void SurfaceProcessor::setProbeRadius(float radius)
+	{
+		probe_radius_ = radius;
+	}
+
+	float SurfaceProcessor::getProbeRadius()
+	{
+		return probe_radius_;
+	}
+
+	void SurfaceProcessor::setDensity(float density)
+	{
+		density_ = density;
+	}
+
+	float SurfaceProcessor::getDensity()
+	{
+		return density_;
+	}
 }
