64b3607d4fb5bbd7897f6afc6b8f33c7f0f8d541
hekl <hekl>
2000-10-22 15:17:52 +0000

    bug fixed (message segmentation fault) added cut copy paste clearclipboard functions added menu support

--- source/VIEW/GUI/WIDGETS/control.C
+++ source/VIEW/GUI/WIDGETS/control.C
@@ -1,13 +1,25 @@
-// $Id: control.C,v 1.1 2000/09/23 13:28:38 hekl Exp $
+// $Id: control.C,v 1.2 2000/10/22 15:17:52 hekl Exp $
 
 #include <BALL/VIEW/GUI/WIDGETS/control.h>
+#include <qpopupmenu.h>
+#include <qmenubar.h>
 
 using namespace std;
+
+namespace BALL
+{
+
+	namespace VIEW
+	{
 
 Control::Control
   (QWidget* parent, const char* name)
 		:	QListView(parent, name),
 			ModularWidget(name),
+			cut_id_(-1),
+			copy_id_(-1),
+			paste_id_(-1),
+			clipboard_id_(-1),
 			selected_(),
 			selection_changed_(true),
 			information_(),
@@ -34,12 +46,31 @@ Control::~Control()
 {
 }
 
-void Control::initializeWidget(MainControl& /* main_control */)
+void Control::initializeWidget(MainControl& main_control)
 {
+	cut_id_ 
+		= main_control.insertMenuEntry
+		   (MainControl::EDIT, "&Cut", this, SLOT(cut()), CTRL+Key_C);
+
+	copy_id_ 
+		= main_control.insertMenuEntry
+		   (MainControl::EDIT, "C&opy", this, SLOT(copy()), CTRL+Key_O);
+
+	paste_id_ 
+		= main_control.insertMenuEntry
+		   (MainControl::EDIT, "&Paste", this, SLOT(paste()), CTRL+Key_P);
+
+	clipboard_id_ 
+		= main_control.insertMenuEntry
+		   (MainControl::EDIT, "Cl&ear Clipboard", this, SLOT(clearClipboard()), CTRL+Key_E);
 }
 
-void Control::finalizeWidget(MainControl& /* main_control */)
+void Control::finalizeWidget(MainControl& main_control)
 {
+	main_control.removeMenuEntry(MainControl::EDIT, "&Cut", this, SLOT(cut()), CTRL+Key_C);
+	main_control.removeMenuEntry(MainControl::EDIT, "C&opy", this, SLOT(copy()), CTRL+Key_O);
+	main_control.removeMenuEntry(MainControl::EDIT, "&Paste", this, SLOT(paste()), CTRL+Key_P);
+	main_control.removeMenuEntry(MainControl::EDIT, "Cl&ear Clipboard", this, SLOT(clearClipboard()), CTRL+Key_E);
 }
 
 void Control::checkMenu(MainControl& /* main_control */)
@@ -433,12 +464,10 @@ void Control::fillSelectionList_()
 	}
 }
 
-
-/*
 void Control::cut()
 {
-	const List<Composite*>& selection = getSelection();
-	if (selection.size() == 0)
+	const List<Composite*> *selection = &getSelection();
+	if (selection->size() == 0)
 	{
 		return;
 	}
@@ -446,9 +475,10 @@ void Control::cut()
 	QString message;
 
 	// notify the main window
-	WindowMessage window_message;
-	message.sprintf("cutting %d systems...", selection.size());
-	window_message.setStatusBar(message.ascii());
+	WindowMessage *window_message = new WindowMessage;
+	message.sprintf("cutting %d objects ...", selection->size());
+	window_message->setStatusBar(message.ascii());
+	window_message->setDeletable(true);
 	notify_(window_message);
 
 	// delete old composites
@@ -465,8 +495,8 @@ void Control::cut()
 
 	// remove all system composites from the tree and from the scene
 	// but do not delete them from memory
-	List<Composite*>::ConstIterator list_it = selection.begin();	
-	for (; list_it != selection.end(); ++list_it)
+	List<Composite*>::ConstIterator list_it = selection->begin();	
+	for (; list_it != selection->end(); ++list_it)
 	{
 		// only if selected composite equals root => remove composite from tree and mainControl
 		if (*list_it == &((*list_it)->getRoot()))
@@ -478,26 +508,32 @@ void Control::cut()
 			removeComposite(*list_it);
 			
 			// remove Composite from from mainControl
-			RemovedCompositeMessage remove_message;
-			remove_message.setComposite(*list_it);
+			RemovedCompositeMessage *remove_message = new RemovedCompositeMessage;
+			remove_message->setComposite(*list_it);
+			remove_message->setDeletable(true);
 			notify_(remove_message);
 		}
 	}
 
+	updateSelection();	
+
 	// update scene
-	SceneMessage scene_message;
-	scene_message.updateOnly();
+	SceneMessage *scene_message = new SceneMessage;
+	scene_message->updateOnly();
+	scene_message->setDeletable(true);
 	notify_(scene_message);
 
 	// notify the main window
-	window_message.setStatusBar("");
-	notify_(window_message);
+	WindowMessage *window_message_2 = new WindowMessage;
+	window_message_2->setStatusBar("");
+	window_message_2->setDeletable(true);
+	notify_(window_message_2);
 }
 
 void Control::copy()
 {
-	const List<Composite*>& selection = getSelection();
-	if (selection.size() == 0)
+	const List<Composite*> *selection = &getSelection();
+	if (selection->size() == 0)
 	{
 		return;
 	}
@@ -505,9 +541,10 @@ void Control::copy()
 	QString message;
 
 	// notify the main window
-	WindowMessage window_message;
-	message.sprintf("copying %d systems...", selection.size());
-	window_message.setStatusBar(message.ascii());
+	WindowMessage *window_message = new WindowMessage;
+	message.sprintf("copying %d objects ...", selection->size());
+	window_message->setStatusBar(message.ascii());
+	window_message->setDeletable(true);
 	notify_(window_message);
 
 	// delete old cutted composites
@@ -523,16 +560,18 @@ void Control::copy()
 	}
 
 	// copy the selected composites into the copy_list_
-	List<Composite*>::ConstIterator list_it = selection.begin();	
-	for (; list_it != selection.end(); ++list_it)
+	List<Composite*>::ConstIterator list_it = selection->begin();	
+	for (; list_it != selection->end(); ++list_it)
 	{
 		// insert deep clone of the composite into the cut list
 		copy_list_.push_back((Composite*)(*list_it)->create());
 	}
 
 	// notify the main window
-	window_message.setStatusBar("");
-	notify_(window_message);
+	WindowMessage *window_message_2 = new WindowMessage;
+	window_message_2->setStatusBar("");
+	window_message_2->setDeletable(true);
+	notify_(window_message_2);
 }
 
 void Control::paste()
@@ -545,28 +584,43 @@ void Control::paste()
 	QString message;
 
 	// notify the main window
-	WindowMessage window_message;
-	message.sprintf("pasting %d systems...", copy_list_.size());
-	window_message.setStatusBar(message.ascii());
+	WindowMessage *window_message = new WindowMessage;
+	message.sprintf("pasting %d objects ...", copy_list_.size());
+	window_message->setStatusBar(message.ascii());
+	window_message->setDeletable(true);
 	notify_(window_message);
 
 	// copying composites
 	List<Composite*>::ConstIterator list_it = copy_list_.begin();	
 	for (; list_it != copy_list_.end(); ++list_it)
 	{
-		addComposite(*list_it, 0);
+		// create a new copy of the composite
+		Composite *new_composite = (Composite*)(*list_it)->create();
+
+		// insert Composite in mainControl
+		NewCompositeMessage *new_message = new NewCompositeMessage;
+		new_message->setDeletable(true);
+		new_message->setComposite(new_composite);
+		
+		new_composite->host(getInformationVisitor_());
+		
+		new_message->setCompositeName(getInformationVisitor_().getName());
+		notify_(new_message);
 	}
 
 	// notify the main window
-	window_message.setStatusBar("");
-	notify_(window_message);
+	WindowMessage *window_message_2 = new WindowMessage;
+	window_message_2->setDeletable(true);
+	window_message_2->setStatusBar("");
+	notify_(window_message_2);
 }
 
 void Control::clearClipboard()
 {
 	// notify the main window
-	WindowMessage window_message;
-	window_message.setStatusBar("clearing clipboard...");
+	WindowMessage *window_message = new WindowMessage;
+	window_message->setStatusBar("clearing clipboard...");
+	window_message->setDeletable(true);
 	notify_(window_message);
 
 	// delete old composites
@@ -582,13 +636,17 @@ void Control::clearClipboard()
 	}
 
 	// notify the main window
-	window_message.setStatusBar("");
-	notify_(window_message);
+	WindowMessage *window_message_2 = new WindowMessage;
+	window_message_2->setStatusBar("");
+	window_message_2->setDeletable(true);
+	notify_(window_message_2);
 }
-*/
 
 
 #		ifdef BALL_NO_INLINE_FUNCTIONS
 #			include <BALL/VIEW/GUI/WIDGETS/control.iC>
 #		endif
 
+	} // namespace VIEW
+
+} // namespace BALL
