c165d06cf362ca3d8737bdc3780ae2896bb42714
hekl <hekl>
2000-11-12 15:32:52 +0000

    switched from QWidgets to QOBjects (for the registering process) added virtual functions for controlling the preferences mechanism removed deregistering mechanism from destructor and moved it to the function aboutToExit in mainControl.

--- include/BALL/VIEW/GUI/WIDGETS/modularWidget.h
+++ include/BALL/VIEW/GUI/WIDGETS/modularWidget.h
@@ -1,4 +1,4 @@
-// $Id: modularWidget.h,v 1.3 2000/09/23 14:15:07 hekl Exp $
+// $Id: modularWidget.h,v 1.4 2000/11/12 15:32:52 hekl Exp $
 
 #ifndef BALL_VIEW_GUI_WIDGETS_MODULARWIDGET_H
 #define BALL_VIEW_GUI_WIDGETS_MODULARWIDGET_H
@@ -9,6 +9,14 @@
 
 #ifndef BALL_CONCEPT_EMBEDDABLE_H
 #	include <BALL/CONCEPT/embeddable.h>
+#endif
+
+#ifndef BALL_FORMAT_INIFILE_H
+# include <BALL/FORMAT/INIFile.h>
+#endif
+
+#ifndef BALL_VIEW_GUI_DIALOGS_PREFERENCES_H
+ #include <BALL/VIEW/GUI/DIALOGS/preferences.h>
 #endif
 
 #ifndef BALL_VIEW_GUI_KERNEL_MAINCONTROL_H
@@ -101,6 +109,46 @@ class ModularWidget
 			menu entries (e.g. disable or enable entries).
 	*/
 	virtual void checkMenu(MainControl& main_control);
+
+	/** Initialize a preferences tab for the widget (if needed).
+			This method is called automatically
+			immediately before the main application 
+			is started. It should add the widget's preferences tabs 
+			(if required).
+	*/
+	virtual void initializePreferencesTab(Preferences &preferences);
+
+	/**	Remove the preferences tab.
+			This method is called by the widget's destructor.
+			It should reverse all actions performed in 
+			initializePreferencesTab (remove tabs).
+	*/
+	virtual void finalizePreferencesTab(Preferences &preferences);
+
+	/** Apply the preferences of the specific tab.
+			In this method the widget can extract any changed values from
+			its preferences tab (if required).
+			This method is called automatically if the apply button in the
+			preferences dialog is pressed.
+	*/
+	virtual void applyPreferences(Preferences &preferences);
+
+	/** Fetch the widgets preferences from the inifile.
+			This method extracts the default values from the given
+			inifile (if needed).
+			This method is called automatically
+			immediately before the main application 
+			is started. It should get the widget's initial values from the inifile. 
+			(if required).
+	*/
+	virtual void fetchPreferences(INIFile &inifile);
+
+	/** Writes the widgets preferences to the inifile.
+			This method is called by the widget's destructor.
+			It should write all needed values to the given inifile (as read from
+			the inifile in the fetchPreferences method).
+	*/
+	virtual void writePreferences(INIFile &inifile);
 	//@}
 
 }; 
