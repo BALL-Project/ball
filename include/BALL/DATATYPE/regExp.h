952e663a9d034ca62e9582e70c7fba2a4fdf8547
amoll <amoll>
2000-12-04 18:18:40 +0000

    added: commentary added: exception qualifier

--- include/BALL/DATATYPE/regExp.h
+++ include/BALL/DATATYPE/regExp.h
@@ -1,4 +1,4 @@
-// $Id: regExp.h,v 1.10 2000/12/04 12:05:49 oliver Exp $
+// $Id: regExp.h,v 1.11 2000/12/04 18:18:40 amoll Exp $
 
 #ifndef BALL_DATATYPE_REGEXP_H
 #define BALL_DATATYPE_REGEXP_H
@@ -38,43 +38,67 @@ namespace BALL
 		*/
 		//@{
 			
-		/**
+		/**	alphabetic letters
+				([A-Za-z])
 		*/
 		static const String ALPHA;
-		/**
+
+		/**	alphanumeric letters
 		*/
 		static const String ALPHANUMERIC;
-		/**
+
+		/**	real numbers
+				("^-?(([0-9]+\\.[0-9]*)|([0-9]+)|(\\.[0-9]+))([eE][---+]?[0-9]+)?$")
 		*/
 		static const String REAL;
-		/**
+
+		/** identifier characters
+				("^[A-Za-z_][A-Za-z0-9_]*$")
 		*/
 		static const String IDENTIFIER;
-		/**
+
+		/** integer characters
+				(-?[a-z])
 		*/
 		static const String INTEGER;
-		/**
+
+		/** hexadecimal and integer characters
+				("^-?(0x|0X|)[:xdigit:]$")
 		*/
 		static const String HEXADECIMAL_INTEGER;
-		/**
+
+		/** lowercase letters
+				([a-z])
 		*/
 		static const String LOWERCASE;
-		/**
+
+		/** non alphabetic characters
+				(all whitout [A-Za-z])
 		*/
 		static const String NON_ALPHA;
-		/**
+
+		/** non alphanumeric characters
+				(all whithout [0-9A-Za-z])
 		*/
 		static const String NON_ALPHANUMERIC;
-		/**
+
+		/** non numeric characters
+				(all whithout [0-9])
 		*/
 		static const String NON_NUMERIC;
-		/**
+
+		/** non whitespace charcters
+				(all whithout \n\t\r\f\v)
 		*/
 		static const String NON_WHITESPACE;
-		/**
+
+		/**	uppercase letters.
+				([A-Z])
 		*/
 		static const String UPPERCASE;
-		/**
+
+		/** whitespace characters.
+				(\n\t\r\f\v)
 		*/
 		static const String WHITESPACE;
 		//@}
@@ -89,134 +113,143 @@ namespace BALL
 		RegularExpression()
 			throw();
 
-		/**	
+		/**	Copy constructor
 		*/
 		RegularExpression(const RegularExpression& regular_expression, bool deep = true)
 			throw();
 
-		/**	
+		/**	Detailled constructor	
+				@param pattern the string to create the pattern from
+				@param wildcard_pattern
 		*/
 		RegularExpression(const String& pattern, bool wildcard_pattern = false)
 			throw();
 
-		/**	
+		/**	Destructor
 		*/
 		virtual ~RegularExpression()
 			throw();
 
-		/**	
+		/**	Clear method.
 		*/
-		virtual void clear();
+		virtual void clear() throw();
 
-		/**	
+		/**	Destroy the instance.
 		*/
-		void destroy();
+		void destroy() throw();
 		//@}
 
 		/**	@name	Assignment
 		*/
 		//@{
 
-		/**	
+		/** Assignment operator
 		*/
-		void set(const RegularExpression& regular_expression, bool deep = true);
+		const RegularExpression& operator = (const RegularExpression& expression) throw();
 
-		/**	
+		/**	Assign from another instance
 		*/
-		void set(const String& pattern, bool wildcard_pattern = false);
+		void set(const RegularExpression& regular_expression, bool deep = true) throw();
 
-		/**	
+		/**	Assign from a string
 		*/
-		void get(RegularExpression& regular_expression, bool deep = true) const;
+		void set(const String& pattern, bool wildcard_pattern = false) throw();
+
+		/**	Assign to an other instance
+		*/
+		void get(RegularExpression& regular_expression, bool deep = true) const throw();
 		//@}
 
-		/**	
+		/**	Get the expression pattern.
 		*/
-		const String& getPattern() const;
+		const String& getPattern() const throw();
 
-		/**	
+		/**	Count subexpressions.
 		*/
-		Size countSubexpressions() const;
+		Size countSubexpressions() const throw();
 
-		/**	
+		/**	Match a text with a given pattern
 		*/
-		static bool match
-			(const char* text, const char* pattern,
-			 int compile_flags = 0 | REG_EXTENDED | REG_NOSUB, int execute_flags = 0 )
+		static bool match(const char* text, const char* pattern,
+											int compile_flags = 0 | REG_EXTENDED | REG_NOSUB, int execute_flags = 0)
 			throw(Exception::NullPointer);
 		
-		/**	
+		/**	Match a text with this regular expression.
+				@param from index in the string to start the matching
 		*/
 		bool match(const String& text, Index from = 0, int execute_flags = 0 ) const
 			throw(Exception::NullPointer, Exception::IndexUnderflow, Exception::IndexOverflow);
 	
-		/**	
+		/**	Match a substring with this regular expression.
+				@param from index in the substring to start the matching
 		*/
 		bool match(const Substring& text, Index from = 0, int execute_flags = 0) const
 			throw(Substring::InvalidSubstring, Exception::IndexUnderflow, Exception::IndexOverflow);
 	
-		/**	
+		/**	Match a C-String with this regular expression.
 		*/
 		bool match(const char* text, int execute_flags = 0) const
 			throw(Exception::NullPointer);
 	
-		/**	
+		/**	Find this expression in a string
+				@param from index in the string to start the matching
+				@param found the result is stored here
 		*/
-		/**	
-		*/
-		bool find
-			(const String& text, Substring& found,
-			 Index from = 0, int execute_flags = 0) const
-			throw(Exception::IndexUnderflow, Exception::IndexOverflow);
+		bool find(const String& text, Substring& found,
+							Index from = 0, int execute_flags = 0) const
+							throw(Exception::IndexUnderflow, Exception::IndexOverflow);
 			
-		/**	
+		/**	Find this expression in a string
+				@param from index in the string to start the matching
+				@param subexpressions the results are stored here
 		*/
-		bool find
-			(const String& text, vector<Substring>& subexpressions,
-			 Index from = 0, int execute_flags = 0) const
-			throw(Exception::IndexUnderflow, Exception::IndexOverflow);
+		bool find(const String& text, vector<Substring>& subexpressions,
+							Index from = 0, int execute_flags = 0) const
+							throw(Exception::IndexUnderflow, Exception::IndexOverflow);
 						
 		/**	@name	Predicates
 		*/
 		//@{
 
-		/**	
+		/**	Test if expression is empty.
 		*/
-		bool isEmpty() const;
+		bool isEmpty() const throw();
 
-		/**	
+		/**	Equality operator
 		*/
-		bool operator == (const RegularExpression& regular_expression) const;
+		bool operator == (const RegularExpression& regular_expression) const throw();
 
-		/**	
+		/**	Inequality operator
 		*/
-		bool operator != (const RegularExpression& regular_expression) const;
+		bool operator != (const RegularExpression& regular_expression) const throw();
 
-		/**	
+		/**	Less operator
 		*/
-		bool operator < (const RegularExpression& regular_expression) const;
+		bool operator < (const RegularExpression& regular_expression) const throw();
 
-		/**	
+		/**	Less or equal operator
 		*/
-		bool operator <= (const RegularExpression& regular_expression) const;
+		bool operator <= (const RegularExpression& regular_expression) const throw();
 
-		/**	
+		/**	Greater or equal operator
 		*/
-		bool operator >= (const RegularExpression& regular_expression) const;
+		bool operator >= (const RegularExpression& regular_expression) const throw();
 
-		/**	
+		/**	Greater operator
 		*/
-		bool operator > (const RegularExpression& regular_expression) const;
+		bool operator > (const RegularExpression& regular_expression) const throw();
 		//@}
 
 		/**	@name	Debugging and Diagnostics
 		*/
 		//@{
-		/**	
-		*/
-		virtual bool isValid() const;
 
-		/**	
+		/**	Test if instance is valid.
+		*/
+		virtual bool isValid() const throw();
+
+		/**	Dump this instance to an ostream
+				@param s the ostream, default is the standard output
 		*/
 		virtual void dump(::std::ostream& s = ::std::cout, Size depth = 0) const
 			throw();
@@ -225,12 +258,13 @@ namespace BALL
 		/**	@name Storers
 		*/
 		//@{
-		/**	
+
+		/**	output operator
 		*/
 		friend ::std::ostream& operator << (::std::ostream& s, const RegularExpression& regular_expression)
 			throw();
 
-		/**	
+		/**	input operator
 		*/
 		friend ::std::istream& operator >> (::std::istream& s, RegularExpression& regular_expression)
 			throw();
