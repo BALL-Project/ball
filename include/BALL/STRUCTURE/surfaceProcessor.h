8bd26554671d83499873b660698091c3a1aca00a
strobel <strobel>
2000-12-07 15:06:27 +0000

    some debug information inserted

--- include/BALL/STRUCTURE/surfaceProcessor.h
+++ include/BALL/STRUCTURE/surfaceProcessor.h
@@ -1,13 +1,15 @@
-// $Id: surfaceProcessor.h,v 1.4 2000/10/31 09:11:23 oliver Exp $
+// $Id: surfaceProcessor.h,v 1.5 2000/12/07 15:06:27 strobel Exp $
 
-#include "reducedSurface.h"
-#include "solventExcludedSurface.h"
-#include "singularities.h"
-#include "triangulatedSurface.h"
-#include "triangulation.h"
+#include <BALL/STRUCTURE/reducedSurface.h>
+#include <BALL/STRUCTURE/solventExcludedSurface.h>
+#include <BALL/STRUCTURE/singularities.h>
+#include <BALL/STRUCTURE/triangulatedSurface.h>
+#include <BALL/STRUCTURE/triangulation.h>
 #include <BALL/MATHS/surface.h>
 #include <BALL/CONCEPT/processor.h>
 #include <BALL/KERNEL/atom.h>
+
+#include <string>
 
 namespace BALL
 {
@@ -34,6 +36,8 @@ namespace BALL
 
 		float getDensity();
 
+		vector<Sphere3>	getSpheres();
+
 		protected:
 		Surface					surface_;
 		vector<Sphere3>	spheres_;
@@ -52,26 +56,109 @@ namespace BALL
 
 	Processor::Result SurfaceProcessor::operator () (Atom& atom)
 	{
-		// spheres_.push_back(Sphere3(atom.getPosition(), atom.getRadius()));
-		spheres_.push_back(Sphere3(atom.getPosition(), 1.2));
-		std::cerr << "SurfaceProcessor::operator (" << atom.getFullName() << ")" << std::endl;
+		//spheres_.push_back(Sphere3(atom.getPosition(), atom.getRadius()));
+		//spheres_.push_back(Sphere3(atom.getPosition(), atom.getElement().getVanDerWaalsRadius()));
+		spheres_.push_back(Sphere3(atom.getPosition(), 1.0));
+
 		return Processor::CONTINUE;
 	}
 
 
 	bool SurfaceProcessor::finish()
 	{
-		std::cerr << "SurfaceProcessor::finish()" << std::endl;
+		double old_epsilon = Constants::EPSILON;
+		Constants::EPSILON = 1e-4;
+				cout << "probe radius:    "; cin >> probe_radius_;
+				spheres_.clear();
+				char* filename;
+				string name;
+				cout << "xyzr-file:       "; cin >> name;
+				filename = (char*)malloc(sizeof(char)*(name.size()+1));
+				for (Position i = 0; i < name.size(); i++)
+				{
+					filename[i] = name[i];
+				}
+				filename[name.size()] = '\0';
+				cout << filename << "\n";
+				float std_radius;
+				cout << "standart radius: "; cin >> std_radius;
+				std::ifstream input(filename);
+				Position size;
+				input >> size;
+				for (Position i = 0; i < size; i++)
+				{
+					float x, y, z, r;
+					input >> x >> y >> z >> r;
+					if (Maths::isZero(std_radius))
+					{
+						spheres_.push_back(Sphere3(Vector3(x,y,z),r));
+					}
+					else
+					{
+						spheres_.push_back(Sphere3(Vector3(x,y,z),std_radius));
+					}
+				}
+				cerr << "initialising reduced surface ...\n";
+
 		ReducedSurface* rs = new ReducedSurface(spheres_,probe_radius_);
+				cerr << "... ok\ncomputing reduced surface ...\n";
 		rs->compute();
+				std::ofstream rsfile("ReducedSurface.log");
+				rsfile << *rs;
+				rsfile.close();
+				/*for (Position p = 0; p < rs->numberOfVertices(); p++)
+				{
+					Index atom = rs->getVertex(p)->getAtom();
+					Sphere3 sphere(rs->getSphere(atom));
+					surface_.vertex.push_back(sphere.p);
+					surface_.normal.push_back(Vector3(0,0,0));
+				}
+				for (Position t = 0; t < rs->numberOfFaces(); t++)
+				{
+					Surface::Triangle triangle;
+					triangle.v1 = rs->getFace(t)->getVertex(0)->getIndex();
+					triangle.v2 = rs->getFace(t)->getVertex(1)->getIndex();
+					triangle.v3 = rs->getFace(t)->getVertex(2)->getIndex();
+					Vector3 test((rs->getSphere(rs->getFace(t)->getVertex(0)->getAtom()).p-
+												rs->getSphere(rs->getFace(t)->getVertex(1)->getAtom()).p) %
+											 (rs->getSphere(rs->getFace(t)->getVertex(0)->getAtom()).p-
+												rs->getSphere(rs->getFace(t)->getVertex(2)->getAtom()).p)		);
+					test.normalize();
+					if (test != rs->getFace(t)->getNormal())
+					{
+						Index temp = triangle.v2;
+						triangle.v2 = triangle.v3;
+						triangle.v3 = temp;
+					}
+					surface_.normal[triangle.v1] += rs->getFace(t)->getNormal();
+					surface_.normal[triangle.v2] += rs->getFace(t)->getNormal();
+					surface_.normal[triangle.v3] += rs->getFace(t)->getNormal();
+					surface_.triangle.push_back(triangle);
+				}*/
+				cerr << "... ok\ninitialising solvent excluded surface ...\n";
 		SolventExcludedSurface* ses = new SolventExcludedSurface(rs);
+				cerr << "... ok\ncomputing solvent excluded surface ...\n";
 		ses->get(rs);
-		TreatSingularities(ses,probe_radius_);
-		TriangulatedSurface* surface = new TriangulatedSurface(*Triangulate(ses,rs,density_));
+				std::ofstream sesfile("SolventExcludedSurface.log");
+				sesfile << *ses;
+				sesfile.close();
+				cerr << "... ok\ntreating singularities ...\n";
+		//TreatSingularities(ses,probe_radius_);
+				density_ = 0.25;
+				cerr << "... ok\ntriangulating surface ...\n";
+		TriangulatedSurface* surface = Triangulate(ses,rs,density_);
+				cerr << "... ok\n";
+				//cout << *surface;
+				cerr << "exporting surface ...\n";
 		surface_ = surface->exportSurface();
+				cerr << "... ok\ndeleting surface ...\n";
 		delete surface;
+				cerr << "... ok\ndeleting solvent excluded surface ...\n";
 		delete ses;
+				cerr << "... ok\ndeleting reduced surface ...\n";
 		delete rs;
+				cerr << "... ok\n";
+		Constants::EPSILON = old_epsilon;
 		return true;
 	}
 
@@ -105,4 +192,10 @@ namespace BALL
 	{
 		return density_;
 	}
+
+	vector<Sphere3>	SurfaceProcessor::getSpheres()
+	{
+		return spheres_;
+	}
+
 }
