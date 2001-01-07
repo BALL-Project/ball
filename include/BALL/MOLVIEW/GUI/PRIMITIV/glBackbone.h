310e755658764fe4e487ade416ea60608260b42c
hekl <hekl>
2001-01-07 15:47:18 +0000

    new model: glBackbone model

---
+++ include/BALL/MOLVIEW/GUI/PRIMITIV/glBackbone.h
@@ -0,0 +1,87 @@
+// $Id: glBackbone.h,v 1.1 2001/01/07 15:47:18 hekl Exp $
+
+#ifndef BALL_MOLVIEW_GUI_PRIMITIV_GLBACKBONE_H
+#define BALL_MOLVIEW_GUI_PRIMITIV_GLBACKBONE_H
+
+#ifndef BALL_COMMON_H
+#	include <BALL/common.h>
+#endif
+
+#ifndef BALL_VIEW_GUI_KERNEL_GLOBJECT_H
+#	include <BALL/VIEW/GUI/KERNEL/glObject.h>
+#endif
+
+#ifndef BALL_VIEW_GUI_PRIMITIV_GLLINE_H
+#	include <BALL/VIEW/GUI/PRIMITIV/glline.h>
+#endif
+
+#ifndef BALL_VIEW_GUI_PRIMITIV_GLSPHERE_H
+#	include <BALL/VIEW/GUI/PRIMITIV/glsphere.h>
+#endif
+
+#ifndef BALL_VIEW_GUI_PRIMITIV_GLTUBE_H
+#	include <BALL/VIEW/GUI/PRIMITIV/gltube.h>
+#endif
+
+#ifndef BALL_MOLVIEW_PRIMITIV_BACKBONE_H
+#	include <BALL/MOLVIEW/PRIMITIV/backbone.h>
+#endif
+
+
+namespace BALL
+{
+	
+	namespace MOLVIEW
+	{
+
+		/**
+		*/
+		class GLBackbone
+			: public Backbone,
+				public GLObject
+		{
+			public:
+
+			/**	@name	Constructors and Destructors
+			*/	
+			//@{
+
+			GLBackbone();
+
+			GLBackbone(const GLBackbone& backbone, bool deep = true);
+
+			GLBackbone(const GeometricObject& geometric_object);
+
+			virtual ~GLBackbone();
+
+			virtual void clear();
+
+			virtual void destroy();
+			//@}
+
+
+			protected:
+
+			virtual bool draw(bool with_names = false);
+
+			virtual bool drawUserDefined();
+
+			virtual bool extract();
+
+
+  		private:
+
+			virtual Sphere* createSphere_();
+			virtual Tube* createTube_();		
+			virtual Line* createLine_();		
+		};
+
+#		ifndef BALL_NO_INLINE_FUNCTIONS
+#			include <BALL/MOLVIEW/GUI/PRIMITIV/glBackbone.iC>
+#		endif
+		
+	} // namespace MOLVIEW
+
+} // namespace BALL
+
+#endif // BALL_MOLVIEW_GUI_PRIMITIV_GLBACKBONE_H
