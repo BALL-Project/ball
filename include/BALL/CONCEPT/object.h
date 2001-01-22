25dba4e88713bf0c5eba9617f26d5e058e4f4f05
amoll <amoll>
2001-01-22 20:50:32 +0000

    removed: ObjectManager added: Object::operator == fixed: operator < fixed: long method Object::compare is not longer inline

--- include/BALL/CONCEPT/object.h
+++ include/BALL/CONCEPT/object.h
@@ -1,4 +1,4 @@
-// $Id: object.h,v 1.10 2001/01/19 23:54:18 amoll Exp $ 
+// $Id: object.h,v 1.11 2001/01/22 20:50:24 amoll Exp $ 
 
 #ifndef BALL_CONCEPT_OBJECT_H
 #define BALL_CONCEPT_OBJECT_H
@@ -63,7 +63,18 @@ namespace BALL
 
 		//@}
  
+ 		/**	@name	Assignment */
+		//@{
+	
+		/** Assignment operator
+				This method does not assign anything!
+				Implemented just for the OCI.
+		*/
+		const Object& operator = (const Object& /* object */)
+			throw();
 		
+		//@}
+
 		/**	@name	Handle Management */
 		//@{
 		///
@@ -90,7 +101,7 @@ namespace BALL
 			throw();
 
 		///
-		virtual bool operator < (const Object& object) const
+		bool operator < (const Object& object) const
 			throw();
 
 		///
@@ -173,300 +184,6 @@ namespace BALL
 #		endif // BALL_SUPPORT_OBJECT_MANAGER
 	};
 
-#		ifdef BALL_SUPPORT_OBJECT_MANAGER
-
-	/**
-	*/
-	class ObjectManager
-	{
-		public:
-
-			/**	@name	Insertion and Removal of Objects */
-			//@{
-
-			///
-			static void insert(Object& object)
-				throw();
-		
-			///
-			static void remove(Object& object)
-				throw();
-			//@}
-		
-			/**	@name	Obejct Statistics */
-			//@{
-	
-			/// Returns the number of object insertions
-			static Size countInsertions()
-				throw();
-
-			/// Returns the number of pbject removals
-			static Size countRemovals()
-				throw();
-
-			/// Returns the number of obejct updates
-			static Size countUpdates()
-				throw();
-
-			/// Returns the total number of registered objects
-			static Size countObjects()
-				throw();
-
-			/// Returns the total number of registered objects of a certain kind
-			template <typename U>
-			Size countObjects(const U& u)
-				throw()
-			{
-				Size count = 0;
-
-		    for (ObjectIterator object_it = begin(); object_it.isValid(); ++object_it)
-				{
-    		  if (RTTI::isKindOf<U>(*object_it))
-					{
-		        ++count;
-					}
-				}
-
-		    return count;
-			}
-			//@}
-
-			/**	@name	Debugging and Diagnostics */
-			//@{
-			///
-			static bool isValid()
-				throw();
-
-			///
-			static void dump(::std::ostream& s = std::cout, Size depth = 0)
-				throw();
-			//@}
-
-			typedef Object* IteratorPosition;
-		
-			class IteratorTraits_
-			{
-				public:
-
-				BALL_CREATE_DEEP(IteratorTraits_)
-
-				IteratorTraits_()
-					throw()
-					:	bound_(0),
-						position_(0)
-				{
-				}
-			
-				IteratorTraits_(const ObjectManager& objectManager)
-					throw()
-					:	bound_((ObjectManager *)&objectManager),
-						position_(0)
-				{
-				}
-				
-				IteratorTraits_(const IteratorTraits_& traits, bool /* deep */ = true)
-					throw()
-					:	bound_(traits.bound_),
-						position_(traits.position_)
-				{
-				}
-			
-				const IteratorTraits_& operator = (const IteratorTraits_& traits)
-					throw()
-				{
-					bound_ = traits.bound_;
-					position_ = traits.position_;
-					return *this;
-				}
-
-				ObjectManager* getContainer()
-					throw()
-				{
-					return bound_;
-				}
-
-				const ObjectManager* getContainer() const
-					throw()
-				{
-					return bound_;
-				}
-
-				bool isSingular() const
-					throw()
-				{
-					return (bound_ == 0);
-				}
-			
-				IteratorPosition& getPosition()
-					throw()
-				{	
-					return position_;
-				}
-
-				const IteratorPosition& getPosition() const
-					throw()
-				{
-					return position_;
-				}
-
-				bool operator == (const IteratorTraits_& traits) const
-					throw()
-				{
-					return (position_ == traits.position_);
-				}
-
-				bool operator !=(const IteratorTraits_& traits) const
-					throw()
-				{
-					return (position_ != traits.position_);
-				}
-			
-				bool isValid() const
-					throw()
-				{
-					return (bound_ != 0 && position_ != 0);
-				}
-
-				void invalidate()
-					throw()
-				{
-					bound_ = 0;
-					position_ = 0;
-				}
-
-				void toBegin()
-					throw()
-				{
-					position_ = bound_->first_;
-				}
-
-				bool isBegin() const
-					throw()
-				{
-					return (position_ == bound_->first_);
-				}
-
-				void toEnd()
-					throw()
-				{
-					position_ = 0;
-				}
-
-				bool isEnd() const
-					throw()
-				{
-					return (position_ == 0);
-				}
-
-				Object& getData()
-					throw()
-				{
-					return *position_;
-				}
-
-				const Object& getData() const
-					throw()
-				{
-					return *position_;
-				}
-
-				void forward()
-					throw()
-				{	
-					position_ = position_->getNext();
-				}
-
-				void toRBegin()
-					throw()
-				{
-					position_ = bound_->last_;
-				}
-
-				bool isRBegin() const
-					throw()
-				{
-					return (position_ == bound_->last_);
-				}
-
-				void toREnd()
-					throw()
-				{
-					position_ = 0;
-				}
-
-				bool isREnd() const
-					throw()
-				{
-					return (position_ == 0);
-				}
-
-				void backward()
-					throw()
-				{
-					position_ = position_->getPrevious();
-				}
-	
-
-
-				private:
-
-				ObjectManager *bound_;
-				IteratorPosition position_;
-			};
-
-			friend class IteratorTraits_;
-
-			typedef BidirectionalIterator<ObjectManager, Object, Object *, IteratorTraits_> ObjectIterator;
-
-			static ObjectIterator begin()
-				throw()
-			{
-				ObjectManager object_manager;
-				return ObjectIterator::begin(object_manager);
-			}
-
-			static ObjectIterator end()
-				throw()
-			{
-				ObjectManager object_manager;
-				return ObjectIterator::end(object_manager);
-			}
-
-
-
-			typedef ReverseBidirectionalIterator<ObjectManager, Object, Object *, IteratorTraits_> ReverseObjectIterator;
-
-			static ReverseObjectIterator rbegin()
-				throw()
-			{
-				ObjectManager object_manager;
-				return ReverseObjectIterator::begin(object_manager);
-			}
-
-			static ReverseObjectIterator rend()
-				throw()
-			{
-				ObjectManager object_manager;
-				return ReverseObjectIterator::end(object_manager);
-			}
-
-		protected:
-		
-		private:
-
-			ObjectManager()
-				throw()
-			{
-			}
-
-			static Object *first_;
-			static Object *last_;
-			static Size number_of_insertions_;
-			static Size number_of_removals_;
-			static Size number_of_objects_;
-	};
-
-#	endif // BALL_SUPPORT_OBJECT_MANAGER
 
 #	ifndef BALL_NO_INLINE_FUNCTIONS
 #		include <BALL/CONCEPT/object.iC>
--- source/CONCEPT/object.C
+++ source/CONCEPT/object.C
@@ -1,4 +1,4 @@
-// $Id: object.C,v 1.7 2000/12/19 12:51:06 amoll Exp $
+// $Id: object.C,v 1.8 2001/01/22 20:50:32 amoll Exp $
 
 #include <BALL/CONCEPT/object.h>
 
@@ -12,16 +12,6 @@ namespace BALL
 {
 
 	Handle Object::global_handle_ = (Handle)0;
-
-#	ifdef BALL_SUPPORT_OBJECT_MANAGER
-
-	Object *ObjectManager::first_ = 0;
-	Object *ObjectManager::last_ = 0;
-	Size ObjectManager::number_of_insertions_ = 0;
-	Size ObjectManager::number_of_removals_ = 0;
-	Size ObjectManager::number_of_objects_ = 0;
-
-#	endif // BALL_SUPPORT_OBJECT_MANAGER
 
 	// the following four methods
 	// may not be inlined - they are called
@@ -56,12 +46,6 @@ namespace BALL
 		:	AutoDeletable(),
 			handle_(Object::global_handle_++)
 	{
-#		ifdef BALL_SUPPORT_OBJECT_MANAGER
-
-		previous_ = next_= 0;
-		ObjectManager::insert(*this);
-
-#		endif // BALL_SUPPORT_OBJECT_MANAGER
 	}
 
 	Object::Object(const Object&, bool)
@@ -69,22 +53,11 @@ namespace BALL
 		:	AutoDeletable(),
 			handle_(Object::global_handle_++)
 	{
-#		ifdef BALL_SUPPORT_OBJECT_MANAGER
-
-		previous_ = next_ = 0;
-		ObjectManager::insert(*this);
-
-#		endif // BALL_SUPPORT_OBJECT_MANAGER
 	}
 
 	Object::~Object()
 		throw()
 	{
-#		ifdef BALL_SUPPORT_OBJECT_MANAGER
-
-		ObjectManager::remove(*this);
-
-#		endif // BALL_SUPPORT_OBJECT_MANAGER
 	}
 
 	void Object::dump(ostream& s, Size depth) const
@@ -118,119 +91,23 @@ namespace BALL
 		return s;
 	}
 
-
-
-#	ifdef BALL_SUPPORT_OBJECT_MANAGER
-
-	void ObjectManager::insert(Object &object)
+	int Object::compare(const Object & object) const
 		throw()
 	{
-		if (first_ == 0)
+		if (*this == object)
 		{
-			first_ = last_ = &object;
-			object.previous_ = object.next_ = 0;
-
+			return 0;
+		} 
+		if (*this < object)
+		{
+			return -1;
 		} 
 		else 
 		{
-			object.previous_ = last_;
-			object.next_ = 0;
-
-			last_ = last_->next_ = &object;
+			return 1;
 		}
-
-		++number_of_objects_;
-		++number_of_insertions_;
-	}
-		
-	void ObjectManager::remove(Object &object)
-		throw()
-	{
-		if (first_ == &object)
-		{
-			if (last_ == &object)
-			{
-				first_ = last_ = 0;
-			}
-			else 
-			{
-				first_ = object.next_;
-				first_->previous_ = 0;
-			}
-		} 
-		else 
-		{
-			if (last_ == &object)
-			{
-				last_ = object.previous_;
-				last_->next_ = 0;
-			} 
-			else 
-			{
-				object.previous_->next_ = object.next_;
-				object.next_->previous_ = object.previous_;
-			}
-		}
-
-		--number_of_objects_;
-		++number_of_removals_;
 	}
 
-	bool ObjectManager::isValid()
-		throw()
-	{
-		Size size = 0;
-		ObjectIterator object_it;
-		
-		for (object_it = begin(); !object_it.isEnd(); ++object_it)
-		{
-			++size;
-		}
-
-		if (size != number_of_objects_)
-		{
-			return false;
-		}
-
-		for (object_it = rbegin(), size = 0; !object_it.isREnd(); --object_it)
-		{
-			++size;
-		}
-
-		return (size == number_of_objects_);
-	}
-
-	void ObjectManager::dump (ostream& s, Size depth)
-		throw()
-	{
-		BALL_DUMP_STREAM_PREFIX(s);
-
-		BALL_DUMP_DEPTH(s, depth);
-		BALL_DUMP_CLASS_HEADER(s, "ObjectManager", 0);
-
-		BALL_DUMP_DEPTH(s, depth);
-		cout << "  updates: " << countUpdates() << endl;
-
-		BALL_DUMP_DEPTH(s, depth);
-		cout << "    insertions: " << countInsertions() << endl;
-
-		BALL_DUMP_DEPTH(s, depth);
-		cout << "    removals: " << countRemovals() << endl;
-
-		BALL_DUMP_DEPTH(s, depth);
-		cout << "  active objects: " << countObjects() << endl;
-
-		for (ObjectIterator object_it = begin(); object_it.isValid(); ++object_it)
-		{
-			BALL_DUMP_DEPTH(s, depth);
-			cout << "    "
-					 << typeid(*object_it).name() << ' '
-					 << (*object_it).getHandle()
-					 << endl;
-		}
-		
-		BALL_DUMP_STREAM_SUFFIX(s);
-	}
 
 #	ifdef BALL_NO_INLINE_FUNCTIONS
 #		include <BALL/CONCEPT/object.iC>
@@ -238,5 +115,4 @@ namespace BALL
 
 } // namespace BALL
 
-#endif // BALL_SUPPORT_OBJECT_MANAGER
 
