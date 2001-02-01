918a7d0c996e542760dea384a1cf27b8f665ed50
amoll <amoll>
2001-02-01 17:52:52 +0000

    fixed: docu cosmetic changes

--- include/BALL/MOLMEC/MDSIMULATION/canonicalMD.h
+++ include/BALL/MOLMEC/MDSIMULATION/canonicalMD.h
@@ -1,8 +1,7 @@
-// $Id: canonicalMD.h,v 1.12 2001/01/29 18:36:47 anker Exp $
+// $Id: canonicalMD.h,v 1.13 2001/02/01 17:52:52 amoll Exp $
 
 #ifndef BALL_MOLMEC_MDSIMULATION_CANONICALMD_H   
 #define BALL_MOLMEC_MDSIMULATION_CANONICALMD_H   
-
 
 // Include all necessary BALL headers 
 #ifndef BALL_COMMON_H
@@ -20,7 +19,6 @@
 #ifndef BALL_MATHS_VECTOR3_H         
 #	include <BALL/MATHS/vector3.h>
 #endif
-
 
 #ifndef BALL_DATATYPE_OPTIONS_H
 #	include <BALL/DATATYPE/options.h>
@@ -47,24 +45,21 @@
 #endif
 
 
-// STL include commands 
 #include <vector>
 
 namespace BALL 
 {
 
-	/** a MD simulation in the canoncial ensemble.
-		A class for doing molecular dynamics simulations      
-		according to the principle of a canonical ensemble (NVT), i.e., 
-		the temperature is kept constant. 
-		This is achieved via the velocity rescaling proposed by
-		Berendsen et al., J. Chem. Physics, 81:3684, 1984.
-		Note that this approach does  not give all properties of a
-		true canonical system. In particular, the heat capacity 
-		cannot be calculated! 
-		\\
-		{\bf Definition:}\URL{BALL/MOLMEC/MDSIMULATION/canonicalMD.h}
-		\\
+	/** A MD simulation in the canoncial ensemble.
+			A class for doing molecular dynamics simulations      
+			according to the principle of a canonical ensemble (NVT), i.e., 
+			the temperature is kept constant. 
+			This is achieved via the velocity rescaling proposed by
+			Berendsen et al., J. Chem. Physics, 81:3684, 1984.
+			Note that this approach does  not give all properties of a
+			true canonical system. In particular, the heat capacity 
+			cannot be calculated!	\\
+			{\bf Definition:}\URL{BALL/MOLMEC/MDSIMULATION/canonicalMD.h}
 	*/
   class CanonicalMD 
 		: public MolecularDynamics
@@ -86,13 +81,13 @@ namespace BALL
 
     BALL_CREATE(CanonicalMD)
 
-    /** The default constructor with no arguments
+    /** The default constructor with no arguments.
     */
     CanonicalMD()
 			throw();
 
     /** This constructor expects a force field 
-        The force field's options are used and no saving of snapshots is done 
+        The force field's options are used and no saving of snapshots is done.
 				@param myforcefield the forcefield we need for the simulation
     */
     CanonicalMD(ForceField& myforcefield)
@@ -100,25 +95,22 @@ namespace BALL
 
     /** This constructor expects a force field and a snapshot manager 
         The force field's options are used. 
-				@param myforcefield the forcefield which is to be used in the
-				simulation
+				@param myforcefield the forcefield to use in the simulation
 				@param ssm  a pointer to the SnapShotManager which will be used to
 				create trajectory files
     */
     CanonicalMD(ForceField& myforcefield, SnapShotManager* ssm)
 			throw();
 
-    /** This constructor wants a force field, a snapshot manager and new 
-				options 
-				@param myforcefield the forcefield which is to be used in the
-				simulation
+    /** This constructor wants a force field, a snapshot manager and new options.
+				@param myforcefield the forcefield which is to be used in the	simulation
 				@param ssm a pointer to the SnapShotManager used for creating
-				trajectory files
+								trajectory files
 				@param myoptions arbitrary options that are to be used by this
-				instance instead of those defined therein
+								instance instead of those defined therein
     */
-    CanonicalMD(ForceField &myforcefield,SnapShotManager *ssm,
-				const Options &myoptions)
+    CanonicalMD(ForceField& myforcefield, SnapShotManager* ssm, 
+								const Options& myoptions)
 			throw();
 
     /** The standard copy constructor.
@@ -132,50 +124,42 @@ namespace BALL
 			throw();
 
     //@}
-
-
     /** @name Assignments
     */
-
     //@{
 
-    /// Assignment operator
+    /// Assignment operator.
     CanonicalMD &operator = (const CanonicalMD& rhs)
 			throw();
 
     //@}
-
-
     /** @name Setup methods
     */
     //@{
 
-    /** This method does general setup things 
+    /** This method does general setup things.
     */
-    virtual bool setup(ForceField &myforcefield, SnapShotManager *ssm)
+    virtual bool setup(ForceField &myforcefield, SnapShotManager* ssm)
 			throw();
 
-    /** This method does general setup things 
+    /** This method does general setup things. 
     */
-    virtual bool setup(ForceField &myforcefield, SnapShotManager *ssm,
+    virtual bool setup(ForceField &myforcefield, SnapShotManager* ssm,
 				const Options &myoptions)
 			throw();
 
-    /** This method is meant for additional preparations  apart from those
-				done in setup 
-				@return {\bf true} if specificSetup() was successful
+    /** This method is meant for additional preparations apart from those done in setup.
+				@return bool, {\bf true} if specificSetup() was successful
     */
     virtual bool specificSetup()
 			throw();
 
     //@} 
-
-
-    /// @name Accessors
+    /** @name Accessors
+		*/
 		//@{
 
-    /** This method sets a new relaxation time for the coupling to an
-        external heat bath
+    /** This method sets a new relaxation time for the coupling to an external heat bath.
 				@param time the time in [unit] 
     */
     void setBathRelaxationTime(double time)
@@ -187,17 +171,17 @@ namespace BALL
     double getBathRelaxationTime() const
 			throw();
 
-    /** Set a new time step  for the numerical integration 
+    /** Set a new time step for the numerical integration.
     */
     virtual void setTimeStep(double time)
 			throw();
 
-    /**  This method does the actual simulation stuff
+    /**  This method does the actual simulation stuff.
          It runs for getMaximalNumberIterations() iterations.
          If restart is true, the counting of iterations starts with the
          number of the last iteration in the previous run.  
 				 @param restart a bool value indicating whether the simulation is
-				 to be run from the beginning
+								to be run from the beginning
     */
     virtual void simulate(bool restart = false)
 			throw();
@@ -230,8 +214,7 @@ namespace BALL
     //_ @name Protected methods
     //_@{
 
-    /*_ A protected method for calculating some
-        factors that are needed all the time
+    /*_ A protected method for calculating some factors that are needed all the time.
     */
     void calculateFactors_()
 			throw();
