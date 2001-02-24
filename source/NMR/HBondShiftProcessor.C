e38ead560a5b29c3c9a0e955682b091866cf7720
anhi <anhi>
2001-02-24 13:07:24 +0000

    *** empty log message ***

--- source/NMR/HBondShiftProcessor.C
+++ source/NMR/HBondShiftProcessor.C
@@ -1,4 +1,4 @@
-// $Id: HBondShiftProcessor.C,v 1.2 2001/02/15 14:55:04 anhi Exp $
+// $Id: HBondShiftProcessor.C,v 1.3 2001/02/24 13:07:24 anhi Exp $
 
 #include <BALL/NMR/HBondShiftProcessor.h>
 #include <stdio.h>
@@ -118,62 +118,108 @@ namespace BALL
 	BALL::Atom::BondConstIterator bi = (*donator_it)->beginBond();
 	
 	for (; +bi; ++bi)
-	  {
-	    Atom* at = (*bi).getPartner(**donator_it);
-	    if (at->getElement() == PTE[Element::H])
+	{
+	  Atom* at = (*bi).getPartner(**donator_it);
+	  if (at->getElement() == PTE[Element::H])
+	    {
+	      // print the partners of the assumed hydrogen bond
+	      
+	      // compute the euclidian distance between acceptor and donator
+	      Vector3 acceptor_pos = (*acceptor_it)->getPosition();
+	      Vector3 donator_h_pos = (at)->getPosition();
+	      
+	      Vector3 don_h = donator_pos - donator_h_pos;
+	      Vector3 don_h_acc = acceptor_pos - donator_h_pos;
+	      
+	      Angle psi = don_h.getAngle(don_h_acc);
+	      	      
+	      BALL::Atom::BondConstIterator accit = (*acceptor_it)->beginBond();
+	      Atom* c;
+	      Vector3 acceptor_plane;
+	      Angle theta;
+	      Angle phi;
+	      
+	      while (+accit) 
 	      {
-		// print the partners of the assumed hydrogen bond
-		
-		// compute the euclidian distance between acceptor and donator
-		Vector3 acceptor_pos = (*acceptor_it)->getPosition();
-		Vector3 donator_h_pos = (at)->getPosition();
-		
-		// this is used to compute the shift	
-		float b_length = donator_h_pos.getDistance(acceptor_pos);
-		
-		// this is used to decide whether there is an hbond or not
-		float distance = donator_pos.getDistance(acceptor_pos);
-	
-		// this is a test! empirical guess
-		// float b_length = (2. / 3) * distance;
-		
-		// do they form a bond?
-		// like in the paper by wuethrich, no bonds are accepted: - outside a certain range
-		//                                                        - on the same residue
-		//                                                        - on neighbouring residues in the chain
-		if ((minimum_bond_length_ <= distance) && (maximum_bond_length_ >= distance) 
-		    && (*acceptor_it)->getResidue() != (*donator_it)->getResidue()
-		    && (abs((*acceptor_it)->getResidue()->getID().toInt() - (*donator_it)->getResidue()->getID().toInt()) > 1))
-		  {
-		    // increase the number of found HBonds for this donator
-		    cout << (*donator_it)->getResidue()->getFullName() << ":" << (*donator_it)->getResidue()->getID() << " -> ";
-		    cout << (*acceptor_it)->getResidue()->getFullName() << ":" << (*acceptor_it)->getResidue()->getID() << " " << endl;
-		    hbond_number++;
-		    if (hbond_number > 1)
-		      {
-			// only accept the nearest partner
-			if (b_length <= nearest_acc)
-			  {
-			    // compute their contribution to the chemical shift
-			    delta_HBOND = a_ * 1./pow(b_length, 3) + b_;
-			    cout << "#: " << hbond_number << " length: " << b_length << " " << distance << " " << delta_HBOND << endl;
-			    nearest_acc = b_length;
-			    hbond_number = 1;
-cerr << (*donator_it)->getResidue()->getFullName() << (*donator_it)->getResidue()->getID() << " " << (*donator_it)->getName() << " -> ";
-cerr << (*acceptor_it)->getResidue()->getFullName() << (*acceptor_it)->getResidue()->getID() << " " << (*acceptor_it)->getName() << " " << b_length << " " << distance << endl;
-			  }
-		      }
-		    else
-		      {
-cerr << (*donator_it)->getResidue()->getFullName() << (*donator_it)->getResidue()->getID() << " " << (*donator_it)->getName() << " -> ";
-cerr << (*acceptor_it)->getResidue()->getFullName() << (*acceptor_it)->getResidue()->getID() << " " << (*acceptor_it)->getName() << " " << b_length << " " << distance << endl;
-			delta_HBOND = a_ * 1./pow(b_length, 3) + b_;
-			cout << "#: " << hbond_number << " length: " << b_length << " " << distance << " " << delta_HBOND << endl;
-			nearest_acc = b_length;
-		      }
-		  }
+		c = (*accit).getPartner(**acceptor_it);
+		if (c->getElement() == PTE[Element::C])
+		{
+		  Vector3 c_pos = c->getPosition();
+		  BALL::Atom::BondConstIterator c_partner_it = c->beginBond();
+		  while (+c_partner_it)
+		    {
+		      Atom* c_partner = (*c_partner_it).getPartner(*c);
+		      if (c_partner->getElement() != PTE[Element::H])
+			{
+			  Vector3 c_partner_pos = c_partner->getPosition();
+			  if ((c_partner_pos != c_pos) && (donator_h_pos != acceptor_pos))
+			    {
+			      acceptor_plane = (acceptor_pos - c_pos) % (c_partner_pos - c_pos);
+			      Vector3 hbond = (donator_h_pos - acceptor_pos);
+			      if ((acceptor_plane != Vector3(0, 0, 0)) && (((c_pos - acceptor_pos) % acceptor_plane) != Vector3(0, 0, 0)))
+				{
+				  theta = acceptor_plane.getAngle(hbond);
+				  Vector3 xaxis = (c_pos - acceptor_pos) % acceptor_plane;
+				  phi = xaxis.getAngle(hbond);
+				  break;
+				}
+			    }
+			}
+		      ++c_partner_it;
+		    }
+		  break;
+		}
+		++accit;
 	      }
-	  }
+	      
+	      
+	      
+	      // this is used to compute the shift	
+	      float b_length = donator_h_pos.getDistance(acceptor_pos);
+	      
+	      // this is used to decide whether there is an hbond or not
+	      float distance = donator_pos.getDistance(acceptor_pos);
+	      
+	      // this is a test! empirical guess
+	      // float b_length = (2. / 3) * distance;
+	      
+	      // do they form a bond?
+	      // like in the paper by wuethrich, no bonds are accepted: - outside a certain range
+	      //							  - on the same residue
+	      //							  - on neighbouring residues in the chain
+	      if ((minimum_bond_length_ <= distance) && (maximum_bond_length_ >= distance) 
+		  && (*acceptor_it)->getResidue() != (*donator_it)->getResidue()
+		  && (abs((*acceptor_it)->getResidue()->getID().toInt() - (*donator_it)->getResidue()->getID().toInt()) > 1))
+		{
+		  // increase the number of found HBonds for this donator
+		  cout << (*donator_it)->getResidue()->getFullName() << ":" << (*donator_it)->getResidue()->getID() << " -> ";
+		  cout << (*acceptor_it)->getResidue()->getFullName() << ":" << (*acceptor_it)->getResidue()->getID() << " " << endl;
+		  hbond_number++;
+		  if (hbond_number > 1)
+		    {
+		      // only accept the nearest partner
+		      if (b_length <= nearest_acc)
+			{
+			  // compute their contribution to the chemical shift
+			  delta_HBOND = a_ * 1./pow(b_length, 3) + b_;
+			  cout << "#: " << hbond_number << " length: " << b_length << " " << distance << " " << delta_HBOND << endl;
+			  nearest_acc = b_length;
+			  hbond_number = 1;
+			  cerr << (*donator_it)->getResidue()->getFullName() << (*donator_it)->getResidue()->getID() << " " << (*donator_it)->getName() << " -> ";
+			  cerr << (*acceptor_it)->getResidue()->getFullName() << (*acceptor_it)->getResidue()->getID() << " " << (*acceptor_it)->getName() << " " << b_length << " " << distance << " phi, theta, psi: " << phi << " " << theta << " " << psi << endl;
+			}
+		    }
+		  else
+		    {
+		      cerr << (*donator_it)->getResidue()->getFullName() << (*donator_it)->getResidue()->getID() << " " << (*donator_it)->getName() << " -> ";
+		      cerr << (*acceptor_it)->getResidue()->getFullName() << (*acceptor_it)->getResidue()->getID() << " " << (*acceptor_it)->getName() << " " << b_length << " " << distance << " phi, theta, psi: " << phi << " " << theta << " " << psi << endl;
+		      delta_HBOND = a_ * 1./pow(b_length, 3) + b_;
+		      cout << "#: " << hbond_number << " length: " << b_length << " " << distance << " " << delta_HBOND << endl;
+		      nearest_acc = b_length;
+		    }
+		}
+	    }
+	}
 	bi = (*donator_it)->beginBond();
 	
       }
@@ -202,61 +248,61 @@ cerr << (*acceptor_it)->getResidue()->getFullName() << (*acceptor_it)->getResidu
   {
     // here, we collect all possible acceptors and donators
     if (RTTI::isKindOf<Atom>(object))
-    {
-      Atom* atom_ptr = RTTI::castTo<Atom>(object);
-
-      // ??? Do I have to use the fragment or the residue of the atom???
-      const String& residue_name = atom_ptr->getFragment()->getName();
-      const String& atom_name = atom_ptr->getName();
-
-      if (   ((residue_name == "ALA") && (atom_name == "N"))
-	  || ((residue_name == "ARG") && ((atom_name == "N") || (atom_name == "NH1") || (atom_name == "NH2")))
-	  || ((residue_name == "ASN") && ((atom_name == "N") || (atom_name == "ND2")))
-	  || ((residue_name == "ASP") && (atom_name == "N"))
-	  || ((residue_name == "CYS") && (atom_name == "N"))
-	  || ((residue_name == "GLN") && ((atom_name == "N") || (atom_name == "NE2")))
-	  || ((residue_name == "GLU") && (atom_name == "N"))
-	  || ((residue_name == "GLY") && (atom_name == "N"))
-	  || ((residue_name == "HIS") && ((atom_name == "N") || (atom_name == "NE2")))
-	  || ((residue_name == "ILE") && (atom_name == "N"))
-	  || ((residue_name == "LEU") && (atom_name == "N"))
-	  || ((residue_name == "LYS") && ((atom_name == "N") || (atom_name == "NZ")))
-	  || ((residue_name == "MET") && (atom_name == "N"))
-	  || ((residue_name == "PHE") && (atom_name == "N"))
-	  || ((residue_name == "PRO") && (atom_name == "N"))
-	  || ((residue_name == "SER") && ((atom_name == "N") || (atom_name == "OG")))
-	  || ((residue_name == "THR") && ((atom_name == "N") || (atom_name == "OG1")))
-	  || ((residue_name == "TRP") && (atom_name == "N"))
-	  || ((residue_name == "TYR") && ((atom_name == "N") || (atom_name == "OH")))
-	  || ((residue_name == "VAL") && (atom_name == "N")))
       {
-	donator_list_.push_back(atom_ptr);
-      }
+	Atom* atom_ptr = RTTI::castTo<Atom>(object);
+
+	// ??? Do I have to use the fragment or the residue of the atom???
+	const String& residue_name = atom_ptr->getFragment()->getName();
+	const String& atom_name = atom_ptr->getName();
+
+	if (   ((residue_name == "ALA") && (atom_name == "N"))
+	       || ((residue_name == "ARG") && ((atom_name == "N") || (atom_name == "NH1") || (atom_name == "NH2")))
+	       || ((residue_name == "ASN") && ((atom_name == "N") || (atom_name == "ND2")))
+	       || ((residue_name == "ASP") && (atom_name == "N"))
+	       || ((residue_name == "CYS") && (atom_name == "N"))
+	       || ((residue_name == "GLN") && ((atom_name == "N") || (atom_name == "NE2")))
+	       || ((residue_name == "GLU") && (atom_name == "N"))
+	       || ((residue_name == "GLY") && (atom_name == "N"))
+	       || ((residue_name == "HIS") && ((atom_name == "N") || (atom_name == "NE2")))
+	       || ((residue_name == "ILE") && (atom_name == "N"))
+	       || ((residue_name == "LEU") && (atom_name == "N"))
+	       || ((residue_name == "LYS") && ((atom_name == "N") || (atom_name == "NZ")))
+	       || ((residue_name == "MET") && (atom_name == "N"))
+	       || ((residue_name == "PHE") && (atom_name == "N"))
+	       || ((residue_name == "PRO") && (atom_name == "N"))
+	       || ((residue_name == "SER") && ((atom_name == "N") || (atom_name == "OG")))
+	       || ((residue_name == "THR") && ((atom_name == "N") || (atom_name == "OG1")))
+	       || ((residue_name == "TRP") && (atom_name == "N"))
+	       || ((residue_name == "TYR") && ((atom_name == "N") || (atom_name == "OH")))
+	       || ((residue_name == "VAL") && (atom_name == "N")))
+	  {
+	    donator_list_.push_back(atom_ptr);
+	  }
      
-      if (   ((residue_name == "ALA") && (atom_name == "O"))
-	  || ((residue_name == "ARG") && (atom_name == "O"))
-	  || ((residue_name == "ASN") && (atom_name == "O"))
-	  || ((residue_name == "ASP") && ((atom_name == "O") || (atom_name == "OD1") || (atom_name == "OD2")))
-	  || ((residue_name == "CYS") && ((atom_name == "O") || (atom_name == "SG")))
-	  || ((residue_name == "GLN") && (atom_name == "O"))
-	  || ((residue_name == "GLU") && ((atom_name == "O") || (atom_name == "OE1") || (atom_name == "OE2")))
-	  || ((residue_name == "GLY") && (atom_name == "O"))
-	  || ((residue_name == "HIS") && ((atom_name == "O") || (atom_name == "ND1") || (atom_name == "NE2")))
-	  || ((residue_name == "ILE") && (atom_name == "O"))
-	  || ((residue_name == "LEU") && (atom_name == "O"))
-	  || ((residue_name == "LYS") && (atom_name == "O"))
-	  || ((residue_name == "MET") && ((atom_name == "O") || (atom_name == "SD")))
-	  || ((residue_name == "PHE") && (atom_name == "O"))
-	  || ((residue_name == "PRO") && (atom_name == "O"))
-	  || ((residue_name == "SER") && ((atom_name == "O") || (atom_name == "OG")))
-	  || ((residue_name == "THR") && ((atom_name == "O") || (atom_name == "OG1")))
-	  || ((residue_name == "TRP") && (atom_name == "O"))
-	  || ((residue_name == "TYR") && (atom_name == "OH"))
-	  || ((residue_name == "VAL") && (atom_name == "O")))
-      { 
-	acceptor_list_.push_back(atom_ptr);
+	if (   ((residue_name == "ALA") && (atom_name == "O"))
+	       || ((residue_name == "ARG") && (atom_name == "O"))
+	       || ((residue_name == "ASN") && (atom_name == "O"))
+	       || ((residue_name == "ASP") && ((atom_name == "O") || (atom_name == "OD1") || (atom_name == "OD2")))
+	       || ((residue_name == "CYS") && ((atom_name == "O") || (atom_name == "SG")))
+	       || ((residue_name == "GLN") && (atom_name == "O"))
+	       || ((residue_name == "GLU") && ((atom_name == "O") || (atom_name == "OE1") || (atom_name == "OE2")))
+	       || ((residue_name == "GLY") && (atom_name == "O"))
+	       || ((residue_name == "HIS") && ((atom_name == "O") || (atom_name == "ND1") || (atom_name == "NE2")))
+	       || ((residue_name == "ILE") && (atom_name == "O"))
+	       || ((residue_name == "LEU") && (atom_name == "O"))
+	       || ((residue_name == "LYS") && (atom_name == "O"))
+	       || ((residue_name == "MET") && ((atom_name == "O") || (atom_name == "SD")))
+	       || ((residue_name == "PHE") && (atom_name == "O"))
+	       || ((residue_name == "PRO") && (atom_name == "O"))
+	       || ((residue_name == "SER") && ((atom_name == "O") || (atom_name == "OG")))
+	       || ((residue_name == "THR") && ((atom_name == "O") || (atom_name == "OG1")))
+	       || ((residue_name == "TRP") && (atom_name == "O"))
+	       || ((residue_name == "TYR") && (atom_name == "OH"))
+	       || ((residue_name == "VAL") && (atom_name == "O")))
+	  { 
+	    acceptor_list_.push_back(atom_ptr);
+	  }
       }
-    }
 
     return Processor::CONTINUE;
   }
