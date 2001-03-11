9fc857fea31ebe2ee619e8f410f48828949e58a0
anker <anker>
2001-03-11 21:03:07 +0000

    added some trivial tests

--- source/TEST/HINFile_test.C
+++ source/TEST/HINFile_test.C
@@ -1,4 +1,4 @@
-// $Id: HINFile_test.C,v 1.2 2000/07/12 19:36:46 oliver Exp $
+// $Id: HINFile_test.C,v 1.3 2001/03/11 21:03:07 anker Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -8,24 +8,27 @@
 
 ///////////////////////////
 
-START_TEST(HINFile, "$Id: HINFile_test.C,v 1.2 2000/07/12 19:36:46 oliver Exp $")
+START_TEST(HINFile, "$Id: HINFile_test.C,v 1.3 2001/03/11 21:03:07 anker Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 
+HINFile* ptr;
+
 CHECK(HINFile::HINFile())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(HINFile::HINFile(const String& filename, File::OpenMode open_mode = File::IN))
-  //BAUSTELLE
+	ptr = new HINFile;
+	CHECK_NOT_EQUAL(ptr, 0)
 RESULT
 
 
 CHECK(HINFile::~HINFile())
+  delete ptr;
+RESULT
+
+
+CHECK(HINFile::HINFile(const String& filename, File::OpenMode open_mode = File::IN))
   //BAUSTELLE
 RESULT
 
@@ -51,12 +54,13 @@ RESULT
 
 
 CHECK(HINFile::hasPeriodicBoundary() const )
-  //BAUSTELLE
+  HINFile box("data/water.hin");
+	TEST_EQUAL(box.hasPeriodicBoundary(), true)
 RESULT
 
 
 CHECK(HINFile::getPeriodicBoundary() const )
-  //BAUSTELLE
+  HINFile box("data/water.hin");
 RESULT
 
 
