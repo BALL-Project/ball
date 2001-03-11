0b7f999b84400c62e6b871df92e995edf49c2f9f
anker <anker>
2001-03-11 19:31:58 +0000

    changed: moved the SnapShotManager class completely to snapShotManager.h removed unnecesssary header inlcusions changed: SnapShotManagers are no longer friends of SnapShots removed: unnecessary methods regarding the length of snapshot instances (*DataLength(), *TotalLength()) added: interfaces for interaction with Systems, e. g. read/write all Atom positions from/to a System added: tentative implementation of input/output stream operators

--- include/BALL/MOLMEC/COMMON/snapShot.h
+++ include/BALL/MOLMEC/COMMON/snapShot.h
@@ -1,4 +1,4 @@
-// $Id: snapShot.h,v 1.16 2001/03/07 22:19:44 amoll Exp $
+// $Id: snapShot.h,v 1.17 2001/03/11 19:31:58 anker Exp $
 
 // This file contains the definitions of the classes 
 // SnapshotManager and Snapshot. 
@@ -18,51 +18,12 @@
 #	include <BALL/KERNEL/system.h>
 #endif
 
-#ifndef BALL_COMMON_EXCEPTION_H
-#	include <BALL/common/exception.h>
-#endif
-
-#ifndef BALL_KERNEL_ATOM_H
-#	include <BALL/KERNEL/atom.h>
-#endif
-
 #ifndef BALL_MATHS_VECTOR3_H
 #	include <BALL/MATHS/vector3.h>
 #endif
 
-#ifndef BALL_KERNEL_PTE_H
-#	include <BALL/KERNEL/PTE.h>
-#endif
-
-#ifndef BALL_DATATYPE_OPTIONS_H
-#	include <BALL/DATATYPE/options.h>
-#endif
-
-#ifndef BALL_MOLMEC_PARAMETER_FORCEFIELDPARAMETER_H
-#	include <BALL/MOLMEC/PARAMETER/forceFieldParameters.h>
-#endif
-
-#ifndef BALL_MOLMEC_PARAMETER_ATOMTYPES_H
-#	include <BALL/MOLMEC/PARAMETER/atomTypes.h>
-#endif
-
-#ifndef BALL_MOLMEC_COMMON_PERIODIC_BOUNDARY_H
-#	include <BALL/MOLMEC/COMMON/periodicBoundary.h>
-#endif
-
-#ifndef BALL_MOLMEC_COMMON_FORCEFIELD_H
-#	include <BALL/MOLMEC/COMMON/forceField.h>
-#endif
-
-#ifndef BALL_FORMAT_TRAJECTORYFILE_H
-#	include <BALL/FORMAT/trajectoryFile.h>
-#endif
-
-
 namespace BALL
 {
-
-	class TrajectoryFile;
 
 	/** SnapShot class, containing the actual data of one single snapshot.
 			It consists of the posistions, velocities and forces for each atom
@@ -73,8 +34,6 @@ namespace BALL
   class SnapShot
   {
 
-		friend class SnapShotManager;
-		
 		public:
 
     BALL_CREATE(SnapShot)
@@ -89,12 +48,6 @@ namespace BALL
 
 		/// Copy constructor.
 		SnapShot(const SnapShot& snapshot)
-			throw();
-
-    /** This constructor expects the number of atoms in the system and
-        reserves enough memory. 
-    */
-    SnapShot(const Size no_of_atoms)
 			throw();
 
     /// The destructor 
@@ -147,44 +100,107 @@ namespace BALL
 		Size getNumberOfAtoms() const
 			throw();
 
-		/// Get the number of bytes needed for writing the data of this instance 
-		Size getDataLength() const
-			throw();
-
-		/// Get the total number of bytes needed for writing this instance 
-		Size getTotalLength() const
+		/// Set the value for the potential energy of this snapshot's system
+		void setPotentialEnergy(DoubleReal potential_energy) 
 			throw();
 
 		/// Get the value for the potential energy of this snapshot's system
-		double getPotentialEnergy() const
+		DoubleReal getPotentialEnergy() const
+			throw();
+
+		/// Set the value for the kinetic energy of this snapshot's system
+		void setKineticEnergy(DoubleReal kinetic_energy)
 			throw();
 
 		/// Get the value for the kinetic energy of this snapshot's system
-		double getKineticEnergy() const
+		DoubleReal getKineticEnergy() const
 			throw();
 
 		/// Specify all atom positions
-		void setAtomPositions(const std::vector<Vector3>& atom_postions)
+		void setAtomPositions(const ::std::vector<Vector3>& atom_postions)
 			throw();
 
 		/// Get the vector containing the atom postions
-		const std::vector<Vector3>& getAtomPositions() const
+		const ::std::vector<Vector3>& getAtomPositions() const
 			throw();
 
 		/// Specify all atom velocities
-		void setAtomVelocitites(const std::vector<Vector3>& atom_velocities)
+		void setAtomVelocitites(const ::std::vector<Vector3>& atom_velocities)
 			throw();
 
 		/// Get the vector containing the atom velocitites
-		const std::vector<Vector3>& getAtomVelocities() const
+		const ::std::vector<Vector3>& getAtomVelocities() const
 			throw();
 
 		/// Specify all atom forces
-		void setAtomForces(const std::vector<Vector3>& atom_forces)
+		void setAtomForces(const ::std::vector<Vector3>& atom_forces)
 			throw();
 
 		/// Get all atom forces
-		const std::vector<Vector3>& getAtomForces() const
+		const ::std::vector<Vector3>& getAtomForces() const
+			throw();
+
+		//@}
+		/// @name System interaction
+		//@{
+
+		/** Take a SnapShot from a system. Copy all positions, velocities and
+				forces from the System {\tt system} to this instance of SnapShot.
+				@param system the System from which to take the data
+		*/
+		void takeSnapShot(const System& system)
+			throw(Exception::OutOfMemory);
+
+		/** Apply the data contained in a SnapShot to a System. Copy all
+				available data to the system. {\bf Note} that some trajectory file
+				formats do not contain all this information and vectors might be
+				zero or even worse undefined when read SnapShots are read from such
+				a file.
+				@param system the System which will be manipulated
+		*/
+		void applySnapShot(System& system) const
+			throw();
+
+		/** Read all atom positions from a System and store them in this
+				instance of SnapShot.
+				@param system the System from which data will be read
+		*/
+		void getAtomPositions(const System& system)
+			throw(Exception::OutOfMemory);
+
+		/** Set all Atom positions of a System to the values stored in this
+				instance of SnapShot.
+				@param system the system which will be manipulated
+		*/
+		void setAtomPositions(System& system) const
+			throw();
+
+		/** Read all atom velocities from a System and store them in this
+				instance of SnapShot.
+				@param system the System from which data will be read
+		*/
+		void getAtomVelocities(const System& system)
+			throw(Exception::OutOfMemory);
+
+		/** Set all Atom velocities of a System to the values stored in this
+				instance of SnapShot.
+				@param system the system which will be manipulated
+		*/
+		void setAtomVelocitites(System& system) const
+			throw();
+
+		/** Read all atom forces from a System and store them in this
+				instance of SnapShot.
+				@param system the System from which data will be read
+		*/
+		void getAtomForces(const System& system)
+			throw(Exception::OutOfMemory);
+
+		/** Set all Atom forces of a System to the values stored in this
+				instance of SnapShot.
+				@param system the system which will be manipulated
+		*/
+		void setAtomForces(System& system) const
 			throw();
 
 		//@}
@@ -213,16 +229,6 @@ namespace BALL
     */
     double kinetic_energy_; 
 
-    /*_ The number of bytes needed for writing the snapshot
-        data section to a file 
-    */
-    Size data_length_; 
-
-    /*_ The total number of bytes needed for writing the snapshot 
-        to a file 
-    */
-    Size total_length_;
-
     //_ An STL vector with the positions of all atoms 
     vector<Vector3> atom_positions_;
 
@@ -239,234 +245,12 @@ namespace BALL
 
 	}; // Snapshot 
 
+	/// Output stream operator for SnapShots
+	::std::ostream& operator << (::std::ostream& os, const SnapShot& ss);
 
-  /**	Snapshot management for MD simulations.
-			This class manages a list of single snapshot objects.
-			Snapshots are numbered starting with 1.	\\
-			{\bf Definition:}\URL{BALL/MOLMEC/COMMON/snapShot.h}
-	*/
-  class SnapShotManager
-  {
+	/// Input stream operator for SnapShots
+	::std::istream& operator >> (::std::istream& is, SnapShot& ss);
 
-    public:
-
-    ///  Local class for handling options
-    struct Option
-    {
-      /** After how many snapshots shall they be flushed to the hard disk 
-					@see Default::FLUSH_TO_DISK_FREQUENCY
-					@param frequency integer
-      */
-      static const char* FLUSH_TO_DISK_FREQUENCY; 
-    };
-
-    /// Local class for handling default values for the options
-    struct Default
-    {
-      /** This is the default value of the number of snapshots to be taken
-					before writing them to hard disk. Default is ...
-					@see Option::FLUSH_TO_DISK_FREQUENCY 
-			*/
-      static const unsigned int FLUSH_TO_DISK_FREQUENCY; 
-    }; 
-
-
-    BALL_CREATE(SnapShotManager)
-
-
-    /// @name Constructors and Destructors
-    //@{
-
-    /// Default constructor
-    SnapShotManager()
-			throw();
-
-    /** This constructor expects a valid system, a valid force field
-				and the name of a snapshot file. If the {\tt overwrite} is true
-				then any existing file of the given name will be overwritten,
-				otherwise the new data will be appended, provided that the systems match. 
-				@param my_system the system to bind this manager to
-				@param my_force_field the force field that is bound to the system
-				@param my_snapshot_file 
-				@param overwrite
-    */
-    SnapShotManager
-			(const System* my_system, const ForceField* my_force_field,
-			 TrajectoryFile* my_snapshot_file, bool overwrite = true)
-			throw();
-
-    /** This constructor expects a valid system, a valid force field
-				and the name of a snapshot file. If the {\tt overwrite} is true
-				then any existing file of the given name will be overwritten,
-				otherwise the new data will be appended, provided that the systems match. 
-				@param my_system the system to bind this manager to
-				@param my_force_field the force field that is bound to the system
-				@param my_options
-				@param filename the name of the snapshot file
-				@param overwrite {\bf true}: overwrite existing snapshot file, 
-												 {\bf false}: append to the file.
-    */
-    SnapShotManager	
-			(const System* my_system, const ForceField* my_force_field,
-			 const Options& my_options, TrajectoryFile* file, bool overwrite = true)
-			throw();
-
-    /// Copy constructor.
-    SnapShotManager(const SnapShotManager& manager)
-			throw();
-
-    /// Destructor.
-    virtual ~SnapShotManager()
-			throw();
-
-    //@}
-    /// @name Setup methods
-    //@{
-
-		/** The setup method does all preparations necessary for using the
-				SnapshotManager.
-				@return true, if setup was succesful
-    */
-    virtual bool setup()
-			throw();
-
-    //@}
-    /** @name Assignment
-    */
-    //@{
-
-    /** The assignment operator.  
-    */
-    const SnapShotManager& operator = (const SnapShotManager& manager)
-			throw();
-
-		/// Clear method
-		virtual void clear()
-			throw();
-
-    //@}
-    /** @name Debugging and diagnostics
-    */
-    //@{
-
-    /** Is the SnapshotManager ready for use?
-    */
-    virtual bool isValid() const
-			throw();
-
-    //@}
-    /** @name Accessors
-    */
-
-    //@{
-
-    /** Set the frequency for saving snapshots to hard disk. 
-        Every 'number' iterations, a save will be  done.         
-				@param number the number of snapshots to take before flushing to disk
-    */
-    void setFlushToDiskFrequency(Size number)
-			throw();
-
-    /** Get the current frequency for doing saves to hard disk
-				@return the number of snapshots to take before flushing them to disk
-    */
-    Size getFlushToDiskFrequency() const
-			throw();
-
-    /** This method takes a snapshot of the system's current state and stores
-				it in main memory. If there is not sufficient space, the snapshots
-				collected so far are flushed to hard disk. The first snapshot taken
-				has index 1.
-    */
-    virtual void takeSnapShot()
-			throw(Exception::OutOfMemory);
-
-    /** This method writes all snapshots taken so far to hard disk
-    */
-    virtual void flushToDisk()
-			throw();
-
-    /** This method returns the total number of snapshots (sum of
-        snapshots in memory and on disk).
-    */ 
-    Size getNumberOfSnapShots() const 
-			throw();
-
-		/// Obtain the number of atoms covered by this SnapshotManager 
-		Size getNumberOfAtoms() const
-			throw();
-
-    //@}
-    /** @name Public Attributes
-    */
-    //@{
-
-    /** The available options for this class.
-    */
-    Options options; 
-
-    //@} 
-
-    protected:
-
-    /*_ @name Protected Attributes
-    */
-    //@{
-
-    /*_ The system to which the SnapshotManager is bound
-    */
-    const System* system_ptr_; 
-
-    /*_ The force field of the current system
-    */
-    const ForceField* force_field_ptr_; 
-
-    /*_ The total number of snapshots 
-    */
-    Size snapshot_counter_;
-
-		// Baustelle: Warum? Steht doch im System.
-    /*_ the number of atoms in the system
-    */
-    Size number_of_atoms_; 
-
-    /*_ A vector containing those snapshot objects currently in memory
-    */
-    vector<SnapShot> snapshot_list_; 
-
-		/*_ The trajectory file where the data is saved in
-		*/
-		TrajectoryFile* trajectory_file_;
-
-    /*_ The frequency of saving snapshots in memory to disk.
-        After flush_to_disk_frequency_ iterations, a save is done. 
-    */
-		// BAUSTELLE: Gehoert das hierher? Macht das Åberhaupt Sinn? Werden denn
-		// bei einem Flush alle bis dahin noch nicht gespeicherten Snapshots
-		// gespeichert oder nur der letzte, also damit eine Art Zeitraffer
-		// generiert?
-    Size flush_to_disk_frequency_; 
-
-		/*_
-		*/
-		Size buffer_counter_;
-
-    /*_  This status flag indicates if the class is ready for use
-    */
-    bool valid_;
-
-    //@}
-    /*_ @name Protected methods
-    */
-    //@{
-
-    /*_ Calculate the kinetic energy 
-    */
-    double calculateKineticEnergy_();
-
-    //@}
-
-	}; // end of class SnapshotManager 
 
 } // end of namespace BALL  
 
