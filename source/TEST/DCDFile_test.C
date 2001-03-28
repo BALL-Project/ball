cef93ceb32d7fb74b25e26120e82109e0d4ab335
anker <anker>
2001-03-28 13:00:55 +0000

    added some trivial tests

--- source/TEST/DCDFile_test.C
+++ source/TEST/DCDFile_test.C
@@ -1,15 +1,14 @@
-// $Id: DCDFile_test.C,v 1.1 2000/12/19 13:25:13 anker Exp $
+// $Id: DCDFile_test.C,v 1.2 2001/03/28 13:00:55 anker Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
 
 // insert includes here
 #include <BALL/FORMAT/DCDFile.h>
-#include <BALL/MOLMEC/MDSIMULATION/molecularDynamics.h>
 
 ///////////////////////////
 
-START_TEST(class_name, "$Id: DCDFile_test.C,v 1.1 2000/12/19 13:25:13 anker Exp $")
+START_TEST(class_name, "$Id: DCDFile_test.C,v 1.2 2001/03/28 13:00:55 anker Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -17,72 +16,108 @@ START_TEST(class_name, "$Id: DCDFile_test.C,v 1.1 2000/12/19 13:25:13 anker Exp 
 using namespace BALL;
 
 ///  insert tests for each member function here         
-///
 
-CHECK(DCDHeader::DCDHeader())
-	// BAUSTELLE
+String dcd_test_file("data/DCD_test.dcd");
+
+CHECK(DCDFile::DCDHeader::DCDHeader() throw())
+  DCDFile::DCDHeader* p = new DCDFile::DCDHeader;
+	TEST_NOT_EQUAL(p, 0)
 RESULT
 
-CHECK(DCDHeader::operator << (os, header))
-	// BAUSTELLE
+DCDFile* p = new DCDFile;
+
+CHECK(DCDFile::DCDFile() throw())
+  p = new DCDFile;
+	TEST_NOT_EQUAL(p, 0)
 RESULT
 
-CHECK(DCDHeader::operator >> (os, header))
-	// BAUSTELLE
+
+CHECK(DCDFile::~DCDFile() throw())
+	delete p;
 RESULT
 
-CHECK(DCDFile::DCDFile())
-	// BAUSTELLE
+
+CHECK(DCDFile::DCDFile(const DCDFile& file) throw())
+  DCDFile first_file;
+	DCDFile second_file(first_file);
+	bool test = (first_file == second_file);
+	TEST_EQUAL(test, true)
 RESULT
 
-CHECK(DCDFile::DCDFile(const DCDFile& file))
-	// BAUSTELLE
+
+CHECK(DCDFile::DCDFile(const String& name, File::OpenMode open_mode) throw())
+  //BAUSTELLE
+  DCDFile test_file(dcd_test_file, File::IN);
+	TEST_EQUAL(test_file.isOpen(), true)
+	TEST_EQUAL(test_file.getOpenMode(), File::IN)
+	test_file.close();
+	TEST_EQUAL(test_file.isOpen(), false)
+
+	DCDFile test_file2("test.out.dcd", File::OUT);
+	TEST_EQUAL(test_file2.isOpen(), true)
+	TEST_EQUAL(test_file2.getOpenMode(), File::OUT)
+	test_file2.close();
+	TEST_EQUAL(test_file2.isOpen(), false)
 RESULT
 
-CHECK(DCDFile::DCDFile(const String& name, File::OpenMode open_mode))
-	// BAUSTELLE
+
+CHECK(DCDFile::DCDFile& operator = (const DCDFile& file) throw())
+  DCDFile one(dcd_test_file, File::IN);
+	DCDFile two;
+	two = one;
+	bool test = (one == two);
+	TEST_EQUAL(test, true)
 RESULT
 
-CHECK(DCDFile::~DCDFile)
-	// BAUSTELLE
+
+CHECK(DCDFile::clear() throw())
+	DCDFile file(dcd_test_file, File::IN);
+	file.clear();
+	DCDFile empty;
+	bool test = (file == empty);
+	TEST_EQUAL(test, true)
 RESULT
 
-CHECK(DCDFile::operator = (const DCDFile& file))
-	// BAUSTELLE
+
+CHECK(DCDFile::bool operator == (const DCDFile& file) throw())
+  DCDFile one(dcd_test_file, File::IN);
+  DCDFile two(dcd_test_file, File::IN);
+	bool test = (one == two);
+	TEST_EQUAL(test, true)
+	DCDFile three;
+	test = (one == three);
+	TEST_EQUAL(test, false)
 RESULT
 
-CHECK(DCDFile::clear())
-	// BAUSTELLE
+
+CHECK(DCDFile::init() throw())
+  //BAUSTELLE
 RESULT
 
-CHECK(DCDFile::operator == (const DCDFile& file))
-	// BAUSTELLE
+
+CHECK(DCDFile::readHeader() throw())
+  //BAUSTELLE
 RESULT
 
-CHECK(DCDFile::readHeader())
-	// BAUSTELLE
+
+CHECK(DCDFile::writeHeader() throw())
+  //BAUSTELLE
 RESULT
 
-CHECK(DCDFile::updateHeader(const SnapShotManager& manager))
-	// BAUSTELLE
+
+CHECK(DCDFile::append(const SnapShot& snapshot) throw())
+  //BAUSTELLE
 RESULT
 
-CHECK(DCDFile::writeHeader())
-	// BAUSTELLE
+
+CHECK(DCDFile::read(SnapShot& snapshot) throw())
+  //BAUSTELLE
 RESULT
 
-CHECK(DCDFile::read(SnapShotManager& manager))
-	// BAUSTELLE
-RESULT
 
-CHECK(DCDFile::write(const SnapShotManager& manager))
-	// BAUSTELLE
+CHECK(DCDFile::flushToDisk(const ::std::vector<SnapShot> buffer) throw())
+  //BAUSTELLE
 RESULT
-
-CHECK(DCDFile::append(const SnapShot& snapshot))
-	// BAUSTELLE
-RESULT
-
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
