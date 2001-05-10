38bd6dfc2612e326f2a93a346f80a87111a35fbb
oliver <oliver>
2001-05-10 13:47:30 +0000

    fixed: some systems (e.g. Tru64) do not define a consistent maximum   path length (file system dependent!). So we have to define a good guess   for ourselves....

--- include/BALL/SYSTEM/fileSystem.h
+++ include/BALL/SYSTEM/fileSystem.h
@@ -1,4 +1,4 @@
-// $Id: fileSystem.h,v 1.2 2000/01/06 09:46:05 oliver Exp $
+// $Id: fileSystem.h,v 1.3 2001/05/10 13:47:30 oliver Exp $
 
 #ifndef BALL_SYSTEM_FILESYSTEM_H
 #define BALL_SYSTEM_FILESYSTEM_H
@@ -9,6 +9,15 @@
 
 #include <limits.h>
 #include <sys/param.h>
+
+// if PATH_MAX is undefined (which sometimes happens on systems
+// where there is no unambiguous value defined due to severel differing
+// filesystems), we define it for ourselves to be 1kB
+#ifndef PATH_MAX
+# define BALL_PATH_MAX 1023
+#else
+# define BALL_PATH_MAX PATH_MAX
+#endif
 
 #ifndef BALL_DATATYPE_STRING_H
 #	include <BALL/DATATYPE/string.h>
@@ -49,7 +58,7 @@ namespace BALL
 			MAX_FILENAME_LENGTH = 256, // NAME_MAX seems to be too restrictive for modern Unixes
 			/**
 			*/
-			MAX_PATH_LENGTH = PATH_MAX
+			MAX_PATH_LENGTH = BALL_PATH_MAX
 		};
 
 		/**	@name Miscellaneous 
