7f9a09fa0bee6bedb93fa654fabf78ca26b5be00
amoll <amoll>
2001-05-26 12:25:24 +0000

    complete overhaul

--- source/config/testfile.py
+++ source/config/testfile.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-import sys, string
+
+# script to search for common problems in BALL-files
+import sys, string, re
+
+if len(sys.argv) <2:
+	print 'zu wenige argumente'
+	sys.exit(-1)
 
 class test:
 	errors=0
@@ -7,17 +13,51 @@ class test:
 	lastline=''
 	linenr= -1
 	f = open(sys.argv[1])
+
+	# expressions for use in all files.
+	exp = [\
+		re.compile('.*BAUSTELLE.*'),												#good old BAUSTELLE
+		re.compile('cout'),																	#no cout in BALL!
+		re.compile('cerr'),																	#no cerr in BALL!
+		re.compile('[\s(]+int[\s\(\)\&\*]+'),								#integer values are bad!
+		re.compile('[\s(]+long[\s\(\)\&\*]+'),              #long values are
+		re.compile('[\s(]+short[\s\(\)\&\*]+'),							#short values are bad!
+		re.compile(';{2}'),																	#;;
+		re.compile('}[\s]*else[\s]*{'),											#} else {
+		re.compile('throw[\s]*Exception::NotImplemented'),  #Exception Not Implemented
+		re.compile('[^:]std::endl'),                        #missing :: before std::endl
+		re.compile('\([\s]*bool[\s]*\)'),										#superflous bool cast
+		re.compile('const[\s]*float[\s]*&'),								#no const float references
+		re.compile('const[\s]*double[\s]*&')								#no const double references
+	]
 	
-	def write(self):
+	# expressions for use in test-files
+	#exp_test = []																				#not yet an idea
+
+	exp_check = [
+		re.compile('RESULT'),
+		re.compile('\w*[^\s]\w*'),
+		re.compile('BAUSTELLE')
+	]
+
+	# expressions for use with header-files
+	exp_header = [
+		re.compile('///[\s]*\Z'),														#empty comment
+		re.compile('@exception[\s]*NotImplemented'),				#no usefull information
+		re.compile('@param[\s]*{'),													#standard problem => tex error
+		re.compile('@return[\s]*{') 												#standard problem => tex error
+	]
+
+	def write(self, error_code):
 		self.errors = self.errors + 1
 		self.linenr = self.linenr + 1
 		self.lastline = self.line
 		self.line = self.f.readline()
-		print	
-		print '------------ ' + `self.linenr` + ' ---------------'
+		print
+		print '------------ ' + `self.linenr` + ' --------------- ' + `error_code`
 		self.lastline = string.strip(self.lastline)
 		self.line = string.strip(self.line)
-		if self.line_has('*/'):
+		if string.find(self.line, '*/') != -1:
 			print self.lastline + '    ' + self.line
 			self.lastline = self.line
 			self.getLine()
@@ -31,64 +71,85 @@ class test:
 	def ende(self):
 		self.f.close()
 		print
-		sys.exit(1)	 	
-	
-	def line_has(self, x):
-		return string.find(self.line, x) != -1
- 	
+		sys.exit(self.errors)
+
+	def test_line(self, x):
+		for i in range(len(self.exp)):
+			if self.exp[i].search(x, 0):
+				self.errors = self.errors + 1	
+				return i
+		return 0
+
 	def getLine(self):
 		self.linenr = self.linenr + 1
 		self.lastline = self.line
 		self.line = self.f.readline()
-		if not self.line: sys.exit(1)
+		if not self.line: return 0
 		self.line = string.strip(self.line[0:-1])
-		if self.line_has('BAUSTELLE')\
-			or self.line_has('throw Exception::NotImplemented(__FILE__, __line__)') \
-			or self.line_has('cout <<'):
-			self.write()
+		erg = self.test_line(self.line)
+		if erg != 0:
+			self.write(erg)
 		return 1
 
 	def BALL_TEST_TEST(self):
 		while self.getLine() == 1:
 			if self.line[:5] =='CHECK':
 				self.getLine()
-				if self.line_has('RESULT') or self.line == '' or self.line_has('BAUSTELLE'):
-					self.write()
-		ende()
- 	
+				for i in range(len(self.exp_check)):
+					if self.exp_check[i].search(self.line, 0):
+						self.errors = self.errors + 1	
+						self.write('test')		
+		self.ende()
+
 	def BALL_HEADER_TEST(self):
 		WOERTER=[]
 		while self.getLine() == 1:
-			if (self.line_has('///') and not self.line_has('////')) or self.line_has('/**'):
-				WOERTER = string.split(self.line)
-				if len(WOERTER) < 2:
-					self.write()
-		ende()
- 	
+			for i in range(len(self.exp_header)):
+				if self.exp_header[i].search(self.line, 0):
+					self.errors = self.errors + 1	
+					self.write('docu')
+		self.ende()
+
 	def BALL_ALL_TEST(self):
 		while self.getLine() == 1:
 			pass
-		ende()	
+		self.ende()
 
+	def debug(self):
+		while self.getLine() == 1:
+			# Header tests
+			for i in range(len(self.exp_header)):
+				if self.exp_header[i].search(self.line, 0):
+					self.errors = self.errors + 1
+					print '  H ', i
+					
+			# Testfile tests	
+			for i in range(len(self.exp_test)):
+				if self.exp_test[i].search(self.line, 0):
+					self.errors = self.errors + 1
+					print '  T ', i
 
-	def __init__(self, filename):
-		global f
-		#f = open(filename)	
-	
+	def __init__(self):
+		# file for debuging this script
+		if string.find(sys.argv[1], 'test.file') != -1:
+			self.debug()
+			sys.exit(self.errors)
+			
 		# file is a TEST-FILE
-		if string.find(filename, '_test.C') != -1:
+		if string.find(sys.argv[1], '_test.C') != -1:
 			self.BALL_TEST_TEST()
+			sys.exit(self.errors)
 			
-		# file is a HEADER-FILE	
-		if string.find(filename, '.h') != -1:
+		# file is a HEADER-FILE
+		if string.find(sys.argv[1], '.h') != -1:
 			self.BALL_HEADER_TEST()
-			
-		# general testing		
+			sys.exit(self.errors)
+
+		# general testing
 		self.BALL_ALL_TEST()
+		sys.exit(self.errors)
 
 
-if len(sys.argv) <2:
-	print 'zu wenige argumente'
-	sys.exit(-1)
-t = test(sys.argv[1])
+
+t = test()
 sys.exit(t.errors)
