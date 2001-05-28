d4c6a87bd2e8c1a306c6b90f57f89bc9e049b5d1
anker <anker>
2001-05-28 11:59:24 +0000

    cosmetic changes

--- source/TEST/Socket_test.C
+++ source/TEST/Socket_test.C
@@ -1,4 +1,4 @@
-// $Id: Socket_test.C,v 1.4 2000/01/14 20:34:48 oliver Exp $
+// $Id: Socket_test.C,v 1.5 2001/05/28 11:59:24 anker Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -6,7 +6,7 @@
 #include <unistd.h>
 ///////////////////////////
 
-START_TEST(Socket, "$Id: Socket_test.C,v 1.4 2000/01/14 20:34:48 oliver Exp $")
+START_TEST(Socket, "$Id: Socket_test.C,v 1.5 2001/05/28 11:59:24 anker Exp $")
 using namespace BALL;
 
 
@@ -15,31 +15,36 @@ using namespace BALL;
 
 
 CHECK(simple socket transmission)
-SockInetBuf sock_inet_buf(SocketBuf::sock_stream);
-sock_inet_buf.bind();
-char c;
-int port = sock_inet_buf.localport();
-STATUS("localport = " << sock_inet_buf.localport())
-STATUS("localhost = " << sock_inet_buf.localhost())
-if (fork())
-{	
-	STATUS("A:listen...")
-  sock_inet_buf.listen();
-	STATUS("A:accept...")
-  IOStreamSocket  s(sock_inet_buf.accept());
-	STATUS("A:get...")
-	s.get(c);
-	STATUS("S:done")
-} else {
-	sleep(1);
-	IOStreamSocket  sio(SocketBuf::sock_stream);
-	int result = sio->connect(sock_inet_buf.localhost(), port);
-	STATUS("B:connect = " << result)
-	sio.put((char)123);
-	STATUS("B:done.")
-	return 0;
-}
-TEST_EQUAL((int)c, (int)123);
+	SockInetBuf sock_inet_buf(SocketBuf::sock_stream);
+	sock_inet_buf.bind();
+	char c;
+	int port = sock_inet_buf.localport();
+	STATUS("localport = " << sock_inet_buf.localport())
+	STATUS("localhost = " << sock_inet_buf.localhost())
+
+	if (fork())
+	{	
+		STATUS("A:listen...")
+		sock_inet_buf.listen();
+		STATUS("A:accept...")
+		IOStreamSocket  s(sock_inet_buf.accept());
+		STATUS("A:get...")
+		s.get(c);
+		s.get(c);
+		STATUS("A:done")
+	} 
+	else
+	{
+		sleep(1);
+		IOStreamSocket  sio(SocketBuf::sock_stream);
+		int result = sio->connect(sock_inet_buf.localhost(), port);
+		STATUS("B:connect = " << result)
+		sio.put((char)123);
+		STATUS("B:done.")
+		return 0;
+	}
+
+	TEST_EQUAL((int)c, (int)123);
 RESULT
 	
 
