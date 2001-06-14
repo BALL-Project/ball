e9e33073379e9c2eaca06ada0c5d637f39b9d838
oliver <oliver>
2001-06-14 11:34:59 +0000

    changed: now a generic implementation of 1/2/3D peaks!

--- source/NMR/peak.C
+++ source/NMR/peak.C
@@ -1,94 +1,25 @@
-// $Id: peak.C,v 1.8 2000/09/23 17:52:05 amoll Exp $
+// $Id: peak.C,v 1.9 2001/06/14 11:34:59 oliver Exp $
 
 #include<BALL/NMR/peak.h>
-#include<BALL/MATHS/common.h>
-
-using namespace std;
 
 namespace BALL
 {
 
-	Peak1D::Peak1D()
-		: value_(0),
-			width_(0.1),
-			height_(1),
-			atom_(0)
+	template <>
+	bool Peak<Vector3>::operator < (const Peak<Vector3>& peak) const
 	{
+		return ((position_.x < peak.position_.x) 
+						|| ((position_.x == peak.position_.x) && 
+								((position_.y < peak.position_.y) 
+								 || (position_.y == peak.position_.y) && (position_.z < peak.position_.z))));
 	}
 
-	Peak1D::Peak1D(const Peak1D& peak)
-		: value_(peak.value_),
-			width_(peak.width_),
-			height_(peak.height_),
-			atom_(peak.atom_)
+	template <>
+	bool Peak<Vector3>::operator > (const Peak<Vector3>& peak) const
 	{
+		return ((position_.x > peak.position_.x) 
+						|| ((position_.x == peak.position_.x) && 
+								((position_.y > peak.position_.y) 
+								 || (position_.y == peak.position_.y) && (position_.z > peak.position_.z))));
 	}
-
-	Peak1D::~Peak1D()
-	{
-	}
-
-	float Peak1D::getValue() const
-	{
-		return value_;
-	}
-
-	float Peak1D::getWidth() const
-	{
-		return width_;
-	}
-
-	float Peak1D::getHeight() const
-	{
-		return height_;
-	}
-
-	const Atom* Peak1D::getAtom() const
-	{
-		return atom_;
-	}
-
-	void Peak1D::setValue(const float& value)
-	{
-		value_ = value;
-	}
-
-	void Peak1D::setWidth(const float& value)
-	{
-		width_ = value;
-	}
-
-	void Peak1D::setAtom(const Atom* atom)
-	{
-		atom_ = atom;
-	}
-
-	void Peak1D::setHeight(const float& value)
-	{
-		height_ = value;
-	}
-
-	void Peak1D::operator = (const Peak1D& peak) 
-	{
-		value_ = peak.value_;
-		width_ = peak.width_;
-		height_ = peak.height_;
-		atom_ = peak.atom_;
-	}
-
-	bool Peak1D::operator == (const Peak1D& peak) const
-	{
-		return Maths::isEqual(value_, peak.value_);
-	}
-
-	bool Peak1D::operator < (const Peak1D& peak) const
-	{
-		return Maths::isLess(value_, peak.value_);
-	}
-
-	std::ostream& operator << (std::ostream& os, const Peak1D& peak)
-	{
-		return os << peak.getValue();
-	}
-
-}	// namespace BALL
+} // namespace BALL
