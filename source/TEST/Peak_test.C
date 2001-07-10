52f22032b16885934ba69f74d815e721b172e75d
oliver <oliver>
2001-07-10 10:39:24 +0000

    *** empty log message ***

--- source/TEST/Peak_test.C
+++ source/TEST/Peak_test.C
@@ -1,4 +1,4 @@
-// $Id: Peak_test.C,v 1.4 2001/06/14 12:01:09 oliver Exp $
+// $Id: Peak_test.C,v 1.5 2001/07/10 10:39:24 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -10,7 +10,7 @@
 
 ///////////////////////////
 
-START_TEST(Peak, "$Id: Peak_test.C,v 1.4 2001/06/14 12:01:09 oliver Exp $")
+START_TEST(Peak, "$Id: Peak_test.C,v 1.5 2001/07/10 10:39:24 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -71,12 +71,47 @@ CHECK(Peak1D::void operator = (const Peak1D& peak))
 RESULT
 
 CHECK(Peak1D::bool operator == (const Peak1D& peak) const )
-  //BAUSTELLE
+	Peak1D peak2 = peak;
+	Peak1D peak3 = peak;
+	TEST_EQUAL(peak2 == peak, true)
+	TEST_EQUAL(peak2 == peak3, true)
+	peak3.setWidth(0.0);
+	TEST_EQUAL(peak2 == peak, true)
+	TEST_EQUAL(peak2 == peak3, false)
+	peak3.setWidth(peak2.getWidth());
+	TEST_EQUAL(peak2 == peak3, true)
+	peak3.setIntensity(0.0);
+	TEST_EQUAL(peak2 == peak3, false)
+	peak3.setIntensity(peak2.getIntensity());
+	TEST_EQUAL(peak2 == peak3, true)
+	peak3.setPosition(0.0);
+	TEST_EQUAL(peak2 == peak3, false)
+	peak3.setPosition(peak2.getPosition());
+	TEST_EQUAL(peak2 == peak3, true)
+	peak3.setAtom(0);
+	TEST_EQUAL(peak2 == peak3, false)
+	peak3.setAtom(peak2.getAtom());
+	TEST_EQUAL(peak2 == peak3, true)
 RESULT
 
 CHECK(Peak1D::bool operator < (const Peak1D& peak) const )
-  //BAUSTELLE
+	Peak1D peak2(peak);
+	TEST_EQUAL(peak2 < peak, false)
+	TEST_EQUAL(peak < peak2, false)
+	peak2.setPosition(peak.getPosition() + 1.0);
+	TEST_EQUAL(peak2 < peak, false)
+	TEST_EQUAL(peak < peak2, true)
 RESULT
+
+CHECK(Peak1D::bool operator > (const Peak1D& peak) const )
+	Peak1D peak2(peak);
+	TEST_EQUAL(peak2 > peak, false)
+	TEST_EQUAL(peak > peak2, false)
+	peak2.setPosition(peak.getPosition() + 1.0);
+	TEST_EQUAL(peak2 > peak, true)
+	TEST_EQUAL(peak > peak2, false)
+RESULT
+
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 END_TEST
--- source/TEST/Spectrum_test.C
+++ source/TEST/Spectrum_test.C
@@ -1,4 +1,4 @@
-// $Id: Spectrum_test.C,v 1.2 2001/07/05 18:59:03 oliver Exp $
+// $Id: Spectrum_test.C,v 1.3 2001/07/10 10:39:24 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -11,14 +11,14 @@
 #include <BALL/KERNEL/system.h>
 ///////////////////////////
 
-START_TEST(Spectrum1D, "$Id: Spectrum_test.C,v 1.2 2001/07/05 18:59:03 oliver Exp $")
+START_TEST(Spectrum1D, "$Id: Spectrum_test.C,v 1.3 2001/07/10 10:39:24 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 using namespace std;
-/*  BAUSTELLE
+
 Spectrum1D* p;
 
 CHECK(Spectrum1D::Spectrum1D())
@@ -30,117 +30,6 @@ CHECK(Spectrum1D::~Spectrum1D())
 	delete p;
 RESULT
 
-CHECK(Spectrum1D::setSystem(System* s)/getSystem())	
-	Spectrum1D spectrum;
-	System system;
-	TEST_EQUAL(spectrum.getSystem(), 0)
-	spectrum.setSystem(&system);
-	TEST_EQUAL(spectrum.getSystem(), &system)
-RESULT
-
-CHECK(Spectrum1D::setDensity(Size density)/getDensity())
-	Spectrum1D spectrum;
-	TEST_EQUAL(spectrum.getDensity(), 100)
-	spectrum.setDensity(50);
-	TEST_EQUAL(spectrum.getDensity(), 50)
-	spectrum.setDensity(100);
-RESULT
-
-ShiftModel sm("data/Spectrum1D_test.ini");
-CHECK(Spectrum1D::setShiftModel(const ShiftModel& model))
-	Spectrum1D spectrum;
-	spectrum.setShiftModel(sm);
-RESULT
-
-
-CHECK(Spectrum1D::getShiftModel() const)
-	Spectrum1D spectrum;
-	spectrum.setShiftModel(sm);
-	// TEST_EQUAL(spectrum.getShiftModel(), sm)
-RESULT
-
-HINFile f("data/Spectrum1D_test.hin");
-System system;
-f >> system;
-
-CHECK(Spectrum1D::calculateShifts())
-	Spectrum1D spectrum;
-	spectrum.setShiftModel(sm);
-	spectrum.setSystem(&system);
-  spectrum.calculateShifts();
-RESULT
-
-CHECK(Spectrum1D::createSpectrum())
-	Spectrum1D spectrum;
-	spectrum.setSystem(&system);
-	spectrum.setShiftModel(sm);
-	spectrum.calculateShifts();
-  spectrum.createSpectrum();
-	TEST_EQUAL(spectrum.getPeakList().size(), 873)
-RESULT
-
-list<Peak1D> peaks;
-
-CHECK(Spectrum1D::getPeakList() const)
-	Spectrum1D spectrum;
-	peaks = spectrum.getPeakList();
-	TEST_EQUAL(peaks.size(), 0)
-RESULT
-
-CHECK(Spectrum1D::getSpectrumMin() const )
-	Spectrum1D spectrum;
-	TEST_EQUAL(spectrum.getSpectrumMin(), 0)
-RESULT
-
-CHECK(Spectrum1D::getSpectrumMax() const )
-	Spectrum1D spectrum;
-	TEST_EQUAL(spectrum.getSpectrumMax(), 0) // ???
-RESULT
-
-CHECK(Spectrum1D::setPeakList(const list<Peak1D>& spectrum))
-	Spectrum1D spectrum;
-	spectrum.setPeakList(peaks);
-RESULT
-
-CHECK(Spectrum1D::sortSpectrum())
-	Spectrum1D spectrum;
-	spectrum.sortSpectrum();
-RESULT
-
-Spectrum1D spectrum;
-spectrum.setSystem(&system);
-spectrum.setShiftModel(sm);
-spectrum.calculateShifts();
-spectrum.sortSpectrum();
-spectrum.createSpectrum();
-
-String filename;
-CHECK(Spectrum1D::plotSpectrum(const String& filename) const )
-	NEW_TMP_FILE(filename)
-	spectrum.setDensity(32768);
-	spectrum.plotSpectrum(filename);
-	TEST_FILE(filename.c_str(), "data/Spectrum1D_test.dat", false)
-RESULT
-
-CHECK(Spectrum1D::plotPeaks(const String& filename) const )
-	NEW_TMP_FILE(filename)
-	spectrum.plotPeaks(filename); 
-	TEST_FILE(filename.c_str(), "data/Spectrum1D_test/plotPeaks.txt", false)
-RESULT
-
-CHECK(Spectrum1D::writePeaks(const String& filename) const )
-	NEW_TMP_FILE(filename)
-	spectrum.writePeaks(filename); 
-	TEST_FILE(filename.c_str(), "data/Spectrum1D_test/writePeaks.txt", false)
-RESULT
-
-CHECK(Spectrum1D::makeDifference(const float&, const String&, const String&, const String&))
-  //BAUSTELLE
-RESULT
-*/
-CHECK(Spectrum1D::setDifference(Spectrum1D*, Spectrum1D*, String, String))
-  //BAUSTELLE
-RESULT
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
