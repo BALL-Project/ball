b70fc725587580d111f736ed8f5411976493433a
amoll <amoll>
2001-07-10 13:36:25 +0000

    new added

--- source/TEST/SolventDescriptor_test.C
+++ source/TEST/SolventDescriptor_test.C
@@ -1,105 +1,98 @@
-// $Id: SolventDescriptor_test.C,v 1.1 2000/08/31 16:47:24 anker Exp $
+// $Id: SolventDescriptor_test.C,v 1.2 2001/07/10 13:36:25 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
-
+ 
 ///////////////////////////
-
-// insert includes here
 #include <BALL/SOLVATION/solventDescriptor.h>
-
 ///////////////////////////
-
-START_TEST(class_name, "$Id: SolventDescriptor_test.C,v 1.1 2000/08/31 16:47:24 anker Exp $")
-
+	  
+START_TEST(class_name, "$Id: SolventDescriptor_test.C,v 1.2 2001/07/10 13:36:25 amoll Exp $")
+		 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
-
+		  
 using namespace BALL;
+			 
+SolventDescriptor* s_ptr;
+CHECK(Cstr)
+	s_ptr = new SolventDescriptor;
+	TEST_NOT_EQUAL(s_ptr, 0)
+	TEST_EQUAL(s_ptr->getName(), "")
+	TEST_EQUAL(s_ptr->getNumberDensity(), 0)
+	TEST_EQUAL(s_ptr->getSolventAtomDescriptorList().size(), 0)
+	TEST_EQUAL(s_ptr->getNumberOfAtomTypes(), 0)
+	SolventAtomDescriptor sad;
+	TEST_EXCEPTION(Exception::IndexOverflow, s_ptr->getAtomDescriptor(99))
+	TEST_EQUAL(s_ptr->isValid(), false)
+RESULT
 
-///  insert tests for each member function here         
-///
-	
 
-CHECK(SolventDescriptor::SolventDescriptor())
+CHECK(Destructor)
+	delete(s_ptr);
+RESULT
+
+
+CHECK(Cpy-Cstr)
+	//
+RESULT
+
+
+CHECK(SolventDescriptor(const String& name, float number_density, const std::vector<SolventAtomDescriptor>& atom_list))
+	//
+RESULT
+
+CHECK(SolventDescriptor::BALL_CREATE(SolventDescriptor))
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::SolventDescriptor(const SolventDescriptor& solvent))
+CHECK(SolventDescriptor::setName(const String& name) throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::SolventDescriptor(const String& name, float number_density, const std::vector<SolventAtomDescriptor>& atom_list))
+CHECK(SolventDescriptor::getName() const  throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::~SolventDescriptor())
+CHECK(SolventDescriptor::setNumberDensity(float number_density) throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::clear())
+CHECK(SolventDescriptor::getNumberDensity() const  throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::destroy())
+CHECK(SolventDescriptor::setSolventAtomDescriptorList(const std::vector<SolventAtomDescriptor>& solvent_atoms) throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::set(const SolventDescriptor& descriptor))
+CHECK(SolventDescriptor::getSolventAtomDescriptorList() const  throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::SolventDescriptor& operator = (const SolventDescriptor& descriptor))
+CHECK(SolventDescriptor::getNumberOfAtomTypes() const  throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::setName(const String& name))
+CHECK(SolventDescriptor::getAtomDescriptor(Position index) const  throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::getName() const )
+CHECK(SolventDescriptor::isValid() const  throw())
   //BAUSTELLE
 RESULT
 
 
-CHECK(SolventDescriptor::setNumberDensity(float number_density))
+CHECK(SolventDescriptor::bool operator == (const SolventDescriptor& descriptor) const  throw())
   //BAUSTELLE
 RESULT
 
-
-CHECK(SolventDescriptor::getNumberDensity() const )
-  //BAUSTELLE
-RESULT
-
-
-CHECK(SolventDescriptor::setSolventAtomDescriptorList(const std::vector<SolventAtomDescriptor>& solvent_atoms))
-  //BAUSTELLE
-RESULT
-
-
-CHECK(SolventDescriptor::getSolventAtomDescriptorList() const )
-  //BAUSTELLE
-RESULT
-
-
-CHECK(SolventDescriptor::getNumberOfAtomTypes() const )
-  //BAUSTELLE
-RESULT
-
-
-CHECK(SolventDescriptor::getAtomDescriptor(Position index) const )
-  //BAUSTELLE
-RESULT
-
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
+///////////////////////////////
 END_TEST
