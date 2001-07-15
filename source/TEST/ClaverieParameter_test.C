69cf3e9c1d12eb595889be6db3d2dee68bb257f6
anker <anker>
2001-07-15 15:17:13 +0000

    added tests for most methods

--- source/TEST/ClaverieParameter_test.C
+++ source/TEST/ClaverieParameter_test.C
@@ -1,4 +1,4 @@
-// $Id: ClaverieParameter_test.C,v 1.1 2000/08/31 17:56:16 anker Exp $
+// $Id: ClaverieParameter_test.C,v 1.2 2001/07/15 15:17:13 anker Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -8,7 +8,7 @@
 
 ///////////////////////////
 
-START_TEST(class_name, "$Id: ClaverieParameter_test.C,v 1.1 2000/08/31 17:56:16 anker Exp $")
+START_TEST(class_name, "$Id: ClaverieParameter_test.C,v 1.2 2001/07/15 15:17:13 anker Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -20,51 +20,96 @@ using namespace BALL;
 	
 // tests for class ClaverieParameter::
 
-
-// tests for class ClaverieParameter::
+ClaverieParameter* cp_ptr = 0;
 
 CHECK(ClaverieParameter::ClaverieParameter())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(ClaverieParameter::ClaverieParameter(Parameters& parameters))
-  //BAUSTELLE
+	cp_ptr = new ClaverieParameter;
+	TEST_NOT_EQUAL(cp_ptr, 0)
+	::std::vector< ::std::pair<float, float> > test_params;
+	bool test = (test_params == cp_ptr->getParameters());
+	TEST_EQUAL(test, true)
+	HashMap<Atom::Type, Index> test_indices;
+	test = (test_indices == cp_ptr->getIndices());
+	TEST_EQUAL(test, true)
 RESULT
 
 
 CHECK(ClaverieParameter::(const ClaverieParameter& param)())
-  //BAUSTELLE
-RESULT
+	ClaverieParameter cp1;
+	ForceFieldParameters fffparam("data/ClaverieParameter_test.ini");
+	cp1.extractSection(fffparam, "ClaverieParameters");
+	ClaverieParameter cp2;
+	bool test = (cp1 == cp2);
+	TEST_NOT_EQUAL(test, true)
 
-
-CHECK(ClaverieParameter::destroy())
-  //BAUSTELLE
+	ClaverieParameter cp3(cp1);
+	test = (cp1 == cp3);
+	TEST_EQUAL(test, true)
 RESULT
 
 
 CHECK(ClaverieParameter::clear())
-  //BAUSTELLE
-RESULT
+	ClaverieParameter empty;
+	ClaverieParameter nonempty;
+	ForceFieldParameters fffparam("data/ClaverieParameter_test.ini");
+	nonempty.extractSection(fffparam, "ClaverieParameters");
+	bool test = (empty == nonempty);
+	TEST_NOT_EQUAL(test, true)
 
-
-CHECK(ClaverieParameter::set(const ClaverieParameter& param))
-  //BAUSTELLE
+	nonempty.clear();
+	test = (empty == nonempty);
+	TEST_EQUAL(test, true)
 RESULT
 
 
 CHECK(ClaverieParameter::ClaverieParameter& operator = (const ClaverieParameter& param))
-  //BAUSTELLE
+	ClaverieParameter cp1;
+	ForceFieldParameters fffparam("data/ClaverieParameter_test.ini");
+	cp1.extractSection(fffparam, "ClaverieParameters");
+	ClaverieParameter cp2;
+	bool test = (cp1 == cp2);
+	TEST_NOT_EQUAL(test, true)
+
+	cp2 = cp1;
+	test = (cp1 == cp2);
+	TEST_EQUAL(test, true)
 RESULT
 
 
 CHECK(ClaverieParameter::hasParameters(Atom::Type solvent_type, Atom::Type solute_type) const )
-  //BAUSTELLE
+	ClaverieParameter cp;
+	ForceFieldParameters fffparam("data/ClaverieParameter_test.ini");
+	cp.extractSection(fffparam, "ClaverieParameters");
+
+	Atom::Type type_HW = fffparam.getAtomTypes().getType("HW");
+	Atom::Type type_OW = fffparam.getAtomTypes().getType("OW");
+	Atom::Type type_CT = fffparam.getAtomTypes().getType("CT");
+	Atom::Type type_HC = fffparam.getAtomTypes().getType("HC");
+
+	bool test = cp.hasParameters(type_HW, type_CT);
+	TEST_EQUAL(test, true)
+
+	test = cp.hasParameters(type_HC, type_OW);
+	TEST_EQUAL(test, true)
 RESULT
 
 
 CHECK(ClaverieParameter::getParameters(Atom::Type solvent_type, Atom::Type solute_type) const )
-  //BAUSTELLE
+	ClaverieParameter cp;
+	ForceFieldParameters fffparam("data/ClaverieParameter_test.ini");
+	cp.extractSection(fffparam, "ClaverieParameters");
+
+	Atom::Type type_HW = fffparam.getAtomTypes().getType("HW");
+	Atom::Type type_OW = fffparam.getAtomTypes().getType("OW");
+	Atom::Type type_CT = fffparam.getAtomTypes().getType("CT");
+	Atom::Type type_HC = fffparam.getAtomTypes().getType("HC");
+
+	::std::pair<float, float> params;
+	
+	params = cp.getParameters(type_HW, type_CT);
+	PRECISION(0.0001)
+	TEST_REAL_EQUAL(params.first, 1.0)
+	TEST_REAL_EQUAL(params.second, 2.85657)
 RESULT
 
 
