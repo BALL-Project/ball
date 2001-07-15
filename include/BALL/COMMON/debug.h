f39b603fd8442eb1de6a10c9b525e408599f7d98
oliver <oliver>
2001-07-15 15:12:02 +0000

    removed: obsolete macro definition (BALL_PRECONDITION and related stuff)

--- include/BALL/COMMON/debug.h
+++ include/BALL/COMMON/debug.h
@@ -1,4 +1,4 @@
-// $Id: debug.h,v 1.2 1999/10/30 12:53:14 oliver Exp $
+// $Id: debug.h,v 1.3 2001/07/15 15:12:02 oliver Exp $
 
 #ifndef BALL_COMMON_DEBUG_H
 #define BALL_COMMON_DEBUG_H
@@ -13,15 +13,4 @@
 #	define BALL_INLINE inline
 #endif
 
-#define BALL_PRECONDITION(a,b)\
-  if (!(a))\
-	{\
-    ::std::cerr << "Failed: " << #a << "/" << #b << ::std::endl << "(" << __FILE__ << ":" << __LINE__ << ")" << ::std::endl;\
-    throw Exception::GeneralException(__FILE__, __LINE__);\
-  }\
-
-#define BALL_NO_CONDITION true
-
-#define BALL_PRECOND(a)
- 
 #endif // BALL_COMMON_DEBUG_H
