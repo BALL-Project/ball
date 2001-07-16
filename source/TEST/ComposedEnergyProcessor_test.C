33761bc33663750acab5afa6733b33a58c48b467
amoll <amoll>
2001-07-16 11:14:31 +0000

    added and fixed all tests

--- source/TEST/ComposedEnergyProcessor_test.C
+++ source/TEST/ComposedEnergyProcessor_test.C
@@ -1,15 +1,13 @@
-// $Id: ComposedEnergyProcessor_test.C,v 1.3 2001/07/16 00:34:48 amoll Exp $
+// $Id: ComposedEnergyProcessor_test.C,v 1.4 2001/07/16 11:14:31 amoll Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
 #include <BALL/ENERGY/composedEnergyProcessor.h>
-#include <BALL/SOLVATION/pair6_12InteractionEnergyProcessor.h>
 #include <BALL/FORMAT/HINFile.h>
 #include <BALL/KERNEL/system.h>
-#include <BALL/MOLMEC/COMMON/radiusRuleProcessor.h>
 ///////////////////////////
 
-START_TEST(ComposedEnergyProcessor_test, "$Id: ComposedEnergyProcessor_test.C,v 1.3 2001/07/16 00:34:48 amoll Exp $")
+START_TEST(ComposedEnergyProcessor_test, "$Id: ComposedEnergyProcessor_test.C,v 1.4 2001/07/16 11:14:31 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -29,20 +27,27 @@ using namespace BALL;
     : change(0)
   {}
     
+	virtual bool start() throw()
+	{
+		numberOfFragments = 0;
+		return true;
+	}
+	
   virtual Processor::Result operator () (AtomContainer& fragment) throw() 
   {
-    change += 1;
+    numberOfFragments += 1;
     EnergyProcessor::operator() (fragment);
     return Processor::CONTINUE;
   }
   
   virtual bool finish() throw() 
   {
-    energy_ = fragment_->countAtoms() * change; 
+    energy_ = fragment_->countAtoms() * (numberOfFragments + change); 
     return true;
   }
 
   float change; 
+	float numberOfFragments;
 };
 
 
@@ -68,30 +73,15 @@ CHECK(Preperations)
 	TEST_EQUAL(pep2->getEnergy(), (S.countAtomContainers() + 2 + 1) * S.countAtoms()) 	
 	TEST_EQUAL(pep2->getEnergy(), 248)
 	result = pep1->getEnergy() + pep2->getEnergy();
-
-	pep1->change = 1.0;
-	pep2->change = 2.0;
  RESULT
 
 
 CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor())
 	ComposedEnergyProcessor* cep = new ComposedEnergyProcessor;
 	TEST_NOT_EQUAL(cep, 0)
-	TEST_EQUAL(cep->isValid(), false)
+	TEST_EQUAL(cep->isValid(), true)
 	TEST_REAL_EQUAL(cep->getEnergy(), 0)
 RESULT
-
-ComposedEnergyProcessor cep;
-
-CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor(const ComposedEnergyProcessor& composed_energy_proc))
-  //BAUSTELLE
-RESULT
-
-
-CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor(EnergyProcessorList proc_list))
-  //BAUSTELLE
-RESULT
-
 
 CHECK(ComposedEnergyProcessor::~ComposedEnergyProcessor())
 	ComposedEnergyProcessor* cep = new ComposedEnergyProcessor;
@@ -99,36 +89,20 @@ CHECK(ComposedEnergyProcessor::~ComposedEnergyProcessor())
 RESULT
 
 
-CHECK(ComposedEnergyProcessor::destroy())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(ComposedEnergyProcessor::clear())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(ComposedEnergyProcessor::set(const ComposedEnergyProcessor& proc))
-  //BAUSTELLE
-RESULT
-
-
-CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor& operator = 
-			(const ComposedEnergyProcessor& proc))
-  //BAUSTELLE
-RESULT
-
+ComposedEnergyProcessor cep;
 
 CHECK(ComposedEnergyProcessor::addComponent(EnergyProcessor* proc))
 	cep.addComponent(pep1);
 	cep.addComponent(pep2);
+	TEST_EQUAL(cep.getNumberOfEnergyProcessors(), 2)
 RESULT
 
 
 CHECK(ComposedEnergyProcessor::removeComponent(EnergyProcessor* proc))
   cep.removeComponent(pep2);
+	TEST_EQUAL(cep.getNumberOfEnergyProcessors(), 1)
 	cep.addComponent(pep2);
+	TEST_EQUAL(cep.getNumberOfEnergyProcessors(), 2)
 RESULT
 
 
@@ -138,6 +112,46 @@ CHECK(ComposedEnergyProcessor::finish())
 RESULT
 
 
+ComposedEnergyProcessor cep2;
+CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor(EnergyProcessorList proc_list))
+  EnergyProcessorList epl;
+	epl.push_back(pep1);
+	epl.push_back(pep2);
+	cep2 = ComposedEnergyProcessor(epl);
+	TEST_EQUAL(cep2.isValid(), true)
+	TEST_EQUAL(cep2.getNumberOfEnergyProcessors(), 2)
+	S.apply(cep2);
+
+	TEST_EQUAL(cep2.isValid(), true)
+	TEST_REAL_EQUAL(cep2.getEnergy(), result)
+RESULT
+
+
+CHECK(ComposedEnergyProcessor::clear())
+  cep2.clear();
+	cep2 = cep;
+	TEST_REAL_EQUAL(cep2.getEnergy(), result)
+	cep2.clear();
+	TEST_REAL_EQUAL(cep2.getEnergy(), 0)
+RESULT
+
+
+CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor& operator = 
+			(const ComposedEnergyProcessor& proc))
+  cep2.clear();
+	cep2 = cep;
+	TEST_REAL_EQUAL(cep2.getEnergy(), result)
+	S.apply(cep2);
+	TEST_REAL_EQUAL(cep2.getEnergy(), result)
+RESULT
+
+
+CHECK(ComposedEnergyProcessor::ComposedEnergyProcessor(const ComposedEnergyProcessor& composed_energy_proc))
+  ComposedEnergyProcessor cep2(cep);
+	TEST_REAL_EQUAL(cep2.getEnergy(), result)
+	S.apply(cep2);
+	TEST_REAL_EQUAL(cep2.getEnergy(), result)
+RESULT
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
