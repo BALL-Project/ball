af4b200cfab9ef22064d380ac904f24f9c79f923
oliver <oliver>
2001-07-16 02:21:36 +0000

    added: test for new ctor

--- source/TEST/Vector4_test.C
+++ source/TEST/Vector4_test.C
@@ -1,11 +1,11 @@
-// $Id: Vector4_test.C,v 1.21 2001/07/15 21:05:47 oliver Exp $
+// $Id: Vector4_test.C,v 1.22 2001/07/16 02:21:36 oliver Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
 #include <BALL/MATHS/vector4.h>
 ///////////////////////////
 
-START_TEST(TVector4, "$Id: Vector4_test.C,v 1.21 2001/07/15 21:05:47 oliver Exp $")
+START_TEST(TVector4, "$Id: Vector4_test.C,v 1.22 2001/07/16 02:21:36 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -63,6 +63,14 @@ CHECK(TVector4(const T* ptr);)
 	TEST_EQUAL(v[2], 3)
 	TEST_EQUAL(v[3], 4)
 	TEST_EXCEPTION(Exception::NullPointer, v = Vector4((float*)0))
+RESULT
+
+CHECK(TVector4(const T& scalar);)
+	v = Vector4(123.456);
+	TEST_EQUAL(v[0], 123.456)
+	TEST_EQUAL(v[1], 123.456)
+	TEST_EQUAL(v[2], 123.456)
+	TEST_EQUAL(v[3], 123.456)
 RESULT
 
 CHECK(TVector4(const T& x, const T& y, const T& z, const T& h = (T)1);)
