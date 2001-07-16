2c3aa041aa704bddbc613d2f89f08e8c6530a03f
sturm <sturm>
2001-07-16 15:37:12 +0000

    first version od this test

--- source/TEST/SolventDescriptor_test.C
+++ source/TEST/SolventDescriptor_test.C
@@ -1,11 +1,11 @@
-// $Id: SolventDescriptor_test.C,v 1.3 2001/07/15 17:32:41 amoll Exp $
+// $Id: SolventDescriptor_test.C,v 1.4 2001/07/16 15:37:12 sturm Exp $
 #include <BALL/CONCEPT/classTest.h>
  
 ///////////////////////////
 #include <BALL/SOLVATION/solventDescriptor.h>
 ///////////////////////////
 	  
-START_TEST(SolventDescriptor, "$Id: SolventDescriptor_test.C,v 1.3 2001/07/15 17:32:41 amoll Exp $")
+START_TEST(SolventDescriptor, "$Id: SolventDescriptor_test.C,v 1.4 2001/07/16 15:37:12 sturm Exp $")
 		 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -17,10 +17,8 @@ CHECK(Cstr)
 	s_ptr = new SolventDescriptor;
 	TEST_NOT_EQUAL(s_ptr, 0)
 	TEST_EQUAL(s_ptr->getName(), "")
-	TEST_EQUAL(s_ptr->getNumberDensity(), 0)
-	TEST_EQUAL(s_ptr->getSolventAtomDescriptorList().size(), 0)
+	TEST_EQUAL(s_ptr->getNumberDensity(), 0.0)
 	TEST_EQUAL(s_ptr->getNumberOfAtomTypes(), 0)
-	SolventAtomDescriptor sad;
 	TEST_EXCEPTION(Exception::IndexOverflow, s_ptr->getAtomDescriptor(99))
 	TEST_EQUAL(s_ptr->isValid(), false)
 RESULT
@@ -30,68 +28,68 @@ CHECK(Destructor)
 	delete(s_ptr);
 RESULT
 
-
-CHECK(Cpy-Cstr)
-	//
-RESULT
-
-
+vector<SolventAtomDescriptor> SADVector;
+SolventAtomDescriptor SAD;
+SAD.element_symbol="L";
+SAD.radius=1.999;
+SADVector.push_back(SAD);
 CHECK(SolventDescriptor(const String& name, float number_density, const std::vector<SolventAtomDescriptor>& atom_list))
-	//
+	s_ptr = new SolventDescriptor("SolvD1",1.234,SADVector);
+	TEST_NOT_EQUAL(s_ptr, 0)
+	TEST_EQUAL(s_ptr->getName(), "SolvD1")
+	TEST_EQUAL(s_ptr->getNumberDensity(), 1.234)
+	TEST_EQUAL(s_ptr->getNumberOfAtomTypes(), 1)
+	TEST_EQUAL(s_ptr->isValid(), true)
 RESULT
 
-CHECK(SolventDescriptor::BALL_CREATE(SolventDescriptor))
-  //BAUSTELLE
+SolventDescriptor* s_ptr2;
+CHECK(Cpy-Cstr)
+	s_ptr2 = new SolventDescriptor(*s_ptr);
+	TEST_NOT_EQUAL(s_ptr2, 0)
+	TEST_EQUAL(s_ptr2->getName(), "SolvD1")
+	TEST_EQUAL(s_ptr2->getNumberDensity(), 1.234)
+	TEST_EQUAL(s_ptr2->getNumberOfAtomTypes(), 1)
+	TEST_EQUAL(s_ptr2->isValid(), true)
+RESULT
+
+CHECK(SolventDescriptor::clear())
+	s_ptr2->clear();
+	TEST_NOT_EQUAL(s_ptr2, 0)
+	TEST_EQUAL(s_ptr2->getName(), "")
+	TEST_EQUAL(s_ptr2->getNumberDensity(),0.0)
+	TEST_EQUAL(s_ptr2->getNumberOfAtomTypes(), 0)
+	TEST_EQUAL(s_ptr->isValid(), false)
 RESULT
 
 
 CHECK(SolventDescriptor::setName(const String& name) throw())
-  //BAUSTELLE
+  s_ptr2->setName("SolvD1");
+	TEST_EQUAL(s_ptr2->getName(), "SolvD1");
 RESULT
-
-
-CHECK(SolventDescriptor::getName() const  throw())
-  //BAUSTELLE
-RESULT
-
 
 CHECK(SolventDescriptor::setNumberDensity(float number_density) throw())
-  //BAUSTELLE
+  s_ptr2->setNumberDensity(1.234);
+	TEST_EQUAL(s_ptr2->getNumberDensity(),1.234);
 RESULT
-
-
-CHECK(SolventDescriptor::getNumberDensity() const  throw())
-  //BAUSTELLE
-RESULT
-
 
 CHECK(SolventDescriptor::setSolventAtomDescriptorList(const std::vector<SolventAtomDescriptor>& solvent_atoms) throw())
-  //BAUSTELLE
+	s_ptr2->setSolventAtomDescriptorList(SADVector);	
+	TEST_EQUAL(s_ptr2->getNumberOfAtomTypes(), 1)
 RESULT
 
 
 CHECK(SolventDescriptor::getSolventAtomDescriptorList() const  throw())
-  //BAUSTELLE
-RESULT
-
-
-CHECK(SolventDescriptor::getNumberOfAtomTypes() const  throw())
-  //BAUSTELLE
+	TEST_EQUAL(s_ptr2->getSolventAtomDescriptorList().size(),1)
 RESULT
 
 
 CHECK(SolventDescriptor::getAtomDescriptor(Position index) const  throw())
-  //BAUSTELLE
+  SolventAtomDescriptor SAD2 = s_ptr2->getAtomDescriptor(0);
+	TEST_EQUAL(SAD2.radius,1.999)
 RESULT
-
-
-CHECK(SolventDescriptor::isValid() const  throw())
-  //BAUSTELLE
-RESULT
-
 
 CHECK(SolventDescriptor::bool operator == (const SolventDescriptor& descriptor) const  throw())
-  //BAUSTELLE
+  TEST_EQUAL(*s_ptr==*s_ptr2,true)
 RESULT
 
 ///////////////////////////////
