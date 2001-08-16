278dd64fef9254f1bb7c6b5d5feeb417698a7e4b
oliver <oliver>
2001-08-16 00:58:49 +0000

    added: implementation

--- source/NMR/createSpectrumProcessor.C
+++ source/NMR/createSpectrumProcessor.C
@@ -1,7 +1,8 @@
-// $Id: createSpectrumProcessor.C,v 1.12 2001/07/14 23:01:30 oliver Exp $
+// $Id: createSpectrumProcessor.C,v 1.13 2001/08/16 00:58:49 oliver Exp $
 
 #include <BALL/NMR/createSpectrumProcessor.h>
 #include <BALL/NMR/shiftModule.h>
+#include <BALL/DATATYPE/regularData1D.h>
 #include <BALL/SYSTEM/path.h>
 #include <BALL/KERNEL/PTE.h>
 #include <BALL/KERNEL/atom.h>
@@ -11,12 +12,18 @@ using namespace std;
 namespace BALL
 {
 
+	const String CreateSpectrumProcessor::IGNORE_SECTION_NAME =  "IgnoreAtoms";
+	const String CreateSpectrumProcessor::AVERAGE_SECTION_NAME =  "AverageAtoms";
+
 	CreateSpectrumProcessor::CreateSpectrumProcessor()
+		throw(Exception::FileNotFound, Exception::ParseError)
 		:	width_(1.0),
 			use_averaging_(true),
 			use_ignore_table_(true),
 			expression_("element(H)")
 	{
+		// call init to read the default config file
+		init();
 	}
 
 	CreateSpectrumProcessor::~CreateSpectrumProcessor ()
@@ -33,63 +40,64 @@ namespace BALL
 		return valid_;
 	}
 
-	void CreateSpectrumProcessor::init()
+	void CreateSpectrumProcessor::init(const String& filename)
 		throw()
 	{
 		valid_ = false;
 
-		// read the contents of the ignore set
-		// this hash set contains all quickly exchanging protons
-		// -- thos protons are not seen in the spectrum and do
-		// thus not produce a paek in the peak list.
-		ignore_atoms_.clear();
 		Path path;
-		String filename = path.find("NMR/ignore_atoms.dat");
-		if (filename == "")
+		String expanded_filename = path.find(filename);
+		if (expanded_filename == "")
 		{
-			Log.warn() << "CreateSpectrumProcessor::start: could not open atom ignore file NMR/ignore_atoms.dat" << endl;
-			return;
+			throw Exception::FileNotFound(__FILE__, __LINE__, filename);
 		}
 
-		ifstream infile(filename.c_str());
-		String name;
-		while (infile.good())
+		INIFile infile(expanded_filename);
+		infile.read();
+
+		if (!infile.isValid())
 		{
-			infile >> name;
-			if (name != "")
-			{
-				ignore_atoms_.insert(name);
-			}
+			throw Exception::ParseError(__FILE__, __LINE__, "Error while reading INIFile ", expanded_filename);
 		}
-		infile.close();
-		infile.clear();
+
 		
-		// read the list of equivalent protons
-		filename = path.find("NMR/equivalent_atoms.dat");
-		if (filename == "")
+		String name;
+		
+
+		if (!infile.hasSection(IGNORE_SECTION_NAME))
 		{
-			Log.warn() << "CreateSpectrumProcessor::start: could not open atom equivalency file NMR/equivalent_atoms.dat" << endl;
-			return;
+			throw Exception::ParseError(__FILE__, __LINE__, "Section not found in INIFile:", String("section ") + IGNORE_SECTION_NAME);
+		}
+		// Read the contents of the ignore set.
+		// This hash set contains all quickly exchanging protons
+		// -- those protons are not seen in the spectrum and do
+		// thus not produce a peak in the peak list.
+		ignore_atoms_.clear();
+		INIFile::LineIterator line = infile.getSectionFirstLine(IGNORE_SECTION_NAME);
+		INIFile::LineIterator last_line = infile.getSectionLastLine(IGNORE_SECTION_NAME);
+		for (; line != last_line; ++line)
+		{
+			ignore_atoms_.insert(*line);
 		}
 
+		// read the list of equivalent protons
 		// clear old contents
 		equivalency_residues_.clear();
 		equivalency_atoms_.clear();
-		
-		ifstream eqfile(filename.c_str());
-		while (eqfile.good())
+		line = infile.getSectionFirstLine(AVERAGE_SECTION_NAME);
+		last_line = infile.getSectionLastLine(AVERAGE_SECTION_NAME);
+		for (; line != last_line; ++line)
 		{
-			String residue, atoms;
-			vector<String> equivalencies;
-			eqfile >> residue >> atoms;
+			String atoms = (*line).after(" ");
+			String residue = (*line).before(" ");
 			if ((residue != "") && (atoms != ""))
 			{
+				vector<String> equivalencies;
 				atoms.split(equivalencies, ",");
 				equivalency_residues_.push_back(residue);
-				equivalency_atoms_.push_back(equivalencies);
+				equivalency_atoms_.push_back();
 			}
 		}
-		eqfile.close();
 
 		// initialization successful
 		valid_ = true;
@@ -186,21 +194,47 @@ namespace BALL
 		return width_;
 	}
 
-	void CreateSpectrumProcessor::enableAveraging(bool flag)
+	void CreateSpectrumProcessor::setAtomAveraging(bool flag)
 		throw()
 	{
 		use_averaging_ = flag;
 	}
 
-	void CreateSpectrumProcessor::enableIgnoreTable(bool flag)
+	bool CreateSpectrumProcessor::getAtomAveraging() const
+		throw()
+	{
+		return use_averaging_;
+	}
+
+	void CreateSpectrumProcessor::setAtomIgnoring(bool flag)
 		throw()
 	{
 		use_ignore_table_ = flag;
 	}
 	
+	bool CreateSpectrumProcessor::getAtomIgnoring() const
+		throw()
+	{
+		return use_ignore_table_;
+	}
+
 	void CreateSpectrumProcessor::setExpression(const String& expression)
 		throw()
 	{
 		expression_.setExpression(expression);
+	}
+
+	const String& CreateSpectrumProcessor::getExpression() const
+		throw()
+	{
+		return expression_.getExpressionString();
+	}
+
+	const RegularData1D& operator << (RegularData1D& data, const PeakList1D& peak_list)
+		throw()
+	{
+		// BAUSTELLE
+
+		return data;
 	}
 }	// namespace BALL
