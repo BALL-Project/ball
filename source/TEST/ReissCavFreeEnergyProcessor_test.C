7f260134967418cd1694a79fcf4110d466171214
aubertin <aubertin>
2001-09-01 22:59:55 +0000

    added tests

--- source/TEST/ReissCavFreeEnergyProcessor_test.C
+++ source/TEST/ReissCavFreeEnergyProcessor_test.C
@@ -1,4 +1,4 @@
-// $Id: ReissCavFreeEnergyProcessor_test.C,v 1.1 2001/08/30 09:00:45 aubertin Exp $
+// $Id: ReissCavFreeEnergyProcessor_test.C,v 1.2 2001/09/01 22:59:55 aubertin Exp $
 #include <BALL/CONCEPT/classTest.h>
 
 ///////////////////////////
@@ -6,76 +6,84 @@
 #include <BALL/FORMAT/HINFile.h>
 #include <BALL/KERNEL/system.h>
 #include <BALL/ENERGY/energyProcessor.h>
+#include <BALL/STRUCTURE/fragmentDB.h>
 ///////////////////////////
 
-START_TEST(ReissCavFreeEnergyProcessor, "$Id: ReissCavFreeEnergyProcessor_test.C,v 1.1 2001/08/30 09:00:45 aubertin Exp $")
+START_TEST(ReissCavFreeEnergyProcessor, "$Id: ReissCavFreeEnergyProcessor_test.C,v 1.2 2001/09/01 22:59:55 aubertin Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 
-
+PRECISION(1.0)
+CHECK(ReissCavFreeEnergyProcessor / Methane)
 HINFile f("data/methane.hin");
 System S;
 f >> S;
-
-
-/*CHECK(EnergyProcessor::EnergyProcessor())
-	EnergyProcessor* ep = new EnergyProcessor;
-	TEST_NOT_EQUAL(ep, 0)
-	TEST_EQUAL(ep->isValid(), true)
-	TEST_REAL_EQUAL(ep->getEnergy(), 0)
+f.close();
+ReissCavFreeEnergyProcessor proc;
+S.apply(proc);
+TEST_EQUAL(proc.getEnergy(),20.68)
+TEST_EQUAL(S.countAtoms(),5)
 RESULT
 
-EnergyProcessor ep;
+CHECK(ReissCavFreeEnergyProcessor::ReissCavFreeEnergyProcessor())
+	ReissCavFreeEnergyProcessor* proc = new ReissCavFreeEnergyProcessor;
+	TEST_NOT_EQUAL(proc, 0)
+	TEST_EQUAL(proc->isValid(), true)
+	TEST_REAL_EQUAL(proc->getEnergy(), 0)
+RESULT 
 
-CHECK(EnergyProcessor::EnergyProcessor(const EnergyProcessor& proc))
-	EnergyProcessor ep2(ep);
+CHECK(ReissCavFreeEnergyProcessor::ReissCavFreeEnergyProcessor(const ReissCavFreeEnergyProcessor& proc))
+  HINFile f("data/methane.hin");
+  System S;
+  f >> S;
+  f.close();
+  ReissCavFreeEnergyProcessor proc;
+  S.apply(proc);
+  ReissCavFreeEnergyProcessor proc2(proc); 
+  //TEST_EQUAL((proc==proc2), true) op == not yet implemented
+  bool test = proc.isValid()==proc2.isValid();
+  TEST_EQUAL(test, true)
+  test = proc.getEnergy()==proc2.getEnergy();
+  TEST_EQUAL(test, true)
 RESULT
+  
 
-
-CHECK(EnergyProcessor::~EnergyProcessor())
-	EnergyProcessor* ep = new EnergyProcessor;
-	delete ep;
-RESULT
-
-
-CHECK(EnergyProcessor::clear())
-  ep.clear();
-RESULT
-
-
-CHECK(EnergyProcessor::EnergyProcessor& operator = (const EnergyProcessor& proc))
-	EnergyProcessor ep2;
-	ep2 = ep;
-RESULT
-
-
-CHECK(EnergyProcessor::start())
-	TEST_EQUAL(ep.start(), true)
-RESULT
-
-
-CHECK(EnergyProcessor::Processor::Result operator () (BaseFragment& fragment))
-  TEST_EQUAL(S.apply(ep), true)
+CHECK(ReissCavFreeEnergyProcessor::~ReissCavFreeEnergyProcessor())
+	ReissCavFreeEnergyProcessor* proc = new ReissCavFreeEnergyProcessor;
+	delete proc;
 RESULT
 
 
-CHECK(EnergyProcessor::getEnergy() const )
-  TEST_REAL_EQUAL(ep.getEnergy(), 0)
+CHECK(ReissCavFreeEnergyProcessor::clear())
+  ReissCavFreeEnergyProcessor proc;
+  proc.clear();
 RESULT
 
 
-CHECK(apply)
-	MyEnergyProcessor mep;
-	mep.change = 2.0;
-	TEST_EQUAL(S.apply(mep), true)
-	TEST_EQUAL(S.countAtoms(), 31)
-	TEST_EQUAL(S.countAtomContainers(), 5)
-	TEST_REAL_EQUAL(mep.getEnergy(), S.countAtoms() * (S.countAtomContainers() + 2 + 1 ))
+CHECK(ReissCavFreeEnergyProcessor::ReissCavFreeEnergyProcessor& operator = (const ReissCavFreeEnergyProcessor& proc))
+  HINFile f("data/methane.hin");
+  System S;
+  f >> S;
+  f.close();
+  ReissCavFreeEnergyProcessor proc;
+  S.apply(proc);
+  ReissCavFreeEnergyProcessor proc2;
+  //proc2 = proc;
+  //TEST_EQUAL((proc == proc2), true)
+  //bool test = proc.isValid()==proc2.isValid();
+  //TEST_EQUAL(test, true)
+  //test = proc.getEnergy()==proc2.getEnergy();
+  //TEST_EQUAL(test, true) operator = not yet implemented
 RESULT
-*/
+
+
+CHECK(ReissCavFreeEnergyProcessor::start())
+  ReissCavFreeEnergyProcessor proc; 
+  TEST_EQUAL(proc.start(), true)
+RESULT
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
