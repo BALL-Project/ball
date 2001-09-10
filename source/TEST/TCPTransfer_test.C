247509f2a8dfb809fadc8d4cbe8ee30ad5b6b5a1
amoll <amoll>
2001-09-10 00:04:21 +0000

    added some tests

--- source/TEST/TCPTransfer_test.C
+++ source/TEST/TCPTransfer_test.C
@@ -1,4 +1,4 @@
-// $Id: TCPTransfer_test.C,v 1.1 2001/09/09 23:52:12 amoll Exp $
+// $Id: TCPTransfer_test.C,v 1.2 2001/09/10 00:04:21 amoll Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 
@@ -6,44 +6,41 @@
 #include <BALL/SYSTEM/TCPTransfer.h>
 ///////////////////////////
 
-START_TEST(TCPTransfer, "$Id: TCPTransfer_test.C,v 1.1 2001/09/09 23:52:12 amoll Exp $")
+START_TEST(TCPTransfer, "$Id: TCPTransfer_test.C,v 1.2 2001/09/10 00:04:21 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
-using namespace std;
 
+TCPTransfer* tcp_ptr;
+CHECK(cstr)
+	tcp_ptr = new TCPTransfer;
+	TEST_NOT_EQUAL(tcp_ptr, 0)
+	TEST_EQUAL(tcp_ptr->getStatusCode(), TCPTransfer::UNINITIALIZED_ERROR)
+RESULT
 
-CHECK(all)
+CHECK(~TCPTransfer_test)
+	delete tcp_ptr;
+RESULT
+
+CHECK(http)
   ofstream out("output", ios::out);
 	TCPTransfer tcp_t(out ,"http://tucows.uni-erlangen.de:80/index.html");
+	TEST_EQUAL(tcp_t.getHostAddress(), "tucows.uni-erlangen.de")
+	TEST_EQUAL(tcp_t.getFileAddress(), "/index.html")
+	TEST_EQUAL(tcp_t.getPort(), 80)
+	TEST_EQUAL(tcp_t.getStatusCode(), TCPTransfer::NO_ERROR)
+	TEST_EQUAL(tcp_t.getReceivedBytes(), 2238)
+	out.close();
+RESULT
+	
 	//TCPTransfer tcp_t(out, "http://postino.mpi-sb.mpg.de/index.html");
 	//TCPTransfer tcp_t(out, "http://www.winmx.com/index.html");
 	//TCPTransfer tcp_t(out, "http://download.nullsoft.com/winamp/client/winamp276_full.exe");
 	//TCPTransfer tcp_t(out, "ftp://ftp.gwdg.de:21/pub/newfiles.gz");
 	//TCPTransfer tcp_t(out, "ftp://ftp.uni-mainz.de/pub/misc/.cache");
 	//TCPTransfer tcp_t(out, "ftp://ftp.gwdg.de:21/pub/mpg/service/lt.db.txt");
-	//cout << tcp_t.getHostAddress() << "|"<< tcp_t.getFileAddress() <<"|"<< tcp_t.getPort() << endl;
-	//cout << "status code "<< tcp_t.getStatusCode() << "   bytes:" << tcp_t.getReceivedBytes()<< endl;
-	//TEST_EQUAL(tcp_t.getStatusCode(), TCPTransfer::NO_ERROR)
-	//TEST_EQUAL(tcp_t.getReceivedBytes(), 1234)
-	TEST_EQUAL(false, true)
-	out.close();
-RESULT
-
-
-/*
-int main()
-{
-	ofstream out("output", ios::out);
-//	TCPTransfer tcp_t(out ,"http://tucows.uni-erlangen.de:80/index.html");
-	TCPTransfer tcp_t(out, "ftp://ftp.gwdg.de:21/pub/newfiles.gz");
-	cout << tcp_t.getHostAddress() << "|"<< tcp_t.getFileAddress() <<"|"<< tcp_t.getPort() << endl;
-	cout << "status code "<< tcp_t.getStatusCode() << "   bytes:" << tcp_t.getReceivedBytes()<< endl;
-	out.close();
-}
-*/
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
