8dc41d459033f01f8f57297e23d79e517e4feda1
oliver <oliver>
2001-12-16 23:22:29 +0000

    added: QSAR stuff

---
+++ include/BALL/QSAR/simpleDescriptors.h
@@ -0,0 +1,11 @@
+// $Id: simpleDescriptors.h,v 1.1 2001/12/16 23:22:29 oliver Exp $
+
+#ifndef BALL_QSAR_DESCRIPTOR_H
+#define BALL_QSAR_DESCRIPTOR_H
+
+namespace BALL
+{
+
+} // namespace BALL
+
+#endif // BALL_QSAR_DESCRIPTOR_H
