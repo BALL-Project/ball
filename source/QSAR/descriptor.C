d5f3a43357cf714100c054d76ac049c1b5dfe8cb
oliver <oliver>
2001-12-17 02:50:03 +0000

    added: first implementation for a generic descriptor and a simple MolWt descr

--- source/QSAR/descriptor.C
+++ source/QSAR/descriptor.C
@@ -1,2 +1,40 @@
-// $Id: descriptor.C,v 1.2 2001/12/17 01:25:41 oliver Exp $
+// $Id: descriptor.C,v 1.3 2001/12/17 02:50:03 oliver Exp $
 
+#include <BALL/QSAR/descriptor.C>
+
+namespace BALL
+{
+
+	Descriptor::Descriptor()
+		:	UnaryProcessor<Molecule>(),
+			name_("generic descriptor")
+	{
+	}
+
+	Descriptor::Descriptor(const String& name)
+		:	UnaryProcessor<Molecule>(),
+			name_(name)
+	{
+	}
+
+	const String& Descriptor::getName() const
+	{
+		return name_;
+	}
+
+	void Descriptor::setName(const String& name) const
+	{
+		name_ = name;
+	}
+
+	Processor::Result Descriptor::operator () (Molecule& molecule)
+	{
+		molecule.setProperty(getName(), compute(molecule));
+		return Processor::CONTINUE;
+	}
+
+	double Descriptor::compute(const Molecule& molecule)
+	{
+		return 0.0;
+	}
+}
--- source/QSAR/simpleDescriptor.C
+++ source/QSAR/simpleDescriptor.C
@@ -1,1 +1,32 @@
-// $Id: simpleDescriptor.C,v 1.1 2001/12/16 23:16:17 oliver Exp $
+// $Id: simpleDescriptor.C,v 1.2 2001/12/17 02:50:03 oliver Exp $
+
+#include <BALL/QSAR/simpleDescriptors.h>
+
+namespace BALL
+{
+
+	MolecularMass::MolecularMass()
+		:	Descriptor("MolecularMass")
+	{
+	}
+	
+	double MolecularMass::compute(const Molecule& molecule)
+	{
+		double mass = 0.0;
+		AtomIterator it = molecule.beginAtom();
+		for (; +it; ++it)
+		{
+			if (it->getElement() == Element::UNKNOWN)
+			{
+				Log.warn() << "MolecularMass: unknown atomic mass for element UNKNOWN (atom: " 
+									 << it->getFullName() << ")" << std::endl;
+			}
+			else
+			{
+				mass += it->getElement().getMass();
+			}
+		}
+		
+		return mass;
+	}
+}
