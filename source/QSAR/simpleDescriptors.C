4fcbbe1055fa70d7ca500e51a4ca8380247b66d8
oliver <oliver>
2002-01-26 22:06:11 +0000

    added: PSA descriptor

--- source/QSAR/simpleDescriptors.C
+++ source/QSAR/simpleDescriptors.C
@@ -1,8 +1,10 @@
-// $Id: simpleDescriptors.C,v 1.3 2001/12/18 01:12:51 oliver Exp $
+// $Id: simpleDescriptors.C,v 1.4 2002/01/26 22:06:11 oliver Exp $
 
 #include <BALL/QSAR/simpleDescriptors.h>
-#include <BALL/KERNEL/PTE.h>
 
+#include <BALL/KERNEL/PTE.h>
+#include <BALL/KERNEL/expression.h>
+#include <BALL/STRUCTURE/numericalSAS.h>
 
 namespace BALL
 {
@@ -40,5 +42,33 @@ namespace BALL
 		}
 		
 		return mass;
+	}
+
+
+
+	PolarSurfaceArea::PolarSurfaceArea()
+		:	Descriptor("PolarSurfaceArea")
+	{
+	}
+	
+	double PolarSurfaceArea::compute(const Molecule& molecule)
+	{
+		// polar atoms are N, O, and H connected to N/O
+		Expression polar("element(O) OR element(N) OR (element(H) AND (connectedTo((N)) OR connectedTo((O))))");
+
+		HashMap<const Atom*, float> atom_areas;
+		calculateSASAtomAreas(molecule, atom_areas, 1.4, 400);
+		
+		double psa = 0.0;
+		HashMap<const Atom*, float>::Iterator it(atom_areas.begin());
+		for (; it != atom_areas.end(); ++it)
+		{
+			if (polar(*(it->first)))
+			{
+				psa += it->second;
+			}
+		}
+
+		return psa;
 	}
 }
