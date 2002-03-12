acc3b85dedecd8f3b1431ee9b3d3ff3e6ca2069b
anker <anker>
2002-03-12 16:14:30 +0000

    fixed: indenting within the pyWidget should work now.

--- source/VIEW/GUI/WIDGETS/pyWidget.C
+++ source/VIEW/GUI/WIDGETS/pyWidget.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: pyWidget.C,v 1.8 2002/02/27 12:25:20 sturm Exp $
+// $Id: pyWidget.C,v 1.9 2002/03/12 16:14:30 anker Exp $
 
 
 #include <BALL/VIEW/GUI/WIDGETS/pyWidget.h>
@@ -230,6 +230,10 @@ void PyWidget::parseLine_()
 	if (line.length() > 4)
 	{
 		line = line.mid(4);
+	}
+	else
+	{
+		line = "";
 	}
 	if (multi_line_mode_ == true)
 	{
