a5d91047d26b2ab2c6edfc41dfbb5c39f1610355
anker <anker>
2002-04-20 12:26:40 +0000

    added: metal term

---
+++ include/BALL/MOLMEC/FRESNO/chemScoreMetal.h
@@ -0,0 +1,116 @@
+// $Id: chemScoreMetal.h,v 1.1.2.1 2002/04/20 12:26:40 anker Exp $
+
+#ifndef BALL_MOLMEC_FRESNO_CHEMSCOREMETAL_H
+#define BALL_MOLMEC_FRESNO_CHEMSCOREMETAL_H
+
+#include <BALL/MOLMEC/COMMON/forceFieldComponent.h>
+
+namespace BALL
+{
+
+	/** ChemScore Metal component
+			{\bf Definition:} \URL{BALL/MOLMEC/FRESNO/chemScoreMetal.h}
+	*/
+	class ChemScoreMetal
+		:	public ForceFieldComponent
+	{
+
+		public:
+
+		/** @name	Constructors and Destructors	
+		*/
+		//@{ 
+
+		/**	Default constructor.
+		*/
+		ChemScoreMetal()
+			throw();
+
+		/**	Constructor.
+		*/
+		ChemScoreMetal(ForceField& force_field)
+			throw();
+
+		/**	Copy constructor
+		*/
+		ChemScoreMetal(const ChemScoreMetal& csm, bool clone_deep = true)
+			throw();
+
+		/**	Destructor.
+		*/
+		virtual ~ChemScoreMetal()
+			throw();
+
+		//@}
+		/**	@name	Assignment
+		*/
+		//@{
+
+		/** Assignment.
+		*/
+		const ChemScoreMetal& operator = (const ChemScoreMetal& csm)
+			throw();
+
+		/** Clear method.
+		*/
+		virtual void clear()
+			throw();
+
+		//@}
+		/**	@name	Predicates.
+		*/
+		//@{
+
+		bool operator == (const ChemScoreMetal& csm) const
+			throw();
+
+		//@}
+		/**	@name	Setup Methods	
+		*/
+		//@{
+
+		/**	Setup method.
+		*/
+		virtual bool setup()
+			throw();
+
+		//@}
+		/**	@name	Accessors	
+		*/
+		//@{
+
+		/**	Calculates and returns the component's energy.
+		*/
+		virtual double updateEnergy()
+			throw();
+
+		/**	Calculates and returns the component's forces.
+		*/
+		virtual void updateForces()
+			throw();
+
+		//@}
+
+		private:
+
+		/*_
+		*/
+		::vector< pair<const Atom*, const Atom*> > possible_metal_interactions_;
+
+		/*_
+		*/
+		double factor_;
+
+		/*_ 
+		*/
+		double r1_;
+
+		/*_ 
+		*/
+		double r2_;
+
+	};
+
+} // namespace BALL
+
+#endif // BALL_MOLMEC_FRESNO_CHEMSCOREMETAL_H
