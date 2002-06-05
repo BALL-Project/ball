c1f34cda73fdae266bdb8d61c3979b87dba58295
oliver <oliver>
2002-06-05 22:46:46 +0000

    *** empty log message ***

--- source/BENCHMARKS/AmberFF_bench.C
+++ source/BENCHMARKS/AmberFF_bench.C
@@ -1,4 +1,4 @@
-// $Id: AmberFF_bench.C,v 1.3.4.4 2002/03/01 02:55:35 oliver Exp $
+// $Id: AmberFF_bench.C,v 1.3.4.5 2002/06/05 22:46:45 oliver Exp $
 #include <BALL/CONCEPT/benchmark.h>
 
 ///////////////////////////
@@ -10,7 +10,7 @@
 
 using namespace BALL;
 
-START_BENCHMARK(AmberFF, 1.0, "$Id: AmberFF_bench.C,v 1.3.4.4 2002/03/01 02:55:35 oliver Exp $")
+START_BENCHMARK(AmberFF, 1.0, "$Id: AmberFF_bench.C,v 1.3.4.5 2002/06/05 22:46:45 oliver Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -20,7 +20,7 @@ System S;
 pdb >> S;
 
 AmberFF amber;
-START_SECTION(Setup, 0.05)
+START_SECTION(20x setup, 0.05)
 	START_TIMER
 		for (int i = 0; i < 20; i++)
 		{
@@ -29,7 +29,7 @@ START_SECTION(Setup, 0.05)
 	STOP_TIMER
 END_SECTION
 
-START_SECTION(Update w/o selection, 0.05)
+START_SECTION(20x update w/o selection, 0.05)
 	START_TIMER
 		for (int i = 0; i < 20; i++)
 		{
@@ -38,7 +38,7 @@ START_SECTION(Update w/o selection, 0.05)
 	STOP_TIMER
 END_SECTION
 
-START_SECTION(Energy calculation w/o selection, 0.2)
+START_SECTION(50x energy calculation w/o selection, 0.2)
 	START_TIMER
 		for (int i = 0; i < 50; i++)
 		{
@@ -47,7 +47,7 @@ START_SECTION(Energy calculation w/o selection, 0.2)
 	STOP_TIMER
 END_SECTION
 
-START_SECTION(Force calculation w/o selection, 0.2)
+START_SECTION(50x force calculation w/o selection, 0.2)
 	START_TIMER
 		for (int i = 0; i < 50; i++)
 		{
@@ -57,7 +57,7 @@ START_SECTION(Force calculation w/o selection, 0.2)
 END_SECTION
 
 S.beginResidue()->select();
-START_SECTION(Update w/ selection, 0.05)
+START_SECTION(50x update w/ selection, 0.05)
 	START_TIMER
 		for (int i = 0; i < 50; i++)
 		{
@@ -66,7 +66,7 @@ START_SECTION(Update w/ selection, 0.05)
 	STOP_TIMER
 END_SECTION
 
-START_SECTION(Energy calculation w/ selection, 0.2)
+START_SECTION(200x energy calculation w/ selection, 0.2)
 	START_TIMER
 		for (int i = 0; i < 200; i++)
 		{
@@ -75,7 +75,7 @@ START_SECTION(Energy calculation w/ selection, 0.2)
 	STOP_TIMER
 END_SECTION
 
-START_SECTION(Force calculation w/ selection, 0.2)
+START_SECTION(200x force calculation w/ selection, 0.2)
 	START_TIMER
 		for (int i = 0; i < 200; i++)
 		{
---
+++ source/BENCHMARKS/MolmecSupport_bench.C
@@ -0,0 +1,51 @@
+// $Id: MolmecSupport_bench.C,v 1.1.2.1 2002/06/05 22:46:46 oliver Exp $
+#include <BALL/CONCEPT/benchmark.h>
+
+///////////////////////////
+
+#include <BALL/MOLMEC/COMMON/support.h>
+#include <BALL/KERNEL/system.h>
+
+///////////////////////////
+
+using namespace BALL;
+
+// args: benchmark name (without '"'), reference time in seconds, CVS ID string
+START_BENCHMARK(MolmecSupport, 10.0, "$Id: MolmecSupport_bench.C,v 1.1.2.1 2002/06/05 22:46:46 oliver Exp $")
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
+System S;
+ForceField ff;
+
+ff.options.setVector(PeriodicBoundary::Option::PERIODIC_BOX_LOWER, Vector3(-20.0));
+ff.options.setVector(PeriodicBoundary::Option::PERIODIC_BOX_UPPER, Vector3(20.0));
+ff.periodic_boundary.enable();
+ff.setup(S);
+
+ForceField::PairVector pair_vector;
+pair_vector.reserve(2000000);
+START_SECTION(calculateNonBondedAtomPairs(type = HASH_GRID), 1.0)
+	START_TIMER
+		MolmecSupport::calculateNonBondedAtomPairs
+			(pair_vector, ff.getAtoms(), ff.periodic_boundary.getBox(),
+			 10.0, true, MolmecSupport::HASH_GRID);
+	STOP_TIMER
+END_SECTION
+
+pair_vector.clear();
+START_SECTION(calculateNonBondedAtomPairs(type = BRUTE_FORCE), 1.0)
+	START_TIMER
+		MolmecSupport::calculateNonBondedAtomPairs
+			(pair_vector, ff.getAtoms(), ff.periodic_boundary.getBox(),
+			 10.0, true, MolmecSupport::BRUTE_FORCE);
+	STOP_TIMER
+END_SECTION
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+END_BENCHMARK
