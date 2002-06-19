a6802d083a411a416707e34363af2c2f49a60895
strobel <strobel>
2002-06-19 12:55:59 +0000

    changed: possibility to compute SAS (provisorial for debugging)

--- include/BALL/STRUCTURE/surfaceProcessor.h
+++ include/BALL/STRUCTURE/surfaceProcessor.h
@@ -1,12 +1,19 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: surfaceProcessor.h,v 1.23 2002/02/27 12:19:48 sturm Exp $
+// $Id: surfaceProcessor.h,v 1.24 2002/06/19 12:55:59 strobel Exp $
+
+#define SES
 
 #include <BALL/STRUCTURE/reducedSurface.h>
-#include <BALL/STRUCTURE/solventExcludedSurface.h>
+#ifdef SES
+#	include <BALL/STRUCTURE/solventExcludedSurface.h>
+#	include <BALL/STRUCTURE/triangulatedSES.h>
+#else
+#	include <BALL/STRUCTURE/solventAccessibleSurface.h>
+#	include <BALL/STRUCTURE/triangulatedSAS.h>
+#endif
 #include <BALL/STRUCTURE/triangulatedSurface.h>
-#include <BALL/STRUCTURE/triangulatedSES.h>
 #include <BALL/MATHS/surface.h>
 #include <BALL/CONCEPT/processor.h>
 #include <BALL/KERNEL/atom.h>
@@ -86,10 +93,9 @@ namespace BALL
 	}
 
 
+	#ifdef SES
 	bool SurfaceProcessor::finish()
 	{
-		double old_epsilon = Constants::EPSILON;
-		//Constants::EPSILON = 1e-4;
 				probe_radius_ = 1.5;
 				density_ = 4.5;
 
@@ -143,7 +149,7 @@ namespace BALL
 				rsfile.open("ReducedSurface.clean.log");
 				rsfile << *rs;
 				rsfile.close();
-				std::ofstream sesfile("SolventExcludedSurface.clean.log");
+				std::ofstream sesfile("SolventExcludedSurface.log");
 				sesfile << *ses;
 				sesfile.close();
 				#endif
@@ -188,9 +194,118 @@ namespace BALL
 				#ifdef debug_surface_processor
 				::std::cerr << "... ok\n";
 				#endif
-		Constants::EPSILON = old_epsilon;
 		return true;
 	}
+	#else
+	bool SurfaceProcessor::finish()
+	{
+				probe_radius_ = 1.5;
+				density_ = 3;
+
+				#ifdef debug_surface_processor
+				::std::cerr << "initialise reduced surface ...\n";
+				#endif
+		ReducedSurface* rs = new ReducedSurface(spheres_,probe_radius_);
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\ncompute reduced surface ...\n";
+				#endif
+		try
+		{
+			rs->compute();
+		}
+		catch (Exception::GeneralException e)
+		{
+			delete rs;
+			::std::cerr << "can not compute rs: exception " << e.getName() << " (" << e.getMessage()
+									<< ") in line " << e.getLine() << " of file " << e.getFile() << "\n";
+			return false;
+		}
+				#ifdef debug_surface_processor_print
+				std::ofstream rsfile("ReducedSurface.log");
+				rsfile << *rs;
+				rsfile.close();
+				#endif
+				#ifdef debug_surface_processor
+				::std::cerr << rs->numberOfAtoms() << " atoms, "
+										<< rs->numberOfVertices() << " vertices, "
+										<< rs->numberOfEdges() << " edges, "
+										<< rs->numberOfFaces() << " faces ... ok\n"
+										<< "initialise solvent excluded surface ...\n";
+				#endif
+		SolventAccessibleSurface* sas = new SolventAccessibleSurface(rs);
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\ncompute solvent excluded surface ...\n";
+				#endif
+		try
+		{
+			sas->compute();
+		}
+		catch (Exception::GeneralException e)
+		{
+			delete sas;
+			delete rs;
+			::std::cerr << "can not compute ses: exception " << e.getName() << " (" << e.getMessage()
+									<< ") in line " << e.getLine() << " of file " << e.getFile() << "\n";
+			return false;
+		}
+				#ifdef debug_surface_processor_print
+				rsfile.open("ReducedSurface.clean.log");
+				rsfile << *rs;
+				rsfile.close();
+				std::ofstream sasfile("SolventAccessibleSurface.log");
+				sasfile << *sas;
+				sasfile.close();
+				#endif
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\ntriangulate surface ...\n";
+				#endif
+		TriangulatedSAS* surface = new TriangulatedSAS(sas,density_);
+		try
+		{
+			surface->compute();
+		}
+		catch (Exception::GeneralException e)
+		{
+			delete sas;
+			delete rs;
+			::std::cerr << "can not triangulate: exception " << e.getName() << " (" << e.getMessage()
+									<< ") in line " << e.getLine() << " of file " << e.getFile() << "\n";
+			return false;
+		}
+				#ifdef debug_surface_processor
+				::std::list<TrianglePoint*> points;
+				::std::list<Triangle*> triangles;
+				surface->getPoints(points);
+				surface->getTriangles(triangles);
+				::std::cerr << "... " << points.size() << " Punkte, "
+										<< triangles.size() << " Dreiecke ... ok\n"
+										<< "export surface ...\n";
+				#endif
+		surface->exportSurface(surface_);
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\ndelete surface ...\n";
+				#endif
+//surface->setIndices();
+//cout << *surface;
+//std::ofstream file("surface.log");
+//surface->setIndices();
+//file << (TriangulatedSurface)*surface;
+//file.close();
+		delete surface;
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\ndelete solvent excluded surface ...\n";
+				#endif
+		delete sas;
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\ndelete reduced surface ...\n";
+				#endif
+		delete rs;
+				#ifdef debug_surface_processor
+				::std::cerr << "... ok\n";
+				#endif
+		return true;
+	}
+	#endif
 
 
 	const Surface& SurfaceProcessor::getSurface() const
