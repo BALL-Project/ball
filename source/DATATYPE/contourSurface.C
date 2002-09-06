77991b93afa71516ede4093bd2e20f0e67eba48d
aubertin <aubertin>
2002-09-06 10:44:56 +0000

    Fixed a problem with HashIndex: -> moved here from contourSurface.h

--- source/DATATYPE/contourSurface.C
+++ source/DATATYPE/contourSurface.C
@@ -1,6 +1,17 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: contourSurface.C,v 1.1 2002/08/30 16:11:22 aubertin Exp $
+// $Id: contourSurface.C,v 1.2 2002/09/06 10:44:56 aubertin Exp $
 
 #include <BALL/DATATYPE/contourSurface.h>
+
+namespace BALL
+{
+
+	template<>
+	HashIndex BALL::Hash(const KeyType& p)
+	{
+		return (HashIndex)(p.first + p.second);
+	}
+  
+}
