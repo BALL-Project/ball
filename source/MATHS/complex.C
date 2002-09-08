5faab69e1b923c1b1b4df0e2fb03e3d27bb77272
anhi <anhi>
2002-09-08 18:46:38 +0000

    Fixed a problem with the operator >> for FFTW_COMPLEX: moved here from FFT1D.h

--- source/MATHS/complex.C
+++ source/MATHS/complex.C
@@ -1,3 +1,22 @@
-// $Id: complex.C,v 1.2 2002/08/27 09:35:25 oliver Exp $
+// $Id: complex.C,v 1.2.2.1 2002/09/08 18:46:38 anhi Exp $
 
 #include <BALL/MATHS/complex.h>
+
+namespace BALL
+{
+	#ifdef BALL_HAS_FFTW
+	std::istream& operator >> (std::istream& s, FFTW_COMPLEX& cpx)
+		throw()
+	{
+		return s >> cpx.re >> cpx.im;
+	}
+
+	std::ostream& operator << (std::ostream& s, const FFTW_COMPLEX& cpx)
+		throw()
+	{
+		
+		return s << cpx.re << " " << cpx.im;
+	}
+	#endif
+
+}
