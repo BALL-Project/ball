95894156a2afdd530fd6b08c2e7384bc4e5a49b3
oliver <oliver>
2002-09-10 06:10:08 +0000

    cosmetic changes

--- include/BALL/STRUCTURE/surfaceProcessor.h
+++ include/BALL/STRUCTURE/surfaceProcessor.h
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: surfaceProcessor.h,v 1.24 2002/06/19 12:55:59 strobel Exp $
+// $Id: surfaceProcessor.h,v 1.25 2002/09/10 06:10:08 oliver Exp $
 
 #define SES
 
@@ -50,6 +50,7 @@ namespace BALL
 		vector<Sphere3>& getSpheres();
 
 		protected:
+
 		Surface					surface_;
 		vector<Sphere3>	spheres_;
 		float						density_;
