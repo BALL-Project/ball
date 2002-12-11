c5493b277d49dc3f31e9117081326b74c5b90542
anker <anker>
2002-12-11 14:45:13 +0000

    removed: set

--- source/TEST/RegularData2D_test.C
+++ source/TEST/RegularData2D_test.C
@@ -1,9 +1,9 @@
-// $Id: RegularData2D_test.C,v 1.8.4.1 2002/12/06 15:29:09 oliver Exp $
+// $Id: RegularData2D_test.C,v 1.8.4.2 2002/12/11 14:45:13 anker Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 #include <BALL/DATATYPE/regularData2D.h>
 
-START_TEST(RegularData2D, "$Id: RegularData2D_test.C,v 1.8.4.1 2002/12/06 15:29:09 oliver Exp $")
+START_TEST(RegularData2D, "$Id: RegularData2D_test.C,v 1.8.4.2 2002/12/11 14:45:13 anker Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -63,14 +63,6 @@ CHECK(getSize())
 	TEST_EQUAL(g.getSize(), 6)
 	RegularData2D g2;
 	TEST_EQUAL(g2.getSize(), 0)
-RESULT
-
-CHECK(set(const RegularData2D& grid))
-	g[1] = 1.2345;
-	RegularData2D g1;
-	g1.set(g);
-	TEST_EQUAL(g1.getSize(), 6)
-	TEST_REAL_EQUAL(g1[1], 1.2345)
 RESULT
 
 CHECK(operator = (const RegularData2D& grid))
