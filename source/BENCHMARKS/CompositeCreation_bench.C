8e2db5bb6acb48ea3d179ff62fc40ccc2d0019db
oliver <oliver>
2002-12-17 19:47:38 +0000

    added: benchmark for the creation of composites

---
+++ source/BENCHMARKS/CompositeCreation_bench.C
@@ -0,0 +1,52 @@
+// $Id: CompositeCreation_bench.C,v 1.1 2002/12/17 19:47:38 oliver Exp $
+#include <BALL/CONCEPT/benchmark.h>
+
+///////////////////////////
+
+#include <BALL/CONCEPT/composite.h>
+#include <vector>
+
+///////////////////////////
+
+using namespace BALL;
+
+START_BENCHMARK(CompositeCreation, 1.0, "$Id: CompositeCreation_bench.C,v 1.1 2002/12/17 19:47:38 oliver Exp $")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+Size N = 100000;
+
+std::vector<Composite*> composites;
+composites.reserve(100000);
+
+START_SECTION(Creation, 0.5)
+
+	Composite* composite;
+	for (Size i = 0; i < N; i++)
+	{
+		START_TIMER
+			composite = new Composite;
+		STOP_TIMER
+		composites.push_back(composite);
+	}
+
+END_SECTION
+
+START_SECTION(Destruction, 0.5)
+
+	Composite* composite;
+	for (Size i = 0; i < N; i++)
+	{
+		composite = composites[i];
+		START_TIMER
+			delete composite;
+		STOP_TIMER
+	}
+END_SECTION
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+END_BENCHMARK
