42d103a29d0162817bdaa662ad5bd5e1eafb4d22
anker <anker>
2003-03-28 14:41:40 +0000

    added: shortest usage information ever

--- source/APPLICATIONS/UTILITIES/assign_typenames_from_rules.C
+++ source/APPLICATIONS/UTILITIES/assign_typenames_from_rules.C
@@ -1,4 +1,4 @@
-// $Id: assign_typenames_from_rules.C,v 1.1 2003/03/05 16:17:29 anker Exp $
+// $Id: assign_typenames_from_rules.C,v 1.2 2003/03/28 14:41:40 anker Exp $
 
 #include <BALL/common.h>
 #include <BALL/FORMAT/INIFile.h>
@@ -7,16 +7,24 @@
 #include <BALL/MOLMEC/COMMON/typenameRuleProcessor.h>
 
 using namespace BALL;
+using namespace std;
 
 int main(int argc, char** argv)
 {
+	if (argc != 4)
+	{
+		cout << "Need 3 arguments: INIFile input.hin output.hin" << endl;
+		return(1);
+	}
+
 	Path path;
 	String filename = path.find(argv[1]);
 	if (filename == "")
 	{
-		Log.warn() << "Could not find rule file " << argv[1] << std::endl;
+		cerr << "Could not find rule file " << argv[1] << std::endl;
 		return(1);
 	}
+
 	INIFile typename_rules(filename);
 	typename_rules.read();
 	TypenameRuleProcessor proc(typename_rules, "TypenameRules");
