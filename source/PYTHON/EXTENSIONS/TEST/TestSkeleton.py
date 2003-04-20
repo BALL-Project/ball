a6298c98b6125ec28fd132d5d16a2e5f0c0669ba
oliver <oliver>
2003-04-20 16:40:18 +0000

    fixed: adapted to new SIP added: additional tests, very basic, yet.

---
+++ source/PYTHON/EXTENSIONS/TEST/TestSkeleton.py
@@ -0,0 +1,11 @@
+#! python
+from ClassTest import *
+
+START_TEST('<class name>', '$Id: TestSkeleton.py,v 1.1 2003/04/20 16:40:18 oliver Exp $')
+
+CHECK('<class name>.<method name>()')
+#
+#
+RESULT()
+
+END_TEST()
