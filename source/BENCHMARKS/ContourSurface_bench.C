2b7919323e79dd97e5a46f16bdf2b7f2d70c6825
oliver <oliver>
2003-05-06 20:20:20 +0000

    fixed: adapted to new interface of TRegData2D

--- source/BENCHMARKS/ContourSurface_bench.C
+++ source/BENCHMARKS/ContourSurface_bench.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: ContourSurface_bench.C,v 1.1 2003/05/03 17:29:31 oliver Exp $
+// $Id: ContourSurface_bench.C,v 1.2 2003/05/06 20:20:20 oliver Exp $
 //
 #include <BALL/CONCEPT/benchmark.h>
 
@@ -13,7 +13,7 @@
 
 using namespace BALL;
 
-START_BENCHMARK(ContourSurface, 1.0, "$Id: ContourSurface_bench.C,v 1.1 2003/05/03 17:29:31 oliver Exp $")
+START_BENCHMARK(ContourSurface, 1.0, "$Id: ContourSurface_bench.C,v 1.2 2003/05/06 20:20:20 oliver Exp $")
 
 
 /////////////////////////////////////////////////////////////
@@ -22,7 +22,7 @@ START_BENCHMARK(ContourSurface, 1.0, "$Id: ContourSurface_bench.C,v 1.1 2003/05/
 
 START_SECTION(Ocatahedra, 0.2)
 	ContourSurface cs;
-	RegularData3D grid(Vector3(-2.0), Vector3(2.0), 5, 5, 5);
+	RegularData3D grid(RegularData3D::IndexType(5, 5, 5), Vector3(-2.0), Vector3(2.0));
 	for (Position i = 0; i < grid.size(); grid[i++] = 1.0);
 	grid[2 + 2 * 5 + 2 * 25] = -1.0;
 	for (Position i = 0; i < 5000; i++)
@@ -35,7 +35,7 @@ END_SECTION
 
 START_SECTION(Large Grid, 0.8)
 	ContourSurface cs2(5.0);
-	RegularData3D grid2(Vector3(-10.0), Vector3(10.0), 200,200,200);
+	RegularData3D grid2(RegularData3D::IndexType(200, 200, 200), Vector3(-10.0), Vector3(10.0));
 	for (Position i = 0; i < grid2.size(); i++)
 	{
 		grid2[i] = grid2.getCoordinates(i).getLength();
