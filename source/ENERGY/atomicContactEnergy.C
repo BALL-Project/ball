f3eead1cf93a5c64111d66dfa516e94f3cdb0a1f
oliver <oliver>
2003-05-07 16:41:44 +0000

    fixed: modified implementation -- memory leaks removed

--- source/ENERGY/atomicContactEnergy.C
+++ source/ENERGY/atomicContactEnergy.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: atomicContactEnergy.C,v 1.4 2003/05/06 20:27:38 oliver Exp $
+// $Id: atomicContactEnergy.C,v 1.5 2003/05/07 16:41:44 oliver Exp $
 //
 
 #include <BALL/ENERGY/atomicContactEnergy.h>
@@ -26,121 +26,141 @@ using namespace std;
 
 namespace BALL 
 {
+	typedef StringHashMap<Atom::Type>								ACETypeTable;
+	typedef std::vector<std::vector<double> >				ACEParameter;
+	typedef std::pair<ACETypeTable, ACEParameter>		ACEData;
 
-	StringHashMap<Atom::Type> buildTable_()
+	 const ACEData& ACEBuildTable
+		(const String& type_filename, const String& parameter_filename)
 	{
-		File				datafile;
-		String			residue;
-		String			atom_name;
-		Atom::Type	atom_type;
+		// A static string hash map holds the types.
+		static ACEData data;
+		static String last_type_filename;
+		static String last_parameter_filename;
 
-		// determine the path to the data file
+		// Check whether the last files read are identical with the
+		// filenames specified. We are fine if they are identical
+		// and reread everything otherwise.
+		if (last_type_filename == type_filename && last_parameter_filename == parameter_filename)
+		{
+			return data;
+		}
+
+		// Determine the path to the type file.
 		Path path;
-		String filename = path.find(ACE_TYPES_FILENAME);
+		String filename = path.find(type_filename);
 		if (filename == "")
 		{
-			throw Exception::FileNotFound(__FILE__, __LINE__, ACE_TYPES_FILENAME);
+			throw Exception::FileNotFound(__FILE__, __LINE__, type_filename);
 		}
 
-		datafile.open(filename);
-
-		// create a new StringHashMap
-		StringHashMap<Atom::Type> table;
-
+		// Read the file.
+		data.first.clear();
+		File datafile(filename);
+		String key;
+		String atom_name;
+		Index atom_type;
 		while (datafile.good())
 		{
-			datafile >> residue >> atom_name >> atom_type;
-			String key(residue);
+			datafile >> key >> atom_name >> atom_type;
 			key.append(":");
 			key.append(atom_name);
-			table[key] = atom_type;
-		}
-					
+			data.first[key] = atom_type;
+		}			
 		datafile.close();
 
-		return table;
+		filename = path.find(parameter_filename);
+		if (filename == "")
+		{
+			throw Exception::FileNotFound(__FILE__, __LINE__, filename);
+		}
+
+		// Read the parameter table.
+		datafile.open(filename);
+		Size number_of_entries;
+		datafile >> number_of_entries;
+		
+		// Read the matrix of ACE parameters (symmetric, number_of_entries x number_of_entries)
+		data.second.resize(number_of_entries);
+		for (Position i = 0; i < number_of_entries; i++)
+		{
+			data.second[i].resize(number_of_entries);
+
+			for (Position j = 0; j < number_of_entries; j++)
+			{
+				datafile >> data.second[i][j];
+			}
+		}
+
+		// Consistency check in the matrix
+		for (Position i = 0; i < number_of_entries; i++)
+		{
+			for (Position j = 0; j < number_of_entries; j++)
+			{
+				if (data.second[i][j] != data.second[j][i])
+				{
+					throw Exception::ParseError(__FILE__, __LINE__, 
+																			String("ACE data from ") + filename,
+																			String("inconsistent table entry at position ") + String(i) + "/" + String(j));
+				}
+			}
+		}
+		
+				
+		// Remember we read the files
+		last_type_filename = type_filename;
+		last_parameter_filename = parameter_filename;
+
+		return data;
 	}
 
 	typedef struct 
 	{
-		Index					type;
+		Index				type;
 		Vector3			v;
-		unsigned		index;
+		Position		index;
 		String			name;
 		const Atom*	atom;
-	} ACEFastAtomType_;
+	} 
+	ACEFastAtomType;
 
 
-	double calculateACE(AtomContainer& atoms) 
+	double calculateACE
+		(const AtomContainer& atom_container, 
+		 const string& type_filename, const string& parameter_filename) 
 	{
-		 
-		Vector3										bounding_box_lower;	
-		Vector3										bounding_box_upper;
-		Vector3										size;
+		// Retrieve the ACE types and parameters.
+		// We do this just once, unless different filenames are 
+		// being specified.
+		const ACEData& data = ACEBuildTable(type_filename, parameter_filename);
+		const ACETypeTable& table = data.first;
+		const ACEParameter& params = data.second;
 
-		StringHashMap<Atom::Type>	table;
-		float											ACE[18][18];
-		FILE*											ACE_file;
-		Index												i;
-		Index												j;
+		// Compute the bounding box of the atom container.
+		BoundingBoxProcessor bounding_box;
+		const_cast<AtomContainer&>(atom_container).apply(bounding_box);
+		Vector3 size = bounding_box.getUpper() - bounding_box.getLower();
 
-		table = buildTable_();
+		// Create a three-dimensional hash grid. The border has to
+		// extend by at least one box size (the max distance used by ACE) over the bounding box.
+		HashGrid3<ACEFastAtomType*> grid(bounding_box.getLower() - Vector3(7.0), size + Vector3(14.0), 6.0);
 
-		Path path;
-		String filename = path.find(ACE_FILENAME);
-		if (filename == "")
-		{
-			throw Exception::FileNotFound(__FILE__, __LINE__, ACE_FILENAME);
-		}
-		ACE_file = fopen(filename.c_str(), "r");
-					
-		for (i = 0; i < 18; i++)
-		{
-			for (j = 0; j < 18; j++)
-			{
-				fscanf(ACE_file, "%f", &(ACE[i][j]));
-			}
-		}
-		fclose(ACE_file);
-		
-		// perform consistency check: matrix has to be symmetric
-		for (i = 0; i < 18; i++)
-		{
-			for (j = i; j < 18; j++)
-			{
-				if (ACE[i][j] != ACE[j][i])
-				{
-					Log.error() << "calculateACE: inconsistent values in contact matrix: ACE[" 
-											<< i << "][" << j << "] != ACE[" << j << "][" << i << "]!" << endl;
-				}
-			}
-		}
-			
-		BoundingBoxProcessor	bounding_box;
+		// Create a vector of ACEFastAtom entries. We will stick pointers 
+		// to those into the hash grid. It will never shrink again, but then 
+		// we won't have to reallocate all the time. This seems like a good 
+		// trade-off for now.
+		static std::vector<ACEFastAtomType> atoms;
 
-		atoms.apply(bounding_box);
-		bounding_box_lower = bounding_box.getLower();
-		bounding_box_upper = bounding_box.getUpper();
-		
-				
-		size = bounding_box_upper - bounding_box_lower;
+		// Remove old stuff
+		atoms.clear();
 
-		HashGrid3<ACEFastAtomType_*>*	grid;
-		grid = new HashGrid3<ACEFastAtomType_*>(bounding_box_lower - Vector3(7.0), size + Vector3(14.0), 6.0);
-
-		ACEFastAtomType_*		atom;
-		String							atom_name;
-		Index									atom_type;
-		unsigned						index = 0;
-
-
-		AtomConstIterator atom_it(atoms.beginAtom());
-		for (atom_it = atoms.beginAtom(); +atom_it; ++atom_it) 
+		String atom_name;
+		Index atom_type;
+		AtomConstIterator atom_it(atom_container.beginAtom());
+		for (; +atom_it; ++atom_it) 
 		{
 			// construct correct name, <RESNAME>:<ATOMNAME>
-			atom_name = atom_it->getFragment()->getName().trim();		
-			atom_name.append(":");
-			atom_name.append(atom_it->getName().trim());
+			atom_name = atom_it->getFullName(Atom::NO_VARIANT_EXTENSIONS);		
 
 			// get the atom type from hash table
 			//    atom type = -1 for hydrogen (not to be considered)
@@ -159,14 +179,14 @@ namespace BALL
 			// insert all neccessary atoms into the grid
 			if (atom_type >= 0)
 			{
-				atom = new ACEFastAtomType_;
-				atom->index = index++;
-				atom->v = atom_it->getPosition();
-				atom->type = atom_type;
-				atom->name = atom_name;
-				atom->atom = &(*atom_it);
-				grid->insert(atom->v, atom);
-				// Log.error() << atom_name << ": " << atom_type << endl;
+				static ACEFastAtomType atom;
+				atom.v = atom_it->getPosition();
+				atom.type = atom_type;
+				atom.name = atom_name;
+				atom.index = atoms.size();
+				atom.atom = &*atom_it;
+
+				atoms.push_back(atom);
 			} 
 			else 
 			{
@@ -175,58 +195,62 @@ namespace BALL
 				{
 					Log.warn() << "calculateACE: ACE not parametrized for " << atom_name << endl;
 				} 
-				// hydrogens are just to be ignored, unknown types are a 
-				// real reason for concern
 				else if (atom_type == -3) 
 				{
+					// hydrogens are just to be ignored, unknown types are a 
+					// real reason for concern
 					Log.warn() << "calculateACE: did not find an ACE type for " << atom_name << endl;
 				}
 			}		
 		}
 
-		float contact_energy = 0;
-		float	sq_distance;
+		std::vector<ACEFastAtomType>::iterator it(atoms.begin());
+		for (; it != atoms.end(); ++it)
+		{
+			grid.insert(it->v, &*it);
+		}
+
+		double contact_energy = 0;
+		double sq_distance;
 		Vector3 v;
 
-		HashGrid3<ACEFastAtomType_*>::BoxIterator					box_it;
-		HashGridBox3<ACEFastAtomType_*>::DataIterator			data_it;
-
-		HashGridBox3<ACEFastAtomType_*>::BoxIterator			box_it2;
-		HashGridBox3<ACEFastAtomType_*>::DataIterator			data_it2;
-
-#ifdef BALL_DEBUG_ACE
-		Size count = 0;
-		AtomIterator debug_atom_it1 = atoms.beginAtom();
-		AtomIterator debug_atom_it2;
-		for (; +debug_atom_it1; ++debug_atom_it1)
-		{
-			for (debug_atom_it2 = debug_atom_it1; +debug_atom_it2; ++debug_atom_it2)
+		#ifdef BALL_DEBUG_ACE
+			Size count = 0;
+			AtomIterator debug_atom_it1 = atoms.beginAtom();
+			AtomIterator debug_atom_it2;
+			for (; +debug_atom_it1; ++debug_atom_it1)
 			{
-				if ((debug_atom_it1 != debug_atom_it2)
-						&& (debug_atom_it1->getPosition().getDistance(debug_atom_it2->getPosition()) <= 6.0))
+				for (debug_atom_it2 = debug_atom_it1; +debug_atom_it2; ++debug_atom_it2)
 				{
-					count++;
+					if ((debug_atom_it1 != debug_atom_it2)
+							&& (debug_atom_it1->getPosition().getDistance(debug_atom_it2->getPosition()) <= 6.0))
+					{
+						count++;
+					}
 				}
 			}
-		}
-		Log.error() << "count(BF) = " << count << endl;
-		count = 0;
-#endif
-		
+			Log.error() << "count(BF) = " << count << endl;
+			count = 0;
+		#endif
 
 		// iterate over all (non empty) grid boxes
-		for (box_it = grid->beginBox(); +box_it; ++box_it)
+		HashGrid3<ACEFastAtomType*>::BoxIterator box_it(grid.beginBox());
+		for (; +box_it; ++box_it)
 		{
+			HashGridBox3<ACEFastAtomType*>::DataIterator			data_it;
+			HashGridBox3<ACEFastAtomType*>::BoxIterator				box_it2;
+			HashGridBox3<ACEFastAtomType*>::DataIterator			data_it2;
+
 			// iterate over all items in the box
-			for (data_it = (*box_it).beginData(); +data_it; ++data_it) 
+			for (data_it = box_it->beginData(); +data_it; ++data_it) 
 			{
 				v = (*data_it)->v;
 
 				// iterate over all neighbouring boxes (includig the box itself!)
-				for (box_it2 = (*box_it).beginBox(); +box_it2; ++box_it2)
+				for (box_it2 = box_it->beginBox(); +box_it2; ++box_it2)
 				{
 					// iterate over all items in the box
-					for (data_it2 = (*box_it2).beginData(); +data_it2; ++data_it2) 
+					for (data_it2 = box_it2->beginData(); +data_it2; ++data_it2) 
 					{
 						// consider only pairs of different atoms and consider each pair only once 
 						if ((*data_it2)->index > (*data_it)->index)
@@ -237,10 +261,10 @@ namespace BALL
 							{
 								if (!(*data_it)->atom->isBoundTo(*(*data_it2)->atom))
 								{
-									contact_energy += ACE[(*data_it)->type][(*data_it2)->type];
-#ifdef BALL_DEBUG_ACE
-									count++;
-#endif
+									contact_energy += params[(*data_it)->type][(*data_it2)->type];
+									#ifdef BALL_DEBUG_ACE
+										count++;
+									#endif
 								}
 							}
 						}
@@ -249,9 +273,9 @@ namespace BALL
 			}
 		}
 
-#ifdef BALL_DEBUG_ACE
+	#ifdef BALL_DEBUG_ACE
 		Log.error() << "count(BF) = " << count << endl;
-#endif
+	#endif
 
 		// scale by a factor of 1 / 21.0 to get energy in kcal/mol
 		// and another factor of 4.184 to get kJ/mol
