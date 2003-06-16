5d9627d046a8bdcf0082fc1c77867575b85d3957
anker <anker>
2003-06-16 15:40:15 +0000

    changed: extracted EnumeratorIndex_test from Enumerator_test fixed: Some method test issues. There are still ???s in the test!

--- source/TEST/Enumerator_test.C
+++ source/TEST/Enumerator_test.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: Enumerator_test.C,v 1.15 2003/05/23 15:39:21 oliver Exp $
+// $Id: Enumerator_test.C,v 1.16 2003/06/16 15:40:15 anker Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 
@@ -17,522 +17,32 @@ void char_assign(char& c1, const char& c2)
 	c1 = c2;
 }
 
-START_TEST(Enumerator, "$Id: Enumerator_test.C,v 1.15 2003/05/23 15:39:21 oliver Exp $")
+START_TEST(Enumerator, "$Id: Enumerator_test.C,v 1.16 2003/06/16 15:40:15 anker Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 
-// tests for class EnumeratorIndex::IncompatibleIndex::
-
-EnumeratorIndex::IncompatibleIndex* ex_ptr;
-
-CHECK(EnumeratorIndex::IncompatibleIndex::IncompatibleIndex(const char* file, int line))
-	ex_ptr = new EnumeratorIndex::IncompatibleIndex(__FILE__,__LINE__);
-	TEST_NOT_EQUAL(ex_ptr, 0)
-RESULT
-
-delete ex_ptr;
-
-// tests for class EnumeratorIndex::
-
-EnumeratorIndex* enumerator_index_ptr;
-
-CHECK(EnumeratorIndex::EnumeratorIndex())
-  enumerator_index_ptr = new EnumeratorIndex;
-	TEST_NOT_EQUAL(enumerator_index_ptr, 0)
-RESULT
-
-CHECK(EnumeratorIndex::~EnumeratorIndex())
-  delete enumerator_index_ptr;
-RESULT
-
-Enumerator<String, String::iterator, char>::SiteList variant_list;
-CHECK(EnumeratorIndex::EnumeratorIndex(const list<pair<VariantIterator, vector<Variant> > >& variant_list))
-  enumerator_index_ptr = new EnumeratorIndex(variant_list);
-  TEST_NOT_EQUAL(enumerator_index_ptr, 0)
-RESULT
-
-CHECK(Size EnumeratorIndex::getSize() const)
-	EnumeratorIndex ei;
-	TEST_EQUAL(ei.getSize(), 0)
-RESULT
-
-CHECK(EnumeratorIndex::getModulus() const)
-  vector<Size> modulus = enumerator_index_ptr->getModulus();
-  TEST_EQUAL(modulus.size(), 0)
-RESULT
-delete enumerator_index_ptr;
-
-CHECK(EnumeratorIndex& EnumeratorIndex::operator << (Size modulus))
-	EnumeratorIndex ei;
-	TEST_EQUAL(ei.getSize(), 0)
-	
-	ei << 2;
-	TEST_EQUAL(ei.getSize(), 1)
-	TEST_EQUAL(ei.getModulus(0), 2)
-	TEST_EQUAL(ei[0], 0)
-
-	ei << 4;
-	TEST_EQUAL(ei.getSize(), 2)
-	TEST_EQUAL(ei.getModulus(0), 2)
-	TEST_EQUAL(ei.getModulus(1), 4)
-	TEST_EQUAL(ei[0], 0)
-	TEST_EQUAL(ei[1], 0)
-
-	ei << 5 << 6;
-	TEST_EQUAL(ei.getSize(), 4)
-	TEST_EQUAL(ei.getModulus(0), 2)
-	TEST_EQUAL(ei.getModulus(1), 4)
-	TEST_EQUAL(ei.getModulus(2), 5)
-	TEST_EQUAL(ei.getModulus(3), 6)
-	TEST_EQUAL(ei[0], 0)
-	TEST_EQUAL(ei[1], 0)
-	TEST_EQUAL(ei[2], 0)
-	TEST_EQUAL(ei[3], 0)
-
-	TEST_EXCEPTION(Exception::OutOfRange, ei << 1)
-	TEST_EXCEPTION(Exception::OutOfRange, ei << 0)
-RESULT
-
-CHECK(Position EnumeratorIndex::operator [] (Position pos) const)
-	EnumeratorIndex ei;
-	ei << 5 << 6 << 7;
-
-	const EnumeratorIndex& c_ei(ei);
-	TEST_EQUAL(c_ei[0], 0)
-	TEST_EQUAL(c_ei[1], 0)
-	TEST_EQUAL(c_ei[2], 0)
-RESULT
-
-CHECK(Position& EnumeratorIndex::operator [] (Position pos))
-	EnumeratorIndex ei;	
-	ei << 5 << 6 << 7;
-
-	const EnumeratorIndex& c_ei(ei);
-	TEST_EQUAL(c_ei[0], 0)
-	TEST_EQUAL(c_ei[1], 0)
-	TEST_EQUAL(c_ei[2], 0)
-	
-	ei[0] = 2;
-	TEST_EQUAL(c_ei[0], 2)
-	TEST_EQUAL(c_ei[1], 0)
-	TEST_EQUAL(c_ei[2], 0)
-	
-	ei[1] = 3;
-	TEST_EQUAL(c_ei[0], 2)
-	TEST_EQUAL(c_ei[1], 3)
-	TEST_EQUAL(c_ei[2], 0)
-	
-	ei[2] = 4;
-	TEST_EQUAL(c_ei[0], 2)
-	TEST_EQUAL(c_ei[1], 3)
-	TEST_EQUAL(c_ei[2], 4)
-RESULT
-
-CHECK(Size EnumeratorIndex::getModulus(Position pos) const)
-	EnumeratorIndex ei;
-	ei << 5 << 6 << 7;
-RESULT
-
-CHECK(EnumeratorIndex::operator = (Position index))
-	EnumeratorIndex ei;
-	ei << 4 << 5 << 6;
-	TEST_EQUAL(ei[0], 0)
-	TEST_EQUAL(ei[1], 0)
-	TEST_EQUAL(ei[2], 0)
-	ei = 1;
-	TEST_EQUAL(ei[2], 1)
-	TEST_EQUAL(ei[1], 0)
-	TEST_EQUAL(ei[0], 0)
-	ei = (4 * 5 * 6 - 1);
-	TEST_EQUAL(ei[2], 5)
-	TEST_EQUAL(ei[1], 4)
-	TEST_EQUAL(ei[0], 3)	
-	TEST_EXCEPTION(Exception::IndexOverflow, ei = 4 * 5 *6)
-RESULT
-
-CHECK(EnumeratorIndex::EnumeratorIndex& operator ++ ())
-	EnumeratorIndex ei;
-	ei << 2 << 3 << 4;
-	ABORT_IF(ei.getSize() < 3)
-	TEST_EQUAL(ei[0], 0) 
-	TEST_EQUAL(ei[1], 0) 
-	TEST_EQUAL(ei[2], 0) 
-	for (Position i = 0; i < (2 * 3 * 4 - 1); ++i, ++ei)
-	{
-		Size current = ei[0] * 3 * 4 + ei[1] * 4 + ei[2];
-		TEST_EQUAL(current, i)
-	}
-	TEST_EQUAL(ei[0], 1) 
-	TEST_EQUAL(ei[1], 2) 
-	TEST_EQUAL(ei[2], 3) 
-	TEST_EXCEPTION(Exception::IndexOverflow, ++ei)
-RESULT
-
-
-CHECK(EnumeratorIndex::EnumeratorIndex& operator -- ())
-	EnumeratorIndex ei;
-	ei << 2 << 3 << 4;
-	ABORT_IF(ei.getSize() < 3)
-	ei[0] = 1;
-	ei[1] = 2;
-	ei[2] = 3;
-	for (Position i = (2 * 3 * 4 - 1); i > 0; --i, --ei)
-	{
-		Size current = ei[0] * 3 * 4 + ei[1] * 4 + ei[2];
-		TEST_EQUAL(current, i)
-	}
-	TEST_EQUAL(ei[0], 0) 
-	TEST_EQUAL(ei[1], 0) 
-	TEST_EQUAL(ei[2], 0) 
-	TEST_EXCEPTION(Exception::IndexUnderflow, --ei)
-RESULT
-
-
-CHECK(const EnumeratorIndex& EnumeratorIndex::operator = (Position rhs))
-	EnumeratorIndex ei;
-	ei << 2 << 3 << 4;
-	ei = 17;	
-
-	EnumeratorIndex ei2;
-	ei2 = ei;
-
-	TEST_EQUAL(ei2.getSize(), ei.getSize())
-	ABORT_IF(ei2.getSize() != 3)
-	TEST_EQUAL(ei2.getModulus(0), ei.getModulus(0))
-	TEST_EQUAL(ei2.getModulus(1), ei.getModulus(1))
-	TEST_EQUAL(ei2.getModulus(2), ei.getModulus(2))
-	TEST_EQUAL(ei2[0], ei[0])
-	TEST_EQUAL(ei2[1], ei[1])
-	TEST_EQUAL(ei2[2], ei[2])
-RESULT
-
-CHECK(bool EnumeratorIndex::operator == (const EnumeratorIndex& rhs))
-	EnumeratorIndex ei;
-	ei << 2 << 3 << 4;
-	ei[0] = 1;
-	ei[1] = 2;
-	ei[2] = 3;
-	
-	EnumeratorIndex ei2(ei);
-	TEST_EQUAL(ei2 == ei, true)
-	TEST_EQUAL(ei == ei2, true)
-	
-	ei2[0] = 0;
-	TEST_EQUAL(ei2 == ei, false)
-	TEST_EQUAL(ei == ei2, false)
-
-	ei2[0] = 1;
-	TEST_EQUAL(ei2 == ei, true)
-	TEST_EQUAL(ei == ei2, true)
-
-	ei2[1] = 0;
-	TEST_EQUAL(ei2 == ei, false)
-	TEST_EQUAL(ei == ei2, false)
-
-	ei2[1] = 2;
-	TEST_EQUAL(ei2 == ei, true)
-	TEST_EQUAL(ei == ei2, true)
-
-	ei2[2] = 0;
-	TEST_EQUAL(ei2 == ei, false)
-	TEST_EQUAL(ei == ei2, false)
-
-	ei2[2] = 3;
-	TEST_EQUAL(ei2 == ei, true)
-	TEST_EQUAL(ei == ei2, true)
-
-	EnumeratorIndex ei3;
-	TEST_EQUAL(ei3 == ei, false)
-	TEST_EQUAL(ei == ei3, false)
-
-	ei3 << 2 << 3;
-	TEST_EQUAL(ei3 == ei, false)
-	TEST_EQUAL(ei == ei3, false)
-
-	ei3 << 4;
-	TEST_EQUAL(ei3 == ei, false)
-	TEST_EQUAL(ei == ei3, false)
-	
-	ei3 << 5;
-	TEST_EQUAL(ei3 == ei, false)
-	TEST_EQUAL(ei == ei3, false)
-	
-	EnumeratorIndex ei4;
-	ei4 << 2 << 3 << 5;
-	TEST_EQUAL(ei4 == ei, false)
-	TEST_EQUAL(ei == ei4, false)
-RESULT
-
-
-CHECK(bool EnumeratorIndex::operator != (const EnumeratorIndex& rhs))
-	EnumeratorIndex ei;
-	ei << 2 << 3 << 4;
-	ei[0] = 1;
-	ei[1] = 2;
-	ei[2] = 3;
-	
-	EnumeratorIndex ei2(ei);
-	TEST_EQUAL(ei2 != ei, false)
-	TEST_EQUAL(ei != ei2, false)
-	
-	ei2[0] = 0;
-	TEST_EQUAL(ei2 != ei, true)
-	TEST_EQUAL(ei != ei2, true)
-
-	ei2[0] = 1;
-	TEST_EQUAL(ei2 != ei, false)
-	TEST_EQUAL(ei != ei2, false)
-
-	ei2[1] = 0;
-	TEST_EQUAL(ei2 != ei, true)
-	TEST_EQUAL(ei != ei2, true)
-
-	ei2[1] = 2;
-	TEST_EQUAL(ei2 != ei, false)
-	TEST_EQUAL(ei != ei2, false)
-
-	ei2[2] = 0;
-	TEST_EQUAL(ei2 != ei, true)
-	TEST_EQUAL(ei != ei2, true)
-
-	ei2[2] = 3;
-	TEST_EQUAL(ei2 != ei, false)
-	TEST_EQUAL(ei != ei2, false)
-
-	EnumeratorIndex ei3;
-	TEST_EQUAL(ei3 == ei, false)
-	TEST_EQUAL(ei == ei3, false)
-	
-	ei3 << 2 << 3;
-	TEST_EQUAL(ei3 == ei, false)
-	TEST_EQUAL(ei == ei3, false)
-
-	ei3 << 4;
-	TEST_EQUAL(ei3 != ei, true)
-	TEST_EQUAL(ei != ei3, true)
-	
-	ei3 << 5;
-	TEST_EQUAL(ei3 != ei, true)
-	TEST_EQUAL(ei != ei3, true)
-	
-	EnumeratorIndex ei4;
-	ei4 << 2 << 3 << 5;
-	TEST_EQUAL(ei3 != ei, true)
-	TEST_EQUAL(ei != ei3, true)
-RESULT
-
-EnumeratorIndex ei;
-ei << 3 << 4 << 5;
-ei[0] = 1;
-ei[1] = 2;
-ei[2] = 3;
-
-EnumeratorIndex ei_equal(ei);
-
-// lesser in the first digit
-EnumeratorIndex ei_lesser_1;
-ei_lesser_1 << 3 << 4 << 5;
-ei_lesser_1[0] = 0;
-ei_lesser_1[1] = 2;
-ei_lesser_1[2] = 3;
-
-// lesser in the 2nd digit
-EnumeratorIndex ei_lesser_2;
-ei_lesser_2 << 3 << 4 << 5;
-ei_lesser_2[0] = 1;
-ei_lesser_2[1] = 1;
-ei_lesser_2[2] = 3;
-
-// lesser in the 3rd digit
-EnumeratorIndex ei_lesser_3;
-ei_lesser_3 << 3 << 4 << 5;
-ei_lesser_3[0] = 1;
-ei_lesser_3[1] = 2;
-ei_lesser_3[2] = 2;
-
-// greater in the first digit
-EnumeratorIndex ei_greater_1;
-ei_greater_1 << 3 << 4 << 5;
-ei_greater_1[0] = 2;
-ei_greater_1[1] = 2;
-ei_greater_1[2] = 3;
-
-// greater in the 2nd digit
-EnumeratorIndex ei_greater_2;
-ei_greater_2 << 3 << 4 << 5;
-ei_greater_2[0] = 1;
-ei_greater_2[1] = 3;
-ei_greater_2[2] = 3;
-
-// greater in the 3rd digit
-EnumeratorIndex ei_greater_3;
-ei_greater_3 << 3 << 4 << 5;
-ei_greater_3[0] = 1;
-ei_greater_3[1] = 2;
-ei_greater_3[2] = 4;
-
-// different modulus size
-EnumeratorIndex ei_lesser_size;
-ei_lesser_size << 3 << 4;
-EnumeratorIndex ei_greater_size;
-ei_greater_size << 3 << 4 << 5 << 6;
-
-// different modulus values
-EnumeratorIndex ei_mod_diff_1;
-ei_mod_diff_1 << 2 << 4 << 5;
-EnumeratorIndex ei_mod_diff_2;
-ei_mod_diff_1 << 3 << 3 << 5;
-EnumeratorIndex ei_mod_diff_3;
-ei_mod_diff_1 << 3 << 4 << 6;
-
-CHECK(bool EnumeratorIndex::operator < (const EnumeratorIndex& rhs))
-	TEST_EQUAL(ei < ei_equal, false)
-	TEST_EQUAL(ei_equal < ei, false)
-	
-	TEST_EQUAL(ei_lesser_1 < ei, true)
-	TEST_EQUAL(ei_lesser_2 < ei, true)
-	TEST_EQUAL(ei_lesser_3 < ei, true)
-	TEST_EQUAL(ei < ei_lesser_1, false)
-	TEST_EQUAL(ei < ei_lesser_2, false)
-	TEST_EQUAL(ei < ei_lesser_2, false)
-
-	TEST_EQUAL(ei_greater_1 < ei, false)
-	TEST_EQUAL(ei_greater_2 < ei, false)
-	TEST_EQUAL(ei_greater_3 < ei, false)
-	TEST_EQUAL(ei < ei_greater_1, true)
-	TEST_EQUAL(ei < ei_greater_2, true)
-	TEST_EQUAL(ei < ei_greater_2, true)
-
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei < ei_lesser_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_lesser_size < ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei < ei_greater_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_greater_size < ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei < ei_mod_diff_1)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_1 < ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei < ei_mod_diff_2)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_2 < ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei < ei_mod_diff_3)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_3 < ei)
-RESULT
-
-
-CHECK(bool EnumeratorIndex::operator > (const EnumeratorIndex& rhs))
-	TEST_EQUAL(ei > ei_equal, false)
-	TEST_EQUAL(ei_equal > ei, false)
-	
-	TEST_EQUAL(ei_lesser_1 > ei, false)
-	TEST_EQUAL(ei_lesser_2 > ei, false)
-	TEST_EQUAL(ei_lesser_3 > ei, false)
-	TEST_EQUAL(ei > ei_lesser_1, true)
-	TEST_EQUAL(ei > ei_lesser_2, true)
-	TEST_EQUAL(ei > ei_lesser_2, true)
-
-	TEST_EQUAL(ei_greater_1 > ei, true)
-	TEST_EQUAL(ei_greater_2 > ei, true)
-	TEST_EQUAL(ei_greater_3 > ei, true)
-	TEST_EQUAL(ei > ei_greater_1, false)
-	TEST_EQUAL(ei > ei_greater_2, false)
-	TEST_EQUAL(ei > ei_greater_2, false)
-
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei > ei_lesser_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_lesser_size > ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei > ei_greater_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_greater_size > ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei > ei_mod_diff_1)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_1 > ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei > ei_mod_diff_2)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_2 > ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei > ei_mod_diff_3)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_3 > ei)
-RESULT
-
-CHECK(bool EnumeratorIndex::operator <= (const EnumeratorIndex& rhs))
-	TEST_EQUAL(ei <= ei_equal, true)
-	TEST_EQUAL(ei_equal <= ei, true)
-	
-	TEST_EQUAL(ei_lesser_1 <= ei, true)
-	TEST_EQUAL(ei_lesser_2 <= ei, true)
-	TEST_EQUAL(ei_lesser_3 <= ei, true)
-	TEST_EQUAL(ei <= ei_lesser_1, false)
-	TEST_EQUAL(ei <= ei_lesser_2, false)
-	TEST_EQUAL(ei <= ei_lesser_2, false)
-
-	TEST_EQUAL(ei_greater_1 <= ei, false)
-	TEST_EQUAL(ei_greater_2 <= ei, false)
-	TEST_EQUAL(ei_greater_3 <= ei, false)
-	TEST_EQUAL(ei <= ei_greater_1, true)
-	TEST_EQUAL(ei <= ei_greater_2, true)
-	TEST_EQUAL(ei <= ei_greater_2, true)
-
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei <= ei_lesser_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_lesser_size <= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei <= ei_greater_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_greater_size <= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei <= ei_mod_diff_1)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_1 <= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei <= ei_mod_diff_2)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_2 <= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei <= ei_mod_diff_3)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_3 <= ei)
-RESULT
-
-
-CHECK(bool EnumeratorIndex::operator >= (const EnumeratorIndex& rhs))
-	TEST_EQUAL(ei >= ei_equal, true)
-	TEST_EQUAL(ei_equal >= ei, true)
-	
-	TEST_EQUAL(ei_lesser_1 >= ei, false)
-	TEST_EQUAL(ei_lesser_2 >= ei, false)
-	TEST_EQUAL(ei_lesser_3 >= ei, false)
-	TEST_EQUAL(ei >= ei_lesser_1, true)
-	TEST_EQUAL(ei >= ei_lesser_2, true)
-	TEST_EQUAL(ei >= ei_lesser_2, true)
-
-	TEST_EQUAL(ei_greater_1 >= ei, true)
-	TEST_EQUAL(ei_greater_2 >= ei, true)
-	TEST_EQUAL(ei_greater_3 >= ei, true)
-	TEST_EQUAL(ei >= ei_greater_1, false)
-	TEST_EQUAL(ei >= ei_greater_2, false)
-	TEST_EQUAL(ei >= ei_greater_2, false)
-
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei >= ei_lesser_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_lesser_size >= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei >= ei_greater_size)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_greater_size >= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei >= ei_mod_diff_1)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_1 >= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei >= ei_mod_diff_2)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_2 >= ei)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei >= ei_mod_diff_3)
-	TEST_EXCEPTION(EnumeratorIndex::IncompatibleIndex, ei_mod_diff_3 >= ei)
-RESULT
-
-
-// tests for class Enumerator::
-
 Enumerator<string, string::iterator, char>* enumerator_ptr;
-CHECK(Enumerator::Enumerator())
+CHECK(Enumerator() throw())
   enumerator_ptr = new Enumerator<string, string::iterator, char>;
 	TEST_NOT_EQUAL(enumerator_ptr, 0);
 RESULT
-	
 
-CHECK(Enumerator::~Enumerator())
+CHECK(~Enumerator() throw())
   delete enumerator_ptr;
 RESULT
 
 String s;
-CHECK(Enumerator::countVariants())
+CHECK(Size countVariants() throw())
 	Enumerator<String, String::iterator, char> enumerator(s, char_assign);	
   TEST_EQUAL(enumerator.countVariants(), 1);
 RESULT
 
 s = "12345";
 Enumerator<String, String::iterator, char> enumerator(s, char_assign);
-CHECK(Enumerator::addVariants(VariantIterator& it, const VariantVector& variants))
+CHECK(void addVariants(const SiteIterator& it, const VariantVector& variants) throw())
 	vector<char> variants; 
 	variants.push_back('0');
 	variants.push_back('1');
@@ -562,7 +72,7 @@ CHECK(Enumerator::addVariants(VariantIterator& it, const VariantVector& variants
 	TEST_EQUAL(enumerator.countVariants(), 100000)
 RESULT
 
-CHECK(Enumerator::createCombination(Position i))
+CHECK(void createCombination(const Position index) throw(Exception::IndexOverflow))
 	for (Position i = 0; i < enumerator.countVariants(); i++) 
 	{
 		enumerator.createCombination(i); 
@@ -572,7 +82,17 @@ CHECK(Enumerator::createCombination(Position i))
 	}
 RESULT
 
-CHECK(Enumerator::deleteVariants(VariantIterator& it, const VariantVector& variants))
+CHECK(void createCombination(const EnumeratorIndex& index) throw(EnumeratorIndex::IncompatibleIndex))
+	for (Position i = 0; i < enumerator.countVariants(); i++) 
+	{
+		enumerator.createCombination(i); 
+		String p = s;
+		p.reverse();
+		TEST_EQUAL(p.toUnsignedInt(), i)
+	}
+RESULT
+
+CHECK(void deleteVariants(const SiteIterator& it, const VariantVector& variants) throw())
 	vector<char> variants; 
 	variants.push_back('0');
 	variants.push_back('1');
@@ -597,12 +117,12 @@ CHECK(Enumerator::deleteVariants(VariantIterator& it, const VariantVector& varia
 	}
 RESULT
 
-CHECK(Enumerator::begin())
+CHECK(Iterator begin() throw())
 	Enumerator<String, String::iterator, char>::Iterator it = enumerator.begin();
 	TEST_EQUAL(it.isValid(), true)
 RESULT
 
-CHECK(Enumerator::end())
+CHECK(Iterator end() throw())
 	Size counter = 0;
 	Enumerator<String, String::iterator, char>::Iterator it = enumerator.begin();
 	for (; it != enumerator.end(); ++it, counter++);
@@ -611,13 +131,13 @@ CHECK(Enumerator::end())
 	TEST_EQUAL(counter, 10000)
 RESULT
 
-CHECK(Enumerator::begin() const)
+CHECK(ConstIterator begin() const throw())
 	Enumerator<String, String::iterator, char>& c_enumerator(enumerator);
 	Enumerator<String, String::iterator, char>::ConstIterator it = c_enumerator.begin();
 	TEST_EQUAL(it.isValid(), true)
 RESULT
 
-CHECK(Enumerator::end() const)
+CHECK(ConstIterator end() const throw())
 	Size counter = 0;
 	Enumerator<String, String::iterator, char>& c_enumerator(enumerator);
 	Enumerator<String, String::iterator, char>::ConstIterator it = c_enumerator.begin();
@@ -625,7 +145,7 @@ CHECK(Enumerator::end() const)
 	TEST_EQUAL(counter, 10000)
 RESULT
 
-CHECK(Enumerator iteration)
+CHECK([EXTRA] Enumerator iteration)
 	Size counter = 0;
 	Enumerator<String, String::iterator, char>::Iterator it = enumerator.begin();
 	for (; it != enumerator.end(); ++it, counter++)
@@ -634,6 +154,18 @@ CHECK(Enumerator iteration)
 		p.reverse();
 		TEST_EQUAL(p.toUnsignedInt(), counter)
 	}
+RESULT
+
+CHECK(Container& getCurrent() throw())
+  // ???
+RESULT
+
+CHECK(Enumerator(Container& container) throw())
+  // ???
+RESULT
+
+CHECK(Enumerator(Container& container, MutatorFunction mutator) throw())
+  // ???
 RESULT
 
 /////////////////////////////////////////////////////////////
