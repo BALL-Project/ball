78106a7d9b08f7c2322e9e0fddd3edca1ba066b9
amoll <amoll>
2003-06-20 14:43:22 +0000

    added some missing tests

--- source/TEST/RegularData3D_test.C
+++ source/TEST/RegularData3D_test.C
@@ -1,12 +1,12 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: RegularData3D_test.C,v 1.12 2003/05/23 10:26:04 oliver Exp $
+// $Id: RegularData3D_test.C,v 1.13 2003/06/20 14:43:22 amoll Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 #include <BALL/DATATYPE/regularData3D.h>
 
-START_TEST(RegularData3D, "$Id: RegularData3D_test.C,v 1.12 2003/05/23 10:26:04 oliver Exp $")
+START_TEST(RegularData3D, "$Id: RegularData3D_test.C,v 1.13 2003/06/20 14:43:22 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -17,16 +17,16 @@ using namespace std;
 String filename;
 RegularData3D*	grid;
 
-CHECK(RegularData3D<T>())
+CHECK(TRegularData3D() throw())
 	grid = new RegularData3D();
 	TEST_NOT_EQUAL(grid, 0)
 RESULT
 
-CHECK(~RegularData3D<T>())
+CHECK(~TRegularData3D() throw())
 	delete grid;
 RESULT
 
-CHECK(RegularData3D<T>(const Indextype& size, const CoordinateType& origin, const CoordinateType& dimension))
+CHECK(TRegularData3D(const IndexType& size, const CoordinateType& origin = CoordinateType(0.0), const CoordinateType& dimension = CoordinateType(1.0)) throw(Exception::OutOfMemory))
 	grid = new RegularData3D(RegularData3D::IndexType(11, 11, 11), Vector3(0.0, 0.0, 0.0), Vector3(10.0, 10.0, 10.0));
 	TEST_NOT_EQUAL(grid, 0)
 	TEST_EQUAL(grid->size(), 1331)
@@ -37,25 +37,21 @@ using BALL::Vector3;
 Vector3	lower(0.0, 0.0, 0.0);
 Vector3	upper(10.0, 10.0, 10.0);
 
-CHECK(RegularData3D<T>(const Vector3& lower, const Vector3& upper, float spacing))
+CHECK(TRegularData3D(const CoordinateType& origin, const CoordinateType& dimension, const CoordinateType& spacing) throw(Exception::OutOfMemory))
 	grid = new RegularData3D(lower, upper - lower, Vector3(1.0));
 	TEST_NOT_EQUAL(grid, 0)
 	TEST_EQUAL(grid->size(), 1331)
 	delete grid;
+	grid = new RegularData3D(lower, upper - lower, Vector3(1.0));
 RESULT
 
-CHECK(RegularData3D<T>(const Vector3& lower, const Vector3& upper, Size, Size, Size))
-	grid = new RegularData3D(RegularData3D::IndexType(11, 11, 11), lower, upper - lower);
-	TEST_NOT_EQUAL(grid, 0)
-RESULT
-
-CHECK(size())
+CHECK(size_type size() const throw())
 	TEST_EQUAL(grid->size(), 1331)
 RESULT
 
 RegularData3D g(RegularData3D::IndexType(11, 11, 11), lower, upper - lower);
 
-CHECK(resize())
+CHECK(void resize(const IndexType& size) throw(Exception::OutOfMemory))
 	Vector3 origin(2.0);
 	Vector3 dimension(10.0);
 
@@ -72,20 +68,20 @@ CHECK(resize())
 	TEST_EQUAL(g1.getDimension(), Vector3(dimension.x * 3. / 2, dimension.y * 4. / 2., dimension.y * 5. / 2.))
 RESULT
 
-CHECK(operator = (const RegularData3D<T>& grid))
+CHECK(TRegularData3D& operator = (const TRegularData3D<ValueType>& data) throw(Exception::OutOfMemory))
 	RegularData3D g1;
 	g1 = g;
 	TEST_EQUAL(g1.size(), 1331)
 	TEST_EQUAL((g1 == g), true)
 RESULT
 
-CHECK(getSize())
+CHECK(const IndexType& getSize() const throw())
 	TEST_EQUAL(grid->getSize().x, 11)
 	TEST_EQUAL(grid->getSize().y, 11)
 	TEST_EQUAL(grid->getSize().z, 11)
 RESULT
 
-CHECK(getSpacing())
+CHECK(const CoordinateType& getSpacing() const throw())
 	TEST_REAL_EQUAL(grid->getSpacing().x, 1.0)
 	TEST_REAL_EQUAL(grid->getSpacing().y, 1.0)
 	TEST_REAL_EQUAL(grid->getSpacing().z, 1.0)
@@ -93,42 +89,55 @@ RESULT
 
 RegularData3D::IndexType	index;
 
-CHECK(getIndex(const Vector3& vector))
-	lower.set(3.49, 3.51, 3.0);
+CHECK(IndexType getClosestIndex(const CoordinateType& v) const throw(Exception::OutOfGrid))
+ 	lower.set(3.49, 3.51, 3.0);
 	index = grid->getClosestIndex(lower);
 	TEST_EQUAL(index.x, 3)
 	TEST_EQUAL(index.y, 4)
 	TEST_EQUAL(index.z, 3)
 RESULT
 
-CHECK(getData(Position))
+CHECK(ValueType& getClosestValue(const CoordinateType& x) throw(Exception::OutOfGrid))
+	g.getClosestValue(Vector3(1.2, 2.3, 4.3)) = 1.232;
+	TEST_REAL_EQUAL(g[RegularData3D::IndexType(1,2,4)], 1.232)
+RESULT
+
+CHECK(const ValueType& getClosestValue(const CoordinateType& x) const throw(Exception::OutOfGrid))
+	const RegularData3D& rd = g;
+	TEST_REAL_EQUAL(rd.getClosestValue(Vector3(1.2, 2.3, 4.3)), 1.232)
+RESULT
+
+CHECK(ValueType& getData(Position index) throw(Exception::OutOfGrid))
 	grid->getData(RegularData3D::IndexType(0, 0, 0)) = 5.4321;		
 	lower = grid->getOrigin();
 	TEST_REAL_EQUAL(grid->getData(0), 5.4321);
 	TEST_REAL_EQUAL(grid->getData(0), grid->getData(grid->getClosestIndex(lower)));
 RESULT
 
-CHECK(operator[]/1/2)
+CHECK(ValueType& operator [] (Position index) throw())
 	(*grid)[3 + 11 * 3 + 11 * 11 * 3] = 1.2345;
-	lower.set(3.0, 3.0, 3.0);
-	TEST_EQUAL((*grid)[3 + 11 * 3 + 11 * 11 * 3], (*grid)[grid->getClosestIndex(lower)]);
 RESULT
 
-CHECK(getCoordinates/1)
+CHECK(const ValueType& operator [] (Position index) const throw())
+	lower.set(3.0, 3.0, 3.0);
+	const RegularData3D& const_grid = *grid;
+	TEST_EQUAL(const_grid[3 + 11 * 3 + 11 * 11 * 3], (*grid)[grid->getClosestIndex(lower)]);
+RESULT
+
+
+CHECK(CoordinateType getCoordinates(Position index) const throw(Exception::OutOfGrid))
 	lower = grid->getCoordinates(0);
 	TEST_REAL_EQUAL(lower.x, 0.0)
 	TEST_REAL_EQUAL(lower.y, 0.0)
 	TEST_REAL_EQUAL(lower.z, 0.0)
-RESULT
 
-CHECK(getCoordinates/2)
 	lower = grid->getCoordinates(2 + 2 * 11 + 2 * 11 * 11);
 	TEST_REAL_EQUAL(lower.x, 2.0)
 	TEST_REAL_EQUAL(lower.y, 2.0)
 	TEST_REAL_EQUAL(lower.z, 2.0)
 RESULT
 
-CHECK(getCoordinates/3)
+CHECK(CoordinateType getCoordinates(const IndexType& index) const throw(Exception::OutOfGrid))
 	upper.set(3.999999, 4.0, 3.0001);
 	lower = grid->getCoordinates(grid->getClosestIndex(upper));
 	TEST_REAL_EQUAL(lower.x, 4.0)
@@ -136,7 +145,7 @@ CHECK(getCoordinates/3)
 	TEST_REAL_EQUAL(lower.z, 3.0)
 RESULT
 
-CHECK(getBoxIndices)
+CHECK(void getEnclosingIndices(const CoordinateType& r, Position& llf, Position& rlf, Position& luf, Position& ruf, Position& llb, Position& rlb, Position& lub, Position& rub) const throw(Exception::OutOfGrid))
 	lower.set(2, 2, 2);
 	Position p1, p2, p3, p4, p5, p6, p7, p8;
 	g.getEnclosingIndices(lower, p1, p2, p3, p4, p5, p6, p7, p8);
@@ -164,7 +173,8 @@ CHECK(getBoxIndices)
 	TEST_EXCEPTION(Exception::OutOfGrid, g.getEnclosingIndices(lower, p1, p2, p3, p4, p5, p6, p7, p8))
 RESULT
 
-CHECK(getBoxData)
+
+CHECK(void getEnclosingValues(const CoordinateType& r, ValueType& llf, ValueType& rlf, ValueType& luf, ValueType& ruf, ValueType& llb, ValueType& rlb, ValueType& lub, ValueType& rub) const throw(Exception::OutOfGrid))
 	lower.set(2, 2, 2);
 	float p1, p2, p3, p4, p5, p6, p7, p8;
 	g[266] = 1;
@@ -189,14 +199,14 @@ CHECK(getBoxData)
 	TEST_EXCEPTION(Exception::OutOfGrid, g.getEnclosingValues(lower, p1, p2, p3, p4, p5, p6, p7, p8))
 RESULT
 
-CHECK(getOrigin)
+CHECK(const CoordinateType& getOrigin() const throw())
 	lower = grid->getOrigin();
 	TEST_REAL_EQUAL(lower.x, 0.0)
 	TEST_REAL_EQUAL(lower.y, 0.0)
 	TEST_REAL_EQUAL(lower.z, 0.0)
 RESULT
 
-CHECK(setOrigin/1)
+CHECK(void setOrigin(const CoordinateType& origin) throw())
 	grid->setOrigin(Vector3(1.0, 1.0, 1.0));
 	lower = grid->getOrigin();
 	TEST_REAL_EQUAL(lower.x, 1.0)
@@ -206,9 +216,7 @@ CHECK(setOrigin/1)
 	TEST_EQUAL(index.x, 2)
 	TEST_EQUAL(index.y, 3)
 	TEST_EQUAL(index.z, 2)
-RESULT
 
-CHECK(setOrigin/2)
 	lower.set(2.0, 2.0, 2.0);
 	grid->setOrigin(lower);
 	lower = grid->getOrigin();
@@ -221,13 +229,13 @@ CHECK(setOrigin/2)
 	TEST_EQUAL(index.z, 1)
 RESULT
 
-CHECK(getDimension)
+CHECK(const CoordinateType& getDimension() const throw())
 	TEST_REAL_EQUAL(grid->getDimension().x, 10.0)
 	TEST_REAL_EQUAL(grid->getDimension().y, 10.0)
 	TEST_REAL_EQUAL(grid->getDimension().z, 10.0)
 RESULT
 
-CHECK(getInterpolatedValue)
+CHECK(ValueType getInterpolatedValue(const CoordinateType& x) const throw(Exception::OutOfGrid))
 	lower.set(2, 2, 2);
 	TEST_EQUAL(g.getInterpolatedValue(lower), 1)
 	lower.set(0, 0, 0);
@@ -240,21 +248,21 @@ RESULT
 
 RegularData3D grid2 = *grid;
 
-CHECK(clear())
+CHECK(void clear() throw())
 	grid2.clear();
 	TEST_EQUAL(grid2.getSize().x, 0)
 	TEST_EQUAL(grid2.getSize().y, 0)
 	TEST_EQUAL(grid2.getSize().z, 0)
 RESULT
 
-CHECK(operator ==)
+CHECK(bool operator == (const TRegularData3D<ValueType>& grid) const throw())
 	grid2 = *grid;
 	TEST_EQUAL(*grid == grid2, true)
 	grid2[5] = -99.9;
 	TEST_EQUAL(*grid == grid2, false)
 RESULT
 
-CHECK(operator !=)
+CHECK(bool operator != (const TRegularData3D<ValueType>& grid) const throw())
 	TEST_EQUAL(*grid != grid2, true)
 	grid2[5] = (*grid)[5];
 	TEST_EQUAL(*grid != grid2, false)
@@ -262,7 +270,8 @@ RESULT
 delete grid;
 
 
-CHECK(isInside()1/1)
+
+CHECK(bool isInside(const CoordinateType& r) const throw())
 	RegularData3D g(RegularData3D::IndexType(11, 11, 11), Vector3(0.0, 0.0, 0.0), Vector3(10.0, 10.0, 10.0));
 	TEST_EQUAL(g.isInside(Vector3(0.0, 0.0, 0.0)), true)
 	Vector3 v(0.0, 0.0, 0.0);
@@ -286,7 +295,7 @@ CHECK(isInside()1/1)
 	TEST_EQUAL(h.isInside(v), true)
 RESULT
 
-CHECK(operator << (ostream& os, const RegularData3D&))
+CHECK([EXTRA]operator << (ostream& os, const RegularData3D&))
 	String filename;
 	NEW_TMP_FILE(filename)
 	
@@ -343,6 +352,141 @@ CHECK(operator << (ostream& os, const RegularData3D&))
 		//TEST_REAL_EQUAL(data[i], in_data[i])
 	}
 RESULT
+
+CHECK([EXTRA]operator >> (instream& os, RegularData3D&))
+RESULT
+
+CHECK(BALL_CREATE(TRegularData3D<ValueType>))
+  // ???
+RESULT
+
+CHECK(ConstIterator begin() const throw())
+	RegularData3D rg;
+	RegularData3D::ConstIterator it = rg.begin();
+	TEST_EQUAL(it == rg.end(), true)
+	rg = RegularData3D(Vector3(0,0,0),Vector3(1,0,0),Vector3(1,1,1));
+	rg[0] = 1.23;
+	it = rg.begin();
+	TEST_REAL_EQUAL(*it, 1.23)
+	it++;
+	TEST_REAL_EQUAL(*it, 0)
+RESULT
+
+CHECK(ConstIterator end() const throw())
+	RegularData3D rg;
+	RegularData3D::ConstIterator it = rg.end();
+	TEST_EQUAL(it == rg.end(), true)
+RESULT
+
+CHECK(Iterator begin() throw())
+	RegularData3D rg;
+	RegularData3D::Iterator it = rg.begin();
+	TEST_EQUAL(it == rg.end(), true)
+	rg = RegularData3D(Vector3(0,0,0),Vector3(1,0,0),Vector3(1,1,1));
+	rg[0] = 1.23;
+	it = rg.begin();
+	*it = 2.34;
+	TEST_REAL_EQUAL(*it, 2.34)
+	it++;
+	TEST_REAL_EQUAL(*it, 0)
+RESULT
+
+CHECK(Iterator end() throw())
+	RegularData3D rg;
+	RegularData3D::Iterator it = rg.end();
+	TEST_EQUAL(it == rg.end(), true)
+RESULT
+
+
+CHECK(IndexType getLowerIndex(const CoordinateType& v) const throw(Exception::OutOfGrid))
+  RegularData3D g(Vector3(5.0, 7.0, 9.0), Vector3(2.0, 3.0, 4.0), Vector3(1.0, 0.5, 0.5));
+	TEST_EQUAL(g.getLowerIndex(Vector3(6.6, 8.76, 9.3)).x, 1)
+	TEST_EQUAL(g.getLowerIndex(Vector3(6.6, 8.76, 9.3)).y, 3)
+	TEST_EQUAL(g.getLowerIndex(Vector3(6.6, 8.76, 9.3)).z, 0)
+	RegularData3D empty;
+	TEST_EXCEPTION(Exception::OutOfGrid, empty.getLowerIndex(Vector3(6.4, 8.72, 1)))
+RESULT
+
+CHECK(IndexType())
+	RegularData3D::IndexType* it = new RegularData3D::IndexType();
+	TEST_NOT_EQUAL(it, 0)
+	TEST_EQUAL(it->x, 0)
+	TEST_EQUAL(it->y, 0)
+	TEST_EQUAL(it->z, 0)
+RESULT
+
+CHECK(IndexType(Position p))
+	RegularData3D::IndexType* it = new RegularData3D::IndexType(5);
+	TEST_NOT_EQUAL(it, 0)
+	TEST_EQUAL(it->x, 5)
+	TEST_EQUAL(it->y, 5)
+	TEST_EQUAL(it->z, 5)
+RESULT
+
+CHECK(IndexType(Position p, Position q, Position r))
+	RegularData3D::IndexType* it = new RegularData3D::IndexType(1,2,3);
+	TEST_NOT_EQUAL(it, 0)
+	TEST_EQUAL(it->x, 1)
+	TEST_EQUAL(it->y, 2)
+	TEST_EQUAL(it->z, 3)
+RESULT
+
+CHECK(TRegularData3D(const TRegularData3D<ValueType>& grid) throw(Exception::OutOfMemory))
+  // ???
+RESULT
+
+CHECK(ValueType operator () (const CoordinateType& x) const throw())
+  // ???
+RESULT
+
+CHECK(ValueType& getData(const IndexType& index) throw(Exception::OutOfGrid))
+  // ???
+RESULT
+
+CHECK(ValueType& operator [] (const IndexType& index) throw())
+  // ???
+RESULT
+
+CHECK(const ValueType& operator [] (const IndexType& index) const throw())
+  // ???
+RESULT
+
+CHECK(bool empty() const throw())
+  // ???
+RESULT
+
+CHECK(const ValueType& getData(Position index) const throw(Exception::OutOfGrid))
+  // ???
+RESULT
+
+CHECK(const ValueType& getData(const IndexType& index) const throw(Exception::OutOfGrid))
+  // ???
+RESULT
+
+CHECK(size_type max_size() const throw())
+  // ???
+RESULT
+
+CHECK(void binaryRead(const String& filename) throw())
+  // ???
+RESULT
+
+CHECK(void binaryWrite(const String& filename) const throw())
+  // ???
+RESULT
+
+CHECK(void rescale(const IndexType& new_size) throw(Exception::OutOfMemory))
+  // ???
+RESULT
+
+CHECK(void setDimension(const CoordinateType& dimension) throw())
+  // ???
+RESULT
+
+CHECK(void swap(TRegularData3D<ValueType>& grid))
+  // ???
+RESULT
+
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
