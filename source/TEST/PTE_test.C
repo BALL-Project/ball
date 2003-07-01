9bcbe940fd034fae4d220a3f02b644559bb70a6b
amoll <amoll>
2003-07-01 13:06:24 +0000

    no message

--- source/TEST/PTE_test.C
+++ source/TEST/PTE_test.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: PTE_test.C,v 1.11 2003/05/23 10:26:04 oliver Exp $
+// $Id: PTE_test.C,v 1.12 2003/07/01 13:06:24 amoll Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 
@@ -10,24 +10,27 @@
 #include "ItemCollector.h"
 ///////////////////////////
 
-START_TEST(Element, "$Id: PTE_test.C,v 1.11 2003/05/23 10:26:04 oliver Exp $")
+START_TEST(Element, "$Id: PTE_test.C,v 1.12 2003/07/01 13:06:24 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 using namespace BALL;
 
+// tests for Element
+
 Element*	e;
-CHECK(Element())
+CHECK(Element() throw())
 	e = new Element;
 	TEST_NOT_EQUAL(e, 0)
 RESULT
 
-CHECK(~Element())
+CHECK(~Element() throw())
 	delete e;
+  // ???
 RESULT
 
-CHECK(Element(Element& element))
+CHECK(Element(const Element& element) throw())
 	Element* e1 = new Element;
 	e1->setName("testname");
 	Element* e2 = new Element(*e1);
@@ -41,18 +44,9 @@ CHECK(Element(Element& element))
 RESULT
 
 
-Element static_element("Aluminum", "Al", 13, 3, 13, 26.981539,  1.43,  1.25, 2.05, 1.61);
-CHECK(Element(const String& name,
-						 const String& symbol,
-						 Group group,
-						 Period period,
-						 AtomicNumber atomic_umber,
-						 float atomic_weight,
-						 float atomic_radius,
-						 float covalent_radius,
-						 float van_der_waals_radius,
-						 float electronegativity))
+CHECK(Element(const String& name, const String& symbol, Group group, Period period, AtomicNumber atomic_umber, float atomic_weight, float atomic_radius, float covalent_radius, float van_der_waals_radius, float electronegativity) throw())
 
+	Element static_element("Aluminum", "Al", 13, 3, 13, 26.981539,  1.43,  1.25, 2.05, 1.61);
 	TEST_EQUAL(static_element.getName(), "Aluminum")
 	TEST_EQUAL(static_element.getSymbol(), "Al")
 	TEST_EQUAL(static_element.getGroup(), 13)
@@ -102,144 +96,155 @@ Element e1;
 Element e2;
 Element e3;
 
-CHECK(setName(const String& name))
+CHECK(void setName(const String& name) throw())
 	e1.setName("e1");
 RESULT
 
-CHECK(getName())
+CHECK(const String& getName() const throw())
 	TEST_EQUAL(e1.getName(), "e1")
 	TEST_EQUAL(e2.getName(), "Unknown")
 RESULT
 
-CHECK(setSymbol(const String& symbol))
+CHECK(void setSymbol(const String& symbol) throw())
 	e1.setSymbol("s");
 RESULT
 
-CHECK(getSymbol())
+CHECK(const String& getSymbol() const throw())
 	TEST_EQUAL(e1.getSymbol(), "s")
 	TEST_EQUAL(e2.getSymbol(), "?")
 RESULT
 
-CHECK(setGroup(Group group))
+CHECK(void setGroup(Group group) throw())
 	e1.setGroup(2);
 RESULT
 
-CHECK(getGroup())
+CHECK(Group getGroup() const throw())
 	TEST_EQUAL(e1.getGroup(), 2)
 	TEST_EQUAL(e2.getGroup(), 0)
 RESULT
 
-CHECK(setPeriod(Period Period))
+CHECK(void setPeriod(Period period) throw())
 	e1.setPeriod(3);
 RESULT
 
-CHECK(getPeriod())
+CHECK(Period getPeriod() const throw())
 	TEST_EQUAL(e1.getPeriod(), 3)
 	TEST_EQUAL(e2.getPeriod(), 0)
 RESULT
 
-CHECK(setAtomicNumber(AtomicNumber AtomicNumber))
+CHECK(void setAtomicNumber(AtomicNumber atomic_number) throw())
 	e1.setAtomicNumber(4);
 RESULT
 
-CHECK(getAtomicNumber())
+CHECK(AtomicNumber getAtomicNumber() const throw())
 	TEST_EQUAL(e1.getAtomicNumber(), 4)
 	TEST_EQUAL(e2.getAtomicNumber(), 0)
 RESULT
 
-CHECK(setAtomicWeight(float AtomicWeight))
+CHECK(void setAtomicWeight(float atomic_weight) throw())
 	e1.setAtomicWeight(5.0);
 RESULT
 
-CHECK(getAtomicWeight())
+CHECK(float getAtomicWeight() const throw())
 	TEST_EQUAL(e1.getAtomicWeight(), 5.0)
 	TEST_EQUAL(e2.getAtomicWeight(), 0.0)
 RESULT
 
-CHECK(setAtomicRadius(float AtomicRadius))
+CHECK(void setAtomicRadius(float atomic_radius) throw())
 	e1.setAtomicRadius(6.0);
 RESULT
 
-CHECK(getAtomicRadius())
+CHECK(float getAtomicRadius() const throw())
 	TEST_EQUAL(e1.getAtomicRadius(), 6.0)
 	TEST_EQUAL(e2.getAtomicRadius(), 0.0)
 RESULT
 
-CHECK(setCovalentRadius(float CovalentRadius))
+CHECK(void setCovalentRadius(float covalent_radius) throw())
 	e1.setCovalentRadius(7.0);
 RESULT
 
-CHECK(getCovalentRadius())
+CHECK(float getCovalentRadius() const throw())
 	TEST_EQUAL(e1.getCovalentRadius(), 7.0)
 	TEST_EQUAL(e2.getCovalentRadius(), 0.0)
 RESULT
 
-CHECK(setVanDerWaalsRadius(float VanDerWaalsRadius))
+CHECK(void setVanDerWaalsRadius(float van_der_waals_radius) throw())
 	e1.setVanDerWaalsRadius(8.0);
 RESULT
 
-CHECK(getVanDerWaalsRadius())
+CHECK(float getVanDerWaalsRadius() const throw())
 	TEST_EQUAL(e1.getVanDerWaalsRadius(), 8.0)
 	TEST_EQUAL(e2.getVanDerWaalsRadius(), 0.0)
 RESULT
 
-CHECK(setElectronegativity(float Electronegativity))
+CHECK(void setElectronegativity(float electronegativity) throw())
 	e1.setElectronegativity(9.0);
 RESULT
 
-CHECK(getElectronegativity())
+CHECK(float getElectronegativity() const throw())
 	TEST_EQUAL(e1.getElectronegativity(), 9.0)
 	TEST_EQUAL(e2.getElectronegativity(), 0.0)
 RESULT
 
-CHECK(operator ==(const Element& element) const)
+CHECK(bool operator == (const Element& element) const throw())
 	TEST_EQUAL(e1 == e2, false)
 	TEST_EQUAL(e3 == e2, true)
 RESULT
 
-CHECK(operator !=(const Element& element) const)
+CHECK(bool operator != (const Element& element) const throw())
 	TEST_EQUAL(e3 != e1, true)
 	TEST_EQUAL(e3 != e2, false)
 RESULT
 
-CHECK(operator <(const Element& element) const)
+CHECK(bool operator < (const Element& element) const throw())
 	TEST_EQUAL(e2 < e1, true)
 	TEST_EQUAL(e2 < e3, false)
 RESULT
 
-CHECK(operator <=(const Element& element) const)
+CHECK(bool operator <= (const Element& element) const throw())
 	TEST_EQUAL(e1 <= e2, false)
 	TEST_EQUAL(e3 <= e2, true)
 	TEST_EQUAL(e2 <= e1, true)
 RESULT
 
-CHECK(operator >=(const Element& element) const)
+CHECK(bool operator >= (const Element& element) const throw())
 	TEST_EQUAL(e2 >= e1, false)
 	TEST_EQUAL(e1 >= e2, true)
 	TEST_EQUAL(e2 >= e2, true)
 RESULT
 
-CHECK(operator >(const Element& element) const)
+CHECK(bool operator >(const Element& element) const throw())
 	TEST_EQUAL(e2 > e1, false)
 	TEST_EQUAL(e1 > e2, true)
 	TEST_EQUAL(e2 > e3, false)
 RESULT
 
-CHECK(isUnknown() const)
+CHECK(bool isUnknown() const throw())
 	TEST_EQUAL(e1.isUnknown(), false)
 	TEST_EQUAL(e2.isUnknown(), true)
 RESULT
 
+CHECK(BALL_CREATE(Element))
+  // ???
+RESULT
+
+CHECK(Element& operator = (const Element& element) throw())
+  // ???
+RESULT
+
 String filename;
 NEW_TMP_FILE(filename)
-CHECK(std::ostream& operator << (std::ostream& s, const Element& element))
+CHECK(friend std::ostream& operator << (std::ostream& s, const Element& element) throw())
 	std::ofstream outstr(filename.c_str(), std::ios::out);
 	outstr << e1;
 	outstr.close();
 	TEST_FILE(filename.c_str(), "data/PTE_test.txt")
 RESULT
 
-CHECK(getElement(Position position))
+
+// test for PTE_:
+
+CHECK(static Element& getElement(Position position) throw())
 	TEST_EQUAL(PTE.getElement(1).getName(), "Aluminum")
 	TEST_EQUAL(PTE.getElement(1).getSymbol(), "Al")
 	TEST_EQUAL(PTE.getElement(1).getGroup(), 13)
@@ -252,44 +257,91 @@ CHECK(getElement(Position position))
 	TEST_REAL_EQUAL(PTE.getElement(1).getElectronegativity(), 1.61)
 RESULT
 
-CHECK(getElement(const String& symbol))
-	TEST_EQUAL(PTE.getElement("Al").getName(), "Aluminum")
+CHECK(static Element& getElement(const String& symbol) throw())
+	TEST_EQUAL(PTE["B"].getName(), "Boron")
 RESULT
 
-CHECK(Element& operator [] (const String& symbol))
-	TEST_EQUAL(PTE["Al"].getName(), "Aluminum")
+CHECK(Element& operator [] (const String& symbol) throw())
+	PTE_ p(PTE);
+	p["B"].setName("MyLovelyBoron2");
+	TEST_EQUAL(p[Element::BORON].getName(), "MyLovelyBoron2")
 RESULT
 
-CHECK(Element& operator [] (Element::Symbol symbol))
-	TEST_EQUAL(PTE[Element::Al].getName(), "Aluminum")
+CHECK(Element& operator [] (Element::Symbol symbol) throw())
+	PTE_ p(PTE);
+	p[Element::B].setName("MyLovelyBoron3");
+	TEST_EQUAL(p[Element::BORON].getName(), "MyLovelyBoron3")
 RESULT
 
-CHECK(Element& operator [] (Position position))
-	TEST_EQUAL(PTE[13].getName(), "Aluminum")
+CHECK(Element& operator [] (Position position) throw())
+	PTE_ p(PTE);
+	p[1].setName("MyLovelyHydrogen1");
+	TEST_EQUAL(p[1].getName(), "MyLovelyHydrogen1")
 RESULT
 
-CHECK(apply(UnaryProcessor<PTE_>& processor))
+CHECK(static bool apply(UnaryProcessor<Element>& applicator) throw())
 	ItemCollector<Element> myproc;
 	PTE.apply(myproc);
-
 	TEST_EQUAL(myproc.getSize(), 112)
-
 	TEST_EQUAL(myproc.getPointer()->getName(), "Actinium")
 RESULT
 
-CHECK(PTE_::operator =)
+CHECK(PTE_& operator = (const PTE_& /*pte*/) throw())
 	PTE_ p1, p2;
 	p1 = p2;
 RESULT
 
-CHECK(PTE_::operator ==)
+CHECK(bool operator == (const PTE_& pte) const throw())
 	PTE_ p1, p2;
 	TEST_EQUAL(p1 == p2, true)
 RESULT
 
-CHECK(PTE_::clear())
+CHECK(void clear() throw())
 	PTE_ p1;
 	p1.clear();
+RESULT
+
+CHECK(BALL_CREATE(PTE_))
+	PTE_* ptr = (PTE_*) PTE.create(true, false);
+	TEST_EQUAL((*ptr)[13].getName(), "Aluminum")
+	delete ptr;
+RESULT
+
+PTE_* p_ptr;
+CHECK(PTE_() throw())
+	p_ptr = new PTE_;
+RESULT
+
+CHECK(~PTE_() throw())
+	delete p_ptr;
+RESULT
+
+CHECK(PTE_(const PTE_& pse) throw())
+	PTE_ p(PTE);
+	TEST_EQUAL(p[13].getName(), "Aluminum")
+RESULT
+
+CHECK(Element& operator [] (Element::Name name) throw())
+	PTE_ p(PTE);
+	p[Element::BORON].setName("MyLovelyBoron");
+	TEST_EQUAL(p[Element::BORON].getName(), "MyLovelyBoron")
+RESULT
+
+CHECK(const Element& operator [] (Element::Name name) const throw())
+	TEST_EQUAL(PTE[Element::BORON].getName(), "MyLovelyBoron")
+RESULT
+
+CHECK(const Element& operator [] (Element::Symbol symbol) const throw())
+	TEST_EQUAL(PTE[Element::H].getName(), "MyLovelyHydrogen1")
+RESULT
+
+CHECK(const Element& operator [] (Position position) const throw())
+	TEST_EQUAL(PTE[1].getName(), "MyLovelyHydrogen1")
+	TEST_EQUAL(PTE[113].isUnknown(), true)
+RESULT
+
+CHECK(const Element& operator [] (const String& symbol) const throw())
+	TEST_EQUAL(PTE[Element::H].getName(), "MyLovelyHydrogen1")
 RESULT
 
 /////////////////////////////////////////////////////////////
