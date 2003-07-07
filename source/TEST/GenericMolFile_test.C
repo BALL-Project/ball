38128366ac3dbc9a0da1e05be91ef59c0c683234
amoll <amoll>
2003-07-07 15:08:49 +0000

    no message

--- source/TEST/GenericMolFile_test.C
+++ source/TEST/GenericMolFile_test.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: GenericMolFile_test.C,v 1.4 2002/12/17 16:40:43 oliver Exp $
+// $Id: GenericMolFile_test.C,v 1.5 2003/07/07 15:08:49 amoll Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 
@@ -9,11 +9,10 @@
 
 #include <BALL/FORMAT/genericMolFile.h>
 #include <BALL/KERNEL/system.h>
-#include <BALL/KERNEL/PTE.h>
 
 ///////////////////////////
 
-START_TEST(GenericMolFile, "$Id: GenericMolFile_test.C,v 1.4 2002/12/17 16:40:43 oliver Exp $")
+START_TEST(GenericMolFile, "$Id: GenericMolFile_test.C,v 1.5 2003/07/07 15:08:49 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -22,115 +21,94 @@ using namespace BALL;
 
 GenericMolFile* ptr;
 
-CHECK(GenericMolFile::GenericMolFile())
+CHECK(GenericMolFile() throw())
 	ptr = new GenericMolFile;
 	TEST_NOT_EQUAL(ptr, 0)
 RESULT
 
 
-CHECK(GenericMolFile::~GenericMolFile())
+CHECK(~GenericMolFile() throw())
   delete ptr;
 RESULT
 
 GenericMolFile mol;
 
-CHECK(GenericMolFile::GenericMolFile(const String& filename, File::OpenMode open_mode = std::ios::in))
+CHECK(GenericMolFile(const String& filename, File::OpenMode open_mode = std::ios::in) throw(Exception::FileNotFound))
   mol = GenericMolFile("data/GenericMolFile_test.dat");
   TEST_EQUAL(mol.isValid(), true)
 RESULT
 
 System system;
-CHECK(GenericMolFile::read(System& system))
-  mol.read(system);
+CHECK(bool read(System& system) throw(Exception::ParseError))
+  TEST_EQUAL(mol.read(system), false)
+  String filename;
+  NEW_TMP_FILE(filename)
+  GenericMolFile mol2(filename, std::ios::out);
+  TEST_EQUAL(mol2.read(system), false)
 RESULT
 
-CHECK(GenericMolFile::write(const System& system))
+CHECK(void write(const System& system))
   String filename;
   NEW_TMP_FILE(filename)
   GenericMolFile mol2(filename, std::ios::out);
 	mol2.write(system);
 RESULT
 
-CHECK(GenericMolFile::GenericMolFile& operator >> (System& system))
+CHECK(GenericMolFile& operator >> (System& system) throw(Exception::ParseError))
 	System system2;
   mol >> system2;
 RESULT
 
 
-CHECK(GenericMolFile::GenericMolFile& operator << (const System& system))
+CHECK(GenericMolFile& operator << (const System& system))
   String filename;
   NEW_TMP_FILE(filename)
   GenericMolFile mol2(filename, std::ios::out);
   mol2 << system;
 RESULT
 
+Molecule m;
+
+CHECK(GenericMolFile& operator << (const Molecule& molecule))
+	mol << m;
+RESULT
+
+CHECK(GenericMolFile& operator >> (Molecule& molecule) throw(Exception::ParseError))
+	mol >> m;
+RESULT
+
+CHECK(GenericMolFile(const GenericMolFile& file))
+  String filename;
+  NEW_TMP_FILE(filename)
+  GenericMolFile mol(filename, std::ios::out);
+	GenericMolFile mol2(mol);
+	TEST_EQUAL(mol2.getName(), filename)
+RESULT
+
+CHECK(Molecule* read() throw(Exception::ParseError))
+	TEST_EQUAL(mol.read(), 0)
+RESULT
+
+CHECK(const GenericMolFile& operator = (const GenericMolFile& rhs) throw())
+  String filename;
+  NEW_TMP_FILE(filename)
+  GenericMolFile mol(filename, std::ios::out);
+	GenericMolFile mol2 = mol;
+	TEST_EQUAL(mol2.getName(), filename)
+RESULT
+
+CHECK(void initRead())
+	mol.initRead();
+RESULT
+
+CHECK(void initWrite())
+	mol.initWrite();
+RESULT
+
+CHECK(void write(const Molecule& molecule))
+	mol.write(m);
+RESULT
+
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
-
-}
-
-catch (BALL::Exception::FileNotFound e)
-{
-	TEST::this_test = false;
-	TEST::test = false;
-	TEST::all_tests = false;
- 	if ((TEST::verbose > 1) || (!TEST::this_test && (TEST::verbose > 0)))
-	{
-		if (TEST::exception == 1)
-			TEST::exception++;
-   	std::cout << std::endl << "    (caught exception of type ";
-		std::cout << e.getName();
-		if ((e.getLine() > 0) && (!(e.getFile() == "")))
-			std::cout << " outside a subtest, which was thrown in line " << e.getLine() << " of file " << e.getFile();
-		std::cout << " while looking for file " << e.getFilename();
-		std::cout << " - unexpected!) " << std::endl;
-	}
-}
-catch (BALL::Exception::GeneralException& e)
-{
-	TEST::this_test = false;
-	TEST::test = false;
-	TEST::all_tests = false;
- 	if ((TEST::verbose > 1) || (!TEST::this_test && (TEST::verbose > 0)))
-	{
-		if (TEST::exception == 1) TEST::exception++;
-   	std::cout << std::endl << "    (caught exception of type ";
-		std::cout << e.getName();
-		if ((e.getLine() > 0) && (!(e.getFile() == "")))
-		{
-			std::cout << " outside a subtest, which was thrown in line " << e.getLine() << " of file " << e.getFile();
-		}
-		std::cout << " - unexpected!) " << std::endl;
-		std::cout << "    (message is: " << e.getMessage() << ")" << std::endl;
-	}
- }
-	
-catch (...)
-{
-	TEST::this_test = false;
-	TEST::test = false;
-	TEST::all_tests = false;
- 	if ((TEST::verbose > 1) || (!TEST::this_test && (TEST::verbose > 0)))
-	{
-   	std::cout << std::endl << "    (caught unidentified and unexpected exception outside a subtest!) " << std::endl;
-	}
-}
-
-	
-while (TEST::tmp_file_list.size() > 0 && TEST::verbose < 1)
-{
-	::BALL::File::remove(TEST::tmp_file_list.back());
-	TEST::tmp_file_list.pop_back();
-}
-	
-if (!TEST::all_tests)
-{
-	std::cout << "FAILED" << std::endl;
-	return 1;
-} 
-else 
-{
-	std::cout << "PASSED" << std::endl;
-	return 0;
-}
-}
+END_TEST
