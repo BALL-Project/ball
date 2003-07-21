c05d415684c6f946eac00f824d9e741671c8ddc3
amoll <amoll>
2003-07-21 08:00:27 +0000

    no message

---
+++ source/APPLICATIONS/MOLVIEW/threads.C
@@ -0,0 +1,182 @@
+// Do nothing, if no thread support
+#ifdef QT_THREAD_SUPPORT
+
+#include "threads.h"
+#include "mainframe.h"
+#include <BALL/MOLMEC/MINIMIZATION/energyMinimizer.h>
+#include <BALL/MOLMEC/MDSIMULATION/molecularDynamics.h>
+#include <BALL/VIEW/GUI/WIDGETS/scene.h>
+#include <BALL/MOLMEC/AMBER/amber.h>
+
+namespace BALL
+{
+	using namespace VIEW;
+
+	SimulationThread::SimulationThread()
+		: QThread()
+	{
+	}
+
+	void SimulationThread::run()
+	{
+	}
+
+	void SimulationThread::updateScene_()
+	{
+		Scene* scene= (Scene*) Scene::getInstance(0);
+		Scene::SceneUpdateEvent* su = new Scene::SceneUpdateEvent;
+		postEvent(scene, su);  // Qt will delete it when done
+	}
+
+	void SimulationThread::output_(const String& string)
+	{
+		Mainframe::SimulationOutput* su = new Mainframe::SimulationOutput;
+		su->setMessage(string);
+		postEvent(main_frame_, su);  // Qt will delete it when done
+	}
+
+	void SimulationThread::outputAmberResult_(const AmberFF& amber)
+	{
+		output_(
+			"\nAMBER Energy:\n" 
+			" - electrostatic     : " + String(amber.getESEnergy()) + " kJ/mol\n" +
+			" - van der Waals     : " + String(amber.getVdWEnergy()) + " kJ/mol\n" +
+			" - bond stretch      : " + String(amber.getStretchEnergy()) + " kJ/mol\n" +
+			" - angle bend        : " + String(amber.getBendEnergy()) + " kJ/mol\n" +
+			" - torsion           : " + String(amber.getTorsionEnergy()) + " kJ/mol\n"+
+			"---------------------------------------\n" +
+			"  total energy       : " + String(amber.getEnergy()) + " kJ/mol\n"
+		);
+	}
+	
+	void SimulationThread::finish_()
+	{
+		Mainframe::SimulationThreadFinished* su = new Mainframe::SimulationThreadFinished;
+		postEvent(main_frame_, su);  // Qt will delete it when done
+	}
+
+	void EnergyMinimizerThread::run()
+	{
+		if (minimizer_ == 0 ||
+				minimizer_->getForceField() == 0 || 
+				minimizer_->getForceField()->getSystem() == 0 ||
+				main_frame_ == 0)
+		{
+			throw Exception::NullPointer(__FILE__, __LINE__);
+		}
+		AmberFF& amber =*(AmberFF*)minimizer_->getForceField();
+
+		// iterate until done and refresh the screen every "steps" iterations
+		while (!minimizer_->minimize(steps_between_updates_, true) &&
+					  minimizer_->getNumberOfIterations() < minimizer_->getMaxNumberOfIterations() &&
+					 !main_frame_->stopedSimulation())
+		{
+			updateScene_();
+			msleep(90);
+
+			QString message;
+			message.sprintf("Iteration %d: energy = %f kJ/mol, RMS gradient = %f kJ/mol A", 
+											minimizer_->getNumberOfIterations(), 
+											amber.getEnergy(), amber.getRMSGradient());
+			output_(message.ascii());
+		}
+
+		outputAmberResult_(amber);
+		output_("final RMS gadient    : " + String(amber.getRMSGradient()) + " kJ/(mol A)   after " 
+						+ String(minimizer_->getNumberOfIterations()) + " iterations\n");
+		finish_();
+	}
+
+	EnergyMinimizerThread::EnergyMinimizerThread()
+		: SimulationThread(),
+			minimizer_(0)
+	{
+	}
+
+	EnergyMinimizerThread::~EnergyMinimizerThread()
+	{
+		if (minimizer_ != 0)
+		{
+			delete minimizer_;
+		}
+	}
+
+	void EnergyMinimizerThread::setEnergyMinimizer(EnergyMinimizer* minimizer) 
+	{ 
+		if (minimizer_ != 0)
+		{
+			delete minimizer_;
+		}
+		minimizer_ = minimizer;
+	}
+
+	// =====================================================
+	void MDSimulationThread::run()
+		throw(Exception::NullPointer)
+	{
+		if (md_ == 0 ||
+				md_->getForceField() == 0 || 
+				md_->getForceField()->getSystem() == 0 || 
+				main_frame_ == 0)
+		{
+			throw Exception::NullPointer(__FILE__, __LINE__);
+		}
+		AmberFF& amber =*(AmberFF*)md_->getForceField();
+
+		// iterate until done and refresh the screen every "steps" iterations
+		
+		while (md_->getNumberOfIterations() < steps_ &&
+					 !main_frame_->stopedSimulation())
+		{
+			md_->simulateIterations(steps_between_updates_, true);
+			updateScene_();
+
+			QString message;
+			message.sprintf("Iteration %d: energy = %f kJ/mol, RMS gradient = %f kJ/mol A", 
+											md_->getNumberOfIterations(), amber.getEnergy(),
+											amber.getRMSGradient());
+			output_(message.ascii());
+			
+			msleep(90);
+			
+			if (save_images_) 
+			{
+				Scene* scene= (Scene*) Scene::getInstance(0);
+				scene->exportPNG();
+			}
+		}
+
+		outputAmberResult_(*(AmberFF*)md_->getForceField());
+		output_("final RMS gadient    : " + String(amber.getRMSGradient()) + " kJ/(mol A)   after " 
+						+ String(md_->getNumberOfIterations()) + " iterations\n");
+		finish_();
+	}
+
+	MDSimulationThread::MDSimulationThread()
+		: SimulationThread(),
+			md_(0),
+			save_images_(false)
+	{
+	}
+
+	MDSimulationThread::~MDSimulationThread()
+	{
+		if (md_ != 0)
+		{ 
+			delete md_;
+		}
+	}
+
+
+	void MDSimulationThread::setMolecularDynamics(MolecularDynamics* md)
+	{ 
+		if (md_ != 0)
+		{
+			delete md_;
+		}
+		
+		md_ = md;
+	}
+
+}
+#endif //Thread support
---
+++ source/APPLICATIONS/MOLVIEW/threads.h
@@ -0,0 +1,125 @@
+#ifndef BALL_APPLICATIONS_MOLVIEW_THREADS_H
+#define BALL_APPLICATIONS_MOLVIEW_THREADS_H
+
+#ifndef BALL_COMMON_H
+	#include <BALL/common.h>
+#endif
+
+#include <qthread.h>
+
+namespace BALL
+{
+	class EnergyMinimizer;
+	class MolecularDynamics;
+	class Mainframe;
+	class AmberFF;
+
+	/** Baseclass for threads in molview.
+	 		To use multithreading in molview, there are several problems arising from
+			the fact, that QT itself is not threadsafe:
+			1. Prevent any output to Log, stdout or sterr.
+				 If a simulation has to print some informations, use the method output_,
+				 which will send an Event to the GUI-thread to do this.
+			2. Dont call any QT-methods directly. If you need some interaction with the
+				 GUI, create a new Event-class and send this event to the Mainframe.
+			3. Especially dont call the refresh methods for the Scene or 
+				 GL-methods. Use updateScene_() instead.
+			4. At the end of the run() method, always call finish_() to notify the 
+				 main thread to delete the simulation thread, otherwise there will be 
+				 a memory leak.
+			If you dont pay attention to these points, dont wonder if molview freezes
+			or crashes!
+	*/
+	class SimulationThread
+		: public QThread
+	{
+		public:
+
+			///
+			SimulationThread();
+			
+			///
+			virtual void run();
+
+			///
+			void setNumberOfStepsBetweenUpdates(Size steps) 
+				{ steps_between_updates_ = steps;}
+
+			///
+			void setMainframe(Mainframe* mf) {main_frame_ = mf;}
+
+		protected:
+
+			/// Provokes an update of the Scene with rebuild of the display-lists
+			void updateScene_();
+
+			/// Sends the string as outout to Log.info
+			void output_(const String& string);
+
+			/// Prints the results from an AMBER-forcefield
+			void outputAmberResult_(const AmberFF& amber);
+
+			/// Notifies the main thread to delete the simulating thread
+			void finish_();
+
+			Size steps_between_updates_;
+			Mainframe* main_frame_;
+		};
+
+
+	/// Thread for EnergyMinimization
+	class EnergyMinimizerThread
+		: public SimulationThread
+	{
+		public:
+
+			///
+			EnergyMinimizerThread();
+			
+			///
+			~EnergyMinimizerThread();
+			
+			///
+			virtual void run();
+
+			///
+			void setEnergyMinimizer(EnergyMinimizer* minimizer);
+
+		protected:
+			EnergyMinimizer* minimizer_;
+	};
+
+
+	/// Thread for MDSimulation
+	class MDSimulationThread
+		: public SimulationThread
+	{
+		public:
+
+			///
+			MDSimulationThread();
+			
+			///
+			~MDSimulationThread();
+			
+			///
+			virtual void run()
+				throw(Exception::NullPointer);
+
+			///
+			void setMolecularDynamics(MolecularDynamics* md);
+
+			///
+			void setNumberOfSteps(Size steps) { steps_ = steps;}
+
+			///
+			void setSaveImages(bool state) { save_images_ = state;}
+
+		protected:
+			MolecularDynamics* md_;
+			Size steps_;
+			bool save_images_;
+	};
+
+}
+#endif
