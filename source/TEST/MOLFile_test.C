d2c0d95ce0657cf93a6941a71ad5bad52fdab4e5
amoll <amoll>
2003-07-25 12:47:09 +0000

    no message

--- source/TEST/MOLFile_test.C
+++ source/TEST/MOLFile_test.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: MOLFile_test.C,v 1.8 2002/12/12 11:34:41 oliver Exp $
+// $Id: MOLFile_test.C,v 1.9 2003/07/25 12:47:09 amoll Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 
@@ -18,7 +18,7 @@
 
 ///////////////////////////
 
-START_TEST(MOLFile, "$Id: MOLFile_test.C,v 1.8 2002/12/12 11:34:41 oliver Exp $")
+START_TEST(MOLFile, "$Id: MOLFile_test.C,v 1.9 2003/07/25 12:47:09 amoll Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -27,18 +27,18 @@ using namespace BALL;
 
 
 MOLFile* mf = 0;
-CHECK(MOLFile::MOLFile())
+CHECK(MOLFile() throw())
 	mf = new MOLFile;
 	TEST_NOT_EQUAL(mf, 0)
 RESULT
 
 
-CHECK(MOLFile::~MOLFile())
+CHECK(~MOLFile() throw())
 	delete mf;
 RESULT
 
 
-CHECK(MOLFile::read(System& system))
+CHECK(bool read(System& system) throw(Exception::ParseError))
 	MOLFile f("data/MOLFile_test1.mol");
 	System system;
 	f.read(system);
@@ -49,20 +49,26 @@ CHECK(MOLFile::read(System& system))
 	TEST_REAL_EQUAL(atom.getPosition().x,  1.5737)
 	TEST_REAL_EQUAL(atom.getPosition().y, -3.1475)
 	TEST_REAL_EQUAL(atom.getPosition().z,  0.0000)
+	system.clear();
+	MOLFile f2("MOLFile_test.C");
+	TEST_EQUAL(f2.read(system), false)
+	MOLFile f3("data/Selectable_test.txt");
+	TEST_EQUAL(f3.read(system), false)
 RESULT
 
 
-CHECK(MOLFile::MOLFile(const String& filename, File::OpenMode open_mode))
+CHECK(MOLFile(const String& filename, File::OpenMode open_mode = std::ios::in) throw(Exception::FileNotFound))
 	MOLFile f("data/MOLFile_test1.mol", std::ios::in);
 	System system;
 	f.read(system);
 	TEST_EQUAL(system.countAtoms(), 23)
 	TEST_EQUAL(system.countMolecules(), 1)
 	TEST_EQUAL(system.countBonds(), 26)
+	TEST_EXCEPTION(Exception::FileNotFound, MOLFile f2("MOLFile_test1.mol"))
 RESULT
 
 
-CHECK(MOLFile::write(const System& system))
+CHECK(bool write(const System& system) throw(File::CanNotWrite))
   Molecule* m = new Molecule;
 	m->setName("MOL");
 	System S;
@@ -94,10 +100,13 @@ CHECK(MOLFile::write(const System& system))
 	f.close();
 	
 	TEST_FILE_REGEXP(filename.c_str(), "data/MOLFile_test2.mol")
+
+	MOLFile f2("MOLFile_test.C", File::IN);
+	TEST_EXCEPTION(File::CanNotWrite, f2.write(S))
 RESULT
 
 
-CHECK(MOLFile::MOLFile& operator >> (System& system))
+CHECK([EXTRA]MOLFile::MOLFile& operator >> (System& system))
   MOLFile f("data/MOLFile_test1.mol");
 	System S;
 	f >> S;
@@ -129,38 +138,66 @@ CHECK(MOLFile::MOLFile& operator >> (System& system))
 	TEST_EQUAL(S.countMolecules(), 1)
 RESULT
 
-CHECK(MOLFile::MOLFile& operator << (const System& system))
-  Molecule* m = new Molecule;
-	m->setName("MOL");
-	System S;
-	S.setName("SYSTEM");
-	S.insert(*m);
-	Atom* a1 = new Atom();
-	Atom* a2 = new Atom();
-	m->insert(*a1);
-	m->insert(*a2);
+CHECK(Molecule* read() throw(Exception::ParseError))
+  MOLFile f("data/MOLFile_test1.mol");
+	Molecule* m = f.read();
+	f.close();
+	TEST_EQUAL(m->countAtoms(), 23)
+	TEST_EQUAL(m->countBonds(), 26)
+	delete m;
+RESULT
 
-	a1->setName("A1");
-	a1->setElement(PTE[Element::N]);
-	a1->setCharge(0.5);
-	a1->setPosition(Vector3(0.1, 0.2, 0.3));
+Molecule* m = new Molecule;
+m->setName("MOL");
+System S;
+S.setName("SYSTEM");
+S.insert(*m);
+Atom* a1 = new Atom();
+Atom* a2 = new Atom();
+m->insert(*a1);
+m->insert(*a2);
 
-	a2->setName("A2");
-	a2->setElement(PTE[Element::O]);
-	a2->setCharge(-0.5);
-	a2->setPosition(Vector3(0.5, 0.6, 0.7));
-	
-	a1->createBond(*a2);
-	a1->getBond(*a2)->setOrder(Bond::ORDER__DOUBLE);
-	
+a1->setName("A1");
+a1->setElement(PTE[Element::N]);
+a1->setCharge(0.5);
+a1->setPosition(Vector3(0.1, 0.2, 0.3));
 
-	String filename;
+a2->setName("A2");
+a2->setElement(PTE[Element::O]);
+a2->setCharge(-0.5);
+a2->setPosition(Vector3(0.5, 0.6, 0.7));
+
+a1->createBond(*a2);
+a1->getBond(*a2)->setOrder(Bond::ORDER__DOUBLE);
+
+String filename;
+
+CHECK([EXTRA]MOLFile::MOLFile& operator << (const System& system))
 	NEW_TMP_FILE(filename)
 	MOLFile f(filename, std::ios::out);
 	f << S;	
 	f.close();
-	
 	TEST_FILE_REGEXP(filename.c_str(), "data/MOLFile_test2.mol")
+RESULT
+
+CHECK(bool write(const Molecule& molecule) throw(File::CanNotWrite))
+	NEW_TMP_FILE(filename)
+	MOLFile f(filename, std::ios::out);
+	f.write(*m);
+	f.close();
+	TEST_FILE_REGEXP(filename.c_str(), "data/MOLFile_test2.mol")
+RESULT
+
+CHECK(BALL_CREATE(MOLFile))
+  MOLFile f("data/MOLFile_test1.mol");
+	MOLFile* f_ptr = (MOLFile*) f.create();
+	TEST_EQUAL(f_ptr->getName(), "data/MOLFile_test1.mol")
+RESULT
+
+CHECK(MOLFile(const MOLFile& file) throw(Exception::FileNotFound))
+  MOLFile f("data/MOLFile_test1.mol");
+	MOLFile f2(f);
+	TEST_EQUAL(f2.getName(), "data/MOLFile_test1.mol")
 RESULT
 
 /////////////////////////////////////////////////////////////
