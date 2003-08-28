e95755daa5ac940b5ae696b584cf628c5d2d7914
amoll <amoll>
2003-08-28 15:05:14 +0000

    cleanup, still not working...

--- source/APPLICATIONS/NMRVIEW/nmrview.C
+++ source/APPLICATIONS/NMRVIEW/nmrview.C
@@ -1,12 +1,14 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: nmrview.C,v 1.5 2003/08/28 14:29:29 amoll Exp $
+// $Id: nmrview.C,v 1.6 2003/08/28 15:05:14 amoll Exp $
 //
 
+#include <qapplication.h>
 #include <BALL/VIEW/KERNEL/mainControl.h>
-#include <BALL/VIEW/DIALOGS/openBruker2DFile.h>
-#include <BALL/VIEW/WIDGETS/regularData2DWidget.h>
+#include <BALL/VIEW/DIALOGS/openBruker1DFile.h>
+//#include <BALL/VIEW/DIALOGS/openBruker2DFile.h>
+//#include <BALL/VIEW/WIDGETS/regularData2DWidget.h>
 
 using namespace BALL::VIEW;
 
@@ -18,8 +20,9 @@ int main(int argc, char **argv)
   application.setMainWidget(&mainframe);
 
 	// create widgets
-	mainframe.setCentralWidget(new RegularData2DWidget(4096, 4096, 0.0, 1.0, &mainframe));
-	new OpenBruker2DFile(&mainframe);
+//	mainframe.setCentralWidget(new RegularData2DWidget(4096, 4096, 0.0, 1.0, &mainframe));
+	new OpenBruker1DFile(&mainframe);
+	//new OpenBruker2DFile(&mainframe);
         
   // start the application
   mainframe.show();
