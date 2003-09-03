a833201fd6308b860d713c4b2d434830cec192cd
anker <anker>
2003-09-03 16:00:03 +0000

    fresnoSupport.C

---
+++ source/MOLMEC/FRESNO/fresnoSupport.C
@@ -0,0 +1,13 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id: fresnoSupport.C,v 1.1 2003/09/03 16:00:03 anker Exp $
+
+#include <BALL/MOLMEC/FRESNO/support.h>
+
+using namespace std;
+
+namespace BALL
+{
+}
+
