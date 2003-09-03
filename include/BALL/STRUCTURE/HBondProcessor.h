ff235d22cf9749fef90738b5e1e8b5afad09858c
anne <anne>
2003-09-03 13:19:03 +0000

    Creation

---
+++ include/BALL/STRUCTURE/HBondProcessor.h
@@ -0,0 +1,166 @@
+
+#ifndef BALL_STRUCTURE_HBONDPROCESSOR_H
+#define BALL_STRUCTURE_HBONDPROCESSOR_H
+
+
+#ifndef  BALL_COMMON_H
+#include <BALL/common.h>
+#endif
+
+#ifndef BALL_KERNEL_PROTEIN_H
+#include <BALL/KERNEL/protein.h>
+#endif
+
+#ifndef  BALL_KERNEL_CHAIN_H
+#include <BALL/KERNEL/chain.h>
+#endif
+
+#ifndef  BALL_KERNEL_BOND_H
+#include <BALL/KERNEL/bond.h>
+#endif
+
+#ifndef BALL_FORMAT_PDBFFILE_H
+#include <BALL/FORMAT/PDBFile.h>
+#endif
+
+#ifndef BALL_FORMAT_HINFILE_H
+#include <BALL/FORMAT/HINFile.h>
+#endif
+
+#ifndef MATH_H
+#include <math.h>
+#endif
+
+#ifndef BALL_STRUCTURE_RESIDUECHECKER_H
+#include <BALL/STRUCTURE/residueChecker.h>
+#endif
+
+#ifndef BALL_STRUCTURE_FRAGMENTDB_H
+#include <BALL/STRUCTURE/fragmentDB.h>
+#endif
+
+#ifndef BALL_MATHS_MATRIX44_H
+#include <BALL/MATHS/matrix44.h> //4x4 Matrix
+#endif
+
+#ifndef BALL_MATHS_VECTOR3_H
+#include <BALL/MATHS/vector3.h>  //TVector3
+#endif
+
+#ifndef BALL_MATHS_ANGLE_H
+#include <BALL/MATHS/angle.h>     //TAngle
+#endif
+
+#ifndef VECTOR
+#include <vector>
+#endif
+
+#ifndef BALL_DATATYPE_HASHGRID_H
+#include <BALL/DATATYPE/hashGrid.h>
+#endif
+
+#ifndef BALL_MATHS_VECTOR4_H
+#include <BALL/MATHS/vector4.h>
+#endif
+
+#ifndef  BALL_STRUCTURE_GEOMETRICPROPERTIES_H
+#include <BALL/STRUCTURE/geometricProperties.h>
+#endif
+
+#ifndef   BALL_KERNEL_SECONDARYSTRUCTURE_H
+#include <BALL/KERNEL/secondaryStructure.h>           
+#endif
+
+#ifndef  BALL_CONCEPT_PROPERTY_H
+#include <BALL/CONCEPT/property.h>
+#endif
+
+namespace BALL
+{
+  
+
+  
+  /** This class represents prosessor
+   */
+
+  class HBondProcessor :public UnaryProcessor<Composite>
+  {
+ 
+  public:
+
+  //constants 
+  const float BOND_LENGTH_N_H;
+  const float BOND_LENGTH_C_O;
+  // The 5.2 Angstroms is the maximum distance between N and O in a hydrogen bond.
+  // The 4.2 Angstroms is an upper bound for the distance between N and O in the same
+  // residue. Therefore, if two residues are farther apart than MAX_LENGTH, N and O
+  // are farther apart than 5.2 Angstroms and there will be no hydrogen bond.
+  const float MAX_LENGTH;
+
+
+
+
+  struct POS{
+    Vector3 posC;
+    Vector3 posN;
+    Vector3 posH;
+    Vector3 posO;
+    Size number;
+    Residue *pres; 
+  };
+  
+
+     /** Default constructor
+     */
+     HBondProcessor();
+    
+    /** Detailed constructor maybe it is helpful to use other criteria for detecting hbonds
+     */
+    //HBondProcessor();
+
+    /** Destructor
+     */
+    ~ HBondProcessor();
+
+
+    /** start
+     */
+
+    /** finish computes all hbonds of the composit
+     */
+    bool finish();
+
+     /** preComputeBonds postions the residues in vec
+     */
+    void preComputeBonds(ResidueIterator& data);
+
+    /** operator()
+     */
+    Processor::Result operator() (Composite &composite);
+
+
+  protected:  
+    Vector3 upper_;
+    Vector3 lower_;
+    
+  private:
+    /*    // ??? Why do we need this???
+    std::ostream& operator << (std::ostream& s, const POS& p)
+      {
+	return s << p.posC << " " << p.posN << " " << p.posH << " " << p.posO << " " << p.number << std::endl;
+      }  */
+
+    /** vec_ includes the atom positions per residue 
+     */
+    vector<POS> vec_; //!!!!!!!!!!!!!posvec_ would be more understandable 
+
+    /** HBondBool_ : matrix to save the existence of a HBond
+     */
+    vector<bool> HBondBool_;
+
+  }; //class HBondProcessor
+
+}//namesspace BALL
+
+
+#endif
---
+++ source/STRUCTURE/HBondProcessor.C
@@ -0,0 +1,248 @@
+#include <BALL/STRUCTURE/HBondProcessor.h>
+
+namespace BALL
+{
+
+	HBondProcessor::HBondProcessor()
+		: BOND_LENGTH_N_H(1.020),
+			BOND_LENGTH_C_O(1.240),
+			MAX_LENGTH(5.2+4.2),
+			vec_()
+	{
+	}
+
+	HBondProcessor::~HBondProcessor()
+	{
+	}
+
+	void HBondProcessor::preComputeBonds(ResidueIterator& data)
+	{
+		//vector<POS> vec;      // includes the atom positions per residue     // class member!!!
+		//	ResidueIterator i;
+		POS pos;
+		int j=0;                // index to serially the residues TOTHINK:maybe it is possible 
+		                        //   to identify the residue by residue ID 
+
+		// iteration over all residues of the protein
+		// to find the C,N,O atoms
+		for(; +data; ++data)
+		{
+			Residue res=*data;
+
+			PDBAtomIterator ai;
+			for(ai=res.beginPDBAtom();+ai;++ai)
+			{
+				if(ai->getName() == "N")
+				{
+					pos.posN=ai->getPosition();
+				}
+				else if(ai->getName() == "C")
+				{
+					pos.posC=ai->getPosition();
+				}
+				else if(ai->getName() == "O")
+				{
+					pos.posO=ai->getPosition();
+				}
+			}
+
+			// evaluate the position of H 
+			pos.posH=pos.posN-((pos.posO-pos.posC)*BOND_LENGTH_N_H/BOND_LENGTH_C_O);
+
+			//set identification number of the residue
+			// pos.number=res.getID().toInt();
+			pos.number=j;
+			j++;
+			
+			// point pres at data
+			pos.pres = &(*data);
+			
+			vec_.push_back(pos);  // insert the residue in position vector
+		}
+	}
+
+	bool HBondProcessor::finish()
+	{
+		// matrix to save the existence of a HBond
+		HBondBool_.resize(vec_.size() * vec_.size(), false); //!!!!!!!!!!!TODO: substitute by secondaryStructure
+
+		//create an Hashgrid
+		//BoundingBoxProcessor box_processor; 
+		// create a mantel box (bounding box)  which should include the protein
+		//system.apply(box_processor);
+
+		// identify the edges of the mantel box
+		//Vector3 lower = box_processor.getLower();
+		//Vector3 upper = box_processor.getUpper();
+
+		//create a grid inside the bounding box
+		HashGrid3<POS> atom_grid(lower_, upper_-lower_, MAX_LENGTH);
+
+		// insert all protein-residues in respect of N-atom
+		// the last residue does not _have_ an O. => don't use it
+		for(Size i=0; i<vec_.size()-1; i++) 
+		{
+			Vector3 r(vec_[i].posN);
+			//!!!!!!!!!!!!!!!!!!!!!!!!!!
+			//    Position x, y, z;
+			//    atom_grid.getIndices(*(atom_grid.getBox(r)), x, y, z);
+			//    std::cout << i << " " <<  x << " " << y << " " << z << " " << r << std::endl;
+			//!!!!!!!!!!!!!!!!!!!!!!!!!!
+			atom_grid.insert(r, vec_[i]);
+		}
+
+		HashGridBox3<POS>::DataIterator data_it;       // iterate over residues of neighbouring boxes
+		HashGridBox3<POS>* box;                        // iterate over all boxes of the grid
+		HashGridBox3<POS>::BoxIterator box_it;         // iterator
+		float energy, dist_ON, dist_CH, dist_OH, dist_CN;
+
+		// now compute the energies and see whether we have a hydrogen bond
+		for (Size i=0; i<vec_.size(); i++)
+		{
+			box = atom_grid.getBox(vec_[i].posN);
+
+			// We iterate over the grid ourselves, since the beginBox()... didn't work... :-(
+			Position x, y, z;                            // indices of the actual box
+			int size_x = atom_grid.getSizeX();           // size of x dimension
+			int size_y = atom_grid.getSizeY();           // size of y dimension
+			int size_z = atom_grid.getSizeZ();           // size of z dimension
+
+			atom_grid.getIndices(*box, x, y, z);         // compute the indices of the actual box
+
+			// Iterate over all the neighbouring boxes
+			// for (box_it=box->beginBox(); +box_it; ++box_it) //as mentioned beginBox doesn't work
+			for (int nx=x-1; (nx < size_x) && (nx < (int)x+2); nx++)
+			{
+				for (int ny=y-1; (ny < size_y) && (ny < (int)y+2); ny++)
+				{
+					for (int nz=z-1; (nz < size_z) && (nz < (int)z+2); nz++)
+					{
+						if ( (nx>=0) && (ny>=0) && (nz>=0) )  // we shouldn'd run outside the box
+						{
+							// compute the neighbour box
+							HashGridBox3<POS> *nb = atom_grid.getBox(nx, ny, nz); 
+
+							//iterate over all residues of the neighbouring box
+							for (data_it = nb->beginData(); +data_it; ++data_it)
+							{
+								// TODO: We don't want H-bonds between neighboring residues!
+								//       Does this criterion always work? We should check for
+								//       an existing bond between data_it and vec[i] instead!
+
+								if ((int)abs((int)(data_it->number - vec_[i].number)) > 1)
+								{
+									// compute the distances between the relevant atoms
+									dist_ON = (vec_[i].posO - data_it->posN).getLength();
+									dist_CH = (vec_[i].posC - data_it->posH).getLength();
+									dist_OH = (vec_[i].posO - data_it->posH).getLength();
+									dist_CN = (vec_[i].posC - data_it->posN).getLength();
+
+									// compute the electrostatic energy of the bond-building groups
+									energy  = 0.42*0.20 * 332.;
+									energy *= (1./dist_ON + 1./dist_CH - 1./dist_OH - 1./dist_CN);
+
+									//			HBondBool_[vec_[i].number + vec_.size()*(data_it->number)] = 
+									//	(energy < -0.5) ? true : false;
+									
+									if (energy < -0.5)
+									{
+										HBondBool_[vec_[i].number + vec_.size()*(data_it->number)] = true;
+										PDBAtomIterator ai;
+										Atom *pacceptor;
+										Atom donor; 
+										for(ai=(vec_[i].pres)->beginPDBAtom(); +ai; ++ai)
+										{
+											if(ai->getName() == "O")
+											{
+												pacceptor=&(*ai);
+											}
+										}	
+										
+										for(ai=(data_it->pres)->beginPDBAtom(); +ai; ++ai)
+										{
+											if(ai->getName() == "N")
+											{
+												donor=*ai;
+											}
+										}		
+										donor.setProperty(NamedProperty("HBOND_DONOR",pacceptor));
+									}
+									else
+									{
+										HBondBool_[vec_[i].number + vec_.size()*(data_it->number)] = false;
+									}
+									
+								}
+							} 
+						}
+					}
+				}
+			}
+		}
+
+		for (Size i=0; i<vec_.size(); i++)
+		{
+			for (Size j=0; j<vec_.size(); j++)
+			{
+				if (HBondBool_[i+j*vec_.size()])
+				{
+					//		std::cout << "HBond between " << i << " O and " << j << " N" << std::endl;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	Processor::Result HBondProcessor::operator() (Composite &composite)
+	{
+		ResidueIterator ri;
+	 // create a mantel box (bounding box)  which should include the composit
+		BoundingBoxProcessor bp;
+
+		// do we have a system?
+		if (RTTI::isKindOf<System>(composite))
+		{
+			System *s = RTTI::castTo<System>(composite);
+			s->apply(bp);
+			upper_ = bp.getUpper();
+			lower_ = bp.getLower();
+
+			ri = s->beginResidue();
+		}
+		if (RTTI::isKindOf<Protein>(composite))
+		{
+			Protein *s = RTTI::castTo<Protein>(composite);
+			s->apply(bp);
+			upper_ = bp.getUpper();
+			lower_ = bp.getLower();
+
+			ri = s->beginResidue();
+		}
+		if (RTTI::isKindOf<Chain>(composite))
+		{
+			Chain *s = RTTI::castTo<Chain>(composite);
+			s->apply(bp);
+			upper_ = bp.getUpper();
+			lower_ = bp.getLower();
+
+			ri = s->beginResidue();
+		}
+
+		if (!(+ri))
+		{
+			return Processor::CONTINUE;
+		}
+
+		// compute the H-Bonds
+		preComputeBonds(ri);
+		return Processor::BREAK;
+
+	}
+
+	std::ostream& operator << (std::ostream& s, const BALL::HBondProcessor::POS& p)
+	{
+		return s << p.posC << " " << p.posN << " " << p.posH << " " << p.posO << " " << p.number << std::endl;
+	}
+
+}
