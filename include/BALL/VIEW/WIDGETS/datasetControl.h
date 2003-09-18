748bef3692c16bf2fb68cec168916f8437a48f52
amoll <amoll>
2003-09-18 18:56:27 +0000

    no message

---
+++ include/BALL/VIEW/WIDGETS/datasetControl.h
@@ -0,0 +1,169 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id: datasetControl.h,v 1.1 2003/09/18 18:55:12 amoll Exp $
+
+#ifndef BALL_VIEW_WIDGETS_DATASETCONTROL_H
+#define BALL_VIEW_WIDGETS_DATASETCONTROL_H
+
+#ifndef BALL_VIEW_KERNEL_COMMON_H
+# include <BALL/VIEW/KERNEL/common.h>
+#endif
+
+#ifndef BALL_MOLMEC_COMMON_SNAPSHOTMANAGER_H
+# include <BALL/MOLMEC/COMMON/snapShotManager.h>
+#endif
+
+#ifndef BALL_VIEW_WIDGETS_GENERICCONTROL_H
+# include <BALL/VIEW/WIDGETS/genericControl.h>
+#endif
+
+#ifndef BALL_DATATYPE_REGULARDATA3D_H
+# include <BALL/DATATYPE/regularData3D.h>
+#endif 
+
+namespace BALL
+{
+	class TrajectoryFile;
+	class Composite;
+
+	namespace VIEW
+	{
+		class SnapshotVisualisationDialog;
+
+		/**	DatasetControl is a widget to manipulate Trajectories and RegularData3D instances.
+		 		It is derived from GenricControl.
+				There are two columns. The <b>Name</b> column and the
+				<b>Type</b> column. In the Name column the item tree will be shown and in 
+				the Type column the type of each item.
+				The method buildContextMenu() can create a customizable context menu.
+				\ingroup ViewWidgets
+		*/
+		class DatasetControl
+			: public GenericControl
+		{
+			Q_OBJECT
+
+			public:
+
+			/** @name Macros.
+		  */
+			//@{
+
+			/** Embeddable Macro.
+			*/
+			BALL_EMBEDDABLE(DatasetControl)
+
+			//@}
+			/**	@name	Constructors and Destructors
+			*/	
+			//@{
+
+			/** Default Constructor.
+					There is a connection from the signal rightButtonClicked from the
+					QT-library to the method onContextMenu(). 
+					(See documentation of QT-library for information concerning widgets and 
+					signal/slot mechanism.) \par
+					Calls registerWidget().
+					\param      parent the parent widget of the DatasetControl 
+					\param      name the name of the DatasetControl 
+					\see        ModularWidget
+			*/
+			DatasetControl(QWidget* parent = 0, const char* name = 0)
+				throw();
+			
+			/** Destructor.
+			*/
+			virtual ~DatasetControl()
+				throw();
+
+			//@} 
+			/**	@name	Accessors: inspectors and mutators 
+			*/ 
+			//@{
+
+			virtual void onNotify(Message *message)
+				throw();
+
+			/**	Initialize the widget.
+			*/
+			virtual void initializeWidget(MainControl& main_control)
+				throw();
+
+			/**	Remove the widget.
+					Reverse all actions performed in initializeWidget
+					(remove menu entries of this DatasetControl).
+					This method will be called by MainControl::aboutToExit.
+			*/
+			virtual void finalizeWidget(MainControl& main_control)
+				throw();
+
+			///
+			virtual void checkMenu(MainControl& main_control)
+				throw();
+			
+			//@}
+			/** @name Public slots 
+			*/ 
+			//@{
+			public slots:
+
+			///
+			void addTrajectory()
+				throw();
+
+			///
+			void add3DGrid()
+				throw();
+
+			///
+			void updateSelection()
+				throw();
+
+			//@} 
+			/** @name Protected members 
+			*/ 
+			//@{
+		  protected slots:
+
+			void deleteTrajectory_();
+			void deleteGrid_();
+			void visualiseTrajectory_();
+			void saveTrajectory_();
+			void save3DGrid_()
+				throw();
+	
+			void onContextMenu_(QListViewItem* item, const QPoint& point, int column);
+
+			//@}
+
+		  protected:
+
+			void insertTrajectory_(TrajectoryFile* file, System& system)
+				throw();
+			
+			void insertGrid_(RegularData3D* file, System& system, const String& name)
+				throw();
+			
+			void insertComposite_(Composite* composite, QListViewItem* item)
+				throw();
+
+			void deleteItem_(QListViewItem* item)
+				throw();
+
+			SnapShotManager* 							context_trajectory_;
+			QListViewItem* 								context_item_;
+
+			SnapshotVisualisationDialog* 	dialog_;
+			Index 												open_trajectory_id_;
+			Index 												open_grid_id_;
+
+			HashMap<QListViewItem*	, SnapShotManager*> 					item_to_trajectory_;
+			HashMap<QListViewItem*	, RegularData3D*>   					item_to_grid_;
+			HashMap<Composite*      , HashSet<QListViewItem*> > 	composite_to_items_;
+			HashMap<QListViewItem*  , Composite*>  								item_to_composite_;
+		};
+		
+} } // namespaces
+
+#endif // BALL_VIEW_WIDGETS_DATASETCONTROL_H
---
+++ source/VIEW/WIDGETS/datasetControl.C
@@ -0,0 +1,341 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id: datasetControl.C,v 1.1 2003/09/18 18:56:27 amoll Exp $
+
+#include <BALL/VIEW/WIDGETS/datasetControl.h>
+#include <BALL/VIEW/KERNEL/mainControl.h>
+#include <BALL/VIEW/KERNEL/message.h>
+#include <BALL/KERNEL/system.h>
+#include <BALL/FORMAT/DCDFile.h>
+#include <BALL/VIEW/DIALOGS/snapShotVisualisation.h>
+#include <qpopupmenu.h>
+#include <qmenubar.h>
+#include <qfiledialog.h>
+#include <qlistview.h>
+#include <qtooltip.h>
+
+using std::endl;
+
+namespace BALL
+{
+	namespace VIEW
+	{
+
+DatasetControl::DatasetControl(QWidget* parent, const char* name)
+	throw()
+	:	GenericControl(parent, name),
+		dialog_(0)
+{
+	listview->addColumn("Name");
+	listview->addColumn("from");
+	listview->setColumnWidth(0, 120);
+	listview->setColumnWidth(1, 60);
+	default_visible_ = false;
+	connect(listview, SIGNAL(selectionChanged()), this, SLOT(updateSelection()));
+}
+
+
+DatasetControl::~DatasetControl()
+	throw()
+{
+  #ifdef BALL_VIEW_DEBUG
+	  Log.error() << "Destructing object " << (void *)this << " of class " 
+								<< RTTI::getName<DatasetControl>() << endl;
+  #endif 
+
+	if (dialog_) delete dialog_;
+}
+
+
+void DatasetControl::initializeWidget(MainControl& main_control)
+	throw()
+{
+	open_trajectory_id_ = 
+		main_control.insertMenuEntry(MainControl::FILE, "Open Trajectory", this, SLOT(addTrajectory()), 0, -1,
+		String("To open a trajectory file, one System has to be selected."));
+	open_grid_id_ = 
+		main_control.insertMenuEntry(MainControl::FILE, "Open 3D Grid", this, SLOT(add3DGrid()), 0, -1,
+		String("To open a 3D grid, one System has to be selected."));
+	GenericControl::initializeWidget(main_control);
+}
+
+
+void DatasetControl::finalizeWidget(MainControl& main_control)
+	throw()
+{
+	main_control.removeMenuEntry(MainControl::FILE, "Open Trajectory", this, SLOT(addTrajectory()));
+	main_control.removeMenuEntry(MainControl::FILE, "Open 3D Grid", this, SLOT(add3DGrid()));
+	GenericControl::finalizeWidget(main_control);
+}
+
+
+void DatasetControl::checkMenu(MainControl& main_control)
+	throw()
+{
+	getMainControl()->menuBar()->setItemEnabled(open_trajectory_id_, main_control.getSelectedSystem());
+	getMainControl()->menuBar()->setItemEnabled(      open_grid_id_, main_control.getSelectedSystem());
+}
+
+
+void DatasetControl::addTrajectory()
+	throw()
+{
+	if (!getMainControl()->getSelectedSystem()) return;
+	QFileDialog *fd = new QFileDialog(this, "", true);
+	fd->setMode(QFileDialog::ExistingFile);
+	fd->setFilter("DCD files(*.dcd)");
+	fd->setCaption("Select a DCD file");
+	fd->setViewMode(QFileDialog::Detail);
+
+	if (!fd->exec()== QDialog::Accepted) return;
+
+	String filename(fd->selectedFile().ascii());
+	delete fd;
+
+	// construct a name for the system(the filename without the dir path)
+	DCDFile* dcd = new DCDFile(filename, File::IN);
+	insertTrajectory_(dcd, *getMainControl()->getSelectedSystem());
+}
+
+void DatasetControl::insertTrajectory_(TrajectoryFile* file, System& system)
+	throw()
+{
+	if (file->getNumberOfAtoms() != system.countAtoms())
+	{
+		setStatusbarText("Number of atoms do not match. Aborting...");
+		delete file;
+		return;
+	}
+
+	SnapShotManager* manager = new SnapShotManager(&system, 0, file);
+
+	String name = file->getName();
+
+	Position pos = 0;
+	for (Position p = 0; p < name.size(); p++)
+	{
+		if (name[p] == FileSystem::PATH_SEPARATOR) pos = p;
+	}
+	
+	if (pos) pos++;
+	name = name.getSubstring(pos);
+	
+	QListViewItem* item = new QListViewItem(listview, name.c_str(), system.getName().c_str());
+	item_to_trajectory_[item] = manager;
+	insertComposite_(&system, item);
+}
+
+
+void DatasetControl::insertComposite_(Composite* composite, QListViewItem* item)
+	throw()
+{
+	listview->triggerUpdate();
+	item_to_composite_[item] = composite;
+	if (composite_to_items_.has(composite))
+	{
+		composite_to_items_[composite].insert(item);
+	}
+	else
+	{
+		HashSet<QListViewItem*> set;
+		set.insert(item);
+		composite_to_items_[composite] = set;
+	}
+}
+
+void DatasetControl::onNotify(Message *message)
+	throw()
+{
+	if (RTTI::isKindOf<CompositeMessage>(*message))
+  {
+    CompositeMessage *composite_message = RTTI::castTo<CompositeMessage>(*message);
+		if (!composite_message->getType() == CompositeMessage::REMOVED_COMPOSITE) return;
+    Composite* composite = (Composite *)composite_message->getComposite();
+		if (!composite_to_items_.has(composite)) return;
+
+		HashSet<QListViewItem*>& to_delete = composite_to_items_[composite];
+
+		HashSet<QListViewItem*>::Iterator lit = to_delete.begin();
+		for (;lit != to_delete.end(); lit++)
+		{
+			context_item_ = *lit;
+			deleteTrajectory_();
+			deleteGrid_();
+		}
+	}   
+	else if (RTTI::isKindOf<NewTrajectoryMessage>(*message))
+	{
+		NewTrajectoryMessage* ntm = RTTI::castTo<NewTrajectoryMessage>(*message);
+		insertTrajectory_(ntm->getTrajectoryFile(), *(System*)ntm->getComposite());
+		return;
+	}
+	else if (RTTI::isKindOf<RegularData3DMessage>(*message))
+	{
+		RegularData3DMessage* ntm = RTTI::castTo<RegularData3DMessage>(*message);
+		insertGrid_(ntm->getRegularData3D(), *(System*)ntm->getComposite(), ntm->getCompositeName());
+		return;
+	}
+}
+
+void DatasetControl::deleteTrajectory_()
+{
+	SnapShotManager* ssm = item_to_trajectory_[context_item_];
+	if (!ssm) return;
+	delete ssm;
+	deleteItem_(context_item_);
+	setStatusbarText("deleted trajectory");
+}
+
+void DatasetControl::deleteItem_(QListViewItem* item)
+	throw()
+{
+	Composite* composite = item_to_composite_[item];
+	composite_to_items_[composite].erase(item);
+	item_to_trajectory_.erase(item);
+	item_to_grid_.erase(item);
+	item_to_composite_.erase(item);
+	delete item;
+	listview->triggerUpdate();
+}
+
+void DatasetControl::deleteGrid_()
+{
+	RegularData3D* ssm = item_to_grid_[context_item_];
+	if (!ssm) return;
+	delete ssm;
+	deleteItem_(context_item_);
+	setStatusbarText("deleted 3D grid");
+}
+
+void DatasetControl::onContextMenu_(QListViewItem* item,  const QPoint& point, int /* column */)
+{
+	if (item == 0) return;
+	context_item_ = item;
+
+	QPopupMenu context_menu;
+	if (item_to_trajectory_.has(item))
+	{
+		context_menu.insertItem("Save", this, SLOT(saveTrajectory_()));
+		context_menu.insertItem("Visualise/Export", this, SLOT(visualiseTrajectory_()));
+		context_menu.insertItem("Delete", this, SLOT(deleteTrajectory_()));
+		//	insertContextMenuEntry("Export to PNGs", this, SLOT(visualiseTrajectory_()));
+	}
+	else if (item_to_grid_.has(item))
+	{
+		context_menu.insertItem("Save", this, SLOT(save3DGrid_()));
+		context_menu.insertItem("Delete", this, SLOT(deleteGrid_()));
+	}
+
+	// show the context menu if it is not empty
+	if (context_menu.count()) context_menu.exec(point);
+}
+
+void DatasetControl::visualiseTrajectory_()
+{
+	if (dialog_) 
+	{
+		dialog_->hide();
+		delete dialog_;
+	}
+
+	SnapShotManager* ssm = item_to_trajectory_[context_item_];
+
+	dialog_ = new SnapshotVisualisationDialog(this);
+	dialog_->setSnapShotManager(ssm);
+	dialog_->show();
+}
+
+void DatasetControl::saveTrajectory_()
+{
+	SnapShotManager* ssm = item_to_trajectory_[context_item_];
+
+	QFileDialog *fd = new QFileDialog(this, "", true);
+	fd->setMode(QFileDialog::AnyFile);
+	fd->setFilter("DCD files(*.dcd)");
+	fd->setCaption("Write to a DCD file");
+	fd->setViewMode(QFileDialog::Detail);
+
+	if (!fd->exec()== QDialog::Accepted) return;
+
+	String filename(fd->selectedFile().ascii());
+	ssm->getTrajectoryFile()->copyTo(filename);
+	setStatusbarText("Writen DCDFile");
+}
+
+void DatasetControl::add3DGrid()
+	throw()
+{
+	QString result = QFileDialog::getOpenFileName("", "*", 0, "Select a RegularData file");
+	if (result.isEmpty()) return;
+
+	RegularData3D* dat = new RegularData3D;
+	File infile;
+	
+	try
+	{
+		infile.open(result.ascii(), std::ios::in);
+	}
+	catch(Exception::FileNotFound)
+	{
+		Log.error() << "File could not be found!" << std::endl;
+		return;
+	}
+
+	infile >> *dat;
+	infile.close();
+	insertGrid_(dat, *getMainControl()->getSelectedSystem(), String(result.ascii()));
+}
+
+void DatasetControl::insertGrid_(RegularData3D* data, System& system, const String& name)
+	throw()
+{
+	QListViewItem* item = new QListViewItem(listview, name, system.getName().c_str());
+	item_to_grid_[item] = data;
+	insertComposite_(&system, item);
+}
+
+void DatasetControl::save3DGrid_()
+	throw()
+{
+	String result = QFileDialog::getSaveFileName("", "*", 0, "Select a RegularData file").ascii();
+	if (result.isEmpty()) return;
+
+	File outfile;
+	
+	try
+	{
+		outfile.open(result, std::ios::out);
+	}
+	catch(Exception::GeneralException)
+	{
+		Log.error() << "File could not be written!" << std::endl;
+		return;
+	}
+
+	outfile << * item_to_grid_[context_item_];
+	outfile.close();
+	setStatusbarText("3D Grid successfully written...");
+}
+
+void DatasetControl::updateSelection()
+	throw()
+{
+	RegularData3DMessage* message = new RegularData3DMessage(RegularData3DMessage::SELECTED);
+	message->setRegularData3D(0);
+	QListViewItemIterator it(listview);
+	for (; it.current(); ++it)
+	{
+		QListViewItem* item = it.current();
+		if (item->isSelected() &&
+				item_to_grid_.has(item))
+		{
+			message->setRegularData3D(item_to_grid_[item]);
+			message->setCompositeName(item->text(0).ascii());
+			break;
+		}
+	}
+	notify_(message);
+}
+
+} } // namespaces
