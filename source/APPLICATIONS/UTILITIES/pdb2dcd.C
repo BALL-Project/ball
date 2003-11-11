dcc521ca3ca9da4245be6006f5c5fdb000eb3d8c
anker <anker>
2003-11-11 09:15:09 +0000

    added little header

--- source/APPLICATIONS/UTILITIES/pdb2dcd.C
+++ source/APPLICATIONS/UTILITIES/pdb2dcd.C
@@ -1,3 +1,8 @@
+// $Id: pdb2dcd.C,v 1.2 2003/11/11 09:15:09 anker Exp $
+//
+// A very simple utility for combining several pdb snapshots of a system
+// into one dcd file
+
 #include <BALL/KERNEL/system.h>
 #include <BALL/DATATYPE/string.h>
 #include <BALL/FORMAT/PDBFile.h>
