8f47b9940795664a0e2e77b89615874912a7377b
anhi <anhi>
2003-12-17 16:44:34 +0000

    Lots of changes to be able to compile under windows

---
+++ Windows/Libs/Python Module/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : Quelltextdatei, die nur die Standard-Includes einbindet
+//  Python Module.pch ist der vorcompilierte Header
+//  stdafx.obj enthält die vorcompilierte Typinformation
+
+#include "stdafx.h"
