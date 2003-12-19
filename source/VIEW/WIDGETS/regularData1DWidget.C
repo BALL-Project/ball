99bf44c71d4a9f7c07823fda9a2355913308358d
anne <anne>
2003-12-19 14:36:25 +0000

    added new classes for regwidgets

--- source/VIEW/WIDGETS/regularData1DWidget.C
+++ source/VIEW/WIDGETS/regularData1DWidget.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: regularData1DWidget.C,v 1.3 2003/09/19 18:18:01 amoll Exp $
+// $Id: regularData1DWidget.C,v 1.4 2003/12/19 14:36:25 anne Exp $
 //
 
 #include <BALL/VIEW/WIDGETS/regularData1DWidget.h>
@@ -9,227 +9,141 @@
 #include <qpointarray.h>
 #include <qpainter.h>
 #include <qwmatrix.h>
+#include <qlayout.h>
+#include <qpointarray.h>
 
 namespace BALL
 {
 	namespace VIEW
 	{
-
-UpdateRegularData1DMessage::UpdateRegularData1DMessage()
-	: Message(),
-		data_(0)
-{
-#ifdef BALL_VIEW_DEBUG
-	Log.error() << "new RegularData1DWidgetMessage " << this << std::endl;
-#endif
-}
-
-UpdateRegularData1DMessage::UpdateRegularData1DMessage(const UpdateRegularData1DMessage& message)
-	: Message(message),
-		data_(message.data_)
-{
-}
-
-UpdateRegularData1DMessage::~UpdateRegularData1DMessage()
-	throw()
-{
-	#ifdef BALL_VIEW_DEBUG
-	Log.error() << "Destructing object " << (void *)this
-							<< " of class " << RTTI::getName<UpdateRegularData1DMessage>() << std::endl;
-	#endif
-}
-
-RegularData1DWidget::RegularData1DWidget(RegularData1D* data, QWidget *parent) 
-	throw()
-	: QScrollView(parent, "1D Diagram"), 
-		ModularWidget("RegularData1DWidget"), 
-		layout_(viewport(),2,2),
-		pm_(),
-		diagram_(viewport()),
-		x_label_(viewport()),
-		y_label_(viewport()),
-		dummy_label_(viewport()),
-		data_(data),
-		width_(200),
-		height_(200),
-		background_color_(QColor(white)),
-		diagram_color_(QColor(red)),
-		line_style_(true)
-{
-#ifdef BALL_VIEW_DEBUG
-	Log.error() << "new RegularData1DWidget " << this << std::endl;
-#endif
-	setCaption("RegularData1DWidget");
-	ModularWidget::registerWidget(this);
-
-	layout_.addWidget(&y_label_, 0, 0);
-	layout_.addWidget(&diagram_, 0, 1);
-	layout_.addWidget(&dummy_label_, 1, 0);
-	layout_.addWidget(&x_label_, 1, 1);
-	layout_.setColStretch(1, 5);
-	layout_.setRowStretch(0, 5);
-
-	x_label_.setAlignment(AlignRight);
-	y_label_.setAlignment(AlignTop);
-
-	diagram_.resize(width_,height_);
-	diagram_.setScaledContents(true);  
-	diagram_.setFrameShape(QFrame::Box);  
-	diagram_.setLineWidth(1);
-}
-
-RegularData1DWidget::~RegularData1DWidget()
-	throw()
-{
-#ifdef BALL_VIEW_DEBUG
-	Log.error() << "destroying RegularData1DWidget " << this << std::endl;
-#endif
-}
-
-void RegularData1DWidget::initializeWidget(MainControl& main_control)
-{
-	main_control.insertMenuEntry(MainControl::TOOLS, "&1D-NMR", this, SLOT(createPlot()));
-}
-
-void RegularData1DWidget::finalizeWidget(MainControl& main_control)
-{
-	main_control.removeMenuEntry(MainControl::TOOLS, "&1D-NMR", this, SLOT(createPlot()));
-}
-
-void RegularData1DWidget::onNotify(Message* message)
-	throw()
-{
-#ifdef BALL_VIEW_DEBUG
-	Log.error() << "RegularData1DWidget " << this << " onNotify " << message << std::endl;
-#endif
-	if ( data_ == 0 ||
-			!RTTI::isKindOf<UpdateRegularData1DMessage>(*message) ||
-			RTTI::castTo<UpdateRegularData1DMessage>(*message)->getData() != data_)
-	{
-		return;
-	}
-
-	createPlot();
-}
-
-const RegularData1DWidget& RegularData1DWidget::operator = (const RegularData1DWidget &widget)
-	throw()
-{
-	data_ = widget.data_;
-	return *this;
-}
-
-void RegularData1DWidget::clear()
-	throw()
-{
-	data_ = 0;
-}
-
-bool RegularData1DWidget::operator == (const RegularData1DWidget &widget) const
-	throw()
-{
-	return (data_ == widget.data_);
-}
-
-void RegularData1DWidget::createPlot()
-{
-	if (data_->size() == 0) return;
-Log.error() << "createPlot " << data_->size() << std::endl;
-	x_label_.setBackgroundColor(background_color_);
-	y_label_.setBackgroundColor(background_color_);
-	diagram_.setBackgroundColor(background_color_);
-	dummy_label_.setBackgroundColor(background_color_);
-	setPaletteBackgroundColor(background_color_);
-
-	//pm_.resize(width_, height_);
-	pm_.resize(data_->size(), height_);
-	pm_.fill(background_color_);
-	resizeContents(data_->size() + 20, height_ + 30);
-	resize(width_, height_+50);
-
-	QPainter p( &pm_ );
-	p.setPen(diagram_color_);
-	for (Size i = 0; i < data_->size(); i++)
-	{
-		if (line_style_)
+		// Constructor
+		RegularData1DWidget::RegularData1DWidget(const RegularData1D& data, QWidget *parent)
+			throw()
+			:CanvasWidget(parent),
+			 data_(data),
+			 diagram_color_(QColor(red)),
+			background_color_(QColor(white)) //,
+			 //	 height_(0),//400), 
+			 //	 line_style_(true)
 		{
-			p.drawLine(i,height_-(Size)(*data_)[i], i, height_);
+			std::cout <<"constructoraufruf!" << std::endl;
 		}
-		else
+
+		//Copyconstructor
+		//!!!!!!!!!!!!!!!TODO cant work until copyconstructor of pixmap works
+
+		/*	RegularData1DWidget::RegularData1DWidget(RegularData1DWidget* widget)
+			throw() 
+			:pixmapWidget(widget),
+			data_(widget->data),
+			diagram_color_(widget->diagram_color_),
+			background_color_(widget->background_color_)//,
+		//	heigth_(widget->heigth_),
+		//	line_style_(widget->line_style)
+		{ 
+		}*/ 
+
+		// Destructor
+		RegularData1DWidget::~RegularData1DWidget()
+		 throw()
 		{
-			p.drawPoint(i,(Position)(*data_)[i]);
 		}
-	}
 
-	Log.error() <<std::endl << (*data_)[width_/2] << std::endl; 
-//  p.drawLine(1,height_-1,width_-1, height_-1);
-//  p.drawLine(1,1,1, height_-1);
+		// creates a polygon from a given vector RegularData1D * data
 
-	p.end();
-	diagram_.setPixmap(pm_);
-	x_label_.setText(x_axis_text_.c_str());
-	y_label_.setText(y_axis_text_.c_str());
-	repaint();
-}
+		void RegularData1DWidget::createPolygon()
+			throw()
+		{
+ 			// no data => no polygon
+			if (data_.size() == 0) return;
 
-void RegularData1DWidget::setXAxisText(const String& text)
-	throw()
-{
-	x_axis_text_ = text;
-}
+			// find the data min and max
+			float min=numeric_limits<float>::max(), max=numeric_limits<float>::min();
+			float dif_min=min;
+			float old=min; //last point
 
-void RegularData1DWidget::setYAxisText(const String& text)
-	throw()
-{
-	y_axis_text_ = text;
-}
+			for (int i=0; i<(int)data_.size(); i++)
+			{
+				if (data_[i] >= max)
+					max = data_[i];
+				if (data_[i] <= min)
+					min = data_[i];
 
-const String& RegularData1DWidget::getXAxisText() const
-	throw()
-{
-	return x_axis_text_;
-}
+				float dif = data_[i] - old;
 
-const String& RegularData1DWidget::getYAxisText() const
-	throw()
-{
-	return y_axis_text_;
-}
+				if ( (dif > 1e-3) && (dif < dif_min) ) 
+				{
+					dif_min = dif;
+				}
 
-void RegularData1DWidget::setBackgroundColor(const QColor& color)
-	throw()
-{
-	background_color_ = color;
-}
+				old = data_[i];
+			}
 
-const QColor& RegularData1DWidget::getBackgroundColor() const
-	throw()
-{
-	return background_color_;
-}
+			height_ = (int)ceil(((max-min) / dif_min) * 5);
+		 
+      if (height_ > 1000)
+	    {
+				// the resulting picture would be much too large
+				height_ = 1000;
+				dif_min = (max - min)/200.;
+			}	
 
-void RegularData1DWidget::setDiagramColor(const QColor& color)
-	throw()
-{
-	diagram_color_ = color;
-}
+			// resize the canvas to fit the data
+			canvas_.resize(5*(data_.size()+2), height_+10);
 
-const QColor& RegularData1DWidget::getDiagramColor() const
-	throw()
-{
-	return diagram_color_;
-}
+			int x_new;
+			int y_new;
+			int x_old = 5;
+			int y_old = height_+5 - (int)round(((data_[0]-min)/dif_min)*5);
+			QCanvasLine *ql;
+			QBrush br(QColor(black));
+			//			ql.setBrush(br);
 
-void RegularData1DWidget::setLineStyle(bool state)
-	throw()
-{
-	line_style_ = state;
-}
+			for (int i=1; i<(int)data_.size(); i++)
+			{
+				x_new = 5*(i+1);
+				y_new = height_+5 - (int)round(((data_[i]-min)/dif_min)*5);
+				
+				ql = new QCanvasLine(&canvas_);
+				ql->setPoints(x_old, y_old, x_new, y_new);
+				objects_.push_back(dynamic_cast<QCanvasItem*> (ql));
+				ql->show();
 
-bool RegularData1DWidget::isLineStyle() const
-	throw()
-{
-	return line_style_;
-}
+				x_old = x_new;
+				y_old = y_new;
+			}
 
-} } // namespace
+			//add the x-axis
+				ql = new QCanvasLine(&canvas_);
+				int startx = 5;
+				int starty = height_+5 - (int)round(((startx-min)/dif_min)*5);
+				int endx   = data_.size()*5;
+				int endy   = height_+5 - (int)round(((data_[endx]-min)/dif_min)*5);
+				
+				ql->setPoints(startx, starty, endx, endy);
+				objects_.push_back(dynamic_cast<QCanvasItem*> (ql));
+				
+			 //QCanvasPolygon* =qp;
+			 
+			//add the y-axis	
+				ql = new QCanvasLine(&canvas_);
+				startx = 0;
+				starty = height_+5 - (int)round(((0-min)/dif_min)*5);
+				endx   = data_.size()*5;
+				endy   = height_+5 - (int)round(((data_[endx]-min)/dif_min)*5);
+				ql->setPoints(startx, starty, endx, endy);
+				objects_.push_back(dynamic_cast<QCanvasItem*> (ql));
+				
+
+			//				polygon_ = new QCanvasPolygon(&canvas_);
+			//			polygon_ = new QCanvasSpline(&canvas_);	
+			// polygon_->setControlPoints((QPointArray)point_data_, false);
+			
+				//insert the polygon into vector<canvasItem*> of canvasWidget
+			//				objects_.push_back(dynamic_cast<QCanvasItem*> (polygon_));
+			}
+
+
+	}//end of namespace VIEW
+
+}//end of namespace BALL
