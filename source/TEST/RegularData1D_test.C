d1697a320d1d7d62a85db5990e3e584e7142d35e
anhi <anhi>
2004-02-25 16:03:37 +0000

    Added a lot of tests

--- source/TEST/RegularData1D_test.C
+++ source/TEST/RegularData1D_test.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: RegularData1D_test.C,v 1.16 2003/06/17 14:00:28 amoll Exp $
+// $Id: RegularData1D_test.C,v 1.17 2004/02/25 16:03:37 anhi Exp $
 
 #include <BALL/CONCEPT/classTest.h>
 
@@ -12,7 +12,7 @@
 
 ///////////////////////////
 
-START_TEST(RegularData1D, "$Id: RegularData1D_test.C,v 1.16 2003/06/17 14:00:28 amoll Exp $")
+START_TEST(RegularData1D, "$Id: RegularData1D_test.C,v 1.17 2004/02/25 16:03:37 anhi Exp $")
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
@@ -26,15 +26,66 @@ CHECK(TRegularData1D() throw())
 	TEST_NOT_EQUAL(rd_ptr, 0)
 RESULT
 
-
 CHECK(~TRegularData1D() throw())
   delete rd_ptr;
 RESULT
 
-CHECK(BALL_CREATE(TRegularData1D<ValueType>))
-	vector<float> v;
+CHECK(TRegularData1D(const CoordinateType& origin, const CoordinateType& dimension, const CoordinateType& spacing) throw(Exception::OutOfMemory))
+	RegularData1D rd(2.0, 4.0, 0.5);
+	TEST_REAL_EQUAL(rd.getOrigin(), 2.0);
+	TEST_REAL_EQUAL(rd.getDimension(), 4.0);
+	TEST_REAL_EQUAL(rd.getSpacing(), 0.5);
+	TEST_EQUAL(rd.getSize(), 9);
+RESULT
+
+CHECK(TRegularData1D(const TRegularData1D& data) throw(Exception::OutOfMemory))
+	RegularData1D rd(2.0, 4.0, 0.5);
+	RegularData1D rd2(rd);
+
+	TEST_REAL_EQUAL(rd2.getOrigin(), 2.0);
+	TEST_REAL_EQUAL(rd2.getDimension(), 4.0);
+	TEST_REAL_EQUAL(rd2.getSpacing(), 0.5);
+	TEST_EQUAL(rd2.getSize(), 9);
+RESULT
+
+CHECK(TRegularData1D(const IndexType& size) throw(Exception::OutOfMemory))
+	RegularData1D rd(5);
+	TEST_EQUAL(rd.getSize(), 5);
+	TEST_REAL_EQUAL(rd.getOrigin(), 0.0);
+	TEST_REAL_EQUAL(rd.getDimension(), 1.0);
+	TEST_REAL_EQUAL(rd.getSpacing(), 0.25);
+RESULT
+
+CHECK(TRegularData1D(const VectorType& data, const CoordinateType& origin = 0.0, const CoordinateType& dimension = 1.0) throw(Exception::OutOfMemory))
+	RegularData1D::VectorType v;
 	v.push_back(1.1);
-	RegularData1D a(v, 1.1, 1.2); 
+	v.push_back(1.2);
+	v.push_back(1.3);
+	v.push_back(1.4);
+
+	RegularData1D rd2(v, 1.0, 0.5);
+	TEST_REAL_EQUAL(rd2.getOrigin(), 1.0)
+	TEST_REAL_EQUAL(rd2.getDimension(), 0.5)
+	TEST_REAL_EQUAL(rd2.getSpacing(), 0.5/3.)
+	TEST_EQUAL(rd2.getSize(), 4)
+	TEST_REAL_EQUAL(rd2[0], 1.1)
+	TEST_REAL_EQUAL(rd2[3], 1.4)
+
+	RegularData1D rd3(v); // The default arguments
+	TEST_REAL_EQUAL(rd3.getOrigin(), 0.0)
+	TEST_REAL_EQUAL(rd3.getDimension(), 1.0)
+	TEST_EQUAL(rd3.getSize(), 4)
+
+	RegularData1D rd4(v, 2.0, 1.0);
+	TEST_REAL_EQUAL(rd4.getOrigin(), 2.0)
+	TEST_REAL_EQUAL(rd4.getDimension(), 1.0)
+	TEST_EQUAL(rd4.getSize(), 4)
+RESULT
+
+CHECK(BALL_CREATE(TRegularData1D<ValueType>))
+	vector<float> v2;
+	v2.push_back(1.1);
+	RegularData1D a(v2, 1.1, 1.2); // This is the vector type constructor
 	RegularData1D* v_ptr = (RegularData1D*)a.create(false, true);
 	TEST_EQUAL(v_ptr->getSize(), 0)
 	TEST_REAL_EQUAL(v_ptr->getOrigin(), 0.0)
@@ -43,6 +94,7 @@ CHECK(BALL_CREATE(TRegularData1D<ValueType>))
 	TEST_EQUAL(v_ptr->getSize(), 1)
 	TEST_REAL_EQUAL(v_ptr->getOrigin(), 1.1)
 	TEST_REAL_EQUAL(v_ptr->getDimension(), 1.2)
+	TEST_REAL_EQUAL((*v_ptr)[0], 1.1)
 	delete v_ptr;
 RESULT
 		
@@ -56,39 +108,13 @@ CHECK(void setOrigin(const CoordinateType& origin) throw())
 	TEST_REAL_EQUAL(rd.getDimension(), 1.0)
 RESULT
 
-
 CHECK(void setDimension(const CoordinateType& dimension) throw())
+	//////// SPACING TESTS!!!
 	rd.setDimension(2.2);
 	TEST_REAL_EQUAL(rd.getOrigin(), 1.1)
 	TEST_REAL_EQUAL(rd.getDimension(), 2.2)
 	rd.setDimension(0.0);
 	TEST_REAL_EQUAL(rd.getDimension(), 0.0)
-RESULT
-
-
-RegularData1D::VectorType v;
-v.push_back(1.1);
-v.push_back(1.2);
-v.push_back(1.3);
-v.push_back(1.4);
-
-CHECK(TRegularData1D(const VectorType& data, const CoordinateType& origin = 0.0, const CoordinateType& dimension = 1.0) throw(Exception::OutOfMemory))
-	RegularData1D rd2(v, 1.0, 0.5);
-	TEST_REAL_EQUAL(rd2.getOrigin(), 1.0)
-	TEST_REAL_EQUAL(rd2.getDimension(), 0.5)
-	TEST_EQUAL(rd2.getSize(), 4)
-	TEST_REAL_EQUAL(rd2[0], 1.1)
-	TEST_REAL_EQUAL(rd2[3], 1.4)
-
-	RegularData1D rd3(v);
-	TEST_REAL_EQUAL(rd3.getOrigin(), 0.0)
-	TEST_REAL_EQUAL(rd3.getDimension(), 1.0)
-	TEST_EQUAL(rd3.getSize(), 4)
-
-	RegularData1D rd4(v, 2.0, 1.0);
-	TEST_REAL_EQUAL(rd4.getOrigin(), 2.0)
-	TEST_REAL_EQUAL(rd4.getDimension(), 1.0)
-	TEST_EQUAL(rd4.getSize(), 4)
 RESULT
 
 
@@ -321,85 +347,152 @@ CHECK(void rescale(const IndexType& new_size) throw(Exception::OutOfMemory))
 }
 RESULT
 
+RegularData1D::VectorType v;
+v.push_back(1.1);
+v.push_back(1.2);
+v.push_back(1.3);
+v.push_back(1.4);
+
+RegularData1D rd_it(v);
 
 CHECK(ConstIterator begin() const throw())
-  // ???
+	TEST_REAL_EQUAL(*(rd_it.begin()), 1.1);
 RESULT
 
 CHECK(ConstIterator end() const throw())
-  // ???
-RESULT
-
-CHECK(CoordinateType getCoordinates(const IndexType& index) const throw(Exception::OutOfGrid))
-  // ???
-RESULT
-
-CHECK(IndexType getClosestIndex(const CoordinateType& x) const throw(Exception::OutOfGrid))
-  // ???
-RESULT
-
-CHECK(IndexType getLowerIndex(const CoordinateType& x) const throw(Exception::OutOfGrid))
-  // ???
+	TEST_REAL_EQUAL(*(--rd_it.end()), 1.4);
 RESULT
 
 CHECK(Iterator begin() throw())
-  // ???
+	TEST_REAL_EQUAL(*(rd_it.begin()), 1.1);
 RESULT
 
 CHECK(Iterator end() throw())
-  // ???
+	TEST_REAL_EQUAL(*(--rd_it.end()), 1.4);
 RESULT
 
-CHECK(TRegularData1D(const CoordinateType& origin, const CoordinateType& dimension, const CoordinateType& spacing) throw(Exception::OutOfMemory))
-  // ???
+CHECK(CoordinateType getCoordinates(const IndexType& index) const throw(Exception::OutOfGrid))
+	RegularData1D rd(0.0, 2.0, 0.5);
+
+	TEST_REAL_EQUAL(rd.getCoordinates(0), 0.0);
+	TEST_REAL_EQUAL(rd.getCoordinates(1), 0.5);
+	TEST_REAL_EQUAL(rd.getCoordinates(4), 2.0);
 RESULT
 
-CHECK(TRegularData1D(const IndexType& size, const CoordinateType& origin = CoordinateType(0.0), const CoordinateType& dimension = CoordinateType(1.0)) throw(Exception::OutOfMemory))
-  // ???
+CHECK(IndexType getClosestIndex(const CoordinateType& x) const throw(Exception::OutOfGrid))
+ 	RegularData1D rd(0.0, 2.0, 0.5);
+
+	TEST_EQUAL(rd.getClosestIndex(0.0), 0);
+	TEST_EQUAL(rd.getClosestIndex(0.1), 0);
+	TEST_EQUAL(rd.getClosestIndex(0.26), 1);
+	TEST_EQUAL(rd.getClosestIndex(0.6), 1);
 RESULT
 
-CHECK(TRegularData1D(const TRegularData1D& data) throw(Exception::OutOfMemory))
-  // ???
+CHECK(IndexType getLowerIndex(const CoordinateType& x) const throw(Exception::OutOfGrid))
+ 	RegularData1D rd(0.0, 2.0, 0.5);
+
+	TEST_EQUAL(rd.getLowerIndex(0.0), 0);
+	TEST_EQUAL(rd.getLowerIndex(0.1), 0);
+	TEST_EQUAL(rd.getLowerIndex(0.26), 0);
+	TEST_EQUAL(rd.getLowerIndex(0.6), 1);
 RESULT
 
 CHECK(ValueType getInterpolatedValue(const CoordinateType& x) const throw(Exception::OutOfGrid))
-  // ???
+	RegularData1D::VectorType v;
+	v.push_back(0.0);
+	v.push_back(0.5);
+	v.push_back(1.0);
+	RegularData1D rd(v, 0.0, 1.0);
+
+	TEST_REAL_EQUAL(rd.getInterpolatedValue(0.0), 0.0);
+	TEST_REAL_EQUAL(rd.getInterpolatedValue(0.5), 0.5);
+	TEST_REAL_EQUAL(rd.getInterpolatedValue(1.0), 1.0);
+	TEST_REAL_EQUAL(rd.getInterpolatedValue(0.2), 0.2);
+	TEST_REAL_EQUAL(rd.getInterpolatedValue(0.6), 0.6);
+	TEST_REAL_EQUAL(rd.getInterpolatedValue(0.9), 0.9);
 RESULT
 
 CHECK(ValueType operator () (const CoordinateType& x) const throw())
-  // ???
+	RegularData1D::VectorType v;
+	v.push_back(0.0);
+	v.push_back(0.5);
+	v.push_back(1.0);
+	RegularData1D rd(v, 0.0, 1.0);
+
+	TEST_REAL_EQUAL(rd(0.0), 0.0)
+	TEST_REAL_EQUAL(rd(0.5), 0.5)
+	TEST_REAL_EQUAL(rd(1.0), 1.0)
+	TEST_REAL_EQUAL(rd(0.2), 0.2)
+	TEST_REAL_EQUAL(rd(0.6), 0.6)
+	TEST_REAL_EQUAL(rd(0.9), 0.9)
 RESULT
 
 CHECK(ValueType& getClosestValue(const CoordinateType& x) throw(Exception::OutOfGrid))
-  // ???
-RESULT
+	RegularData1D::VectorType v;
+	v.push_back(0.0);
+	v.push_back(0.5);
+	v.push_back(1.0);
+	RegularData1D rd(v, 0.0, 1.0);
 
-CHECK(bool empty() const throw())
-  // ???
-RESULT
-
-CHECK(bool isInside(const CoordinateType& x) const throw())
-  // ???
-RESULT
-
-CHECK(bool operator != (const TRegularData1D& data) const throw())
-  // ???
-RESULT
-
-CHECK(const CoordinateType& getSpacing() const throw())
-  // ???
+	TEST_REAL_EQUAL(rd.getClosestValue(0.0), 0.0)
+	TEST_REAL_EQUAL(rd.getClosestValue(0.4), 0.5)
+	TEST_REAL_EQUAL(rd.getClosestValue(0.6), 0.5)
+	TEST_REAL_EQUAL(rd.getClosestValue(0.9), 1.0)
 RESULT
 
 CHECK(const ValueType& getClosestValue(const CoordinateType& x) const throw(Exception::OutOfGrid))
-  // ???
+ 	RegularData1D::VectorType v;
+	v.push_back(0.0);
+	v.push_back(0.5);
+	v.push_back(1.0);
+	const RegularData1D rd(v, 0.0, 1.0);
+
+	TEST_REAL_EQUAL(rd.getClosestValue(0.0), 0.0)
+	TEST_REAL_EQUAL(rd.getClosestValue(0.4), 0.5)
+	TEST_REAL_EQUAL(rd.getClosestValue(0.6), 0.5)
+	TEST_REAL_EQUAL(rd.getClosestValue(0.9), 1.0)
+RESULT
+
+CHECK(bool empty() const throw())
+	RegularData1D rd(0.0, 0.0, 0.0);
+
+	TEST_EQUAL(rd.empty(), true);
+RESULT
+
+CHECK(bool isInside(const CoordinateType& x) const throw())
+	RegularData1D rd(0.0, 1.0, 0.5);
+
+	TEST_EQUAL(rd.isInside(-0.01), false);
+	TEST_EQUAL(rd.isInside(0.2), true);
+	TEST_EQUAL(rd.isInside(1.1), false);
+RESULT
+
+CHECK(bool operator != (const TRegularData1D& data) const throw())
+	RegularData1D rd(0.0, 1.0, 0.5);
+	RegularData1D rd2(0.2, 1.0, 0.5);
+
+	TEST_EQUAL(rd != rd2, true);
+	TEST_EQUAL(rd != rd, false);
+RESULT
+
+CHECK(const CoordinateType& getSpacing() const throw())
+ 	RegularData1D rd(0.0, 1.0, 0.5);
+
+	TEST_REAL_EQUAL(rd.getSpacing(), 0.5);	
 RESULT
 
 CHECK(size_type max_size() const throw())
-  // ???
+  RegularData1D::VectorType v;
+	RegularData1D rd(v);
+
+	TEST_EQUAL(rd.max_size(), v.max_size());
 RESULT
 
 CHECK(size_type size() const throw())
-  // ???
+  RegularData1D::VectorType v(5);
+	RegularData1D rd(v);
+
+	TEST_EQUAL(rd.size(), v.size());
 RESULT
 
 CHECK(void binaryRead(const String& filename) throw())
