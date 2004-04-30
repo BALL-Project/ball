46e30cafead42ad4a77c361493eff2b3d7f95cd3
amoll <amoll>
2004-04-30 14:02:27 +0000

    no message

--- source/APPLICATIONS/BALLVIEW/main.C
+++ source/APPLICATIONS/BALLVIEW/main.C
@@ -1,13 +1,15 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: main.C,v 1.1 2004/04/16 11:24:47 amoll Exp $
+// $Id: main.C,v 1.3 2004/04/30 14:09:34 amoll Exp $
 //
 
 // order of includes is important: first qapplication, than BALL includes
 #include <qapplication.h>
 #include "mainframe.h"
+#include <BALL/SYSTEM/path.h>
 #include <iostream>
+#include <qmessagebox.h>
 
 #ifndef BALL_PLATFORM_WINDOWS
 int main(int argc, char **argv)
@@ -20,23 +22,25 @@ int WINAPI WinMain( HINSTANCE, HINSTANCE, PSTR cmd_line, int )
 #endif
 
 
+	QApplication application(argc, argv);
+
 	char*	BALLView_data_path = getenv("BALLVIEW_DATA_PATH");
-	if (BALLView_data_path == 0)
-	{
-#ifdef BALL_PLATFORM_WINDOWS
-		char*	ball_data_path = getenv("BALL_DATA_PATH");
-		if (ball_data_path == 0)
-		{
-			std::cerr << "Warning: Neither BALL_DATA_PATH nor BALLVIEW_DATA_PATH environment variable are set." << std::endl;
-		}
-#endif
-	}
-	else
+	if (BALLView_data_path != 0)
 	{
  		putenv((char*)((BALL::String("BALL_DATA_PATH=") + BALL::String(BALLView_data_path)).c_str()));
 	}
 
-	QApplication application(argc, argv);
+	BALL::Path d;
+	if (BALL::String(d.find("CHARMM")) == "")
+	{
+		QMessageBox::critical(0, "Error while starting BALLView",
+				QString("Could not find the BALL data directory!\n") + 
+				"Please set the BALL_DATA_PATH or BALLVIEW_DATA_PATH\n" + 
+				"environment variable to the directory containing the\n" + 
+				"BALL or BALLView data directory (e.g. to C:\\BALL\\data).", 
+				QMessageBox::Abort,  QMessageBox::NoButton);
+		exit(-1);
+	}
 
 	// Create the mainframe.
 	BALL::Mainframe mainframe;
@@ -49,10 +53,14 @@ int WINAPI WinMain( HINSTANCE, HINSTANCE, PSTR cmd_line, int )
 	// Show the main window.
 	mainframe.show();
 
-	// If there are additional command line arguments, interpret
-	// them as files to open.
+	// If there are additional command line arguments, interpret them as files to open or logging flag.
 	for (BALL::Index i = 1; i < argc; ++i)
 	{
+		if (BALL::String(argv[i]) == "-l") 
+		{
+			mainframe.enableLoggingToFile();
+			continue;
+		}
 		mainframe.openFile(argv[i]);
 	}
 
