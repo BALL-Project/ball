7d1b76ae8cb1e422b3de2be6f4869792ff9e8121
amoll <amoll>
2004-05-12 12:47:16 +0000

    no message

--- source/EXAMPLES/PYTHON/simulated_annealing.py
+++ source/EXAMPLES/PYTHON/simulated_annealing.py
@@ -1,49 +1,55 @@
+#   example script for simulated annealing
+#   output heated structures as Hinfile
+#   one system has to be loaded before calling the script
 mc = MainControl.getInstance(0)
 ms = MolecularStructure.getInstance(0)
 ff = ms.getForceField()
 cm = mc.getCompositeManager()
-system = mc.getCompositeManager().getComposites()[0]
-ff.setup(system)
+c = cm.getComposites()
+system = c[0]
+mds = CanonicalMD()
 ssm = SnapShotManager()
-#ssm.setSystem(system)
-#ssm.setForceField(ff)
-mds = CanonicalMD(ff, ssm)
+ff.setup(system)
+mds.setup(ff, ssm)
 mds.setTimeStep(0.001)
 time = 0
 starttemperature = 0
-zieltemperature = 293
+endtemperature =600
 temperature = 0
 counter = 0
 step = 1.
-limit = 100
-heatlimit=200
-step = float(zieltemperature - starttemperature) / float(limit)
-print step
+limit = 600
+heatlimit=1050
+step = float(endtemperature - starttemperature) / float(limit)
 mds.setReferenceTemperature(0)
-#pB = PeriodicBoundary(ff)
-#pB.setup()
-#pB.addSolvent("/home/student/anne/H2O.hin")
 temperature = starttemperature
 while counter < limit:
- mds.simulateIterations(50)
- mds.setReferenceTemperature(temperature)
- mc.update(system)
- counter = counter + 1
- temperature = float(step)  * float(counter)
-
-temperature = zieltemperature
-while counter < heatlimit:
- mds.simulateIterations(50)
- mds.setReferenceTemperature(temperature)
- mc.update(system)
- counter = counter + 1
+  mds.simulateIterations(50)
+  mds.setReferenceTemperature(temperature)
+  mc.update(system)
+  counter = counter + 1
+  temperature = float(step)  * float(counter)
 
 counter = 0
-temperature = zieltemperature
-while counter < limit:
+temperature = endtemperature
+while counter < heatlimit:
+  mds.simulateIterations(1000)
+  mds.setReferenceTemperature(temperature)
+  mc.update(system)
+  outfile = HINFile("heating1_"+str(counter)+".hin", File.OUT)
+  outfile.write(system)
+  outfile.close()
+  counter = counter + 1
+
+mc.update(system)
+counter = limit
+temperature = endtemperature
+while counter > 0:
  mds.simulateIterations(50)
  mds.setReferenceTemperature(temperature)
  mc.update(system)
  counter = counter - 1
  temperature = float(step) * float(counter)
 
+mc.update(system)
+print ff.updateEnergy()
