e061e07cdd71cd5ceb932f5eee4a00d4b5def9b5
amoll <amoll>
2004-05-15 10:57:13 +0000

    fixed

--- source/TUTORIAL/bounding_box.C
+++ source/TUTORIAL/bounding_box.C
@@ -1,13 +1,10 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: bounding_box.C,v 1.7 2004/04/16 11:51:31 amoll Exp $
+// $Id: bounding_box.C,v 1.7.2.1 2004/05/15 10:57:13 amoll Exp $
 // BALLView tutorial example
 // ------------------------
-// create a processor that computes the bounding box of a molecular structure
-
-// standard includes
-#include <iostream>
+// visualise the bounding box of a molecular structure
 
 // BALL includes
 #include <BALL/common.h>
@@ -17,8 +14,8 @@
 #include <BALL/VIEW/KERNEL/mainControl.h>
 #include <BALL/VIEW/WIDGETS/scene.h>
 #include <BALL/VIEW/WIDGETS/geometricControl.h>
-#include "../APPLICATIONS/BALLVIEW/mainframe.h "
 #include <BALL/VIEW/WIDGETS/molecularControl.h>
+#include "../APPLICATIONS/BALLVIEW/mainframe.h"
 
 #include <qapplication.h>
 
@@ -36,12 +33,17 @@ class MyMainframe
 {
 	public:
 		MyMainframe(QWidget* parent = 0, const char* name = 0)
+			:	MainControl(parent, name)
 		{
 			Scene* scene_ = new Scene(this, "3D View");
 			scene_->setMinimumSize(10, 10);
 			setCentralWidget(scene_);
 
 			GeometricControl* geometric_control_ = new GeometricControl(this, "Representations");
+			CHECK_PTR(geometric_control_);
+
+			MolecularControl* molecular_control_ = new MolecularControl(this, "Molecules");
+			CHECK_PTR(molecular_control_);
 		}	
 };
 
@@ -50,20 +52,21 @@ int main(int argc, char **argv)
   QApplication application(argc, argv);
   MyMainframe mainframe;
   application.setMainWidget(&mainframe);
-	// 	mainframe.registerThis();
   // start the application
   mainframe.show();
+
   // read a molecule from a file
   HINFile infile("bounding_box_example.hin") ;
   System* system = new System();
   infile >> *system;
   infile.close();
- 	mainframe.insert(*system);
+  mainframe.insert(*system);
 
   // apply the bounding box processor
   GLBoundingBoxModel bb_processor;
 	bb_processor.setColor(ColorRGBA(255,255,255));
   system->apply(bb_processor);
+
   Representation* rep = new Representation();
 	List<GeometricObject*>::Iterator it = bb_processor.getGeometricObjects().begin();
 	for (;it != bb_processor.getGeometricObjects().end(); ++it)
