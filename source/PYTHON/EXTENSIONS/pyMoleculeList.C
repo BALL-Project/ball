afddd1e187f59ea0193a2e370ca50eebebb658c8
oliver <oliver>
2004-09-22 21:07:06 +0000

    adapted Py support to new (fixed) version of SIP

--- source/PYTHON/EXTENSIONS/pyMoleculeList.C
+++ source/PYTHON/EXTENSIONS/pyMoleculeList.C
@@ -1,10 +1,13 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: pyMoleculeList.C,v 1.2 2002/02/27 12:23:59 sturm Exp $
+// $Id: pyMoleculeList.C,v 1.3 2004/09/22 21:07:05 oliver Exp $
+//
 
 #include <BALL/PYTHON/pyMoleculeList.h>
 #include <BALL/KERNEL/molecule.h>
+#include <BALL/CONCEPT/composite.h>
+#include <BALL/KERNEL/fragment.h>
 #include <BALL/KERNEL/atomContainer.h>
 
 namespace BALL
