6f1433657e7ddaf6d4a56e591ca27231ee1b680d
oliver <oliver>
2004-12-02 18:48:55 +0000

    added: Benchmark for simple data structures (contains HashMap only right now)

---
+++ source/BENCHMARKS/DataStructures_bench.C
@@ -0,0 +1,81 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id: DataStructures_bench.C,v 1.1 2004/12/02 18:48:55 oliver Exp $
+//
+
+#include <BALL/CONCEPT/benchmark.h>
+
+///////////////////////////
+
+#include <BALL/DATATYPE/hashMap.h>
+#include <BALL/DATATYPE/hashSet.h>
+#include <map>
+#include <utility>
+#include <stdlib.h>
+
+///////////////////////////
+
+using namespace BALL;
+
+
+START_BENCHMARK(DataStructures, 10.0, "$Id: DataStructures_bench.C,v 1.1 2004/12/02 18:48:55 oliver Exp $")
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+HashSet<Size> hashed_numbers;
+Size MAX_NUMBERS = 5000000;
+while (hashed_numbers.size() < MAX_NUMBERS)
+{
+	hashed_numbers.insert(Size(random()));
+}
+std::vector<Size> numbers(MAX_NUMBERS);
+std::copy(hashed_numbers.begin(), hashed_numbers.end(), numbers.begin());
+
+START_SECTION(HashMap::insert, 1.0)
+	HashMap<Size, Size> hm;
+	for (Position i = 0; i < numbers.size(); ++i)
+	{
+		START_TIMER
+			hm.insert(std::make_pair(numbers[i], i));
+		STOP_TIMER
+	}
+END_SECTION
+
+START_SECTION(HashMap::lookup, 1.0)
+	Size s;
+	for (Position i = 0; i < numbers.size(); ++i)
+	{
+		START_TIMER
+			s = hm[numbers[i]];
+		STOP_TIMER
+	}
+END_SECTION
+
+/*
+START_SECTION(map::insert, 0.0)
+	std::map<Size, Size> m;
+	for (Position i = 0; i < numbers.size(); ++i)
+	{
+		START_TIMER
+			m.insert(std::make_pair(numbers[i], i));
+		STOP_TIMER
+	}
+END_SECTION
+
+START_SECTION(map::lookup, 0.0)
+	for (Position i = 0; i < numbers.size(); ++i)
+	{
+		START_TIMER
+			s = m[numbers[i]];
+		STOP_TIMER
+	}
+END_SECTION
+*/
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+END_BENCHMARK
