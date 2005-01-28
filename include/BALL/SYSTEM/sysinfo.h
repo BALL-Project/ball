06d990bf66afd6871404ff5965969ce636c4cd83
amoll <amoll>
2005-01-28 13:16:44 +0000

    added temp fix for undefined LongIndex

--- include/BALL/SYSTEM/sysinfo.h
+++ include/BALL/SYSTEM/sysinfo.h
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: sysinfo.h,v 1.6 2005/01/27 15:29:54 oliver Exp $
+// $Id: sysinfo.h,v 1.7 2005/01/28 13:16:44 amoll Exp $
 //
 
 #ifndef BALL_SYSTEM_SYSINFO_H
@@ -13,6 +13,9 @@
 
 namespace BALL
 {
+	/// where does thos LongIndex come from ?????
+	typedef long LongIndex;
+	
 	/** Namespace for methods to obtain informations about the systems hardware.
 	 		See the man page for sysinfo, GetSystemInfo and /proc/cpuinfo under Linux.
 	 		@see http://msdn.microsoft.com/library/en-us/memory/base/memorystatus_str.asp
