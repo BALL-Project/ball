8c36ea97984551cf09afe32571e19ba17dc7f7ae
oliver <oliver>
2005-02-11 16:26:20 +0000

    *** empty log message ***

---
+++ source/FORMAT/PDBdefs.C
@@ -0,0 +1,9 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id: PDBdefs.C,v 1.1 2005/02/11 16:26:20 oliver Exp $
+//
+
+#include <BALL/FORMAT/PDBdefs.h>
+
+// no code here... just type definitions and enums.
