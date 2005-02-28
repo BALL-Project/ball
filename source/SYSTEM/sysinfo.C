32ce7ff5091accee48acf5975ef2c6df452055a2
oliver <oliver>
2005-02-28 20:20:31 +0000

    fixed: did not comile under Darwin!

--- source/SYSTEM/sysinfo.C
+++ source/SYSTEM/sysinfo.C
@@ -1,17 +1,22 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: sysinfo.C,v 1.9 2005/01/29 18:02:02 oliver Exp $
+// $Id: sysinfo.C,v 1.10 2005/02/28 20:20:31 oliver Exp $
 //
 
 #include <BALL/SYSTEM/sysinfo.h>
 
-#ifndef BALL_PLATFORM_WINDOWS
+#ifdef BALL_HAS_SYSINFO
 #	include <sys/sysinfo.h>
 #	include <BALL/SYSTEM/file.h>
 #else
-#	define WIN32_LEAN_AND_MEAN
-#	include <windows.h>
+# ifdef BALL_PLATFORM_WINDOWS
+#	  define WIN32_LEAN_AND_MEAN
+#	  include <windows.h>
+# endif
+# ifdef BALL_PLATFORM_DARWIN
+#  include <sys/sysctl.h>
+#	endif
 #endif
 
 namespace BALL
@@ -30,12 +35,7 @@ namespace BALL
 
 		LongIndex getFreeMemory()
 		{
-#ifdef BALL_PLATFORM_WINDOWS
-			MEMORYSTATUSEX statex;
-			GlobalMemoryStatusEx (&statex);
-			return static_cast<LongIndex>(statex.ullAvailPhys);
-#else
-#ifdef BALL_OS_LINUX
+#ifdef BALL_HAS_SYSINFO
 			struct sysinfo info;
 			LongIndex result = sysinfo(&info);
 			if (result == -1) 
@@ -44,19 +44,19 @@ namespace BALL
 			}
 			return info.freeram * info.mem_unit;
 #else
+#	ifdef BALL_PLATFORM_WINDOWS
+			MEMORYSTATUSEX statex;
+			GlobalMemoryStatusEx (&statex);
+			return static_cast<LongIndex>(statex.ullAvailPhys);
+#	else
 			return -1;
-#endif
+#	endif
 #endif
 		}
 
 		LongIndex getTotalMemory()
 		{
-#ifdef BALL_PLATFORM_WINDOWS
- 			MEMORYSTATUSEX statex;
-			GlobalMemoryStatusEx (&statex);
-			return static_cast<LongIndex>(statex.ullTotalPhys);
-#else
-#ifdef BALL_OS_LINUX
+#ifdef BALL_HAS_SYSINFO
 			struct sysinfo info;
 			LongIndex result = sysinfo(&info);
 			if (result == -1) 
@@ -65,6 +65,11 @@ namespace BALL
 			}
 			return info.totalram * info.mem_unit;
 #else
+#	ifdef BALL_PLATFORM_WINDOWS
+ 			MEMORYSTATUSEX statex;
+			GlobalMemoryStatusEx (&statex);
+			return static_cast<LongIndex>(statex.ullTotalPhys);
+#	else
 			return -1;
 #endif
 #endif
@@ -72,67 +77,67 @@ namespace BALL
 
 		LongIndex getBufferedMemory()
 		{
-#ifdef BALL_PLATFORM_WINDOWS
-			return -1;
-#else
-#ifdef BALL_OS_LINUX
+#ifdef BALL_HAS_SYSINFO
 			struct sysinfo info;
 			LongIndex result = sysinfo(&info);
 			if (result == -1) return result;
 			return info.bufferram * info.mem_unit;
 #else
+#	ifdef BALL_PLATFORM_WINDOWS
 			return -1;
-#endif
+#	else
+			return -1;
+#	endif
 #endif
 		}
 
 		Time getUptime()
 		{
-#ifdef BALL_PLATFORM_WINDOWS
-			return -1;
-#else
-#ifdef BALL_OS_LINUX
+#ifdef BALL_HAS_SYSINFO
 			struct sysinfo info;
 			LongIndex result = sysinfo(&info);
 			if (result == -1) return result;
 			return info.uptime;
 #else
+#	ifdef BALL_PLATFORM_WINDOWS
 			return -1;
-#endif
+#	else
+			return -1;
+#	endif
 #endif
 		}
 
 		Index getNumberOfProcessors()
 		{
-#ifdef BALL_PLATFORM_WINDOWS
+#ifdef BALL_HAS_SYSINFO
+			return get_nprocs();
+#else
+#	ifdef BALL_PLATFORM_WINDOWS
 			SYSTEM_INFO sysinfo;
 			GetSystemInfo(&sysinfo);
 			return sysinfo.dwNumberOfProcessors;
-#else
-#ifdef BALL_OS_LINUX
-			return get_nprocs();
-#else
+#	else
 			return -1;
-#endif
+#	endif
 #endif
 		}
 
 
 		LongIndex getFreeSwapSpace()
 		{
-#ifdef BALL_PLATFORM_WINDOWS
- 			MEMORYSTATUSEX statex;
-			GlobalMemoryStatusEx (&statex);
-			return (LongIndex) statex.ullAvailPageFile;
-#else
-#ifdef BALL_OS_LINUX
+#ifdef BALL_HAS_SYSINFO
 			struct sysinfo info;
 			LongIndex result = sysinfo(&info);
 			if (result == -1) return result;
 			return info.freeswap * info.mem_unit;
 #else
+#	ifdef BALL_PLATFORM_WINDOWS
+ 			MEMORYSTATUSEX statex;
+			GlobalMemoryStatusEx (&statex);
+			return (LongIndex) statex.ullAvailPageFile;
+#	else
 			return -1;
-#endif
+#	endif
 #endif
 		}
 
