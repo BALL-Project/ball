9d76e81f5e18d8c301bb37f616767a6e615362ac
amoll <amoll>
2005-03-10 16:57:39 +0000

    added support for solaris

--- source/SYSTEM/sysinfo.C
+++ source/SYSTEM/sysinfo.C
@@ -1,14 +1,28 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: sysinfo.C,v 1.14 2005/03/01 14:54:52 amoll Exp $
+// $Id: sysinfo.C,v 1.15 2005/03/10 16:57:39 amoll Exp $
 //
 
 #include <BALL/SYSTEM/sysinfo.h>
 
+//   #define BALL_PLATFORM_SOLARIS
+
+#ifdef BALL_PLATFORM_SOLARIS
+# undef BALL_HAS_SYS_SYSINFO_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <kstat.h>
+# include <sys/sysinfo.h>
+# include <sys/stat.h>
+# include <sys/swap.h>
+# include <sys/param.h>
+# include <unistd.h>
+#endif
+
 #ifdef BALL_HAS_SYS_SYSINFO_H
-#	include <sys/sysinfo.h>
-#	include <BALL/SYSTEM/file.h>
+#	 include <sys/sysinfo.h>
+#	 include <BALL/SYSTEM/file.h>
 #else
 # ifdef BALL_PLATFORM_WINDOWS
 #	  define WIN32_LEAN_AND_MEAN
@@ -16,7 +30,7 @@
 # endif
 # ifdef BALL_OS_DARWIN
 #  include <sys/sysctl.h>
-#	endif
+# endif
 #endif
 
 namespace BALL
@@ -131,12 +145,85 @@ namespace BALL
 		}
 
 #else
+#ifdef BALL_PLATFORM_SOLARIS
+
+	LongIndex getFreeMemory()
+	{
+	 	return ((LongIndex)sysconf(_SC_AVPHYS_PAGES)) * sysconf(_SC_PAGE_SIZE);
+	}
+
+	Index getNumberOfProcessors()
+	{
+		return sysconf(_SC_NPROCESSORS_CONF);
+	}
+
+	LongIndex getTotalMemory()
+	{
+		return ((LongIndex)sysconf(_SC_PHYS_PAGES)) * sysconf(_SC_PAGE_SIZE);
+	}
+
+	LongIndex getAvailableMemory()
+	{
+		return getFreeMemory();
+	}
+
+	LongIndex getFreeSwapSpace()
+	{
+		long pgsize_in_kbytes = sysconf(_SC_PAGE_SIZE) / 1024L;
+
+		int swap_count=swapctl(SC_GETNSWP, NULL);
+		if (swap_count == -1 || swap_count == 0) return swap_count;
+
+		/*
+		 *  Although it's not particularly clear in the documentation, you're
+		 *  responsible for creating a variable length structure (ie. the
+		 *  array is within the struct rather than being pointed to
+		 *  by the struct).  Also, it is necessary for you to allocate space
+		 *  for the path strings (see /usr/include/sys/swap.h).
+		 */
+		swaptbl_t* st = (swaptbl_t*)malloc(sizeof(int) + swap_count * sizeof(struct swapent));
+		if (st == NULL) return -1;
+
+		st->swt_n = swap_count;
+		for (int i=0; i < swap_count; i++) 
+		{
+			if ((st->swt_ent[i].ste_path = (char*)malloc(MAXPATHLEN)) == NULL)
+			{
+				return -1;
+			}
+		}
+
+		swap_count = swapctl(SC_LIST, (void*)st);
+		if (swap_count == -1) return -1;
+
+		long swap_avail = 0;
+		for (int i = 0; i < swap_count; i++) 
+		{
+			swap_avail += st->swt_ent[i].ste_free * pgsize_in_kbytes;
+		}
+
+		delete st;
+
+		return ((LongIndex)swap_avail) * 1024;
+	}
+
+	LongIndex getBufferedMemory()
+	{
+		return 0;
+	}
+
+	Time getUptime()
+	{
+		return -1;
+	}
+
+
+#else
 #ifdef BALL_OS_DARWIN
 
 		LongIndex getAvailableMemory()
 		{
-			LongIndex mem = getFreeMemory();
-			return mem;
+			return getFreeMemory();
 		}
 
 		LongIndex getFreeMemory()
@@ -207,6 +294,7 @@ namespace BALL
 			return -1;
 		}
 
+#endif
 #endif
 #endif
 #endif
