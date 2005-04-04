fae4bb6fa5759a31dd09512bd06909895e3e0b83
anhi <anhi>
2005-04-04 15:47:14 +0000

    First commit

---
+++ include/BALL/STRUCTURE/DOCKING/amberEvaluation.h
@@ -0,0 +1,59 @@
+#ifndef AMBEREVALUATION_H
+#define AMBEREVALUATION_H
+
+#include <BALL/STRUCTURE/DOCKING/forceFieldEvaluation.h>
+#include <BALL/MOLMEC/AMBER/amber.h>
+
+namespace BALL
+{
+		/** A class for energetic evaluators of docking results using
+		 *  an AMBER force field as scoring function.
+		 */
+		class AmberEvaluation : public ForceFieldEvaluation
+		{
+			public:
+
+				// Default constructor.
+				AmberEvaluation()
+					throw();
+				
+				AmberEvaluation(AmberFF& ff)
+					throw();
+
+				AmberEvaluation(AmberFF& ff, Options& option)
+					throw();
+
+				virtual ~AmberEvaluation()
+					throw();
+				
+				/** Operations
+				*/
+				void setForceField(ForceField& ff)
+					throw();
+
+				void setOptions(const Options& options)
+					throw();
+
+				ForceField& getForceField()
+					throw();
+
+				const ForceField& getForceField() const
+					throw();
+
+				Options& getOptions()
+					throw();
+
+				const Options& getOptions() const
+					throw();
+				
+				std::vector<ConformationSet::Conformation> operator () (const ConformationSet& conformations)
+					throw();
+
+			protected:
+
+				ForceField* ff_;
+				Options     options_;
+				bool 				delete_force_field_;
+		};
+}
+#endif
