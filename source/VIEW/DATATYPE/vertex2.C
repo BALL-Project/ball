8e7c7b234facecfd2de693f94dbd0e70e74c1dbd
amoll <amoll>
2005-04-18 13:30:00 +0000

    backport from V1_1 branch

--- source/VIEW/DATATYPE/vertex2.C
+++ source/VIEW/DATATYPE/vertex2.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: vertex2.C,v 1.5 2005/03/20 18:37:58 amoll Exp $
+// $Id: vertex2.C,v 1.6 2005/04/18 13:30:00 amoll Exp $
 
 #include <BALL/VIEW/DATATYPE/vertex2.h>
 
