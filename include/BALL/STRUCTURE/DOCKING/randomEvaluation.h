6425244ae3eb422ed85a7784615a7519a0908cbb
anhi <anhi>
2005-06-08 16:47:29 +0000

    Added

---
+++ include/BALL/STRUCTURE/DOCKING/randomEvaluation.h
@@ -0,0 +1,31 @@
+#ifndef ENERGETICEVALUATION_H
+#define ENERGETICEVALUATION_H
+
+#include <BALL/STRUCTURE/DOCKING/conformationSet.h>
+#include <BALL/DATATYPE/hashMap.h>
+
+#include <vector>
+
+namespace BALL
+{
+		/** A basis class for energetic evaluators of docking results.
+		 *	This trivial class will return the identity scoring, leaving
+		 *	permutation and score unchanged.
+		 */
+		class RandomEvaluation
+		{
+			public:
+
+				RandomEvaluation()
+					throw();
+				
+				virtual ~RandomEvaluation()
+					throw();
+				
+				/** Operations
+				*/
+				std::vector<ConformationSet::Conformation> operator () (const ConformationSet& conformations)
+					throw();
+		};
+}
+#endif
