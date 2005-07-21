ab6dd08aa8e09bafa9eed52e8d12a6c9ccf414ca
oliver <oliver>
2005-07-21 09:29:37 +0000

    *** empty log message ***

--- source/APPLICATIONS/NMRVIEW/nmrview.C
+++ source/APPLICATIONS/NMRVIEW/nmrview.C
@@ -1,12 +1,14 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-#include <BALL/VIEW/GUI/KERNEL/mainControl.h>
-#include <BALL/MOLVIEW/GUI/DIALOGS/openBruker2DFile.h>
-#include <BALL/VIEW/GUI/WIDGETS/regularData2DWidget.h>
+// $Id: nmrview.C,v 1.8.4.1 2005/07/21 09:29:37 oliver Exp $
+//
+
+#include <BALL/VIEW/KERNEL/mainControl.h>
+#include <BALL/VIEW/DIALOGS/openBruker2DFile.h>
+#include <BALL/VIEW/WIDGETS/regularData2DWidget.h>
 
 using namespace BALL::VIEW;
-using namespace BALL::MOLVIEW;
 
 int main(int argc, char **argv)
 {
