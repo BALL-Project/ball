5cb2fca5be03195cc49c590112eef0a73e524fd6
amoll <amoll>
2005-10-31 16:29:06 +0000

    no message

--- Windows/startBALLView.bat
+++ Windows/startBALLView.bat
@@ -1,5 +1,6 @@
 SET BALL_DATA_PATH=..\data
 SET LIBSDIR=$(BALL_PATH)\libs\
-SET PATH="%QTDIR%\lib;$(LIBSDIR)\libBALL\Release;$(LIBSDIR)\libView\Release;$(LIBSDIR)\Python Module\Release;$(BALL_PATH)\Windows\Applications\BALLVIEW\Release"
+SET RESULTDIR=Release
+SET PATH="%QTDIR%\lib;%LIBSDIR%\libBALL\%RESULT_DIR%;%LIBSDIR%\libView\%RESULT_DIR%;%LIBSDIR%\Python Module\%RESULT_DIR%;%BALL_PATH%\Windows\Applications\BALLVIEW\%RESULT_DIR%"
 echo using PATH %PATH%
 BALLView.exe 2> ballview.log
