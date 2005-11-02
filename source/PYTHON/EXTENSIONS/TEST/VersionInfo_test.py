700a32623c5229d3f30dd94eccbe9c7c70fc79c7
oliver <oliver>
2005-11-02 21:30:14 +0000

    *** empty log message ***

---
+++ source/PYTHON/EXTENSIONS/TEST/VersionInfo_test.py
@@ -0,0 +1,11 @@
+#! python
+from ClassTest import *
+
+START_TEST('<class name>', '$Id: VersionInfo_test.py,v 1.1.2.1 2005/11/02 21:30:14 oliver Exp $')
+
+CHECK('<class name>.<method name>()')
+#
+#
+RESULT()
+
+END_TEST()
