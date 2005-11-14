b574badccad1415435b1d9443702ba4be08e123c
amoll <amoll>
2005-11-14 14:55:30 +0000

    no message

--- source/EXAMPLES/PYTHON/simulated_annealing.py
+++ source/EXAMPLES/PYTHON/simulated_annealing.py
@@ -1,7 +1,11 @@
 #   example script for simulated annealing
 #   output heated structures as Hinfile
 #   one system has to be loaded before calling the script
+if getSystems() == []:
+  print "No System loaded! aborting..:"
+
 system = getSystem(0)
+
 ff = getMolecularStructure().getForceField()
 ff.setup(system)
 ssm = SnapShotManager()
@@ -12,43 +16,41 @@ mds.setReferenceTemperature(0)
 	
 time = 0
 starttemperature = 0
-endtemperature =600
+endtemperature = 600
 temperature = 0
 counter = 0
 step = 1.
 limit = 600
-heatlimit=1050
+heatlimit = 1050
 step = float(endtemperature - starttemperature) / float(limit)
 
 temperature = starttemperature
-while counter < limit:
-  mds.simulateIterations(50)
-  mds.setReferenceTemperature(temperature)
-  getMainControl().update(system)
-  counter = counter + 1
-  temperature = float(step)  * float(counter)
+for counter in range(0, limit):
+	mds.simulateIterations(50)
+	mds.setReferenceTemperature(temperature)
+	getMainControl().update(system)
+	getMainControl().processEvents(2000)
+	temperature = float(step) * float(counter)
 
-counter = 0
 temperature = endtemperature
-while counter < heatlimit:
-  mds.simulateIterations(1000)
-  mds.setReferenceTemperature(temperature)
-  getMainControl().update(system)
-  outfile = HINFile("heating1_"+str(counter)+".hin", File.OUT)
-  outfile.write(system)
-  outfile.close()
-  counter = counter + 1
+for counter in range(0, heatlimit):
+	mds.simulateIterations(1000)
+	mds.setReferenceTemperature(temperature)
+	getMainControl().update(system)
+	getMainControl().processEvents(2000)
+	outfile = HINFile("heating1_"+str(counter)+".hin", File.MODE_OUT)
+	outfile.write(system)
+	outfile.close()
 
 getMainControl().update(system)
 
-counter = limit
 temperature = endtemperature
-while counter > 0:
- mds.simulateIterations(50)
- mds.setReferenceTemperature(temperature)
- getMainControl().update(system)
- counter = counter - 1
- temperature = float(step) * float(counter)
+for counter in range(limit, 0):
+	mds.simulateIterations(50)
+	mds.setReferenceTemperature(temperature)
+	getMainControl().update(system)
+	getMainControl().processEvents(2000)
+	temperature = float(step) * float(counter)
 
 getMainControl().update(system)
 print ff.updateEnergy()
