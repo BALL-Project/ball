90590a1429994c6c0326dcd7b034d0c69c5e557f
anhi <anhi>
2006-01-04 14:53:02 +0000

    Added dock dialogs

---
+++ include/BALL/VIEW/DIALOGS/dockDialog.h
@@ -0,0 +1,362 @@
+//   // -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+
+#ifndef BALL_VIEW_DIALOGS_DOCKDIALOG_H
+#define BALL_VIEW_DIALOGS_DOCKDIALOG_H
+
+#ifndef BALL_VIEW_KERNEL_PREFERENCESENTRY_H
+# include <BALL/VIEW/KERNEL/preferencesEntry.h>
+#endif
+
+#ifndef BALL_FORMAT_INIFILE_H
+#include <BALL/FORMAT/INIFile.h>
+#endif
+
+#ifndef BALL_KERNEL_SYSTEM_H
+#include <BALL/KERNEL/system.h>
+#endif
+
+#ifndef BALL_MOLMEC_COMMON_RADIUSRULEPROCESSOR_H
+# include <BALL/MOLMEC/COMMON/radiusRuleProcessor.h>
+#endif
+
+#ifndef BALL_MOLMEC_COMMON_CHARGERULEPROCESSOR_H
+# include <BALL/MOLMEC/COMMON/chargeRuleProcessor.h>
+#endif
+
+#ifndef BALL_STRUCTURE_DEFAULTPROCESSORS_H
+# include <BALL/STRUCTURE/defaultProcessors.h>
+#endif
+
+#ifndef BALL_DATATYPE_OPTIONS_H
+# include <BALL/DATATYPE/options.h>
+#endif
+
+#include "dockDialogData.h"
+#include "dockResult.h"
+#include "dockProgressDialog.h"
+//#include "dockingController.h"
+
+
+namespace BALL
+{
+	namespace VIEW
+	{
+		class DockingController;
+		
+	
+		/**	Dialog for docking / redocking two systems. 
+		  * In this dialog the user choose the two docking partners, the docking algorithm and the scoring function.
+			* Furthermore he can set the options for the algorithm and scroing function and can apply processors to the docking parnters.
+			* For redocking he can specify the euler angles.
+    		\ingroup  ViewDialogs
+		 */
+		class BALL_EXPORT DockDialog : 
+			public DockDialogData,
+			public PreferencesEntry
+		{ 
+			Q_OBJECT
+			
+			public:
+			
+				/**	@name	Constructors and Destructors
+				 */	
+				//@{
+			
+				/** Default Constructor.
+				 *	Calls  \link PreferencesEntry::registerObject_ PreferencesEntry::registerObject_ \endlink.
+				 *	Sets flag \link DockDialog::is_redock_ is_redock_ \endlink to false.
+				 *	@param      parent the parent widget of the DockDialog
+				 *	@param      name the name of the DockDialog
+				 *	@param			modal the modal flag
+				 *	@param			fl the widget flags
+				 *	@see        QDialog
+				 *	@see				PreferncesEntry
+				 */
+				DockDialog(QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0)
+					throw();
+					
+				/** Destructor.
+				 */
+				virtual ~DockDialog()
+					throw();
+				//@}
+
+				/**	@name	Assignment
+				 */	
+				//@{
+				
+				/**  Assignment operator
+				 */
+				const DockDialog& operator =(const DockDialog& dock_dialog)
+					throw();
+				//@}
+					
+				/**	@name	Accessors: inspectors and mutators
+			 	 */
+				//@{
+				
+				/** Sets two systems as docking partners.
+				 *	@param      system1 first docking partner
+				 *	@param      system2 second docking partner
+				 */
+				void setSystems(System* system1, System* system2)
+					throw();
+				
+				/**  Get first docking partner.
+				 */
+				System* getSystem1()
+					throw();
+				
+				/**  Get second docking partner.
+				 */
+				System* getSystem2()
+					throw();
+				
+				/** Get algorithm options.
+				 */
+				Options& getAlgorithmOptions()
+					throw();
+				
+				/** Get scoring options.
+				 */
+				Options& getScoringOptions()
+					throw();
+				
+				/** Sets the flags \link DockDialog::is_redock_ is_redock_ \endlink and \link DockDialog::has_changed_ has_changed_ \endlink.
+				 */
+				void isRedock(bool is_redock)
+					throw();
+					
+				/** Adds docking algorithm to combobox and its advanced option dialog to hashmap.
+				 *	@param      name the name of the algorithm
+				 *	@param      algorithm the value of enum DockingController::Algorithm
+				 *	@param      dialog pointer to an advanced option dialog
+				 */
+				// remark: score_func isn't enum type because we then have cyclic includes of DockDialog and DockingController
+				void addAlgorithm(const QString& name, const int algorithm, QDialog* dialog)
+					throw();
+					
+				/** Adds scoring function to combobox and its advanced option dialog to hashmap, if it has such a dialog.
+				 *	@param      name the name of the scoring function
+				 *	@param      score_func the value of enum DockingController::ScoringFunction
+				 *	@param      dialog pointer to an advanced option dialog
+				 */
+				// remark: score_func isn't enum type because we then have cyclic includes of DockDialog and DockingController
+				void addScoringFunction(const QString& name, const int score_func, QDialog* dialog=0)
+					throw();
+				
+				/**	Builds hashmaps for algorithm advanced option dialogs and for scoring function advanced option dialogs.
+				 *	Also builds hashmap with the allowed scoring functions for the different algorithms.
+				 *  Is called by \link DockingController::initializeWidget DockingController::initializeWidget \endlink.
+				 *	@see				addAlgorithm
+				 *	@see				addScoringFunction
+				 */
+				void initializeWidget()
+					throw();
+
+				/** Fetchs the preferences from the INIFile.
+					* Calls \link PreferencesEntry::readPreferenceEntries PreferencesEntry::readPreferenceEntries \endlink.
+					* Calls \link DockDialog::fetchPreferences_ fetchPreferences_ \endlink to read the redocking options.
+					* This method is called in \link DockingController::fetchPreferences DockingController::fetchPreferences \endlink.
+				 	*	@see    writePreferences
+				 	*/
+				void fetchPreferences(INIFile& file)
+					throw();
+				
+				/** Writes the preferences to the INIFile.
+					* Calls \link PreferencesEntry::writePreferenceEntries PreferencesEntry::writePreferenceEntries \endlink.
+				  * This method is called in \link DockingController::writePreferences DockingController::writePreferences \endlink.
+				  * @see    fetchPreferences
+				  */
+				void writePreferences(INIFile& file)
+					throw();
+				
+					
+				/** Resets the dialog to the standard values.
+				 */
+				void reset()
+					throw();
+				//@}
+					
+			public slots:
+	
+				/** Shows and raises the dialog.
+					* Dialog is adapted for docking / redocking.
+				 *	Calls \link fillSystemComboboxes_ fillSystemComboboxes_ \endlink in case of docking.
+				 */
+				void show();
+				
+				/** Is called when reset button is pressed.
+				 * Calls \link DockDialog::reset reset \endlink.
+				 */
+				void resetPressed();
+			
+				/** Is called when OK button is pressed.
+				 * 	For docking, it checks if two different systems are chosen.
+				 *  Hides the dialog and calls \link DockDialog::applyValues_ applyValues_ \endlink
+				 *  and \link DockDialog::applyProcessors_ applyProcessors_ \endlink.
+				 */
+				void okPressed();
+				
+				/** Is called when cancel button is pressed.
+				 *	Hides the dialog.
+				 */
+				void cancelPressed();
+				
+				/** Is called when advanced button for algorithm options is pressed.
+			 	 *	Shows corresponding options dialog.
+				 */
+				void algAdvancedPressed();
+				
+				/** Is called when advanced button for scoring function options is pressed.
+			 	 *	Shows corresponding options dialog, if it exits.
+			 	 */
+				void scoringAdvancedPressed();
+				
+				/** Is called when a system in the combobox is chosen as docking partner 1.
+				 *  Calls \link DockDialog::partnerChosen_ partnerChosen_ \endlink.
+				 */
+				void partner1Chosen();
+				
+				/** Is called when a system in the combobox is chosen as docking partner 2.
+				 *  Calls \link DockDialog::partnerChosen_ partnerChosen_ \endlink.
+				 */
+				void partner2Chosen();
+				
+				/** Is called when a scoring function in the combobox is chosen.
+				 *	If the chosen scoring function has advanced options, the advanced_button will be enabled.
+				 */
+				void scoringFuncChosen();
+				
+				/** Is called when an algorithm in the combobox is chosen.
+				 *	If the chosen algorithm has advanced options, the advanced_button will be enabled.
+				 */
+				void algorithmChosen();
+				
+				/** Is called when browse button to get a charges config file from table is pressed.
+					*  Calls \link DockDialog::selectFile_ selectFile_ \endlink.
+				 */
+				void browseChargesData();
+				
+				/** Is called when browse button to get a charges config file by rules is pressed.
+				 *  Calls \link DockDialog::selectFile_ selectFile_ \endlink.
+				 */
+				void browseChargesRules();
+				
+				/** Is called when browse button to get a radii config file from table is pressed.
+				 *  Calls \link DockDialog::selectFile_ selectFile_ \endlink.
+				 */
+				void browseRadiiData();
+				
+				/** Is called when browse button to get a radii config file by rules s pressed.
+				 *  Calls \link DockDialog::selectFile_ selectFile_ \endlink.
+				 */
+				void browseRadiiRules();
+				
+				
+			protected:
+			
+				/** Sets options \link DockDialog::algorithm_opt_ algorithm_opt_ \endlink and 
+				  * \link DockDialog::scoring_opt_ scoring_opt_ \endlink with values the user has chosen.  
+				 */
+				void applyValues_() throw();
+				
+				/** Applies processors to the systems.
+				 */
+				bool applyProcessors_() throw();
+				
+				/** Shows chosen file in the dialog.
+				 */
+				void selectFile_(QLineEdit& lineedit) throw();
+					
+				/** Fills the system comboboxes.
+				 *  If the user has already selected one or two systems, they are the current items in the comboboxes. 
+				 */
+				void fillSystemComboboxes_() throw();
+				
+				/** Reads the redocking values from INIFile into vector backup_.
+				 	*	If INIFile has not yet a section <b> REDOCKING </b>, fill vector \link DockDialog::backup_ backup_ \endlink with default values.
+				 	*	@param    	file the INIFile that is read
+					*	@param     	entry key of entry that is read
+					*	@param    	default_value default value
+					* @see				fetchPreferences
+				 */
+				void fetchPreferences_(INIFile& file, const String& entry, const QString& default_value) throw();
+			
+				/** Swaps the option values between vector backup_ and dialog.
+				 *  Is called in \link DockDialog::show show \endlink if \link DockDialog::has_changed_ has_changed_ \endlink is true
+				 *  and in \link DockDialog::writePreferences writePreferences \endlink if \link DockDialog::is_redock_ is_redock_ \endlink is true
+				 */
+				void swapValues_() throw();
+				
+				
+			private:
+				
+				/** Copy constructor.
+					* Remark: Copy contructor is private because it is not completed. 
+					* The copy constuctor of the QT widgets is private and cannot be called.  
+					*/
+				DockDialog(const DockDialog& dock_dialog)
+					throw();
+			
+				/** Flag which indicates if we do docking or redocking.
+					*/
+				bool is_redock_;
+				
+				/** Flag:
+					* True if we do docking and did redocking before or otherwise.
+					* False if we do (re)docking and also did (re)docking before.
+					*/
+				bool has_changed_;
+			
+				/** key: DockingController::Algorithm
+				 *  value: advanced options dialog
+				 */
+				HashMap<int, QDialog*> algorithm_dialogs_;
+				
+				/** key: DockingController::ScoringFunction
+				 *  value: advanced options dialog
+				 */
+				HashMap<int, QDialog*> scoring_dialogs_;
+			
+				/** key: DockingController::Algorithm
+				 *  value: vector of scoring functions which can be used with this algorithm
+				 */
+				HashMap<int, vector<int> > allowed_sf_;
+				
+			  /** vector contains pointers to all systems that are loaded into BALLView
+			   */
+			  vector<System*> loaded_systems_;
+
+				/** Pointer to docking partner 1
+				 */
+				System* docking_partner1_;
+				
+				/** Pointer to docking partner 2
+				 */
+				System* docking_partner2_;	
+				
+				/** Options for the docking algorithm and scoring function
+				 */
+				Options algorithm_opt_, scoring_opt_;
+		
+				/** Needed to guarantee that both, docking and redocking preferences can be written to INIFile
+					* When we do docking, redocking values are in the vector and when we do redocking, the docking values are in there.
+					* In \link DockDialog::fetchPreferences fetchPreferences \endlink, we read the last redocking values from INIFile in this vector
+					* and in \link DockDialog::writePreferences writePreferences \endlink, we write the redocking values in INIFile from this vector
+					*/
+				vector<QString> backup_;
+				
+				/** Processors
+				 */
+				RadiusRuleProcessor 		radius_rule_processor_;
+				ChargeRuleProcessor 		charge_rule_processor_;
+				AssignRadiusProcessor 	radius_processor_;
+				AssignChargeProcessor 	charge_processor_;
+		};
+			
+	} // end of namespace View
+} // end of namespace BALL
+#endif
---
+++ source/VIEW/DIALOGS/dockProgressDialog.C
@@ -0,0 +1,199 @@
+// $Id: dockProgressDialog.C,v 1.1.2.1 2006/01/04 14:53:02 anhi Exp $
+//
+
+#include "dockProgressDialog.h"
+
+#include <qprogressbar.h>
+#include <qtextedit.h>
+#include <qpushbutton.h>
+#include <qmessagebox.h>
+#include <qlabel.h>
+
+//#define BALL_VIEW_DEBUG
+
+namespace BALL
+{
+  namespace VIEW
+  {
+    // Constructor
+    DockProgressDialog::DockProgressDialog(QWidget* parent,  const char* name, bool modal, WFlags fl)
+      throw()
+      : DockProgressDialogData(parent, name, modal, fl),
+	alg_(0)
+    {
+#ifdef BALL_VIEW_DEBUG
+      Log.info() << "new DockProgressDialog " << this << std::endl;
+#endif
+      
+      connect(&timer_, SIGNAL(timeout()), SLOT(updateProgress_()));
+    }
+    
+    // Copy constructor.
+    DockProgressDialog::DockProgressDialog(const DockProgressDialog& dock_prog_dialog)
+    throw()
+    : //DockProgressDialogData(dock_prog_dialog),
+    alg_(dock_prog_dialog.alg_),
+    //timer_(dock_prog_dialog.timer_),
+    start_time_(dock_prog_dialog.start_time_)
+    {}
+    
+    // Destructor	
+    DockProgressDialog::~DockProgressDialog()
+      throw()
+    {
+#ifdef BALL_VIEW_DEBUG
+      Log.info() << "Destructing object " << this << " of class DockProgressDialog" << std::endl;
+#endif 
+    }
+    
+    // Assignment operator
+    const DockProgressDialog& DockProgressDialog::operator =(const DockProgressDialog& dock_prog_dialog)
+      throw()
+    {
+      if (&dock_prog_dialog != this)
+	{
+	  alg_ = dock_prog_dialog.alg_;
+	  //timer_ = dock_prog_dialog.timer_;           // QTimer::operator=(const QTimer&)' is private !
+	  start_time_ = dock_prog_dialog.start_time_;
+	}
+      return *this;
+    }
+    
+    void DockProgressDialog::setDockingAlgorithm(DockingAlgorithm* alg)
+      throw()
+    {
+      alg_ = alg;
+    }
+    
+    const DockingAlgorithm* DockProgressDialog::getDockingAlgorithm() const
+      throw()
+    {
+      return alg_;
+    }
+    
+    // Fill ProgressDialog with information about the chosen
+    // docking partners, algorithm, scoring function and options
+    void DockProgressDialog::fillDialog(const QString& p1, const QString& p2, const QString& alg, const QString& sf,
+					const Options& alg_opt, const Options& sf_opt)
+      throw()
+    {
+      QString s = "Docking partner 1: ";
+      general_params->append(s.append(p1));
+      s = "Docking partner 2: ";
+      general_params->append(s.append(p2));
+      s = "Algorithm: ";
+      general_params->append(s.append(alg));
+      s = "Scoring function: ";
+      general_params->append(s.append(sf));
+      
+      options->append("*** Options of algorithm ***");
+      Options::ConstIterator it = alg_opt.begin();
+      for (; +it; ++it)
+			{
+				s = it->first;
+				s.append(" : ");
+				options->append(s.append(it->second));
+			}
+      
+      if (sf_opt.isEmpty())
+			{
+				options->append("\nThere are no options for this scoring function.");
+			}
+      else
+			{
+				options->append("\n*** Options of scoring function ***");
+				it = sf_opt.begin();
+				for (; +it; ++it)
+					{
+						s = it->first;
+						s.append(" : ");
+						options->append(s.append(it->second));
+					}
+			}
+    }
+    
+    //
+    void DockProgressDialog::show()
+    {
+      // start timer, true -> it is a single shot
+      timer_.start(1000, true);
+      
+      // remember start time
+      start_time_ = QDateTime::currentDateTime();
+      
+      // show dialog to user
+      DockProgressDialogData::show();
+    }
+    
+    // TODO: pause algorithm!!!
+    void DockProgressDialog::pauseClicked()
+    {
+      if(pause_button->text() == "Pause")
+			{
+				pause_button->setText("Continue");
+				alg_->pause();
+			}
+      else
+			{
+				pause_button->setText("Pause");
+				alg_->proceed();    
+			}
+    }
+    
+    //
+    void DockProgressDialog::abortClicked()
+		{
+			alg_->abort();
+			//dialog is closed and deleted
+			close(true);
+		}
+		
+		//
+		void DockProgressDialog::updateProgress_()
+		{
+			if (alg_->wasAborted()) return;
+			
+			float progress = alg_->getProgress();
+			// set progress
+			progress_bar->setProgress((int)(progress * 100.0), 100);
+			// calculate remaining time
+			int run_time = start_time_.secsTo(QDateTime::currentDateTime());
+			int remain_time, hours, min, sec;
+			QString s;
+			if (progress > 0.01)
+			{
+				remain_time = (int)(((1.0 - progress)/progress) * run_time);
+				hours = remain_time / 3600;
+				min = (remain_time % 3600) / 60;
+				sec = (remain_time % 3600) % 60;
+				QString convert;
+				s.setNum(hours);
+				s.append(":");
+				if (!(min/10))
+				{
+				 	s.append("0");
+				}
+				s.append(convert.setNum(min));
+				s.append(":");
+				if (!(sec/10))
+				{
+				 	s.append("0");
+				}
+				s.append(convert.setNum(sec));
+			}
+			else
+			{
+			 	s.append("--:--:--");
+			}
+			remaining_time->setText(s);
+			
+			// if docking has finished, close dialog
+			// else restart timer
+			if (!alg_->hasFinished())
+			{
+			 	timer_.start(1000, true);
+			}
+		}
+		
+	} // end of namespace View
+} // end of namespace BALL
