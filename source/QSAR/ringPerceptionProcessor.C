dcc94ba771df14a8b01d724dfdcb97db600aaa0b
amoll <amoll>
2006-02-08 22:42:05 +0000

    backport

--- source/QSAR/ringPerceptionProcessor.C
+++ source/QSAR/ringPerceptionProcessor.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: ringPerceptionProcessor.C,v 1.10.2.1 2006/01/30 12:17:38 amoll Exp $
+// $Id: ringPerceptionProcessor.C,v 1.10.2.2 2006/02/08 22:42:05 amoll Exp $
 //
 
 #include <BALL/QSAR/ringPerceptionProcessor.h>
@@ -588,9 +588,19 @@ namespace BALL
 	HashMap<Size, EdgeItem<Index, Index> *> RingPerceptionProcessor::index_to_bond;
 	vector<BitVector> RingPerceptionProcessor::rings;
 	vector<BitVector> RingPerceptionProcessor::matrix;
+	vector<BitVector> RingPerceptionProcessor::forwarded_rings_;
+	vector<BitVector> RingPerceptionProcessor::tested_beers_;
 
 	void RingPerceptionProcessor::TNode::recieve()
 	{
+#ifdef BALL_QSAR_RINGPERCEPTIONPROCESSOR_DEBUG
+		cerr << "size of recieve buffer is " << recieve_buffer.size() << endl;
+		for (vector<PathMessage>::iterator it1 = recieve_buffer.begin(); it1 != recieve_buffer.end(); ++it1)
+		{
+			cerr << it1->beep << " (" << it1->beep.countValue(true) << ")" << endl;
+		}
+#endif
+
 		vector<BitVector> do_not_forward;
 
 		// build the A array
@@ -609,6 +619,7 @@ namespace BALL
 				{
 					vector<PathMessage> new_message;
 					new_message.push_back(it2->second[0]);
+					
 					for (Size i=1;i!=it2->second.size();++i)
 					{
 						do_not_forward.push_back(it2->second[i].beep);
@@ -629,9 +640,10 @@ namespace BALL
 				for(++it3; +it3; ++it3)
 				{
 					BitVector beer = it2->second[0].beep | it3->second[0].beep;
-					do_not_forward.push_back(it2->second[0].beep);
+					
 					do_not_forward.push_back(it3->second[0].beep);
-					BalducciPearlmanRingSelector_(beer);
+					do_not_forward.push_back(it2->second[0].beep);
+					forwarded_rings_.push_back(beer);
 				}
 				array_B[it2->first].push_back(it2->second[0]);
 			}
@@ -647,7 +659,7 @@ namespace BALL
 					BitVector beer = it2->beep | it3->beep;
 					do_not_forward.push_back(it2->beep);
 					do_not_forward.push_back(it3->beep);
-					BalducciPearlmanRingSelector_(beer);
+					forwarded_rings_.push_back(beer);
 				}
 			}
 		}
@@ -676,13 +688,16 @@ namespace BALL
 
 	void RingPerceptionProcessor::TNode::send()
 	{
+#ifdef BALL_QSAR_RINGPERCEPTIONPROCESSOR_DEBUG
+		cerr << "size of send buffer is " << send_buffer.size() << endl;
+#endif
 		for (Size i=0;i!=send_buffer.size();++i)
 		{
 			PathMessage pm = send_buffer[i];
-			NodeItem<Index, Index> * a = tnode_to_atom[this];
+			NodeItem<Index, Index>* a = tnode_to_atom[this];
 			for (NodeItem<Index, Index>::Iterator bit=a->begin(); bit!=a->end(); ++bit)
 			{
-				TNode * node = 0;
+				TNode* node = 0;
 				// determine which node
 				if (&(*bit)->getSource() == a)
 				{
@@ -719,6 +734,14 @@ namespace BALL
 
 	void RingPerceptionProcessor::BalducciPearlmanRingSelector_(BitVector beer)
 	{
+#ifdef BALL_QSAR_RINGPERCEPTIONPROCESSOR_DEBUG
+		cerr << "new beer: " << beer << " (" << beer.countValue(true) << ")" << endl;
+		cerr << "matrix before: " << matrix.size() << endl;
+		for (vector<BitVector>::const_iterator it = matrix.begin(); it != matrix.end(); ++it)
+		{
+			cerr << *it << " (" << it->countValue(true) << ")" << endl;
+		}
+#endif
 		// linear independency tests
 		if (rings.size() ==  0)
 		{
@@ -726,27 +749,96 @@ namespace BALL
 			matrix.push_back(beer);
 			return;
 		}
-		// walk through all columns of the new entry
-		for (Size i=0;i!=beer.getSize();++i)
+
+		// 1. perform gaussian elimination
+		vector<BitVector> tmp_matrix = matrix;
+		//tmp_matrix.push_back(beer);
+		BitVector new_beer = beer;
+		Size hi_bit(0);
+		Size r_begin(0);
+		for (Size i = 0; i != new_beer.getSize(); ++i)
 		{
-			// has bit set?
-			if (beer[i])
+			if (new_beer[i])
 			{
-				for (Size j=0;j!=matrix.size();++j)
+				for (Size r = r_begin; r < tmp_matrix.size(); ++r)
 				{
-					if (matrix[j][i])
+					for (Size c = 0; c != tmp_matrix[r].getSize(); ++c)
 					{
-						break;
+						if (tmp_matrix[r][c])
+						{
+							hi_bit = c;
+							break;
+						}
 					}
-					if (j == matrix.size()-1 && !matrix[j][i])
+					if (i == hi_bit)
 					{
-						rings.push_back(beer);
-						matrix.push_back(beer);
-						return;
+						r_begin = r+1;
+						new_beer ^= tmp_matrix[r];
+						break;
 					}
 				}
 			}
 		}
+
+		if (new_beer.countValue(true) == 0)
+		{
+			return;
+		}
+		
+		// if linearly independent add to the matrix
+		// sort matrix w.r.t highest bit (= maintain echelon format)
+		tmp_matrix = matrix;
+		vector<BitVector> new_matrix;
+		vector<BitVector> processed;
+		tmp_matrix.push_back(new_beer);
+		for (Size i = 0; i != tmp_matrix[0].getSize(); ++i)
+		{
+			for (Size j = 0; j != tmp_matrix.size(); ++j)
+			{
+				if (find(processed.begin(), processed.end(), tmp_matrix[j]) == processed.end() && tmp_matrix[j][i])
+				{
+					new_matrix.push_back(tmp_matrix[j]);
+					processed.push_back(tmp_matrix[j]);
+				}
+			}
+		}
+	
+#ifdef BALL_QSAR_RINGPERCEPTIONPROCESSOR_DEBUG	
+		cerr << "sorted matrix: " << new_matrix.size() << endl;
+		for (vector<BitVector>::const_iterator it = new_matrix.begin(); it != new_matrix.end(); ++it)
+		{
+			cerr << *it << " (" << it->countValue(true) << ")" << endl;
+		}
+#endif
+	
+/*
+		// 2. maintain gauss form
+		hi_bit = 0;
+		// for each column
+		for (Size c = 0; c != new_matrix[0].getSize(); ++c)
+		{
+			// for each row
+			for (Size r = c + 1; r < new_matrix.size(); ++r)
+			{
+				// find highest bit
+				for (Size j = 0; j != new_matrix[r].getSize(); ++j)
+				{
+					if (new_matrix[r][j])
+					{
+						hi_bit = j;
+						break;
+					}
+				}
+				if (hi_bit == c)
+				{
+					new_matrix[r] = new_matrix[c] ^ new_matrix[r];
+				}
+			}			
+		}
+*/
+		
+		matrix = new_matrix;
+		rings.push_back(beer);
 	}
 
 	Size RingPerceptionProcessor::BalducciPearlmanAlgorithm_(vector<vector<Atom*> >& sssr, MolecularGraph& graph)
@@ -761,7 +853,9 @@ namespace BALL
 		tnode_to_atom.clear();
 		rings.clear();
 		matrix.clear();
-	
+		forwarded_rings_.clear();
+		tested_beers_.clear();
+		
 		// 1. init the flow-network
 
 		// do the node to tnode mapping
@@ -791,7 +885,7 @@ namespace BALL
 				// set the bit for the first (outgoing) edge
 				beep.toggleBit(bond_to_index[*bit]);
 				pm.beep = beep;
-				TNode * tnode = 0;
+				TNode* tnode = 0;
 				// determine which node to set
 				if ((*bit)->getSource() == *ait)
 				{
@@ -813,10 +907,14 @@ namespace BALL
 		Size num_rings = num_bonds - num_atoms + 1;
 
 		// the nodes are forced to talk until they get enough rings
-		Size count(0);
+		Size count(1);
 		while (rings.size() < num_rings)
 		{
 			count++;
+
+#ifdef BALL_QSAR_RINGPERCEPTIONPROCESSOR_DEBUG
+			cerr << count << ". round (" << rings.size() << " of " << num_rings << ")" << endl;
+#endif
 			
 			// calling all sends 
 			for (MolecularGraph::NodeIterator ait=graph.beginNode(); ait!=graph.endNode(); ++ait)
@@ -828,8 +926,38 @@ namespace BALL
 			{
 				atom_to_tnode[&*ait]->recieve();
 			}
-		
-			// this is just a workaround, due to deficiencies of the balducci pearlman algorithm
+
+			// now invoke the BalducciPearlmanRingSelector_ which selects the correct rings of size > 2 * count -2
+			// first process rings of size 2 * count - 1 (odd sized rings of this phase)
+			vector<BitVector> even_sized;
+			for (vector<BitVector>::iterator it = forwarded_rings_.begin(); it != forwarded_rings_.end(); ++it)
+			{
+				if (it->countValue(true) == 2 * count - 1)
+				{
+					if (find(tested_beers_.begin(), tested_beers_.end(), *it) == tested_beers_.end())
+					{
+						tested_beers_.push_back(*it);
+						BalducciPearlmanRingSelector_(*it);
+					}
+				}
+				else
+				{
+					even_sized.push_back(*it);
+				}
+			}
+			// now process the even-sized rings
+			for (vector<BitVector>::const_iterator it = even_sized.begin(); it != even_sized.end(); ++it)
+			{
+				if (find(tested_beers_.begin(), tested_beers_.end(), *it) == tested_beers_.end())
+				{
+					tested_beers_.push_back(*it);
+					BalducciPearlmanRingSelector_(*it);
+				}
+			}
+			// clean up for next round
+			forwarded_rings_.clear();
+			
+			// this is just a workaround, due to deficiencies of the balducci pearlman algorithm TODO needed any more?
 			if (count > BALL_QSAR_RINGPERCEPTIONPROCESSOR_MAX_RUNS)
 			{
 				break;
