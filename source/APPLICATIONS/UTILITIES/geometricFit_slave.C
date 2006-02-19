91ef156dd35c2bd357b634e4575780ac8b862076
anhi <anhi>
2006-02-19 14:56:58 +0000

    *** empty log message ***

---
+++ source/APPLICATIONS/UTILITIES/geometricFit_slave.C
@@ -0,0 +1,17 @@
+/** This should be spawned by geometricFit **/
+#include <BALL/STRUCTURE/DOCKING/geometricFit.h>
+
+#include <iostream>
+
+using namespace BALL;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	printf("Woke up...\n");
+	// this is simple...
+	GeometricFit gf;
+	gf.MPI_Slave_start(argc, argv);
+
+	return 0;
+}
