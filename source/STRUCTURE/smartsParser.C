780d5603bfade1f097187936fb87149c5ce479bb
bertsch <bertsch>
2006-02-27 18:59:42 +0000

    fixed wrong aromaticity property

--- source/STRUCTURE/smartsParser.C
+++ source/STRUCTURE/smartsParser.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: smartsParser.C,v 1.8 2006/02/27 12:26:04 bertsch Exp $
+// $Id: smartsParser.C,v 1.9 2006/02/27 18:59:42 bertsch Exp $
 //
 
 #include <BALL/STRUCTURE/smartsParser.h>
@@ -223,14 +223,17 @@ namespace BALL
 			{
 				s = PTE[symbol.toUnsignedInt()].getSymbol();
 			}
-			if (islower(symbol[0]))
-			{
-				s.toUpper(0, 1);
-				setProperty("Aromatic", true);
-			}
 			else
 			{
-				setProperty("Aliphatic", true);
+				if (islower(symbol[0]))
+				{
+					s.toUpper(0, 1);
+					setProperty("Aromatic", true);
+				}
+				else
+				{
+					setProperty("Aliphatic", true);
+				}
 			}
 			setProperty("Symbol", s);
 #ifdef SMARTS_PARSER_DEBUG
@@ -645,6 +648,7 @@ namespace BALL
 			// in ring of size
 			if (property_name == "InRingSize")
 			{
+				
 				if (!atom->getProperty("InRing").getBool())
 				{
 					return false;
