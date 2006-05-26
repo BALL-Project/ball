6ca3374740839600f78688a2e39b32794aadfaca
leonhardt <leonhardt>
2006-05-26 14:52:30 +0000

    forgotten to commit

---
+++ include/BALL/VIEW/DIALOGS/evolutionDockingDialog.h
@@ -0,0 +1,199 @@
+//   // -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+
+#ifndef BALL_VIEW_DIALOGS_EVOLUTIONDOCKINGDIALOG_H
+#define BALL_VIEW_DIALOGS_EVOLUTIONDOCKINGDIALOG_H
+
+#ifndef BALL_VIEW_KERNEL_PREFERENCESENTRY
+# include <BALL/VIEW/KERNEL/preferencesEntry.h>
+#endif
+
+#ifndef BALL_DATATYPE_OPTIONS_H
+#include <BALL/DATATYPE/options.h>
+#endif
+
+#include <BALL/VIEW/UIC/evolutionDockingDialogData.h>
+
+namespace BALL
+{
+	namespace VIEW
+	{
+		/**	Dialog for options of the docking algorithm EvolutionDocking.
+    		\ingroup  ViewDialogs
+				@see EvolutionDocking
+		 */
+		class BALL_VIEW_EXPORT EvolutionDockingDialog : 
+			public QDialog,
+			public Ui_EvolutionDockingDialogData,
+			public PreferencesEntry
+		{ 
+			Q_OBJECT
+			
+			public:
+			
+				
+				/**	@name	Constructors and Destructors
+				 */	
+				//@{
+			
+				/** Default Constructor.
+				 *	Calls \link PreferencesEntry::registerObject_ PreferencesEntry::registerObject_ \endlink
+				 *	@param      parent the parent widget of the EvolutionDockingDialog
+				 *	@param      name the name of the EvolutionDockingDialog
+				 *	@param			modal the modal flag
+				 *	@param			fl the widget flags
+				 *	@see        QDialog
+				 *	@see				PreferncesEntry
+				 */
+				EvolutionDockingDialog(QWidget* parent = 0, const char* name = 0)
+					throw();
+					
+				/** Destructor.
+				 */
+				virtual ~EvolutionDockingDialog()
+					throw();
+        //@}
+				
+				/**	@name	Assignment
+				 */
+				//@{
+				/**  Assignment operator
+				 */
+				const EvolutionDockingDialog& operator =(const EvolutionDockingDialog& ev_dock_dialog)
+					throw();
+				//@}
+					
+				/**	@name	Accessors: inspectors and mutators
+				 */
+				//@{
+				 
+				/** Fills options with values of the dialog.
+					*	@param      options the options that are filled
+					*/
+				void getOptions(Options& options)
+					throw();
+
+				/** Sets the flags \link EvolutionDockingDialog::is_redock_ is_redock_ \endlink and 
+				  * \link EvolutionDockingDialog::has_changed_ has_changed_ \endlink.
+					*/
+				void isRedock(bool is_redock)
+					throw();
+				
+				/** Fetchs the preferences from the INIFile.
+					* Calls \link PreferencesEntry::readPreferenceEntries PreferencesEntry::readPreferenceEntries \endlink.
+					* Calls \link EvolutionDockingDialog::fetchPreferences_ fetchPreferences_ \endlink to read the redocking options.
+					* This method is called in \link DockDialog::fetchPreferences DockDialog::fetchPreferences \endlink.
+				 	*	@see    writePreferences
+				 	*/
+				void fetchPreferences(INIFile& file)
+					throw();
+				
+				/** Writes the preferences to the INIFile.
+					* Calls \link PreferencesEntry::writePreferenceEntries PreferencesEntry::writePreferenceEntries \endlink.
+				 * This method is called in \link DockDialog::writePreferences DockDialog::writePreferences \endlink.
+				 * @see    fetchPreferences
+				 */
+				void writePreferences(INIFile& file)
+					throw();
+					
+				//@}
+					
+			public slots:
+				
+				/** Shows dialog to user.
+					*/
+				void show();
+			 
+			 	/** Is called when reset button is pressed.
+				 	* Calls \link EvolutionDockingDialog::reset reset \endlink.
+				 	*/
+				void reset();
+				
+				/** Is called when cancel button is pressed.
+				 *	Hides dialog.
+				 */
+				virtual void reject();
+
+				//
+				virtual void accept();
+
+				/** Is called when radio button for using a created grid is pressed. 
+				 	* It enables the browse button and to corresponding line edit.
+				 	*/
+				void enableFileBrowsing();
+
+				/** Is called when radio button for using a new grid is pressed. 
+				 	* It disables the browse button and to corresponding line edit.
+				 	*/
+				void disableFileBrowsing();
+
+				/** Is called when browse button is pressed. 
+				 	* It shows a file dialog.
+				 	*/
+				void browseGridFile();
+
+				/** Is called when the amber radio button is pressed. 
+		 			*/
+				void useAmberFF();
+
+				/** Is called when the charmm radio button is pressed. 
+		 			*/
+				void useCharmmFF();
+
+				/** Is called when force field options button is pressed. 
+					* It shows the force field option dialog.
+					*/
+		   void showForceFieldOptions();
+
+				
+			protected:
+			
+				/** Function to read the redocking options from INIFile into vector backup_.
+					* If INIFile has not yet section <b> GEOMETRIC_FIT_OPTIONS_REDOCK </b>, 
+					* fill vector \link DockDialog::backup_ backup_ \endlink with default values.
+					*	@param    	file the INIFile that is read
+					*	@param     	entry key of entry that is read
+					*	@param    	default_value default value
+					* @see				fetchPreferences
+					*/
+				void fetchPreferences_(INIFile& file, const String& entry, const QString& default_value) throw();
+				
+				/** Swaps the option values between vector backup_ and dialog.
+				 *  Is called in \link EvolutionDockingDialog::show show \endlink if \link EvolutionDockingDialog::has_changed_ has_changed_ \endlink 
+				 *  is true and in \link EvolutionDockingDialog::writePreferences writePreferences \endlink if 
+				 *  \link EvolutionDockingDialog::is_redock_ is redock_ \endlink is true.
+				 */
+				void swapValues_() throw();
+			
+				
+			private:
+			
+				/** Copy constructor.
+				 	* Remark: Copy contructor is private because it is not completed. 
+					* The copy constuctor of the QT widgets is private and cannot be called.    
+					*/
+				EvolutionDockingDialog(const EvolutionDockingDialog& ev_dock_dialog)
+					throw();
+			
+				/** Flag:
+					* True if we do docking and did redocking before or otherwise.
+					* False if we do (re)docking and also did (re)docking before.
+					*/
+				bool has_changed_;
+				
+				/** Flag which indicates if we do docking or redocking.
+					*/
+				bool is_redock_;
+				
+				/** Needed to guarantee that both, docking and redocking preferences can be written to INIFile
+					* When we do docking, redocking options are in the vector and when we do redocking, the docking options are in there.
+					* In \link EvolutionDockingDialog::fetchPreferences fetchPreferences \endlink, we read the last redocking options from INIFile in this vector 
+					* and in \link EvolutionDockingDialog::writePreferences writePreferences \endlink, we write the redocking options in INIFile from this vector
+					*/
+				vector<QString> backup_;
+
+		};
+		
+} } // Namespaces
+#endif
