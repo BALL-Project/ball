7e759cae23587fa928e64b83640875437fa6f983
oliver <oliver>
2006-06-08 09:26:30 +0000

    *** empty log message ***

--- source/BENCHMARKS/DataStructures_bench.C
+++ source/BENCHMARKS/DataStructures_bench.C
@@ -1,7 +1,10 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: DataStructures_bench.C,v 1.3 2005/12/23 17:02:31 amoll Exp $
+// $Id: DataStructures_bench.C,v 1.4 2006/06/08 09:26:30 oliver Exp $
+//
+// Author:
+//  Andreas Moll
 //
 
 #include <BALL/CONCEPT/benchmark.h>
@@ -11,35 +14,35 @@
 #include <BALL/DATATYPE/hashMap.h>
 #include <BALL/DATATYPE/hashSet.h>
 #include <utility>
-#include <stdlib.h>
+#include <algorithm>
+#include <map>
+// #include <ext/hash_map>
+
+#include <stdlib.h> // rand
+
+
 
 ///////////////////////////
 
 using namespace BALL;
 
-#ifdef BALL_PLATFORM_WINDOWS
-long int random()
+// Random number generator function
+Size randomGen()
 {
-	return rand();
+	return (Size)rand();
 }
-#endif
 
-START_BENCHMARK(DataStructures, 11.0, "$Id: DataStructures_bench.C,v 1.3 2005/12/23 17:02:31 amoll Exp $")
+START_BENCHMARK(DataStructures, 11.0, "$Id: DataStructures_bench.C,v 1.4 2006/06/08 09:26:30 oliver Exp $")
 
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
-HashSet<Size> hashed_numbers;
-Size MAX_NUMBERS = 500000;
-while (hashed_numbers.size() < MAX_NUMBERS)
-{
-	hashed_numbers.insert(Size(random()));
-}
+const Size MAX_NUMBERS = 500000;
 std::vector<Size> numbers(MAX_NUMBERS);
-std::copy(hashed_numbers.begin(), hashed_numbers.end(), numbers.begin());
+std::generate(numbers.begin(), numbers.end(), randomGen);
 
-START_SECTION(HashMap::insert, 1.0)
+START_SECTION(HashMap::insert(std::pair<key_type, value_type>), 1.0)
 	HashMap<Size, Size> hm;
 	for (Position i = 0; i < numbers.size(); ++i)
 	{
@@ -49,7 +52,7 @@ START_SECTION(HashMap::insert, 1.0)
 	}
 END_SECTION
 
-START_SECTION(HashMap::lookup, 1.0)
+START_SECTION(HashMap::operator [] (key_type), 1.0)
 	Size s;
 	for (Position i = 0; i < numbers.size(); ++i)
 	{
@@ -59,9 +62,9 @@ START_SECTION(HashMap::lookup, 1.0)
 	}
 END_SECTION
 
-/*
-START_SECTION(map::insert, 0.0)
-	__gnu_cxx::hash_map<Size, Size> m;
+
+START_SECTION(map::insert(std::pair<key_type, value_type>), 0.0)
+	std::map<Size, Size> m;
 	for (Position i = 0; i < numbers.size(); ++i)
 	{
 		START_TIMER
@@ -70,11 +73,31 @@ START_SECTION(map::insert, 0.0)
 	}
 END_SECTION
 
-START_SECTION(map::lookup, 0.0)
+START_SECTION(map::operator [] (key_type), 0.0)
 	for (Position i = 0; i < numbers.size(); ++i)
 	{
 		START_TIMER
 			s = m[numbers[i]];
+		STOP_TIMER
+	}
+END_SECTION
+
+/*
+START_SECTION(hash_map::insert, 0.0)
+	__gnu_cxx::hash_map<Size, Size> ghm;
+	for (Position i = 0; i < numbers.size(); ++i)
+	{
+		START_TIMER
+			ghm.insert(std::make_pair(numbers[i], i));
+		STOP_TIMER
+	}
+END_SECTION
+
+START_SECTION(hash_map::lookup, 0.0)
+	for (Position i = 0; i < numbers.size(); ++i)
+	{
+		START_TIMER
+			s = ghm[numbers[i]];
 		STOP_TIMER
 	}
 END_SECTION
