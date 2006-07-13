959e3941fbd9d00b1effb25d7be7574c30f235a5
amoll <amoll>
2006-07-13 22:06:20 +0000

    no message

--- source/EXAMPLES/PYTHON/cartoon_test2.py
+++ source/EXAMPLES/PYTHON/cartoon_test2.py
@@ -3,18 +3,18 @@ fd = MolecularFileDialog.getInstance(0)
 sc = Scene.getInstance(0)
 ms = MolecularStructure.getInstance(0)
 
-getMainControl().getPrimitiveManager().setMultithreadingMode(0)
+setMultithreading(0)
 	
 dp.setTransparency(0)
 dp.setDrawingPrecision(DRAWING_PRECISION_HIGH)
 dp.selectMode(DRAWING_MODE_SOLID)
-dp.selectColoringMethod(COLORING_ELEMENT)
+dp.selectColoringMethod(COLORING_SECONDARY_STRUCTURE)
 dp.selectModel(MODEL_CARTOON)
 
+showOnlyScene()
 fd.openFile("/var/tmp/test.pdb")
-
 sc.exportPNG("/var/tmp/test1.png")
 
-ms.calculateSecondaryStructure()
-sc.exportPNG("/var/tmp/test2.png")
-quit
+#ms.calculateSecondaryStructure()
+#sc.exportPNG("/var/tmp/test2.png")
+quit()
