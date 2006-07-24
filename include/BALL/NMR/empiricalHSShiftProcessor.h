812ee73309d34149fba566b4c8e037889c679c9c
Anna Katharina Dehof <anne@bioinf.uni-sb.de>
2006-07-24 16:26:53 +0000

    added bicubic spline to empiricalShift

--- include/BALL/NMR/empiricalHSShiftProcessor.h
+++ include/BALL/NMR/empiricalHSShiftProcessor.h
@@ -127,120 +127,165 @@ namespace BALL
 		//@}
 
 		protected:
-			
-				class BALL_EXPORT PropertiesForShift_
-				{
-					public:
-						PropertiesForShift_() throw();
+		
+	/** nested classe for computing Bicubic splines 
+ 	*/
+		class CubicSpline1D
+		{
+			public:
+				CubicSpline1D() {};
 
-						/** current target atom
-						 */
-						Atom*				atom;
-						/**	is the current atom in the first residue of a chain? 
-						 */
-						bool 				is_first_residue;
-						/** is the atom before in the first residue of a chain? 
-						 */
-						bool				prev_is_first_residue;
-						/**	is the atom behind in the first residue of a chain? 
-						 */
-						bool				next_is_first_residue;
-						
-						/** amino acid type of the (previous/following) residue of the atom in degree
-						 */
-						char	amino_acid_type;
-						char	prev_amino_acid_type;
-						char	next_amino_acid_type;
-						
-						/** secondary structure of the (previous/following) residue of the atom in degree
-						 */
-						char 	secondary_structure;
-						char  prev_secondary_structure;
-						char 	next_secondary_structure;
-						
-						/** psi angle of the (previous/following) residue of the atom in degree
-						 */
-						float				psi_angle;
-						float 			prev_psi_angle;
-						float 			next_psi_angle;
+				void createSpline(const std::vector<float>& sample_positions, 
+						const std::vector<float>& sample_values); // spline
+				float operator () (float x); // splint
 
-						/** phi angle of the (previous/following) residue of the atom in degree
-						 */
-						float				phi_angle;
-						float				prev_phi_angle;
-						float				next_phi_angle;
+				std::vector<float>& getCurvature();
+				void setCurvature(std::vector<float> curvature);
+				void setValues(std::vector<float> values);
+				void setPositions(std::vector<float> positions);
+			private :
+				std::vector<float> sample_positions_;
+				std::vector<float> sample_values_;
+				std::vector<float> curvature_;
+		};
 
-						/** chi angle of the (previous/following) residue of the atom in degree
-						 */
-						float				chi_angle;
-						float				prev_chi_angle;
-						float				next_chi_angle;
-						
-						/** chi2 angle of the (previous/following) residue of the atom in degree
-						 */
-						float				chi2_angle;
-						float				prev_chi2_angle;
-						float				next_chi2_angle;
 
-						/**	length of the HA-HBond of the (previous/following) residue
-						 */
-						float 			HA_bond_len;
-						float 			prev_HA_bond_len;
-						float 			next_HA_bond_len;
+		class CubicSpline2D
+		{
+			public:
+				CubicSpline2D() {};
 
-						/**	length of the HA2-HBond of the (previous/following) residue
-						 */
-						float 			HA2_bond_len;
-						float 			prev_HA2_bond_len;
-						float 			next_HA2_bond_len;
+				//precompute the second derivatives	
+				void createBiCubicSpline(const std::vector<std::vector<float> >& sample_positions_x,
+						const std::vector<float>& sample_positions_y,
+						const std::vector<std::vector<float> >& sample_values); // splie2
+				float operator () (float x, float y); // splin2
 
-						/**	length of the HN-HBond of the (previous/following) residue
-						 */
-						float 			HN_bond_len;
-						float 			prev_HN_bond_len;
-						float 			next_HN_bond_len;
-						
-						/**	length of the O-HBond of the (previous/following) residue
-						 */
-						float 			OH_bond_len;
-						float 			prev_OH_bond_len;
-						float 			next_OH_bond_len;
+			private :
+				std::vector< std::vector<float> > sample_positions_x_;
+				std::vector<float> sample_positions_y_;
+				std::vector<CubicSpline1D> splines_;
+		};
 
-						/**	has the (previous/following) residue a disulfid bond?
-						 */
-						bool 				has_disulfid_bond;
-						bool 				prev_has_disulfid_bond;
-						bool 				next_has_disulfid_bond;
-						
-						/**	has the (previous/following) residue a HA-HBond?
-						 */
-						bool 				has_HA_Hbond;
-						bool 				prev_has_HA_Hbond;
-						bool 				next_has_HA_Hbond;
 
-						/**	has the (previous/following) residue a HA2-HBond?
-						 */
-						bool 				has_HA2_Hbond;
-						bool 				prev_has_HA2_Hbond;
-						bool 				next_has_HA2_Hbond;
-						
-						/**	has the (previous/following) residue a HN-HBond?
-						 */
-						bool 				has_HN_Hbond;
-						bool 				prev_has_HN_Hbond;
-						bool 				next_has_HN_Hbond;
 
-						/**	has the (previous/following) residue a O-HBond?
-						 */
-						bool 				has_O_Hbond;
 
-						/** hydrogen bond status
-						 */
-						bool				hydrogen_bond_status;
 
-				}; // end of nested class
+		class BALL_EXPORT PropertiesForShift_
+		{
+			public:
+				PropertiesForShift_() throw();
+
+				/** current target atom
+				 */
+				Atom*				atom;
+				/**	is the current atom in the first residue of a chain? 
+				 */
+				bool 				is_first_residue;
+				/** is the atom before in the first residue of a chain? 
+				 */
+				bool				prev_is_first_residue;
+				/**	is the atom behind in the first residue of a chain? 
+				 */
+				bool				next_is_first_residue;
+
+				/** amino acid type of the (previous/following) residue of the atom in degree
+				 */
+				char	amino_acid_type;
+				char	prev_amino_acid_type;
+				char	next_amino_acid_type;
+
+				/** secondary structure of the (previous/following) residue of the atom in degree
+				 */
+				char 	secondary_structure;
+				char  prev_secondary_structure;
+				char 	next_secondary_structure;
+
+				/** psi angle of the (previous/following) residue of the atom in degree
+				 */
+				float				psi_angle;
+				float 			prev_psi_angle;
+				float 			next_psi_angle;
+
+				/** phi angle of the (previous/following) residue of the atom in degree
+				 */
+				float				phi_angle;
+				float				prev_phi_angle;
+				float				next_phi_angle;
+
+				/** chi angle of the (previous/following) residue of the atom in degree
+				 */
+				float				chi_angle;
+				float				prev_chi_angle;
+				float				next_chi_angle;
+
+				/** chi2 angle of the (previous/following) residue of the atom in degree
+				 */
+				float				chi2_angle;
+				float				prev_chi2_angle;
+				float				next_chi2_angle;
+
+				/**	length of the HA-HBond of the (previous/following) residue
+				 */
+				float 			HA_HBond_len;
+				float 			prev_HA_HBond_len;
+				float 			next_HA_HBond_len;
+
+				/**	length of the HA2-HBond of the (previous/following) residue
+				 */
+				float 			HA2_HBond_len;
+				float 			prev_HA2_HBond_len;
+				float 			next_HA2_HBond_len;
+
+				/**	length of the HN-HBond of the (previous/following) residue
+				 */
+				float 			HN_HBond_len;
+				float 			prev_HN_HBond_len;
+				float 			next_HN_HBond_len;
+
+				/**	length of the O-HBond of the (previous/following) residue
+				 */
+				float 			O_HBond_len;
+				float 			prev_O_HBond_len;
+				float 			next_O_HBond_len;
+
+				/**	has the (previous/following) residue a disulfid bond?
+				 */
+				bool 				has_disulfid_bond;
+				bool 				prev_has_disulfid_bond;
+				bool 				next_has_disulfid_bond;
+
+				/**	has the (previous/following) residue a HA-HBond?
+				 */
+				bool 				has_HA_HBond;
+				bool 				prev_has_HA_HBond;
+				bool 				next_has_HA_HBond;
+
+				/**	has the (previous/following) residue a HA2-HBond?
+				 */
+				bool 				has_HA2_HBond;
+				bool 				prev_has_HA2_HBond;
+				bool 				next_has_HA2_HBond;
+
+				/**	has the (previous/following) residue a HN-HBond?
+				 */
+				bool 				has_HN_HBond;
+				bool 				prev_has_HN_HBond;
+				bool 				next_has_HN_HBond;
+
+				/**	has the (previous/following) residue a O-HBond?
+				 */
+				bool 				has_O_HBond;
+				bool 				prev_has_O_HBond;
+				bool 				next_has_O_HBond;
+
+				/** hydrogen bond status
+				 */
+				bool				hydrogen_bond_status;
+
+		}; // end of nested class
 					
-				/*_	The targeted atoms collected by {\tt operator ()}.
+		/*_	The targeted atoms collected by {\tt operator ()}.
 				*/
 				std::vector<PropertiesForShift_> 				targets_;
 				
@@ -275,6 +320,13 @@ namespace BALL
 					float 		getHA_HBondLen_(Residue* residue) throw();
 					float 		getHA2_HBondLen_(Residue* residue) throw();
 					float 		getHN_HBondLen_(Residue* residue) throw();
+					float 		getO_HBondLen_(Residue* residue) throw();
+					bool 			hasDisulfidBond_(Residue* residue) throw();
+					bool 			hasHA_HBond_(Residue* residue) throw();
+					bool 			hasHA2_HBond_(Residue* residue) throw();
+					bool 			hasHN_HBond_(Residue* residue) throw();
+					bool 			hasO_HBond_(Residue* residue) throw();
+
 
 
 	};//End of class
