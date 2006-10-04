eaf13973e92458f5d132eb07105ce43c050bc577
amoll <amoll>
2006-10-04 11:48:03 +0000

    no message

--- source/PYTHON/EXTENSIONS/pyRegularData3DList.C
+++ source/PYTHON/EXTENSIONS/pyRegularData3DList.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: pyRegularData3DList.C,v 1.1.2.1 2006/10/04 11:33:09 amoll Exp $
+// $Id: pyRegularData3DList.C,v 1.1.2.2 2006/10/04 11:48:02 amoll Exp $
 //
 
 #include <BALL/PYTHON/pyRegularData3DList.h>
@@ -23,9 +23,9 @@ namespace BALL
 		: List<RegularData3D*>()
   {
 		List<std::pair<RegularData3D*, String> >::const_iterator it(l.begin());
-		for (; it != composite_list.end(); ++it)
+		for (; it != l.end(); ++it)
 		{
-			push_back(const_cast<RegularData3D*>(*it->first));
+			push_back(const_cast<RegularData3D*>(it->first));
 		}
 	}
 
