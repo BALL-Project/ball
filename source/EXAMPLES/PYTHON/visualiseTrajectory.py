7cc1695676c7abb1ee38712301e75d1f396ebda6
amoll <amoll>
2007-04-13 11:32:04 +0000

    no message

--- source/EXAMPLES/PYTHON/visualiseTrajectory.py
+++ source/EXAMPLES/PYTHON/visualiseTrajectory.py
@@ -7,36 +7,20 @@ dp.enableCreationForNewMolecules(1)
 openFile(Path().find("structures/AlaAla.hin"))
 dcd = DCDFile(Path().find("structures/alaala.dcd"))
 system = getSystem(0)
-number_ss = dcd.getNumberOfSnapShots()
-
-ssm = SnapShotManager(system, getMolecularStructure().getForceField(), dcd)
-nr_ss = dcd.getNumberOfSnapShots()
-
+ssm = SnapShotManager(system, AmberFF(), dcd)
 rep = Representation()
-rep.setDrawingPrecision(DRAWING_PRECISION_MEDIUM)
-
-positions = []
-for atom in atoms(system):
-	if atom.getElement().getSymbol() == "H":
-		continue
-	positions.append(atom.getPosition())
-
-	
 current_ss = 0
+
 while ssm.applyNextSnapShot():
-	current_ss = current_ss + 1
-	ratio = float(current_ss) / float(nr_ss)
-	i = 0
-	for atom in atoms(system):
-		if atom.getElement().getSymbol() == "H":
-			continue
+	current_ss +=  1
+	ratio = float(current_ss) / float(dcd.getNumberOfSnapShots())
+	for atom in atoms(system, "!element(H)"):
 		sphere = Sphere()
-		sphere.setPosition((positions[i] + atom.getPosition()) / 2.0)
+		sphere.setPosition(atom.getPosition())
 		sphere.setComposite(atom)
 		sphere.setColor(ColorRGBA(1.0 - ratio, 1.0 - ratio, ratio))
 		sphere.setRadius(0.03)
 		rep.insert(sphere)			
-		i = i + 1
 
 getMainControl().insert(rep)
 getMainControl().update(rep)
