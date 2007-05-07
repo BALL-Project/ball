9ccca2d8326c81e38bcb55c22090b4f14f7501c4
zeistacita <zeistacita>
2007-05-07 11:49:25 +0000

    Added standard comments

--- source/STRUCTURE/DOCKING/conformationSet.C
+++ source/STRUCTURE/DOCKING/conformationSet.C
@@ -1,3 +1,9 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id: conformationSet.C,v 1.2.22.2 2007/05/07 11:49:25 zeistacita Exp $
+//
+
 #include <BALL/STRUCTURE/DOCKING/conformationSet.h>
 
 #include <BALL/KERNEL/atomContainer.h>
