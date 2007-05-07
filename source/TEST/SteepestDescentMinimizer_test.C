827fa0dc261a3b912e0832f0c011e52894fd8f90
aleru <aleru>
2007-05-07 11:50:49 +0000

    New steepest descent test

--- source/TEST/SteepestDescentMinimizer_test.C
+++ source/TEST/SteepestDescentMinimizer_test.C
@@ -1,7 +1,7 @@
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// $Id: SteepestDescentMinimizer_test.C,v 1.5.30.1 2007/03/25 21:49:02 oliver Exp $
+// $Id: SteepestDescentMinimizer_test.C,v 1.5.30.2 2007/05/07 11:50:49 aleru Exp $
 //
 
 #include <BALL/CONCEPT/classTest.h>
@@ -16,7 +16,7 @@
 
 ///////////////////////////
 
-START_TEST(ConjugateGradienMinimizer, "$Id: SteepestDescentMinimizer_test.C,v 1.5.30.1 2007/03/25 21:49:02 oliver Exp $")
+START_TEST(ConjugateGradienMinimizer, "$Id: SteepestDescentMinimizer_test.C,v 1.5.30.2 2007/05/07 11:50:49 aleru Exp $")
 
 using namespace BALL;
 
@@ -201,82 +201,79 @@ CHECK(SteepestDescentMinimizer::minimize(Size, bool))
 	
 	FF.updateEnergy();
 	FF.updateForces();
-
+	
 	PRECISION(1e-3)
-  TEST_REAL_EQUAL(FF.getEnergy(), -0.359813)
+	TEST_REAL_EQUAL(FF.getEnergy(), -0.359813)
 	PRECISION(7e-3)
-  TEST_REAL_EQUAL(a1->getPosition().getDistance(a2->getPosition()), 3.81244)
+	TEST_REAL_EQUAL(a1->getPosition().getDistance(a2->getPosition()), 3.81244)
 RESULT
 
 CHECK(SteepestDescentMinimizer::minimize(Size, bool) ethan)
-  System S;
-  HINFile f("data/ethan.hin");
-  f >> S;
-  FF.options[AmberFF::Option::ASSIGN_CHARGES] = "false";
-  FF.setup(S);
-
-  TEST_EQUAL(FF.isValid(), true)
-  FF.updateEnergy();
-  FF.updateForces();
-  PRECISION(1E-4)
-  TEST_REAL_EQUAL(FF.getEnergy(), 18.5605)
-
-  SteepestDescentMinimizer sd(FF);
-
-  sd.setEnergyOutputFrequency(5);
-  sd.setMaxGradient(0.418);
+	System S;
+	HINFile f("data/ethan.hin");
+	f >> S;
+	FF.options[AmberFF::Option::ASSIGN_CHARGES] = "false";
+	FF.setup(S);
+	
+	TEST_EQUAL(FF.isValid(), true)
+	FF.updateEnergy();
+	FF.updateForces();
+	PRECISION(1E-4)
+	TEST_REAL_EQUAL(FF.getEnergy(), 18.5605)
+	
+	SteepestDescentMinimizer sd(FF);
+	sd.setEnergyOutputFrequency(5);
+	sd.setMaxGradient(0.418);
 	sd.setMaxNumberOfIterations(10000);
-  sd.setEnergyDifferenceBound(0.00000001);
-
-  TEST_EQUAL(sd.isValid(), true)
-  FF.updateEnergy();
-  FF.updateForces();
-  bool result = sd.minimize(500);
-
-  TEST_EQUAL(result, true)
-  float energy = FF.updateEnergy();
-  FF.updateForces();
-
-  PRECISION(6E-2)
-  TEST_REAL_EQUAL(energy, 5.906)
-
-  AtomIterator atit;
-  Vector3 pos[8];
-  int i=0;
-
-  for (atit = S.beginAtom(); +atit; ++atit)
-  {
-    pos[i] = atit->getPosition();
-    ++i;
+	sd.setEnergyDifferenceBound(0.00000001);
+	
+	TEST_EQUAL(sd.isValid(), true)
+	FF.updateEnergy();
+	FF.updateForces();
+	bool result = sd.minimize(500);
+	
+	TEST_EQUAL(result, true)
+	float energy = FF.updateEnergy();
+	FF.updateForces();
+	
+	PRECISION(6E-2)
+	TEST_REAL_EQUAL(energy, 5.906)
+	
+	AtomIterator atit;
+	Vector3 pos[8];
+	int i=0;
+	
+	for (atit = S.beginAtom(); +atit; ++atit)
+	{
+		pos[i] = atit->getPosition();
+		++i;
 	}
-
-  PRECISION(5E-2)
-  Angle torsion;
-  torsion = getTorsionAngle(pos[2].x, pos[2].y, pos[2].z, pos[0].x, pos[0].y, pos[0].z, pos[1].x, pos[1].y, pos[1].z, pos[6].x, pos[6].y, pos[6].z);
-  TEST_REAL_EQUAL(torsion.toRadian(), 1.047)
-  torsion = getTorsionAngle(pos[3].x, pos[3].y, pos[3].z, pos[0].x, pos[0].y, pos[0].z, pos[1].x, pos[1].y, pos[1].z, pos[5].x, pos[5].y, pos[5].z);
-  TEST_REAL_EQUAL(torsion.toRadian(), 1.047)
-  torsion = getTorsionAngle(pos[4].x, pos[4].y, pos[4].z, pos[0].x, pos[0].y, pos[0].z, pos[1].x, pos[1].y, pos[1].z, pos[7].x, pos[7].y, pos[7].z);
-  TEST_REAL_EQUAL(torsion.toRadian(), 1.047)
-
-  PRECISION(1E-2)
-  Angle tet;
-  tet = (pos[2] - pos[0]).getAngle(pos[3] - pos[0]);
-  TEST_REAL_EQUAL(tet.toRadian(), 1.902)
-  tet = (pos[2] - pos[0]).getAngle(pos[4] - pos[0]);
-  TEST_REAL_EQUAL(tet.toRadian(), 1.902)
-  tet = (pos[4] - pos[0]).getAngle(pos[3] - pos[0]);
-  TEST_REAL_EQUAL(tet.toRadian(), 1.902)
-  tet = (pos[5] - pos[1]).getAngle(pos[6] - pos[1]);
-  TEST_REAL_EQUAL(tet.toRadian(), 1.902)
-  tet = (pos[5] - pos[1]).getAngle(pos[7] - pos[1]);
-  TEST_REAL_EQUAL(tet.toRadian(), 1.902)
-  tet = (pos[7] - pos[1]).getAngle(pos[6] - pos[1]);
-  TEST_REAL_EQUAL(tet.toRadian(), 1.902)
-
-
+	
+	PRECISION(5E-2)
+	Angle torsion;
+	torsion = getTorsionAngle(pos[2].x, pos[2].y, pos[2].z, pos[0].x, pos[0].y, pos[0].z, pos[1].x, pos[1].y, pos[1].z, pos[6].x, pos[6].y, pos[6].z);
+	TEST_REAL_EQUAL(torsion.toRadian(), 1.047)
+	torsion = getTorsionAngle(pos[3].x, pos[3].y, pos[3].z, pos[0].x, pos[0].y, pos[0].z, pos[1].x, pos[1].y, pos[1].z, pos[5].x, pos[5].y, pos[5].z);
+	TEST_REAL_EQUAL(torsion.toRadian(), 1.047)
+	torsion = getTorsionAngle(pos[4].x, pos[4].y, pos[4].z, pos[0].x, pos[0].y, pos[0].z, pos[1].x, pos[1].y, pos[1].z, pos[7].x, pos[7].y, pos[7].z);
+	TEST_REAL_EQUAL(torsion.toRadian(), 1.047)
+	
+	PRECISION(1E-2)
+	Angle tet;
+	tet = (pos[2] - pos[0]).getAngle(pos[3] - pos[0]);
+	TEST_REAL_EQUAL(tet.toRadian(), 1.902)
+	tet = (pos[2] - pos[0]).getAngle(pos[4] - pos[0]);
+	TEST_REAL_EQUAL(tet.toRadian(), 1.902)
+	tet = (pos[4] - pos[0]).getAngle(pos[3] - pos[0]);
+	TEST_REAL_EQUAL(tet.toRadian(), 1.902)
+	tet = (pos[5] - pos[1]).getAngle(pos[6] - pos[1]);
+	TEST_REAL_EQUAL(tet.toRadian(), 1.902)
+	tet = (pos[5] - pos[1]).getAngle(pos[7] - pos[1]);
+	TEST_REAL_EQUAL(tet.toRadian(), 1.902)
+	tet = (pos[7] - pos[1]).getAngle(pos[6] - pos[1]);
+	TEST_REAL_EQUAL(tet.toRadian(), 1.902)
+	
 RESULT
-
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
