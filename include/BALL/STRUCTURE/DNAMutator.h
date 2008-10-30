820132362b681fd27a8eed7836c1e08425ac68a1
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2008-10-30 16:57:56 +0000

    DNAMutator can use minimizers to refine a mutation
    This adds the support for arbitrary energy minimizers, that
    are used to refine the mutation after a first purely geometrical
    placing of the bases

--- include/BALL/STRUCTURE/DNAMutator.h
+++ include/BALL/STRUCTURE/DNAMutator.h
@@ -14,6 +14,7 @@
 namespace BALL
 {
 	class FragmentDB;
+	class EnergyMinimizer;
 	class Residue;
 	class AtomContainer;
 	class Atom;
@@ -27,10 +28,20 @@ namespace BALL
 			enum Base { ADENINE = 0, THYMINE = 1, GUANINE = 2, CYTOSINE = 3, URACILE = 4 };
 
 			/**
-			 * Constructs a DNAMutator instance using the specified FragmentDB
-			 * If nothing or NULL is passed a default FragmentDB is used
+			 * Constructs a DNAMutator 
+			 *
+			 * @param frag A FragmentDB instance that shall be used to obtain structure
+			 *        of the new bases. If nothing or NULL is passed a default 
+			 *        FragmentDB is used. The FragmentDB is initialized as needed causing
+			 *        a short delay when mutating the first base. If you do not want this
+			 *        either pass a valid FragmentDB instance or call DNAMutator::setup()
+			 *        prior to using.
+			 *
+			 * @param mini The minimizer that shall be used for optimization of the mutated
+			 *        base. If nothing or NULL is passed optimization is disabled.
+			 *        You can pass a new minimizer anytime using DNAMutator::setMinimizer().
 			 */
-			DNAMutator(FragmentDB* frag = NULL);
+			DNAMutator(EnergyMinimizer* mini = NULL, FragmentDB* frag = NULL);
 
 			/**
 			 * The destructor of the the DNAMutator must delete the FragmentDB instance
@@ -39,23 +50,55 @@ namespace BALL
 			~DNAMutator();
 
 			/**
+			 * Calling this method explicitly circumvents lazy loading of the FragmentDB.
+			 * This method has no effect if mutate has already been called or if valid instances
+			 * of the fragmentDB has been passed via the constructor.
+			 */
+			void setup();
+
+			/**
 			 * This method changes a given base to another one. Changing the base
 			 * on the opposite strand is <br>not</br> handled. Most likely you will
 			 * need to localy reoptimize the structure after the insertion.
 			 *
 			 * @param res A pointer to the Residue that will be changed
+			 *
 			 * @param base A string specifiying the base which will be used to replace
-			 *             the current one. Can be one of "A", "T", "G", "C" and "U".
+			 *        the current one. Can be one of "A", "T", "G", "C" and "U".
+			 *
+			 * @param optimize This flag indicates whether the new base should undergo a
+			 *        local optimization. Note that if you decide against a reoptimization
+			 *        only geometric properties of the original base are taken into consideration, which
+			 *        might be _very_ crude.
+			 *        In order to be able to perform an optimization you must provide the mutator
+			 *        with a valid instance of an EnergyMinimizer via the constructor or
+			 *				DNAMutator::setMinimizer()
 			 *
 			 * @todo - Add capabilities into the FragmentDB that allows the retrival of Molecule type information
-			 *       - Currently only Purine - Purine and Pyrimidine - Pyrimidine conversion is "exact". It is quite
-			 *         hard to get Purine - Pyrimidine conversion right without hardcoding
 			 */
-			void mutate(Residue* res, Base base) throw(Exception::InvalidOption);
+			void mutate(Residue* res, Base base, bool optimize=true) throw(Exception::InvalidOption);
+
+			void setMinimizer(EnergyMinimizer* mini);
+			void setFragmentDB(FragmentDB* frag);
+
+			void setMaxOptimizationSteps(Size steps);
 
 		private:
-			FragmentDB* db_;
 			bool keep_db_;
+
+			EnergyMinimizer* minimizer_;
+			FragmentDB* db_;
+
+			Size num_steps_;
+
+			void freeDB_();
+
+			/**
+			 * Reoptimize the given fragment using the minimizer stored in minimizer_.
+			 *
+			 * @return false on error.
+			 */
+			bool optimize_(Residue* frag);
 
 			/**
 			 * This function returns a pointer to the nitrogen atom that attaches
@@ -79,10 +122,16 @@ namespace BALL
 			Atom* getConnectionAtom(Atom* at);
 			Vector3 getOrthogonalVector(const Vector3& n, const Atom* base, const Atom* at);
 
+			/**
+			 * The methods below decide whether a base is a Purine or a Pyrimidine.
+			 * In an ideal distant future this should be stored in the fragment and thus 
+			 * be not necessary.
+			 */
 			bool isPurine(const Atom& baseNitrogen) const;
 			bool isPyrimidine(const Atom& baseNitrogen) const;
 
 			static const char* bases_[];
+			static const Size default_num_steps_;
 	};
 }
 
