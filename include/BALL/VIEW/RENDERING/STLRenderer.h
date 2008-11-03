64c8f70e1fabffa3fa537bca6845ed2f0e34e64a
Oliver Kohlbacher <Oliver@Airosol.local>
2008-11-03 14:17:36 +0100

    added missing files for 3D printing

---
+++ include/BALL/VIEW/RENDERING/STLRenderer.h
@@ -0,0 +1,148 @@
+//STLRenderer: Version 1.0 (2008.09.12) Annette Treichel
+
+#ifndef BALL_VIEW_RENDERING_STLRENDERER_H
+#define BALL_VIEW_RENDERING_STLRENDERER_H
+
+#ifndef BALL_VIEW_RENDERING_RENDERER_H
+# include <BALL/VIEW/RENDERING/renderer.h>
+#endif
+
+#ifndef BALL_SYSTEM_FILE_H
+# include <BALL/SYSTEM/file.h>
+#endif
+
+#ifndef BALL_MATHS_VECTOR3_H
+# include <BALL/MATHS/vector3.h>
+#endif
+
+#ifndef BALL_MATHS_MATRIX44_H
+# include <BALL/MATHS/matrix44.h>
+#endif
+
+namespace BALL
+{
+	namespace VIEW
+	{
+
+/** STLRenderer class.
+		Representation of all primitives as Meshes in stl 
+		==> extrernal rendering of the same scene
+		<br>
+		<br>
+		\ingroup ViewRendering
+*/
+
+class BALL_VIEW_EXPORT STLRenderer : public Renderer
+{
+	public:
+
+	BALL_CREATE(STLRenderer)
+
+	/** @name Constructors and Destructors.
+	 */
+	//@{
+
+	/// Default constructor.
+	STLRenderer()
+		throw();
+
+	/** Detailed constructor.
+			\param name The name of the file we will create
+	 */
+	STLRenderer(const String& name)
+		throw(Exception::FileNotFound);
+	
+	/// Destructor.
+	virtual ~STLRenderer()
+		throw();
+
+	/// Clear method.
+	virtual void clear()
+		throw();
+
+	//@}
+	/** @name Accessors
+	 */
+	//@{
+
+	/** Sets the name of the file we will create.
+			\param name The file name
+	 */
+	void setFileName(const String& name)
+		throw(Exception::FileNotFound);
+
+
+	/** Converts a Vector3 into a String in VRML format as stl works in the same perspektive.
+	 */
+	String VRMLVector3(Vector3 input)
+		throw();
+
+	//@}
+	/** @name Processor specific methods
+	 */
+	//@{
+	
+	/** Start method. 
+			This method creates the file and writes the header.
+	 */
+	virtual bool init(const Stage& stage)
+		throw();
+
+	/** Finish method.
+			This method writes the ending of the file and closes it.
+	 */
+	virtual bool finish()
+		throw();
+
+	//@}
+
+	void renderSphere_(const Sphere& sphere)
+		throw();
+	
+	void renderMesh_(const Mesh& mesh)
+		throw();
+
+	void renderTube_(const Tube& tube)
+		throw();
+
+	void renderTwoColoredTube_(const TwoColoredTube& tube)
+		throw();
+
+	void renderLine_(const Line& tube)
+		throw();
+
+	void renderTwoColoredLine_(const TwoColoredLine& tube)
+		throw();
+
+	void out_(const String& data)
+		throw();
+
+	void outheader_(const String& data)
+		throw() {out_(data); current_indent_ += 1;}
+
+	void outfinish_(const String& data)
+		throw() {out_(data); current_indent_ -= 1;}
+
+	Size width, height;
+
+	protected:
+
+	void header_(const Vector3& translation, const ColorRGBA& color, 
+							 const String& rotation = "")
+		throw();
+		
+	void footer_()
+		throw();
+
+	File outfile_;
+
+	Vector3   origin_;
+	Matrix4x4 rotation_;
+	Index current_indent_;
+	String endingName_;
+};
+  
+} } // namespaces
+
+#endif // BALL_VIEW_RENDERING_STLRENDERER_H
+
---
+++ source/VIEW/DIALOGS/printingDialog.C
@@ -0,0 +1,331 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// $Id:
+
+#include <BALL/VIEW/DIALOGS/printingDialog.h>
+#include <BALL/VIEW/KERNEL/mainControl.h>
+#include <BALL/VIEW/KERNEL/message.h>
+#include <BALL/VIEW/KERNEL/common.h>
+#include <QtGui/QFileDialog>
+#include <QtGui/qmessagebox.h>
+#include <QtGui/qlineedit.h>
+
+
+namespace BALL
+{
+	namespace VIEW
+	{
+
+PrintingDialog::PrintingDialog(QWidget* parent, const char* name)
+	throw()
+	:	QDialog(parent),
+		Ui_PrintingDialogData()
+{
+#ifdef BALL_VIEW_DEBUG
+	Log.error() << "new PrintingDialog " << this << std::endl;
+#endif
+
+	setupUi(this);
+	
+  // signals and slots connections
+	QObject::connect( browse_button, SIGNAL( clicked() ), this, SLOT( browseFiles() ) );
+  QObject::connect( buttonOk, SIGNAL( clicked() ), this, SLOT( accept() ) );
+  QObject::connect( buttonCancel, SIGNAL( clicked() ), this, SLOT( reject() ) );
+  QObject::connect( prototyping_radio, SIGNAL( clicked() ), this, SLOT( protomode() ) );
+  QObject::connect( view_radio, SIGNAL( clicked() ), this, SLOT( viewmode() ) );	
+
+	setObjectName(name);
+}
+
+PrintingDialog::~PrintingDialog()
+	throw()
+{
+#ifdef BALL_VIEW_DEBUG
+	Log.error() << "deleting PrintingDialog " << this << std::endl;
+#endif
+}
+
+
+
+void PrintingDialog::exec()
+{
+	MainControl* mc = getMainControl();
+	if (mc == 0) return;
+
+	RepresentationManager& rm = mc->getRepresentationManager();
+
+	//creation of the listpart of the GUI
+ 	listview->setColumnCount(3);
+	listview->setRowCount(rm.getRepresentations().size());
+	listview->setHorizontalHeaderItem(0, new QTableWidgetItem());
+	listview->setHorizontalHeaderItem(1, new QTableWidgetItem());
+	listview->setHorizontalHeaderItem(2, new QTableWidgetItem());
+ 	listview->horizontalHeaderItem(0)->setText("Model");
+ 	listview->horizontalHeaderItem(1)->setText("Coloring");
+ 	listview->horizontalHeaderItem(2)->setText("Properties");
+	listview->setColumnWidth(0, 140);
+	listview->setColumnWidth(1, 140);
+	listview->setColumnWidth(2, 140);
+
+	const ModelInformation& mi = mc->getModelInformation();
+
+	Position row= 0;
+	RepresentationList::ConstIterator it = rm.getRepresentations().begin();
+	
+	//filling of the list: as "for viewing" is the standard configuration all reps are checkable 
+	for (; it != rm.getRepresentations().end(); it++)
+	{
+		const Representation& rep = **it;
+
+		QTableWidgetItem* item = new QTableWidgetItem(rep.getName().c_str());
+		item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
+
+		//the same represantations are checked as in BALLView
+		if ( (*it)->isHidden() )
+		{	
+			item->setCheckState(Qt::Unchecked);
+		}
+		else
+		{
+			item->setCheckState(Qt::Checked);
+		}
+
+		listview->setItem(row, 0, item);
+
+		item = new QTableWidgetItem(mi.getColoringName(rep.getColoringMethod()).c_str());
+		listview->setItem(row, 1, item);
+
+		item = new QTableWidgetItem(rep.getProperties().c_str());
+		listview->setItem(row, 2, item);
+		row++;
+	}
+	split_ = false;
+	vrml_ = false;
+	stl_ = false;
+	raise();
+	QDialog::exec();
+}
+
+
+void PrintingDialog::accept()
+{
+	hide();
+
+	//before we do anything at all we have to store the information which representations are now visible
+	//as the export works by switching them on and off => saving base stats
+	MainControl* mc = getMainControl();
+	RepresentationManager& rm = mc->getRepresentationManager();
+	RepresentationList::ConstIterator rit;
+
+	Position count = 0;
+				
+	rit = rm.getRepresentations().begin();
+	for (; rit != rm.getRepresentations().end(); rit++)
+	{	
+		if(!((*rit)->isHidden()))
+		{
+			basestats[count]=true;
+			(*rit)->setHidden(true);
+		}	
+		else 
+		{
+			basestats[count]=false;
+		}
+		count ++;
+	}		
+
+//we have to get to know which representations the user wants to export:
+	for (Position pos = 0; pos < (Position)listview->rowCount(); pos++)
+	{
+		const QTableWidgetItem& item = *listview->item(pos, 0);
+		if (item.checkState() == Qt::Checked)
+		{
+			reps[pos] = true;
+		}
+		else
+		{
+			reps[pos] = false;
+		}
+	}
+
+	//now we look whether we have to split them into several files:
+	if ((prototyping_radio->isChecked()) || (split_radio->isChecked()))
+	{
+		split_ = true;
+	}
+	else 
+	{
+		split_ = false;
+	}
+
+	if (vrml_box->isChecked())
+	{
+		vrml_ = true;
+	}
+	else
+	{
+		vrml_ = false;
+	}
+
+	if (stl_box->isChecked())
+	{
+		stl_ = true;
+	}
+	else
+	{
+		stl_ = false;
+	}
+	setFilename(file_edit->displayText());
+}
+
+
+	void PrintingDialog::browseFiles()
+	{
+		QFileDialog fd(0, "Export as 3D file", getMainControl()->getWorkingDir().c_str(), "*.*");
+		fd.setAcceptMode(QFileDialog::AcceptSave);
+		fd.selectFile(filename_);
+		fd.setFileMode(QFileDialog::AnyFile);
+		if (fd.exec() != QDialog::Accepted ||	fd.selectedFiles().size() == 0)
+		{
+			return;
+		}
+		QString fname = *fd.selectedFiles().begin();
+		setFilename(fname);
+		file_edit->setText(fname);
+	}
+
+	bool PrintingDialog::export_vrml()
+	{
+		return vrml_;
+	}	
+
+	bool PrintingDialog::export_stl()
+	{
+		return stl_;
+	}	
+
+	bool PrintingDialog::split()
+	{
+		return split_;
+	}
+
+	void PrintingDialog::protomode()
+	{
+		MainControl* mc = getMainControl();
+		if (mc == 0) return;
+
+		RepresentationManager& rm = mc->getRepresentationManager();
+
+		const ModelInformation& mi = mc->getModelInformation();
+
+		Position row= 0;
+		bool printable;
+		RepresentationList::ConstIterator it = rm.getRepresentations().begin();
+		for (; it != rm.getRepresentations().end(); it++)
+		{
+			const Representation& rep = **it;
+			printable = true;
+
+			List<GeometricObject*>::ConstIterator git = (*it)->getGeometricObjects().begin();
+			for (; git != (*it)->getGeometricObjects().end(); git++)
+			{
+				//now we test if the geo object is printable
+				//therefor geometric object has the method isType that returns a Position value in relation to the number of the representation
+				//all geometric objects that are just helpers as the grid but are not exported are counted as printable
+				// mesh (5) is the only real printable representation
+				bool pgeo = (((*git)->isType()) == 0 || ((*git)->isType()) == 1 || ((*git)->isType()) == 2 || ((*git)->isType()) == 3);
+				pgeo = ( pgeo || ((*git)->isType()) == 5 || ((*git)->isType()) == 9 );
+			
+				if ( !pgeo )
+				//case: not printable
+				{	
+					printable = false;
+					break;
+				}
+			}
+			
+			if (printable)
+			//item is the same as normal
+			{
+			
+				QTableWidgetItem* item = new QTableWidgetItem(rep.getName().c_str());
+				item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
+				if ( (*it)->isHidden() )
+				{	
+					item->setCheckState(Qt::Unchecked);
+				}
+				else
+				{
+					item->setCheckState(Qt::Checked);
+				}
+
+				listview->setItem(row, 0, item);
+
+				item = new QTableWidgetItem(mi.getColoringName(rep.getColoringMethod()).c_str());
+				listview->setItem(row, 1, item);
+
+				item = new QTableWidgetItem(rep.getProperties().c_str());
+				listview->setItem(row, 2, item);
+				row++;
+			}
+			else
+			{
+				QTableWidgetItem* item = new QTableWidgetItem(rep.getName().c_str());
+				item->setFlags( Qt::ItemIsEnabled | Qt::ItemIsEnabled );
+				item->setCheckState(Qt::Unchecked);
+
+				listview->setItem(row, 0, item);
+
+				item = new QTableWidgetItem(mi.getColoringName(rep.getColoringMethod()).c_str());
+				item->setFlags( Qt::ItemIsEnabled | Qt::ItemIsEnabled );
+				listview->setItem(row, 1, item);
+
+				item = new QTableWidgetItem(rep.getProperties().c_str());
+				item->setFlags( Qt::ItemIsEnabled | Qt::ItemIsEnabled );
+				listview->setItem(row, 2, item);
+				row++;
+			}
+		}
+		raise();
+	}	
+			
+	void PrintingDialog::viewmode()
+	{
+		MainControl* mc = getMainControl();
+		if (mc == 0) return;
+
+		RepresentationManager& rm = mc->getRepresentationManager();
+
+		const ModelInformation& mi = mc->getModelInformation();
+
+		Position row= 0;
+		RepresentationList::ConstIterator it = rm.getRepresentations().begin();
+		for (; it != rm.getRepresentations().end(); it++)
+		{
+			const Representation& rep = **it;
+
+			QTableWidgetItem* item = new QTableWidgetItem(rep.getName().c_str());
+			item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
+			if ( (*it)->isHidden() )
+			{	
+				item->setCheckState(Qt::Unchecked);
+			}
+			else
+			{
+				item->setCheckState(Qt::Checked);
+			}
+
+			listview->setItem(row, 0, item);
+
+			item = new QTableWidgetItem(mi.getColoringName(rep.getColoringMethod()).c_str());
+			listview->setItem(row, 1, item);
+
+			item = new QTableWidgetItem(rep.getProperties().c_str());
+			listview->setItem(row, 2, item);
+			row++;
+		}
+		raise();
+
+	}		
+} } // namespaces
