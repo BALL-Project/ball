b67d978f37490a98b0e1ba8807d14ebbc98aab09
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2008-11-11 12:24:14 +0100

    Added missing header files for plugin support

---
+++ include/BALL/PLUGIN/pluginManager.h
@@ -0,0 +1,41 @@
+#ifndef BALL_PLUGIN_PLUGINMANAGER_H
+#define BALL_PLUGIN_PLUGINMANAGER_H
+
+#include <QtCore/QString>
+#include <QtCore/QHash>
+
+class QPluginLoader;
+
+namespace BALL
+{
+	class BALLPlugin;
+
+	class PluginManager 
+	{
+		public:
+			~PluginManager();
+
+			static PluginManager& instance();
+
+			void setPluginDirectory(const QString& dir);
+
+			BALLPlugin* loadPlugin(const QString& plugin_name);
+			bool unloadPlugin(const QString& plugin); 
+
+			QObject* getPluginInstance(const QString& plugin);
+			QObject* getPluginInstance(int pos);
+
+			int getPluginCount() const;
+
+		private:
+			PluginManager();
+			PluginManager(const PluginManager&);
+			const PluginManager& operator=(const PluginManager&);
+
+			QString plugin_dir_;
+			QHash<QString, QPluginLoader*> loaders_;
+	};
+}
+
+#endif //BALL_PLUGIN_PLUGINMANAGER_H
+
