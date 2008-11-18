334429f915c72e53a904970ec7b08de9b05cdc2d
Stefan Nickels <cyphr@mobilenick.(none)>
2008-11-18 14:26:19 +0100

    First working version of VRPN-HD Plugin

---
+++ source/EXTENSIONS/VRPNHD/include/VRPNHDDriver.h
@@ -0,0 +1,41 @@
+#ifndef VRPNHDDRIVER_H
+#define VRPNHDDRIVER_H
+
+#include <BALL/VIEW/INPUT/inputDeviceDriver.h>
+#include <vrpn_Tracker.h>
+#include <QtCore/QThread>
+
+namespace BALL
+{
+	namespace VIEW
+	{
+
+		class VRPNHDDriver : public InputDeviceDriver, protected QThread
+		{
+			public:
+				VRPNHDDriver();
+				VRPNHDDriver(QWidget* receiver);
+
+				bool setUp();
+				bool tearDown();
+				
+				void handle_function(int s, double x, double y, double z, double q1, double q2, double q3, double q4);
+
+				void setEnabled(bool enabled);
+				void setServer(QString server);
+				
+				int vrpn_got_report;
+
+			private:
+				void run();
+				int deadzone(double x);
+				
+				QString server_;
+				vrpn_Tracker_Remote* tracker_;
+		};
+
+	}
+}
+
+#endif //VRPNHDDRIVER_H
+
---
+++ source/EXTENSIONS/VRPNHD/source/VRPNHDPlugin.C
@@ -0,0 +1,65 @@
+#include <VRPNHDPlugin.h>
+
+#include <VRPNHDDriver.h>
+
+
+Q_EXPORT_PLUGIN2(pluginVRPNHD, BALL::VIEW::VRPNHDPlugin)
+
+namespace BALL
+{
+	namespace VIEW
+	{
+		VRPNHDPlugin::VRPNHDPlugin()
+			: receiver_(), icon_(":logo.png"), is_active_(false)
+		{
+		}
+
+		void VRPNHDPlugin::setReceiver(QWidget* receiver)
+		{
+			receiver_ = receiver;
+		}
+
+		QString VRPNHDPlugin::getName() const
+		{
+			return QString("VRPN-HD");
+		}
+
+		QString VRPNHDPlugin::getDescription() const
+		{
+			return QString("A driver for a head tracking device via VRPN.");
+		}
+
+		const QPixmap* VRPNHDPlugin::getIcon() const
+		{
+			return &icon_;
+		}
+
+		QDialog* VRPNHDPlugin::getConfigDialog()
+		{
+			return NULL;
+		}
+
+		InputDeviceDriver* VRPNHDPlugin::startDriver()
+		{
+			VRPNHDDriver* driver = new VRPNHDDriver(receiver_); 
+			driver->setServer("Tracker0@localhost");
+		  driver->setUp();
+			driver->setEnabled(true);
+			return(driver);
+		}
+
+		void VRPNHDPlugin::activate()
+		{
+			is_active_ = true;
+
+			startDriver();
+		}
+
+		void VRPNHDPlugin::deactivate()
+		{
+			is_active_ = false;
+		}
+
+	}
+}
+
