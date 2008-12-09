dd24684524c65ecb1cdc619d969759d186678947
Anne Dehof <anne@bioinf.uni-sb.de>
2008-12-09 22:53:01 +0100

    Added a dialog for improved handling of stereo settings.

---
+++ include/BALL/VIEW/DIALOGS/stereoSettingsDialog.h
@@ -0,0 +1,59 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+
+#ifndef BALL_VIEW_DIALOGS_STEREOSETTINSGDIALOG_H
+#define BALL_VIEW_DIALOGS_STEREOSETTINSGDIALOG_H
+
+#ifndef BALL_COMMON_GLOBAL_H
+# include <BALL/COMMON/global.h>
+#endif
+
+#ifdef BALL_COMPILER_MSVC
+# include <BALL/VIEW/UIC/ui_stereoSettingsDialog.h>
+#else
+# include <BALL/VIEW/UIC/stereoSettingsDialogData.h>
+#endif
+
+#ifndef BALL_VIEW_KERNEL_MODULARWIDGET_H
+# include <BALL/VIEW/KERNEL/modularWidget.h>
+#endif
+
+#include <QtGui/QDialog>
+
+namespace BALL
+{
+	namespace VIEW
+	{
+		class Camera;
+
+		/** Dialog to set the focal plane to a given value
+				\ingroup ViewDialogs
+		*/
+		class BALL_VIEW_EXPORT StereoSettingsDialog 
+			: public QDialog,
+				public Ui_StereoSettingsDialogData,
+				public ModularWidget
+		{ 
+				Q_OBJECT
+				BALL_EMBEDDABLE(StereoSettingsDialog, ModularWidget)
+
+			public:
+				StereoSettingsDialog(QWidget* parent = 0, const char* name = "StereoSettingsDialog");
+				~StereoSettingsDialog() throw();
+
+				/** Initialization. This method is called automatically before the main application is started. 
+						It adds the	dialog's menu entries and connections.
+				*/
+				virtual void initializeWidget(MainControl& main_control)
+					throw();
+						
+			public slots:
+				void okPressed();
+				virtual void apply();
+				virtual void show();
+				void computeSettingsFromModelDistance();
+		};
+
+} } // namespaces
+#endif
---
+++ source/VIEW/DIALOGS/stereoSettingsDialog.C
@@ -0,0 +1,159 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+#include <BALL/VIEW/DIALOGS/stereoSettingsDialog.h>
+#include <BALL/VIEW/WIDGETS/scene.h>
+#include <BALL/VIEW/KERNEL/mainControl.h>
+#include <BALL/VIEW/KERNEL/stage.h>
+
+#include <QtGui/qpushbutton.h>
+#include <QtGui/qlineedit.h> 
+
+namespace BALL
+{
+	namespace VIEW
+	{
+
+StereoSettingsDialog::StereoSettingsDialog( QWidget* parent,  const char* name)
+	: QDialog(parent),
+		Ui_StereoSettingsDialogData(),
+		ModularWidget(name)
+{
+	setupUi(this);
+
+  // signals and slots connections
+  connect( ok_button, SIGNAL( pressed() ), this, SLOT( okPressed() ) );
+  connect( cancel_button, SIGNAL( pressed() ), this, SLOT( close() ) );
+	connect( apply_button, SIGNAL( pressed() ), this, SLOT( apply() )  );
+	connect( reset_to_standard_button, SIGNAL( pressed() ), this, SLOT( computeSettingsFromModelDistance() ) );
+
+	setModal(false);
+	setObjectName(name);
+
+	//TODO
+	screen_height->setText(String("200").c_str());
+	screen_width->setText(String("300").c_str());
+	viewer_distance_from_screen->setText(String("100").c_str());
+	viewer_height->setText(String("170").c_str());
+
+	ModularWidget::registerWidget(this);
+	hide();
+}
+
+StereoSettingsDialog::~StereoSettingsDialog()
+	throw()
+{
+}
+
+void StereoSettingsDialog::show()
+{
+	if (!Scene::getInstance(0))
+	{
+		QDialog::show();
+		return;
+	}
+
+	Stage* stage = Scene::getInstance(0)->getStage();
+	if (!stage)
+	{
+		QDialog::show();
+		return;
+	}
+
+	focal_distance->setText(String(stage->getFocalDistance()).c_str());
+	eye_separation->setText(String(stage->getEyeDistance()).c_str());
+
+	QDialog::show();
+}
+
+void StereoSettingsDialog::initializeWidget(MainControl&)
+	throw()
+{
+	insertMenuEntry(MainControl::DISPLAY_STEREO, "Stereo Settings", this, SLOT(show()));
+}
+
+void StereoSettingsDialog::computeSettingsFromModelDistance()
+{
+	// try to compute sensible stereo settings
+	Camera& camera = Scene::getInstance(0)->getStage()->getCamera();
+	Vector3 view_vector = camera.getViewVector();
+	view_vector.normalize();
+	const Vector3& view_point = camera.getViewPoint();
+
+	RepresentationManager& r = MainControl::getInstance(0)->getRepresentationManager();
+	RepresentationList::const_iterator rit = r.getRepresentations().begin();
+
+	float min_separation = 600;
+
+	for (; rit != r.getRepresentations().end(); ++rit)
+	{
+		List<GeometricObject*>::ConstIterator it = (*rit)->getGeometricObjects().begin();
+
+		vector<Vector3> positions;
+		for (; it != (*rit)->getGeometricObjects().end(); ++it)
+		{
+			(*it)->getVertices(positions);
+			// iterate over them and find the closest point along the view vector
+			for (Position i=0; i<positions.size(); i++)
+			{
+				float separation = (positions[i] - view_point)*view_vector;
+				min_separation = std::min(std::max(separation, 0.f), min_separation);
+			}
+			positions.clear();
+		}
+	}
+	focal_distance->setText(String(2.1*min_separation).c_str());
+	eye_separation->setText(String(1./20.*2.1*min_separation).c_str());
+}	
+	
+void StereoSettingsDialog::apply()
+{
+	Stage* stage = Scene::getInstance(0)->getStage();
+	stage->setFocalDistance((focal_distance->text()).toFloat());
+	stage->setEyeDistance((eye_separation->text()).toFloat());
+	Scene::getInstance(0)->update();
+}
+
+void StereoSettingsDialog::okPressed()
+{
+	apply();
+	accept();
+	return;
+
+	MainControl *main_control = MainControl::getMainControl(parentWidget());
+
+	float v_screen_height, v_screen_width, v_viewer_distance_from_screen, v_viewer_height;
+
+	try
+	{	
+		v_screen_height = (screen_height->text()).toFloat();
+		v_screen_width = (screen_width->text()).toFloat();
+		v_viewer_distance_from_screen = (viewer_distance_from_screen->text()).toFloat();
+		v_viewer_height = (viewer_height->text()).toFloat();
+	}
+	catch(...)
+	{
+		main_control->setStatusbarText("Invalid Values!");
+		return;
+	}
+
+	if (   (v_screen_height < 0) || (v_screen_width < 0) 
+			|| (v_viewer_distance_from_screen < 0)
+			|| (v_viewer_height < 0))
+	{
+		Log.error() << "Invalid values for StereoSettings: negative value(s)." << std::endl;
+		return;
+	}
+	
+	//TODO
+	//compute the focal plane and eye distance
+
+	Camera& camera = Scene::getInstance(0)->getStage()->getCamera();
+	//camera.setViewPoint(vp);
+	//camera.setLookAtPosition(lp);
+	std::cout << " neue focal plane" << std::endl;
+	accept();
+}
+
+// NAMESPACE
+} }
