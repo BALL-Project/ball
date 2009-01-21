b701a501020f22069d1f46ecf09b63b21f44b5b4
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2009-01-21 21:14:45 +0100

    Finalized rewriting of old NumericalSAS code.
    The "only" thing left to do is test more accurately if it still
    computes the same as before.

--- include/BALL/STRUCTURE/numericalSAS.h
+++ include/BALL/STRUCTURE/numericalSAS.h
@@ -9,12 +9,16 @@
 #	include <BALL/common.h>
 #endif
 
-#ifndef BALL_MATHS_VECTOR3_H
-#	include <BALL/MATHS/vector3.h>
+#ifndef BALL_DATATYPE_OPTIONS_H
+# include <BALL/DATATYPE/options.h>
 #endif
 
 #ifndef BALL_MATHS_SURFACE_H
-#	include <BALL/MATHS/surface.h>
+# include <BALL/MATHS/surface.h>
+#endif
+
+#ifndef BALL_MATHS_VECTOR3_H
+#	include <BALL/MATHS/vector3.h>
 #endif
 
 namespace BALL 
@@ -23,18 +27,273 @@ namespace BALL
 	class AtomContainer;
 	template <typename Key, typename T>
   class HashMap;
+	class TriangulatedSphere;
 
 	/**	@name	Fast Numerical Calculation of Solvent Accessible Surface Area.
-			These functions are based on the algorithm by Eisenhaber, Lijnzaad, Argos, Sander,
-			and Scharf ("The Double Cubic Lattice Method: Efficient Approaches to 
-			numerical Integration of Surface Area and Volume and to Dot
-			Surface Contouring of Molecular Assemblies", J. Comput. Chem. (1995),
-			<b>  15 </b>, 273-284).
-	
+			These functions are based on the algorithm by Eisenhaber, Lijnzaad, Argos,
+			Sander, and Scharf ("The Double Cubic Lattice Method: Efficient Approaches to
+			numerical Integration of Surface Area and Volume and to Dot Surface Contouring
+			of Molecular Assemblies", J. Comput. Chem. (1995), <b>  15 </b>, 273-284)
+			and Eisenhaber and Argos ("Improved Strategy in Analytic Surface Calculation
+			for Molecular Systems: Handling of Singularities and Computational Efficiency"
+			J. Comput. Chem. (1993), <b> 14 </b>, 1272-1280).
+
 			\ingroup Surface
 			@{
 	*/
-	
+	class BALL_EXPORT NumericalSAS
+	{
+		public:
+			/**	@name Constant Definitions
+			*/
+			//@{
+
+			/** Option names
+			 */
+			struct BALL_EXPORT Option
+			{
+				/** This flag decides whether total area and area per atom will
+				 *  be computed (default = true).
+				 */
+				static const String COMPUTE_AREA;
+
+				/** This flag decides whether total volume and volume per atom will
+				 *  be computed (default = true).
+				 */
+				static const String COMPUTE_VOLUME;
+
+				/** This flag decides whether surface points and normals will be
+				 *  explicitly computed and stored in the class (default = false).
+				 */
+				static const String COMPUTE_SURFACE;
+
+				/** This flag decides whether surface points and normals per atom
+				 *  will be explicitly computed and stored in the class 
+				 *  (default = false).
+				 */
+				static const String COMPUTE_SURFACE_PER_ATOM;
+
+				/** This flag decides whether a vector of (atom center, surface) per atom
+				 *  will be explicitly computed and stored in the class (default = false).
+				 */
+				static const String COMPUTE_SURFACE_MAP;
+
+				/** The number of point samples per sphere. Please note that this
+				 *  number is a lower limit, since sampling is performed through
+				 *  icosahedron or dodecahedron refinement that cannot produce
+				 *  arbitrary point numbers.
+				 */
+				static const String NUMBER_OF_POINTS;
+
+				/** The radius of the spherical probe used for the SAS definition.
+				 */
+				static const String PROBE_RADIUS;
+			};
+
+			/** Default values for NumericalSAS options.
+			 */
+			struct BALL_EXPORT Default
+			{
+				/** This flag decides whether total area and area per atom will
+				 *  be computed (default = true).
+				 */
+				static const bool COMPUTE_AREA;
+
+				/** This flag decides whether total volume and volume per atom will
+				 *  be computed (default = true).
+				 */
+				static const bool COMPUTE_VOLUME;
+
+				/** This flag decides whether surface points and normals will be
+				 *  explicitly computed and stored in the class (default = false).
+				 */
+				static const bool COMPUTE_SURFACE;
+
+				/** This flag decides whether surface points and normals per atom
+				 *  will be explicitly computed and stored in the class 
+				 *  (default = false).
+				 */
+				static const bool COMPUTE_SURFACE_PER_ATOM;
+
+				/** This flag decides whether a vector of (atom center, surface) per atom
+				 *  will be explicitly computed and stored in the class (default = false).
+				 */
+				static const bool COMPUTE_SURFACE_MAP;
+
+				/** Default number of point samples per sphere (400). Please note 
+				 *  that this number is a lower limit, since sampling is performed 
+				 *  through icosahedron or dodecahedron refinement that cannot 
+				 *  produce arbitrary point numbers. (@see Option::NUMBER_OF_POINTS)
+				 */
+				static const Size NUMBER_OF_POINTS;
+
+				/** Default radius of the spherical probe used for the SAS
+				 *  definition (1.5 \AA). (@see Option::PROBE_RADIUS)
+				 */
+				static const float PROBE_RADIUS;
+			};
+			//@}
+
+
+			/** @name Constructors and Destructors. **/
+			//@{
+
+			BALL_CREATE(NumericalSAS)
+
+			/** Default Constructor.
+			 */
+			NumericalSAS();
+
+			/** Detailed Constructor.
+			 */
+			NumericalSAS(const Options& options);
+
+			/** Destructor.
+			 */
+			~NumericalSAS();
+			//@}
+			
+			/** @name Accessors. **/
+			//@{
+
+			void operator() (const AtomContainer& fragment);
+
+			/** Returns the total area of the fragment.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if area computation has not been
+			 * 	disabled through the options.
+			 */
+			float getTotalArea() {return total_area_;}
+
+			/** Returns the area per atom of the fragment.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if area computation has not been
+			 * 	disabled through the options.
+			 */
+			HashMap<const Atom*, float>& getAtomAreas() {return atom_areas_;}
+
+			/** Returns the area per atom of the fragment, const version.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if area computation has not been
+			 * 	disabled through the options.
+			 */
+			const HashMap<const Atom*, float>& getAtomAreas() const {return atom_areas_;}
+
+			/** Returns the total volume of the fragment.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if volume computation has not been
+			 * 	disabled through the options.
+			 */
+			float getTotalVolume() {return total_volume_;}
+
+			/** Returns the volume per atom of the fragment.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if volume computation has not been
+			 * 	disabled through the options.
+			 */
+			HashMap<const Atom*, float>& getAtomVolumes() {return atom_volumes_;}
+
+			/** Returns the volume per atom of the fragment, const version.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if volume computation has not been
+			 * 	disabled through the options.
+			 */
+			const HashMap<const Atom*, float>& getAtomVolumes() const {return atom_volumes_;}
+
+			/** Returns the solvent accessible surface as a collection of points and normals.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if surface computation has not been
+			 * 	disabled through the options.
+			 */
+			Surface& getSurface() {return surface_;}
+
+			/** Returns the solvent accessible surface as a collection of points and normals, const version.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if surface computation has not been
+			 * 	disabled through the options.
+			 */
+			const Surface& getSurface() const {return surface_;}
+
+			/** Returns the solvent accessible surface as a collection of points and normals per atom.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if surface computation per atom has not been
+			 * 	disabled through the options.
+			 */
+			HashMap<const Atom*, Surface>& getSurfacePerAtom() {return atom_surfaces_;}
+
+			/** Returns the solvent accessible surface as a collection of points and normals per atom, const version.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if surface computation per atom has not been
+			 * 	disabled through the options.
+			 */
+			const HashMap<const Atom*, Surface>& getSurfacePerAtom() const {return atom_surfaces_;}
+
+			/** Returns the solvent accessible surface as a collection of points and normals per atom center.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if surface computation per atom center has not been
+			 * 	disabled through the options.
+			 */
+			std::vector< std::pair<Vector3, Surface> >& getSurfaceMap() {return atom_surface_map_;}
+
+			/** Returns the solvent accessible surface as a collection of points and normals per atom center, const version.
+			 *
+			 * 	This function only returns sensible values after a call
+			 * 	to operator() and only if surface computation per atom center has not been
+			 * 	disabled through the options.
+			 */
+			const std::vector< std::pair<Vector3, Surface> >& getSurfaceMap() const {return atom_surface_map_;}
+			
+			//@}
+
+			/** Options.
+			 */
+			Options options;
+
+		protected:
+			/** Set values from options. */
+			void setDefaultOptions_();
+
+			/** Precompute the sphere tesselation for the requested
+			 *  number of points.
+			 */
+			Size computeSphereTesselation_(TriangulatedSphere& result, int num_points);
+
+			/// the AtomContainer we are bound to
+			AtomContainer const* fragment_;
+
+			/// mapping of atom to SAS area
+			HashMap<Atom const*, float> atom_areas_;
+
+			/// total solvent accessible area of the fragment
+			float total_area_;
+
+			/// mapping of atom to SAS volume
+			HashMap<Atom const*, float> atom_volumes_;
+
+			/// total solvent accessible volume of the fragment
+			float total_volume_;
+
+			/// the SAS as a surface
+			Surface surface_;
+
+			/// mapping of atom to surface
+			HashMap<Atom const*, Surface> atom_surfaces_;
+
+			/// vector of (atom center, surface)
+			std::vector< std::pair<Vector3, Surface> > atom_surface_map_;
+	};
+
 	/** Calculate the solvent accessible surface area numerically.
 			This method returns the total 
 			Solvent Accessible Surface (SAS) of a BALL kernel object. 
@@ -43,8 +302,10 @@ namespace BALL
 			@param  probe_radius the probe radius used for the SAS
 			@param  number_of_dots the number of dots used per atom
 			@return the total SAS area in $ A^2$
+			@deprecated Use NumericalSAS::getTotalArea() instead
 	*/
-	BALL_EXPORT float calculateSASArea(const AtomContainer& fragment, float probe_radius = 1.5,
+	BALL_EXPORT BALL_DEPRECATED 
+	float calculateSASArea(const AtomContainer& fragment, float probe_radius = 1.5,
 												 Size number_of_dots = 400); 
 
 	/** Calculate the solvent accessible volume numerically.
@@ -55,8 +316,10 @@ namespace BALL
 			@param  probe_radius the probe radius used for the SAS
 			@param  number_of_dots the number of dots used per atom
 			@return the volume in $ A^3$
+			@deprecated Use NumericalSAS::getTotalVolume() instead
 	*/
-	BALL_EXPORT float calculateSASVolume(const AtomContainer& fragment, float probe_radius = 1.5,
+	BALL_EXPORT BALL_DEPRECATED
+	float calculateSASVolume(const AtomContainer& fragment, float probe_radius = 1.5,
 													 Size number_of_dots = 400); 
 
 	/**	Calculate the Solvent Accessible Surface area for each atom.
@@ -68,8 +331,10 @@ namespace BALL
 			@param	probe_radius the probe radius used for the SAS
 			@param	number_of_dots the number of dots used per atom
 			@return the total SAS area in $ A^2$
+			@deprecated Use NumericalSAS::getAtomAreas() instead
 	*/
-	BALL_EXPORT float calculateSASAtomAreas(const AtomContainer& fragment, HashMap<const Atom*,float>& atom_areas,
+	BALL_EXPORT BALL_DEPRECATED
+	float calculateSASAtomAreas(const AtomContainer& fragment, HashMap<const Atom*,float>& atom_areas,
 															float probe_radius = 1.5, Size number_of_dots = 400);
 	
 	/**	Calculate a point set on the Solvent Accessible Surface.
@@ -88,8 +353,10 @@ namespace BALL
 			@param	probe_radius the probe radius used for the SAS
 			@param	number_of_dots the number of dots used per atom
 			@return the total SAS area in $ A^2$
+			@deprecated Use NumericalSAS::getSurface() instead
 	*/
-	BALL_EXPORT float calculateSASPoints(const AtomContainer& fragment, Surface& surface_points,
+	BALL_EXPORT BALL_DEPRECATED
+	float calculateSASPoints(const AtomContainer& fragment, Surface& surface_points,
 													 float probe_radius = 1.5,  Size number_of_dots = 400);
 
 	/** Calculate a point set on the Solvent Accessible Surface for each
@@ -101,8 +368,10 @@ namespace BALL
 			@param	probe_radius the probe radius used for the SAS
 			@param	number_of_dots the number of dots used per atom
 			@return the total SAS area in $ A^2$
+			@deprecated Use NumericalSAS::getSurfaceMap() instead
 	 */
-	BALL_EXPORT float calculateSASAtomPoints(const AtomContainer& fragment, 
+	BALL_EXPORT BALL_DEPRECATED
+	float calculateSASAtomPoints(const AtomContainer& fragment, 
 											 				 std::vector< std::pair<Vector3, Surface> >& atom_surfaces,
 															 float probe_radius = 1.5,  Size number_of_dots = 400);
    /** @} */
