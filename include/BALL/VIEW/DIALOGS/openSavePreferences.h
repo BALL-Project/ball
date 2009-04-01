77228d9bbf96f8af24f3be8f4e0eab9d5ecf747d
Lukas Marsalek <marsalek@cs.uni-sb.de>
2009-04-01 19:45:28 +0200

    Fixed Windows build after addition of shortcuts code. They were not included at all in the windows build. So far fixed in Release mode. Still needs to be fixed in Debug mode.`

--- include/BALL/VIEW/DIALOGS/openSavePreferences.h
+++ include/BALL/VIEW/DIALOGS/openSavePreferences.h
@@ -1,14 +1,14 @@
 #ifndef BALL_VIEW_DIALOGS_OPENSAVEPREFERENCES_H
 #define BALL_VIEW_DIALOGS_OPENSAVEPREFERENCES_H
 
-#ifdef BALL_COMPILER_MSVC
-# include <BALL/VIEW/UIC/ui_openSavePreferencesData.h>
-#else
-# include <BALL/VIEW/UIC/openSavePreferencesData.h>
-#endif
-
 #ifndef BALL_VIEW_KERNEL_PREFERENCESENTRY
 # include <BALL/VIEW/KERNEL/preferencesEntry.h>
+#endif
+
+#ifdef BALL_COMPILER_MSVC
+# include <BALL/VIEW/UIC/ui_openSavePreferences.h>
+#else
+# include <BALL/VIEW/UIC/openSavePreferencesData.h>
 #endif
 
 #include <QtGui/QWidget>
