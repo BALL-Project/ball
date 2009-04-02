f99570c36c887580f952cf387f8df370d69e3be9
Lukas Marsalek <marsalek@cs.uni-sb.de>
2009-04-02 15:43:27 +0200

    Removed obsolete directories Windows/Applications/* and Windows/Libs/* Updated startup scripts startBALLView.bat and startBALLViewDebug.bat to reflect new directory structure

--- Windows/startBALLView.bat
+++ Windows/startBALLView.bat
@@ -1,7 +1,7 @@
 SET BALL_DATA_PATH=..\data
-SET LIBSDIR=%BALL_PATH%\Windows\libs
 SET RESULTDIR=Release
-copy %BALL_PATH%\Windows\Applications\BALLVIEW\%RESULTDIR%\BALLView.* %LIBSDIR%\"Python Module"\%RESULTDIR%
-SET PATH=%QTDIR%\lib;%LIBSDIR%\libBALL\%RESULTDIR%;%LIBSDIR%\libView\%RESULTDIR%;%LIBSDIR%\"Python Module"\%RESULTDIR%;;%BALL_PATH%\Windows\Contrib\ONC_RPC\bin
+SET BALL_BIN_PATH=%BALL_PATH%\Windows\BALL-VisualStudio\%RESULTDIR%
+SET PATH=%QTDIR%\lib;%BALL_BIN_PATH%;%BALL_PATH%\Windows\Contrib\bin;%BALL_PATH%\Windows\Contrib\dlls
 echo using PATH %PATH%
 BALLView.exe
+
