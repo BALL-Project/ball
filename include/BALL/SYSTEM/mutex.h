42cd1a86d6efb584831a4e8cb07d9306192131ad
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2009-04-03 18:06:21 +0200

    Some fixes for mutex compilation on older setups.

--- include/BALL/SYSTEM/mutex.h
+++ include/BALL/SYSTEM/mutex.h
@@ -104,7 +104,11 @@ namespace BALL
 	{
 		public:
 			TReadWriteLock(bool is_recursive = false)
+// NOTE: Qt only supports non-recursive read write locks since version 4.3. Before that, they were always recursive.
+#if (QT_VERSION >= QT_VERSION_CHECK(4, 3, 0))
 				: QReadWriteLock( is_recursive ? Recursive : NonRecursive )
+#else
+				: QReadWriteLock()
 			{
 			}
 	};
