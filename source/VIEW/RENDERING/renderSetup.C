125ee2f224e5fc5529bb12d8f71df5b77d933e4b
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2009-05-23 18:53:07 +0200

    Added option for number of RTfact rendering threads.

--- source/VIEW/RENDERING/renderSetup.C
+++ source/VIEW/RENDERING/renderSetup.C
@@ -232,7 +232,14 @@ namespace BALL
 		void RenderSetup::run()
 		{
 #ifdef USE_TBB
-			tbb::task_scheduler_init init;
+			const char* num_threads_s = getenv("BALL_NUM_RENDER_THREADS");
+
+			Index num_threads = tbb::task_scheduler_init::automatic;
+			if (num_threads_s != 0)
+				num_threads = atoi(num_threads_s);
+
+			printf("Using %d render threads\n", num_threads);
+			tbb::task_scheduler_init init(num_threads);
 #endif
 			if (gl_target_)
 				gl_target_->ignoreEvents(true);
