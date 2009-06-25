81a3cee4c1086d429fbc494935b82a0ea4539bb0
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2009-06-26 01:44:37 +0200

    Add a script wrapping the splitted modules
    This script just imports both modules such that old scripts are still
    running without issues.

---
+++ source/PYTHON/EXTENSIONS/BALL.py
@@ -0,0 +1,5 @@
+from BALLCore import *
+try:
+	from VIEW import *
+except:
+	print "Warning: Could not find VIEW.\nVerify that BALL is built with VIEW support and check your PYTHONPATH. Continuing..."
