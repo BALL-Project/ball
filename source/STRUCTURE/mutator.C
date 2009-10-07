4672149fa36b15c37a771e001b0b062d5b72214a
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2009-10-07 15:52:54 +0200

    Add an initial version of an unified mutator interface
    This interface is most likley to change in the future.

---
+++ source/STRUCTURE/mutator.C
@@ -0,0 +1,45 @@
+#include <BALL/STRUCTURE/mutator.h>
+
+#include <BALL/STRUCTURE/fragmentDB.h>
+
+namespace BALL
+{
+	Mutator::Mutator(FragmentDB* db)
+		: db_(db), keep_db_(true)
+	{
+	}
+
+	Mutator::~Mutator()
+	{
+		freeDB_();
+	}
+
+	void Mutator::setup()
+	{
+		if(!db_) {
+			keep_db_ = false;
+			db_ = new FragmentDB("");
+		}
+	}
+
+	void Mutator::mutate(MutatorOptions opts)
+	{
+		setup();
+
+		mutate_impl_(opts);
+
+		if(opts & OPTIMIZE) {
+			optimize();
+		}
+	}
+
+	void Mutator::freeDB_()
+	{
+		if(!keep_db_ && db_) {
+			delete db_;
+			db_ = 0;
+		}
+	}
+
+}
+
