93308d62cab5ccbab4f0b057c520b1273f50fadc
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2009-10-13 19:58:43 +0200

    Add a proper icon loader to VIEW
    This loader reads directories complying to the freedesktop.org for
    the specified icon. It handles icons which are available in multiple
    resolutions and features cachhing of already loaded icons.

---
+++ include/BALL/VIEW/KERNEL/iconLoader.h
@@ -0,0 +1,99 @@
+#ifndef BALL_VIEW_KERNEL_ICONLOADER_H
+#define BALL_VIEW_KERNEL_ICONLOADER_H
+
+#include <list>
+#include <boost/shared_ptr.hpp>
+#include <BALL/DATATYPE/hashMap.h>
+
+#include <QtCore/QMutex>
+#include <QtCore/QReadWriteLock>
+#include <QtCore/QStringList>
+
+class QIcon;
+
+namespace BALL
+{
+	class String;
+
+	namespace VIEW
+	{
+		/**
+		 * This class is an icon loader for the VIEW classes.
+		 * It handles icon themes as defined in the freedesktop.org standard.
+		 * It automatically detects all present resolutions of an icon and loads them.
+		 * Furthermore it features an icon cache in order to minimize disk access. There
+		 * are essentially two ways to use this class: Use it in a singleton wise fashion or
+		 * create an specialized instance.
+		 */
+		class IconLoader
+		{
+			public:
+				/**
+				 * Creates an IconLoader instance using BALL_DATA_PATH/graphics/icons as
+				 * icon path.
+				 */
+				IconLoader();
+
+				/**
+				 * Creates an IconLoader using the directories in icon_dirs as icon paths.
+				 *
+				 * @param icon_dirs A list of paths to icon directories.
+				 */
+				explicit IconLoader(const QStringList& icon_dirs);
+
+				/**
+				 * Creates an IconLoader using the directories in icon_dirs as icon paths.
+				 *
+				 * @param icon_dirs A list of paths to icon directories.
+				 */
+				explicit IconLoader(const std::list<String>& icon_dirs);
+
+				/**
+				 * Destructor. It deletes all cached icons and thus invalidates all external
+				 * pointers to these icons.
+				 */
+				~IconLoader();
+
+				/**
+				 * Return the global instance of the icon loader.
+				 */
+				static IconLoader& instance();
+
+				/**
+				 * Add another icon directory. The directory must be organised
+				 * in a freedesktop.org compliant fashion. Essentially this means its layout
+				 * has to look like this:
+				 * 	path/nxn/context/icon.png
+				 *
+				 * @param path The path to an icon directory.
+				 */
+				void appendIconPath(const String& path);
+
+				/**
+				 * Retrieve the icon identified by name from the directory.
+				 *
+				 * @param The icons name. A usual example looks like: "actions/document-save"
+				 */
+				const QIcon& getIcon(const String& name);
+
+			private:
+				//Threads and singleton
+				static QMutex mutex_;
+				static boost::shared_ptr<IconLoader> loader_;
+				QReadWriteLock hash_map_lock_;
+
+				//Private members
+				const QIcon* const invalid_;
+				std::list<int> sizes_;
+				QStringList icon_dirs_;
+				HashMap<String, QIcon*> icon_map_;
+
+				//Private methods
+				void setup_();
+				QIcon* loadIcon_(const String& name);
+		};
+	}
+}
+
+#endif //BALL_VIEW_KERNEL_ICONLOADER_H
+
---
+++ source/VIEW/KERNEL/iconLoader.C
@@ -0,0 +1,157 @@
+#include <BALL/VIEW/KERNEL/iconLoader.h>
+
+#include <QtCore/QDir>
+#include <QtCore/QString>
+#include <QtGui/QIcon>
+
+#include <BALL/SYSTEM/path.h>
+#include <BALL/DATATYPE/string.h>
+
+namespace BALL
+{
+	namespace VIEW
+	{
+
+		QMutex IconLoader::mutex_;
+		boost::shared_ptr<IconLoader> IconLoader::loader_;
+
+		IconLoader::IconLoader()
+			: invalid_(new QIcon())
+		{
+			Path p;
+			icon_dirs_.append(QString(p.getDataPath().c_str()) + "/graphics/icons");
+
+			setup_();
+		}
+
+		IconLoader::IconLoader(const QStringList& icon_dirs)
+			: invalid_(new QIcon()), icon_dirs_(icon_dirs)
+		{
+			setup_();
+		}
+
+		IconLoader::IconLoader(const std::list<String>& icon_dirs)
+			: invalid_(new QIcon())
+		{
+			for(std::list<String>::const_iterator it = icon_dirs.begin(); it != icon_dirs.end(); ++it) {
+				icon_dirs_.append(it->c_str());
+			}
+
+			setup_();
+		}
+
+		IconLoader::~IconLoader()
+		{
+			for(HashMap<String, QIcon*>::iterator it = icon_map_.begin(); it != icon_map_.end(); ++it) {
+				delete it->second;
+			}
+
+			delete invalid_;
+		}
+
+		void IconLoader::setup_()
+		{
+			sizes_.push_back(16);
+			sizes_.push_back(22);
+			sizes_.push_back(32);
+			sizes_.push_back(48);
+			sizes_.push_back(64);
+			sizes_.push_back(128);
+		}
+
+		void IconLoader::appendIconPath(const String& path)
+		{
+			icon_dirs_.append(path.c_str());
+		}
+
+		const QIcon& IconLoader::getIcon(const String& name)
+		{
+			//See if we have already loaded this icon
+			hash_map_lock_.lockForRead();
+			HashMap<String, QIcon*>::iterator res = icon_map_.find(name);
+			hash_map_lock_.unlock();
+
+			if(res != icon_map_.end()) {
+				return *res->second;
+			}
+
+			hash_map_lock_.lockForWrite();
+			//We cannot be sure that another thread did not yet load the icon.
+			//This must be rechecked or memory leaks will occur
+			res = icon_map_.find(name);
+			if(res != icon_map_.end()) {
+				hash_map_lock_.unlock();
+				return *res->second;
+			}
+			QIcon* tmp = loadIcon_(name);
+			hash_map_lock_.unlock();
+
+			if(!tmp) {
+				Log.error() << "IconLoader: Could not find the icon " << name << std::endl;
+				return *invalid_;
+			}
+
+			return *tmp;
+		}
+
+		IconLoader& IconLoader::instance()
+		{
+			if(!loader_)
+			{
+				mutex_.lock();
+
+				// Double check that the instance has not been created while we were aquiring the lock
+				// Yes, it has to look like this ;-)
+				if(!loader_)
+				{
+					loader_ = boost::shared_ptr<IconLoader>(new IconLoader());
+				}
+
+				mutex_.unlock();
+			}
+
+			return *loader_;
+		}
+
+		QIcon* IconLoader::loadIcon_(const String& name)
+		{
+			const QString filename = QString(name.c_str()) + ".png";
+
+			for(QStringList::iterator it = icon_dirs_.begin(); it != icon_dirs_.end(); ++it) {
+				QDir base_dir(*it);
+
+				if(!base_dir.exists()) {
+					Log.error() << "Could not locate the icon directory!" << std::endl;
+					return 0;
+				}
+
+				QString category_name;
+				QIcon* result = 0;
+
+				for(std::list<int>::iterator sit = sizes_.begin(); sit != sizes_.end(); ++sit) {
+					QDir size_dir = *it + "/" + QString::number(*sit) + "x" + QString::number(*sit);
+
+					if(!size_dir.exists()) {
+						continue;
+					}
+
+					if(size_dir.exists(filename)) {
+						if(!result) {
+							result = new QIcon();
+						}
+						result->addFile(size_dir.path() + "/" + filename);
+					}
+				}
+
+				if(result) {
+					icon_map_[name] = result;
+					return result;
+				}
+			}
+
+			return 0;
+		}
+
+	}
+}
+
