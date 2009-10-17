fe241ee56eeedaea0ae274028c623a931f9fe2c5
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2009-10-17 15:23:04 +0200

    Added libBALL include directories to CMake.

---
+++ include/BALL/DATATYPE/sources.cmake
@@ -0,0 +1,6 @@
+### list all filenames of the directory here ###
+SET(GROUP DATATYPE)
+
+FILE(GLOB HEADERS_LIST "include/BALL/${GROUP}/*.h" "include/BALL/${GROUP}/*.iC")	
+
+ADD_BALL_HEADERS("${GROUP}" "${HEADERS_LIST}")
