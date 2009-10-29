20a566630d493802480e089542c6db72c72bb27b
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2009-10-29 21:42:07 +0100

    Added missing FindRTfact.make

---
+++ cmake/FindRTfact.cmake
@@ -0,0 +1,53 @@
+## Detect the raytracing library rtfact
+
+INCLUDE(CheckLibraryExists)
+
+## Set some possible paths to use when looking for the RTfact installation
+SET(RTFACT_POSSIBLE_ROOT_DIRS
+	${RTFACT_ROOT_DIR}
+	$ENV{RTFACT_ROOT_DIR}
+)
+
+## First, try to find the required header files (RTremote/Renderer.hpp)
+FIND_PATH(RTFACT_INCLUDE_PATH 
+	NAMES RTremote/Renderer.hpp
+	PATHS ${RTFACT_POSSIBLE_ROOT_DIRS}
+	PATH_SUFFIXES include
+	DOC "RTfact header include dir")
+
+IF (NOT RTFACT_INCLUDE_PATH)
+	MESSAGE(STATUS "Could not find RTfact header file (RTremote/Renderer.hpp)! Real time ray tracing will be disabled!")
+ELSE()
+	SET(RTFACT_INCLUDE_DIR ${RTFACT_INCLUDE_PATH} CACHE STRING "Full path to the RTfact headers")
+
+	## Now, try to find the rtfact library itself.
+	FIND_LIBRARY(RTFACT_OPT_LIBRARY 
+		NAMES RTfactRemote
+		PATHS ${RTFACT_POSSIBLE_ROOT_DIRS} ${RTFACT_POSSIBLE_ROOT_DIRS}/remote/build
+		PATH_SUFFIXES lib
+		DOC "RTfact library, optimized") 
+
+	## And a possible debug version
+	FIND_LIBRARY(RTFACT_DEBUG_LIBRARY
+		NAMES RTfactRemoted
+		PATHS ${RTFACT_POSSIBLE_ROOT_DIRS}
+		PATH_SUFFIXES lib
+		DOC "RTfact library, debug") 
+
+	IF (RTFACT_OPT_LIBRARY OR RTFACT_DEBUG_LIBRARY)
+		BALL_COMBINE_LIBS(RTFACT_LIBRARIES ${RTFACT_OPT_LIBRARY} ${RTFACT_DEBUG_LIBRARY})
+		SET(RTFACT_FOUND TRUE)
+	ELSE()
+		MESSAGE(STATUS "Could not find RTfact libraries!")
+		SET(RTFACT_FOUND FALSE)
+	ENDIF()
+
+	INCLUDE(FindPackageHandleStandardArgs)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(RTFACT DEFAULT_MSG
+		RTFACT_INCLUDE_DIR
+		RTFACT_LIBRARIES)
+
+	MARK_AS_ADVANCED(RTFACT_INCLUDE_DIR)
+	MARK_AS_ADVANCED(RTFACT_LIBRARIES)
+
+ENDIF()
