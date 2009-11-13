89347f00eb4f8e1bb4a915fc5f7049964e100a1b
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2009-11-13 22:41:13 +0100

    Added package description file for CPack.

---
+++ cmake/BALLPackageDescriptionFile.cmake
@@ -0,0 +1,10 @@
+
+This package contains
+
+BALL     - the Biochemical Algorithms Library and
+BALLView - BALL's graphical molecular viewing and editing application
+
+For more information, please visit our website at 
+
+					http://www.ball-project.org
+
