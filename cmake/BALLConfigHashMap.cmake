32c01bd3c12e2fc7a4fed0bee67a2ff0bb8a26b8
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2009-11-20 13:45:29 +0100

    Fixed the HashMap detection in CMake.
    The main problem was that cached variables were not unset in the foreach
    loops. So only the first entry in each list was looked at (namespaces,
    etc. Now only the HashMap implementation needs to be fixed for the
    other non tr1 hash_maps/unordered_sets.

--- cmake/BALLConfigHashMap.cmake
+++ cmake/BALLConfigHashMap.cmake
@@ -6,13 +6,28 @@
 
 INCLUDE(CheckIncludeFileCXX)
 
-MESSAGE(STATUS "Checking for hash map")
+MACRO(BALL_HASH_MAP_COMPILES MAP_NAME MAP_NAMESPACE MAP_INCLUDE RESULT)
+	CHECK_CXX_SOURCE_COMPILES("#include <${MAP_INCLUDE}>
+		int main(int, char**)
+		{
+			${MAP_NAMESPACE}::${MAP_NAME}<int, int> a;
+
+			return 0;
+		}" ${RESULT} ${COMPILE_DEFINITIONS}
+	)
+ENDMACRO(BALL_HASH_MAP_COMPILES)
+
+
+
+
+
+MESSAGE(STATUS "Checking for unordered_map")
 
 ## First, try to find an unordered_map implementation
 #  For now, we have to disable <unordered_map> without prefix to avoid conflicts with boost
 #  CHECK_INCLUDE_FILE_CXX("unordered_map" BALL_HAS_STD_UNORDERED_MAP ${COMPILE_DEFINITIONS})
 IF(BALL_HAS_STD_UNORDERED_MAP)
-	MESSAGE(STATUS "Found as <unordered_map>!")
+	MESSAGE(STATUS "Checking for unordered_map - Found as <unordered_map>!")
 	SET(BALL_MAP_NAMESPACE "std")
 	SET(BALL_MAP_NAME "std::unordered_map<Key,T>")
 	SET(BALL_EXTEND_HASH_IN_STD_NS TRUE)
@@ -20,7 +35,11 @@ IF(BALL_HAS_STD_UNORDERED_MAP)
 ELSE()
 	CHECK_INCLUDE_FILE_CXX("tr1/unordered_map" BALL_HAS_TR1_UNORDERED_MAP ${COMPILE_DEFINITIONS})
 	IF (BALL_HAS_TR1_UNORDERED_MAP)
-		MESSAGE(STATUS "Found as <tr1/unordered_map>!")
+		UNSET(BALL_HAS_TR1_UNORDERED_MAP CACHE)
+		BALL_HASH_MAP_COMPILES("eeunordered_map" "std::tr1" "tr1/unordered_map" BALL_HAS_TR1_UNORDERED_MAP)
+	ENDIF()
+	IF (BALL_HAS_TR1_UNORDERED_MAP)
+		MESSAGE(STATUS "Checking for unordered_map - Found as <tr1/unordered_map>!")
 		SET(BALL_MAP_NAMESPACE "tr1")
 		SET(BALL_MAP_NAME "std::tr1::unordered_map<Key,T>")
 		SET(BALL_EXTEND_HASH_IN_STD_NS TRUE)
@@ -28,7 +47,11 @@ ELSE()
 	ELSE()
 		CHECK_INCLUDE_FILE_CXX("boost/unordered_map.hpp" BALL_HAS_BOOST_UNORDERED_MAP ${COMPILE_DEFINITIONS})
 		IF (BALL_HAS_BOOST_UNORDERED_MAP)
-			MESSAGE(STATUS "Found as <boost/unordered_map.hpp>!")
+			UNSET(BALL_HAS_BOOST_UNORDERED_MAP CACHE)
+			BALL_HASH_MAP_COMPILES("unordered_map" "boost" "boost/unordered_map.hpp" BALL_HAS_BOOST_UNORDERED_MAP)
+		ENDIF()
+		IF (BALL_HAS_BOOST_UNORDERED_MAP)
+			MESSAGE(STATUS "Checking for unordered_map - Found as <boost/unordered_map.hpp>!")
 			SET(BALL_MAP_NAMESPACE "boost")
 			SET(BALL_MAP_NAME "boost::unordered_map<Key,T>")
 			SET(BALL_EXTEND_HASH_IN_STD_NS FALSE)
@@ -38,45 +61,40 @@ ELSE()
 ENDIF()
 
 IF (NOT BALL_HAS_UNORDERED_MAP)
-	SET(EXT_INCLUDE_PREFIX_FOUND FALSE)
+	MESSAGE(STATUS "Checking for hash_map GNU extension")
 	FOREACH(TMP_EXT_INCLUDE_PREFIX "" "ext/")
-		IF (NOT EXT_INCLUDE_PREFIX_FOUND)
-			CHECK_INCLUDE_FILE_CXX(${TMP_EXT_INCLUDE_PREFIX}hash_map EXT_INCLUDE_PREFIX_FOUND ${COMPILE_DEFINITIONS})
-			IF (EXT_INCLUDE_PREFIX_FOUND)
-				MESSAGE(STATUS "Found as <"${TMP_EXT_INCLUDE_PREFIX}"hash_map>")
-				IF (NOT TMP_EXT_INCLUDE_PREFIX STREQUAL "")
-					SET(BALL_EXT_INCLUDE_PREFIX TMP_EXT_INCLUDE_PREFIX)
-				ENDIF()
+		UNSET(EXT_INCLUDE_PREFIX_FOUND CACHE)
+		CHECK_INCLUDE_FILE_CXX(${TMP_EXT_INCLUDE_PREFIX}hash_map EXT_INCLUDE_PREFIX_FOUND ${COMPILE_DEFINITIONS})
+		IF (EXT_INCLUDE_PREFIX_FOUND)
+			MESSAGE(STATUS "Checking for hash_map GNU extension - Found as <${TMP_EXT_INCLUDE_PREFIX}hash_map>")
+			IF (NOT TMP_EXT_INCLUDE_PREFIX STREQUAL "")
+				SET(BALL_EXT_INCLUDE_PREFIX TMP_EXT_INCLUDE_PREFIX)
 			ENDIF()
+			BREAK()
 		ENDIF()
 	ENDFOREACH()
 
-	## Find the correct namespace to use
-	SET(EXT_NAMESPACE_FOUND FALSE)
-	FOREACH(TMP_EXT_NAMESPACE "std" "stdext" "__gnu_cxx")
-		IF (NOT EXT_NAMESPACE_FOUND)
-			CHECK_CXX_SOURCE_COMPILES("#include <${BALL_EXT_INCLUDE_PREFIX}hash_map>
-				int main(int /*argc*/, char** /*argv*/)
-				{
-					${TMP_EXT_NAMESPACE}::hash_map<int, int> a;
-
-					return 0;
-				}"
-			EXT_NAMESPACE_FOUND)
+	IF (EXT_INCLUDE_PREFIX_FOUND)
+		## Find the correct namespace to use
+		MESSAGE(STATUS "Checking for hash_map GNU extension namespace")
+		FOREACH(TMP_EXT_NAMESPACE "std" "stdext" "__gnu_cxx")
+			UNSET(EXT_NAMESPACE_FOUND CACHE)
+			BALL_HASH_MAP_COMPILES("hash_map" "${TMP_EXT_NAMESPACE}" "${BALL_EXT_INCLUDE_PREFIX}hash_map" EXT_NAMESPACE_FOUND)
 			IF (EXT_NAMESPACE_FOUND)
 				SET(BALL_MAP_NAMESPACE ${TMP_EXT_NAMESPACE})
-				SET(BALL_MAP_NAME ${TMP_EXT_NAMESPACE}"::hash_map<Key, T>")
+				SET(BALL_MAP_NAME "${TMP_EXT_NAMESPACE}::hash_map<Key, T>")
 				SET(BALL_HAS_HASH_MAP)
+				MESSAGE(STATUS "Checking for hash_map GNU extension namespace - ${TMP_EXT_NAMESPACE}")
+				BREAK()
 			ENDIF()
-		ENDIF()
-	ENDFOREACH()
+		ENDFOREACH()
+	ENDIF()
 
 	IF (NOT EXT_INCLUDE_PREFIX_FOUND OR NOT EXT_NAMESPACE_FOUND)
 		MESSAGE(STATUS "No suitable hash_map implementation found! Falling back to std::map!")
 		SET(BALL_MAP_NAMESPACE "std")
 		SET(BALL_MAP_NAME "std::map<Key, T>")
 	ELSE()
-		MESSAGE(Check whether LongSize hash is required)
 		CHECK_CXX_SOURCE_COMPILES("#include <${BALL_EXT_INCLUDE_PREFIX}hash_map>
 			namespace ${TMP_EXT_NAMESPACE} {
 				template<>
