cb73b31bcdcbfb642d2e9e9d5dbde6df8f1d30bf
Stefan Nickels <nicste@bioinf.uni-sb.de>
2010-05-06 17:40:08 +0200

    Rewrite of Download ED Dialog to get rid of TCPTransfer

--- include/BALL/VIEW/DIALOGS/downloadElectronDensity.h
+++ include/BALL/VIEW/DIALOGS/downloadElectronDensity.h
@@ -14,14 +14,19 @@
 # include <BALL/DATATYPE/hashSet.h>
 #endif
 
+#include <BALL/VIEW/UIC/ui_downloadElectronDensity.h>
+
+#include <QtNetwork/QNetworkReply>
+
+class QProgressBar;
+class QNetworkAccessManager;
+
 namespace BALL
 {
 	class TCPTransfer;
 
 	namespace VIEW
 	{
-
- 		class FetchHTMLThread;
 
 		/** Dialog to search for and download electron density maps from either the <a href="http://eds.bmc.uu.se/eds/">Uppsala Electron-Density Server</a>
 				or the <a href="www.ebi.ac.uk/msd/emdb/">Electron Microscopy Data Bank</a>.
@@ -38,24 +43,19 @@ namespace BALL
 				
 				///
 				DownloadElectronDensity(QWidget* parent = 0, const char* name = "DownloadElectronDensityDialog", 
-												bool modal = FALSE, Qt::WFlags fl = 0 )
-					throw();
+												bool modal = FALSE, Qt::WFlags fl = 0 );
 
 				///
-				~DownloadElectronDensity()
-					throw();
+				~DownloadElectronDensity();
 
 				///
-				virtual void initializeWidget(VIEW::MainControl& main_control)
-					throw();
+				virtual void initializeWidget(VIEW::MainControl& main_control);
 
 				///
-				virtual void fetchPreferences(INIFile& inifile)
-					throw();
+				virtual void fetchPreferences(INIFile& inifile);
 
 				///
-				virtual void writePreferences(INIFile& inifile)
-					throw();
+				virtual void writePreferences(INIFile& inifile);
 				
 				/** Set the prefix for the Uppsala Electron Density Server url<br>
 						Standard: http://www.eds.bmc.uu.se/eds/sfd/
@@ -78,8 +78,7 @@ namespace BALL
 				void setEMDBSuffix(String s) { emdb_suffix_ = s;}
 				
 				///
-				void checkMenu(MainControl& mc)
-					throw();
+				void checkMenu(MainControl& mc);
 			
 			public slots:
 
@@ -87,36 +86,38 @@ namespace BALL
 				void slotDownload();
 
 				///
-				void serverChanged();
-				
-				///
 				void idChanged();
-				
+
+				///
+				void serverChanged();
+
 				///
 				void typeChanged();
-				
+
 				///
 				void pdbDownloadChecked();
 
 				/// 
 				virtual void abort();
 
+				///
+				void downloadFinished();
+
+				///
+				void downloadProgress(qint64 received, qint64 total);
+
 			protected:
 
 				//_
-				void downloadStarted_()
-					throw();
+				void downloadStarted_();
 
 				//_  
-				void downloadEnded_()
-					throw();
+				void downloadEnded_();
 
 				bool threadedDownload_(const String& url);
 				void removeFile_(const String& filename);
 
-				void setProxyAndTransfer_(TCPTransfer& tcp);
 				
-				FetchHTMLThread 				*thread_;
 				bool 										aborted_;
 				bool 										error_;
 
@@ -128,6 +129,13 @@ namespace BALL
 				QAction* menu_id_;
 				String   eds_prefix_, eds_infix_, eds_suffix_; 
 				String emdb_prefix_, emdb_infix_, emdb_suffix_;
+				
+				// the current network reply
+				QNetworkReply* current_reply_;
+
+				// the current progress bar
+				QProgressBar*  progress_bar_;
+				QNetworkAccessManager* network_manager_;
 		};
 
 	} 
--- source/VIEW/DIALOGS/downloadElectronDensity.C
+++ source/VIEW/DIALOGS/downloadElectronDensity.C
@@ -3,10 +3,8 @@
 
 #include <BALL/VIEW/DIALOGS/downloadElectronDensity.h>
 
-#include <BALL/SYSTEM/TCPTransfer.h>
 #include <BALL/KERNEL/system.h>
 #include <BALL/FORMAT/lineBasedFile.h>
-#include <BALL/FORMAT/PDBFile.h>
 #include <BALL/VIEW/KERNEL/mainControl.h>
 #include <BALL/VIEW/WIDGETS/datasetControl.h>
 #include <BALL/VIEW/DATATYPE/standardDatasets.h>
@@ -17,23 +15,25 @@
 
 #include <fstream>
 #include <iostream>
-//#include "/usr/include/boost/iostreams/filtering_streambuf.hpp"
-//#include "/usr/include/boost/iostreams/copy.hpp"
-//#include "/usr/include/boost/iostreams/filter/gzip.hpp"
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 
-#include <QtGui/qlineedit.h> 
-#include <QtGui/qradiobutton.h>
-#include <QtGui/qcheckbox.h>
-#include <QtGui/qcombobox.h>
-#include <QtGui/qimage.h>
-#include <QtGui/qpushbutton.h>
-#include <QtGui/qapplication.h>
+#include <QtGui/QLineEdit> 
+#include <QtGui/QRadioButton>
+#include <QtGui/QCheckBox>
+#include <QtGui/QComboBox>
+#include <QtGui/QImage>
+#include <QtGui/QPushButton>
+#include <QtGui/QApplication>
+#include <QtGui/QProgressBar>
 
-#include <QtNetwork/qhttp.h>
-#include <QtNetwork/qftp.h>
+#include <QtCore/QUrl>
+#include <QtCore/QFile>
+
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
 
 #ifdef BALL_HAS_SSTREAM
 # include <sstream>
@@ -49,19 +49,19 @@ namespace BALL
 	{
 
 		DownloadElectronDensity::DownloadElectronDensity(QWidget* parent, const char* name, bool, Qt::WFlags fl)
-			throw()
 			: QDialog(parent, fl),
-			Ui_DownloadElectronDensityData(),
-			ModularWidget(name),
-			thread_(0),
-			aborted_(false),
-			error_(false),
-			eds_prefix_("http://eds.bmc.uu.se/eds/dfs/"),
-			eds_infix_("/"),
-			eds_suffix_(".omap"),
-			emdb_prefix_("ftp://ftp1.ebi.ac.uk/pub/databases/emdb/structures/EMD-"),
-			emdb_infix_("/map/"),
-			emdb_suffix_(".map.gz")
+				Ui_DownloadElectronDensityData(),
+				ModularWidget(name),
+				aborted_(false),
+				error_(false),
+				eds_prefix_("http://eds.bmc.uu.se/eds/dfs/"),
+				eds_infix_("/"),
+				eds_suffix_(".omap"),
+				emdb_prefix_("ftp://ftp1.ebi.ac.uk/pub/databases/emdb/structures/EMD-"),
+				emdb_infix_("/map/"),
+				emdb_suffix_(".map.gz"),
+				progress_bar_(0),
+				network_manager_(0)
 		{
 #ifdef BALL_VIEW_DEBUG
 			Log.error() << "new DownloadElectronDensity" << this << std::endl;
@@ -81,91 +81,38 @@ namespace BALL
 			hide();
 			entryId->setFocus();
 
-			thread_ = new FetchHTMLThread();
-			thread_->setMainControl(getMainControl());
 		}
 
 		DownloadElectronDensity::~DownloadElectronDensity()
-			throw()
 		{
 #ifdef BALL_VIEW_DEBUG
-			Log.info() << "Destructing object " << this << " of class DownloadPDBFile" << std::endl; 
+			Log.info() << "Destructing object " << this << " of class DownloadElectronDensity" << std::endl; 
 #endif 
 
-			if (thread_ != 0) 
+			if (current_reply_)
 			{
-				if (thread_->isRunning())
-				{
-					thread_->terminate();
-					thread_->wait();
-				}
-				delete thread_;
+				current_reply_->abort();
+				delete(current_reply_);
 			}
+
+			if (progress_bar_)
+				delete progress_bar_;
 		}
 
 		void DownloadElectronDensity::initializeWidget(MainControl&)
-			throw()
 		{
-			menu_id_ = insertMenuEntry(MainControl::FILE_OPEN, "Download PDB", this,
-					SLOT(show()), Qt::CTRL+Qt::Key_T);
+			String description = "Shortcut|File|Open|Download_Electron_Density";
+			menu_id_ = insertMenuEntry(MainControl::FILE_OPEN, "Download Electron Density", this,
+																	 SLOT(show()), description,
+																	 QKeySequence("Ctrl+T"));
+			
 			setMenuHint("Download an electron density map from EDS or EMDB");
 			setMenuHelp("tips.html#download_edmap");
 			setIcon("download.png", true);
 		}
 
-		bool DownloadElectronDensity::threadedDownload_(const String& url)
-		{
-			error_ = false;
-			downloadStarted_();
-			thread_->setURL(url);
-			thread_->start();
-			
-			Size last_bytes = 0;
-			while (thread_->isRunning())
-			{
-				qApp->processEvents();
-
-				Size bytes = thread_->getTCPTransfer().getReceivedBytes();
-				if (bytes != last_bytes)
-				{
-					setStatusbarText("received " + String(bytes) + " bytes", true);
-					last_bytes = bytes;
-				}
-
-				thread_->wait(200);
-			}
-
-			
-			if (aborted_) return false;
-
-			if (thread_->getTCPTransfer().getReceivedBytes() == 0 ||
-					thread_->getTCPTransfer().getStatusCode() != TCPTransfer::OK) 
-			{
-				if (thread_->getTCPTransfer().getReceivedBytes() == 0)
-				{
-					setStatusbarText(String("Could not download the given file. Maybe it does not exist? ") +
-							thread_->getTCPTransfer().getErrorCode() + " occured.", true);
-				}
-				else
-				{
-					setStatusbarText(String("Failed to download file, an ") + 
-							thread_->getTCPTransfer().getErrorCode() + " occured. " +
-							"Maybe you need a proxy?" , true);
-				}
-				error_ = true;
-				return false;
-			}
-
-			return true;
-		}
-
 		void DownloadElectronDensity::slotDownload()
 		{
-			RegularData3D* d3 = new RegularData3D();
-			RegularData3DDataset* set = new RegularData3DDataset();
-
-			//try
-			//{
 				String id = ascii(entryId->currentText());
 				String url = "";
 				String filename_onserver = ""; 
@@ -195,88 +142,132 @@ namespace BALL
 					filename_onserver += emdb_suffix_;
 					url += filename_onserver;
 				}
-				thread_->setFilename(temp_filename);
+				//thread_->setFilename(temp_filename);
 				cout << url << endl;
-				bool ok = threadedDownload_(url);
-				downloadEnded_();
+				//bool ok = threadedDownload_(url);
+				//downloadEnded_();
 			
+				if (progress_bar_)
+					delete(progress_bar_);
 
-				if (!ok) 
-				{
-					delete d3;
-					delete set;
-					cout << "Mist" << endl;
-					return;
-				}
+				progress_bar_ = new QProgressBar(progress_label);
+				progress_bar_->resize(progress_label->size());
+				progress_bar_->show();
 
-				if(server->currentIndex() == 0)
-				{
-					DSN6File map_file(temp_filename);
-					map_file.read(*d3);
-					map_file.close();
-				}
-				else
-				{
-					//String unzipped_filename = filename.before(".gz");
-					String unzipped_filename = VIEW::createTemporaryFilename();
-					ifstream file(temp_filename.c_str(), ios_base::in | ios_base::binary);
-					//cout << "ifstream build" << endl;
-					boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
-					//cout << "filtering_streambuf build" << endl;
-					in.push(boost::iostreams::gzip_decompressor());
-					//cout << "unzipper set up" << endl;
-					in.push(file);
-					//cout << "handed file to unzipper" << endl;
-					CCP4File map_file(unzipped_filename, ios::out | ios::binary);
-					boost::iostreams::copy(in, map_file);
-					map_file.reopen(ios::in);
-					//cout << "copied unzipped stream to map_file" << endl;
-					//CCP4File map_file(filename.c_str(), ios_base::in | ios_base::binary);
-					//boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
-					//in.push(boost::iostreams::gzip_decompressor());
-					//map_file.reopen(ios::out | ios::binary);
-					//boost::iostreams::copy(in, map_file);
-					//map_file.reopen(ios::in);
-
-					map_file.read(*d3);
-					map_file.close();
-					removeFile_(unzipped_filename);
-				}
-
-				set->setData(d3);
-				set->setName(id);
-				set->setType(RegularData3DController::type);
-
-				notify_(new DatasetMessage(set, DatasetMessage::ADD));
-				removeFile_(temp_filename);
-
-				setStatusbarText(String("read ") + id + " electron density map", true);
-
-				DatasetController* dc = DatasetControl::getInstance(0)->getController(RegularData3DController::type);
-				RegularData3DController& rcon = *(RegularData3DController*) dc;
-
-				if((eds_maptype->currentIndex() == 1) && (server->currentIndex() == 0))//diff map
-				{
-					rcon.computeIsoContourSurface(*set, ColorRGBA(0,255,0), 3*d3->calculateSD());
-					rcon.computeIsoContourSurface(*set, ColorRGBA(255,0,0), -3*d3->calculateSD());
-				}
-				else
-				{
-					rcon.computeIsoContourSurface(*set, ColorRGBA(0,100,255), d3->calculateSD());
-				}
-
-				close();
-				entryId->clearEditText();
-
-				download->setDefault(true);
-				entryId->setFocus();
-			//catch(...)
-			//{
-			//	setStatusbarText("download PDB file failed", true);
-			//	//delete system;
-			//}
+				network_manager_ = new QNetworkAccessManager(this);
+				current_reply_ = network_manager_->get(QNetworkRequest(QUrl(url.c_str())));
+				connect(current_reply_, SIGNAL(finished()), this, SLOT(downloadFinished()));
+				connect(current_reply_, SIGNAL(downloadProgress(qint64, qint64)), this, SLOT(downloadProgress(qint64, qint64)));
 		}
 
+		void DownloadElectronDensity::downloadFinished()
+		{
+			String id = ascii(entryId->currentText());
+			
+			if (current_reply_->error() != QNetworkReply::NoError)
+			{
+				Log.error() << "Could not download Electron Density map! Reason given was \"" + (String)(current_reply_->errorString()) + "\""<< std::endl;
+				setStatusbarText("Could not download electron Density map! Reason given was \"" + (String)(current_reply_->errorString()) + "\"");
+
+				error_ = true;
+			}
+			else
+			{
+				RegularData3D* d3 = new RegularData3D();
+				RegularData3DDataset* set = new RegularData3DDataset();
+
+
+				try {
+					String temp_filename = VIEW::createTemporaryFilename();
+
+					QFile outfile(temp_filename.c_str());
+					outfile.open(QIODevice::ReadWrite);
+
+					outfile.write(current_reply_->readAll());
+					outfile.close();
+				
+					if(server->currentIndex() == 0)
+					{
+						DSN6File map_file(temp_filename);
+						map_file.read(*d3);
+						map_file.close();
+					}
+					else
+					{
+						//String unzipped_filename = filename.before(".gz");
+						String unzipped_filename = VIEW::createTemporaryFilename();
+						ifstream file(temp_filename.c_str(), ios_base::in | ios_base::binary);
+						//cout << "ifstream build" << endl;
+						boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+						//cout << "filtering_streambuf build" << endl;
+						in.push(boost::iostreams::gzip_decompressor());
+						//cout << "unzipper set up" << endl;
+						in.push(file);
+						//cout << "handed file to unzipper" << endl;
+						CCP4File map_file(unzipped_filename, ios::out | ios::binary);
+						boost::iostreams::copy(in, map_file);
+						map_file.reopen(ios::in);
+						//cout << "copied unzipped stream to map_file" << endl;
+						//CCP4File map_file(filename.c_str(), ios_base::in | ios_base::binary);
+						//boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+						//in.push(boost::iostreams::gzip_decompressor());
+						//map_file.reopen(ios::out | ios::binary);
+						//boost::iostreams::copy(in, map_file);
+						//map_file.reopen(ios::in);
+
+						map_file.read(*d3);
+						map_file.close();
+						removeFile_(unzipped_filename);
+					}
+
+					set->setData(d3);
+					set->setName(id);
+					set->setType(RegularData3DController::type);
+
+					notify_(new DatasetMessage(set, DatasetMessage::ADD));
+					removeFile_(temp_filename);
+
+					setStatusbarText(String("read ") + id + " electron density map", true);
+
+					DatasetController* dc = DatasetControl::getInstance(0)->getController(RegularData3DController::type);
+					RegularData3DController& rcon = *(RegularData3DController*) dc;
+
+					if((eds_maptype->currentIndex() == 1) && (server->currentIndex() == 0))//diff map
+					{
+						rcon.computeIsoContourSurface(*set, ColorRGBA(0,255,0), 3*d3->calculateSD());
+						rcon.computeIsoContourSurface(*set, ColorRGBA(255,0,0), -3*d3->calculateSD());
+					}
+					else
+					{
+						rcon.computeIsoContourSurface(*set, ColorRGBA(0,100,255), d3->calculateSD());
+					}
+
+					close();
+					entryId->clearEditText();
+
+					download->setDefault(true);
+					entryId->setFocus();
+				}
+				catch(...)
+				{
+					setStatusbarText("download Electron Density map failed", true);
+					delete d3;
+					delete set;
+				}
+			}
+				
+			current_reply_->deleteLater();
+			current_reply_ = 0;
+			
+			network_manager_->deleteLater();
+			network_manager_ = 0;
+
+			progress_bar_->deleteLater();
+			progress_bar_ = 0;
+
+			downloadEnded_();
+		}
+		
 		void DownloadElectronDensity::idChanged()
 		{
 			download->setEnabled(entryId->currentText() != "");
@@ -294,29 +285,26 @@ namespace BALL
 
 		void DownloadElectronDensity::pdbDownloadChecked()
 		{
+			//TODO: Implement
 		}
 
+		void DownloadElectronDensity::downloadProgress(qint64 received, qint64 total)
+		{
+			progress_bar_->setRange(0, total);
+			progress_bar_->setValue(received);
+
+			setStatusbarText("received " + String((unsigned int)received) + " / " + String((unsigned int)total) + " bytes", true);
+		}
 
 		void DownloadElectronDensity::abort()
 		{
-			if (thread_ == 0) return;
-			aborted_ = true;
-
-			thread_->abort();
-			thread_->wait(5500);
-
-			if (thread_->isRunning())
-			{
-				thread_->terminate();
-				thread_->wait();
-			}
-			removeFile_(thread_->getFilename());
+			if (current_reply_)
+				current_reply_->abort();
 
 			downloadEnded_();
 		}
 
 		void DownloadElectronDensity::downloadStarted_()
-			throw()
 		{
 			aborted_ = false;
 			error_   = false;
@@ -328,7 +316,6 @@ namespace BALL
 		}
 
 		void DownloadElectronDensity::downloadEnded_()
-			throw()
 		{
 			if (!aborted_ && !error_)
 			{
@@ -342,10 +329,6 @@ namespace BALL
 			qApp->processEvents();
 			entryId->setFocus();
 
-			if (error_)
-			{
-				removeFile_(thread_->getFilename());
-			}
 		}
 
 		void DownloadElectronDensity::removeFile_(const String& filename)
@@ -357,52 +340,42 @@ namespace BALL
 			catch(...) {}
 		}
 
-		void DownloadElectronDensity::setProxyAndTransfer_(TCPTransfer& tcp)
+		void DownloadElectronDensity::fetchPreferences(INIFile& inifile)
 		{
-			MainControl* mc = getMainControl();
-			tcp.setProxy(mc->getProxy(), mc->getProxyPort());
-			tcp.transfer();
+			if (!inifile.hasSection("ElectronDensityFiles") ||
+					!inifile.hasEntry("ElectronDensityFiles", "History"))
+			{
+				return;
+			}
+
+			String files = inifile.getValue("ElectronDensityFiles", "History");
+			vector<String> fields;
+			files.split(fields, "# ");
+			for (Position p = 0; p < fields.size(); p++)
+			{
+				QString file(fields[p].c_str());
+				if (entryId->findText(file) == -1)
+				{
+					entryId->addItem(file);
+				}
+			}
 		}
 
-		void DownloadElectronDensity::fetchPreferences(INIFile& inifile)
-			throw()
-			{
-				if (!inifile.hasSection("ElectronDensityFiles") ||
-						!inifile.hasEntry("ElectronDensityFiles", "History"))
-				{
-					return;
-				}
-
-				String files = inifile.getValue("ElectronDensityFiles", "History");
-				vector<String> fields;
-				files.split(fields, "# ");
-				for (Position p = 0; p < fields.size(); p++)
-				{
-					QString file(fields[p].c_str());
-					if (entryId->findText(file) == -1)
-					{
-						entryId->addItem(file);
-					}
-				}
-			}
-
 		void DownloadElectronDensity::writePreferences(INIFile& inifile)
-			throw()
+		{
+			String files;
+			for (Index p = 0; p < entryId->count(); p++)
 			{
-				String files;
-				for (Index p = 0; p < entryId->count(); p++)
-				{
-					String s = ascii(entryId->itemText(p));
-					if (s == "") continue;
-					files += s + "#";
-				}
-
-				inifile.appendSection("ElectronDensityFiles");
-				inifile.insertValue("ElectronDensityFiles", "History", files);
+				String s = ascii(entryId->itemText(p));
+				if (s == "") continue;
+				files += s + "#";
 			}
+
+			inifile.appendSection("ElectronDensityFiles");
+			inifile.insertValue("ElectronDensityFiles", "History", files);
+		}
 
 		void DownloadElectronDensity::checkMenu(MainControl& mc)
-			throw()
 		{
 			menu_id_->setEnabled(!mc.compositesAreLocked());
 		}
