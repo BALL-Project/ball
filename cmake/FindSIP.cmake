0cbf028c97eab28448d6c1efc0948b1f2996224d
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2010-05-20 19:59:45 +0200

    Use sip-config to find the sip executable etc.
    Original commit by Oliver Kohlbacher.
    Needed some fixes to work in all possible cases

--- cmake/FindSIP.cmake
+++ cmake/FindSIP.cmake
@@ -1,3 +1,28 @@
+# Find SIP
+# ~~~~~~~~
+#
+# SIP website: http://www.riverbankcomputing.co.uk/sip/index.php
+#
+# Find the installed version of SIP. FindSIP should be called after Python
+# has been found.
+#
+# This file defines the following variables:
+#
+# SIP_VERSION - The version of SIP found expressed as a 6 digit hex number
+#     suitable for comparision as a string.
+#
+# SIP_VERSION_STR - The version of SIP found as a human readable string.
+#
+# SIP_BINARY_PATH - Path and filename of the SIP command line executable.
+#
+# SIP_INCLUDE_DIR - Directory holding the SIP C++ header file.
+#
+# SIP_DEFAULT_SIP_DIR - Default directory where .sip files should be installed
+#     into.
+
+# Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
+# Redistribution and use is allowed according to the terms of the BSD license.
+
 # Try to find the python interface generator SIP (http://www.riverbankcomputing.co.uk/sip/index.php)
 #
 # Unfortunately, we cannot use the standard way of calling sipconfig.py, since this seems to hard-code
@@ -20,11 +45,35 @@
 # SIP_INCLUDE_DIR = Directory holding the SIP C++ header file.
 #
 
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+
+
 IF(SIP_VERSION)
   # Already in cache, be silent
   SET(SIP_FOUND TRUE)
 ELSE(SIP_VERSION)
 
+  FIND_FILE(_find_sip_py FindSIP.py PATHS ${CMAKE_MODULE_PATH})
+
+  EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} ${_find_sip_py} OUTPUT_VARIABLE sip_config ERROR_QUIET)
+  IF(sip_config)
+    STRING(REGEX REPLACE "^sip_version:([^\n]+).*$" "\\1" SIP_VERSION ${sip_config})
+    STRING(REGEX REPLACE ".*\nsip_version_str:([^\n]+).*$" "\\1" SIP_VERSION_STR ${sip_config})
+    STRING(REGEX REPLACE ".*\nsip_bin:([^\n]+).*$" "\\1" SIP_EXECUTABLE ${sip_config})
+    STRING(REGEX REPLACE ".*\ndefault_sip_dir:([^\n]+).*$" "\\1" SIP_DEFAULT_SIP_DIR ${sip_config})
+    STRING(REGEX REPLACE ".*\nsip_inc_dir:([^\n]+).*$" "\\1" SIP_INCLUDE_DIR ${sip_config})
+    SET(SIP_FOUND TRUE)
+  ENDIF(sip_config)
+
+  IF(SIP_FOUND)
+    IF(NOT SIP_FIND_QUIETLY)
+      MESSAGE(STATUS "Found SIP version: ${SIP_VERSION_STR}")
+    ENDIF(NOT SIP_FIND_QUIETLY)
+  ENDIF(SIP_FOUND)
+ENDIF(SIP_VERSION)
+
+IF(NOT SIP_VERSION)
 	# determine the version of python we use
 	EXECUTE_PROCESS ( COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_version; print get_python_version()"
 		OUTPUT_VARIABLE PYTHON_VERSION
@@ -35,10 +84,6 @@ ELSE(SIP_VERSION)
 
   # Use the path to the python installation as a hint for finding sip
   SET(SIP_POSSIBLE_INC_DIRS "${PYTHON_INCLUDE_PATH}")
-	EXECUTE_PROCESS ( COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"
-		OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
-		OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
 
   FIND_PATH(SIP_INCLUDE_DIR
     NAMES sip.h sip/sip.h Python/sip.h
@@ -78,7 +123,7 @@ ELSE(SIP_VERSION)
     ${SIP_CONFIG_BIN_DIRS}
   )
 
-  # (d) Try to extract version information from sip.h:
+  # (c) Try to extract version information from sip.h:
   
   FILE(READ ${SIP_INCLUDE_DIR}/sip.h SIP_H_TEXT)
   STRING(REGEX MATCH   "define SIP_VERSION[ \t]+([0-9a-fx]+)"       SIP_VERSION "${SIP_H_TEXT}" )
@@ -86,17 +131,38 @@ ELSE(SIP_VERSION)
 
   STRING(REGEX MATCH   "define SIP_VERSION_STR[ \t]+\"([^\"]*)\""        SIP_VERSION_STR "${SIP_H_TEXT}"     )
   STRING(REGEX REPLACE "define SIP_VERSION_STR[ \t]+\"([^\"]*)\""  "\\1" SIP_VERSION_STR "${SIP_VERSION_STR}")
+ENDIF(NOT SIP_VERSION)
 
-  INCLUDE(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(SIP DEFAULT_MSG 
-    SIP_EXECUTABLE 
-    SIP_LIBRARIES 
-    SIP_INCLUDE_DIR
-    SIP_VERSION
-    SIP_VERSION_STR)
+IF(NOT SIP_LIBRARIES)
+  # Try to find the sip library:
+	EXECUTE_PROCESS ( COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"
+		OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
 
-  MARK_AS_ADVANCED(SIP_EXECUTABLE)
-  MARK_AS_ADVANCED(SIP_LIBRARIES)
-  MARK_AS_ADVANCED(SIP_INCLUDE_DIR)
+  # Use the path to the python installation as a hint for finding sip
+  GET_FILENAME_COMPONENT(SIP_POSSIBLE_LIB_DIRS "${PYTHON_LIBRARIES}"  ABSOLUTE)
+	LIST(APPEND SIP_POSSIBLE_LIB_DIRS "/usr/lib/pyshared/python${PYTHON_VERSION}")
 
-ENDIF(SIP_VERSION)
+	SET(OLD_CMAKE_FIND_LIBRARY_PREFIXES "${CMAKE_FIND_LIBRARY_PREFIXES}" CACHE INTERNAL "")
+	SET(CMAKE_FIND_LIBRARY_PREFIXES "")
+  FIND_LIBRARY(SIP_LIBRARIES
+    NAMES sip libsip
+    PATHS ${SIP_POSSIBLE_LIB_DIRS} ${PYTHON_SITE_PACKAGES}
+    PATH_SUFFIXES lib dlls
+    DOC "SIP module")
+	SET(CMAKE_FIND_LIBRARY_PREFIXES "${OLD_CMAKE_FIND_LIBRARY_PREFIXES}")
+ENDIF()
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SIP DEFAULT_MSG
+	SIP_EXECUTABLE
+	SIP_LIBRARIES
+	SIP_INCLUDE_DIR
+	SIP_VERSION
+	SIP_VERSION_STR
+)
+
+MARK_AS_ADVANCED(SIP_EXECUTABLE)
+MARK_AS_ADVANCED(SIP_LIBRARIES)
+MARK_AS_ADVANCED(SIP_INCLUDE_DIR)
