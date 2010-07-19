56d65675191e846a5ac4cdc92c6eea9925d4fcc0
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2010-07-19 12:49:59 +0200

    Delete the autotools build system
    This build system was not properly maintained any more. CMake should
    be able to build the  complete BALL project more reliably and faster
    than this build system.

--- source/config/uiccorrect.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find ./ -type f -name \*.[h,iC,C] | xargs grep -l "#include <BALL/VIEW/UIC" 2>/dev/null > uifiles.dat
-
-for file in `cat uifiles.dat`; do
-sed -i s"+#include <BALL/VIEW/UIC/\(.*\)Data.h>+#ifdef BALL_COMPILER_MSVC\n# include <BALL/VIEW/UIC/ui_\1.h>\n#else\n# include <BALL/VIEW/UIC/\1Data.h>\n#endif\n+g" $file;
-done
