9f16158aa99c538bee61d9e51c4c83412398ca78
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2010-09-17 17:44:14 +0200

    Die, BALL/Windows, die die die

--- Windows/BALL-VisualStudio/Release/BALL.py
+++
@@ -1,5 +0,0 @@
-from BALLCore import *
-try:
-	from VIEW import *
-except:
-	print "Warning: Could not find VIEW.\nVerify that BALL is built with VIEW support and check your PYTHONPATH. Continuing..."
--- Windows/startBALLView.bat
+++
@@ -1,7 +0,0 @@
-SET BALL_DATA_PATH=..\data
-SET RESULTDIR=Release
-SET BALL_BIN_PATH=%BALL_PATH%\Windows\BALL-VisualStudio\%RESULTDIR%
-SET PATH=%QTDIR%\lib;%BALL_BIN_PATH%;%BALL_PATH%\Windows\Contrib\bin;%BALL_PATH%\Windows\Contrib\dlls
-echo using PATH %PATH%
-BALLView.exe
-
