4ab2c7d2a7f2efc7a9e09533bacf8aa25c5b02a5
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2010-10-10 21:19:28 +0200

    Fixed stringStub on Linux

--- source/VIEW/KERNEL/stringStub.C
+++ source/VIEW/KERNEL/stringStub.C
@@ -1,6 +1,9 @@
 // stringStub.h / stringStub.C are needed for VC2010!
 #include <string>
+#include <BALL/COMMON/common.h>
 
 
+#ifdef BALL_COMPILER_MSVC
 template std::string::size_type std::string::npos;
 template std::wstring::size_type std::wstring::npos;
+#endif
