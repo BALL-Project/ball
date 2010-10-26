94f1cb076902e3917fd383e7f65fa7b8d14dd1c5
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2010-10-26 17:56:57 +0200

    Fixed export of Matrix and Vector template instances
    BALL did not correctly export Matrix and Vector specializations
    which lead to linker errors.

--- include/BALL/MATHS/LINALG/matrix.h
+++ include/BALL/MATHS/LINALG/matrix.h
@@ -14,23 +14,20 @@ namespace BALL {
 
   template <class valuetype, class mtraits=StandardTraits>
   class Matrix;
-
 }
 
 #include <BALL/MATHS/LINALG/matrix.ih>
 
-#ifdef BALL_OS_WINDOWS
-	namespace BALL
-	{
-		// explicit instantiation to make it work on Windows
-		template class BALL_EXPORT Matrix<float, StandardTraits>;
-		template class BALL_EXPORT Matrix<double, StandardTraits>;
-		template class BALL_EXPORT Matrix<ComplexFloat, StandardTraits>;
-		template class BALL_EXPORT Matrix<ComplexDouble, StandardTraits>;
-	}
-#endif
+namespace BALL
+{
+	// explicit instantiation needed to export the template
+	template class BALL_EXPORT Matrix<float, StandardTraits>;
+	template class BALL_EXPORT Matrix<double, StandardTraits>;
+	template class BALL_EXPORT Matrix<ComplexFloat, StandardTraits>;
+	template class BALL_EXPORT Matrix<ComplexDouble, StandardTraits>;
+}
 
-#include <BALL/MATHS/LINALG/vector.ih>
+#include <BALL/MATHS/LINALG/vector.h>
 
 #include <BALL/MATHS/LINALG/upperTriangularMatrix.ih>
 #include <BALL/MATHS/LINALG/upperTriangularMatrixConstructors.iC>
@@ -50,11 +47,5 @@ namespace BALL {
 #include <BALL/MATHS/LINALG/matrixConstructors.iC>
 #include <BALL/MATHS/LINALG/matrixBasicFunctions.iC>
 #include <BALL/MATHS/LINALG/matrixIO.iC>
-	
-
-#include <BALL/MATHS/LINALG/vectorConstructors.iC>
-#include <BALL/MATHS/LINALG/vectorIO.iC>
-#include <BALL/MATHS/LINALG/vectorBasicFunctions.iC>
-
 
 #endif // MALL_KERNEL_MATRIX_H
--- include/BALL/MATHS/LINALG/vector.h
+++ include/BALL/MATHS/LINALG/vector.h
@@ -1,6 +1,10 @@
 #ifndef BALL_LINALG_VECTOR_H
 #define BALL_LINALG_VECTOR_H
 
+#ifndef BALL_COMMON_GLOBAL_H
+# include <BALL/COMMON/global.h>
+#endif
+
 namespace BALL {
 
   template <class valuetype>
@@ -12,25 +16,19 @@ namespace BALL {
 
 #include <BALL/MATHS/LINALG/vector.ih>
 
-#ifdef BALL_OS_WINDOWS
-	namespace BALL
-	{
-		// explicit instantiation to make it work on Windows
-		template class BALL_EXPORT Vector<float>;
-		template class BALL_EXPORT Vector<double>;
-		template class BALL_EXPORT Vector<ComplexFloat>;
-		template class BALL_EXPORT Vector<ComplexDouble>;
-	}
-#endif
+namespace BALL
+{
+	// explicit instantiation needed to export the template
+	template class BALL_EXPORT Vector<float>;
+	template class BALL_EXPORT Vector<double>;
+	template class BALL_EXPORT Vector<ComplexFloat>;
+	template class BALL_EXPORT Vector<ComplexDouble>;
+}
 
-#include <BALL/MATHS/LINALG/matrix.ih>	
+#include <BALL/MATHS/LINALG/matrix.h>
 
 #include <BALL/MATHS/LINALG/vectorConstructors.iC>
 #include <BALL/MATHS/LINALG/vectorIO.iC>
 #include <BALL/MATHS/LINALG/vectorBasicFunctions.iC>
-
-#include <BALL/MATHS/LINALG/matrixConstructors.iC>
-#include <BALL/MATHS/LINALG/matrixBasicFunctions.iC>
-#include <BALL/MATHS/LINALG/matrixIO.iC>
 
 #endif // BALL_LINALG_VECTOR_H
