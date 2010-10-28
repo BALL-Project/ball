4e3ead9ed863ad24918c021e340e62708ff53dfd
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2010-10-28 21:06:17 +0200

    Fix the SVDSolver fix

--- include/BALL/MATHS/LINALG/SVDSolver.h
+++ include/BALL/MATHS/LINALG/SVDSolver.h
@@ -11,17 +11,18 @@ namespace BALL {
 	class SVDSolver;
 }
 
+#ifdef BALL_OS_WINDOWS
+	namespace BALL
+	{
+		// explicit instantiation to make it work on Windows
+		template class BALL_EXPORT SVDSolver<float, StandardTraits>;
+		template class BALL_EXPORT SVDSolver<double, StandardTraits>;
+		template class BALL_EXPORT SVDSolver<ComplexFloat, StandardTraits>;
+		template class BALL_EXPORT SVDSolver<ComplexDouble, StandardTraits>;
+	}
+#endif
+
 #include <BALL/MATHS/LINALG/SVDSolver.ih>
-
-namespace BALL
-{
-	// explicit instantiation to make it work on Windows
-	template class BALL_EXPORT SVDSolver<float, StandardTraits>;
-	template class BALL_EXPORT SVDSolver<double, StandardTraits>;
-	template class BALL_EXPORT SVDSolver<ComplexFloat, StandardTraits>;
-	template class BALL_EXPORT SVDSolver<ComplexDouble, StandardTraits>;
-}
-
 #include <BALL/MATHS/LINALG/SVDSolver.iC>
 
 #endif
