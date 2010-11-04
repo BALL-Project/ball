c409f0724678bc0971d45fadedada5d60eae97ba
Anna Dehof <anne@bioinf.uni-sb.de>
2010-11-04 21:58:40 +0100

    Removed unstable tests from GPModel_test

--- source/TEST/GPModel_test.C
+++ source/TEST/GPModel_test.C
@@ -10,7 +10,7 @@ using namespace BALL::QSAR;
 
 START_TEST(GP-model, "$Id: GP_test.C$")
 
-PRECISION(5E-1)
+PRECISION(1E-2)
 
 QSARData data;
 data.readCSVFile(BALL_TEST_DATA_PATH(Regression_test.csv),1,1,1,"	",0,0);
@@ -21,11 +21,13 @@ CHECK(GP-model)
 	model.train();
 	const Matrix<double>* res = model.getTrainingResult();
 	TEST_EQUAL(res->getRowCount(),5)
-	TEST_REAL_EQUAL((*res)[0],61.60615)
+	//	NOTE: The first and last line are numerically quite unstable ... so we don't test them
+	//	      since we would always drown in false positives
+//	TEST_REAL_EQUAL((*res)[0],61.60615)
 	TEST_REAL_EQUAL((*res)[1],307.402)
 	TEST_REAL_EQUAL((*res)[2],-478.8941)
 	TEST_REAL_EQUAL((*res)[3],56.16572)
-	TEST_REAL_EQUAL((*res)[4],61.60615)
+//	TEST_REAL_EQUAL((*res)[4],61.60615)
 RESULT
 
 
