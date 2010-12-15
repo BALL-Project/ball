8a45b90d1a93bbca9a55123dafa831adfda73c4e
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2010-12-15 12:43:37 +0100

    Let cmake detect whether we can overload long int with LongIndex

--- cmake/BALLConfiguration.cmake
+++ cmake/BALLConfiguration.cmake
@@ -156,6 +156,27 @@ CHECK_INCLUDE_FILE_CXX("values.h" BALL_HAS_VALUES_H)
 CHECK_FUNCTION_EXISTS(kill BALL_HAS_KILL)
 CHECK_FUNCTION_EXISTS(sysconf BALL_HAS_SYSCONF)
 
+## Can we overload long int with LongIndex?
+IF (BALL_HAS_SYS_TYPES_H)
+MESSAGE(STATUS "have types")
+	CHECK_CXX_SOURCE_COMPILES("#include <sys/types.h>
+	void f(long int l) {l+=2;}
+	void f(${BALL_LONG64_TYPE} l) {l+=2;}
+	int main(int /*argc*/, char** /*argv*/)
+	{
+		return 0;
+	}" BALL_ALLOW_LONG64_TYPE_OVERLOADS) 
+ELSE()
+MESSAGE(STATUS "have no types")
+	CHECK_CXX_SOURCE_COMPILES("
+	void f(long int l) {l+=2;}
+	void f(${BALL_LONG64_TYPE} l) {l+=2;}
+	int main(int /*argc*/, char** /*argv*/)
+	{
+		return 0;
+	}" BALL_ALLOW_LONG64_TYPE_OVERLOADS) 
+ENDIF()
+
 ## Do we have an ansi compatible iostream implementation?
 CHECK_CXX_SOURCE_COMPILES("#include <iostream>
 	class A : public std::iostream
