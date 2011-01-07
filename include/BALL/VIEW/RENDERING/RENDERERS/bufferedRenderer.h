8201249f4dc53eb293bd431c061f8577ca3b065b
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2011-01-07 01:24:21 +0100

    Export BufferedRenderer and fix its coding style.

--- include/BALL/VIEW/RENDERING/RENDERERS/bufferedRenderer.h
+++ include/BALL/VIEW/RENDERING/RENDERERS/bufferedRenderer.h
@@ -26,112 +26,110 @@ namespace BALL
 		 *  setFrameBufferFormat() function. After that, you can call renderToBuffer(),
 		 *  as long as the format of the RenderTarget is the same that you set before.
 		 */
-		class BufferedRenderer
+		class BALL_VIEW_EXPORT BufferedRenderer
 			: public Renderer
 		{
-		public:
-			BufferedRenderer()
-				: offset_(0),
-				  stride_(0)
-		 { }
+			public:
 
-			virtual ~BufferedRenderer() { }
+				BufferedRenderer()
+					: offset_(0),
+						stride_(0)
+				{ }
 
-			/** Tries to choose a format for buffered rendering.
-			 *  @return true if the format could be set,
-			 *          false if it is not supported.
-			 */
-			bool setFrameBufferFormat(const FrameBufferFormat &format)
-			{
-				if ( supports(format) )
+				/** Tries to choose a format for buffered rendering.
+				 *  @return true if the format could be set,
+				 *  @return false if it is not supported.
+				 */
+				bool setFrameBufferFormat(const FrameBufferFormat &format)
 				{
-					bufferFormat = format;
-					formatUpdated();					
+					if (supports(format))
+					{
+						bufferFormat = format;
+						formatUpdated();					
 
-					return true;
+						return true;
+					}
+
+					return false;
 				}
 
-				return false;
-			}
-
-			/** Render the current frame to the target's buffer using the \link Stage \endlink setup.
-			 *  Throws FrameBufferFormatException if the RenderTarget's format is not
-			 *  the one that you set before with setFrameBufferFormat().
-			 */
-			void renderToBuffer(RenderTarget* renderTarget, const Stage& stage)
-                throw(BALL::Exception::InvalidFormat, BALL::Exception::NoBufferAvailable)
-			{
-				prepareBufferedRendering(stage);
-
-				FrameBufferPtr buffer = renderTarget->getBuffer();
-				assert(buffer);
-
-				if (buffer->getFormat() != bufferFormat)
+				/** Render the current frame to the target's buffer using the \link Stage \endlink setup.
+				 *  Throws FrameBufferFormatException if the RenderTarget's format is not
+				 *  the one that you set before with setFrameBufferFormat().
+				 *  @throw Exception::InvalidFormat if the FrameBufferFormat is not supported by the renderer
+				 *  @throw Exception::NoBufferAvailable if the frame buffer was not correctly initialized
+				 */
+				void renderToBuffer(RenderTarget* renderTarget, const Stage& stage)
 				{
-					throw BALL::Exception::InvalidFormat(__FILE__, __LINE__, "Buffer with wrong framebuffer format supplied");
+					prepareBufferedRendering(stage);
+
+					FrameBufferPtr buffer = renderTarget->getBuffer();
+					assert(buffer);
+
+					if (buffer->getFormat() != bufferFormat)
+					{
+						throw Exception::InvalidFormat(__FILE__, __LINE__, "Buffer with wrong framebuffer format supplied");
+					}
+
+					renderToBufferImpl( buffer );
+
+					renderTarget->releaseBuffer(buffer);
 				}
 
-				renderToBufferImpl( buffer );
+				/** Checks if a particular PixelFormat is supported by the renderer. */
+				virtual bool supports(const PixelFormat &format) const = 0;
 
-				renderTarget->releaseBuffer(buffer);
-			}
+				/** Returns a supported resolution within the given limits.
+				 *  The x resolution will be between min.x and max.x and the y resolution
+				 *  will be between min.y and max.y.
+				 *  The returned resolution must be supported by the renderer with the
+				 *  requested PixelFormat.
+				 *  @throw Exception::FormatUnsupported if the FrameBufferFormat is not supported by the renderer
+				 */
+				virtual Resolution getSupportedResolution(const Resolution &min, const Resolution &max,
+				                                          const PixelFormat &format) const = 0;
 
-			/** Checks if a particular PixelFormat is supported by the renderer. */
-			virtual bool supports(const PixelFormat &format) const = 0;
+				/** Set an offset into the data buffer 
+				 */
+				virtual void setOffset(Size offset) {offset_ = offset;}
 
-			/** Returns a supported resolution within the given limits.
-			 *  The x resolution will be between min.x and max.x and the y resolution
-			 *  will be between min.y and max.y.
-			 *  The returned resolution must be supported by the renderer with the
-			 *  requested PixelFormat.
-			 */
-			virtual Resolution getSupportedResolution(
-				const Resolution &min, const Resolution &max,
-				const PixelFormat &format) const
-                throw(BALL::Exception::FormatUnsupported) = 0;
+				/** Set a stride for the data buffer
+				 */
+				virtual void setStride(Size stride) {stride_ = stride;}
 
-			/** Set an offset into the data buffer 
-			 */
-			virtual void setOffset(Size offset) {offset_ = offset;}
-			
-			/** Set a stride for the data buffer
-			 */
-			virtual void setStride(Size stride) {stride_ = stride;}
+			protected:
 
-		protected:
+				/** Checks if a particular FrameBufferFormat is supported by the renderer.
+				 *  This checks the combination of pixel format and resolution.
+				 *  If this returns true, the renderer is able to write to a buffer in
+				 *  this format.
+				 */
+				virtual bool supports(const FrameBufferFormat &format) const = 0;
 
-			/** Checks if a particular FrameBufferFormat is supported by the renderer.
-			 *  This checks the combination of pixel format and resolution.
-			 *  If this returns true, the renderer is able to write to a buffer in
-			 *  this format.
-			 */
-			virtual bool supports(const FrameBufferFormat &format) const = 0;
+				/** This is called once the FrameBufferFormat has been set so the renderer
+				 *  can perform any initialization steps that are needed.
+				 */
+				virtual void formatUpdated() = 0;
 
-			/** This is called once the FrameBufferFormat has been set so the renderer
-			 *  can perform any initialization steps that are needed.
-			 */
-			virtual void formatUpdated() = 0;
+				/** Prepare for the rendering, taking into account settings in Stage, eg. Camera, Lights, Background colors, etc.	
+				 */
+				virtual void prepareBufferedRendering(const Stage& stage) = 0;
 
-			/** Prepare for the rendering, taking into account settings in Stage, eg. Camera, Lights, Background colors, etc.	
-			 */
-			virtual void prepareBufferedRendering(const Stage& stage) = 0;
+				/** Render to the frame buffer.
+				 *  @pre The frame buffer's format is the one stored in bufferFormat and
+				 *       prepareBufferedRendering() has been called.
+				 */
+				virtual void renderToBufferImpl(FrameBufferPtr buffer) = 0;            
 
-			/** Render to the frame buffer.
-			 *  @pre The frame buffer's format is the one stored in bufferFormat and
-			 *       prepareBufferedRendering() has been called.
-			 */
-			virtual void renderToBufferImpl(FrameBufferPtr buffer) = 0;            
-		    
-			/** Returns the current FrameBufferFormat. */
-			const FrameBufferFormat &getFrameBufferFormat() const
-			{ return bufferFormat; }
+				/** Returns the current FrameBufferFormat. */
+				const FrameBufferFormat &getFrameBufferFormat() const { return bufferFormat; }
 
-			Size offset_;
-			Size stride_;
+				Size offset_;
+				Size stride_;
 
-		private:
+			private:
 
-			FrameBufferFormat bufferFormat;
+				FrameBufferFormat bufferFormat;
 		};
 	} //namespace VIEW
 
