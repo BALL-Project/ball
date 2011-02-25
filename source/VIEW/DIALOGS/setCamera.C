14562d11953992b0e66900e948eef1db0eebc257
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2011-02-25 01:17:03 +0100

    Allow to set the up-Vector in setCamera.

--- source/VIEW/DIALOGS/setCamera.C
+++ source/VIEW/DIALOGS/setCamera.C
@@ -22,20 +22,24 @@ SetCamera::SetCamera( QWidget* parent,  const char* name, bool modal, Qt::WFlags
 	setupUi(this);
 	
   // signals and slots connections
-  connect( ok_button, SIGNAL( pressed() ), this, SLOT( okPressed() ) );
-  connect( cancel_button, SIGNAL( pressed() ), this, SLOT( close() ) );
+  connect( buttonBox, SIGNAL( accepted() ), this, SLOT( okPressed() ) );
+  connect( buttonBox, SIGNAL( rejected() ), this, SLOT( close() ) );
 
 	setModal(modal);
 	setObjectName(name);
 	const Camera& camera = ((Scene*) parent)->getStage()->getCamera();
 
-	view_x->setText(String((Index)camera.getViewPoint().x).c_str());
-	view_y->setText(String((Index)camera.getViewPoint().y).c_str());
-	view_z->setText(String((Index)camera.getViewPoint().z).c_str());
+	view_x->setText(QString::number(camera.getViewPoint().x, 'f', 3));
+	view_y->setText(QString::number(camera.getViewPoint().y, 'f', 3));
+	view_z->setText(QString::number(camera.getViewPoint().z, 'f', 3));
 
-	look_x->setText(String((Index)camera.getLookAtPosition().x).c_str());
-	look_y->setText(String((Index)camera.getLookAtPosition().y).c_str());
-	look_z->setText(String((Index)camera.getLookAtPosition().z).c_str());
+	look_x->setText(QString::number(camera.getLookAtPosition().x, 'f', 3));
+	look_y->setText(QString::number(camera.getLookAtPosition().y, 'f', 3));
+	look_z->setText(QString::number(camera.getLookAtPosition().z, 'f', 3));
+	
+	look_up_x->setText(QString::number(camera.getLookUpVector().x, 'f', 3));
+	look_up_y->setText(QString::number(camera.getLookUpVector().y, 'f', 3));
+	look_up_z->setText(QString::number(camera.getLookUpVector().z, 'f', 3));
 }
 
 SetCamera::~SetCamera()
@@ -48,16 +52,21 @@ void SetCamera::okPressed()
 	MainControl *main_control = MainControl::getMainControl(parentWidget());
 	hide();
 
-	float vx, vy, vz, lx, ly, lz;
+	float vx, vy, vz, lx, ly, lz, lux, luy, luz;
 
 	try
 	{
-		vx = ascii(view_x->text()).toFloat();
-		vy = ascii(view_y->text()).toFloat();
-		vz = ascii(view_z->text()).toFloat();
-		lx = ascii(look_x->text()).toFloat();
-		ly = ascii(look_y->text()).toFloat();
-		lz = ascii(look_z->text()).toFloat();
+		vx = (view_x->text()).toFloat();
+		vy = (view_y->text()).toFloat();
+		vz = (view_z->text()).toFloat();
+		
+		lx = (look_x->text()).toFloat();
+		ly = (look_y->text()).toFloat();
+		lz = (look_z->text()).toFloat();
+		
+		lux = (look_up_x->text()).toFloat();
+		luy = (look_up_y->text()).toFloat();
+		luz = (look_up_z->text()).toFloat();
 	}
 	catch(...)
 	{
@@ -67,6 +76,7 @@ void SetCamera::okPressed()
 
 	Vector3 vp(vx,vy,vz);
 	Vector3 lp(lx,ly,lz);
+	Vector3 lu(lux,luy,luz);
 
 	if (vp == lp) 
 	{
@@ -77,6 +87,7 @@ void SetCamera::okPressed()
 	Camera& camera = ((Scene*) parentWidget())->getStage()->getCamera();
 	camera.setViewPoint(vp);
 	camera.setLookAtPosition(lp); 
+	camera.setLookUpVector(lu); 
 }
 
 // NAMESPACE
