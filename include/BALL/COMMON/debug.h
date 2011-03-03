82549929e1eb845b3a17e68f2f49ed1baac1bb77
Andreas Hildebrandt <anhi@bioinf.uni-sb.de>
2011-03-04 00:22:18 +0100

    Add a backtrace macro to BALL/COMMON/debug.h

--- include/BALL/COMMON/debug.h
+++ include/BALL/COMMON/debug.h
@@ -46,10 +46,49 @@
 		throw e;\
 	}\
 
+# ifdef BALL_COMPILER_GXX
+# include <execinfo.h>
+# include <cxxabi.h>
+
+# define BALL_PRINT_BACKTRACE()\
+	{\
+		void *bt_addresses[100];\
+		char **bt;\
+		\
+		int bt_size = backtrace(bt_addresses, 100);\
+		bt = backtrace_symbols(bt_addresses, bt_size);\
+		\
+		char* demangled=0;\
+		size_t num_demangled;\
+		int status;\
+		std::vector<String> split;\
+		\
+		for(int i = 0; i < bt_size; i++)\
+		{\
+			String mangled(bt[i]);\
+			\
+			Log.warn() << i << ": " << bt[i] << " ";\
+			mangled = mangled.getField(1, "()");\
+			mangled = mangled.getField(0, "+");\
+			\
+			char* out = abi::__cxa_demangle(mangled.c_str(), demangled, &num_demangled, &status);\
+			if (status == 0) Log.warn() << "demangled: " << String(out);\
+			if (num_demangled > 0) free(demangled);\
+			demangled = 0;\
+			Log.warn() << std::endl;\
+		}\
+		free(bt);\
+	}\
+
+# else
+# define BALL_PRINT_BACKTRACE()
+# endif
+
 #else
 
 # define BALL_PRECONDITION_EXCEPTION(condition, message)
 # define BALL_POSTCONDITION_EXCEPTION(condition, message)
+# define BALL_PRINT_BACKTRACE()
 
 #endif // BALL_DEBUG
 
