d16e243c36bf84ccd5794460dd04fe7ecac542ed
Nikola Koch <nikoch@bioinf.uni-sb.de>
2011-05-31 16:43:43 +0200

    Initial sequence

---
+++ include/BALL/SEQUENCE/sequence.h
@@ -0,0 +1,98 @@
+#ifndef BALL_SEQUENCE_SEQUENCE_H
+#define BALL_SEQUENCE_SEQUENCE_H
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef BALL_DATATYPE_STRING_H
+	#include <BALL/DATATYPE/string.h>
+#endif
+
+#ifndef BALL_CONCEPT_COMPOSITE_H
+	#include <BALL/CONCEPT/composite.h>
+#endif
+
+#ifndef BALL_SEQUENCE__SEQUENCEITERATOR_H
+	#include <BALL/SEQUENCE/sequenceIterator.h>
+#endif
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+	class BALL_EXPORT Sequence
+{
+
+	public:
+
+
+		//////////////////////////////////Constructors and Deconstructor//////////////////////////////////////////
+
+
+
+		/**
+		 *Default Constructor
+		 */
+		Sequence();
+
+		/**
+		 *Detailed Constructor
+		 *@param name the name of the Sequence
+		 *@param origin the composite from which the sequence originates
+		 */
+		Sequence(String& name, Composite& origin);
+
+		/**
+		 *Copy Constructor
+		 */
+		Sequence(Sequence& seq);
+
+		/**
+		 *Deconstrcutor
+		 **/
+		~Sequence();
+
+		/////////////////////////////////////Getter and Setter/////////////////////////////////////////////////////////////
+
+		/**
+		 * @param name the new name to be set
+		 * @throws Exception::GeneralException if an invalid name was specified
+		 */
+		void setName(const BALL::String& name);
+
+		/**
+		 *@return the name of the sequence
+		 */
+		BALL::String getname();
+
+
+		/**
+		 *@param origin the origin which is to be setted
+		 */
+		void setOrigin(BALL::Composite& origin);
+
+		/**
+		 *@return the origin of the Sequence
+		 */
+		BALL::Composite& getOrign();	
+
+		/////////////////////////////////////////////////Misc///////////////////////////////////////////
+
+		/**
+		 *@return a SequenceIterator which points to the first character of the Sequence
+		 */
+		BALL::SequenceIterator& begin();				
+
+		/**
+		 *@return a SequenceIterator which points to the last character of the Sequence
+		 */
+		BALL::SequenceIterator& end();
+
+	private:
+
+		BALL::String name;
+
+		BALL::Composite origin;
+
+};
+
+
+#endif // BALL_SEQUENCE_SEQUENCE_H
