4e661f4332d793973a2992ff6a8b1a866c7ffaa3
Anna Dehof <anne@bioinf.uni-sb.de>
2011-06-09 17:13:18 +0200

    Improved BALLView python example selectNeighborResidues.py

--- source/EXAMPLES/PYTHON/BALLView/selectNeighbourResidues.py
+++ source/EXAMPLES/PYTHON/BALLView/selectNeighbourResidues.py
@@ -1,30 +1,33 @@
 # select all residues in proximity of less than maxdistance from selected atoms
 maxdistance = 10
 
-system = getSystems()[0]
-
 cl = []
 
-for atom1 in atoms(system):
-  if not atom1.isSelected(): 
-    continue
-  for residue in residues(system):
-    close=False
-    for atom2 in atoms(residue):
-      dist = (atom1.getPosition() - atom2.getPosition()).getLength()
-      if not atom2.isSelected() and dist < maxdistance:
-        close=True
-        break
-    if close:
-      cl.append(residue)
+for system1 in getSystems():
+  for atom1 in atoms(system1):
+    if not atom1.isSelected(): 
+      continue
+    for system in getSystems():
+      for residue in residues(system):
+        close=False
+        for atom2 in atoms(residue):
+          dist = (atom1.getPosition() - atom2.getPosition()).getLength()
+          if not atom2.isSelected() and dist < maxdistance:
+            close=True
+            break
+        if close:
+          cl.append(residue)
+  
+for system in getSystems():
+  system.deselect()
 
-system.deselect()
+print "selected", len(cl), "residues"
 
 for residue in cl:
   for atom in atoms(residue):
     if not atom.isSelected():
       atom.select()
 
-
-getMainControl().update(system)
+for sysem in getSystems():
+  getMainControl().update(system)
 
