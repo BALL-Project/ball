0208010393162cf88b322d4959421a0c6efdfac4
Nikola Koch <nikoch@bioinf.uni-sb.de>
2011-06-14 13:37:22 +0200

    added sources.cmake

---
+++ include/BALL/SEQUENCE/sources.cmake
@@ -0,0 +1,6 @@
+### the directory name ###
+SET(GROUP SEQUENCE)
+	
+FILE(GLOB HEADERS_LIST "include/BALL/${GROUP}/*.h" "include/BALL/${GROUP}/*.iC")	
+
+ADD_BALL_HEADERS("${GROUP}" "${HEADERS_LIST}")
