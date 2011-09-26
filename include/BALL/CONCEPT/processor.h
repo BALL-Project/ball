3278b726a0f0e8379c8bab182f9fa66c597071a1
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2011-09-26 09:59:46 +0200

    Added a const version of Unary-/BinaryProcessor
    This allows to run Processors that simply aggregate data
    on const composites.

--- include/BALL/CONCEPT/processor.h
+++ include/BALL/CONCEPT/processor.h
@@ -110,6 +110,37 @@ namespace BALL
 		//@}
 	};
 
+	/**
+	 * This is the baseclass for UnaryProcessors that do not change the objects they
+	 * are applied to. If your processor should work on const objects this is the
+	 * right class to derive from.
+	 */
+	template<typename T>
+	class ConstUnaryProcessor : public UnaryFunctor<T, Processor::Result>
+	{
+		public:
+			BALL_CREATE(ConstUnaryProcessor)
+
+			virtual ~ConstUnaryProcessor()
+			{
+			}
+
+			virtual bool start()
+			{
+				return true;
+			}
+
+			virtual bool finish()
+			{
+				return true;
+			}
+
+			virtual Processor::Result operator()(const T&)
+			{
+				return Processor::CONTINUE;
+			}
+	};
+
 	/**	Generic Binary Processor Class
 	*/
 	template <typename T1, typename T2>
@@ -164,6 +195,51 @@ namespace BALL
 		/**	operator ()
 		*/
 		virtual Processor::Result operator () (T1&, T2&)
+		{
+			return Processor::CONTINUE;
+		}
+		//@}
+	};
+
+	template <typename T1, typename T2>
+	class ConstBinaryProcessor
+		: public BinaryFunctor<T1, T2, Processor::Result>
+	{
+		public:
+
+		/**	@name	Constructors and Destructors
+		*/
+		//@{
+		BALL_CREATE(ConstBinaryProcessor)
+
+		/**	Destructor
+		*/
+		virtual ~ConstBinaryProcessor()
+		{
+		}
+		//@}
+
+		/**	@name	Processor-specific methods
+		*/
+		//@{
+
+		/**	start method
+		*/
+		virtual bool start()
+		{
+			return true;
+		}
+
+		/**	finish method
+		*/
+		virtual bool finish()
+		{
+			return true;
+		}
+
+		/**	operator ()
+		*/
+		virtual Processor::Result operator () (const T1&, const T2&)
 		{
 			return Processor::CONTINUE;
 		}
