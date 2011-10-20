19b40f68c6e88f9ca5a11b4d7edd56706b9a0650
Anna Dehof <anne@bioinf.uni-sb.de>
2011-10-20 15:42:13 +0200

    Improved the python Bond Order Assignment example

--- source/EXAMPLES/PYTHON/UTILITIES/assignBondOrders.py
+++ source/EXAMPLES/PYTHON/UTILITIES/assignBondOrders.py
@@ -1,7 +1,9 @@
 #
-# Anna Dehof 2009-02-02
-#   get the system and assign bond orders 
+# Anna Dehof 2011-10-20
+#   Compute and assign bond orders for a given 
+#   ligand based on atomic valence penalty rules.
 #
+
 import sys
 from BALL import *
 
@@ -28,33 +30,42 @@ file.read(system)
 file.close()
 
 # print the number of atoms read from the file
-print "read ", system.countAtoms(), " atoms."
-print "read ", system.countBonds(), " bonds."
+print "Read", system.countAtoms(), "atoms and", system.countBonds(), "bonds from file"
+print  "   ", sys.argv[1]
 
 # create an BondOrderAssignmentProcessor
-abop = BALL.AssignBondOrderProcessor()
-abop.options.setBool(BALL.AssignBondOrderProcessor.Option.KEKULIZE_RINGS, True) 
-abop.options.setBool(BALL.AssignBondOrderProcessor.Option.OVERWRITE_SINGLE_BOND_ORDERS, True)
-abop.options.setBool(BALL.AssignBondOrderProcessor.Option.OVERWRITE_DOUBLE_BOND_ORDERS, True)
-abop.options.setBool(BALL.AssignBondOrderProcessor.Option.OVERWRITE_TRIPLE_BOND_ORDERS, True)
-abop.options.set(BALL.AssignBondOrderProcessor.Option.ALGORITHM, BALL.AssignBondOrderProcessor.Algorithm.A_STAR)
-abop.options.setReal(BALL.AssignBondOrderProcessor.Option.BOND_LENGTH_WEIGHTING,0)
-abop.options.setInteger(BALL.AssignBondOrderProcessor.Option.MAX_NUMBER_OF_SOLUTIONS,10)
-abop.options.setBool(BALL.AssignBondOrderProcessor.Option.COMPUTE_ALSO_NON_OPTIMAL_SOLUTIONS, True)
-abop.options.setBool(BALL.AssignBondOrderProcessor.Option.ADD_HYDROGENS, False)
+abop = AssignBondOrderProcessor()
+abop.options.setBool(AssignBondOrderProcessor.Option.KEKULIZE_RINGS, True) 
+abop.options.setBool(AssignBondOrderProcessor.Option.OVERWRITE_SINGLE_BOND_ORDERS, True)
+abop.options.setBool(AssignBondOrderProcessor.Option.OVERWRITE_DOUBLE_BOND_ORDERS, True)
+abop.options.setBool(AssignBondOrderProcessor.Option.OVERWRITE_TRIPLE_BOND_ORDERS, True)
+abop.options.set(AssignBondOrderProcessor.Option.ALGORITHM, AssignBondOrderProcessor.Algorithm.A_STAR)
+abop.options.setReal(AssignBondOrderProcessor.Option.BOND_LENGTH_WEIGHTING,0)
+abop.options.setInteger(AssignBondOrderProcessor.Option.MAX_NUMBER_OF_SOLUTIONS,10)
+abop.options.setBool(AssignBondOrderProcessor.Option.COMPUTE_ALSO_NON_OPTIMAL_SOLUTIONS, True)
+abop.options.setBool(AssignBondOrderProcessor.Option.ADD_HYDROGENS, False)
 
 print "Current penalty: " , abop.evaluatePenalty(system)
 
 # apply the processor
 system.apply(abop)
 
+print "Computed", abop.getNumberOfComputedSolutions(), "bond order assignments:"
+
 # print all solutions penalty
 for i in range(abop.getNumberOfComputedSolutions()):
-  print "solution ", str(i) , ": penalty ", str(abop.getTotalPenalty(i)) #, " , ", abop.getNumberOfAddedHydrogens(i) , " added hydrogens."
+  print "   solution", str(i) , ": penalty", str(abop.getTotalPenalty(i)) #, " , ", abop.getNumberOfAddedHydrogens(i) , " added hydrogens."	
+  # apply the i-th solution
+  if (abop.apply(i)):
+    # and write the solution into an MOL2file	
+    outfile_name = String(sys.argv[2]) + "_" + str(i) + ".mol2";
+    print "   Writing solution", i, "as", outfile_name
+    outfile = MOL2File(outfile_name, File.MODE_OUT)
+    outfile.write(system)
+    outfile.close()
 
-# apply the last solution
-abop.apply(abop.getNumberOfComputedSolutions()-1)
-
+# done
+print "Done."
 
 # for use in BALLView
 #getMainControl().update(system)
