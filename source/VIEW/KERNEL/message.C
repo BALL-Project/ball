55a7a1c8926298d42dc560d509ffd3a4167280a6
Anna Dehof <anne@bioinf.uni-sb.de>
2012-01-26 11:41:59 +0100

    clean up throw remains

--- source/VIEW/KERNEL/message.C
+++ source/VIEW/KERNEL/message.C
@@ -16,21 +16,18 @@ namespace BALL
 	{
 
 Message::Message()
-	
 	: connection_object_(0),
 		deletable_(true)
 {
 }
 
 Message::Message(const Message& message)
-	
 	: connection_object_(message.connection_object_),
 		deletable_(message.deletable_)
 {
 }
 
 Message::~Message()
-	
 {
 }
 
@@ -58,7 +55,6 @@ bool DatasetMessage::isValid() const
 
 
 CompositeMessage::CompositeMessage()
-	
 	: Message(),
 		type_(UNDEFINED),
 		composite_(0),
@@ -67,12 +63,11 @@ CompositeMessage::CompositeMessage()
 		show_selection_infos_(true)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new CompositeMessage " << type_ << std::endl;		
+		Log.error() << "new CompositeMessage " << type_ << std::endl;
 	#endif
 }
 
 CompositeMessage::CompositeMessage(const Composite& composite, CompositeMessageType type, bool update_representations)
-	
 	: Message(),
 		type_(type),
 		composite_((Composite*)&composite),
@@ -81,12 +76,11 @@ CompositeMessage::CompositeMessage(const Composite& composite, CompositeMessageT
 		show_selection_infos_(true)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new CompositeMessage " << type_ << std::endl;		
+		Log.error() << "new CompositeMessage " << type_ << std::endl;
 	#endif
 }
 
 CompositeMessage::CompositeMessage(const CompositeMessage& message)
-	
 	: Message(message),
 		type_(message.type_),
 		composite_(message.composite_),
@@ -95,16 +89,15 @@ CompositeMessage::CompositeMessage(const CompositeMessage& message)
 		show_selection_infos_(message.show_selection_infos_)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new CompositeMessage " << type_ << std::endl;		
+		Log.error() << "new CompositeMessage " << type_ << std::endl;
 	#endif
 }
 
 CompositeMessage::~CompositeMessage()
-	
 {
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "Destructing " << this << " CompositeMessage" << endl;
-	#endif 
+	#endif
 }
 
 SceneMessage::SceneMessage(SceneMessageType type)
@@ -113,7 +106,7 @@ SceneMessage::SceneMessage(SceneMessageType type)
 		stage_()
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new SceneMessage " << type << " " << this << std::endl;		
+		Log.error() << "new SceneMessage " << type << " " << this << std::endl;
 	#endif
 }
 
@@ -123,7 +116,7 @@ SceneMessage::SceneMessage(const SceneMessage& message)
 		stage_(message.stage_)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new SceneMessage2 " << type_ << " " << this << std::endl;		
+		Log.error() << "new SceneMessage2 " << type_ << " " << this << std::endl;
 	#endif
 }
 
@@ -131,14 +124,14 @@ SceneMessage::~SceneMessage()
 {
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "Destructing " << this << " SceneMessage" << endl;
-	#endif 
+	#endif
 }
 
 void SceneMessage::setType(SceneMessageType type)
-{ 
+{
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "SceneMessage::setType " << type << std::endl;
-	#endif 
+	#endif
 
 	type_ = type;
 }
@@ -151,38 +144,33 @@ GenericSelectionMessage::GenericSelectionMessage()
 }
 
 GenericSelectionMessage::GenericSelectionMessage(const GenericSelectionMessage& message)
-	
 	: Message(message),
 		selection_(message.selection_)
 {
 }
 
 GenericSelectionMessage::~GenericSelectionMessage()
-	
 {
 }
 
 
 GeometricObjectSelectionMessage::GeometricObjectSelectionMessage()
-	
 	: Message()
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new GeometricObjectSelectionMessage" << std::endl;		
+		Log.error() << "new GeometricObjectSelectionMessage" << std::endl;
 	#endif
 }
 
 GeometricObjectSelectionMessage::~GeometricObjectSelectionMessage()
-	
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "Destructing object " << (void *)this 
+		Log.error() << "Destructing object " << (void *)this
 								<< " of class " << RTTI::getName<GeometricObjectSelectionMessage>() << endl;
-	#endif 
+	#endif
 }
 
 ControlSelectionMessage::ControlSelectionMessage()
-	
 	: GenericSelectionMessage()
 {
 	#ifdef BALL_VIEW_DEBUG
@@ -190,127 +178,114 @@ ControlSelectionMessage::ControlSelectionMessage()
 	#endif
 }
 
-NewSelectionMessage::NewSelectionMessage() 
-	
+NewSelectionMessage::NewSelectionMessage()
 	: Message(),
 		open_(false)
-{ 
+{
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "new NewSelectionMessage" << std::endl;
 	#endif
 }
 
 RepresentationMessage::RepresentationMessage()
-	
 	: representation_(0),
 		type_(UNDEFINED)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new RepresentationMessage " << this<< std::endl;		
+		Log.error() << "new RepresentationMessage " << this<< std::endl;
 	#endif
 }
 
 RepresentationMessage::RepresentationMessage(Representation& rep, RepresentationMessageType type)
-	
 	: representation_(&rep),
 		type_(type)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new RepresentationMessage " << type << " " << this << std::endl;		
+		Log.error() << "new RepresentationMessage " << type << " " << this << std::endl;
 	#endif
 }
 
 RepresentationMessage::~RepresentationMessage()
-	
 {
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "Destructing " << this << " RepresentationMessage" << endl;
-	#endif 
+	#endif
 }
 
 void RepresentationMessage::setType(RepresentationMessageType type)
-	 
-{ 
+{
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "RepresentationMessage::setType " << type << std::endl;
-	#endif 
+	#endif
 
 	type_ = type;
 }
 
 MolecularTaskMessage::MolecularTaskMessage(MolecularTaskMessageType type)
-	
 	: type_(type)
 {
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "new MolecularTaskMessage" << std::endl;
-	#endif 
+	#endif
 }
 
 void MolecularTaskMessage::setType(MolecularTaskMessageType type)
-	 
 {
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "MolecularTaskMessage::setType " << type << std::endl;
-	#endif 
+	#endif
 
 	type_ = type;
 }
 
 
 CreateRepresentationMessage::CreateRepresentationMessage()
-	 
-	: Message(), 
+	: Message(),
 		composites_(),
 		model_type_(MODEL_UNKNOWN),
 		coloring_method_(COLORING_UNKNOWN)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new CreateRepresentationMessage" << std::endl;		
+		Log.error() << "new CreateRepresentationMessage" << std::endl;
 	#endif
 }
 
 CreateRepresentationMessage::CreateRepresentationMessage(const list<Composite*>& composites,
 		ModelType model_type, ColoringMethod coloring_method)
- 	 
 	: Message(),
-		composites_(composites), 
+		composites_(composites),
 		model_type_(model_type),
 		coloring_method_(coloring_method)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new CreateRepresentationMessage" << std::endl;		
+		Log.error() << "new CreateRepresentationMessage" << std::endl;
 	#endif
 }
 
 TransformationMessage::TransformationMessage()
-	
 	: Message(),
 		matrix_()
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new TransformationMessage" << std::endl;		
+		Log.error() << "new TransformationMessage" << std::endl;
 	#endif
 }
 
 TransformationMessage::TransformationMessage(const Matrix4x4& m)
-	
 	: Message(),
 		matrix_(m)
 {
 	#ifdef BALL_VIEW_DEBUG
-		Log.error() << "new TransformationMessage" << std::endl;		
+		Log.error() << "new TransformationMessage" << std::endl;
 	#endif
 }
 
 FinishedSimulationMessage::FinishedSimulationMessage()
-	
 	: Message()
 {
 }
 
 ShowHelpMessage::ShowHelpMessage(String url, String project, String entry)
-	
 	: Message(),
 		url_(url),
 		project_(project),
@@ -319,16 +294,14 @@ ShowHelpMessage::ShowHelpMessage(String url, String project, String entry)
 }
 
 RegisterHelpSystemMessage::RegisterHelpSystemMessage()
-	
 	: Message(),
 		object_(0),
 		url_(""),
 		register_(true)
 {
 }
-		
+
 DockingFinishedMessage::DockingFinishedMessage()
-	
 	: Message(),
 		conformation_set_(0)
 {
@@ -338,7 +311,6 @@ DockingFinishedMessage::DockingFinishedMessage()
 }
 
 DockingFinishedMessage::DockingFinishedMessage(bool abort)
-	
 	: Message(),
 		conformation_set_(0)
 {
@@ -349,7 +321,6 @@ DockingFinishedMessage::DockingFinishedMessage(bool abort)
 }
 
 DockingFinishedMessage::~DockingFinishedMessage()
-	
 {
 	#ifdef BALL_VIEW_DEBUG
 		Log.error() << "Destructing " << this << "DockingFinishedMessage" << std::endl;
@@ -359,6 +330,6 @@ DockingFinishedMessage::~DockingFinishedMessage()
 
 #	ifdef BALL_NO_INLINE_FUNCTIONS
 #		include <BALL/VIEW/KERNEL/message.iC>
-#	endif 
+#	endif
 
 } } // namespaces
