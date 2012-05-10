10b924c5b13ddc1892cf938933790e3119d8eb0e
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2012-05-10 13:03:43 +0200

    Enhance MolFileFactory
    Author: Charlotta Schaerfe <schaerfe@informatik.uni-tuebingen.de>
    Author: Luis de la Garza <delagarza@informatik.uni-tuebingen.de>
    Author: Nina Fischer <nfischer@informatik.uni-tuebingen.de>
    Author: Philipp Thiel <philipp.thiel@cgc.mpg.de>
    
    Based on code by: Marcel Schuhmann <schumann@informatik.uni-tuebingen.de>

--- include/BALL/FORMAT/molFileFactory.h
+++ include/BALL/FORMAT/molFileFactory.h
@@ -1,5 +1,5 @@
-#ifndef MOLFILEFACTORY_H
-#define MOLFILEFACTORY_H
+#ifndef BALL_FORMAT_MOLFILEFACTORY_H
+#define BALL_FORMAT_MOLFILEFACTORY_H
 
 #include <BALL/SYSTEM/file.h>
 
@@ -16,18 +16,35 @@ class GenericMolFile;
  */
 class BALL_EXPORT MolFileFactory
 {
-    public:
-        /**
-         * This method tries to determine the filetype based on the file extension of name and
-         * returns a pointer to an object of the appropriate subclass of GenericMolFile if available.
-         * @param name the path to the requested molecule file
-         * @return a pointer to a subclass of GenericMolFile, NULL if none is appropriate. Be aware, that
-         *         the file handle has not been checked for validity and that you have to delete it yourself
-         */
-        static GenericMolFile* open(const String& name, File::OpenMode open_mode = std::ios::in);
+	public:
+		/**
+		* This method tries to determine the filetype based on the file extension of name and
+		* returns a pointer to an object of the appropriate subclass of GenericMolFile if available.
+		* @param name the path to the requested molecule file
+		* @return a pointer to a subclass of GenericMolFile, NULL if none is appropriate. Be aware, that
+		*         the file handle has not been checked for validity and that you have to delete it yourself
+		*/
+		static GenericMolFile* open(const String& name, File::OpenMode open_mode = std::ios::in);
+
+		/** When trying to open a file in write-mode, this function can be used to pass along a desired default-format. If the given filename does not have a supported extension, the specified default format will be used. */
+		static GenericMolFile* open(const String& name, File::OpenMode open_mode, String default_format);
+
+		/** When trying to open a file in write-mode, this function can be used to pass along a desired default-format. If the given filename does not have a supported extension, the format of 'default_format_file' will be used. */
+		static GenericMolFile* open(const String& name, File::OpenMode open_mode, GenericMolFile* default_format_file);
+
+		/** Return a comma-separated string containing the file-extensions that are supported by MolFileFactory */
+		static String getSupportedFormats();
+
+		/** Return true if the extension of the specified filename is supported; otherwise return false. */
+		static bool isFileExtensionSupported(String filename);
+
+	private:
+
+		/** Try to automatically detect the format of an input-file using keywords that are specific for the respective format. */
+		static GenericMolFile* detectFormat(const String& name);
 };
 
 }
 
-#endif //MOLFILEFACTORY_H
+#endif //BALL_FORMAT_MOLFILEFACTORY_H
 
--- source/FORMAT/molFileFactory.C
+++ source/FORMAT/molFileFactory.C
@@ -9,6 +9,11 @@
 #include <BALL/FORMAT/SDFile.h>
 #include <BALL/FORMAT/XYZFile.h>
 
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+
 #include <BALL/DATATYPE/string.h>
 
 namespace BALL
@@ -16,6 +21,135 @@ namespace BALL
 
 	GenericMolFile* MolFileFactory::open(const String& name, File::OpenMode open_mode)
 	{
+		String tmp = name;
+		bool compression = false;
+		String filename = name;
+		String zipped_filename = "";
+
+		if (tmp.hasSuffix(".gz"))
+		{
+			compression = true;
+		}
+		else if (open_mode == ios::in && !isFileExtensionSupported(filename)) // check whether file is zipped
+		{
+			ifstream zipped_file(filename.c_str(), ios_base::in | ios_base::binary);
+			boost::iostreams::filtering_istream in;
+			in.push(boost::iostreams::gzip_decompressor());
+			in.push(zipped_file);
+			bool ok = false;
+			try
+			{
+				string s;
+				ok = std::getline(in,s);
+			}
+			catch (const boost::iostreams::gzip_error& e)
+			{
+				compression = false;
+			}
+			if (ok) compression = true;
+		}
+
+		if (compression)
+		{
+			compression = true;
+			zipped_filename = tmp;
+			String unzipped_filename;
+			if (tmp.hasSuffix(".gz"))
+			{
+				tmp = tmp.before(".gz");
+				String ext = tmp.substr(tmp.find_last_of("."));
+				File::createTemporaryFilename(unzipped_filename,ext);
+			}
+			else // unknown extension
+			{
+				File::createTemporaryFilename(unzipped_filename);
+			}
+
+			if (open_mode == ios::in)
+			{
+				ifstream zipped_file(zipped_filename.c_str(), ios_base::in | ios_base::binary);
+				boost::iostreams::filtering_istream in;
+				in.push(boost::iostreams::gzip_decompressor());
+				in.push(zipped_file);
+				ofstream unzipped_file(unzipped_filename.c_str());
+				boost::iostreams::copy(in, unzipped_file);
+			}
+
+			filename = unzipped_filename;
+			tmp = unzipped_filename.right(5);
+		}
+		else
+		{
+			tmp = filename.right(5);
+		}
+		tmp.toLower(0, 5);
+
+		GenericMolFile* gmf = 0;
+		if (tmp.hasSuffix(".ac"))
+		{
+			gmf = new AntechamberFile(filename, open_mode);
+		}
+		else if(tmp.hasSuffix(".pdb") || tmp.hasSuffix(".ent") || tmp.hasSuffix(".brk"))
+		{
+			gmf = new PDBFile(filename, open_mode);
+		}
+		else if(tmp.hasSuffix(".hin"))
+		{
+			gmf = new HINFile(filename, open_mode);
+		}
+		else if(tmp.hasSuffix(".mol"))
+		{
+			gmf = new MOLFile(filename, open_mode);
+		}
+		else if(tmp.hasSuffix(".sdf"))
+		{
+			gmf = new SDFile(filename, open_mode);
+		}
+		else if(tmp.hasSuffix(".mol2"))
+		{
+			gmf = new MOL2File(filename, open_mode);
+		}
+		else if(tmp.hasSuffix(".xyz"))
+		{
+			gmf = new XYZFile(filename, open_mode);
+		}
+		/*
+		else if(tmp.hasSuffix(".drf"))
+		{
+			gmf = new Docking::DockResultFile(filename, open_mode);
+		}
+		*/
+		else
+		{
+			if (open_mode == ios::in)
+			{
+				gmf = detectFormat(filename);
+				// Make sure that temporary input-file is deleted when GenericMolFile is closed.
+				if (compression)
+				{
+					gmf->defineInputAsTemporary();
+				}
+				return gmf;
+			}
+			return NULL;
+		}
+
+		if (compression)
+		{
+			if (open_mode == ios::in)
+			{
+				// Make sure that temporary input-file is deleted when GenericMolFile is closed.
+				gmf->defineInputAsTemporary();
+			}
+			else
+			{
+				// Make sure that temporary output-file is compressed and then deleted when GenericMolFile is closed.
+				gmf->enableOutputCompression(zipped_filename);
+			}
+		}
+		return gmf;
+
+		/*
 		String tmp = name.right(5);
 		tmp.toLower(0, 5);
 		
@@ -36,7 +170,204 @@ namespace BALL
 		} else {
 			return NULL;
 		}
+		*/
 	}
 
+	bool MolFileFactory::isFileExtensionSupported(String filename)
+	{
+		vector<String> exts;
+		String s = getSupportedFormats();
+		s.split(exts,",");
+		for (Size i=0; i<exts.size(); i++)
+		{
+			if (filename.hasSuffix(exts[i]))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	GenericMolFile* MolFileFactory::open(const String& name, File::OpenMode open_mode, String default_format)
+	{
+		GenericMolFile* file = open(name, open_mode);
+		if (file)
+		{
+			return file;
+		}
+
+		if (open_mode == ios::out)
+		{
+			bool compression = false;
+			String filename = name;
+			String zipped_filename = "";
+			if (default_format.hasSuffix(".gz"))
+			{
+				compression = true;
+				zipped_filename = filename;
+				default_format = default_format.before(".gz");
+				String unzipped_filename;
+				File::createTemporaryFilename(unzipped_filename, default_format);
+				filename = unzipped_filename;
+			}
+
+			if (default_format == "ac")
+			{
+				file = new AntechamberFile(filename, open_mode);
+			}
+			else if(default_format == "pdb" || default_format == "ent" || default_format == "brk")
+			{
+				file = new PDBFile(filename, open_mode);
+			}
+			else if(default_format == "hin")
+			{
+				file = new HINFile(filename, open_mode);
+			}
+			else if(default_format == "mol")
+			{
+				file = new MOLFile(filename, open_mode);
+			}
+			else if(default_format == "sdf")
+			{
+				file = new SDFile(filename, open_mode);
+			}
+			else if(default_format == "mol2")
+			{
+				file = new MOL2File(filename, open_mode);
+			}
+			else if(default_format == "xyz")
+			{
+				file = new XYZFile(filename, open_mode);
+			}
+			/*
+			else if(default_format == "drf")
+			{
+				file = new Docking::DockResultFile(filename, open_mode);
+			}
+			*/
+			// Make sure that temporary output-file is compressed and then deleted when GenericMolFile is closed.
+			if (compression)
+			{
+				file->enableOutputCompression(zipped_filename);
+			}
+		}
+		return file;
+	}
+
+	GenericMolFile* MolFileFactory::open(const String& name, File::OpenMode open_mode, GenericMolFile* default_format_file)
+	{
+		GenericMolFile* file = open(name, open_mode);
+		if (file)
+		{
+			return file;
+		}
+
+		if (open_mode == ios::out)
+		{
+			bool compression = false;
+			String filename = name;
+			String zipped_filename = "";
+			if (default_format_file->isCompressedFile())
+			{
+				compression = true;
+				zipped_filename = filename;
+				String unzipped_filename;
+				File::createTemporaryFilename(unzipped_filename);
+				filename = unzipped_filename;
+			}
+
+			if (dynamic_cast<AntechamberFile*>(default_format_file))
+			{
+				file = new AntechamberFile(filename, open_mode);
+			}
+			else if(dynamic_cast<PDBFile*>(default_format_file))
+			{
+				file = new PDBFile(filename, open_mode);
+			}
+			else if(dynamic_cast<HINFile*>(default_format_file))
+			{
+				file = new HINFile(filename, open_mode);
+			}
+			else if(dynamic_cast<SDFile*>(default_format_file))
+			{
+				file = new SDFile(filename, open_mode);
+			}
+			else if(dynamic_cast<MOL2File*>(default_format_file))
+			{
+				file = new MOL2File(filename, open_mode);
+			}
+			else if(dynamic_cast<MOLFile*>(default_format_file))
+			{
+				file = new MOLFile(filename, open_mode);
+			}
+			else if(dynamic_cast<XYZFile*>(default_format_file))
+			{
+				file = new XYZFile(filename, open_mode);
+			}
+			/*
+			else if(dynamic_cast<Docking::DockResultFile*>(default_format_file))
+			{
+				file = new Docking::DockResultFile(filename, open_mode);
+			}
+			*/
+			// Make sure that temporary output-file is compressed and then deleted when GenericMolFile is closed.
+			if (compression)
+			{
+				file->enableOutputCompression(zipped_filename);
+			}
+		}
+		return file;
+	}
+
+	String MolFileFactory::getSupportedFormats()
+	{
+		String formats = "mol2, sdf, drf, pdb, ac, ent, brk, hin, mol, xyz, mol2.gz, sdf.gz, drf.gz, pdb.gz, ac.gz, ent.gz, brk.gz, hin.gz, mol.gz, xyz.gz";
+		return formats;
+	}
+
+	GenericMolFile* MolFileFactory::detectFormat(const String& name)
+	{
+		LineBasedFile input(name, ios::in);
+		bool empty_file = true;
+
+		// Read at most 400 lines.
+		// If no format-indicator is found, return NULL
+		for (Size no=1; no < 400 && input.readLine(); no++)
+		{
+			String& line = input.getLine();
+			if (!line.trim().empty())
+			{
+				empty_file = false;
+			}
+			if (line.hasPrefix("@<TRIPOS>MOLECULE"))
+			{
+				return new MOL2File(name, ios::in);
+			}
+			else if (line.hasPrefix("$$$$") || line.hasPrefix("M  END"))
+			{
+				return new SDFile(name, ios::in);
+			}
+			/*
+			else if (line.hasPrefix("<dockingfile>"))
+			{
+				return new Docking::DockResultFile(name, ios::in);
+			}
+			*/
+			else if (line.hasPrefix("HEADER") || line.hasPrefix("ATOM") || line.hasPrefix("USER"))
+			{
+				return new PDBFile(name, ios::in);
+			}
+		}
+
+		if (empty_file)
+		{
+			Log.error() << endl << "[Error:] Specified input file is empty!" << endl << endl;
+		}
+		else
+		{
+			Log.error() << endl << "[Error:] Specified input file has unknown extension and its format could not be detected automatically!" << endl << endl;
+		}
+		return NULL;
+	}
 }
 
