6d51e8099771b0a9dcd4f570a81e072f02628089
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-25 17:01:50 +0200

    Improved test of ConformationSet

--- source/TEST/ConformationSet_test.C
+++ source/TEST/ConformationSet_test.C
@@ -35,6 +35,23 @@ CHECK(readDCDFile())
 		TEST_EQUAL(cs.size(), 10)
 RESULT
 
+CHECK(writeDCDFile(const String& filename, const Size num = 0))
+		ConformationSet cs;
+
+		PDBFile pdb(BALL_TEST_DATA_PATH(PoseClustering_test.pdb));
+		System sys;
+		pdb.read(sys);
+		cs.setup(sys);
+
+		cs.readDCDFile(BALL_TEST_DATA_PATH(PoseClustering_test.dcd));
+		cs.resetScoring();
+		String tmp_filename;
+		NEW_TMP_FILE(tmp_filename)
+
+		cs.writeDCDFile(tmp_filename);
+		TEST_FILE_REGEXP(tmp_filename.c_str(), BALL_TEST_DATA_PATH(PoseClustering_test.dcd))
+RESULT
+
 CHECK(setup())
 	PDBFile pdb(BALL_TEST_DATA_PATH(PoseClustering_test.pdb));
 	System sys;
