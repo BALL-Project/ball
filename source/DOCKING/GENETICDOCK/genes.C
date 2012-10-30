9995ba55d53574efdaf78f79de73ce21b8e8c665
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:05:13 +0100

    deleted invalid licensing

--- source/DOCKING/GENETICDOCK/genes.C
+++ source/DOCKING/GENETICDOCK/genes.C
@@ -1,7 +1,6 @@
-// ----------------------------------------------------
-// $Maintainer: Marcel Schumann $
-// $Authors: Jan Fuhrmann, Marcel Schumann $
-// ----------------------------------------------------
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #include <BALL/DOCKING/GENETICDOCK/genes.h>
 #include <BALL/MATHS/randomNumberGenerator.h>
