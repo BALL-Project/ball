9c1a7084c431d625eb99375eb5f4c129cb601082
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:02:29 +0100

    deleted invalid licensing

--- include/BALL/QSAR/gpModel.h
+++ include/BALL/QSAR/gpModel.h
@@ -1,36 +1,16 @@
-/* gpModel.h
- * 
- * Copyright (C) 2009 Marcel Schumann
- * 
- * This file is part of QuEasy -- A Toolbox for Automated QSAR Model
- * Construction and Validation.
- * QuEasy is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * 
- * QuEasy is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
-
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
 //
 
-#ifndef GPMODEL
-#define GPMODEL
+#ifndef BALL_QSAR_GPMODEL_H
+#define BALL_QSAR_GPMODEL_H
 
-#ifndef KMODEL
+#ifndef BALL_QSAR_KERNELMODEL_H
 #include <BALL/QSAR/kernelModel.h>
 #endif
 
-#ifndef NLMODEL
+#ifndef BALL_QSAR_NONLINEARMODEL_H
 #include <BALL/QSAR/nonlinearModel.h>
 #endif
 
