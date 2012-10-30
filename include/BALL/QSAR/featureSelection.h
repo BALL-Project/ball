ef7ef45096488cc8fdf13cc4ed13dbfeda7fc9d0
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:02:23 +0100

    deleted invalid licensing

--- include/BALL/QSAR/featureSelection.h
+++ include/BALL/QSAR/featureSelection.h
@@ -1,40 +1,19 @@
-/* featureSelection.h
- * 
- * Copyright (C) 2009 Marcel Schumann
- * 
- * This file is part of QuEasy -- A Toolbox for Automated QSAR Model
- * Construction and Validation.
- * QuEasy is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * 
- * QuEasy is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
-
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
-// 
 
-#ifndef FEATURESEL
-#define FEATURESEL
+#ifndef BALL_QSAR_FEATURESELECTION_H
+#define BALL_QSAR_FEATURESELECTION_H
 
-#ifndef MODEL
+#ifndef BALL_QSAR_MODEL_H
 #include <BALL/QSAR/Model.h>
 #endif
 
-#ifndef KMODEL
+#ifndef BALL_QSAR_KERNELMODEL_H
 #include <BALL/QSAR/kernelModel.h>
 #endif
 
-#ifndef LMODEL
+#ifndef BALL_QSAR_LINEARMODEL_H
 #include <BALL/QSAR/linearModel.h>
 #endif
 
