ff630488a3f44837fc76b2ebb15c8bf177991244
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:01:51 +0100

    deleted invalid licensing

--- include/BALL/DOCKING/COMMON/sideChainOptimizer.h
+++ include/BALL/DOCKING/COMMON/sideChainOptimizer.h
@@ -1,26 +1,18 @@
-/* sideChainOptimizer.h
-*
-* Copyright (C) 2011 Marcel Schumann
-*
-* This program free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #ifndef BALL_DOCKING_COMMON_SIDECHAINOPTIMIZER
 #define BALL_DOCKING_COMMON_SIDECHAINOPTIMIZER
 
+#ifndef BALL_SCORING_COMMON_SCORINGFUNCTION_H
 #include <BALL/SCORING/COMMON/scoringFunction.h>
+#endif
+
+#ifndef BALL_STRUCTURE_ROTAMERLIBRARY_H
 #include <BALL/STRUCTURE/rotamerLibrary.h>
+#endif
+
 #include <set>
 #include <list>
 
