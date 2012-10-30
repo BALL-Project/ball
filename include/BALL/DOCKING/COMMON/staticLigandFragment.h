87911098493d53948cb972f12ca5b1f8d3f81d2d
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:01:54 +0100

    deleted invalid licensing

--- include/BALL/DOCKING/COMMON/staticLigandFragment.h
+++ include/BALL/DOCKING/COMMON/staticLigandFragment.h
@@ -1,32 +1,17 @@
-/* staticLigandFragment.h
-*
-* Copyright (C) 2011 Marcel Schumann
-*
-* This program free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
-
-// ----------------------------------------------------
-// $Maintainer: Marcel Schumann $
-// $Authors: Marcel Schumann $
-// ----------------------------------------------------
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #ifndef BALL_DOCKING_COMMON_STATICLIGANDFRAGMENT
 #define BALL_DOCKING_COMMON_STATICLIGANDFRAGMENT
 
+#ifndef BALL_MATH_MATRIX44_H
 #include <BALL/MATHS/matrix44.h>
-#include <BALL/KERNEL/atom.h>
+#endif
 
+#ifndef BALL_KERNEL_ATOM_H
+#include <BALL/KERNEL/atom.h>
+#endif
 
 namespace BALL
 {
