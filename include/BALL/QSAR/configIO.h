b5fbdaec10312ac907a4b989a10d5115214112fb
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:02:18 +0100

    deleted invalid licensing

--- include/BALL/QSAR/configIO.h
+++ include/BALL/QSAR/configIO.h
@@ -1,31 +1,17 @@
-/* configIO.h
- * 
- * Copyright (C) 2009 Marcel Schumann
- * 
- * This file is part of QuEasy -- A Toolbox for Automated QSAR Model
- * Construction and Validation.
- * QuEasy is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * 
- * QuEasy is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #ifndef BALL_QSAR_CONFIGIO_H
 #define BALL_QSAR_CONFIGIO_H
 
-#include <istream>
+#ifndef BALL_DATATYPE_STRING_H
 #include <BALL/DATATYPE/string.h>
+#endif
+
 #include <istream>
 #include <set>
- 
+
 namespace BALL
 {
 	namespace QSAR
