91f1b3996a2d10aa57bf5782121919c181175145
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:02:07 +0100

    deleted invalid licensing

--- include/BALL/QSAR/allModel.h
+++ include/BALL/QSAR/allModel.h
@@ -1,44 +1,24 @@
-/* allModel.h
- * 
- * Copyright (C) 2009 Marcel Schumann
- * 
- * This file is part of QuEasy -- A Toolbox for Automated QSAR Model
- * Construction and Validation.
- * QuEasy is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * 
- * QuEasy is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
-
-  // -*- Mode: C++; tab-width: 2; -*-
+// -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
 //
 
-#ifndef ALLMODEL
-#define ALLMODEL
+#ifndef BALL_QSAR_ALLMODEL_H
+#define  BALL_QSAR_ALLMODEL_H
 
-#ifndef NLMODEL
+#ifndef BALL_QSAR_NONLINEARMODEL_H
 #include <BALL/QSAR/nonlinearModel.h>
 #endif
 
 
 namespace BALL
 {
-	namespace QSAR 
+	namespace QSAR
 	{
 
 		/** class for automated lazy learning (ALL-QSAR) */
 		class BALL_EXPORT ALLModel : public NonLinearModel
-		{ 
+		{
 			public:
 				/** @name Constructors and Destructors
 				 */
