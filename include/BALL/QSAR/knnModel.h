f5a8d04a94915147f99d95d231e7f9bdba0408f5
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:02:37 +0100

    deleted invalid licensing

--- include/BALL/QSAR/knnModel.h
+++ include/BALL/QSAR/knnModel.h
@@ -1,38 +1,18 @@
-/* knnModel.h
- * 
- * Copyright (C) 2009 Marcel Schumann
- * 
- * This file is part of QuEasy -- A Toolbox for Automated QSAR Model
- * Construction and Validation.
- * QuEasy is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * 
- * QuEasy is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
-
-  // -*- Mode: C++; tab-width: 2; -*-
+// -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
 //
 //
 
-#ifndef KNNMODEL
-#define KNNMODEL
+#ifndef BALL_QSAR_KNNMODEL_H
+#define BALL_QSAR_KNNMODEL_H
 
-#ifndef ALLMODEL
+#ifndef BALL_QSAR_ALLMODEL_H
 #include <BALL/QSAR/allModel.h>
 #endif
 
 namespace BALL
 {
-	namespace QSAR 
+	namespace QSAR
 	{
 
 		/** class for automated lazy learning (ALL-QSAR) */
