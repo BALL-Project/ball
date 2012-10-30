88bc437432059f030866c4e2aa56dc9c73722e90
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:02:01 +0100

    deleted invalid licensing

--- include/BALL/DOCKING/IMGDOCK/IMGDock.h
+++ include/BALL/DOCKING/IMGDOCK/IMGDock.h
@@ -1,36 +1,34 @@
-/* IMGDock.h
-*
-* Copyright (C) 2011 Marcel Schumann
-*
-* This program free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
-
-// ----------------------------------------------------
-// $Maintainer: Marcel Schumann $
-// $Authors: Marcel Schumann $
-// ----------------------------------------------------
-
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #ifndef BALL_DOCKING_IMGDOCK_IMGDOCK_H
 #define BALL_DOCKING_IMGDOCK_IMGDOCK_H
 
+#ifndef BALL_DOCKING_COMMON_DOCKINGALGORITHEM_H
 #include <BALL/DOCKING/COMMON/dockingAlgorithm.h>
+#endif
+
+#ifndef BALL_MOLMEC_COMMON_FORCEFIELD_H
 #include <BALL/MOLMEC/COMMON/forceField.h>
+#endif
+
+#ifndef BALL_MATH_MATRIX44_H
 #include <BALL/MATHS/matrix44.h>
+#endif
+
+#ifndef BALL_SCORING_COMMON_SCORINGFUNCTION_H
 #include <BALL/SCORING/COMMON/scoringFunction.h>
+#endif
+
+#ifndef BALL_DOCKING_COMMON_STATICLIGANDFRAGMENT_H
 #include <BALL/DOCKING/COMMON/staticLigandFragment.h>
+#endif
+
+#ifndef BALL_DOCKING_COMMON_SIDECHAINOPTIMIZER_H
 #include <BALL/DOCKING/COMMON/sideChainOptimizer.h>
+#endif
+
 #include <map>
 
 
