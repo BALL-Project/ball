f6bb63cf86e6f8d4a9e1beba6f97bd382a671b58
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:01:37 +0100

    deleted invalid licensing

--- include/BALL/DOCKING/COMMON/constraints.h
+++ include/BALL/DOCKING/COMMON/constraints.h
@@ -1,34 +1,30 @@
-/* constraint.h
-*
-* Copyright (C) 2011 Marcel Schumann
-*
-* This program free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
-
-// ----------------------------------------------------
-// $Maintainer: Marcel Schumann $
-// $Authors: Marcel Schumann $
-// ----------------------------------------------------
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #ifndef BALL_DOCKING_COMMON_CONSTRAINTS
 #define BALL_DOCKING_COMMON_CONSTRAINTS
 
+
+#ifndef BALL_MATHS_VECTOR3_H
 #include <BALL/MATHS/vector3.h>
+#endif
+
+#ifndef BALL_MATHS_MATRIX44_H
 #include <BALL/MATHS/matrix44.h>
+#endif
+
+#ifndef BALL_MATHS_PLANE3_H
 #include <BALL/MATHS/plane3.h>
+#endif
+
+#ifndef BALL_KERNEL_SYSTEM_H
 #include <BALL/KERNEL/system.h>
+#endif
+
+#ifndef BALL_DATATYPE_HASHGRID_H
 #include <BALL/DATATYPE/hashGrid.h>
+#endif
 
 
 namespace BALL
