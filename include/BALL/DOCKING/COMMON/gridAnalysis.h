ff7d2e2152a923ca376b95cb2e07d68196ddbf28
Anna Dehof <anne@bioinf.uni-sb.de>
2012-10-30 15:01:46 +0100

    deleted invalid licensing

--- include/BALL/DOCKING/COMMON/gridAnalysis.h
+++ include/BALL/DOCKING/COMMON/gridAnalysis.h
@@ -1,31 +1,18 @@
-/* gridAnalysis.h
-*
-* Copyright (C) 2011 Marcel Schumann
-*
-* This program free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
-
-// ----------------------------------------------------
-// $Maintainer: Marcel Schumann $
-// $Authors: Marcel Schumann $
-// ----------------------------------------------------
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
 
 #ifndef BALL_DOCKING_COMMON_GRIDANALYSIS_H
 #define BALL_DOCKING_COMMON_GRIDANALYSIS_H
 
+#ifndef BALL_SCORING_COMMON_SCORINGFUNCTION_H
 #include <BALL/SCORING/COMMON/scoringFunction.h>
+#endif
+
+#ifndef BALL_DATATYPE_REGULARDATA3D_H
 #include <BALL/DATATYPE/regularData3D.h>
+#endif
+
 #include <map>
 
 namespace BALL
