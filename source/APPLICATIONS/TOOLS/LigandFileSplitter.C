594c73e87f2e97087bcc68f32fee1cd6f0766104
Luis de la Garza <delagarza@informatik.uni-tuebingen.de>
2012-11-18 18:40:40 +0100

    Added 'ligands_per_file' parameter

--- source/APPLICATIONS/TOOLS/LigandFileSplitter.C
+++ source/APPLICATIONS/TOOLS/LigandFileSplitter.C
@@ -12,24 +12,60 @@
 using namespace BALL;
 using namespace std;
 
+void validateParameters(CommandlineParser& params)
+{
+    // we need at least one of the following: no, ligands_per_file
+    // TODO: move this "one of the following parameters" logic to CommandlineParser!
+    if (!params.has("no") && !params.has("ligands_per_file"))
+    {
+        Log.error() << "One of the parameters 'no' and 'ligands_per_file' is required." << endl;
+        exit(1);
+    }
+    if (params.has("no") && params.has("ligands_per_file"))
+    {
+        Log.error() << "At most one of the parameters 'no' and 'ligands_per_file' is required. You have provided both." << endl;
+        exit(1);
+    }
+    // TODO: move this to CommandlineParser... using 'registerParameterRestrictions' does not seem to be working
+    if (params.has("no"))
+    {
+        if (params.get("no").toInt() < 1)
+        {
+            Log.error() << "The provided value for parameter 'no', " << params.get("no").toInt() << ", is invalid. Values must be greater or equal to 1." << endl;
+            exit(1);
+        }
+    }
+    if (params.has("ligands_per_file"))
+    {
+        if (params.get("ligands_per_file").toInt() < 1)
+        {
+            Log.error() << "The provided value for parameter 'ligands_per_file', " << params.get("ligands_per_file").toInt() << ", is invalid. Values must be greater or equal to 1." << endl;
+            exit(1);
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
-	CommandlineParser parpars("LigandFileSplitter", "split molecule files", VERSION, String(__DATE__), "Preparation");
+    CommandlineParser parpars("LigandFileSplitter", "split molecule files", VERSION, String(__DATE__), "Preparation");
 	parpars.registerParameter("i", "input molecule file", INFILE, true);
-	parpars.registerParameter("no", "no. of splits to be created", BALL::INT, true);
+    parpars.registerParameter("no", "no. of splits to be created", BALL::INT, false);
+    parpars.registerParameter("ligands_per_file", "max. number of ligands to output to a file", BALL::INT, false);
 	parpars.registerParameter("o", "output filenames; if none are specified, input filename postfixed with IDs will be used", OUTFILELIST, false);
 	String man = "LigandFileSplitter splits a molecule file into a given number of subsets.\nNote that the molecules are not sorted in any way for this.";
 	parpars.setToolManual(man);
 	parpars.setSupportedFormats("i","mol2,sdf,drf");
 	parpars.setSupportedFormats("o","mol2,sdf,drf");
 	parpars.setOutputFormatSource("o","i");
-	parpars.parse(argc, argv);
+    parpars.parse(argc, argv);
 
-	GenericMolFile* input = MolFileFactory::open(parpars.get("i"));
+    validateParameters(parpars);
+
+    GenericMolFile* input = MolFileFactory::open(parpars.get("i"));
 
 	// Determine number of molecules in input files.
 	// In case of DockResultFiles, we do not need to process all contained molecules in order to achieve this; we can simply count the result-section entries.
-	int no_compounds = 0;
+    int no_compounds = 0;
 	DockResultFile* drf_input = dynamic_cast<DockResultFile*>(input);
 	if (drf_input)
 	{
@@ -53,11 +89,26 @@ int main(int argc, char* argv[])
 
 	input->close();
 	delete input;
+
+    // if ligands_per_file was provided, simply insert the parameter 'no' with a proper value
+    int numberOfFiles = -1;
+    if (parpars.has("ligands_per_file"))
+    {
+        int ligandsPerFile = parpars.get("ligands_per_file").toInt();
+        // we know the number of ligands per file and the number of input ligands, so:
+        numberOfFiles = no_compounds / ligandsPerFile;
+        if ((numberOfFiles * ligandsPerFile) < no_compounds)
+        {
+            numberOfFiles++;
+        }
+    }
+
 	input = MolFileFactory::open(parpars.get("i"));
 	drf_input = dynamic_cast<DockResultFile*>(input);
 	if (drf_input) drf_input->selectAllResultsForInput();
 
-	int no_splits = parpars.get("no").toInt();
+    // if numberOfFiles != -1, it means that ligands_per_file was set
+    int no_splits = numberOfFiles == -1 ? parpars.get("no").toInt() : numberOfFiles;
 	vector<String> output_files;
 	if (parpars.has("o"))
 	{
