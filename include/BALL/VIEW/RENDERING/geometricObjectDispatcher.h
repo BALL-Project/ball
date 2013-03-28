9d66da810fe937507973efd57f5bd17e6bf3b721
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2013-03-28 19:03:00 +0100

    Add a GeometricObjectDispatcher class
    This class dispatches geometric objects to a set of virtual methods.

---
+++ include/BALL/VIEW/RENDERING/geometricObjectDispatcher.h
@@ -0,0 +1,48 @@
+#ifndef BALL_VIEW_RENDERING_GEOMETRICOBJECTDISPATCHER_H
+#define BALL_VIEW_RENDERING_GEOMETRICOBJECTDISPATCHER_H
+
+namespace BALL
+{
+	namespace VIEW
+	{
+		class Line;
+		class MultiLine;
+		class Tube;
+		class Mesh;
+		class Label;
+		class Point;
+		class Sphere;
+		class Disc;
+		class Box;
+		class SimpleBox;
+		class TwoColoredLine;
+		class TwoColoredTube;
+		class ClippingPlane;
+		class GridVisualisation;
+		class QuadMesh;
+		class GeometricObject;
+
+		class GeometricObjectDispatcher
+		{
+			protected:
+				virtual void render_(const GeometricObject*);
+
+				virtual void renderSphere_           (const Sphere&           );
+				virtual void renderTwoColoredTube_   (const TwoColoredTube&   );
+				virtual void renderMesh_             (const Mesh&             );
+				virtual void renderTwoColoredLine_   (const TwoColoredLine&   );
+				virtual void renderPoint_            (const Point&            );
+				virtual void renderDisc_             (const Disc&             );
+				virtual void renderLine_             (const Line&             );
+				virtual void renderTube_             (const Tube&             );
+				virtual void renderBox_              (const Box&              );
+				virtual void renderSimpleBox_        (const SimpleBox&        );
+				virtual void renderLabel_            (const Label&            );
+				virtual void renderMultiLine_        (const MultiLine&        );
+				virtual void renderGridVisualisation_(const GridVisualisation&);
+				virtual void renderQuadMesh_         (const QuadMesh&         );
+		};
+	}
+}
+
+#endif //BALL_VIEW_RENDERING_GEOMETRICOBJECTDISPATCHER_H
