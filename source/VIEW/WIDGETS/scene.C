04fe601ce543091ebe62be6b6ebfc9f5b04befec
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2013-04-10 13:19:06 +0200

    HACK scene.C to apply materials for RTfact on Windows...
    This is terrible and should be fixed correctly asap...

--- source/VIEW/WIDGETS/scene.C
+++ source/VIEW/WIDGETS/scene.C
@@ -315,7 +315,22 @@ namespace BALL
 						if (pm.startRendering(rep))
 						{
 							for (Position i=0; i<renderers_.size(); ++i)
+							{
 								renderers_[i]->bufferRepresentation(*rep);
+#ifdef BALL_OS_WINDOWS
+								// this is an ugly hack, but on Windows representations currently don't get their materials straigt
+								if (rep->hasProperty("Rendering::Material"))
+								{
+									NamedProperty rt_mat_property = rep->getProperty("Rendering::Material");
+									boost::shared_ptr<PersistentObject> mat_ptr = rt_mat_property.getSmartObject();
+									rt_material = *dynamic_cast<Stage::Material*>(mat_ptr.get());
+									
+									updateMaterialForRepresentation(rep, rt_material);
+								}
+								else
+									updateMaterialForRepresentation(rep);
+#endif
+							}
 
 							pm.finishedRendering(rep);
 						}
