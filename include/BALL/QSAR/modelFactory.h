06eb8914cfe6d26bf6038c6dceed75d9ee7f2e59
Anna Dehof <anne@bioinf.uni-sb.de>
2013-11-08 16:17:13 +0100

    Clarified licence

--- include/BALL/QSAR/modelFactory.h
+++ include/BALL/QSAR/modelFactory.h
@@ -1,27 +1,5 @@
-/* modelFactory.h
- * 
- * Copyright (C) 2009 Marcel Schumann
- * 
- * This file is part of QuEasy -- A Toolbox for Automated QSAR Model
- * Construction and Validation.
- * QuEasy is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * 
- * QuEasy is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
-
 // -*- Mode: C++; tab-width: 2; -*-
 // vi: set ts=2:
-//
-// $Id: factory.h,v 1.10 2003/08/26 08:04:07 oliver Exp $
 //
 
 #ifndef BALL_QSAR_MODELFACTORY_H
