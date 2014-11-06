02a952368a05c740c539f2a526d0d7e5ecc7069a
Hans-Philipp Brachvogel <philipp-brachvogel@web.de>
2014-11-06 15:35:38 +0100

    WARNING: needed to include my connectedComponentsProcessor and thus i had to also change sipBALLCorepart2.cpp , the file seems to be untracked by git. The playTool is now able to fragment all rings of an input molecule into connected ringsystems. However: the very important connection points are still missing

--- source/APPLICATIONS/LigStruct/playTool.C
+++ source/APPLICATIONS/LigStruct/playTool.C
@@ -7,19 +7,22 @@
 
 #include <BALL/STRUCTURE/defaultProcessors.h>
 #include <BALL/KERNEL/molecule.h>
-
+#include <BALL/STRUCTURE/molecularSimilarity.h>
 #include <BALL/FORMAT/SDFile.h>
 
+#include <BALL/STRUCTURE/connectedComponentsProcessor.h>
 #include <openbabel/obconversion.h>
 #include <openbabel/mol.h>
 #include <openbabel/shared_ptr.h>
+#include <openbabel/ring.h>
+
 #include "testHeader.h"
 
 using namespace OpenBabel;
 using namespace BALL;
 using namespace std;
 
-shared_ptr<OBMol> GetMol(const std::string &filename)
+shared_ptr<OBMol> getMol(const std::string &filename)
 {
   // Create the OBMol object.
   shared_ptr<OBMol> mol(new OBMol);
@@ -69,11 +72,106 @@ int main(int argc, char* argv[])
 		return 2;
 	}
 
+	shared_ptr<OBMol> obMol = getMol(parpars.get("i"));
 	Molecule ball_mol;
 	input >> ball_mol;
 	input.close();
 
-
+	// init ring and rotable information:
+	obMol->FindRingAtomsAndBonds();
+	
+	// number of rotables and location of rotables:
+	OBBondIterator b_it;
+	obMol->BeginBond(b_it);
+	for (; b_it != obMol->EndBonds(); b_it++)
+	{
+		OBBond *bond = (*b_it);
+		if (bond->IsRotor())
+		{
+			cout <<"rotor at: "<< bond->GetBeginAtom()->GetType()<< " - "<< bond->GetEndAtom()->GetType()<< endl;
+		}
+	}
+	cout << "Number of rotables: "<<obMol->NumRotors()<<endl;
+	cout <<endl;
+	
+	// NUM of ring atoms and its atoms:
+	OBAtomIterator at_it;
+	obMol->BeginAtom(at_it);
+	int num_ring_atms = 0;
+	OBMol rings;
+	OBMol rings2 = *obMol;
+	for(; at_it != obMol->EndAtoms(); at_it++)
+	{
+		OBAtom* at = (*at_it);
+		if (at->IsInRing())
+		{
+			rings.AddAtom(*at);
+			num_ring_atms++;
+			cout <<"Ring Atm: "<< at->GetType()<<at->GetIndex()<< endl;
+		}
+		else
+		{
+//			cout<<"going to delete..."<<endl;
+//			rings2.DeleteAtom(at);
+			cout <<"NORMAL Atm: "<< at->GetType()<<at->GetIndex()<< endl;
+		}
+	}
+	cout << "Number of ring atoms: "<<num_ring_atms<<endl;
+	cout<<endl;
+	
+	//Ring bonds:
+	vector<OBBond*> for_deletion;
+  FOR_BONDS_OF_MOL(b, rings2)
+    if (!b->IsInRing())
+      for_deletion.push_back(&(*b));
+  for(vector<OBBond*>::iterator it=for_deletion.begin(); it!=for_deletion.end(); ++it) {
+    rings2.DeleteBond(*it);
+  }
+	
+	vector<OBAtom*> atm_deletion;
+	FOR_ATOMS_OF_MOL(at, rings2)
+			if (!at->IsInRing())
+				atm_deletion.push_back(&(*at));
+	for(vector<OBAtom*>::iterator it2=atm_deletion.begin(); it2!=atm_deletion.end(); ++it2) {
+    rings2.DeleteAtom(*it2);
+	}
+	
+	OBBondIterator rb_it;
+	obMol->BeginBond(rb_it);
+	int bondcnt = 0;
+	for(; rb_it != obMol->EndBonds(); rb_it++)
+	{
+		OBBond* bn = *rb_it;
+		if ((*rb_it)->IsInRing())
+		{
+			cout<< "adding worked "<<rings.AddBond(**rb_it);
+			cout<<" ring bond: "<< bn->GetBeginAtom()->GetType() << bn->GetBeginAtom()->GetIndex()<<" - " << bn->GetEndAtom()->GetType()<< bn->GetEndAtom()->GetIndex()<< endl;
+			
+			bondcnt++;
+			cout<< "total number of bonds in ring file: "<<rings.NumBonds()<<endl;
+		}
+	}
+	cout << "total ring-bonds: "<<bondcnt << endl;
+	cout<<endl;
+	
+	
+	OBConversion conv;
+  conv.SetOutFormat("sdf");
+	ofstream ring_file;
+	ofstream ring_file2;
+	ring_file.open (String(parpars.get("o")+"found_rings.sdf"));
+	ring_file2.open (String(parpars.get("o")+"2found_rings.sdf"));
+	conv.Write(&rings, &ring_file);
+	conv.Write(&rings2, &ring_file2);
+	ring_file.close();
+	ring_file2.close();
+	
+	cout <<endl;
+	
+	Molecule* ball_mol2 = MolecularSimilarity::createMolecule(rings2, true);
+	ConnectedComponentsProcessor conpro;
+	ball_mol2->apply(conpro);
+	
 	// write the optimized structure to a file whose
 	// name is given as the second command line argument
 	String outfile_name = String(parpars.get("o"));
