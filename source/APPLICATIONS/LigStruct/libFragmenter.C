fc52f733a9954fd4bdcd206f8b304bf67daee00e
Hans-Philipp Brachvogel <philipp-brachvogel@web.de>
2014-11-07 12:59:37 +0100

    implemented multi-structure file fragmentation. One could theretically test this tool with a database to find bugs. Still missing: an option for an optional torision file that specifies via SMARTS which bonds are rotable and thus tells where to split.

---
+++ source/APPLICATIONS/LigStruct/libFragmenter.C
@@ -0,0 +1,105 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+#include <BALL/FORMAT/commandlineParser.h>
+#include <BALL/FORMAT/SDFile.h>
+#include <BALL/KERNEL/molecule.h>
+#include <BALL/STRUCTURE/molecularSimilarity.h>
+#include <BALL/STRUCTURE/connectedComponentsProcessor.h>
+
+#include <openbabel/mol.h>
+#include <openbabel/shared_ptr.h>
+#include <openbabel/obconversion.h>
+
+using namespace OpenBabel;
+using namespace BALL;
+using namespace std;
+
+/// ################# H E L P E R    F U N C T I O N S #################
+// Write several result molecules to one file
+void writeMolVec(vector<Molecule> &input, SDFile* handle)
+{
+	for(int i = 0; i < input.size(); i++)
+	{
+		(*handle) << input[i];
+	}
+}
+
+
+// (from http://openbabel.org/api/2.3.0/obconformersearch_default_8cpp-example.shtml)
+
+/// ################# M A I N #################
+int main(int argc, char* argv[])
+{
+	CommandlineParser parpars("libFragmenter", "cut a molecule along its rotable bonds, generating fragments", 0.1, String(__DATE__), "Preparation");
+	parpars.registerParameter("i", "input SDF", INFILE, true);
+	parpars.registerParameter("o", "output SDF", OUTFILE, true);
+
+	parpars.setSupportedFormats("i","sdf");
+	parpars.setSupportedFormats("o","sdf");
+	parpars.setOutputFormatSource("o","i");
+
+	String manual = "For creating a fragment library.";
+	parpars.setToolManual(manual);
+
+	parpars.parse(argc, argv);
+	
+	// START of CODE#############################################################
+
+	// read input molecule:
+	//shared_ptr<OBMol> obMol = getMol(parpars.get("i"));
+	
+	OBMol obMol;
+	
+	
+	// Read open-babel molecule as input:
+	OBConversion conv;
+	OBFormat *format = conv.FormatFromExt(parpars.get("i"));
+	
+	if (!format || !conv.SetInFormat(format))
+		cout << "Could not find input format for file " << parpars.get("i") << endl;
+
+	ifstream ifs(parpars.get("i")); // open file
+	
+	if (!ifs)
+		cout << "Could not open " << parpars.get("i") << " for reading." << endl;
+	
+	// open output file:
+	String outfile_name = String(parpars.get("o"));
+	SDFile outfile(outfile_name, ios::out);
+	
+	// Read all molecules.
+	vector<OBBond*> for_deletion;
+	vector<OBBond*>::iterator it;
+	vector<Molecule> fragments;
+	Molecule* ball_mol;
+	while ( conv.Read(&obMol, &ifs) )
+	{
+		// init ring and rotable information:
+		obMol.FindRingAtomsAndBonds();
+		
+		for_deletion.clear();
+		fragments.clear();
+		
+		// Delete rotor-bonds to create fragments (of rings and rigid-groups)
+		FOR_BONDS_OF_MOL(b, obMol)
+			if (b->IsRotor()) // && !b->IsInRing()) // in babel ring bonds are never rotors!!!!
+				for_deletion.push_back(&(*b));
+		for(it=for_deletion.begin(); it!=for_deletion.end(); ++it)
+			obMol.DeleteBond(*it);
+		
+		// get everything that is larger than 2 molecules (convert to BALL for that)
+		ball_mol = MolecularSimilarity::createMolecule(obMol, true);
+		ConnectedComponentsProcessor conpro;
+		ball_mol->apply(conpro);
+		conpro.getMinAtomsComponents(fragments, 2);
+		
+		// write to output:
+		writeMolVec(fragments, &outfile);
+		delete ball_mol;
+		obMol.Clear();
+	}
+	
+	outfile.close();
+	ifs.close();
+}
