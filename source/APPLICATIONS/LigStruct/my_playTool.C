121c30a9be7b77b09a83e4996e63082e0354909a
Hans-Philipp Brachvogel <philipp-brachvogel@web.de>
2015-01-14 11:48:30 +0100

    added bridging bond identification and improved rotable bond assignment

--- source/APPLICATIONS/LigStruct/my_playTool.C
+++ source/APPLICATIONS/LigStruct/my_playTool.C
@@ -33,13 +33,18 @@ using namespace OpenBabel;
 using namespace BALL;
 using namespace std;
 
-bool isRotableBond(Bond &bnd)
+bool hasTrippleBond(Atom &atm)
 {
-	if( bnd.getOrder() > 1 || bnd.hasProperty("InRing"))
-		return false;
-	else
-		return true;
+	for(Atom::BondIterator bit = atm.beginBond(); +bit; ++bit)
+	{
+		if( bit->getOrder() == 3)
+			return true;
+	}
+	
+	return false;
 }
+
+
 
 bool isRigidAtom(Atom &atm)
 {
@@ -63,6 +68,54 @@ bool isRigidAtom(Atom &atm)
 			return false;
 	}
 	
+	return false;
+}
+
+
+bool isRotableBond(Bond &bnd)
+{
+	// ring bonds or not single bonds: not rotable
+	if( bnd.getOrder() > 1 || bnd.hasProperty("InRing"))
+		return false;
+	
+	// if any of the binding partners is a terminal atom: not rotable
+	if( bnd.getFirstAtom()->countBonds() ==1 || bnd.getSecondAtom()->countBonds() ==1)
+		return false;
+
+//	// The following 3uncommented 3 lines DO NOT correctly cover all cases:
+//	// considering a bond that comes from a tripple bond and goes to a rigid fragment 
+//	// as rigid is often correct BUT: the assignment is incorrect if the tripple bond
+//	// is surrounded by rigid groups.
+//	// BETTER: consider bonds before and after a tripple bond as one single rotable
+//	// bond. Rotating the pre- and aft-bond individually would not make any sense.
+//	// Not sure if I will implement this improved version.
+	
+//	if( hasTrippleBond( *bnd.getFirstAtom() ) || hasTrippleBond( *bnd.getSecondAtom() ))
+//		if( isRigidAtom(*bnd.getFirstAtom()) && isRigidAtom(*bnd.getSecondAtom()))
+//			return false;
+	
+	return true;
+}
+
+
+bool isBridgingBond(Bond &bnd)
+{
+	if( !isRotableBond(bnd) )
+		return false;
+	
+	bool atm1_fixed = isRigidAtom(* bnd.getFirstAtom() );
+	bool atm2_fixed = isRigidAtom(* bnd.getSecondAtom() );
+	
+	// rotable-bond between two fixed atoms: is a bridge
+	if( atm1_fixed && atm2_fixed)
+		return true;
+	
+	// rotable-bond between a fixed and flexible atom: is a bridge
+	if( (atm1_fixed && !atm2_fixed) ||
+			(!atm1_fixed && atm2_fixed) )
+		return true;
+	
+	// rotable-bond between two flexibles: NOT a bridge
 	return false;
 }
 
@@ -112,6 +165,24 @@ int main(int argc, char* argv[])
 		}
 	}
 	
+	cout<<endl;
+	AtomIterator ati;
+	Atom::BondIterator bit;
+	BALL_FOREACH_BOND(*ball_mol, ati, bit)
+	{
+		Atom* atm1 = bit->getFirstAtom();
+		Atom* atm2 = bit->getSecondAtom();
+		cout<<"Bond: "<<atm1->getElement().getSymbol()<<"-"<<atm2->getElement().getSymbol()<<" ";
+		
+		if( isBridgingBond(*bit) )
+			cout<<" - IS BRIDGE - ";
+		
+		if( isRotableBond(*bit) )
+			cout<<"is rotable"<<endl;
+		else
+			cout<<"is fixed"<<endl;
+	}
+
 	Log << "......done!"<<endl;
 }
 
