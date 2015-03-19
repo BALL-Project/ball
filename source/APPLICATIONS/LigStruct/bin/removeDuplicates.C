fcf6158be32ffc7b67f655d487b9c0b4e8fc6261
Hans-Philipp Brachvogel <philipp-brachvogel@web.de>
2015-03-19 12:11:02 +0100

    id check

--- source/APPLICATIONS/LigStruct/bin/removeDuplicates.C
+++ source/APPLICATIONS/LigStruct/bin/removeDuplicates.C
@@ -48,30 +48,28 @@ int main(int argc, char* argv[])
 		// some user info every 1000 molecules:
 		if( cnt % 1000 == 0)
 		{
-//			cout << "\r" << flush;
-			cout << "     filtered: "<< cnt<<" structures to "<<unique_ids.size()<<" uniques "<<endl;
+			cout << "\r" << flush;
+			cout << "     filtered: "<< cnt<<" structures to "<<unique_ids.size()<<" uniques ";
 		}
 		cnt++;
 		
 		// assign canonical aromatic labels:
 		AromaticityProcessor ar_pr;
-//		mol->apply(ar_pr);
+		mol->apply(ar_pr);
 		
 			UCK key_gen( *mol, true, 5 ); //generate canonical key
 			
 			String key = key_gen.getUCK(); //check if the key is new
-			//			cout<<"Key is: "<<key<<endl;
 			if( !unique_ids.has( key ))
 			{
-				unique_ids.insert( key_gen.getUCK());
-				
+				unique_ids.insert( key );
+				mol->setProperty("key", key);
 				outfile << *mol;
 			}
 			
 		// read the next molecule
 		mol = in_file.read();
 	}
-	
 	
 	in_file.close();
 	outfile.close();
