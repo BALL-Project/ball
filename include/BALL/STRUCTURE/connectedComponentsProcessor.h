4a3f44b84addf130a776bc9bf9c91b814f20faf7
Hans-Philipp Brachvogel <philipp-brachvogel@web.de>
2015-06-08 12:35:04 +0200

    updated connectedComponentProcessor using BOOST::DisjointSet

--- include/BALL/STRUCTURE/connectedComponentsProcessor.h
+++ include/BALL/STRUCTURE/connectedComponentsProcessor.h
@@ -5,17 +5,28 @@
 # include <BALL/CONCEPT/processor.h>
 #endif
 
-#ifndef BALL_KERNEL_SYSTEM_H
-# include <BALL/KERNEL/system.h>
+#ifndef BALL_KERNEL_MOLECULE_H
+#include <BALL/KERNEL/molecule.h>
 #endif
 
+#ifndef BOOST_PENDING_DISJOINT_SETS_HPP
+#include <boost/pending/disjoint_sets.hpp>
+#endif
 namespace BALL
 {
 	/** Connected Component Processor
 	    \ingroup StructureMiscellaneous
 	*/
 
-	/** Computation of the connected components of the molecular graph.
+	/** \brief Computation of the connected components of the molecular graph.
+	 * 
+	 * The processor can be applied to all instances of \link AtomContainer 
+	 * AtomContainer \endlink to group the contained atoms to their connected 
+	 * component. This is done according to the <b>intra bonds</b> assoziated 
+	 * with each atom. Thus this enables to identify sub-groups of <b>covalently 
+	 * connected atoms</b> in the atom container. This is especially useful when 
+	 * a molecule entry actually contains more than one independent molecule 
+	 * (like ions plus a drug molecule).
 	 */
 	class BALL_EXPORT ConnectedComponentsProcessor
 		: public UnaryProcessor<AtomContainer>
@@ -25,6 +36,7 @@ namespace BALL
 		 */
 		//@{
 		typedef std::vector<Atom*>      Component;
+		typedef std::vector<Molecule>   MolVec;
 		typedef	std::vector<Component>  ComponentVector;
 		//@}
 
@@ -62,37 +74,65 @@ namespace BALL
 
 		//@}
 
-		/** @name Accessors.
+		/** @name Accessors and convenience methods.
 		*/
 		//@{
 
-		/// Return the number of connected components found.
+		/// Returns the number of connected components found.
 		Size getNumberOfConnectedComponents();
-
-		/// Return the vector of connected components.
-		ComponentVector& getComponents();
-
-		/// Return the vector of connected components, const variant.
-		const ComponentVector& getComponents() const;
-
-		/** Split a System into its connected components.
-				
-				This function inserts one \link Molecule Molecule \endlink per connected component
-				into to_split. The Atoms will be re-parented and ripped out of their previous parents. 
-				<b>Note</b> that this destroys any further hierarchical organization (e.g. Chains, Fragments, 
-				...) that was previously present.
-
-				This function will apply the connected component computation itself, so it is not
-				necessary to call apply() previously.
-
-				@param to_split the System to split
-				@return Size - the number of connected components
+		
+		/** Returns a copy of the protected member "components_" in 'comp', if
+		 *  the processor was applied to any atom container before. comp then
+		 *  contains all connected molecules/components of that molecule.
+		 * 
+		 * The contained Component entries are vectors of atom pointers. Any 
+		 * previously contained element in comp will be discarded.
+		 * 
+		 * The result will again contain all atoms of the molecule the processor
+		 * was applied to before.
+		 * 
+		 * @param comp - will contain all found components
+		 *
 		*/
-		Size splitIntoMolecules(System& to_split);
-
+		void getComponents(ComponentVector &comp);
+		
+		/** Convenience method. Inserts the atoms of the largest molecule into
+		 * a input of type \link Molecule Molecule \endlink. <b>Note:</b> thus that 
+		 * input normally needs to be empty
+		 * (or at least free of other atom entries) otherwise the atoms are simply 
+		 * added to the pre-existing set. Properties of the original molecule
+		 * are NOT transfered to the output molecule.
+		 * 
+		 * @param result - atoms that belong to the larges contained molecule will 
+		 *								 be inserted into result
+		*/
+		void getLargestComponent(Molecule &result);
+		
+		/** Convenience method. Inserts elements of type \link Molecule Molecule 
+		 * \endlink that have at least a minimum  number of atoms into the input 
+		 * vector. Properties of the original molecule are NOT transfered to the 
+		 * output molecules.
+		 * 
+		 * @param result - \link Molecule Molecule \endlink's that have the minimum 
+		 *								 number of atoms will be inserted here
+		 * @param min - minimal amount of atoms that are needed for a molecule to 
+		 *							be selected as result
+		*/
+		void getMinAtomsComponents(MolVec &result, const int &min);
+		
+		/** Convenience method. Inserts all identified \link Molecule Molecule 
+		 * \endlink's into the input vector. Properties of the original molecule
+		 * are NOT transfered to the output molecules.
+		 * 
+		 * @param result - All real \link Molecule Molecule \endlink's will be 
+		 *								 inserted here
+		*/
+		void getAllComponents(MolVec& results);
 		//@}
 
 		protected:
+			typedef boost::disjoint_sets < int*, int*,
+							boost::find_with_full_path_compression > DisjointSet;
 			ComponentVector components_;
 	};
 }
