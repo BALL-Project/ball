a3b44e94f6bcb629af4d41ab84ee64d7e815cce6
Hans-Philipp Brachvogel <philipp-brachvogel@web.de>
2015-06-19 12:23:11 +0200

    updated the ligand3dgen includes.cmake

--- include/BALL/STRUCTURE/LIGAND3DGEN/sources.cmake
+++ include/BALL/STRUCTURE/LIGAND3DGEN/sources.cmake
@@ -1,25 +1,7 @@
-#########################################
-# by pbrach                             #
-#########################################
-
 ### list all filenames of the directory here ###
-SET(GROUP LIGAND3DGEN)
- 
-SET(DIRECTORY include/BALL/STRUCTURE/LIG3DGEN)
+SET(GROUP STRUCTURE/LIGAND3DGEN)
 
-FILE(GLOB HEADERS_LIST "${DIRECTORY}/*.h")
+FILE(GLOB HEADERS_LIST "include/BALL/${GROUP}/*.h" "include/BALL/${GROUP}/*.iC")
 
-### add full path to the filenames ###
-SET(HEADERS)
-FOREACH(i ${HEADERS_LIST})
-    ### make sure we do not have absolute paths flying around...
-    GET_FILENAME_COMPONENT(i ${i} NAME)
-    LIST(APPEND HEADERS ${DIRECTORY}/${i})
-ENDFOREACH()
+ADD_BALL_HEADERS("${GROUP}" "${HEADERS_LIST}")
 
-### pass source file list to the upper instance ###
-SET(BALL_headers ${BALL_headers} ${HEADERS})
-
-### source group definition ###
-STRING(REGEX REPLACE "/" "\\\\" S_GROUP ${GROUP})
-SOURCE_GROUP("Header Files\\\\${S_GROUP}" FILES ${HEADERS})
