4ff21cbef928ea592dc7076d99011e123c991712
pbrach <pbrach@jacket.maker>
2015-07-21 14:25:00 +0200

    Fixed the RingPerceptionProcessor: completely removed the Figueras Algorithm as it was shown in 2004 in 'Counterexamples in Chemical Ring Perception' by Berger, Flamm, Gleiss, Leydold and Stadler to be failing in certain cases, also the implementation simply did not generate the InRing properties. Furthermore the Balducci-Pearlman algorithm was fixed: the previous implementation was totally fine according to the description in the original publication, however 4 additional checks needed to be introduced to guarantee that certain edge pathes (BEEPs) and rings BEERs were valid to be further processed. The fixed version was tested on 100k structures from the crystallography open database and the results were compared to the Ring assignment from OpenBabel.

--- include/BALL/QSAR/ringPerceptionProcessor.h
+++ include/BALL/QSAR/ringPerceptionProcessor.h
@@ -8,243 +8,193 @@
 #define BALL_QSAR_RINGPERCEPTIONPROCESSOR_H
 
 #ifndef BALL_KERNEL_ATOMCONTAINER_H
-	#include <BALL/KERNEL/atomContainer.h>
+#include <BALL/KERNEL/atomContainer.h>
 #endif
 
 #ifndef BALL_STRUCTURE_SIMPLEMOLECULARGRAPH_H
-	#include <BALL/STRUCTURE/simpleMolecularGraph.h>
+#include <BALL/STRUCTURE/simpleMolecularGraph.h>
 #endif
 
 #ifndef BALL_DATATYPE_OPTIONS_H
-	#include <BALL/DATATYPE/options.h>
+#include <BALL/DATATYPE/options.h>
 #endif
+
 
 #include <stack>
 #include <vector>
 
 namespace BALL
 {
-	/**	Processor, which marks all atoms and bonds in a ring structure with the
-	    Composite Property "InRing".
+/**	Processor, which marks all atoms and bonds in a ring structure with the
+			Composite Property "InRing".
 			calculateSSSR() can also compute the number of rings found.
 			
-			The processor is an implementation of Figueras algorithm, described in:
-			J. Figueras, J. Chem. Inf. Comput. Sci., 1996, 36(5), 986-991
-
-			and the Balducci Pearlman algorithm described in:
+			The processor is an implementation of the Balducci Pearlman algorithm
+			described in:
 			Renzo Balducci, Robert S. Pearlman, J. Chem. Inf. Comput. Sci., 34:822-831, 1994
 	*/
-	class BALL_EXPORT RingPerceptionProcessor
+class BALL_EXPORT RingPerceptionProcessor
 		:	public UnaryProcessor<AtomContainer>
-	{
-		public:
+{
+public:
 
-			/** @name Constants and Definitions
-			*/
-			//@{
-			/// Option names
-			struct Option
-			{
-				/** The name of the algorithm which sould be used 
-				 *	for the ring perception. At the moment this can be 
-				 *	either Figueras algorithm or as default the Balducci/
-				 *	Pearlman algorithm. \par
-				 *	Valid values are <b> Figueras <b> for the Figueras and
-				 *	<b> Balducci <b> for the Balducci Pearlman algorithm (default)
-				 */
-				static const char* ALGORITHM_NAME;
-			};
-
-			/// default options for the ring perception
-			struct Default
-			{
-				/** Default name of the algorithm. This is set to the
-				 *	provable correct Balducci/Pearlman algorithm,
-				 *	<b> Balducci <b>
-				 */
-				static const char* ALGORITHM_NAME;
-			};
-
-
-			BALL_CREATE(RingPerceptionProcessor)
+	BALL_CREATE(RingPerceptionProcessor)
 	
-			/** @name Constrcutors and Destructors
+	/** @name Constrcutors and Destructors
 			*/
-			//@{
-			/** Default constructor
+	//@{
+	/** Default constructor
 			*/
-			RingPerceptionProcessor();
+	RingPerceptionProcessor();
 
-			/** Copy constructor
+	/** Copy constructor
 			*/
-			RingPerceptionProcessor(const RingPerceptionProcessor& rp);
+	RingPerceptionProcessor(const RingPerceptionProcessor& rp);
 
-			/** Destructor
+	/** Destructor
 			*/
-			virtual ~RingPerceptionProcessor();
+	virtual ~RingPerceptionProcessor();
 
-			//@}
-			/** @name Assignment
+	//@}
+	/** @name Assignment
 			*/
-			//@{
+	//@{
 
-			/** Assignment operator
+	/** Assignment operator
 			*/
-			RingPerceptionProcessor& operator = (const RingPerceptionProcessor& rp);
+	RingPerceptionProcessor& operator = (const RingPerceptionProcessor& rp);
 
-			//@}
-			/** @name Accessors
+	//@}
+	/** @name Accessors
 			*/
-			//@{
+	//@{
 
-			/** Method to get a smallest set of smallest rings (SSSR) from a molecule.
+	/** Method to get a smallest set of smallest rings (SSSR) from a molecule.
 					@param SSSR, vector of rings, where the rings are stored in vector<Atom*>
 					@param AtomContiner, from which AtomContainer the rings are to be percepted
 			*/
-			Size calculateSSSR(vector<vector<Atom*> >& sssr, AtomContainer& ac);
-			//@}
+	Size calculateSSSR(vector<vector<Atom*> >& sssr, AtomContainer& ac);
+	//@}
 
-			/** Getter which returns all the 3 - 6 membered rings, calculateSSSR with
+	/** Getter which returns all the 3 - 6 membered rings, calculateSSSR with
 			 *  the Balducci-Pearlman Algorithm (defalt) is needed prior this call.
 			 */
-			const vector<vector<Atom*> >& getAllSmallRings() const;
-			
-			/** @name Processor-related methods
-			*/
-			//@{
-			Processor::Result operator () (AtomContainer& ac);
-			//@}
-	
-			/** Method that finds all biconnected components, the algorithm is freely
-					adapted from a standard bcc (binary connected components) algorithm. 
-					Returns the number of bccs found.
-			*/
-			Size findAllBCC(std::vector<SimpleMolecularGraph*>& bcc, SimpleMolecularGraph& graph);
+	const vector<vector<Atom*> >& getAllSmallRings() const;
 
-			/*_ Options for the ring perception
+	/** @name Processor-related methods
 			*/
-			Options options;									
+	//@{
+	Processor::Result operator () (AtomContainer& ac);
+	//@}
 
-			/** sets the default options of this processor
-			*/
-			void setDefaultOptions();
+protected:
 
-		protected:
-	
-			/*_ @name Accessors
-			*/
-			//@{
-			/*_ Implementation of the Figueras algorithm. This algorithm has some build
-					in bugs, and should not be used any more. Hence as default the provable
-					correct Balducci/Pearlman algorithm is used.
-			*/
-			Size FiguerasAlgorithm_(vector<vector<Atom*> >& sssr, AtomContainer& ac);
+	// Balducci and Pearlman algorithm
+	struct PathMessage_;
 
-			/*_ Method that return the smallest ring which atom n participates
-					@param atom from which the search is started
-					@param ring set in which the found ring is stored (if any)
+	/*_ The tnode structure described in the paper
 			*/
-			Size getRing_(Atom* n, HashSet<Atom*>& ring_set);
+	struct TNode_
+	{
+		/// method to process the messages in the recieve buffer
+		void recieve();
 
-			/*_ A bond, which when deleted leads to the smallest ring
-					is deleted. 
-					@param ring_set atoms to test
-					@param ac the atom container the algorithm works on
+		/// method to process the messages in the send buffer
+		void send();
+
+		bool haveZeroIntersection(BitVector& beep1, BitVector& beep2);
+
+		bool haveSingleIntersection(BitVector& beep1, BitVector& beep2);
+
+		bool nodeIsNew(BitVector& beep, NodeItem<Index, Index>* node);
+
+		/// the recieve buffer, where messages are stored in
+		std::vector<PathMessage_> recieve_buffer;
+
+		/// the send buffer, where messages are stored in
+		std::vector<PathMessage_> send_buffer;
+	};
+
+	/*_ The pathMsg structure described in the paper
 			*/
-			void checkEdges_(HashSet<Atom*>& ring_set, AtomContainer& ac);
-	
-			/*_ Method that finds all biconnected components, the algorithm is freely 
-					adapted from a standard bcc algorithm.
-			*/
-			Size findAllBCC_(std::vector<SimpleMolecularGraph*>& bcc, SimpleMolecularGraph& graph);
-	
-			/*_ recursive function that finds bccs
-			*/
-			void DFSBCC_( std::vector<SimpleMolecularGraph*>& bccs, Size dfbi, 
-										HashMap<NodeItem<Index, Index>*, Size> DFBIndex, 
-										NodeItem<Index, Index>* v);
-																		
-			HashSet<NodeItem<Index, Index>* > visited_;
-			HashSet<EdgeItem<Index, Index>* > visited_bonds_;
-			HashMap<NodeItem<Index, Index>* , Size> P_;
-			HashMap<NodeItem<Index, Index>*, NodeItem<Index, Index>* > parents_;
-			std::stack<EdgeItem<Index, Index>* > BCC_;
-	
-	
-			// Balducci and Pearlman algorithm
-			struct PathMessage_;
-			
-			/*_ The tnode structure described in the paper
-			*/
-			struct TNode_
-			{
-				/// method to process the messages in the recieve buffer
-				void recieve();
-				
-				/// method to process the messages in the send buffer
-				void send();
-	
-				/// the recieve buffer, where messages are stored in
-				std::vector<PathMessage_> recieve_buffer;
-	
-				/// the send buffer, where messages are stored in
-				std::vector<PathMessage_> send_buffer;
-			};
+	struct PathMessage_
+	{
+		void push(EdgeItem<Index, Index>* bond, TNode_* node);
 		
-			/*_ The pathMsg structure described in the paper
-			*/
-			struct PathMessage_
-			{
-				void push(EdgeItem<Index, Index>* bond, TNode_* node);
-		
-				// path of the message
-				BitVector beep;
-				
-				/// pointer to the first node this message was sent from
-				TNode_* nfirst;
-			
-				// pointer to the last node of the messages' path
-				TNode_* nlast;
+		// path of the message
+		BitVector beep;
 
-				/// pointer to the first edge of the message path
-				EdgeItem<Index, Index>* efirst;
-			};
+		/// pointer to the first node this message was sent from
+		TNode_* nfirst;
 
-			/// mapping for internal TNode structure and the nodes of the molecular graph
-			static HashMap<TNode_*, NodeItem<Index, Index>* > tnode_to_atom_;
-			static HashMap<NodeItem<Index, Index>* , TNode_*> atom_to_tnode_;
-			
-			/// mapping for the path representation as bitvectors
-			static HashMap<EdgeItem<Index, Index>*, Size> bond_to_index_;
-			static HashMap<Size, EdgeItem<Index, Index>*> index_to_bond_;
-			
-			/// the SSSR detected by the algorithm
-			static std::vector<BitVector> rings_;
-			
-			/// the matrix for the independency tests
-			static std::vector<BitVector> matrix_;
-			
-			/// the rings of the ith phase, which are to be forwarded to the ring selector
-			static std::vector<BitVector> forwarded_rings_;
-			
-			/// rings (beer) which have already been tested
-			static std::vector<BitVector> tested_beers_;
+		// pointer to the last node of the messages' path
+		TNode_* nlast;
 
-			/// contains all 3 to 6 membered rings after the procedure of the Balducci-Pearlman algorithm
-			static std::vector<std::vector<Atom*> > all_small_rings_;
+		/// pointer to the first edge of the message path
+		EdgeItem<Index, Index>* efirst;
+	};
 
-			/// contains all 3 to 6 membered rings as beers
-			static std::vector<BitVector> all_small_beers_;
-			
-			/*_ function that gets a binary edge-encoded ring as a BitVector
+	/// mapping for internal TNode structure and the nodes of the molecular graph
+	static HashMap<TNode_*, NodeItem<Index, Index>* > tnode_to_atom_;
+	static HashMap<NodeItem<Index, Index>* , TNode_*> atom_to_tnode_;
+
+	/// mapping for the path representation as bitvectors
+	static HashMap<EdgeItem<Index, Index>*, Size> bond_to_index_;
+	static HashMap<Size, EdgeItem<Index, Index>*> index_to_bond_;
+
+	/// the SSSR detected by the algorithm
+	static std::vector<BitVector> rings_;
+
+	/// the matrix for the independency tests
+	static std::vector<BitVector> matrix_;
+
+	/// the rings of the ith phase, which are to be forwarded to the ring selector
+	static std::vector<BitVector> forwarded_rings_;
+
+	/// rings (beer) which have already been tested
+	static std::vector<BitVector> tested_beers_;
+
+	/// contains all 3 to 6 membered rings after the procedure of the Balducci-Pearlman algorithm
+	static std::vector<std::vector<Atom*> > all_small_rings_;
+
+	/// contains all 3 to 6 membered rings as beers
+	static std::vector<BitVector> all_small_beers_;
+
+	/*_ function that gets a binary edge-encoded ring as a BitVector
 					and adds it to the ringset if its linearly independend
 			*/
-			static void BalducciPearlmanRingSelector_(BitVector bit_vector);
+	static void BalducciPearlmanRingSelector_(BitVector bit_vector);
 	
-			/*_ Implementation of the Balducci/Pearlman algorithm 
+	/*_ Implementation of the Balducci/Pearlman algorithm
 			*/
-			Size BalducciPearlmanAlgorithm_(std::vector<std::vector<Atom*> >& sssr, SimpleMolecularGraph& graph);
-	};
+	Size BalducciPearlmanAlgorithm_(std::vector<std::vector<Atom*> >& sssr, SimpleMolecularGraph& graph);
+};
+
+
+/**
+ * RingProcessor Exception
+ *
+ * It is possible that due to invalid input or the failure of the
+ * algorithim, we can not assign the SSSR correctly. In such a case
+ * we throw a RingProcessorException
+ */
+namespace Exception
+{
+class BALL_EXPORT RingProcessorException: public GeneralException
+{
+public:
+	RingProcessorException(const char* file, int line)
+		: GeneralException(file, line, "RingProcessorException", "")
+	{
+		message_ = "\nCould not find a valid set of smallest rings for an input molecule.\n\n";
+		message_+= "Either the input molecule was not valid (e.g.: not a single connected component)\n";
+		message_+= "or the algorithm failed on this special topology.";
+
+		globalHandler.setMessage(message_);
+	}
+};
+} // namespace Exception
+
 } // namespace BALL
 
 #endif // BALL_QSAR_RINGPERCEPTIONPROCESSOR_H
