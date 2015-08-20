092efb684c7b6f175a55db28855c49e596017e64
Thomas Kemmer <thomas@binaryden.de>
2015-08-20 15:08:48 +0200

    PresentaBALL makeover (German version)

---
+++ data/HTMLBasedInterface/scripts/toggleanswers.js
@@ -0,0 +1,7 @@
+function showAnswer(answer_id) {
+	document.getElementById(answer_id).style.visibility="visible";
+}
+
+function hideAnswer(answer_id) {
+	document.getElementById(answer_id).style.visibility="hidden";
+}
