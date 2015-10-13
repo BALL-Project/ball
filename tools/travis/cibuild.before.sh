3846394911cdae236b72d63cff6e709a3b4d5ec8
pthiel <thiel@informatik.uni-tuebingen.de>
2015-10-13 12:39:57 +0200

    Fixing ...

--- tools/travis/cibuild.before.sh
+++ tools/travis/cibuild.before.sh
@@ -3,6 +3,7 @@ git clone https://github.com/BALL-Project/ball_contrib.git
 pushd ball_contrib
 git checkout tarball_revisited
 
+mkdir build
 pushd build
 cmake .. -DPACKAGES="eigen3;sip;tbb;fftw3;openbabel" -DTHREADS=4
 make
