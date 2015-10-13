35b5301b754aebef848089f2fd75a93ef7181a92
pthiel <thiel@informatik.uni-tuebingen.de>
2015-10-13 14:06:04 +0200

    Fixing ...

--- tools/travis/cibuild.before.sh
+++ tools/travis/cibuild.before.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y git cmake gcc clang build-essential
+#sudo apt-get -y upgrade
+sudo apt-get -y install git cmake gcc clang build-essential
 sudo apt-get -y install flex bison libpython2.7-dev qt5-default libqt5webkit5-dev
 
 git clone https://github.com/BALL-Project/ball_contrib.git
