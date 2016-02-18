72e7ed9d394ca04d848edb45f9e98cc1b10ecbb7
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2016-02-18 22:24:37 +0100

    Fix FindCTDopts.cmake if CTDopts is not installed.
    String variables should be quoted to avoid cases where an empty variable
    is treated as if not present.

--- cmake/FindCTDopts.cmake
+++ cmake/FindCTDopts.cmake
@@ -10,7 +10,7 @@ IF (PYTHONINTERP_FOUND)
 	# CTDopts is a module, so we don't need to find a script, rather, just make sure it is properly installed
 	FIND_FILE(_find_ctdopts_py FindCTDopts.py PATHS ${CMAKE_MODULE_PATH})
 	EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} ${_find_ctdopts_py} OUTPUT_VARIABLE stdout ERROR_VARIABLE stderr RESULT_VARIABLE exit_code OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_STRIP_TRAILING_WHITESPACE)
-	STRING(STRIP ${stdout} stdout)
+	STRING(STRIP "${stdout}" stdout)
 	IF((stdout STREQUAL "CTDopts seems to be working.") AND (exit_code EQUAL 0))
     	SET(CTDOPTS_MODULE TRUE)
 	ELSE()
