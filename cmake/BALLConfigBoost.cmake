3cecd3f7a28734d30acd0b5f83c0d3c8136b6821
pthiel <thiel@informatik.uni-tuebingen.de>
2016-05-03 13:47:08 +0200

    Small changes in BALLConfigBoost.cmake

--- cmake/BALLConfigBoost.cmake
+++ cmake/BALLConfigBoost.cmake
@@ -1,7 +1,7 @@
 # Find and configure Boost library
 
 # Mandatory boost components
-SET(BALL_BOOST_COMPONENTS_MANDATORY
+SET(BALL_BOOST_COMPONENTS
 	bzip2
 	chrono
 	date_time
@@ -13,11 +13,8 @@ SET(BALL_BOOST_COMPONENTS_MANDATORY
 	zlib
 )
 
-# Add optional boost components
-SET(BALL_BOOST_COMPONENTS ${BALL_BOOST_COMPONENTS_MANDATORY} regex)
-
 # Additional Boost versions that should be included by CMake
-# CMake 2.8.12, which is minimum for BALL, already knowns versions 1.33 up to 1.56
+# CMake 2.8.12, which is minimum for configuring BALL, already knowns versions 1.33 up to 1.56
 SET(Boost_ADDITIONAL_VERSIONS "1.60" "1.59" "1.58" "1.57")
 
 # Detailed messaging in case of failures
@@ -26,6 +23,7 @@ SET(Boost_DETAILED_FAILURE_MSG ON)
 # Invoke CMake FindBoost module
 FIND_PACKAGE(Boost 1.55 REQUIRED COMPONENTS ${BALL_BOOST_COMPONENTS})
 
+# Required definitions on Windows machines
 IF(WIN32)
 	ADD_DEFINITIONS(-DBOOST_ALL_NO_LIB)
 	ADD_DEFINITIONS(-DBOOST_ALL_DYN_LINK)
