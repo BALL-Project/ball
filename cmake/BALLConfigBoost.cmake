7bf8e1636884416da1b31ae2e7f030112ce0f036
pthiel <thiel@informatik.uni-tuebingen.de>
2016-12-13 17:24:57 +0100

    Added boost bzip2 and zlib for Win32

--- cmake/BALLConfigBoost.cmake
+++ cmake/BALLConfigBoost.cmake
@@ -11,6 +11,14 @@ SET(BALL_BOOST_COMPONENTS
 	thread
 )
 
+
+# Required libraries and definitions on Windows OS
+IF(WIN32)
+	LIST(APPEND BALL_BOOST_COMPONENTS bzip2 zlib)
+	ADD_DEFINITIONS(-DBOOST_ALL_NO_LIB)
+	ADD_DEFINITIONS(-DBOOST_ALL_DYN_LINK)
+ENDIF()
+
 # Additional Boost versions that should be included by CMake
 # CMake 2.8.12, which is minimum for configuring BALL, already knowns versions 1.33 up to 1.56
 SET(Boost_ADDITIONAL_VERSIONS "1.60" "1.59" "1.58" "1.57")
@@ -20,9 +28,3 @@ SET(Boost_DETAILED_FAILURE_MSG ON)
 
 # Invoke CMake FindBoost module
 FIND_PACKAGE(Boost 1.55 REQUIRED COMPONENTS ${BALL_BOOST_COMPONENTS})
-
-# Required definitions on Windows machines
-IF(WIN32)
-	ADD_DEFINITIONS(-DBOOST_ALL_NO_LIB)
-	ADD_DEFINITIONS(-DBOOST_ALL_DYN_LINK)
-ENDIF()
