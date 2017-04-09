ec62decbcc90e80b570f8aaf0a7fd885885b7c2e
Thomas Kemmer <thomas@binaryden.de>
2017-04-09 23:57:03 +0200

    [BALL] PyInterpreter: Split interpreter interface from interpreter This will make it easier to replace the current interpreter kernel.

---
+++ include/BALL/PYTHON/legacyKernel.h
@@ -0,0 +1,35 @@
+#ifndef BALL_PYTHON_LEGACYKERNEL_H
+#define BALL_PYTHON_LEGACYKERNEL_H
+
+#include <BALL/PYTHON/pyKernel.h>
+
+namespace BALL
+{
+	class LegacyKernel : public PyKernel
+	{
+		public:
+			LegacyKernel();
+			~LegacyKernel();
+
+			bool isStarted() const override;
+			std::string getErrorMessage() const override;
+
+			std::pair<bool, std::string> run(std::string str) override;
+			bool runFile(std::string filename) override;
+			bool execute(const std::string& module, const std::string& func, const KeyValArgs& args) override;
+
+		private:
+			/**
+			 * Imports the Python module with the given name.
+			 *
+			 * @param name Python module
+			 * @return new reference to the imported module, or nullptr in case of an error
+			 */
+			PyObject* loadModule(const std::string& name);
+
+			PyObject* cio_;
+			PyObject* context_;
+	};
+}
+
+#endif // BALL_PYTHON_LEGACYKERNEL_H
--- source/PYTHON/pyInterpreter.C
+++ source/PYTHON/pyInterpreter.C
@@ -3,134 +3,51 @@
 //
 
 #include <BALL/PYTHON/pyInterpreter.h>
-#include <BALL/PYTHON/BALLPythonConfig.h>
 
 #include <BALL/FORMAT/lineBasedFile.h>
+#include <BALL/PYTHON/BALLPythonConfig.h>
+#include <BALL/PYTHON/legacyKernel.h>
 
-#include <Python.h>
+using std::string;
 
 namespace BALL
 {
-
-	String error_message_;
-	PyObject* context_;
-
+	PyKernel* PyInterpreter::kernel_ = nullptr;
 	String PyInterpreter::start_log_;
-	bool PyInterpreter::valid_ = false;
-
-	PyInterpreter::PyInterpreter()
-	{
-	}
-
-	PyObject* runSingleString_(const String& str, int mode)
-	{
-		// clear previous errors
-		PyErr_Clear();
-		error_message_ = "";
-		
-		// run the string through the interpreter
-		PyObject* result = PyRun_String(const_cast<char*>(str.c_str()), mode, context_, context_);
-		if (PyErr_Occurred())
-		{
-			PyObject* type;
-			PyObject* value;
-			PyObject* range;
-
-			char* message;
-			error_message_ = "ERROR: ";
-			PyErr_Fetch(&type, &value, &range);
-			if (PyArg_Parse(value, "s", &message))
-			{
-				error_message_ += message;
-			}
-			else
-			{	
-				error_message_ += " (error message could not be parsed)";
-			}
-
-			PyErr_Print();
-
-			error_message_ += "\n";
-
-			return 0;
-		}
-		
-		return result;
-	}
-
-	bool PyInterpreter::isInitialized()
-	{
-		return Py_IsInitialized();
-	}
+	PyInterpreter::PathStrings PyInterpreter::sys_path_;
 
 	void PyInterpreter::finalize()
 	{
-		if (isInitialized())
-		{
-			// TODO: for some reason, finalize crashes on Windows, at least if no python
-			//       module has been found. We need to fix this correctly.
-#ifndef BALL_COMPILER_MSVC
-			Py_Finalize();
-#endif
-		}
-
-		valid_ = false;
+		if(kernel_) delete kernel_;
+		kernel_ = nullptr;
 	}
 
 	void PyInterpreter::initialize()
 	{
-		valid_ = false;
+		// finalize the interpreter if it is already running
+		if(kernel_) finalize();
+
+		kernel_ = new LegacyKernel();
+		bool ok;
 		start_log_.clear();
 
-		// finalize the interpreter if it is already running
-		if (Py_IsInitialized())
-		{
-			Py_Finalize();
-		}
-
-		// on some platforms, we have to point the system to our own python packages
-#ifdef BALL_OS_WINDOWS
-		// TODO: decide through a registry key which python version to use
-		Py_SetProgramName(BALL_PATH "/bin/python.exe");
-#endif
-
-		// initialize the interpreter
-		Py_Initialize();
-
-		// get a borrowed reference to the main - module
-		PyObject* main_module = PyImport_AddModule("__main__");
-
-		if (main_module == 0) 
-		{
-			Log.error() << "Python intialization failed: could not add \"__main__\" module! No Python support available." << std::endl;
-			return;
-		}
-
-		// and create a valid context for our further Python - calls
-		context_ = PyModule_GetDict(main_module);
-	
-		if (context_ == 0) 
-		{
-			Log.error() << "Could not get valid context for Python module \"__main__\"! No Python support available." << std::endl;
-			return;
-		}
-
 		// import sys
-		runSingleString_("import sys", Py_single_input);
-		start_log_ += error_message_;
+		run("import sys", ok);
+		if (!ok) start_log_ += kernel_->getErrorMessage();
 
 		// Add the BALL library path to the Python search path
 		// to make sure Python can find the BALL extensions.
 		Path p;
 
 #ifndef BALL_COMPILER_MSVC
-		runSingleString_("sys.path.append(\"" BALL_PATH "/lib/" "\")", Py_single_input);
+		run("sys.path.append(\"" BALL_PATH "/lib/" "\")", ok);
+		if (!ok) start_log_ += kernel_->getErrorMessage();
 
 #ifdef BALL_OS_DARWIN
-	// on MacOS, try to find the python packages in the frameworks directory of our bundle
-	String site_path = String(p.find("../Frameworks"));
-	if (site_path != "")
-		sys_path_.push_back(site_path);
+		// on MacOS, try to find the python packages in the frameworks directory of our bundle
+		String site_path = String(p.find("../Frameworks"));
+		if (site_path != "")
+			sys_path_.push_back(site_path);
 #endif
 
 #else
@@ -153,247 +70,81 @@ namespace BALL
 		String python_startup_path = String(p.find("startup.py")).before("startup.py");
 		python_startup_path.trim();
 		while (python_startup_path.substitute("\\", "/") != String::EndPos) {};
-
-		if (python_startup_path != "")
-			sys_path_.push_back(python_startup_path);
-
-		start_log_ += error_message_;
+		if (python_startup_path != "") sys_path_.push_back(python_startup_path);
 
 		// Add additional paths (user-defined) to the end of the search path.
-		// 
-		std::vector<String>::const_iterator it(sys_path_.begin());
-		for (; it != sys_path_.end(); ++it)
+		for (const auto& s: sys_path_)
 		{
-			runSingleString_("sys.path.append(\"" + *it + "\")", Py_single_input);
-			start_log_ += error_message_;
+			run(string("sys.path.append(\"") + s.c_str() + "\")", ok);
+			if (!ok) start_log_ += kernel_->getErrorMessage();
 		}
-
-		// import the site module, and the one required for the output redirection
-		runSingleString_("import site, cStringIO", Py_single_input);
-		start_log_ += error_message_;
-
-		PyObject *sip_module = PyImport_ImportModule("sip");
-		if (sip_module == 0) 
-		{
-			Log.error() << "Could not import Python module \"sip\"! No Python support available." << std::endl;
-			return;
-		}
-
-		Py_DECREF(sip_module);
 
 		// make sure that we found the correct sip version
-		valid_ = true;
+		run("import sip", ok);
+		if (!ok) start_log_ += kernel_->getErrorMessage();
+		auto module_sip_version = run("sip.SIP_VERSION", ok).trim().toUnsignedInt();
+		if (!ok) start_log_ += kernel_->getErrorMessage();
+		auto module_sip_version_str = run("sip.SIP_VERSION_STR", ok).trim();
+		if (!ok) start_log_ += kernel_->getErrorMessage();
 
-		run("import sip", valid_);
-		const unsigned int module_sip_version = run("print(sip.SIP_VERSION)", valid_).trim().toUnsignedInt();
-		const String module_sip_version_str = run("print(sip.SIP_VERSION_STR)", valid_).trim();
-
-		const unsigned int module_major_minor = module_sip_version & 0xFFFFFF00;
-		const unsigned int module_bugfix      = module_sip_version & 0x000000FF;
-		const unsigned int ball_major_minor   = BALL_SIP_VERSION   & 0xFFFFFF00;
-		const unsigned int ball_bugfix        = BALL_SIP_VERSION   & 0x000000FF;
+		auto module_major_minor = module_sip_version & 0xFFFFFF00;
+		auto module_bugfix      = module_sip_version & 0x000000FF;
+		unsigned ball_major_minor   = BALL_SIP_VERSION & 0xFFFFFF00;
+		unsigned ball_bugfix        = BALL_SIP_VERSION & 0x000000FF;
 
 		if ((module_major_minor != ball_major_minor) || (module_bugfix < ball_bugfix))
 		{
-			String sip_module_path = run("print(sip)", valid_).trim();
-
-			error_message_ += "ERROR: Version of imported sip module is not compatible with the version BALL was built against.\n"
-			                  "If BALL was compiled using SIP version x.y.z then it can be used with any SIP version x.y.z' where "
-												"z' >= z.\n";
-			error_message_ += "Got (from " + sip_module_path + ") " + module_sip_version_str + "; Expected " + BALL_SIP_VERSION_STR +"\n";
-
-			Log.error() << error_message_ << std::endl;
-
-			start_log_ += error_message_;
-
-			valid_ = false;
-
+			auto sip_module_path = run("sip", ok).trim();
+			if (!ok) start_log_ += kernel_->getErrorMessage();
+			start_log_ += "ERROR: Version of imported sip module is not compatible with the version BALL "
+			              "was built against.\n"
+			              "If BALL was compiled using SIP version x.y.z then it can be used with any SIP "
+			              "version x.y.z' where 'z' >= z.\n"
+			              "Got (from " + sip_module_path + ") " + module_sip_version_str + "; Expected " +
+			              BALL_SIP_VERSION_STR +"\n";
+			finalize();
 			return;
 		}
 
 		// import the BALL module
-		start_log_ += run("from BALL import *", valid_);
-
-		if (!valid_) 
+		run("from BALL import *", ok);
+		if (!ok)
 		{
+			start_log_ += kernel_->getErrorMessage();
 			Log.error() << "Could not import the BALL library! No Python support available." << std::endl;
+			finalize();
 		}
-	}
-
-	PyObject* loadModule_(const QString& name)
-	{
-		PyObject* module_dict = PyImport_GetModuleDict();
-
-		if(!module_dict)
-		{
-			Log.error() << "Could not obtain module dictionary" << std::endl;
-			return 0;
-		}
-
-		PyObject* mod_name = PyString_FromString(name.toLatin1().data());
-
-		PyObject* module = 0;
-
-		if(PyDict_Contains(module_dict, mod_name))
-		{
-			module = PyDict_GetItem(module_dict, mod_name);
-		}
-		else
-		{
-			//This could leak the imported module. Need to check...
-			module = PyImport_ImportModule(name.toLatin1().data());
-		}
-
-		Py_XDECREF(mod_name);
-
-
-		if(!module || PyErr_Occurred())
-		{
-			PyErr_Print();
-			return 0;
-		}
-
-		return module;
-	}
-
-	bool PyInterpreter::execute(const QString& module, const QString& func_name, const QList<QPair<QString, QString> >& params)
-	{
-		PyObject* mod = loadModule_(module);
-
-		if(mod == 0)
-		{
-			Log.error() << "Could not load module " << module.toStdString() << std::endl;
-			return false;
-		}
-
-		PyObject* func = PyObject_GetAttrString(mod, func_name.toLatin1().data());
-
-		if(!func || !PyCallable_Check(func))
-		{
-			PyErr_Print();
-
-			return false;
-		}
-
-		PyObject* dict = PyDict_New();
-
-		if(!dict)
-		{
-			Log.error() << "Error: Could not create named arguments dictionary" << std::endl;
-			return false;
-		}
-
-		for(QList<QPair<QString, QString> >::const_iterator it = params.begin(); it != params.end(); ++it)
-		{
-			if(it->first == "action" || it->first == "module" || it->first == "method")
-			{
-				continue;
-			}
-
-			PyObject* val = PyString_FromString(it->second.toLatin1().data());
-
-			if(!val)
-			{
-				Log.error() << "Could not create parameter" << it->first.toStdString() << "=" << it->second.toStdString() << " Skipping." << std::endl;
-				continue;
-			}
-
-			PyDict_SetItemString(dict, it->first.toLatin1().data(), val);
-		}
-
-		Py_DECREF(dict);
-
-
-		PyObject* dummy = PyTuple_New(0);
-
-		if(!dummy)
-		{
-			Log.error() << "Could not allocate dummy tuple" << std::endl;
-			return false;
-		}
-
-		PyObject* res = PyObject_Call(func, dummy, dict);
-
-		Py_XDECREF(res);
-		Py_DECREF(dummy);
-
-		return true;
 	}
 
 	String PyInterpreter::run(const String& s, bool& state)
 	{
-		if (!valid_) return "";
-
 		state = false;
-		if (runSingleString_("CIO = cStringIO.StringIO()", Py_single_input) == 0 ||
-		    runSingleString_("stdout=sys.stdout", Py_single_input) == 0 ||
-		    runSingleString_("stderr=sys.stderr", Py_single_input) == 0 ||
-		    runSingleString_("sys.stdout=CIO", Py_single_input) == 0 ||
-		    runSingleString_("sys.stderr=CIO", Py_single_input) == 0) 
-		{
-			return error_message_;
-		}
-		
-		state = (runSingleString_(s, Py_single_input) != 0);
+		if (!isInitialized()) return "";
 
-		if (!state)
-		{
-			// save the error message, because resetting stdout would overwrite it
-			String result = error_message_;
-
-			runSingleString_("sys.stdout=stdout", Py_single_input);
-			runSingleString_("sys.stderr=stderr", Py_single_input);
-
-			return result;
-		}
-
-		// retrieve output
-		char* buf = 0;
-		PyObject* result = runSingleString_("str(CIO.getvalue())", Py_eval_input);
-		if (result != 0)
-		{
-			PyArg_Parse(result, "s", &buf);
-		}
-
-		runSingleString_("sys.stdout=stdout", Py_single_input);
-		runSingleString_("sys.stderr=stderr", Py_single_input);
-
-		return String(buf);
+		String ret;
+		std::tie(state, ret) = kernel_->run(s.c_str());
+		return ret;
 	}
 
 	String PyInterpreter::runFile(const String& filename)
 	{
-		if (!valid_) return "";
+		if (!isInitialized()) return "";
 
-		FILE* file = fopen(filename.c_str(), "r");
-
-		if(!file)
-		{
-			throw Exception::FileNotFound(__FILE__, __LINE__, filename);
-		}
-
-		PyErr_Clear();
-		PyRun_SimpleFileEx(file, filename.c_str(), true);
-
-		if (PyErr_Occurred())
-		{
-			std::cout << "Error occurred while executing " << filename << "\nPrinting Traceback:" << std::endl;
-			PyErr_Print();
-		}
-
+		kernel_->runFile(filename.c_str());
 		return "";
 	}
 
-	void PyInterpreter::setSysPath(const PathStrings& path_strings)
+	bool PyInterpreter::execute(const QString& module, const QString& func_name, const QList<QPair<QString, QString> >& params)
 	{
-		sys_path_ = path_strings;
-	}
-	
-	const PyInterpreter::PathStrings& PyInterpreter::getSysPath()
-	{
-		return sys_path_;
-	}
+		if (!isInitialized()) return false;
 
-	PyInterpreter::PathStrings PyInterpreter::sys_path_;
+		PyKernel::KeyValArgs args;
+		for (const auto& pair: params)
+		{
+			if (pair.first == "action" || pair.first == "module" || pair.first == "method") continue;
+			args[pair.first.toStdString()] = pair.second.toStdString();
+		}
 
+		return kernel_->execute(module.toStdString(), func_name.toStdString(), args);
+	}
 } // namespace BALL
