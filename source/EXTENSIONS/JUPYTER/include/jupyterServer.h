99392c5e9154f30e0f04ea9967f5ad6a82d2e486
Thomas Kemmer <thomas@binaryden.de>
2017-05-03 12:37:02 +0200

    [Jupyter] Define JupyterServer {g,s}etters in header

--- source/EXTENSIONS/JUPYTER/include/jupyterServer.h
+++ source/EXTENSIONS/JUPYTER/include/jupyterServer.h
@@ -15,14 +15,15 @@ namespace BALL
 				JupyterServer(QObject* parent, const QString& exe_path, unsigned int port, bool debug, const QString& nbdir);
 				virtual ~JupyterServer();
 
-				const QString& getExePath();
-				void setExePath(const QString& exe_path);
-				unsigned int getPort();
-				void setPort(unsigned int port);
-				bool getDebug();
-				void setDebug(bool debug);
-				const QString& getNbdir();
-				void setNbdir(const QString& nbdir);
+				const QString& getExePath() { return exe_path_; }
+				unsigned int getPort() { return port_; }
+				bool getDebug() { return debug_; }
+				const QString& getNbdir() { return nbdir_; }
+
+				void setExePath(const QString& exe_path) { exe_path_ = exe_path; }
+				void setPort(unsigned int port) { port_ = port; }
+				void setDebug(bool debug) { debug_ = debug; }
+				void setNbdir(const QString& nbdir) { nbdir_ = nbdir; }
 
 				bool isRunning();
 				void start();
