50f448417ca78e8146d8433f71b5a4b1def087a7
Thomas Kemmer <thomas@binaryden.de>
2017-07-27 17:22:19 +0200

    [BALL] Fix Python bindings for UnaryProcessor derivatives Many sip classes have used incorrect base classes, preventing the BALLCore module from being built with sip 4.19+ (partially fixes #606).

---
+++ include/BALL/PYTHON/pyUnaryProcessors.h
@@ -0,0 +1,20 @@
+#ifndef BALL_PYTHON_PYPROCESSOR_H
+#define BALL_PYTHON_PYPROCESSOR_H
+
+#include <BALL/KERNEL/atomContainer.h>
+
+namespace BALL
+{
+	using AtomContainerProcessor = UnaryProcessor<AtomContainer>;
+	using AtomProcessor = UnaryProcessor<Atom>;
+	using BondProcessor = UnaryProcessor<Bond>;
+	using ChainProcessor = UnaryProcessor<Chain>;
+	using CompositeProcessor = UnaryProcessor<Composite>;
+	using FragmentProcessor = UnaryProcessor<Fragment>;
+	using ResidueProcessor = UnaryProcessor<Residue>;
+
+	using ConstAtomProcessor = ConstUnaryProcessor<Atom>;
+	using ConstCompositeProcessor = ConstUnaryProcessor<Composite>;
+}
+
+#endif //BALL_PYTHON_PYPROCESSOR_H
