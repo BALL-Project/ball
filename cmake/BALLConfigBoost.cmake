f07a74fd000709e7f0ecac981a96c7f39430862a
Thomas Kemmer <thomas@binaryden.de>
2017-11-10 18:34:01 +0100

    [BALL] Update Boost_ADDITIONAL_VERSIONS

--- cmake/BALLConfigBoost.cmake
+++ cmake/BALLConfigBoost.cmake
@@ -27,8 +27,9 @@ IF(WIN32)
 ENDIF()
 
 # Additional Boost versions that should be included by CMake
-# CMake 2.8.12, which is minimum for configuring BALL, already knowns versions 1.33 up to 1.56
-SET(Boost_ADDITIONAL_VERSIONS "1.60" "1.59" "1.58" "1.57")
+# CMake 3.1, which is minimum for configuring BALL, already knowns versions 1.33 up to 1.56
+SET(Boost_ADDITIONAL_VERSIONS "1.65.0" "1.65" "1.64.0" "1.64" "1.63.0" "1.63" "1.62.0" "1.62" "1.61.0" "1.61"
+                              "1.60.0" "1.60" "1.59.0" "1.59" "1.58.0" "1.58" "1.57.0" "1.57")
 
 # Detailed messaging in case of failures
 SET(Boost_DETAILED_FAILURE_MSG ON)
