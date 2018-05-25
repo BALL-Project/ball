d6052ffcaa9bc70c1eca5b0ca392341188a9d436
Thomas Kemmer <thomas@binaryden.de>
2018-05-25 14:08:29 +0200

    [VIEW] Remove unused GLEW include

--- source/VIEW/RENDERING/glRenderWindow.C
+++ source/VIEW/RENDERING/glRenderWindow.C
@@ -3,9 +3,6 @@
 //
 
 #include <BALL/VIEW/KERNEL/common.h>
-#ifdef BALL_HAS_GLEW
-#	include <GL/glew.h>
-#endif
 
 #include <BALL/VIEW/RENDERING/glRenderWindow.h>
 #include <BALL/VIEW/WIDGETS/scene.h>
